4d6cdfc766a3211bc7a42651f6e1e8a07387cd11
sonicdcer <alejandro.asenjo88@gmail.com>
2022-10-18 16:11:57 -0300

    func_8019A6A8

--- src/st/dre/11A64.c
+++ src/st/dre/11A64.c
@@ -133,7 +133,14 @@ INCLUDE_ASM("asm/st/dre/nonmatchings/11A64", func_8019A4D8);
 
 INCLUDE_ASM("asm/st/dre/nonmatchings/11A64", func_8019A590);
 
-INCLUDE_ASM("asm/st/dre/nonmatchings/11A64", func_8019A6A8);
+s32 func_8019A6A8(void) {
+    s16 value = D_8006C3B8->posX.Data.high - D_800733DA;
+    
+    if (value < 0) {
+        value = -value;
+    }
+    return value;
+}
 
 s32 func_8019A6E4(void) {
     s32 value = D_8006C3B8->posY.Data.high - D_800733DE;
