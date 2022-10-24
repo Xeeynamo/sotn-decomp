cd0a1db4a854bf053fbf2815b50e44323235c845
frog404 <hugo.valery@gmail.com>
2022-10-25 00:47:07 +0200

    rest of hex comment: RECT, DR_ENV, POLY_GT3, POLY_G4, SPRT, SPRT_16, SPRT_8, DRAWENV, DISPENV, DR_MODE

--- include/psxsdk/libgpu.h
+++ include/psxsdk/libgpu.h
@@ -3,42 +3,75 @@
 #include <types.h>
 
 typedef struct {
-    short x, y; /* offset point on VRAM */
-    short w, h; /* width and height */
-} RECT;         // size = 0x8
+    /* 0x0 */ short x;
+    /* 0x2 */ short y; /* offset point on VRAM */
+    /* 0x4 */ short w; /* width */
+    /* 0x6 */ short h; /* height */
+} RECT; /* size = 0x8 */ 
 
 typedef struct {
-    u_long tag;
-    u_long code[15];
-} DR_ENV; /* Packed Drawing Environment */
+    /* 0x0 */ u_long tag;
+    /* 0x4 */ u_long code[15];
+} DR_ENV; /* Packed Drawing Environment, size = 0x40 */
 
 typedef struct {
-    u_long tag;
-    u_char r0, g0, b0, code;
-    short x0, y0;
-    u_char u0, v0;
-    u_short clut;
-    u_char r1, g1, b1, p1;
-    short x1, y1;
-    u_char u1, v1;
-    u_short tpage;
-    u_char r2, g2, b2, p2;
-    short x2, y2;
-    u_char u2, v2;
-    u_short pad2;
-} POLY_GT3; /* Gouraud Textured Triangle */
+    /* 0x00 */ u_long tag;
+    /* 0x04 */ u_char r0;
+    /* 0x05 */ u_char g0;
+    /* 0x06 */ u_char b0;
+    /* 0x07 */ u_char code;
+    /* 0x08 */ short x0;
+    /* 0x0A */ short y0;
+    /* 0x0C */ u_char u0;
+    /* 0x0D */ u_char v0;
+    /* 0x0E */ u_short clut;
+    /* 0x10 */ u_char r1;
+    /* 0x11 */ u_char g1;
+    /* 0x12 */ u_char b1;
+    /* 0x13 */ u_char p1;
+    /* 0x14 */ short x1;
+    /* 0x16 */ short y1;
+    /* 0x18 */ u_char u1
+    /* 0x19 */ u_char v1;
+    /* 0x1A */ u_short tpage;
+    /* 0x1C */ u_char r2;
+    /* 0x1D */ u_char g2;
+    /* 0x1E */ u_char b2;
+    /* 0x1F */ u_char p2;
+    /* 0x2A */ short x2;
+    /* 0x2C */ short y2;
+    /* 0x2E */ u_char u2;
+    /* 0x2F */ u_char v2;
+    /* 0x30 */ u_short pad2;
+} POLY_GT3; /* Gouraud Textured Triangle, size = 0x32 */
 
 typedef struct {
-    u_long tag;
-    u_char r0, g0, b0, code;
-    short x0, y0;
-    u_char r1, g1, b1, pad1;
-    short x1, y1;
-    u_char r2, g2, b2, pad2;
-    short x2, y2;
-    u_char r3, g3, b3, pad3;
-    short x3, y3;
-} POLY_G4; /* Gouraud Quadrangle */
+    /* 0x00 */ u_long tag;
+    /* 0x04 */ u_char r0;
+    /* 0x05 */ u_char g0;
+    /* 0x06 */ u_char b0;
+    /* 0x07 */ u_char code;
+    /* 0x08 */ short x0
+    /* 0x0A */ short y0;
+    /* 0x0C */ u_char r1;
+    /* 0x0D */ u_char g1;
+    /* 0x0E */ u_char b1;
+    /* 0x0F */ u_char pad1;
+    /* 0x10 */ short x1;
+    /* 0x12 */ short y1;
+    /* 0x14 */ u_char r2;
+    /* 0x15 */ u_char g2;
+    /* 0x16 */ u_char b2;
+    /* 0x17 */ u_char pad2;
+    /* 0x18 */ short x2;
+    /* 0x2A */ short y2;
+    /* 0x2C */ u_char r3;
+    /* 0x2D */ u_char g3;
+    /* 0x2E */ u_char b3;
+    /* 0x2F */ u_char pad3;
+    /* 0x30 */ short x3;
+    /* 0x32 */ short y3;
+} POLY_G4; /* Gouraud Quadrangle, size = 0x34 */
 
 typedef struct {
     /* 0x00 */ u_long tag;
@@ -81,54 +114,70 @@ typedef struct {
 } POLY_GT4; /* Gouraud Textured Quadrangle, size = 0x34*/
 
 typedef struct {
-    u_long tag;
-    u_char r0, g0, b0, code;
-    short x0, y0;
-    u_char u0, v0;
-    u_short clut;
-    short w, h;
-} SPRT; /* free size Sprite */
+    /* 0x00 */ u_long tag;
+    /* 0x04 */ u_char r0;
+    /* 0x05 */ u_char g0;
+    /* 0x06 */ u_char b0;
+    /* 0x07 */ u_char code;
+    /* 0x08 */ short x0;
+    /* 0x0A */ short y0;
+    /* 0x0C */ u_char u0;
+    /* 0x0D */ u_char v0;
+    /* 0x0E */ u_short clut;
+    /* 0x10 */ short w;
+    /* 0x12 */ short h;
+} SPRT; /* free size Sprite, size = 0x14 */
 
 typedef struct {
-    u_long tag;
-    u_char r0, g0, b0, code;
-    short x0, y0;
-    u_char u0, v0;
-    u_short clut;
-} SPRT_16; /* 16x16 Sprite */
+    /* 0x00 */ u_long tag;
+    /* 0x04 */ u_char r0;
+    /* 0x05 */ u_char g0;
+    /* 0x06 */ u_char b0;
+    /* 0x07 */ u_char code;
+    /* 0x08 */ short x0;
+    /* 0x0A */ short y0;
+    /* 0x0C */ u_char u0;
+    /* 0x0D */ u_char v0;
+    /* 0x0E */ u_short clut;
+} SPRT_16; /* 16x16 Sprite, size = 0x10 */
 
 typedef struct {
-    u_long tag;
-    u_char r0, g0, b0, code;
-    short x0, y0;
-    u_char u0, v0;
-    u_short clut;
-} SPRT_8; /* 8x8 Sprite */
+    /* 0x00 */ u_long tag;
+    /* 0x04 */ u_char r0;
+    /* 0x05 */ u_char g0;
+    /* 0x06 */ u_char b0;
+    /* 0x07 */ u_char code;
+    /* 0x08 */ short x0;
+    /* 0x0A */ short y0;
+    /* 0x0C */ u_char u0;
+    /* 0x0D */ u_char v0;
+    /* 0x0E */ u_short clut;
+} SPRT_8; /* 8x8 Sprite, size = 0x10 */
 
 typedef struct {
-    RECT clip;         /* clip area */
-    short ofs[2];      /* drawing offset */
-    RECT tw;           /* texture window */
-    u_short tpage;     /* texture page */
-    u_char dtd;        /* dither flag (0:off, 1:on) */
-    u_char dfe;        /* flag to draw on display area (0:off 1:on) */
-    u_char isbg;       /* enable to auto-clear */
-    u_char r0, g0, b0; /* initital background color */
-    DR_ENV dr_env;     /* reserved */
-} DRAWENV;
+    /* 0x00 */ RECT clip;         /* clip area */
+    /* 0x08 */ short ofs[2];      /* drawing offset */
+    /* 0x0A */ RECT tw;           /* texture window */
+    /* 0x12 */ u_short tpage;     /* texture page */
+    /* 0x14 */ u_char dtd;        /* dither flag (0:off, 1:on) */
+    /* 0x15 */ u_char dfe;        /* flag to draw on display area (0:off 1:on) */
+    /* 0x16 */ u_char isbg;       /* enable to auto-clear */
+    /* 0x17 */ u_char r0, g0, b0; /* initital background color */
+    /* 0x18 */ DR_ENV dr_env;     /* reserved */
+} DRAWENV; /* size = 0x58 */
 
 typedef struct {
-    RECT disp;         /* display area */
-    RECT screen;       /* display start point */
-    u_char isinter;    /* interlace 0: off 1: on */
-    u_char isrgb24;    /* RGB24 bit mode */
-    u_char pad0, pad1; /* reserved */
-} DISPENV;
+    /* 0x00 */ RECT disp;         /* display area */
+    /* 0x08 */ RECT screen;       /* display start point */
+    /* 0x10 */ u_char isinter;    /* interlace 0: off 1: on */
+    /* 0x11 */ u_char isrgb24;    /* RGB24 bit mode */
+    /* 0x12 */ u_char pad0, pad1; /* reserved */
+} DISPENV; /* size = 0x13 */
 
 typedef struct {
-    u_long tag;
-    u_long code[2];
-} DR_MODE; /* Drawing Mode */
+    /* 0x00 */ u_long tag;
+    /* 0x04 */ u_long code[2];
+} DR_MODE; /* Drawing Mode, size = 0x08 */
 
 extern void AddPrim(void* ot, void* p);
 extern void SetShadeTex(void* p, int tge);
