ae1160f7e87274fda24ac6605cbb37ac5ceef324
sonicdcer <alejandro.asenjo88@gmail.com>
2022-12-02 12:31:39 -0300

    func_800F17C8

--- src/dra/42398.c
+++ src/dra/42398.c
@@ -777,7 +777,15 @@ s32 func_800F16D0(void) {
 
 INCLUDE_ASM("asm/dra/nonmatchings/42398", func_800F1770);
 
-INCLUDE_ASM("asm/dra/nonmatchings/42398", func_800F17C8);
+u8 func_800F17C8(s8 arg0[], s32 arg1, s32 arg2) {
+    s32 temp_v0 = (arg1 / 2) + (arg2 * 4);
+    
+    if (!(arg1 & 1)) {
+        return arg0[temp_v0] & 0xF;
+    }
+    
+    return (u8)arg0[temp_v0] >> 4;
+}
 
 INCLUDE_ASM("asm/dra/nonmatchings/42398", func_800F180C);
 
