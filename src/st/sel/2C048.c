050b90f097f54c7ebc808d36ebb683f14b968fe5
Luciano Ciccariello <xeeynamo@hotmail.com>
2022-12-21 16:33:15 +0000

    Add SEL overlay

---
+++ src/st/sel/2C048.c
@@ -0,0 +1,335 @@
+#include "common.h"
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AC048);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AC084);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AC174);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ACBE4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ACC3C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ACC7C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ACDFC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ACEC0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ACF7C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ACFBC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD134);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD1D0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD218);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD260);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD490);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD590);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD66C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD78C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AD968);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ADC3C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801ADF94);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AE6D0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AE9A8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AEA8C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AEB74);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AECA0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AED48);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AEE74);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801AEED8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B17C8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B18CC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B18F4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1924);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B195C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B19F4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1A98);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1B88);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1C78);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1CFC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1D68);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1D88);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1DA8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1ED0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1EF4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1F34);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1F4C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B1FD8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2108);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B248C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B24F8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2564);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B259C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B25D4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2608);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B263C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2670);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B26A0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2700);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B27A8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B28D4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2984);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B29C0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2A9C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2AFC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2B78);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2BD4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2C70);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2CF8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2D1C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2D6C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2DAC);
+
+void func_801B2DDC(void) {}
+
+void func_801B2DE4(void) {}
+
+void func_801B2DEC(void) {}
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2DF4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2E5C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B2F50);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3120);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3164);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B367C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3694);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B38B4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3A54);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3A94);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3E14);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3E2C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3F7C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B3F94);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B4048);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B410C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B4B30);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B4B9C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B4BB0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B4C68);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B4D78);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B4DE0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B4FFC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B519C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B5350);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B54C8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B5548);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B55C8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B585C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B5A7C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B60D4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B642C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B6480);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B6648);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B66A4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B675C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B68E0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B690C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B69F8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B76F0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B786C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B78BC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B79D4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B80F0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8298);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8338);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B83BC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8414);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8498);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B84F0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8500);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8518);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8714);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B873C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B881C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B884C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B88F4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B89C8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8A10);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8A8C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8D24);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8DB0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8DE8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B8E20);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B94BC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B9698);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B9744);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B97BC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B988C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B9924);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B994C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B99E4);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B9B7C);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B9C18);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B9C80);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA460);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA498);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA524);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA5C0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA5CC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA648);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA668);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA688);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA6A8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA6CC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA6F0);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA7EC);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA880);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA910);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BA9A8);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BAA40);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BAB70);
+
+INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801BABA4);
