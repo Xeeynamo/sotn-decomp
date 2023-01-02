09ea7c6456b8880adf47e09c90fe24770dfbf5d0
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-01-02 22:31:22 +0000

    func_801B29C0 NON_MATCHING double while(0)

--- src/st/sel/2C048.c
+++ src/st/sel/2C048.c
@@ -562,7 +562,30 @@ s32 func_801B2984(u32 arg0) {
     return 0x20;
 }
 
+#ifndef NON_MATCHING
 INCLUDE_ASM("config/../asm/st/sel/nonmatchings/2C048", func_801B29C0);
+#else
+extern u8 D_8018046D[];
+
+void func_801B29C0(u8* str, s32 x, s32 y, s32 tga) {
+    u8 ch;
+    u8* new_var2 = D_8018046D;
+    u8* new_var = D_8018046D + 1;
+    do {
+        do {
+        loop_1:
+            ch = *(str++);
+        } while (0);
+        if (ch != 0) {
+            s32 temp_v1 = func_801B2984(ch) * 3;
+            func_801B27A8(x, y, 16, 16, new_var[temp_v1], new_var2[temp_v1],
+                          0x200, 12, tga, 0x40);
+            x += 16;
+            goto loop_1;
+        }
+    } while (0);
+}
+#endif
 
 void func_801B2A9C(s32 img, s32 x, s32 y, s32 tge) {
     func_801B27A8(x, y, 8, 8, (img & 0xF) * 8, (img & 0xF0) / 2, 0x196, 0x1E,
