e53ef657351be693f4b2b89f8ad0a55b1197019f
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-01-12 18:58:59 +0000

    Flip ABS_ALT with ABS as the latter is unused

--- include/macros.h
+++ include/macros.h
@@ -10,8 +10,8 @@ typedef enum {
 #define PLAYER g_EntityArray[PLAYER_CHARACTER]
 
 #define SQ(x) ((x) * (x))
-#define ABS(x) ((x) >= 0 ? (x) : -(x))
-#define ABS_ALT(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS_ALT(x) ((x) >= 0 ? (x) : -(x))
 #define DECR(x) ((x) == 0 ? 0 : --(x))
 
 #define CLAMP(x, min, max) ((x) < (min) ? (min) : (x) > (max) ? (max) : (x))
