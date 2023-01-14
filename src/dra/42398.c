4ab4c1eb1e1c3afb07e45cbb9876d352159e1429
sonicdcer <alejandro.asenjo88@gmail.com>
2023-01-14 13:30:51 -0300

    func_800F9E18 scratch

--- src/dra/42398.c
+++ src/dra/42398.c
@@ -2716,6 +2716,7 @@ void func_800F9DD0(u8* arg0, u8* arg1) {
     }
 }
 
+// https://decomp.me/scratch/6BXwg 95.81%
 INCLUDE_ASM("asm/dra/nonmatchings/42398", func_800F9E18);
 
 INCLUDE_ASM("asm/dra/nonmatchings/42398", func_800F9F40);
