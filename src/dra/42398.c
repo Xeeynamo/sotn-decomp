aad2e549d79c18a2711a8c11b476ab6b79723a19
sonicdcer <alejandro.asenjo88@gmail.com>
2023-01-18 17:54:55 -0300

    func_80134388

--- src/dra/42398.c
+++ src/dra/42398.c
@@ -5979,7 +5979,48 @@ void func_8013415C(void) {
 INCLUDE_ASM("asm/dra/nonmatchings/42398", func_801341B4);
 void func_801341B4();
 
-INCLUDE_ASM("asm/dra/nonmatchings/42398", func_80134388);
+void func_80134388(void) {
+    s32 temp;
+
+    switch (D_8013AE80) {
+    case 0:
+        D_8013AE80++;
+        D_801390A0 = 1;
+        D_80139A78 = 0;
+        break;
+
+    case 1:
+        D_80139A78++;
+        if (D_8013B668 > 0) {
+            temp = D_80139820 * D_80139A6C * D_80139A78;
+            if (temp < 0) {
+                temp += 0x1FF;
+            }
+            D_8013B668 = D_80139820 - (temp >> 9);
+            if (D_8013B668 >> 0x10) {
+                D_8013B668 = 0;
+            }
+        } else {
+            D_8013B668 = 0;
+        }
+        func_801324B4(0, D_8013B668, D_8013B668);
+        if (D_8013B668 == 0) {
+            D_8013AE80++;
+        }
+        break;
+
+    case 2:
+        func_80132C2C(2);
+
+    default:
+        D_8013AE80 = 0;
+        D_801390A0 = D_8013AE80;
+        D_8013B61C = 0;
+        func_80132E38();
+        break;
+    }
+}
+
 void func_80134388();
 
 extern Unkstruct_80138FB4* D_801390C8;
