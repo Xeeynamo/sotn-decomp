c276f3db07d0a074cdc8a365336b50b8b5288f50
sozud <122322823+sozud@users.noreply.github.com>
2023-01-19 19:47:04 -0800

    Decompile func_800FD7C0

--- src/dra/5D7C0.c
+++ src/dra/5D7C0.c
@@ -3,4 +3,24 @@
 #include "objects.h"
 #include "sfx.h"
 
-INCLUDE_ASM("asm/dra/nonmatchings/5D7C0", func_800FD7C0);
+u32 func_800FD7C0(u32 itemNum, u32 compareType) {
+    if (compareType < 5) {
+        switch (compareType) {
+        case 0:
+            return (player_equip_head[0] == itemNum) +
+                   (player_equip_body == itemNum);
+        case 1:
+            return player_equip_cloak == itemNum;
+        case 2:
+            return player_equip_ring1 == itemNum;
+        case 3:
+            return player_equip_ring2 == itemNum;
+        case 4:
+            return (D_80097C14 == itemNum) + (D_80097C18 == itemNum);
+        }
+    }
+    // seems to require missing return
+}
+
+// fix missing padding at end of jump table. ASPSX issue?
+const u32 rodataPadding_800DCBD8 = 0;
