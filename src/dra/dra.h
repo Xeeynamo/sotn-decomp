9f0a7c356b2007db23ca035ef8d43e72d5d76264
Alejandro Asenjo Nitti <96613413+sonicdcer@users.noreply.github.com>
2023-01-29 16:01:06 -0300

    func_80125C2C (#83)

--- src/dra/dra.h
+++ src/dra/dra.h
@@ -258,6 +258,7 @@ extern s32 D_80137F6C;
 extern void* D_80137F7C;
 extern s32 D_80137F9C;
 extern s32 D_80138008;
+extern s32 D_8013841C;
 extern s32 D_80138430;
 extern s32 D_80138438;
 extern s32 D_80138444;
