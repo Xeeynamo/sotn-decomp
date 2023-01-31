32568b8ae583712ac42ccd944b4142431e420e3f
sonicdcer <alejandro.asenjo88@gmail.com>
2023-01-30 23:51:08 -0300

    refactor func_80113AAC

--- src/dra/73AAC.c
+++ src/dra/73AAC.c
@@ -3,7 +3,7 @@
 #include "objects.h"
 #include "sfx.h"
 
-extern u16 D_80072F6A[];
+extern s16 D_80072F6A[];
 extern s16 D_800733F8;
 extern u16 D_800733FA;
 
@@ -14,35 +14,35 @@ void func_80113AAC(void) {
     var_s1 = 0;
     D_80072F6A[0]++;
     if (func_8010FDF8(2) == 0) {
-        switch (D_80073406) {
+        switch (PLAYER.unk2E) {
         case 0:
             if (D_80072F20 & 2) {
                 func_801139CC(3);
-                if (((s16)D_80072F6A[0]) >= 5) {
-                    D_80073406 = 2;
-                    D_800733F6 = 0x800;
-                    D_800733FA = 2;
-                    D_800733F8 = 0;
-                    D_800733F1 |= 4;
-                    D_800733EC = (D_800733EC + 1) & 1;
+                if ((D_80072F6A[0]) >= 5) {
+                    PLAYER.unk2E = 2;
+                    PLAYER.unk1E = 0x800;
+                    PLAYER.unk22 = 2;
+                    PLAYER.unk20 = 0;
+                    PLAYER.unk19 |= 4;
+                    PLAYER.facing = (PLAYER.facing + 1) & 1;
                     func_8010DA48(0x2B);
                 } else {
-                    D_80073406 = 3;
+                    PLAYER.unk2E = 3;
                 }
-            } else if (((s16)D_80072F6A[0]) >= 0x1D) {
-                D_80073406 = 1;
-                D_800733E4 = -0x60000;
+            } else if (D_80072F6A[0] > 28) {
+                PLAYER.unk2E = 1;
+                PLAYER.accelerationY = -0x60000;
                 func_8010DA48(0x1B);
             }
             break;
 
         case 1:
             if (D_80072F20 & 2) {
-                D_80073406 = 2;
+                PLAYER.unk2E = 2;
                 func_801139CC(3);
             } else {
-                g_EntityArray[PLAYER_CHARACTER].accelerationY += 0x6000;
-                if (g_EntityArray[PLAYER_CHARACTER].accelerationY > 0x8000) {
+                PLAYER.accelerationY += 0x6000;
+                if (PLAYER.accelerationY > 0x8000) {
                     var_s1 = 1;
                 }
             }
@@ -51,13 +51,13 @@ void func_80113AAC(void) {
         case 2:
             D_800733F8 = 0;
             D_800733FA = 2;
-            g_EntityArray[PLAYER_CHARACTER].unk19 |= 4;
-            if (((s16)D_80072F6A[0]) >= 0x39) {
+            PLAYER.unk19 |= 4;
+            if (D_80072F6A[0] >= 0x39) {
                 func_8010DA48(0x2D);
-                D_800733F6 = 0;
-                D_80073406 = 4;
-                g_EntityArray[PLAYER_CHARACTER].unk19 &= 0xFB;
-                D_800733EC = (D_800733EC + 1) & 1;
+                PLAYER.unk1E = 0;
+                PLAYER.unk2E = 4;
+                PLAYER.unk19 &= 0xFB;
+                PLAYER.facing = (PLAYER.facing + 1) & 1;
             }
             break;
 
@@ -68,7 +68,7 @@ void func_80113AAC(void) {
             break;
 
         case 4:
-            g_EntityArray[PLAYER_CHARACTER].accelerationY += 0x1000;
+            PLAYER.accelerationY += 0x1000;
             if (D_8007342A < 0) {
                 var_s1 = 2;
             }
@@ -80,9 +80,9 @@ void func_80113AAC(void) {
             if ((var_s1 - 1) != new_var) {
                 func_8010DA48(0x1C);
             }
-            D_800733EE = 0x8100;
-            D_80073406 = 1;
-            D_80073404 = 4;
+            PLAYER.palette = 0x8100;
+            PLAYER.unk2E = 1;
+            PLAYER.step = 4;
         }
     }
 }
