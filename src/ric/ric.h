fdd42351664ff6139cf3e252cbba1a8d3c60d0d1
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-02-07 10:59:06 +0000

    RIC create header

---
+++ src/ric/ric.h
@@ -0,0 +1,46 @@
+
+#include "common.h"
+#include "game.h"
+#include "objects.h"
+#include "sfx.h"
+
+extern s32 D_80155368[];
+
+void func_8015D020(void);
+bool func_8015DBB0(s32);
+void func_8015C93C(s32 speed);
+s32 func_8015C9CC(void);
+void func_8015CA84(s32 speed);
+void func_8015CD98(s32 accelerationX);
+void func_8015CDE0(s32);
+void func_8015CE7C(void);
+s32 func_8015CF08(void);
+s32 func_8015E380(s32);
+void func_8015F9F0(Entity* entity);
+void func_8015FAB8(Entity*);
+Entity* func_801606BC(Entity* entity, u32 arg1, s32 arg2);
+
+extern u16 D_80072F9A; // main.h?
+extern s16 D_801545EA[];
+extern s16 D_80154604;
+extern s16 D_80154606;
+extern s8 D_80154688;
+extern s32 D_80155488;
+extern s32 D_801554B0;
+extern s32 D_801553BC;
+extern s32 D_801554C0;
+extern s32 D_801554C8;
+extern s32 D_801554D0;
+extern s32 D_80155670;
+extern s32 D_80155750;
+extern /*?*/ s32 D_8015591C;
+extern /*?*/ s32 D_80155950;
+extern u8 D_80174FAC;
+extern u8 D_80174FB0;
+extern u8 D_80174FB4;
+extern u8 D_80174FB8;
+extern u16 D_80175950;
+extern u16 D_80175952;
+extern u16 D_80175954;
+extern s16 D_80175956;
+extern s16 D_80175956;
