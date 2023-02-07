e2ff507d889bdecf2823102049a3adfd1988c882
sonicdcer <alejandro.asenjo88@gmail.com>
2023-02-07 15:26:10 -0300

    fix

--- src/ric/1F348.c
+++ src/ric/1F348.c
@@ -1,56 +1,5 @@
 #include "ric.h"
 
-void func_8015C908(s32 step);
-void func_8015C920(s32* unk0);
-void func_8015D020(void);
-bool func_8015DBB0(s32);
-void func_8015C93C(s32 speed);
-s32 func_8015C9CC(void);
-void func_8015CA84(s32 speed);
-void func_8015CD98(s32 accelerationX);
-void func_8015CDE0(s32);
-void func_8015CE7C(void);
-s32 func_8015CF08(void);
-s32 func_8015E380(s32);
-void func_8015F9F0(Entity* entity);
-void func_8015FAB8(Entity*);
-Entity* func_801606BC(Entity* entity, u32 arg1, s32 arg2);
-
-extern s16 D_801545EA[];
-extern s16 D_80154604;
-extern s16 D_801545A8;
-extern s16 D_801545AA;
-extern s16 D_801545AC;
-extern s16 D_80154606;
-extern s8 D_80154688;
-extern s32 D_80155368[];
-extern s32 D_801553BC;
-extern s32 D_80155488;
-extern s32 D_801554B0;
-extern s32 D_801554C0;
-extern s32 D_801554C8;
-extern s32 D_801554D0;
-extern s32 D_80155748;
-extern s32 D_801558B4;
-extern s32 D_801558D4;
-extern s32 D_801558DC;
-extern s32 D_80155528;
-extern s32 D_80155670;
-extern s32 D_80155750;
-extern /*?*/ s32 D_8015591C;
-extern /*?*/ s32 D_80155950;
-extern s32 D_80174F74;
-extern s32 D_80174F78;
-extern u8 D_80174FAC;
-extern u8 D_80174FB0;
-extern u8 D_80174FB4;
-extern u8 D_80174FB8;
-extern u16 D_80175950;
-extern u16 D_80175952;
-extern u16 D_80175954;
-extern s16 D_80175956;
-extern s16 D_80175956;
-
 void func_8015B348(void) {
     switch (PLAYER.unk2E) {
     case 0:
