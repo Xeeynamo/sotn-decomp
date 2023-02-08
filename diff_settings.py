ebb2fab0f678a7b8aa300d93e33fd7151815a880
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-02-08 14:42:25 +0000

    Update diff script to work with servant overlays

--- diff_settings.py
+++ diff_settings.py
@@ -9,20 +9,55 @@ def add_custom_arguments(parser):
     )
 
 
-def apply(config, args):
-    overlay = args.overlay or 'dra'
-    isMapOverlay = overlay.startswith("st/")
-    name = overlay[3:] if isMapOverlay else overlay
-    longname = f'st{overlay[3:]}' if isMapOverlay else overlay
+def apply_base(config, name):
+    config['baseimg'] = 'iso/' + (f'{name}.bin').upper()
+    config['myimg'] = 'build/' + (f'{name}.bin').upper()
+    config['mapfile'] = f'build/{name}.map'
+    config['source_directories'] = [
+        f'src/{name}', 'include', f'asm/{name}']
+    config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
 
-    if isMapOverlay:
-        config['baseimg'] = 'iso/' + (f'ST/{name}/{name}.BIN').upper()
+
+def apply_bin(config, name):
+    config["arch"] = "mipsel"
+    config['baseimg'] = 'iso/' + (f'BIN/{name}.BIN').upper()
+    config['myimg'] = 'build/' + (f'{name}.bin').upper()
+    config['mapfile'] = f'build/{name}.map'
+    config['source_directories'] = [
+        f'src/{name}', 'include', f'asm/{name}']
+    config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
+
+
+def apply_servant(config, name):
+    config["arch"] = "mipsel"
+    config['baseimg'] = 'iso/' + (f'SERVANT/{name}.bin').upper()
+    config['myimg'] = 'build/' + (f'{name}.bin').upper()
+    config['mapfile'] = f'build/{name}.map'
+    config['source_directories'] = [
+        f'src/servant/{name}', 'include', f'asm/servant/{name}']
+    config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
+
+
+def apply_stage(config, name):
+    config["arch"] = "mipsel"
+    config['baseimg'] = 'iso/' + (f'ST/{name}/{name}.BIN').upper()
+    config['myimg'] = 'build/' + (f'{name}.bin').upper()
+    config['mapfile'] = f'build/st{name}.map'
+    config['source_directories'] = [
+        f'src/st/{name}', 'include', f'asm/st/{name}']
+    config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
+
+
+def apply(config, args):
+    name = args.overlay or 'dra'
+    if name.startswith("st/"):
+        apply_stage(config, name[3:])
+    elif name.startswith("tt_"):
+        apply_servant(config, name)
+    elif name is "dra" or name is "main":
+        apply_base(config, name)
     else:
-        config['baseimg'] = 'iso/' + (f'{name}.BIN').upper()
+        apply_bin(config, name)
 
     config["arch"] = "mipsel"
-    config['myimg'] = 'build/' + (f'{name}.bin').upper()
-    config['mapfile'] = f'build/{longname}.map'
-    config['source_directories'] = [
-        f'src/{overlay}', 'include', f'asm/{overlay}']
     config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
