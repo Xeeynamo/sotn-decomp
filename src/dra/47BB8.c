e12667246c67dee6787b296c8a127ac3876c5dc8
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-02-16 00:26:36 +0000

    DRA func_800E7AEC matching

---
+++ src/dra/47BB8.c
@@ -0,0 +1,415 @@
+#include "common.h"
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E7BB8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E7D08);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", LoadStageTileset);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E7E08);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E81FC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E8D24);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E8D54);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", InitializePads);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", ReadPads);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E8EE4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E908C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E912C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E91B0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9208);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E928C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E92E4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E92F4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E930C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9508);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9530);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9610);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9640);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E96E8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E97BC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9804);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9880);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9B18);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", GetSavePalette);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", GetSaveIcon);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800E9C14);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EA2B0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EA48C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EA538);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EA5AC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EA5E4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EA720);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EA7CC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EAD0C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EAD7C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EAEA4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EAEEC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EAF28);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EAFC8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EB03C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EB098);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EB314);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EB4F8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EB534);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EB6B4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EB720);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EB758);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EBB70);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EBBAC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800ECBF8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800ECE2C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800ECE58);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", SetRoomForegroundLayer);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", SetRoomBackgroundLayer);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", LoadRoomLayer);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EDA70);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EDA94);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EDAE4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EDB08);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EDB58);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", AllocPolygons);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EDD9C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", FreePolygons);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EDEDC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", CheckCollision);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800EFBF8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F0334);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F04A4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F0578);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F0608);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F087C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F0940);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", SetNextRoomToLoad);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F0CD8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1424);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F14CC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F16D0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1770);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F17C8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F180C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1868);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F18C4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1954);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1A3C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1B08);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1D54);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1EB0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F1FC4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F2014);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F2120);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F223C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F2288);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F2404);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F24F4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F2658);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F27F4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F2860);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F298C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F483C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", IsAlucart);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F4994);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F4D38);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F4F48);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F4FD0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F53A4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F53D4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F548C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", IsOutsideDrawArea);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", ScissorPolyG4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", ScissorPolyGT4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", ScissorSprite);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F5904);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F5A90);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F5AE4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawMenuSprite);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawMenuRect);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F5E68);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawRelicsMenu);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawMenuAlucardPortrait);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F62E8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6304);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawMenuAlucardCloakPreview);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6508);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6568);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6618);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F66BC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawMenuChar);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawMenuStr);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawMenuInt);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6998);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6A48);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6A70);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6BEC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6CC0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F6DC8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawSettingsButton);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawSettingsReverseCloak);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawSettingsSound);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F7218);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F7244);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F72BC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawPauseMenu);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawSpellMenu);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F7F64);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F82F4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", DrawSystemMenu);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F84CC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F86E4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F8754);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F8858);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F892C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F8990);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F8C98);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F8E18);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F8F28);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F9690);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F96F4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F97DC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F9808);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F98AC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F99B8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F9D40);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F9D88);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F9DD0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F9E18);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800F9F40);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FA034);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FA3C4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FA60C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FA7E8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FA8C4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FA9DC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAB1C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAB8C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FABBC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FABEC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAC0C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAC30);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAC48);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAC98);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FACB8);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAD34);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FADC0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAE98);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAEC4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FAF44);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FB004);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FB0FC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FB160);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FB1EC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FB23C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FB9BC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FBAC4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FBC24);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FD39C);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FD4C0);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FD5BC);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FD664);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FD688);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FD6C4);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FD744);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", func_800FD760);
+
+INCLUDE_ASM("config/../asm/dra/nonmatchings/47BB8", GetEquipmentName);
