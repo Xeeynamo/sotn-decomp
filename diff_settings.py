87dd3600fb40f7379ac624a43d2c2be38588acb2
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-02-21 22:03:18 +0000

    Fix diff

--- diff_settings.py
+++ diff_settings.py
@@ -2,6 +2,12 @@
 
 def add_custom_arguments(parser):
     parser.add_argument(
+        "--version",
+        default="us",
+        dest='version',
+        help="Decide what version of the game to use (us, jp12, pspeu, etc.)",
+    )
+    parser.add_argument(
         "--overlay",
         default="dra",
         dest='overlay',
@@ -9,55 +15,56 @@ def add_custom_arguments(parser):
     )
 
 
-def apply_base(config, name):
-    config['baseimg'] = 'iso/' + (f'{name}.bin').upper()
-    config['myimg'] = 'build/' + (f'{name}.bin').upper()
-    config['mapfile'] = f'build/{name}.map'
+def apply_base(config, version, name):
+    config['baseimg'] = f'disks/{version}' + (f'{name}.bin').upper()
+    config['myimg'] = f'build/{version}' + (f'{name}.bin').upper()
+    config['mapfile'] = f'build/{version}/{name}.map'
     config['source_directories'] = [
-        f'src/{name}', 'include', f'asm/{name}']
+        f'src/{name}', 'include', f'asm/{version}/{name}']
     config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
 
 
-def apply_bin(config, name):
+def apply_bin(config, version, name):
     config["arch"] = "mipsel"
-    config['baseimg'] = 'iso/' + (f'BIN/{name}.BIN').upper()
-    config['myimg'] = 'build/' + (f'{name}.bin').upper()
-    config['mapfile'] = f'build/{name}.map'
+    config['baseimg'] = f'disks/{version}' + (f'BIN/{name}.BIN').upper()
+    config['myimg'] = f'build/{version}' + (f'{name}.bin').upper()
+    config['mapfile'] = f'build/{version}/{name}.map'
     config['source_directories'] = [
-        f'src/{name}', 'include', f'asm/{name}']
+        f'src/{name}', 'include', f'asm/{version}/{name}']
     config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
 
 
-def apply_servant(config, name):
+def apply_servant(config, version, name):
     config["arch"] = "mipsel"
-    config['baseimg'] = 'iso/' + (f'SERVANT/{name}.bin').upper()
-    config['myimg'] = 'build/' + (f'{name}.bin').upper()
-    config['mapfile'] = f'build/{name}.map'
+    config['baseimg'] = f'disks/{version}' + (f'SERVANT/{name}.bin').upper()
+    config['myimg'] = f'build/{version}' + (f'{name}.bin').upper()
+    config['mapfile'] = f'build/{version}/{name}.map'
     config['source_directories'] = [
-        f'src/servant/{name}', 'include', f'asm/servant/{name}']
+        f'src/servant/{name}', 'include', f'asm/{version}/servant/{name}']
     config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
 
 
-def apply_stage(config, name):
+def apply_stage(config, version, name):
     config["arch"] = "mipsel"
-    config['baseimg'] = 'iso/' + (f'ST/{name}/{name}.BIN').upper()
-    config['myimg'] = 'build/' + (f'{name}.bin').upper()
-    config['mapfile'] = f'build/st{name}.map'
+    config['baseimg'] = f'disks/{version}' + (f'ST/{name}/{name}.BIN').upper()
+    config['myimg'] = f'build/{version}' + (f'{name}.bin').upper()
+    config['mapfile'] = f'build/{version}/st{name}.map'
     config['source_directories'] = [
-        f'src/st/{name}', 'include', f'asm/st/{name}']
+        f'src/st/{name}', 'include', f'asm/{version}/st/{name}']
     config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
 
 
 def apply(config, args):
     name = args.overlay or 'dra'
+    version = args.version or 'us'
     if name.startswith("st/"):
-        apply_stage(config, name[3:])
+        apply_stage(config, version, name[3:])
     elif name.startswith("tt_"):
-        apply_servant(config, name)
+        apply_servant(config, version, name)
     elif name is "dra" or name is "main":
-        apply_base(config, name)
+        apply_base(config, version, name)
     else:
-        apply_bin(config, name)
+        apply_bin(config, version, name)
 
     config["arch"] = "mipsel"
     config['objdump_executable'] = 'mipsel-linux-gnu-objdump'
