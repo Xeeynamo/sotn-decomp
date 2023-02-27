d348d24347b782139f7e712d94bde7224aeb925d
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-02-27 19:41:48 +0000

    Create reporting page

---
+++ src/Chart.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import uPlot from 'uplot';
+import UplotReact from 'uplot-react';
+import 'uplot/dist/uPlot.min.css';
+
+export const Chart = ({
+    moduleName,
+    data,
+}) => {
+    const toPercent = function(x) {
+        return x ? x.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}) : "--" 
+    }
+
+    const options = {
+        title: moduleName,
+        width: 800,
+        height: 400,
+        series: [
+          {
+            label: "Date"
+          },
+          {
+            label: "Functions",
+            points: { show: true },
+            width: 1.5,
+            stroke: "rgba(255, 255, 0, 1)",
+            fill: "rgba(255, 255, 0, 0.3)",
+            value: (self, value) => toPercent(value)
+          },
+          {
+            label: "Code",
+            points: { show: true },
+            width: 1.5,
+            stroke: "rgba(255, 0, 0, 1)",
+            fill: "#FF2D24A0",
+            value: (self, value) => toPercent(value)
+          },
+        ],
+        scales: {
+            x: {
+                time: true,
+            },
+            y: {
+                range: [0, 1]
+            }
+        },
+        axes: [
+            {
+                stroke: "#c7d0d9",
+                grid: {
+                    width: 1 / devicePixelRatio,
+                    stroke: "#2c3235",
+                },
+                ticks: {
+                    width: 1 / devicePixelRatio,
+                    stroke: "#2c3235",
+                },
+            },
+            {
+                stroke: "#c7d0d9",
+                grid: {
+                    width: 1 / devicePixelRatio,
+                    stroke: "#2c3235",
+                },
+                ticks: {
+                    width: 1 / devicePixelRatio,
+                    stroke: "#2c3235",
+                },
+                values: (self, ticks) => ticks.map(x => x*100 + "%"),
+            },
+        ]
+    }
+    const target = null   
+
+    return (<UplotReact
+        options={options}
+        data={data}
+        target={target}
+        onCreate={(chart) => {}}
+        onDelete={(chart) => {}}
+    />);
+}
---
+++ src/ChartsVersion.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import { Chart } from './Chart';
+
+export const ChartsVersion = ({
+    gameVersion
+}) => {
+    const getEntryProgress = function(name, entry) {
+        return entry[name] / entry[`${name}/total`]
+    }
+
+    const normaliseData = function(data, slug, ver, type) {
+        const src = data[slug][ver][type]
+        const timeline = src.map(x => ({
+                timestamp: x.timestamp,
+                gitHash: x.git_hash,
+            }))
+        const overlays = Object.keys(src[0].measures)
+            .map(x => x.split('/')[0])
+            .filter((v, idx, a) => a.indexOf(v) === idx)
+
+        return {
+            timeline: timeline,
+            overlays: overlays.map(ovl => ({
+                name: ovl,
+                percentage: src.map(x => getEntryProgress(ovl, x.measures))
+            }))
+        }
+    }
+
+    const renderOvlChart = function(name, timeline, ovlCode, ovlFuncs)  {
+        const data = [
+            timeline.map(x => x.timestamp),
+            ovlFuncs,
+            ovlCode,
+          ]
+        return (
+            <Chart moduleName={name} data={data}/>
+        )
+    }
+
+    const [progressCode, setProgressCode] = useState(undefined)
+    const [progressFuncs, setProgressFuncs] = useState(undefined)
+
+    const fetchProgress = (type, setter) => {
+      const host = "https://progress.deco.mp"
+      const url = `${host}/data/sotn/${gameVersion}/${type}/?mode=all`
+      return fetch(url)
+            .then((response) => response.json())
+            .then((data) => setter(normaliseData(data, "sotn", gameVersion, type)))
+    }
+  
+    useEffect(() => {
+        fetchProgress('code', setProgressCode);
+        fetchProgress('functions', setProgressFuncs);
+    },[])
+
+    return (progressCode && progressFuncs &&
+        progressCode.overlays.map(ovl => {
+            const timeline = progressCode.timeline
+            const code = progressCode.overlays
+                .filter(x => x.name === ovl.name)[0].percentage
+            const funcs = progressFuncs.overlays
+                .filter(x => x.name === ovl.name)[0].percentage
+            return renderOvlChart(ovl.name, timeline, code, funcs)
+        }))
+}
