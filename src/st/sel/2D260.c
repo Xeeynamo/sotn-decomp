da892e4cef66589411b8aab1a99c5c99f84ec987
sonicdcer <alejandro.asenjo88@gmail.com>
2023-03-06 21:46:19 -0300

    NON_EQUIVALENT

--- src/st/sel/2D260.c
+++ src/st/sel/2D260.c
@@ -875,7 +875,7 @@ void func_801B3E14(s32 arg0) {
     g_memCardRStepSub = arg0;
 }
 
-#ifndef NON_MATCHING
+#ifndef NON_EQUIVALENT
 INCLUDE_ASM("asm/us/st/sel/nonmatchings/2D260", func_801B3E2C);
 #else
 s32 func_801B3E2C(void) {
@@ -1113,6 +1113,7 @@ void func_801B4FFC(void) {
                 unkEntity->step++;
             }
         }
+
     case 2:
         break;
     }
