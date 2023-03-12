45fdb17084739505cfe8a33a3cc28ce72aac5b4b
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-03-12 00:13:28 +0000

    quick fix

--- include/common.h
+++ include/common.h
@@ -7,9 +7,11 @@
 
 #define LOH(x) (*(s16*)&(x))
 #ifdef HACKS
-#define ALIGNED4 __attribute__(aligned(4))
+#define ALIGNED4 __attribute__((aligned(4)))
+#define NOP asm volatile("nop")
 #else
 #define ALIGNED4
+#define NOP
 #endif
 
 int sprintf(char* dst, const char* fmt, ...);
