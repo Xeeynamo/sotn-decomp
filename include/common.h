2d303fa538bc82263cfae7983094de46104e8e33
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-03-12 00:11:04 +0000

    Fix __attribute__ decompilation error

--- include/common.h
+++ include/common.h
@@ -6,6 +6,11 @@
 #include "types.h"
 
 #define LOH(x) (*(s16*)&(x))
+#ifdef HACKS
+#define ALIGNED4 __attribute__(aligned(4))
+#else
+#define ALIGNED4
+#endif
 
 int sprintf(char* dst, const char* fmt, ...);
 
