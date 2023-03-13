bfe1747f4a6ca5df728f4daa00174b5119f369da
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-03-13 01:43:07 +0000

    Fix permuter when using ASM hacks

--- include/common.h
+++ include/common.h
@@ -6,7 +6,7 @@
 #include "types.h"
 
 #define LOH(x) (*(s16*)&(x))
-#ifdef HACKS
+#if defined(HACKS) && !defined(PERMUTER)
 // The following hacks are required to get a matching decompilation. If removed
 // they will still logically match the original function.
 #define ALIGNED4 __attribute__((aligned(4)))
