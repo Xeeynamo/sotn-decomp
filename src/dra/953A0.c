4e26f5e3deb74b2fe23cc5e25024eed0d9ed7ff8
sozud <122322823+sozud@users.noreply.github.com>
2023-03-15 15:23:38 -0700

    DRA decompile func_80134C60 (#151)

--- src/dra/953A0.c
+++ src/dra/953A0.c
@@ -42,16 +42,15 @@ void func_801353A0(void) {
 
 INCLUDE_ASM("asm/us/dra/nonmatchings/953A0", func_80135484);
 
-void func_80135624(s16 arg0, s32 arg1, s32 arg2, s16 arg3, s16 arg4) {
+void func_80135624(s16 arg0, s32 arg1, s32 arg2, s16 volume, s16 distance) {
     if (arg2 != 0) {
         D_80138F28 |= (1 << ((arg1 + 6) * 2)) + (1 << (((arg1 + 6) * 2) + 1));
     }
-    func_80132A04((arg1 * 2) + 12, D_800BF554[arg0 * 7], D_800BF555[arg0 * 7],
-                  D_800BF559[arg0 * 7], D_800BF556[arg0 * 7], arg3, arg4);
-    do { // TODO: !FAKE
-    } while (0);
+    func_80132A04((arg1 * 2) + 12, D_800BF554[arg0].vabid,
+                  D_800BF554[arg0].prog, D_800BF554[arg0].tone,
+                  D_800BF554[arg0].note, volume, distance);
     D_8013B650[arg1] = arg0;
-    D_8013AED4[arg1] = D_800BF55A[arg0 * 7];
+    D_8013AED4[arg1] = D_800BF554[arg0].pad1;
 }
 
 INCLUDE_ASM("asm/us/dra/nonmatchings/953A0", func_8013572C);
