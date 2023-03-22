70acbf7a8f97aed17528fe519bfed0a26371faf7
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-03-22 21:03:23 +0000

    DRA LoadSaveData match and full save struct mapped

--- src/st/sel/38D24.c
+++ src/st/sel/38D24.c
@@ -46,8 +46,48 @@ void func_801B8DE8(u8* dst, s32 arg1) {
 void StoreSaveData(SaveData* save, s32 arg1, s32 arg2);
 INCLUDE_ASM("asm/us/st/sel/nonmatchings/38D24", StoreSaveData);
 
-s32 LoadSaveData(SaveData* save); // LoadSaveData
-INCLUDE_ASM("asm/us/st/sel/nonmatchings/38D24", LoadSaveData);
+s32 LoadSaveData(SaveData* save) {
+    s32 i;
+    u32 prevCompletionFlags1;
+    u32 prevCompletionFlags2;
+    PlayerStatus* srcStatus;
+    MenuNavigation* srcNav;
+    GameSettings* settings;
+
+    if (save->saveSize != (sizeof(SaveData))) {
+        return -1;
+    }
+
+    g_StageId = save->stageID;
+    g_IsTimeAttackUnlocked = save->isTimeAttackUnlocked;
+    g_CurrentPlayableCharacter = save->playableCharacter;
+    g_roomCount = save->exploredRoomCount;
+    g_CurrentRoom.left = save->roomX;
+    g_CurrentRoom.top = save->roomY;
+
+    srcStatus = &save->status;
+    srcNav = &save->menuNavigation;
+    settings = &save->settings;
+    __builtin_memcpy(&g_Status, srcStatus, sizeof(g_Status));
+    __builtin_memcpy(&g_MenuNavigation, srcNav, sizeof(g_MenuNavigation));
+
+    prevCompletionFlags1 = g_Settings.D_8003CB00;
+    prevCompletionFlags2 = g_Settings.D_8003CB04;
+    __builtin_memcpy(&g_Settings, settings, sizeof(g_Settings));
+    g_Settings.D_8003CB00 |= prevCompletionFlags1;
+    g_Settings.D_8003CB04 |= prevCompletionFlags2;
+
+    for (i = 0; i < 0x300; i++) {
+        D_8003BDEC[i] = save->castleFlags[i];
+    }
+
+    for (i = 0; i < 0x800; i++) {
+        D_8006BB74[i] = save->castleMap[i];
+    }
+
+    g_randomNext = save->rng;
+    return 0;
+}
 
 void func_801B9698(char* dstSaveName, s32 saveSlot);
 #ifndef NON_MATCHING
