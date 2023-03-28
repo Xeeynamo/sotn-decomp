e33fa87e3c7684fb897f7e06a7e3b35bdfba4aad
sonicdcer <alejandro.asenjo88@gmail.com>
2023-03-28 12:04:42 -0300

    CEN EntityHeartDrop

--- src/st/cen/D600.c
+++ src/st/cen/D600.c
@@ -4,18 +4,7 @@
  * Description: Center Cube Area (Final Boss)
  */
 
-#include "common.h"
-#include "stage.h"
-
-extern PfnEntityUpdate D_80180390[];
-extern u16 D_801805D8[];
-extern s16 D_80180BBC[];
-void CreateEntityFromCurrentEntity(u16 objectId, Entity* entity);
-extern LayoutObject* D_8019C764;
-extern LayoutObject* D_8019C768;
-extern s16 D_8019D3B4;
-extern s16 D_8019D3B6;
-extern s32 D_8019D3B8;
+#include "cen.h"
 
 // background block of rock
 INCLUDE_ASM("asm/us/st/cen/nonmatchings/D600", EntityBackgroundBlock);
---
+++ src/st/cen/cen.h
@@ -0,0 +1,17 @@
+#ifndef CEN_H
+#define CEN_H
+
+#include "common.h"
+#include "stage.h"
+
+extern PfnEntityUpdate D_80180390[];
+extern u16 D_801805D8[];
+extern s16 D_80180BBC[];
+void CreateEntityFromCurrentEntity(u16 objectId, Entity* entity);
+extern LayoutObject* D_8019C764;
+extern LayoutObject* D_8019C768;
+extern s16 D_8019D3B4;
+extern s16 D_8019D3B6;
+extern s32 D_8019D3B8;
+
+#endif
