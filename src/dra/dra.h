7e8a1bbc350a2990899b20439c15fec9c127c550
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-04-30 09:27:05 +0100

    DRA func_800F1868 matching

--- src/dra/dra.h
+++ src/dra/dra.h
@@ -459,7 +459,7 @@ void func_800F0334(s32);
 void func_800F0578(s32 arg0);
 s32 func_800F087C(u32, u32);
 bool SetNextRoomToLoad(u32 chunkX, u32 chunkY);
-void func_800F1868(s32, s32, void*);
+void func_800F1868(s32, s32, u8*);
 void func_800F18C4(s32, s32);
 void func_800F1954(s32, s32, s32);
 void func_800F1EB0(s32, s32, s32);
