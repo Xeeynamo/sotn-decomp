e7107fe6899d3c11c0f1a5d9268281ea523efe05
NotExactlySiev <sadrabousi@gmail.com>
2023-05-08 14:11:55 +0330

    ST WRP ClutLerp duplicate

--- src/st/wrp/D020.c
+++ src/st/wrp/D020.c
@@ -1,4 +1,5 @@
 #include "stage.h"
+#include "game.h"
 
 PfnEntityUpdate PfnEntityUpdates[];
 
@@ -839,7 +840,45 @@ INCLUDE_ASM("asm/us/st/wrp/nonmatchings/D020", func_801907EC);
 
 INCLUDE_ASM("asm/us/st/wrp/nonmatchings/D020", func_80190DCC);
 
-INCLUDE_ASM("asm/us/st/wrp/nonmatchings/D020", func_801914CC);
+#ifndef NON_MATCHING
+INCLUDE_ASM("asm/us/st/wrp/nonmatchings/D020", ClutLerp);
+#else
+void ClutLerp(RECT* rect, u16 palIdxA, u16 palIdxB, s32 steps, u16 offset) {
+    u16 buf[COLORS_PER_PAL];
+    RECT bufRect;
+    s32 factor;
+    u32 t;
+    u32 r, g, b;
+    s32 i, j;
+    u16 *palA, *palB;
+
+    bufRect.x = rect->x;
+    bufRect.w = COLORS_PER_PAL;
+    bufRect.h = 1;
+
+    palA = g_Clut + palIdxA * COLORS_PER_PAL;
+    palB = g_Clut + palIdxB * COLORS_PER_PAL;
+
+    for (i = 0; i < steps; i++) {
+        factor = i * 4096 / steps;
+        for (j = 0; j < COLORS_PER_PAL; j++) {
+            r = (palA[j] & 0x1F) * (4096 - factor) + (palB[j] & 0x1F) * factor;
+            g = ((palA[j] >> 5) & 0x1F) * (4096 - factor) + ((palB[j] >> 5) & 0x1F) * factor;
+            b = ((palA[j] >> 10) & 0x1F) * (4096 - factor) + ((palB[j] >> 10) & 0x1F) * factor;
+            
+            t = palA[j] & 0x8000;
+            t |= palB[j] & 0x8000;
+            
+            buf[j] =  t | (r >> 12) | ((g >> 12) << 5) | ((b >> 12) << 10);
+        }
+
+        bufRect.y = rect->y + i;
+        LoadImage(&bufRect, buf);
+        D_8003C104[offset + i] = GetClut(bufRect.x, bufRect.y);
+    }
+}
+#endif
+
 
 INCLUDE_ASM("asm/us/st/wrp/nonmatchings/D020", func_801916C4);
 
