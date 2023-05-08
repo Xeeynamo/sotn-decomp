f3a7b3106ff6d079d72bb10ea4755cbd4f45af29
sonicdcer <alejandro.asenjo88@gmail.com>
2023-05-08 17:27:33 -0300

    func_801BD0B4

--- src/st/np3/3246C.c
+++ src/st/np3/3246C.c
@@ -1553,7 +1553,29 @@ u16 func_801BD06C(s32 x, s32 y) {
     return ratan2(diffY, diffX);
 }
 
-INCLUDE_ASM("asm/us/st/np3/nonmatchings/3246C", func_801BD0B4);
+u16 func_801BD0B4(u16 arg0, s16 arg1, s16 arg2) {
+    u16 var_v0 = arg1;
+    u16 temp_a2 = arg2 - arg1;
+    u16 var_v0_2;
+
+    if (temp_a2 & 0x800) {
+        var_v0_2 = (0x800 - temp_a2) & 0x7FF;
+    } else {
+        var_v0_2 = temp_a2;
+    }
+
+    if (var_v0_2 > arg0) {
+        if (temp_a2 & 0x800) {
+            var_v0 = arg1 - arg0;
+        } else {
+            var_v0 = arg1 + arg0;
+        }
+
+        return var_v0;
+    }
+
+    return arg2;
+}
 
 void func_801BD114(u8 step) {
     g_CurrentEntity->step = step;
