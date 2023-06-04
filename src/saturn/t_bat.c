f193497c6f29a085c02818da8d215c455e3fbf55
sozud <sozud@users.noreply.github.com>
2023-06-03 23:43:42 -0700

    Extract Alchemy Lab, decompile func_060e1ff8

--- src/saturn/t_bat.c
+++ src/saturn/t_bat.c
@@ -1,13 +1,5 @@
 #include "inc_asm.h"
-
-typedef signed char s8;
-typedef signed short s16;
-typedef signed int s32;
-typedef signed long long s64;
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long long u64;
+#include "sattypes.h"
 
 // entity layout is different somehow
 typedef struct Entity {
