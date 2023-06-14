f7ca959cd1b2f8286d6e6b3266f90ef7a0f3d455
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-06-14 23:04:06 +0100

    HD merge 5087C

--- include/include_asm.h
+++ include/include_asm.h
@@ -7,6 +7,18 @@
 #ifndef PERMUTER
 
 #ifndef INCLUDE_ASM
+
+#ifdef INCLUDE_ASM_NEW
+#define INCLUDE_ASM(FOLDER, NAME)                                              \
+    __asm__(".section .text\n"                                                 \
+            "\t.align\t2\n"                                                    \
+            "\t.globl\t" #NAME "\n"                                            \
+            "\t.ent\t" #NAME "\n" #NAME ":\n"                                  \
+            ".include \"asm/" VERSION "/" FOLDER "/" #NAME ".s\"\n"            \
+            "\t.set reorder\n"                                                 \
+            "\t.set at\n"                                                      \
+            "\t.end\t" #NAME);
+#else
 #define INCLUDE_ASM(FOLDER, NAME)                                              \
     __asm__(".section .text\n"                                                 \
             "\t.align\t2\n"                                                    \
@@ -16,7 +28,8 @@
             "\t.set reorder\n"                                                 \
             "\t.set at\n"                                                      \
             "\t.end\t" #NAME);
-//".include \"asm/" VERSION "/" FOLDER "/" #NAME ".s\"\n"
+#endif
+
 #endif
 
 // omit .global
