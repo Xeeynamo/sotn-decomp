1b02a3aba758412b992c923592aec0f037c23a68
sozud <122322823+sozud@users.noreply.github.com>
2023-06-15 14:22:28 -0700

    Create Saturn asm directories by type (#251)
    Previously all the Saturn asm was placed in the same folder. Now there
    are folders for matching, nonmatching and data. The C files are scanned
    for the INCLUDE_ASM macro similarly to splat. This makes it easier to
    keep track of which functions still need to be worked on.
    
    Co-authored-by: sozud <sozud@users.noreply.github.com>

--- src/saturn/t_bat.c
+++ src/saturn/t_bat.c
@@ -12,25 +12,25 @@ typedef struct Entity {
     u16 subId;
 } Entity;
 
-INCLUDE_ASM("asm/saturn/t_bat", d60CF000, d_060CF000);
-INCLUDE_ASM("asm/saturn/t_bat", f60CF060, func_060CF060);
-INCLUDE_ASM("asm/saturn/t_bat", f60CF294, func_060CF294);
-INCLUDE_ASM("asm/saturn/t_bat", f60CF2E8, func_060CF2E8);
-INCLUDE_ASM("asm/saturn/t_bat", f60CF410, func_060CF410);
-INCLUDE_ASM("asm/saturn/t_bat", f60CF5F4, func_060CF5F4);
-INCLUDE_ASM("asm/saturn/t_bat", f60CF6B4, func_060CF6B4);
-INCLUDE_ASM("asm/saturn/t_bat", f60CFB00, func_060CFB00);
-INCLUDE_ASM("asm/saturn/t_bat", f60CFC48, func_060CFC48);
-INCLUDE_ASM("asm/saturn/t_bat", f60D0490, func_060D0490);
+INCLUDE_ASM("asm/saturn/t_bat/data", d60CF000, d_060CF000);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60CF060, func_060CF060);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60CF294, func_060CF294);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60CF2E8, func_060CF2E8);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60CF410, func_060CF410);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60CF5F4, func_060CF5F4);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60CF6B4, func_060CF6B4);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60CFB00, func_060CFB00);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60CFC48, func_060CFC48);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D0490, func_060D0490);
 
 void f60D0938() {}
 void f60D0944() {}
 void f60D0950() {}
 void f60D095C() {}
-INCLUDE_ASM("asm/saturn/t_bat", f60D0968, func_060D0968);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D0968, func_060D0968);
 void f60D0A4C() {}
 void f60D0A58() {}
-INCLUDE_ASM("asm/saturn/t_bat", f60D0A64, func_060D0A64);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D0A64, func_060D0A64);
 void f60D1010() {}
 void f60D101C() {}
 void f60D1028() {}
@@ -49,7 +49,7 @@ void func_80173C2C(Entity* entity) {
     func_0600FFB8(entity); // DestroyEntity
 }
 
-INCLUDE_ASM("asm/saturn/t_bat", f60D1070, func_060D1070);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D1070, func_060D1070);
 
 // PSX: TT_000:func_80173E78
 // SAT: T_BAT:f_021B8 / func_060D11B8
@@ -69,7 +69,7 @@ s32 func_80173E78(s32 arg0, s32 arg1) {
     return arg0;
 }
 
-INCLUDE_ASM("asm/saturn/t_bat", f60D11DC, func_060D11DC);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D11DC, func_060D11DC);
 
 // PSX: func_80173F30
 // SAT: func_060D1224
@@ -80,7 +80,7 @@ s32 func_80173F30(Entity* entity, s16 x, s16 y) {
            0xFFF; // was entity->posY.i.hi
 }
 
-INCLUDE_ASM("asm/saturn/t_bat", f60D125C, func_060D125C);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D125C, func_060D125C);
 
 s32 func_0600F914(s32, s32);
 
@@ -95,10 +95,10 @@ s32 func_80173FE8(Entity* entity, s32 x, s32 y) {
                          diffX); // SquareRoot12
 }
 
-INCLUDE_ASM("asm/saturn/t_bat", f60D12DC, func_060D12DC);
-INCLUDE_ASM("asm/saturn/t_bat", f60D141C, func_060D141C);
-INCLUDE_ASM("asm/saturn/t_bat", f60D1640, func_060D1640);
-INCLUDE_ASM("asm/saturn/t_bat", f60D16D0, func_060D16D0);
-INCLUDE_ASM("asm/saturn/t_bat", f60D1784, func_060D1784);
-INCLUDE_ASM("asm/saturn/t_bat", f60D1808, func_060D1808);
-INCLUDE_ASM("asm/saturn/t_bat", d60D1858, d_060D1858);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D12DC, func_060D12DC);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D141C, func_060D141C);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D1640, func_060D1640);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D16D0, func_060D16D0);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D1784, func_060D1784);
+INCLUDE_ASM("asm/saturn/t_bat/f_nonmat", f60D1808, func_060D1808);
+INCLUDE_ASM("asm/saturn/t_bat/data", d60D1858, d_060D1858);
