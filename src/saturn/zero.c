768d0340e9d6eabdda457e5e0f00c855c957d27c
sozud <sozud@users.noreply.github.com>
2023-06-15 00:38:37 -0700

    Create Saturn asm directories by type

--- src/saturn/zero.c
+++ src/saturn/zero.c
@@ -1,8 +1,8 @@
 #include "inc_asm.h"
-INCLUDE_ASM("asm/saturn/zero", d6004080, d_06004080);
-INCLUDE_ASM("asm/saturn/zero", f600EE88, func_0600EE88);
-INCLUDE_ASM("asm/saturn/zero", d600EFBC, d_0600EFBC);
-INCLUDE_ASM("asm/saturn/zero", f600F914, func_0600F914);
-INCLUDE_ASM("asm/saturn/zero", d600F96C, d_0600F96C);
-INCLUDE_ASM("asm/saturn/zero", f600FFB8, func_0600FFB8);
-INCLUDE_ASM("asm/saturn/zero", d6010008, d_06010008);
+INCLUDE_ASM("asm/saturn/zero/data", d6004080, d_06004080);
+INCLUDE_ASM("asm/saturn/zero/f_nonmat", f600EE88, func_0600EE88);
+INCLUDE_ASM("asm/saturn/zero/data", d600EFBC, d_0600EFBC);
+INCLUDE_ASM("asm/saturn/zero/f_nonmat", f600F914, func_0600F914);
+INCLUDE_ASM("asm/saturn/zero/data", d600F96C, d_0600F96C);
+INCLUDE_ASM("asm/saturn/zero/f_nonmat", f600FFB8, func_0600FFB8);
+INCLUDE_ASM("asm/saturn/zero/data", d6010008, d_06010008);
