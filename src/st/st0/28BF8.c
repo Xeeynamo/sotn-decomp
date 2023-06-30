21fde404fc067a3f5e705ea11f7143d496169a6b
sonicdcer <alejandro.asenjo88@gmail.com>
2023-06-30 03:55:34 -0300

    refactor func_801AA218

--- src/st/st0/28BF8.c
+++ src/st/st0/28BF8.c
@@ -18,12 +18,9 @@ INCLUDE_ASM("asm/us/st/st0/nonmatchings/28BF8", func_801A910C);
 INCLUDE_ASM("asm/us/st/st0/nonmatchings/28BF8", EntityDialogue);
 
 void func_801AA218(s16 arg0) {
-    s16 temp_a1;
-    s16 temp_v1;
-    s16 var_v0;
+    s16 temp_a1 = ((0xE0 - arg0) / 2) + 0x80;
+    s16 temp_v1 = temp_a1 - *D_8009740C;
 
-    temp_a1 = ((0xE0 - arg0) / 2) + 0x80;
-    temp_v1 = temp_a1 - *D_8009740C;
     if (temp_v1 >= 3) {
         *D_8009740C = *D_8009740C + 2;
     } else if (temp_v1 < -2) {
