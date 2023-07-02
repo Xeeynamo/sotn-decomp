405695bf742b8c519107d0effc76a7b9d6438512
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-07-02 17:48:23 +0100

    Saturn: Extract warp program

---
+++ src/saturn/warp.c
@@ -0,0 +1,11 @@
+#include "inc_asm.h"
+INCLUDE_ASM("asm/saturn/warp/data", d60DC000, d_060DC000);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC040, func_060DC040);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC4D4, func_060DC4D4);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC7D8, func_060DC7D8);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DCAFC, func_060DCAFC);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DCE50, func_060DCE50);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DCF5C, func_060DCF5C);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DD038, func_060DD038);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DD894, func_060DD894);
+INCLUDE_ASM("asm/saturn/warp/data", d60DDD74, d_060DDD74);
