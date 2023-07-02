b7e130db7d23668952d13a4a24c0726adc6f9383
Alejandro Asenjo Nitti <96613413+sonicdcer@users.noreply.github.com>
2023-07-02 20:34:50 -0300

    ST0 func_801A8328 (#307)

--- src/st/st0/27D64.c
+++ src/st/st0/27D64.c
@@ -1,23 +1,22 @@
 #include "st0.h"
 
-void func_801A7D64(Entity* arg0) {
+void func_801A7D64(Entity* self) {
     s32 temp_v0;
-    ObjInit2* temp_s0 = &D_80180638[arg0->params];
+    ObjInit2* obj = &D_80180638[self->params];
 
-    if (arg0->step == 0) {
+    if (self->step == 0) {
         InitializeEntity(D_801805BC);
-        arg0->animSet = temp_s0->animSet;
-        arg0->zPriority = temp_s0->zPriority;
-        arg0->unk5A = temp_s0->unk4.s;
-        arg0->palette = temp_s0->palette;
-        arg0->unk19 = temp_s0->unk8;
-        arg0->blendMode = temp_s0->blendMode;
-        temp_v0 = temp_s0->unkC;
-        if (temp_v0 != 0) {
-            arg0->flags = temp_v0;
+        self->animSet = obj->animSet;
+        self->zPriority = obj->zPriority;
+        self->unk5A = obj->unk4.s;
+        self->palette = obj->palette;
+        self->unk19 = obj->unk8;
+        self->blendMode = obj->blendMode;
+        if (obj->unkC != 0) {
+            self->flags = obj->unkC;
         }
     }
-    AnimateEntity(temp_s0->unk10, arg0);
+    AnimateEntity(obj->unk10, self);
 }
 
 bool func_801A7E2C(Entity* self) {
@@ -96,7 +95,90 @@ void EntityLockCamera(Entity* entity) {
 
 INCLUDE_ASM("asm/us/st/st0/nonmatchings/27D64", func_801A805C);
 
-INCLUDE_ASM("asm/us/st/st0/nonmatchings/27D64", func_801A8328);
+void func_801A8328(Entity* self) {
+    s16 firstPrimIndex;
+    Entity* newEntity;
+    Collider collider;
+    Primitive* prim;
+    s32 accelX;
+    s16 temp2;
+
+    switch (self->step) {
+    case 0:
+        if (self->params & 0x100) {
+            InitializeEntity(D_801805D4);
+            self->animSet = ANIMSET_OVL(4);
+            self->unk5A = 0x5B;
+            self->palette = 0x212;
+            self->animCurFrame = 0x15;
+            self->zPriority = 0x6A;
+            self->step = 256;
+            break;
+        }
+
+        InitializeEntity(D_8018058C);
+        firstPrimIndex = g_api.AllocPrimitives(PRIM_GT4, 2);
+        if (firstPrimIndex != -1) {
+            prim = &g_PrimBuf[firstPrimIndex];
+            self->primIndex = firstPrimIndex;
+            LOW(self->ext.generic.unk7C.s) = prim;
+            self->flags |= FLAG_HAS_PRIMS;
+            func_801BD80C(prim, firstPrimIndex);
+            prim->tpage = 0x16;
+            prim->clut = 0x216;
+            prim->u0 = prim->u2 = 0x98;
+            prim->u1 = prim->u3 = 0xA7;
+            temp2 = (self->params * 16) - 124;
+            prim->v2 = prim->v3 = temp2 + 15;
+            prim->v0 = prim->v1 = temp2;
+            prim->next->x1 = self->posX.i.hi;
+            prim->next->y0 = self->posY.i.hi;
+            LOH(prim->next->r2) = 0x10;
+            LOH(prim->next->b2) = 0x10;
+            prim->next->b3 = 0x80;
+            prim->priority = self->zPriority;
+            prim->blendMode = 2;
+            accelX = ((Random() & 7) << 0xC) + 0x8000;
+            self->accelerationX = accelX;
+            if (self->facing == 0) {
+                self->accelerationX = -accelX;
+            }
+            self->accelerationY = ((Random() & 7) << 0xC) - 0x8000;
+        } else {
+            DestroyEntity(self);
+            break;
+        }
+
+    case 1:
+        MoveEntity();
+
+        prim = LOW(self->ext.generic.unk7C.s);
+        self->accelerationY += 0x2000;
+        prim->next->x1 = self->posX.i.hi;
+        prim->next->y0 = self->posY.i.hi;
+
+        if (self->facing != 0) {
+            prim->next->tpage += 0x10;
+        } else {
+            prim->next->tpage -= 0x10;
+        }
+
+        func_801BD0C0(prim);
+
+        g_api.CheckCollision(self->posX.i.hi, (s16)(self->posY.i.hi + 8),
+                             &collider, 0);
+        if (collider.effects & EFFECT_SOLID) {
+            g_api.PlaySfx(0x691);
+            newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
+            if (newEntity != NULL) {
+                CreateEntityFromCurrentEntity(E_EXPLOSION, newEntity);
+                newEntity->params = 0;
+            }
+            DestroyEntity(self);
+            break;
+        }
+    }
+}
 
 void func_801A8620(Entity* entity) {
     s16 dist;
