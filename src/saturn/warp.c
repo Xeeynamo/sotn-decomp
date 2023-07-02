8fcf99ad5f71323aee5a44edaad3f4a5c81f3c91
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-07-02 19:20:04 +0100

    Add new Saturn symbols

--- src/saturn/warp.c
+++ src/saturn/warp.c
@@ -1,11 +1,32 @@
 #include "inc_asm.h"
+#include "sattypes.h"
+
 INCLUDE_ASM("asm/saturn/warp/data", d60DC000, d_060DC000);
-INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC040, func_060DC040);
-INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC4D4, func_060DC4D4);
-INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC7D8, func_060DC7D8);
-INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DCAFC, func_060DCAFC);
+
+void EntityWarpRoom(Entity* self);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC040, EntityWarpRoom);
+
+// inlined in case 0 of EntityWarpRoom on PSX
+void WarpRoomInit(Entity* self);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC4D4, WarpRoomInit);
+
+// inlined in the final chunk of code from EntityWarpRoom on PSX
+void WarpRoomAnimateBg(Entity* self);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DC7D8, WarpRoomAnimateBg);
+
+void EntityWarpSmallRocks(Entity* self);
+INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DCAFC, EntityWarpSmallRocks);
+
+void func_060DCE50(s16 arg0);
 INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DCE50, func_060DCE50);
+
+void func_060DCF5C(u32 arg0);
 INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DCF5C, func_060DCF5C);
+
+void func_060DD038(Entity* self);
 INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DD038, func_060DD038);
+
+void func_060DD894(Entity* self);
 INCLUDE_ASM("asm/saturn/warp/f_nonmat", f60DD894, func_060DD894);
+
 INCLUDE_ASM("asm/saturn/warp/data", d60DDD74, d_060DDD74);
