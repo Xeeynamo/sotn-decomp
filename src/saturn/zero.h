6b2f2c43ec88e022f09153dd25d2dfc62824542e
sozud <122322823+sozud@users.noreply.github.com>
2023-07-05 16:14:09 -0700

    Decompile SetStageOverlayAddress (#315)
    This match seems odd since I would have expected the addresses to be
    literals. With @sonicdcer
    
    I'll rebase when the other PR is merged.
    
    https://decomp.me/scratch/KYz2v
    https://decomp.me/scratch/MyAL

---
+++ src/saturn/zero.h
@@ -0,0 +1,12 @@
+#ifndef ZERO_BIN_H
+#define ZERO_BIN_H
+#include "sattypes.h"
+extern s32 DAT_0601ac28[];
+extern s32 DAT_0601ac2c[];
+extern s32 DAT_0601ac30[];
+extern s32 DAT_0601ac34[];
+extern s32 DAT_0601ac38[];
+extern s32 DAT_0601ac3c[];
+extern s32 DAT_0601ac40[];
+extern s32 DAT_0601ac44[];
+#endif
