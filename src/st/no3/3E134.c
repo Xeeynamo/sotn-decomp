2d753d162f1802883c8c9be0191c825804183a94
Alejandro Asenjo Nitti <96613413+sonicdcer@users.noreply.github.com>
2023-07-07 13:43:19 -0300

    ST0 func_801B4AF0 & duplicates (#321)
    co-authored-by: @Mc-muffin

--- src/st/no3/3E134.c
+++ src/st/no3/3E134.c
@@ -675,7 +675,36 @@ void PreventEntityFromRespawning(Entity* entity) {
 
 #include "st/AnimateEntity.h"
 
-INCLUDE_ASM("asm/us/st/no3/nonmatchings/3E134", func_801C4E4C);
+u8 func_801C4E4C(u8 frames[], Entity* self, u8 arg2) {
+    u16 animFrameStart = self->animFrameIdx * 2;
+    u8* var_s1 = &frames[animFrameStart];
+    s16 var_a1 = 0;
+
+    if (self->animFrameDuration == 0) {
+        if (*var_s1 != 0) {
+            if (*var_s1 == 0xFF) {
+                return 0;
+            }
+            self->animFrameDuration = *var_s1++ + (u8)self->ext.stub[0x3F];
+            self->animCurFrame = *var_s1++;
+            self->animFrameIdx++;
+            var_a1 = 128;
+        } else {
+            var_s1 = frames;
+            self->animFrameIdx = 0;
+            self->animFrameDuration = 0;
+            self->ext.stub[0x3F] = (arg2 * Random()) >> 8;
+            self->animFrameDuration = *var_s1++ + (u8)self->ext.stub[0x3F];
+            self->animCurFrame = *var_s1;
+            self->animFrameIdx++;
+            return 0;
+        }
+    }
+    self->animFrameDuration--;
+    self->animCurFrame = var_s1[-1];
+    var_a1 |= 1;
+    return var_a1;
+}
 
 /*
  * Returns the absolute distance from g_CurrentEntity to player in the X Axis
