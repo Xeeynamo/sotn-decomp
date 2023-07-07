0e6528e047c19c9893b0da92711d897fe258e02c
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-07-07 19:05:12 +0100

    func_8018BA10 and duplicates

--- src/st/wrp/6FD0.c
+++ src/st/wrp/6FD0.c
@@ -2237,69 +2237,58 @@ void FallEntity(void) {
     }
 }
 
-#ifndef NON_MATCHING
-INCLUDE_ASM("asm/us/st/wrp/nonmatchings/6FD0", func_8018BA10);
-#else
-s32 func_8018BA10(u16* arg0) {
-    s16 new_var;
-    Collider res;
-    Collider resBack;
-    s16 i;
+s32 func_8018BA10(u16* sensors) {
+    Collider col;
+    Collider colBack;
     s16 x;
     s16 y;
-    s32 var_v0;
+    s16 i;
+
     MoveEntity();
     FallEntity();
     if (g_CurrentEntity->accelerationY >= 0) {
-        i = g_CurrentEntity->posX.i.hi;
-        x = i;
+        x = g_CurrentEntity->posX.i.hi;
         y = g_CurrentEntity->posY.i.hi;
         for (i = 0; i < 4; i++) {
-            x += *(arg0++);
-            y += *(arg0++);
-            g_api.CheckCollision(x, y, &res, 0);
-            if (res.unk0 & 0x8000) {
+            x += *sensors++;
+            y += *sensors++;
+            g_api.CheckCollision(x, y, &col, 0);
+            if (col.effects & EFFECT_UNK_8000) {
                 if (i == 1) {
-                    if (res.unk0 & 1) {
-                        g_api.CheckCollision(x, y - 8, &resBack, 0);
-                        if (!(resBack.unk0 & 1)) {
-                            new_var = *((s16*)(&res.unk18));
+                    if (col.effects & EFFECT_SOLID) {
+                        g_api.CheckCollision(x, y - 8, &colBack, 0);
+                        if (!(colBack.effects & EFFECT_SOLID)) {
+                            g_CurrentEntity->posY.i.hi =
+                                (u16)g_CurrentEntity->posY.i.hi + 4 + col.unk18;
                             g_CurrentEntity->accelerationX = 0;
                             g_CurrentEntity->accelerationY = 0;
-                            g_CurrentEntity->posY.i.hi =
-                                (((u16)g_CurrentEntity->posY.i.hi) + 4) +
-                                (*((s16*)(&res.unk18)));
-                            g_CurrentEntity->flags &= ~0x10000000;
+                            g_CurrentEntity->flags &= ~FLAG_UNK_10000000;
                             return 1;
                         }
                     }
                     continue;
                 }
             }
-            if ((res.unk0 & 5) && (i != 1)) {
-                if (res.unk0 & 4) {
-                    g_CurrentEntity->flags &= ~0x10000000;
+            if (col.effects & EFFECT_NOTHROUGH && i != 1) {
+                if (col.effects & EFFECT_QUICKSAND) {
+                    g_CurrentEntity->flags &= ~FLAG_UNK_10000000;
                     return 4;
                 }
-                g_api.CheckCollision(x, y - 8, &resBack, 0);
-                if (!(resBack.unk0 & 1)) {
-                    new_var = *((s16*)(&res.unk18));
-                    x = g_CurrentEntity->posY.i.hi;
-                    x = x + new_var;
-                    new_var = x;
+                g_api.CheckCollision(x, y - 8, &colBack, 0);
+                if (!(colBack.effects & EFFECT_SOLID)) {
+                    g_CurrentEntity->posY.i.hi =
+                        g_CurrentEntity->posY.i.hi + col.unk18;
                     g_CurrentEntity->accelerationX = 0;
                     g_CurrentEntity->accelerationY = 0;
-                    g_CurrentEntity->posY.i.hi = new_var;
-                    g_CurrentEntity->flags &= ~0x10000000;
+                    g_CurrentEntity->flags &= ~FLAG_UNK_10000000;
                     return 1;
                 }
             }
         }
     }
-    g_CurrentEntity->flags |= 0x10000000;
+    g_CurrentEntity->flags |= FLAG_UNK_10000000;
     return 0;
 }
-#endif
 
 INCLUDE_ASM("asm/us/st/wrp/nonmatchings/6FD0", func_8018BC88);
 
