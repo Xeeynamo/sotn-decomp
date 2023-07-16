c364ae38342f438928864d636332d9ce77ea3d9e
sozud <122322823+sozud@users.noreply.github.com>
2023-07-16 12:44:18 -0700

    Decompile func_06008588 (#363)
    This handles some sort of conversion and transfer of the background
    graphics tiles, a little info about it here
    https://github.com/Xeeynamo/sotn-decomp/wiki/Saturn-Internals#graphics-info
    
    https://decomp.me/scratch/TXaHJ
    
    ---------
    
    Co-authored-by: Luciano Ciccariello <Xeeynamo@users.noreply.github.com>

--- src/saturn/zero.c
+++ src/saturn/zero.c
@@ -133,7 +133,57 @@ INCLUDE_ASM("asm/saturn/zero/f_nonmat", f600841C, func_0600841C);
 INCLUDE_ASM("asm/saturn/zero/f_nonmat", f6008464, func_06008464);
 INCLUDE_ASM("asm/saturn/zero/f_nonmat", f6008488, func_06008488);
 INCLUDE_ASM("asm/saturn/zero/f_nonmat", f6008524, func_06008524);
-INCLUDE_ASM("asm/saturn/zero/f_nonmat", f6008588, func_06008588);
+
+// Handles transfer of background tile graphics
+// func_06008588
+void func_06008588(int param_1) {
+    s32 cnt;
+    struct Unk0605d6c0* puVar5;
+    struct Unk0605CD90* puVar6;
+
+    puVar5 = &DAT_0605d6c0[param_1 * 4];
+    puVar6 = &DAT_0605CD90[param_1];
+    if (puVar5->flags == 0) {
+        return;
+    }
+    if ((puVar5->flags & 1) != 0) {
+        cnt = func_0600F96C(puVar6->unkc, &DAT_060485e0, puVar6->unk18);
+        DmaScroll(&DAT_060485e0, puVar6->dst0, cnt);
+    }
+    if ((puVar5->flags & 2) != 0) {
+        cnt = func_0600F96C(puVar6->unk10, &DAT_060485e0, puVar6->unk1c);
+        DmaScroll(&DAT_060485e0, puVar6->dst4, cnt);
+    }
+    if ((puVar5->flags & 4) != 0) {
+        if (DAT_060086d4 == 4) {
+            func_060089F0(puVar6);
+        } else {
+            func_0600871C(puVar6, &DAT_0605c680, param_1);
+        }
+    }
+    if ((puVar5->flags & 8) != 0) {
+        DmaScroll(puVar5->src, puVar5->dest, puVar5->cnt);
+    }
+    if ((puVar5->flags & 0x10) != 0) {
+        DmaScroll(puVar5->src, puVar5->dest, puVar5->cnt);
+    }
+    if ((puVar5->flags & 0x20) != 0) {
+        DmaScroll(puVar5->src, puVar5->dest, puVar5->cnt);
+    }
+    if ((puVar5->flags & 0x40) != 0) {
+        cnt = func_0600F96C(puVar6->unkc, DMA_SRC_ADDR, puVar6->unk18);
+        DmaScroll(DMA_SRC_ADDR, puVar6->dst0, cnt);
+    }
+    if ((puVar5->flags & 0x80) != 0) {
+        cnt = func_0600F96C(puVar6->unk10, DMA_SRC_ADDR, puVar6->unk1c);
+        DmaScroll(DMA_SRC_ADDR, puVar6->dst4, cnt);
+    }
+    puVar5->flags = 0;
+    puVar5->cnt = 0;
+    puVar5->dest = 0;
+    puVar5->src = 0;
+    return;
+}
 
 // _DMA_SCROLL
 // func_060086E8
