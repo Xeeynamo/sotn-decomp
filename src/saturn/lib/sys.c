5fee820eb45ff1c768fa4c97cf845bf7522b254b
sozud <122322823+sozud@users.noreply.github.com>
2023-07-21 14:39:37 -0700

    Split saturn libs, add function (#385)
    This splits the saturn libs into individual files and adds one function
    to test the workflow. The splitter can't split multiple files at the
    moment so the linker file is made manually. I plan to fix this
    eventually. I updated the splitter to not overwrite linker files that
    already exist. I'm open to suggestions on the directory structure but
    this seemed like the easiest way given the dosemu copying stuff.

---
+++ src/saturn/lib/sys.c
@@ -0,0 +1,14 @@
+#include "sattypes.h"
+#include "inc_asm.h"
+
+// SEGA_SYS.A
+const char* sys_version = "SYS Version 2.50 1997-06-10";
+const u32 pad_06030af8 = 0x00000000;
+
+// _SYS_SetUintMacSave
+INCLUDE_ASM("asm/saturn/zero/f_nonmat", f6030AFC, func_06030AFC);
+
+// _SYS_GetUintMacSave
+INCLUDE_ASM("asm/saturn/zero/f_nonmat", f6030B88, func_06030B88);
+
+INCLUDE_ASM_NO_ALIGN("asm/saturn/zero/data", d6030BF4, d_6030BF4);
---
+++ src/saturn/lib/xpt.h
@@ -0,0 +1,15 @@
+#ifndef XPT_H
+#define XPT_H
+typedef unsigned char Uint8;
+typedef signed char Sint8;
+typedef unsigned short Uint16;
+typedef signed short Sint16;
+typedef unsigned long Uint32;
+typedef signed long Sint32;
+typedef float Float32;
+typedef double Float64;
+
+typedef int Int;
+
+typedef int Bool;
+#endif
