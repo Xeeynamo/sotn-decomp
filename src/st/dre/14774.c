df8296712f60c5c50c1a7fe4578ca85eb468fba6
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-07-22 15:34:30 -0300

    func_801947C8 duplicate

--- src/st/dre/14774.c
+++ src/st/dre/14774.c
@@ -10,7 +10,65 @@ void func_80194774(void) {
     D_801A3EDE = D_801A3EE0 + 20;
 }
 
-INCLUDE_ASM("asm/us/st/dre/nonmatchings/14774", func_801947C8);
+s32 func_801947C8(s32 arg0) {
+    Primitive* prim;
+    s16 firstPrimIndex;
+
+    firstPrimIndex = g_api.AllocPrimitives(PRIM_SPRT, 7);
+    D_801A3F10[0] = firstPrimIndex;
+    if (firstPrimIndex == -1) {
+        D_801A3F10[0] = 0;
+        return 0;
+    }
+    D_801A3ED8 = arg0;
+    D_801A3F14 = 0;
+    D_801A3F0C = -1;
+    D_801A3F08 = -1;
+    func_80194774();
+
+    if (prim && prim) { // !FAKE
+    }
+
+    prim = D_801A3EF0[0] = &g_PrimBuf[D_801A3F10[0]];
+
+    prim->blendMode = 8;
+    prim = D_801A3EF0[1] = prim->next;
+
+    prim->blendMode = 8;
+    prim = D_801A3EF0[2] = prim->next;
+
+    prim->blendMode = 8;
+    prim = D_801A3EF0[3] = prim->next;
+
+    prim->blendMode = 8;
+    prim = D_801A3EF0[4] = prim->next;
+
+    prim->blendMode = 8;
+    prim = D_801A3EF0[5] = prim->next;
+
+    prim->type = 4;
+    prim->blendMode = 8;
+
+    prim = prim->next;
+    prim->type = 3;
+    prim->r0 = prim->r1 = prim->r2 = prim->r3 = 0xFF;
+    prim->b0 = prim->b1 = prim->b2 = prim->b3 = prim->g0 = prim->g1 = prim->g2 =
+        prim->g3 = 0;
+    prim->x0 = prim->x2 = 4;
+    prim->x1 = prim->x3 = 0xF8;
+    prim->priority = 0x1FD;
+    prim->blendMode = 8;
+
+    prim = prim->next;
+    prim->type = 1;
+    prim->x0 = 3;
+    prim->y0 = 0x2F;
+    prim->v0 = 0x4A;
+    prim->r0 = prim->g0 = prim->b0 = 0xFF;
+    prim->priority = 0x1FC;
+    prim->blendMode = 8;
+    return 1;
+}
 
 void func_8019498C(s16 yOffset) {
     RECT rect;
