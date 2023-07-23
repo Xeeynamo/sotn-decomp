71654e0c86a92cda1ed6a4a82ecf05ffbcf9a4d3
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-07-23 13:31:06 -0300

    EntityCSMoveAlucard

--- src/st/dre/dre.h
+++ src/st/dre/dre.h
@@ -50,7 +50,7 @@ LayoutEntity* D_801802F4[];
 
 /* *** Initializers *** */
 extern u16 g_InitializeData0[];
-extern u16 D_8018047C[];
+extern u16 D_8018047C[]; // EntityCSMoveAlucard
 extern u16 D_80180488[]; // EntityBackgroundClouds
 extern u16 D_80180494[];
 extern u16 D_801804A0[];
@@ -93,6 +93,7 @@ extern u8 D_80180780[]; // Animation
 extern const u8 D_80180794[];
 extern s32 D_801807F8[];
 extern u8 D_80180830[];
+extern u8 D_80180944[]; // Animation
 extern s16 D_80180D80[];
 extern u16 g_InitializeEntityData0[];
 extern u16 D_801804AC[];
