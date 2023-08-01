f31eb3b4066d17ed03db670a859dfe8714d7cdc7
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2023-08-01 22:53:40 +0100

    Splat libsnd out of main (#423)
    Fully splits `libsnd` into individual files following the original PSY-Q
    SDK file names. The same is done with `libcd` and `libspu` just to
    define the boundaries of `libsnd`. I also correctly names **all** the
    function symbols from the sound library with their right offset. We
    previously had some wrong symbol offsets, leading to the inability to
    create their C counterparts with `INCLUDE_ASM`. This PR will start to
    open the doors to decompiling `main` or understand better how `DRA`
    communicates with `main`.
    
    This took an incredible amount of time as I had to do it manually. I
    would have written a script, but there are some variants I would prefer
    to handle manually. To achieve this I used the [PSY-Q
    signatures](https://github.com/lab313ru/psx_psyq_signatures/blob/e9e46e7e133ef275a79bfce650924f98edb086bc/350/LIBSND.LIB.json).
    I also discovered the game uses PSY-Q 3.5, which brings some piece of
    mind to what they used to develop the game.
    
    After this PR my plan is to:
    * Finish to split the other PSY-Q libs
    * Move `asm/main/*.s` into their own C files
    * Include `main` progress in our reporting

---
+++ src/main/psxsdk/libcd/c_011.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libcd/c_011", StCdInterrupt);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libcd/c_011", mem2mem);
+
+INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libcd/c_011", dma_execute);
---
+++ src/main/psxsdk/libsnd/replay.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/replay", _SsSndReplay);
