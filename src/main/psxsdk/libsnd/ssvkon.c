ba09901f67ffa5b12b4179ae620df984d697c65f
sozud <122322823+sozud@users.noreply.github.com>
2023-08-02 12:01:22 -0700

    Decompile more libspu

--- src/main/psxsdk/libsnd/ssvkon.c
+++ src/main/psxsdk/libsnd/ssvkon.c
@@ -1,3 +1,8 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/ssvkon", SsVoKeyOn);
+void SpuVmSeKeyOn(s32, s32, u16, u8, u16, u16);
+
+s32 SsVoKeyOn(s32 vab_pro, s32 pitch, u16 volL, u16 volR) {
+    SpuVmSeKeyOn(
+        (vab_pro << 8) >> 0x10, vab_pro & 0xFF, pitch >> 8, pitch, volL, volR);
+}
--- src/main/psxsdk/libsnd/ut_rfb.c
+++ src/main/psxsdk/libsnd/ut_rfb.c
@@ -1,4 +1,12 @@
 #define INCLUDE_ASM_NEW
 #include "common.h"
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libsnd/ut_rfb", SsUtSetReverbFeedback);
+extern s32 D_8003C758;
+extern s32* _svm_rattr;
+
+void SsUtSetReverbFeedback(s16 feedback) {
+    s32* temp = &_svm_rattr;
+    *temp = 0x10;
+    D_8003C758 = feedback;
+    func_80029300(temp);
+}
