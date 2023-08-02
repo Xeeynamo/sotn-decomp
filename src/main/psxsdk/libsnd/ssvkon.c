2b1d3f6a07c0811e7783d684c98742ceba4062b2
sozud <122322823+sozud@users.noreply.github.com>
2023-08-02 16:17:20 -0700

    fixes

--- src/main/psxsdk/libsnd/ssvkon.c
+++ src/main/psxsdk/libsnd/ssvkon.c
@@ -1,8 +1,8 @@
 #include "common.h"
 
-void SpuVmSeKeyOn(s32, s32, u16, u8, u16, u16);
+void SpuVmSeKeyOn(s32, u8, u16, u8, u16, u16);
 
 s32 SsVoKeyOn(s32 vab_pro, s32 pitch, u16 volL, u16 volR) {
     SpuVmSeKeyOn(
-        (vab_pro << 8) >> 0x10, vab_pro & 0xFF, pitch >> 8, pitch, volL, volR);
+        (vab_pro << 8) >> 0x10, vab_pro, pitch >> 8, pitch, volL, volR);
 }
