1da073d523852533ff591651878f9ce834af845e
sozud <122322823+sozud@users.noreply.github.com>
2023-08-01 19:00:06 -0700

    more funcs

--- src/main/psxsdk/libsnd/ssvol.c
+++ src/main/psxsdk/libsnd/ssvol.c
@@ -2,8 +2,16 @@
 
 INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/ssvol", _SsSndSetVol);
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/ssvol", SsSeqSetVol);
+void SpuVmSetSeqVol(s16, u16, u16, s32);
+
+void SsSeqSetVol(s16 arg0, s32 arg1, s32 arg2) {
+    SpuVmSetSeqVol(arg0, arg1, arg2, 0);
+}
 
 INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/ssvol", SsSepSetVol);
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/ssvol", SsSeqGetVol);
+s32 SpuVmGetSeqVol(s16, s16*, s16*);
+
+void SsSeqGetVol(s16 access_num, s16 seq_num, s16* voll, s16* volr) {
+    SpuVmGetSeqVol(access_num | (seq_num << 8), voll, volr);
+}
