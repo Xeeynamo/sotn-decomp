fc16f225379508c25261fdb63789eb6e43805548
sozud <122322823+sozud@users.noreply.github.com>
2023-08-02 16:20:53 -0700

    Decompile more libspu (#432)

--- src/main/psxsdk/libsnd/ssstart.c
+++ src/main/psxsdk/libsnd/ssstart.c
@@ -8,7 +8,15 @@ void SsStart(void) { _SsStart(1); }
 
 void SsStart2(void) { _SsStart(0); }
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/ssstart", _SsTrapIntrVSync);
+extern void (*D_80032EF8)();
+extern void (*D_80032EFC)();
+
+void _SsTrapIntrVSync(void) {
+    if (D_80032EFC != NULL) {
+        D_80032EFC();
+    }
+    D_80032EF8();
+}
 
 INCLUDE_ASM(
     "asm/us/main/nonmatchings/psxsdk/libsnd/ssstart", _SsSeqCalledTbyT_1per2);
