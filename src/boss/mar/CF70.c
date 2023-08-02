b81d709f9b9d3645df570d3305ff6bc781b845b1
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-08-02 01:14:46 -0300

    fix split, everything OK

--- src/boss/mar/CF70.c
+++ src/boss/mar/CF70.c
@@ -1,106 +1,106 @@
 #include "mar.h"
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018CF70);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018CF70);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018CFA0);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018CFA0);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018D29C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018D29C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018D3A4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018D3A4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018E4BC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018E4BC);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018EB60);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018EB60);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018EC24);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018EC24);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018ED3C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018ED3C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018EE54);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018EE54);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018EEA0);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018EEA0);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018EEF8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018EEF8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018EFF4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018EFF4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F108);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F108);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F154);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F154);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F1AC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F1AC);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F2A8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F2A8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F3BC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F3BC);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F534);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F534);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", CreateEntityFromCurrentEntity);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", CreateEntityFromCurrentEntity);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F65C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F65C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F6D8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F6D8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8018F750);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8018F750);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190388);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190388);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801903F4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801903F4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190470);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190470);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801904B8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801904B8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190570);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190570);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190688);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190688);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801906C4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801906C4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801906F8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801906F8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", MoveEntity);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", MoveEntity);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019076C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019076C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190798);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190798);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190A10);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190A10);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190BF8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190BF8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190C58);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190C58);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190C84);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190C84);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190CA0);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190CA0);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190D0C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190D0C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190D44);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190D44);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190D8C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190D8C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190DD4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190DD4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190E2C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190E2C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190EB8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190EB8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190EE8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190EE8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190F20);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190F20);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190F68);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190F68);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190FC8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190FC8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80190FE8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80190FE8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191004);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191004);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", InitializeEntity);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", InitializeEntity);
 
 void EntityDummy(Entity* self) {
     if (self->step == 0) {
@@ -108,93 +108,93 @@ void EntityDummy(Entity* self) {
     }
 }
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801911BC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801911BC);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801912E4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801912E4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019143C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019143C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191780);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191780);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191838);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191838);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801918BC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801918BC);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191A18);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191A18);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191A98);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191A98);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191B74);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191B74);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191C8C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191C8C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191D30);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191D30);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191D80);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191D80);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80191DA8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80191DA8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019261C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019261C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80192718);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80192718);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801927BC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801927BC);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80192DA4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80192DA4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80193054);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80193054);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80193B24);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80193B24);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80193C40);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80193C40);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019419C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019419C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019428C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019428C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019439C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019439C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801944CC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801944CC);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801945B4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801945B4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801946A4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801946A4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80194798);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80194798);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801949C8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801949C8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80194AC4);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80194AC4);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80195044);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80195044);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80195144);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80195144);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80195210);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80195210);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801952D8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801952D8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80195390);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80195390);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80195568);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80195568);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80195B48);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80195B48);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80196248);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80196248);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80196440);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80196440);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80196538);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80196538);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80196708);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80196708);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019738C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019738C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80197720);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80197720);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80197BD0);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80197BD0);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80197CBC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80197CBC);
 
 // ID 0x16
 void EntityClockRoomController(Entity* self) {
@@ -736,22 +736,22 @@ void EntityStoneDoor(Entity* self) {
 // Entity ID 0x1C
 void func_80199114(Entity* self) {}
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019911C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019911C);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80199504);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80199504);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80199654);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80199654);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80199684);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80199684);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80199700);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80199700);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80199788);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80199788);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_80199868);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_80199868);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801998BC);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801998BC);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_801998E8);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_801998E8);
 
-INCLUDE_ASM("asm/us/boss/mar/nonmatchings/AC0C", func_8019994C);
+INCLUDE_ASM("asm/us/boss/mar/nonmatchings/CF70", func_8019994C);
