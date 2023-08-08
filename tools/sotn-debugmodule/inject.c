b9c47cda10fb8735e059d8b5007591c024a76243
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-08-08 16:11:29 +0100

    Manipulate addresses to predict InjectMain location

--- tools/sotn-debugmodule/inject.c
+++ tools/sotn-debugmodule/inject.c
@@ -19,16 +19,13 @@ typedef struct {
     u32 Unk3C;
 } ServantDesc;
 
-// The INJECT value will change every time information is added to the .data or
-// .rodata section. To get a refreshed value, compile the debug module, look for
-// sotn-debugmode.map, find InjectMain and copy&paste the hex value on its left.
-const u32 INJECT_MAIN_ADDR = 0x802809b4;
-const u32 MODULE_ADDR = 0x80280000;
-const u32 LOAD_ADDR = 0x80170000;
+const u32 INJECT_MAIN_ADDR = 0x80280040; // sotn-debugmodule.map
+const u32 MODULE_ADDR = 0x80280000;      // sotn-debugmodule.ld
+const u32 LOAD_ADDR = 0x80170000;        // Familiar overlay address
 const u32 INJECT = INJECT_MAIN_ADDR - (MODULE_ADDR - LOAD_ADDR);
 
 void Dummy(Entity*);
-ServantDesc g_ServantDesc = {
+ServantDesc g_ServantDesc __attribute__((section(".inject-head"))) = {
     INJECT, Dummy, Dummy, Dummy, Dummy, Dummy, Dummy, Dummy,
     Dummy,  Dummy, Dummy, Dummy, Dummy, Dummy, Dummy, Dummy,
 
@@ -37,7 +34,7 @@ ServantDesc g_ServantDesc = {
 #define JAL(addr) ((((u32)(addr)&0x3FFFFFFU) >> 2) | 0x0C000000U)
 
 int MainLoop();
-void InjectMain(void) {
+void __attribute__((section(".inject-func"))) InjectMain(void) {
     u32* const InjectPoint = 0x800E3D00U;
     int i;
 
@@ -46,7 +43,7 @@ void InjectMain(void) {
 
     // The game loads this overlay in 0x80170000, but as this module is compiled
     // to use 0x80280000 as a base, we copy the overlay to there.
-    __builtin_memcpy((void*)0x80280000, (void*)0x80170000, 40960U);
+    __builtin_memcpy((void*)MODULE_ADDR, (void*)LOAD_ADDR, 40960U);
 
     // Now we say to 'entrypoint_sotn' to call our main every frame
     *InjectPoint = JAL(MainLoop);
