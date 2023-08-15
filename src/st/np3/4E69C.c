0a18d5e9f1b30f6f6bb6e574285593331b92e7f4
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-08-15 18:43:57 -0300

    func_801D0A00

--- src/st/np3/4E69C.c
+++ src/st/np3/4E69C.c
@@ -252,7 +252,19 @@ void EntityGurkhaSword(Entity* self) {
     }
 }
 
-INCLUDE_ASM("asm/us/st/np3/nonmatchings/4E69C", func_801D0A00);
+void func_801D0A00(s16* arg0) {
+    func_801CD91C(&g_CurrentEntity[arg0[1]]);
+    func_801CD91C(&g_CurrentEntity[arg0[0]]);
+    func_801CD91C(g_CurrentEntity + 18);
+    func_801CD83C(&g_CurrentEntity[arg0[2]]);
+    func_801CD83C(&g_CurrentEntity[arg0[3]]);
+
+    for (arg0 += 4; *arg0 != 0; arg0++) {
+        if (*arg0 != 0xFF) {
+            func_801CD83C(&g_CurrentEntity[*arg0]);
+        }
+    }
+}
 
 INCLUDE_ASM("asm/us/st/np3/nonmatchings/4E69C", func_801D0B40);
 
