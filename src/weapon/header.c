4e1ed4289d48ae9b37052dde30710ec1ab4786de
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2023-08-16 02:57:59 +0100

    Add all weapon signatures (#477)
    I poked inside the exported functions and detected all the signatures
    used. This will be helpful to keep decompiling DRA. I took the chance to
    decompile `func_ptr_80170004` from `w_015` and all the `GetWeaponId`.
    Interestingly, `w_057` and `w_058` has the wrong implementation of
    `GetWeaponId`, suggesting developers copy&pasted stuff around after all.
    
    This depends from #476 . Let's not merge before that.

--- src/weapon/header.c
+++ src/weapon/header.c
@@ -1,25 +1,9 @@
 #include <weapon.h>
-
-void EntityWeaponAttack(Entity* self);
-void func_ptr_80170004(void);
-void func_ptr_80170008(void);
-void func_ptr_8017000C(void);
-void func_ptr_80170010(void);
-void func_ptr_80170014(void);
-int func_ptr_80170018(void);
-void LoadWeaponPalette(s32 clutIndex);
-void func_ptr_80170020(void);
-void func_ptr_80170024(void);
-void func_ptr_80170028(void);
-void func_ptr_8017002C(void);
-void func_ptr_80170030(void);
-void func_ptr_80170034(void);
-void func_ptr_80170038(void);
-void func_ptr_8017003C(void);
+#include "weapon_private.h"
 
 Weapon g_Weapon = {
     EntityWeaponAttack, func_ptr_80170004, func_ptr_80170008, func_ptr_8017000C,
-    func_ptr_80170010,  func_ptr_80170014, func_ptr_80170018, LoadWeaponPalette,
-    func_ptr_80170020,  func_ptr_80170024, func_ptr_80170028, func_ptr_8017002C,
-    func_ptr_80170030,  func_ptr_80170034, func_ptr_80170038, func_ptr_8017003C,
+    func_ptr_80170010,  func_ptr_80170014, GetWeaponId,       LoadWeaponPalette,
+    func_ptr_80170020,  func_ptr_80170024, func_ptr_80170028, WeaponUnused2C,
+    WeaponUnused30,     WeaponUnused34,    WeaponUnused38,    WeaponUnused3C,
 };
--- src/weapon/weapon_private.h
+++ src/weapon/weapon_private.h
@@ -1,5 +1,28 @@
+#ifndef WEAPON_PRIVATE_H
+#define WEAPON_PRIVATE_H
+
 #define INCLUDE_ASM_NEW
 #include <weapon.h>
 
+// exported
 void EntityWeaponAttack(Entity* self);
 void LoadWeaponPalette(s32 clutIndex);
+void func_ptr_80170004(Entity* self);
+void func_ptr_80170008(Entity* self);
+void func_ptr_8017000C(Entity* self);
+void func_ptr_80170010(Entity* self);
+void func_ptr_80170014(Entity* self);
+int GetWeaponId(void);
+void func_ptr_80170020(Entity* self);
+void func_ptr_80170024(Entity* self);
+void func_ptr_80170028(Entity* self);
+void WeaponUnused2C(void);
+void WeaponUnused30(void);
+void WeaponUnused34(void);
+void WeaponUnused38(void);
+void WeaponUnused3C(void);
+
+// internals
+void DestroyEntity(Entity* entity);
+
+#endif
