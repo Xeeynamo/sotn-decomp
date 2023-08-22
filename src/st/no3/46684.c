6bafdc82258281ea397cfe31f4c1a34696e8d6cb
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-08-21 21:39:51 -0300

    review

--- src/st/no3/46684.c
+++ src/st/no3/46684.c
@@ -1358,22 +1358,17 @@ void EntitySplashWater(Entity* self) {
                             temp_a0 = D_80183858[temp_s5];
                             if (temp_a0 < 0) {
                                 if (temp_t8 >= prim->x1) {
-                                    prim->y2 = prim->y2 +
-                                               (prim->x1 - prim->x0) / temp_a0;
+                                    prim->y2 += (prim->x1 - prim->x0) / temp_a0;
                                 } else {
-                                    prim->y2 = prim->y2 +
-                                               (temp_t8 - prim->x0) / temp_a0;
+                                    prim->y2 += (temp_t8 - prim->x0) / temp_a0;
                                     prim->y3 = prim->y3 -
                                                (prim->x1 - temp_t8) / temp_a0;
                                 }
                             } else if (prim->x0 >= temp_t8) {
-                                prim->y3 =
-                                    prim->y3 - (prim->x1 - prim->x0) / temp_a0;
+                                prim->y3 -= (prim->x1 - prim->x0) / temp_a0;
                             } else {
-                                prim->y2 =
-                                    prim->y2 + (temp_t8 - prim->x0) / temp_a0;
-                                prim->y3 =
-                                    prim->y3 - (prim->x1 - temp_t8) / temp_a0;
+                                prim->y2 += (temp_t8 - prim->x0) / temp_a0;
+                                prim->y3 -= (prim->x1 - temp_t8) / temp_a0;
                             }
                         }
                     }
