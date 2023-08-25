27a1ec4dab1f43de83991b17f1f9910d7e5fd776
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2023-08-25 04:59:19 +0100

    Decompile all w_040 and w_042 (#510)
    As w_042 is similar to w_040 I decided to decompile them both.
    
    ### w_040 - Monster Vial 1
    
    
    ![f_040](https://github.com/Xeeynamo/sotn-decomp/assets/6128729/edbd9d1a-6d23-4081-9537-2642db8b007c)
    
    ### w_042 - Monster Vial 3
    
    
    ![f_042](https://github.com/Xeeynamo/sotn-decomp/assets/6128729/02daf207-b291-4d51-86e5-d1e459015529)
    
    ### EntityWeaponAttack
    
    `EntityWeaponAttack` is the vial dropping to the ground. When
    `CheckCollision` detects a collision with the solid pavement, the vial
    gets immediately destroyed. There is some wild magic happening, which I
    think it's happening when the code does `g_api.func_8011AAFC(`. That
    might trigger the function `func_ptr_80170004`, which will summon the
    vial monster.
    
    ### func_ptr_80170004
    
    The state machine of the summon itself. We have `self->unk1A` and
    `self->unk1C`, which if I recall correctly they indicates ScaleX and
    ScaleY respectively. `self->unk19 |= 3` will tell to use both horizontal
    and vertical scaling. When the scaling reaches 100% (or `0x100`) after
    64 frames, the summon will get inherit the properties of...
    `self->ext.weapon.parent->ext.weapon.equipId;`? That is the potential
    bug that the folks from the SOTN Speedrun server described to me.
    `g_api.func_8011AAFC` seems to invoke the projectile.
    
    ### func_ptr_80170008
    
    This seems to be the projectile function. Again, this is also calling
    `SetWeaponProperties`, which will copy the projectile attack properties
    from a dubious source. On `w_040` the logic is pretty simple as it is
    just keep adding the X coord until something is hit. On `w_042` both X
    and Y are involved as the thrown bone will form an arch. The thrown bone
    seems to not check the stage collisions.
    
    ### func_ptr_8017000C
    
    I think this is the light effect at the base of the summon when it is
    still spawning. The function looks so simple I cannot imagine otherwise.

--- src/weapon/w_040.c
+++ src/weapon/w_040.c
@@ -1,15 +1,229 @@
 #include "weapon_private.h"
 #include "shared.h"
 
-INCLUDE_ASM("weapon/nonmatchings/w_040", func_11C000_8017AC14);
+extern AnimationFrame D_11C000_8017A724[];
+extern AnimationFrame D_11C000_8017A748[];
+extern AnimationFrame D_11C000_8017A7DC[];
+extern AnimationFrame D_11C000_8017A7B8[];
+extern AnimationFrame D_11C000_8017A804[];
+extern AnimationFrame D_11C000_8017A80C[];
+extern FrameProperty D_11C000_8017A844[];
+extern s32 D_11C000_8017A85C[];
+extern s32 D_11C000_8017B540;
 
-INCLUDE_ASM("weapon/nonmatchings/w_040", EntityWeaponAttack);
+void func_11C000_8017AC14(void) {
+    RECT rect;
+    RECT rectDummy;
+    s16 color;
 
-INCLUDE_ASM("weapon/nonmatchings/w_040", func_ptr_80170004);
+    color = (D_8003C8C4 >> 1) % 2 ? 0x039C : 0x199D;
+    D_8006EDCC[g_HandId][10] = color;
 
-INCLUDE_ASM("weapon/nonmatchings/w_040", func_ptr_80170008);
+    rect.x = 0;
+    rect.y = 0xF1;
+    rect.w = 0x100;
+    rect.h = 3;
+    LoadImage(&rect, D_8006EDCC);
+}
 
-INCLUDE_ASM("weapon/nonmatchings/w_040", func_ptr_8017000C);
+void EntityWeaponAttack(Entity* self) {
+    Collider col;
+    s32 var_a2;
+    s16 xMod;
+
+    switch (self->step) {
+    case 0:
+        SetSpriteBank1(g_Animset);
+        self->animSet = ANIMSET_OVL(16);
+        self->palette = 0x110;
+        self->unk5A = 0x64;
+        if (g_HandId != 0) {
+            self->palette += 0x18;
+            self->unk5A += 2;
+            self->animSet += 2;
+        }
+
+        self->zPriority = PLAYER.zPriority + 2;
+        self->facing = PLAYER.facing;
+        self->flags = FLAG_UNK_08000000 | FLAG_UNK_100000;
+        self->unk4C = D_11C000_8017A804;
+        self->posY.i.hi -= 4;
+
+        D_11C000_8017B540 %= 4;
+        SetSpeedX(D_11C000_8017A85C[D_11C000_8017B540]);
+        self->velocityY = -FIX(2.5);
+        g_Player.D_80072F14 = 4;
+        D_11C000_8017B540++;
+        self->step++;
+        break;
+    case 1:
+        self->posX.val += self->velocityX;
+        self->posY.val += self->velocityY;
+        self->velocityY += FIX(0.15625);
+        g_api.CheckCollision(self->posX.i.hi, self->posY.i.hi, &col, 0);
+        if (col.effects & EFFECT_SOLID) {
+            self->posY.i.hi += col.unk18;
+            self->unk4C = D_11C000_8017A7DC;
+            self->animFrameDuration = 0;
+            self->animFrameIdx = 0;
+            self->blendMode = 0x30;
+            g_api.func_80134714(0x619, 0x50, 0);
+            g_api.func_8011AAFC(self, ((g_HandId + 1) << 0xC) | 0x38, 0);
+            self->step++;
+            return;
+        }
+
+        if (self->velocityX < 0) {
+            xMod = -4;
+        } else {
+            xMod = 4;
+        }
+        g_api.CheckCollision(
+            (s16)(xMod + self->posX.i.hi), self->posY.i.hi, &col, 0);
+        if (col.effects & EFFECT_UNK_0002) {
+            if (xMod < 0) {
+                self->posX.i.hi += col.unkC;
+            } else {
+                self->posX.i.hi += col.unk4;
+            }
+            self->velocityX /= -2;
+        }
+
+        g_api.CheckCollision(
+            self->posX.i.hi, (s16)(self->posY.i.hi - 8), &col, 0);
+        if (col.effects & EFFECT_SOLID) {
+            self->posY.i.hi += col.unk20 + 1;
+            self->velocityY = FIX(1);
+            self->velocityX /= 2;
+            return;
+        }
+        break;
+    case 2:
+        if (self->animFrameDuration < 0) {
+            DestroyEntity(self);
+        }
+        break;
+    }
+}
+
+void func_ptr_80170004(Entity* self) {
+    switch (self->step) {
+    case 0:
+        self->animSet = self->ext.weapon.parent->animSet;
+        self->unk5A = self->ext.weapon.parent->unk5A;
+        self->palette = self->ext.weapon.parent->palette;
+        self->facing = (self->facing + 1) & 1;
+        self->flags = FLAG_UNK_08000000;
+        self->zPriority = self->ext.weapon.parent->zPriority - 2;
+        self->unk4C = D_11C000_8017A724;
+        self->unk19 |= 3;
+        self->unk1C = 0;
+        self->unk1A = 0;
+        self->rotPivotY = 0x16;
+        self->posY.i.hi -= 0x16;
+        self->step++;
+        break;
+    case 1:
+        self->unk1A += 4;
+        if (self->unk1A >= 0x100) {
+            self->unk1A = 0x100;
+            self->unk4C = D_11C000_8017A748;
+            self->animFrameIdx = 0;
+            self->animFrameDuration = 0;
+            self->ext.weapon.equipId =
+                self->ext.weapon.parent->ext.weapon.equipId;
+            SetWeaponProperties(self, 0);
+            self->step++;
+        }
+        self->unk1C = self->unk1A;
+        break;
+    case 2:
+        if (self->animFrameDuration == 1) {
+            if ((self->animFrameIdx == 0xB || self->animFrameIdx == 0xF ||
+                 self->animFrameIdx == 0x13) &&
+                g_api.func_8011AAFC(self, ((g_HandId + 1) << 0xC) | 0x3E, 0) !=
+                    NULL) {
+                g_api.PlaySfx(0x655);
+            }
+        }
+        if (self->animFrameDuration < 0) {
+            self->unk4C = D_11C000_8017A80C;
+            self->animFrameIdx = 0;
+            self->animFrameDuration = 0;
+            g_api.PlaySfx(0x6E7);
+            self->step++;
+        }
+        break;
+    case 3:
+        if (self->unk1A == 0x40) {
+            g_api.func_8011AAFC(self, ((g_HandId + 1) << 0xC) | 0x46, 0);
+        }
+        if (self->animFrameIdx >= 5) {
+            self->unk1A -= 4;
+        }
+        if (self->unk1A < 0) {
+            DestroyEntity(self);
+            return;
+        }
+        self->unk1C = self->unk1A;
+        break;
+    }
+    g_api.UpdateAnim(D_11C000_8017A844, NULL);
+    func_11C000_8017AC14();
+}
+
+void func_ptr_80170008(Entity* self) {
+    s32 var_a1;
+
+    if (self->step == 0) {
+        if (self->ext.weapon.parent->entityId == 0) {
+            DestroyEntity(self);
+            return;
+        }
+        self->animSet = self->ext.weapon.parent->animSet;
+        self->unk5A = self->ext.weapon.parent->unk5A;
+        self->palette = self->ext.weapon.parent->palette;
+        self->flags = FLAG_UNK_08000000;
+        self->zPriority = self->ext.weapon.parent->zPriority - 2;
+        self->unk4C = D_11C000_8017A724;
+        self->posY.i.hi -= 0xA;
+        var_a1 = 0x18;
+        if (self->facing == 0) {
+            var_a1 = -0x18;
+        }
+        self->posX.i.hi = var_a1 + self->posX.i.hi;
+        SetSpeedX(-FIX(2.5));
+        self->unk4C = D_11C000_8017A7B8;
+        self->ext.weapon.equipId = self->ext.weapon.parent->ext.weapon.equipId;
+        self->attackElement |= 0x8000;
+        SetWeaponProperties(self, 0);
+        self->step++;
+    } else {
+        self->posX.val += self->velocityX;
+        if (self->hitFlags != 0) {
+            DestroyEntity(self);
+            return;
+        }
+    }
+
+    g_api.UpdateAnim(D_11C000_8017A844, NULL);
+}
+
+void func_ptr_8017000C(Entity* self) {
+    if (self->step == 0) {
+        self->animSet = self->ext.weapon.parent->animSet;
+        self->unk5A = self->ext.weapon.parent->unk5A;
+        self->palette = self->ext.weapon.parent->palette;
+        self->flags = FLAG_UNK_08000000 | FLAG_UNK_100000;
+        self->zPriority = self->ext.weapon.parent->zPriority + 2;
+        self->unk4C = D_11C000_8017A7DC;
+        self->blendMode = 0x30;
+        self->posY.i.hi += 0x10;
+        self->step++;
+    } else if (self->animFrameDuration < 0) {
+        DestroyEntity(self);
+    }
+}
 
 void func_ptr_80170010(Entity* self) {}
 
