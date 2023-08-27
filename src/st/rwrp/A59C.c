5e29affd896dfd64d71222d932f38eb2d17bb40a
cram0 <j.augugliaro@outlook.fr>
2023-08-27 18:52:16 +0200

    Added st/rwrp PreventEntityFromRespawning

--- src/st/rwrp/A59C.c
+++ src/st/rwrp/A59C.c
@@ -153,7 +153,14 @@ INCLUDE_ASM("asm/us/st/rwrp/nonmatchings/A59C", DestroyEntity);
 
 INCLUDE_ASM("asm/us/st/rwrp/nonmatchings/A59C", func_8018D5EC);
 
-INCLUDE_ASM("asm/us/st/rwrp/nonmatchings/A59C", PreventEntityFromRespawning);
+void PreventEntityFromRespawning(Entity* entity) {
+    if (entity->entityRoomIndex) {
+        u32 value = (entity->entityRoomIndex - 1);
+        u16 index = value / 32;
+        u16 bit = value % 32;
+        g_entityDestroyed[index] |= 1 << bit;
+    }
+}
 
 INCLUDE_ASM("asm/us/st/rwrp/nonmatchings/A59C", func_8018D6B0);
 
