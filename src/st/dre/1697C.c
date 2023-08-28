b32a64a9e9ae236f2164df080adf62873bcb0a7e
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-08-28 18:31:30 -0300

    split file

---
+++ src/st/dre/1697C.c
@@ -0,0 +1,7 @@
+#include "dre.h"
+
+// Fades to white
+INCLUDE_ASM("asm/us/st/dre/nonmatchings/14774", EntityFadeToWhite1);
+
+// Fades to white ID 24
+INCLUDE_ASM("asm/us/st/dre/nonmatchings/14774", EntityFadeToWhite2);
