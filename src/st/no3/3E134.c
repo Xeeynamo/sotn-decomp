d85b0f0ec3e00cbb8861c42382ea4849b71d7d50
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-09-02 00:39:45 -0300

    func_801BE598

--- src/st/no3/3E134.c
+++ src/st/no3/3E134.c
@@ -139,7 +139,66 @@ void func_801BE544(void) {
     D_801D7D2A = D_801D7D2C + 0x14;
 }
 
-INCLUDE_ASM("asm/us/st/no3/nonmatchings/3E134", func_801BE598);
+s32 func_801BE598(s32 textDialogue) {
+    Primitive* prim;
+    s16 firstPrimIndex;
+
+    firstPrimIndex = g_api.AllocPrimitives(PRIM_SPRT, 7);
+    g_Dialogue.primIndex[2] = firstPrimIndex;
+    if (firstPrimIndex == -1) {
+        g_Dialogue.primIndex[2] = 0;
+        return 0;
+    }
+    g_Dialogue.nextCharDialogue = textDialogue;
+    g_Dialogue.unk3C = 0;
+    g_Dialogue.primIndex[1] = -1;
+    g_Dialogue.primIndex[0] = -1;
+    func_801BE544();
+
+    //! FAKE:
+    if (prim && prim) {
+    }
+
+    prim = g_Dialogue.prim[0] = &g_PrimBuf[g_Dialogue.primIndex[2]];
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[1] = prim->next;
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[2] = prim->next;
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[3] = prim->next;
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[4] = prim->next;
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[5] = prim->next;
+
+    prim->type = 4;
+    prim->blendMode = BLEND_VISIBLE;
+
+    prim = prim->next;
+    prim->type = 3;
+    prim->r0 = prim->r1 = prim->r2 = prim->r3 = 0xFF;
+    prim->g0 = prim->g1 = prim->g2 = prim->g3 = 0;
+    prim->b0 = prim->b1 = prim->b2 = prim->b3 = 0;
+    prim->x0 = prim->x2 = 4;
+    prim->x1 = prim->x3 = 0xF8;
+    prim->priority = 0x1FD;
+    prim->blendMode = BLEND_VISIBLE;
+
+    prim = prim->next;
+    prim->type = 1;
+    prim->x0 = 3;
+    prim->y0 = 0x2F;
+    prim->v0 = 0x4A;
+    prim->r0 = prim->g0 = prim->b0 = 0xFF;
+    prim->priority = 0x1FC;
+    prim->blendMode = BLEND_VISIBLE;
+    return 1;
+}
 
 void func_801BE75C(s16 yOffset) {
     RECT rect;
