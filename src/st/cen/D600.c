02f9df730abc61456f282da360a3dbd9f85ee6b7
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-09-02 00:47:07 -0300

    func_8018E180

--- src/st/cen/D600.c
+++ src/st/cen/D600.c
@@ -233,7 +233,23 @@ void func_8018E124(s16 arg0) {
     ClearImage(&rect, 0, 0, 0);
 }
 
-INCLUDE_ASM("asm/us/st/cen/nonmatchings/D600", func_8018E180);
+void func_8018E180(void) {
+    Primitive* prim;
+
+    func_8018E124(g_Dialogue.nextCharY);
+    prim = g_Dialogue.prim[g_Dialogue.nextCharY];
+    prim->tpage = 0x10;
+    prim->clut = g_Dialogue.clutIndex;
+    prim->y0 = g_Dialogue.nextLineY;
+    prim->u0 = 0;
+    prim->x0 = g_Dialogue.startX;
+    prim->x0 = prim->x0 + 4;
+    prim->v0 = g_Dialogue.nextCharY * 0xC - 0x80;
+    prim->u1 = 0xC0;
+    prim->v1 = 0xC;
+    prim->priority = 0x1FF;
+    prim->blendMode = 0;
+}
 
 INCLUDE_ASM("asm/us/st/cen/nonmatchings/D600", func_8018E238);
 
