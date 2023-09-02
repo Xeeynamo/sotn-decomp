46f8fb9bb21896650efc8f177682d80e792ab3ab
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2023-09-02 01:01:42 -0300

    func_801B6480 (Different version)

--- src/st/sel/3642C.c
+++ src/st/sel/3642C.c
@@ -10,7 +10,69 @@ void func_801B642C(void) {
     D_801BC35A = D_801BC35C + 0x14;
 }
 
-INCLUDE_ASM("asm/us/st/sel/nonmatchings/3642C", func_801B6480);
+s32 func_801B6480(s32 textDialogue) {
+    Primitive* prim;
+    s16 firstPrimIndex;
+
+    firstPrimIndex = g_api.AllocPrimitives(PRIM_SPRT, 7);
+    g_Dialogue.primIndex[2] = firstPrimIndex;
+    if (firstPrimIndex == -1) {
+        g_Dialogue.primIndex[2] = 0;
+        return 0;
+    }
+    g_Dialogue.nextCharDialogue = textDialogue;
+    g_Dialogue.unk3C = 0;
+    g_Dialogue.primIndex[1] = -1;
+    g_Dialogue.primIndex[0] = -1;
+    func_801B642C();
+
+    //! FAKE:
+    if (prim && prim) {
+    }
+
+    prim = g_Dialogue.prim[0] = &g_PrimBuf[g_Dialogue.primIndex[2]];
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[1] = prim->next;
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[2] = prim->next;
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[3] = prim->next;
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[4] = prim->next;
+
+    prim->blendMode = BLEND_VISIBLE;
+    prim = g_Dialogue.prim[5] = prim->next;
+
+    prim->type = 4;
+    prim->blendMode = BLEND_VISIBLE;
+
+    prim = prim->next;
+    prim->type = 3;
+    prim->b0 = prim->b1 = 0x7F;
+    prim->b2 = prim->b3 = 0;
+    prim->g0 = prim->g1 = prim->g2 = prim->g3 = 0;
+    prim->r0 = prim->r1 = prim->r2 = prim->r3 = 0;
+    prim->x2 = 4;
+    prim->x0 = 4;
+    prim->x3 = 0xF8;
+    prim->x1 = 0xF8;
+    prim->priority = 0x1FD;
+    prim->blendMode = BLEND_VISIBLE;
+
+    prim = prim->next;
+    prim->type = 1;
+    prim->r0 = prim->g0 = prim->b0 = 16;
+    prim->x0 = 3;
+    prim->y0 = 0x2F;
+    prim->v0 = 0x4A;
+    prim->priority = 0x1FC;
+    prim->blendMode = BLEND_VISIBLE;
+    return 1;
+}
 
 void func_801B6648(s16 yOffset) {
     RECT rect;
