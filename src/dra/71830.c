17e9f570475a57c3b930e07495ee759e2425552d
cram0 <j.augugliaro@outlook.fr>
2023-09-04 12:44:03 +0200

    Revert "remove comment"
    This reverts commit b7f4de22346275f5c65b4aab4057f8b162b7e520.

--- src/dra/71830.c
+++ src/dra/71830.c
@@ -70,6 +70,11 @@ void func_8011197C(void) {
     PLAYER.palette = D_801396E8;
 }
 
+// INCLUDE_ASM("dra/nonmatchings/71830", func_801119C4);
+
+extern s32 D_80137FDC;
+extern s8 aCharal02x;
+
 bool func_801119C4(void) {
     if (D_801396EA == 0) {
         if (g_Player.padTapped & PAD_L2) {
