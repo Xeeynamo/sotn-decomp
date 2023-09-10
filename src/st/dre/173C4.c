ad95e38790c8830600c6a73a0027a21e89302ac1
bismurphy <tjmurphy@mit.edu>
2023-09-10 00:44:33 -0400

    Finish EnemyDef struct members (#576)
    Given that InitializeEntity copies these values from EnemyDef directly
    into Entity, and we have names for these struct members of Entity, it is
    reasonable to name the EnemyDef members to match.
    
    Almost all the changes are in InitializeEntity since these struct
    members are rarely touched. A fairly straightforward find-and-replace.

--- src/st/dre/173C4.c
+++ src/st/dre/173C4.c
@@ -661,10 +661,10 @@ void InitializeEntity(u16 arg0[]) {
     g_CurrentEntity->hitPoints = enemyDef->hitPoints;
     g_CurrentEntity->attack = enemyDef->attack;
     g_CurrentEntity->attackElement = enemyDef->attackElement;
-    g_CurrentEntity->hitboxState = enemyDef->unkC;
+    g_CurrentEntity->hitboxState = enemyDef->hitboxState;
     g_CurrentEntity->hitboxWidth = enemyDef->hitboxWidth;
     g_CurrentEntity->hitboxHeight = enemyDef->hitboxHeight;
-    g_CurrentEntity->flags = enemyDef->unk24;
+    g_CurrentEntity->flags = enemyDef->flags;
     g_CurrentEntity->hitboxOffX = 0;
     g_CurrentEntity->hitboxOffY = 0;
     g_CurrentEntity->step_s = 0;
