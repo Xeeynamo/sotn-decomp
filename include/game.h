c6c960d62857225e9bd89235c9cd05dd354e96e8
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-09-11 19:40:33 +0100

    Last minute compilation fixes

--- include/game.h
+++ include/game.h
@@ -1052,7 +1052,7 @@ typedef struct {
     /* 8003C844 */ void* (*func_8010E0A8)(void);
     /* 8003C848 */ void (*func_800FE044)(s32, s32);
     /* 8003C84C */ void (*AddToInventory)(u16 itemId, s32 itemCategory);
-    /* 8003C850 */ RelicOrb* D_800A8720;
+    /* 8003C850 */ RelicOrb* relicDefs;
     /* 8003C854 */ void (*InitStatsAndGear)(bool debugMode);
     /* 8003C858 */ s32 (*func_80134714)(s32 arg0, s32 arg1, s32 arg2);
     /* 8003C85C */ s32 (*func_80134678)(s16 arg0, u16 arg1);
@@ -1116,7 +1116,7 @@ extern s32 (*g_api_TimeAttackController)(
 extern void* (*g_api_func_8010E0A8)(void);
 extern void (*g_api_func_800FE044)(s32, s32);
 extern void (*g_api_AddToInventory)(u16 itemId, s32 itemCategory);
-extern RelicOrb* g_api_D_800A8720;
+extern RelicOrb* g_api_relicDefs;
 extern s32 (*g_api_func_80134714)(s32 arg0, s32 arg1, s32 arg2);
 extern s32 (*g_api_func_80134678)(s16 arg0, u16 arg1);
 extern void (*g_api_func_800F53A4)(void);
