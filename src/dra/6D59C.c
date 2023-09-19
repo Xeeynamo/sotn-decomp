015aaea84b49958f85de9f08f4ce2a0f403acff1
bismurphy <tjmurphy@mit.edu>
2023-09-19 17:25:02 -0400

    Decompile DRA func_80110394 (#616)
    Worked out a DECOMP_ME_WIP, those are always fun.
    
    Nothing too special here otherwise, just checking some collisions it
    seems.

--- src/dra/6D59C.c
+++ src/dra/6D59C.c
@@ -900,8 +900,39 @@ bool func_8010FDF8(s32 branchFlags) {
     return 0;
 }
 
-// DECOMP_ME_WIP func_80110394 https://decomp.me/scratch/Akstc 94.80%
-INCLUDE_ASM("dra/nonmatchings/6D59C", func_80110394);
+s16 func_80110394(void) {
+    // Variables that change during execution
+    Collider collider;
+    s32 yvar;
+    s32 collisions;
+    s32 i;
+    // Values that are set once and never again (but not const for some reason)
+    s32 xpos = PLAYER.posX.i.hi;
+    s32 xp4 = xpos + 4;
+    s32 xm4 = xpos - 4;
+    s32 coll_flags = EFFECT_SOLID_FROM_ABOVE | EFFECT_SOLID;
+
+    for (i = 0; i < 4; i++) {
+        yvar = PLAYER.posY.i.hi + D_800ACF6C[i];
+        CheckCollision(xpos, yvar, &collider, 0);
+        collisions = 0;
+        if ((collider.effects & coll_flags) == EFFECT_SOLID) {
+            collisions += 1;
+        }
+        CheckCollision(xp4, yvar, &collider, 0);
+        if ((collider.effects & coll_flags) == EFFECT_SOLID) {
+            collisions += 1;
+        }
+        CheckCollision(xm4, yvar, &collider, 0);
+        if ((collider.effects & coll_flags) == EFFECT_SOLID) {
+            collisions += 1;
+        }
+        if (collisions != 0) {
+            return i + 1;
+        }
+    }
+    return 0;
+}
 
 INCLUDE_ASM("dra/nonmatchings/6D59C", func_801104D0);
 
