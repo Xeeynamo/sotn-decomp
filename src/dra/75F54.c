a9c4dd750d5b86787ae7cb882390d8d3a2549133
bismurphy <tjmurphy@mit.edu>
2023-09-23 19:42:26 -0400

    Decompile DRA func_801182F8 (#630)
    Thankfully, when decompiling this one, I very quickly realized that it
    looks extremely similar to `func_801177A0` and `func_8012EAD0`. Only a
    couple lines different. Mostly a copy-paste. I'm not sure if there are
    more functions in this group, I will try to hunt for them.
    
    Notably, this is the last function which calls dra.DecelerateY.
    Interestingly, there are 5 functions which do so: ControlBatForm,
    ControlMistForm, and the three functions in this family. These ones do
    not take player inputs though. Neat!
    
    Not sure what these 3 actually do. They are all only called by
    EntityAlucard. Now that we have all 3, maybe we can start to study them
    and work out what they do. Or maybe we will have to wait for
    EntityAlucard to get worked out.
    
    Co-authored-by: Luciano Ciccariello <Xeeynamo@users.noreply.github.com>

--- src/dra/75F54.c
+++ src/dra/75F54.c
@@ -1105,7 +1105,91 @@ void ControlMistForm(void) {
     }
 }
 
-INCLUDE_ASM("dra/nonmatchings/75F54", func_801182F8);
+void func_801182F8(void) {
+    byte pad[0x28];
+    s32 i;
+    s32 else_cycles;
+
+    if ((g_Player.pl_vram_flag & 1) && (PLAYER.velocityY > 0)) {
+        PLAYER.velocityY = 0;
+    }
+    if ((g_Player.pl_vram_flag & 2) && (PLAYER.velocityY < 0)) {
+        PLAYER.velocityY = 0;
+    }
+    DecelerateX(0x200);
+    DecelerateY(0x200);
+    else_cycles = 0;
+    for (i = 0; i < 4; i++) {
+        if (D_800ACED0.pairs[i].unk2 < D_800ACE90[i]) {
+            D_800ACED0.pairs[i].unk2++;
+        } else {
+            else_cycles++;
+        }
+        if (D_800ACE88[i] < D_800ACEC0[i].unk2) {
+            D_800ACEC0[i].unk2--;
+        } else {
+            else_cycles++;
+        }
+        // This means the lower parts only run once!
+        if (i != 0) {
+            continue;
+        }
+        if (PLAYER.step_s != 0) {
+            if (D_8013AECC >= 12) {
+                continue;
+            }
+            D_8013AECC++;
+        } else {
+            if (g_Player.pl_vram_flag & 0x8000) {
+                PLAYER.posY.i.hi--;
+            }
+        }
+    }
+    if ((g_Player.pl_vram_flag & 3) == 3) {
+        PLAYER.step_s = 1;
+        PLAYER.velocityY = 0;
+        PLAYER.velocityX = 0;
+    }
+    if (else_cycles == 8) {
+        if (func_80111D24()) {
+            return;
+        }
+        PLAYER.animSet = 1;
+        PLAYER.unk5A = 0;
+        func_8010DA48(0xCB);
+        if (PLAYER.step_s != 0) {
+            func_8010DA48(0xCC);
+        }
+        if (g_Entities[16].step < 3) {
+            g_Entities[16].step = 3;
+            return;
+        }
+        if (g_Entities[16].step == 5) {
+            PLAYER.palette = 0x8100;
+            func_8010FAF4();
+            func_8011AAFC(g_CurrentEntity, 0x5B002CU, 0);
+            if (PLAYER.step_s != 0) {
+                func_8010E4D0();
+                return;
+            }
+            func_8010E83C(0);
+            PLAYER.posY.i.hi -= 3;
+            if (!(g_Player.pl_vram_flag & 0x8000)) {
+                PLAYER.velocityY = -0x10000;
+            }
+#if defined(VERSION_US)
+            g_Player.unk20[0] = 0x18;
+#elif defined(VERSION_HD)
+            D_800ACEDC_hd = 0x18;
+#endif
+            g_Player.unk44 |= 0x100;
+            func_80111CC0();
+        }
+    }
+    if (func_80111DE8(1) != 0) {
+        PLAYER.velocityX = 0;
+    }
+}
 
 void func_80118614(void) {
     if (PLAYER.animFrameDuration < 0) {
