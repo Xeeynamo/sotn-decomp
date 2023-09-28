aa8f1dad89a4b62012d374fdc59d624540434519
bismurphy <tjmurphy@mit.edu>
2023-09-28 14:14:14 -0400

    Decompile w_029 EntityWeaponAttack (#650)
    Continuing to finish out w_029 now that we have g_GameTimer % 5 figured
    out.
    
    There is a very unfortunate thing here. We are using
    `self->ext.weapon.lifetime`, but EntityWeaponShieldSpell just had a
    whole mess about 7C and 7D and needing to create ext.weapon29. Clearly,
    weapon29 references this location as both a s16, and as two u8's. I
    really don't know what to make of that. Happy to adjust things here if
    we want to handle that differently, but for now this seems to work. It
    will be cool if we can get this strange weapon fully decompiled.

--- src/weapon/w_029.c
+++ src/weapon/w_029.c
@@ -2,12 +2,143 @@
 // Fire shield, Unknown#213
 #include "weapon_private.h"
 #include "shared.h"
-
+extern s32 D_CF000_8017AC78;
+extern s32 D_CF000_8017ACC0;
 extern u16 D_CF000_8017ACF8[];
 extern u16 D_CF000_8017AD04[];
 extern AnimationFrame D_CF000_8017AD24[];
 
-INCLUDE_ASM("weapon/nonmatchings/w_029", EntityWeaponAttack);
+void EntityWeaponAttack(Entity* self) {
+    s32 newUnkAC;
+    s32 handButton;
+    s32 unkAC_offset;
+
+    newUnkAC = 0;
+    unkAC_offset = 0;
+    if (g_HandId != 0) {
+        handButton = PAD_CIRCLE;
+    } else {
+        handButton = PAD_SQUARE;
+    }
+    if (!(handButton & g_Player.padPressed) && (self->step < 3)) {
+        self->animFrameDuration = 0;
+        self->animFrameIdx = 0;
+        self->step = 3;
+    }
+    if (self->step != 4) {
+        self->posX.val = PLAYER.posX.val;
+        self->posY.val = PLAYER.posY.val;
+        self->facingLeft = PLAYER.facingLeft;
+    }
+    if ((g_Player.unk0C & 0x10000) && (self->step != 4)) {
+        self->zPriority = PLAYER.zPriority + 2;
+        self->step = 4;
+        if (g_Player.pl_vram_flag & 1) {
+            self->velocityX = PLAYER.velocityX;
+        } else {
+            self->velocityX = PLAYER.velocityX * 2;
+        }
+        self->velocityY = FIX(-3.5);
+        self->ext.weapon.lifetime = 128;
+        self->flags = FLAG_UNK_08000000;
+        self->animCurFrame = 0x3E;
+    }
+    if ((PLAYER.step == 2) && (PLAYER.step_s != 2)) {
+        unkAC_offset++;
+    }
+    switch (self->step) {
+    case 0:
+        SetSpriteBank1(g_Animset);
+        if (self->params & ANIMSET_OVL_FLAG) {
+            self->animSet = ANIMSET_OVL(18);
+            self->ext.weapon.unk80 = 0x128;
+            self->unk5A = 0x66;
+        } else {
+            self->animSet = ANIMSET_OVL(16);
+            self->ext.weapon.unk80 = 0x110;
+            self->unk5A = 0x64;
+        }
+        self->flags = FLAG_UNK_40000 + FLAG_UNK_20000;
+        self->zPriority = PLAYER.zPriority - 2;
+        g_Player.unk48 = 1;
+        SetWeaponProperties(self, 0);
+        self->step++;
+    case 1:
+        self->ext.weapon.unkAC = unkAC_offset + 10;
+        if (self->animFrameDuration < 0) {
+            self->step++;
+        }
+        break;
+    case 2:
+        switch (PLAYER.ext.player.unkAC) {
+        case 9:
+        case 10:
+        case 11:
+            newUnkAC++;
+        case 7:
+            newUnkAC++;
+        case 8:
+            newUnkAC++;
+        case 12:
+            newUnkAC++;
+        case 13:
+            newUnkAC++;
+        case 24:
+        case 25:
+            newUnkAC++;
+        case 14:
+        case 15:
+            newUnkAC++;
+        case 26:
+            newUnkAC += 2;
+            self->animFrameIdx = PLAYER.animFrameIdx;
+            break;
+        default:
+            self->animFrameIdx = 0;
+            newUnkAC = newUnkAC + unkAC_offset;
+            break;
+        }
+        self->ext.weapon.unkAC = newUnkAC;
+        self->animFrameDuration = 2;
+        break;
+    case 3:
+        g_Player.unk48 = 0;
+        self->ext.weapon.unkAC = unkAC_offset + 12;
+        if (self->animFrameDuration < 0) {
+            DestroyEntity(self);
+            return;
+        }
+        break;
+    case 4:
+        self->hitboxState = 0;
+        g_Player.unk48 = 0;
+        self->unk19 |= 4;
+        self->posY.val += self->velocityY;
+        self->posX.val += self->velocityX;
+        self->velocityY += FIX(20.0 / 128);
+        self->rotAngle = self->rotAngle + 0x80;
+        if (--self->ext.weapon.lifetime < 16) {
+            self->unk19 |= 0x80;
+        }
+        if (--self->ext.weapon.lifetime == 0) {
+            DestroyEntity(self);
+            return;
+        }
+        break;
+    }
+    if (self->step != 4) {
+        g_api.func_8010DBFC(&D_CF000_8017AC78, &D_CF000_8017ACC0);
+        if (g_GameTimer % 5 == 0) {
+            g_api.func_8011AAFC(self, ((g_HandId + 1) << 0xC) + 0x38, 0);
+        }
+    }
+    self->palette =
+        self->ext.weapon.unk80 + D_CF000_8017ACF8[g_GameTimer / 2 % 5];
+    self->unk19 = PLAYER.unk19;
+    self->unk1C = PLAYER.unk1C;
+    self->rotPivotY = PLAYER.rotPivotY;
+    return;
+}
 
 void func_ptr_80170004(Entity* self) {
     if (self->ext.weapon.parent->ext.weapon.equipId != 15) {
