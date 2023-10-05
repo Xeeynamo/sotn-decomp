fa21ce144239c86315abac96860c917bf2155111
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2023-10-05 09:26:29 +0100

    Enhance make extract on PSX (#662)
    Previously if either the YAML or the symbol list were changed, we were
    forced to either manually delete the `asm` folder of the specific
    overlay or to invoke `make clean`, where the latter forced us to
    re-extract all the overlays via `make -j extract`.
    
    That changes with this PR. How `make` works is `target: dep1 dep2` where
    if one of the dependencies has the last modified date greater than the
    target, the rule is triggered again. Previously we were extracting
    overlays doing `make extract_stcen`. But since every overlay extraction
    generates a linker script I now changed the rules to do `make
    build/us/stcen.ld` to extract the same overlay. As I explained above, if
    either the YAML or one of the related symbols changes, the linker
    modified date will be older and the extraction for that overlay to be
    trigger again.
    
    The above allowed me to stop polluting the repo root with the linker
    scripts as they are now moved into `build/us` or `build/hd` depending of
    what you're trying to build. If you do `make extract` twice in a row you
    will be welcomed with a `make: Nothing to be done for 'extract'.`.
    
    There are still some instances where you **might** need `make clean`
    beforehand, especially when modifying the `segment` section in the YAML
    or renaming the symbols. A `rm -rf asm/$(VERSION)/$*` can help but I
    want to see if the current solution will be enough.
    
    ### Please try removing `make clean` from your workflow, once this is
    merged, to quickly detect possible problems with this new approach.
    
    I am also planning to make more substantial changes on our build-chain
    like this PR or #660 in the future. I am aiming to small incremental
    changes over time in case I break someone's flow or detect design flaws.
    I am also considering the breaking changes introduced in make 4.4, which
    will most likely be included in Ubuntu 24.04 LTS. I set March 2024 as a
    deadline to finish all the new build-chain work.
    
    ---------
    
    Co-authored-by: sozud <122322823+sozud@users.noreply.github.com>

---
+++ Makefile.psx.mk
@@ -0,0 +1,51 @@
+# configuration
+PSX_OVLS		:= dra ric weapon 
+PSX_KSTAGES		:= cen dre mad no3 np3 nz0 sel st0 wrp
+PSX_RSTAGES		:= rwrp
+PSX_ALLSTAGES	:= $(addprefix st,$(PSX_KSTAGES)) $(addprefix st,$(PSX_RSTAGES))
+PSX_SERVANTS	:= tt_000
+PSX_US_TARGETS	:= main $(PSX_OVLS) $(PSX_ALLSTAGES) $(PSX_SERVANTS)
+PSX_HD_TARGETS	:= dra
+
+# immovable
+PSX_BASE_SYMS	:= $(CONFIG_DIR)/symbols.$(VERSION).txt
+
+extract_us: $(addprefix $(BUILD_DIR)/,$(addsuffix .ld,$(PSX_US_TARGETS)))
+extract_hd: $(addprefix $(BUILD_DIR)/,$(addsuffix .ld,$(PSX_HD_TARGETS)))
+
+extract_disk_us: extract_disk_psxus
+extract_disk_hd: extract_disk_pspeu
+extract_disk_psp%:
+	mkdir -p disks/psp$*
+	7z x disks/sotn.psp$*.iso -odisks/psp$*/
+extract_disk_psx%: $(SOTNDISK)
+	$(SOTNDISK) extract disks/sotn.$*.cue disks/$*
+
+$(BUILD_DIR)/main.ld: $(CONFIG_DIR)/splat.$(VERSION).main.yaml $(SPLAT_APP)
+	$(SPLAT) $<
+$(BUILD_DIR)/dra.ld: $(CONFIG_DIR)/splat.$(VERSION).dra.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).dra.txt $(SPLAT_APP)
+	cat $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).dra.txt > $(CONFIG_DIR)/generated.symbols.$(VERSION).dra.txt
+	$(SPLAT) $<
+	rm $(CONFIG_DIR)/generated.symbols.$(VERSION).dra.txt
+$(BUILD_DIR)/ric.ld: $(CONFIG_DIR)/splat.$(VERSION).ric.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).ric.txt $(SPLAT_APP)
+	cat $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).ric.txt > $(CONFIG_DIR)/generated.symbols.$(VERSION).ric.txt
+	$(SPLAT) $<
+	rm $(CONFIG_DIR)/generated.symbols.$(VERSION).ric.txt
+$(BUILD_DIR)/stmad.ld: $(CONFIG_DIR)/splat.$(VERSION).stmad.yaml $(CONFIG_DIR)/symbols.beta.txt $(CONFIG_DIR)/symbols.stmad.txt $(SPLAT_APP)
+	cat $(CONFIG_DIR)/symbols.beta.txt $(CONFIG_DIR)/symbols.stmad.txt > $(CONFIG_DIR)/generated.symbols.stmad.txt
+	$(SPLAT) $<
+	rm $(CONFIG_DIR)/generated.symbols.stmad.txt
+	$(GFXSTAGE) d disks/$(VERSION)/ST/MAD/F_MAD.BIN $(ASSETS_DIR)/st/mad
+$(BUILD_DIR)/st%.ld: $(CONFIG_DIR)/splat.$(VERSION).st%.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).st%.txt $(SPLAT_APP)
+	cat $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).st$*.txt > $(CONFIG_DIR)/generated.symbols.$(VERSION).st$*.txt
+	$(SPLAT) $<
+	rm $(CONFIG_DIR)/generated.symbols.$(VERSION).st$*.txt
+	$(GFXSTAGE) d disks/$(VERSION)/ST/$$(echo '$*' | tr '[:lower:]' '[:upper:]')/F_$$(echo '$*' | tr '[:lower:]' '[:upper:]').BIN $(ASSETS_DIR)/st/$*
+$(BUILD_DIR)/tt_%.ld: $(CONFIG_DIR)/splat.$(VERSION).tt_%.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).tt_%.txt $(SPLAT_APP)
+	cat $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).tt_$*.txt > $(CONFIG_DIR)/generated.symbols.$(VERSION).tt_$*.txt
+	$(SPLAT) $<
+	rm $(CONFIG_DIR)/generated.symbols.$(VERSION).tt_$*.txt
+$(BUILD_DIR)/weapon.ld: $(CONFIG_DIR)/splat.$(VERSION).weapon.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).weapon.txt $(CONFIG_DIR)/symbols.$(VERSION).weapon.txt.in $(SPLAT_APP)
+	cat $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).weapon.txt $(CONFIG_DIR)/symbols.$(VERSION).weapon.txt.in > $(CONFIG_DIR)/generated.symbols.$(VERSION).weapon.txt
+	$(SPLAT) $<
+	rm $(CONFIG_DIR)/generated.symbols.$(VERSION).weapon.txt
