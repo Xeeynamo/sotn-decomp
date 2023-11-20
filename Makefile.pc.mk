3db01b7d82f068865d16a1680d4025dfd8f4f214
sozud <122322823+sozud@users.noreply.github.com>
2023-11-20 12:22:26 -0800

    Remove extension

--- Makefile.pc.mk
+++ Makefile.pc.mk
@@ -4,4 +4,4 @@ CPP_FLAGS_PC := -w -fsanitize=address -DVERSION_PC -DPERMUTER
 C_FILES_PC := $(SRC_DIR)/pc/main.c $(SRC_DIR)/dra/91EBC.c $(SRC_DIR)/dra/92F60.c $(SRC_DIR)/dra/93290.c $(SRC_DIR)/dra/93BDC.c $(SRC_DIR)/dra/94F50.c $(SRC_DIR)/dra/953A0.c
 
 build_pc:
-	$(CPP_PC) $(CPP_FLAGS_PC) -I$(INCLUDE_DIR) -I$(SRC_DIR)/dra/ $(C_FILES_PC) -o $(BUILD_DIR)/sound.exe
+	$(CPP_PC) $(CPP_FLAGS_PC) -I$(INCLUDE_DIR) -I$(SRC_DIR)/dra/ $(C_FILES_PC) -o $(BUILD_DIR)/sound
