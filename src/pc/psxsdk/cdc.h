9ba85913e0a57961151a36e1a178f897aef36a41
sozud <122322823+sozud@users.noreply.github.com>
2023-11-21 15:55:36 -0800

    Sound test XA support (#800)
    This takes the XA decoding code from Mednafen to implement XA support.
    The CD filename needs to be specified on the command line. A little bit
    of libcd is implemented so that XA filtering etc. works. There's no CUE
    parsing support since opening track1.bin just works in my case.

---
+++ src/pc/psxsdk/cdc.h
@@ -0,0 +1,18 @@
+
+
+#ifndef CDC_H
+#define CDC_H
+#include <inttypes.h>
+
+struct CD_Audio_Buffer {
+    int16_t Samples[2][0x1000]; // [0][...] = l, [1][...] = r
+    uint32_t Size;
+    uint32_t Freq;
+    uint32_t ReadPos;
+};
+
+extern struct CD_Audio_Buffer AudioBuffer;
+
+void GetCDAudio(int32_t samples[2]);
+void XA_ProcessSector(const uint8_t* sdata, struct CD_Audio_Buffer* ab);
+#endif
--- src/pc/psxsdk/libcd.c
+++ src/pc/psxsdk/libcd.c
@@ -2,22 +2,101 @@
 #include <psxsdk/libcd.h>
 #include <log.h>
 
+#include <inttypes.h>
+#include "cdc.h"
+#include <stdio.h>
+
+FILE* cd_fp = NULL;
+bool reading = false;
+const int sector_size = 2352;
+int current_file = 0;
+int current_channel = 0;
+
+void OpenCd(char* filename) {
+    cd_fp = fopen(filename, "rb");
+
+    if (!cd_fp) {
+        DEBUGF("Couldn't open CD.\n");
+    }
+}
+
+int CdReading() { return reading; }
+
 CdlCB CdReadyCallback(CdlCB func) {
     DEBUGF("not implemented");
     return func;
 }
 
+char* CdSyncModeToStr(int mode) {
+    switch (mode) {
+    case CdlNop:
+        return "CdlNop";
+    case CdlSetloc:
+        return "CdlSetloc";
+    case CdlPlay:
+        return "CdlPlay";
+    case CdlForward:
+        return "CdlForward";
+    case CdlBackward:
+        return "CdlBackward";
+    case CdlReadN:
+        return "CdlReadN";
+    case CdlSetfilter:
+        return "CdlSetfilter";
+    case CdlSetmode:
+        return "CdlSetmode";
+    case CdlGetTD:
+        return "CdlGetTD";
+    default:
+    }
+
+    return "";
+}
+
+#define DECODE_BCD(x) (((x) >> 4) * 10 + ((x)&0xF))
+
+int CdPosToInt(CdlLOC* p) {
+    return (75 * (60 * DECODE_BCD(p->minute) + DECODE_BCD(p->second))) +
+           DECODE_BCD(p->sector) - 150;
+}
+
 int CdControl(u_char com, u_char* param, u_char* result) {
-    DEBUGF("com %d", com);
+    DEBUGF("com %d %s", com, CdSyncModeToStr(com));
+
+    if (!cd_fp) {
+        DEBUGF("Cd not open.\n");
+    }
+
+    switch (com) {
+    case CdlSetloc:
+        CdlLOC* pos = (CdlLOC*)param;
+        fseek(cd_fp, CdPosToInt(pos) * sector_size, SEEK_SET);
+        break;
+    case CdlReadN:
+        reading = true;
+        break;
+    case CdlSetfilter:
+        current_file = param[0];
+        current_channel = param[1];
+        break;
+    }
     return 1;
 }
 
 int CdSync(int mode, u_char* result) {
-    DEBUGF("mode %0d", mode);
+    DEBUGF("mode %0d %s", mode, CdSyncModeToStr(mode));
     return CdlComplete;
 }
 
 int CdMix(CdlATV* vol) {
     DEBUGF("not implemented");
     return 0;
+}
+
+void ExecCd() {
+    if (reading) {
+        uint8_t sector[sector_size];
+        fread(sector, sizeof(uint8_t), sector_size, cd_fp);
+        XA_ProcessSector(sector, &AudioBuffer);
+    }
 }
