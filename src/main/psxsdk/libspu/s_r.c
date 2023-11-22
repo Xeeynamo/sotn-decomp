347c47851606d5f85c60a15d3d0e96fcb8712e40
sozud <122322823+sozud@users.noreply.github.com>
2023-11-22 04:19:43 -0800

    More libspu (#803)

--- src/main/psxsdk/libspu/s_r.c
+++ src/main/psxsdk/libspu/s_r.c
@@ -1,3 +1,16 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libspu/s_r", SpuRead);
+void _spu_read(u32, u32);
+extern s32 _spu_inTransfer;
+extern s32 _spu_transferCallback;
+
+u32 SpuRead(u8* arg0, u32 size) {
+    if (size > 0x7F000U) {
+        size = 0x7F000;
+    }
+    _spu_read(arg0, size);
+    if (_spu_transferCallback == 0) {
+        _spu_inTransfer = 0;
+    }
+    return size;
+}
--- src/main/psxsdk/libspu/s_sic.c
+++ src/main/psxsdk/libspu/s_sic.c
@@ -1,3 +1,16 @@
 #include "common.h"
+#include "psxsdk/libspu.h"
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libspu/s_sic", SpuSetIRQCallback);
+s32 _SpuCallback();
+extern void (*_spu_IRQCallback)();
+
+SpuIRQCallbackProc SpuSetIRQCallback(SpuIRQCallbackProc func) {
+    void (*temp_s0)();
+
+    temp_s0 = _spu_IRQCallback;
+    if (func != temp_s0) {
+        _spu_IRQCallback = func;
+        _SpuCallback();
+    }
+    return temp_s0;
+}
