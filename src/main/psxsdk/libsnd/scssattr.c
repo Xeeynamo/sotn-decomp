51f2d5b6fb7c2a4fa7b9564460b3d3d2197d3b03
sozud <122322823+sozud@users.noreply.github.com>
2023-11-24 15:27:26 -0800

    libsnd libspu (#809)

--- src/main/psxsdk/libsnd/scssattr.c
+++ src/main/psxsdk/libsnd/scssattr.c
@@ -1,3 +1,34 @@
 #include "common.h"
+#include "libsnd_internal.h"
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/scssattr", SsSetSerialAttr);
+void SsSetSerialAttr(s32 arg0, s32 arg1, s32 arg2) {
+    SpuCommonAttr attr;
+    s32 temp_a0;
+    s32 temp_a1;
+    s32 temp_v1;
+
+    if (!(arg0 & 0xFF)) {
+        temp_v1 = arg1 & 0xFF;
+        if (temp_v1 == 0) {
+            attr.mask = 0x200;
+            attr.cd.mix = arg2 & 0xFF;
+        }
+        if (temp_v1 == 1) {
+            attr.mask = 0x100;
+            attr.cd.reverb = arg2 & 0xFF;
+        }
+    }
+    temp_a0 = arg0 & 0xFF;
+    temp_a1 = arg1 & 0xFF;
+    if (temp_a0 == 1) {
+        if (temp_a1 == 0) {
+            attr.mask = 0x2000;
+            attr.ext.mix = arg2 & 0xFF;
+        }
+        if (temp_a1 == temp_a0) {
+            attr.mask = 0x1000;
+            attr.ext.reverb = arg2 & 0xFF;
+        }
+    }
+    SpuSetCommonAttr(&attr);
+}
--- src/main/psxsdk/libspu/spu.c
+++ src/main/psxsdk/libspu/spu.c
@@ -12,7 +12,23 @@ INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libspu/spu", _spu_r_);
 
 INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libspu/spu", _spu_t);
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libspu/spu", _spu_write);
+s32 _spu_t(s32, ...);
+s32 _spu_writeByIO(s32, s32);
+extern s32 _spu_mem_mode_plus;
+extern s32 _spu_transMode;
+extern u16 _spu_tsa;
+
+s32 _spu_write(s32 arg0, s32 arg1) {
+
+    if (_spu_transMode != 0) {
+        _spu_writeByIO(arg0, arg1);
+        return arg1;
+    }
+    _spu_t(2, _spu_tsa << _spu_mem_mode_plus);
+    _spu_t(1);
+    _spu_t(3, arg0, arg1);
+    return arg1;
+}
 
 INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libspu/spu", _spu_read);
 
