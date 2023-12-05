a951b65679eb6211b832eb0c192c100095bd605f
Joey Murphy <tjmurphy@mit.edu>
2023-12-05 11:57:24 -0500

    Add PlayerSteps into ric.h

--- src/ric/ric.h
+++ src/ric/ric.h
@@ -9,7 +9,46 @@ typedef enum {
 } EntityIDs;
 
 typedef enum {
-    Dummy,
+    Player_Stand,
+    Player_Walk,
+    Player_Crouch,
+    Player_Fall,
+    Player_Jump,
+    Player_MorphBat,
+    Player_AlucardStuck,
+    Player_MorphMist,
+    Player_HighJump,
+    Player_UnmorphBat,
+    Player_Hit,
+    Player_StatusStone,
+    Player_BossGrab, // Darkwing Bat and Akmodan II
+    Player_KillWater,
+    Player_UnmorphMist,
+    Player_SwordWarp, // Alucard Sword and Osafune Katana
+    Player_Kill,
+    Player_Unk17,
+    Player_Teleport, // also Grand Cross and Spiral Axe
+    Player_FlameWhip,
+    Player_Hydrostorm,
+    Player_ThousandBlades,
+    Player_RichterFourHolyBeasts,
+    Player_Slide,
+    Player_Unk24, // MorphWolf and Richter's Tackle
+    Player_Unk25, // UnMorphWolf and Sprint
+    Player_SlideKick,
+    Player_Unk27, // other item crashes
+    Player_SpellDarkMetamorphosis = 32,
+    Player_SpellSummonSpirit,
+    Player_SpellHellfire,
+    Player_SpellTetraSpirit,
+    Player_Spell36,
+    Player_SpellSoulSteal,
+    Player_Unk38,
+    Player_SpellSwordBrothers,
+    Player_AxearmorStand,
+    Player_AxearmorWalk,
+    Player_AxearmorJump,
+    Player_AxearmorHit,
 } PlayerSteps;
 
 extern void func_80159C04(void);
