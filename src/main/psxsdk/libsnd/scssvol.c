ea6fd198ddcb9e8447bcea9193b24fb7ca3f7589
sozud <122322823+sozud@users.noreply.github.com>
2023-12-17 02:56:26 -0500

    libsnd libspu funcs (#854)
    Co-authored-by @sonicdcer

--- src/main/psxsdk/libsnd/scssvol.c
+++ src/main/psxsdk/libsnd/scssvol.c
@@ -1,3 +1,29 @@
 #include "common.h"
+#include "libsnd_internal.h"
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/scssvol", SsSetSerialVol);
+void SsSetSerialVol(u8 s_num, s16 voll, s16 volr) {
+    SpuCommonAttr attr;
+    if (s_num == 0) {
+        attr.mask = 0xC0;
+        if (voll >= 128) {
+            voll = 127;
+        }
+        if (volr >= 128) {
+            volr = 127;
+        }
+        attr.cd.volume.left = voll * 258;
+        attr.cd.volume.right = volr * 258;
+    }
+    if (s_num == 1) {
+        attr.mask = 0xC00;
+        if (voll >= 128) {
+            voll = 127;
+        }
+        if (volr >= 128) {
+            volr = 127;
+        }
+        attr.ext.volume.left = voll * 258;
+        attr.ext.volume.right = volr * 258;
+    }
+    SpuSetCommonAttr(&attr);
+}
