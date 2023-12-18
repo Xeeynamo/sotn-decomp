692c28b2af07ada9108f8099e8ba8eb009022af4
Luciano Ciccariello <xeeynamo@hotmail.com>
2023-12-18 18:27:16 +0000

    Amend suggested changes to update_animation
    Thanks @bismurphy for the suggestion!

--- src/st/update_animation.h
+++ src/st/update_animation.h
@@ -17,24 +17,24 @@ s32 UpdateAnimation(u8* texAnimations, Primitive* prim) {
             tempUv = nextAnimation[0] + (nextAnimation[1] << 8);
             nextAnimation += 2;
             sp0 = nextAnimation[0] + (nextAnimation[1] << 8);
-            *(s16*)&prim->u0 = tempUv;
-            *(s16*)&prim->u1 = tempUv + *((u8*)(&sp0));
+            LOH(prim->u0) = tempUv;
+            LOH(prim->u1) = tempUv + *((u8*)(&sp0));
             new_var = *((u8*)&sp0 + 1);
-            *(s16*)&prim->u3 = tempUv + sp0;
+            LOH(prim->u3) = tempUv + sp0;
+            LOH(prim->u2) = tempUv + (new_var << 8);
             ++prim->p1;
-            *(s16*)&prim->u2 = tempUv + (new_var << 8);
         } else {
             prim->p1 = 0;
             prim->p2 = 0;
             prim->p2 = texAnimations[0];
             tempUv = texAnimations[1] + (texAnimations[2] << 8);
             sp0 = texAnimations[3] + (texAnimations[4] << 8);
-            *(s16*)&prim->u0 = tempUv;
-            *(s16*)&prim->u1 = tempUv + (*(u8*)&sp0);
+            LOH(prim->u0) = tempUv;
+            LOH(prim->u1) = tempUv + (*(u8*)&sp0);
             new_var2 = *((u8*)&sp0 + 1);
-            *(s16*)&prim->u3 = tempUv + sp0;
+            LOH(prim->u3) = tempUv + sp0;
+            LOH(prim->u2) = tempUv + (new_var2 << 8);
             ++prim->p1;
-            *(s16*)&prim->u2 = tempUv + (new_var2 << 8);
             return 0;
         }
     }
