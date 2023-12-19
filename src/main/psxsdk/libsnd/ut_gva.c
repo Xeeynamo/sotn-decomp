e9d43e1ae275eb1925e62bc95f2f1f835a032a03
sozud <122322823+sozud@users.noreply.github.com>
2023-12-19 12:55:43 -0500

    SsUtGetVagAtr (#870)

--- src/main/psxsdk/libsnd/ut_gva.c
+++ src/main/psxsdk/libsnd/ut_gva.c
@@ -1,3 +1,32 @@
 #include "common.h"
+#include "libsnd_internal.h"
 
-INCLUDE_ASM("asm/us/main/nonmatchings/psxsdk/libsnd/ut_gva", SsUtGetVagAtr);
+short SsUtGetVagAtr(
+    short vabId, short progNum, short toneNum, VagAtr* vagatrptr) {
+    s16 temp_v1;
+    if (svm_vab_used[vabId] == 1) {
+        SpuVmVSetUp(vabId, progNum);
+        temp_v1 = toneNum + (D_800978CF * 0x10);
+        vagatrptr->prior = _svm_tn[temp_v1].prior;
+        vagatrptr->mode = _svm_tn[temp_v1].mode;
+        vagatrptr->vol = _svm_tn[temp_v1].vol;
+        vagatrptr->pan = _svm_tn[temp_v1].pan;
+        vagatrptr->center = _svm_tn[temp_v1].center;
+        vagatrptr->shift = _svm_tn[temp_v1].shift;
+        vagatrptr->max = _svm_tn[temp_v1].max;
+        vagatrptr->min = _svm_tn[temp_v1].min;
+        vagatrptr->vibW = _svm_tn[temp_v1].vibW;
+        vagatrptr->vibT = _svm_tn[temp_v1].vibT;
+        vagatrptr->porW = _svm_tn[temp_v1].porW;
+        vagatrptr->porT = _svm_tn[temp_v1].porT;
+        vagatrptr->pbmin = _svm_tn[temp_v1].pbmin;
+        vagatrptr->pbmax = _svm_tn[temp_v1].pbmax;
+        vagatrptr->adsr1 = _svm_tn[temp_v1].adsr1;
+        vagatrptr->adsr2 = _svm_tn[temp_v1].adsr2;
+        vagatrptr->prog = _svm_tn[temp_v1].prog;
+        vagatrptr->vag = _svm_tn[temp_v1].vag;
+        return 0;
+    }
+
+    return -1;
+}
