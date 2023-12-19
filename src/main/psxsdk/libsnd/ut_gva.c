10a0ec5d6f4f82a098b71bbe235ba894fd82468e
sozud <122322823+sozud@users.noreply.github.com>
2023-12-19 12:48:00 -0500

    signature

--- src/main/psxsdk/libsnd/ut_gva.c
+++ src/main/psxsdk/libsnd/ut_gva.c
@@ -1,30 +1,30 @@
 #include "common.h"
 #include "libsnd_internal.h"
 
-s16 SsUtGetVagAtr(s16 arg0, s16 arg1, s32 arg2, VagAtr* arg3) {
-    s32 temp_v1;
-
-    if (svm_vab_used[arg0] == 1) {
-        SpuVmVSetUp(arg0, arg1);
-        temp_v1 = (s32)((arg2 + (D_800978CF * 0x10)) << 0x10) >> 0x10;
-        arg3->prior = _svm_tn[temp_v1].prior;
-        arg3->mode = _svm_tn[temp_v1].mode;
-        arg3->vol = _svm_tn[temp_v1].vol;
-        arg3->pan = _svm_tn[temp_v1].pan;
-        arg3->center = _svm_tn[temp_v1].center;
-        arg3->shift = _svm_tn[temp_v1].shift;
-        arg3->max = _svm_tn[temp_v1].max;
-        arg3->min = _svm_tn[temp_v1].min;
-        arg3->vibW = _svm_tn[temp_v1].vibW;
-        arg3->vibT = _svm_tn[temp_v1].vibT;
-        arg3->porW = _svm_tn[temp_v1].porW;
-        arg3->porT = _svm_tn[temp_v1].porT;
-        arg3->pbmin = _svm_tn[temp_v1].pbmin;
-        arg3->pbmax = _svm_tn[temp_v1].pbmax;
-        arg3->adsr1 = _svm_tn[temp_v1].adsr1;
-        arg3->adsr2 = _svm_tn[temp_v1].adsr2;
-        arg3->prog = _svm_tn[temp_v1].prog;
-        arg3->vag = _svm_tn[temp_v1].vag;
+short SsUtGetVagAtr(
+    short vabId, short progNum, short toneNum, VagAtr* vagatrptr) {
+    s16 temp_v1;
+    if (svm_vab_used[vabId] == 1) {
+        SpuVmVSetUp(vabId, progNum);
+        temp_v1 = toneNum + (D_800978CF * 0x10);
+        vagatrptr->prior = _svm_tn[temp_v1].prior;
+        vagatrptr->mode = _svm_tn[temp_v1].mode;
+        vagatrptr->vol = _svm_tn[temp_v1].vol;
+        vagatrptr->pan = _svm_tn[temp_v1].pan;
+        vagatrptr->center = _svm_tn[temp_v1].center;
+        vagatrptr->shift = _svm_tn[temp_v1].shift;
+        vagatrptr->max = _svm_tn[temp_v1].max;
+        vagatrptr->min = _svm_tn[temp_v1].min;
+        vagatrptr->vibW = _svm_tn[temp_v1].vibW;
+        vagatrptr->vibT = _svm_tn[temp_v1].vibT;
+        vagatrptr->porW = _svm_tn[temp_v1].porW;
+        vagatrptr->porT = _svm_tn[temp_v1].porT;
+        vagatrptr->pbmin = _svm_tn[temp_v1].pbmin;
+        vagatrptr->pbmax = _svm_tn[temp_v1].pbmax;
+        vagatrptr->adsr1 = _svm_tn[temp_v1].adsr1;
+        vagatrptr->adsr2 = _svm_tn[temp_v1].adsr2;
+        vagatrptr->prog = _svm_tn[temp_v1].prog;
+        vagatrptr->vag = _svm_tn[temp_v1].vag;
         return 0;
     }
 
