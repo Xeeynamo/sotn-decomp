c011a3c461c983241bf4b79b4c09c5e2ae90655e
bismurphy <tjmurphy@mit.edu>
2023-12-27 04:30:58 -0500

    Decompile RIC func_8015C4AC (#894)
    Extremely similar to a DRA function, which is noted in the code.

--- src/ric/202A8.c
+++ src/ric/202A8.c
@@ -61,7 +61,58 @@ void func_8015C2A8(void) {
     }
 }
 
-INCLUDE_ASM("asm/us/ric/nonmatchings/202A8", func_8015C4AC);
+// Same function in DRA is func_8010D59C
+void func_8015C4AC(void) {
+    byte stackpad[40];
+    Primitive* prim;
+    s32 i;
+
+    if (g_Entities[1].ext.entSlot1.unk0 != 0) {
+        return;
+    }
+    if ((g_Player.padTapped & GAMEBUTTONS) ||
+        ((g_Player.padHeld ^ g_Player.padPressed) & g_Player.padHeld &
+         GAMEBUTTONS) ||
+        (PLAYER.velocityY > FIX(0.5))) {
+        g_Entities[1].ext.entSlot1.unk2 = 0;
+        g_Entities[1].ext.entSlot1.unk3 = 0;
+    } else {
+        if (g_Entities[1].ext.entSlot1.unk2 >= 10) {
+            return;
+        }
+        if (g_Entities[1].ext.entSlot1.unk3 == 0) {
+            g_Entities[1].ext.entSlot1.unk3 =
+                D_801545B0[g_Entities[1].ext.entSlot1.unk2];
+        }
+        if (!(--g_Entities[1].ext.entSlot1.unk3 & 0xFF)) {
+            g_Entities[1].ext.entSlot1.unk2++;
+            g_Entities[1].ext.entSlot1.unk3 =
+                D_801545B0[g_Entities[1].ext.entSlot1.unk2];
+        }
+    }
+    if (g_Entities[1].animFrameIdx != 0) {
+        g_Entities[1].animFrameIdx--;
+        return;
+    }
+    prim = &g_PrimBuf[g_Entities[1].primIndex];
+    for (prim = &g_PrimBuf[g_Entities[1].primIndex], i = 0; prim != NULL;
+         prim = prim->next, i++) {
+        if (i == g_Entities[1].entityId) {
+            prim->r0 = prim->g0 = prim->b0 = 0x80;
+            prim->x0 = PLAYER.posX.i.hi;
+            prim->y0 = PLAYER.posY.i.hi;
+            prim->x1 = PLAYER.animCurFrame;
+            prim->y1 = 0;
+            prim->x2 = PLAYER.facingLeft;
+            prim->y2 = PLAYER.palette;
+        }
+    }
+    g_Entities[1].animFrameIdx = 2;
+    g_Entities[1].entityId++;
+    if (g_Entities[1].entityId >= 6) {
+        g_Entities[1].entityId = 0;
+    }
+}
 
 INCLUDE_ASM("asm/us/ric/nonmatchings/202A8", func_8015C6D4);
 
