22c86430ea6b92206579311534e0aab025d18794
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2023-12-30 09:54:50 +0100

    Add File specific funcs (#898)
    I am not sure if we want to use this pattern. Here I am proposing to
    wrap functions to open files and read the entirety of a file.
    
    The objective is to avoid performing sanity checks for `fopen`, `fread`
    and `malloc`, other than ensuring allocated resources are correctly
    de-allocated on both happy and unhappy paths.
    
    How it works is the first parameter is always the callback, while
    starting from the second are all the parameters necessary to instantiate
    and allocate the resources to be passed to the callback.

--- src/pc/pc.h
+++ src/pc/pc.h
@@ -7,10 +7,15 @@
 #define VERSION_US
 #define VERSION "us"
 
+#include <stdio.h>
 #include <log.h>
 
 #define DISP_WIDTH 256
 #define DISP_HEIGHT 256
 #define SCREEN_SCALE 2
+
+bool FileRead(bool (*cb)(FILE* file), const char* path);
+bool FileStringify(bool (*cb)(const char* content), const char* path);
+bool FileUseContent(bool (*cb)(void* content), const char* path);
 
 #endif
