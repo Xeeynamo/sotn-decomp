23d8e72ec89929d1a52b9242a202e0237288010f
Joey Murphy <tjmurphy@mit.edu>
2024-02-01 15:12:05 -0500

    Decompile RIC func_80166784

--- src/ric/2A060.c
+++ src/ric/2A060.c
@@ -227,7 +227,441 @@ void EntityTeleport(Entity* self) {
     }
 }
 
-INCLUDE_ASM("ric/nonmatchings/2A060", func_80166784);
+// Entity #10, uses blueprint #10. Appears related to poisoning, due to call in
+// func_8015D3CC
+void func_80166784(Entity* self) {
+    byte stackpad[40];
+    s32 sp38;
+    s16 upperParams;
+    s16 lowerParams;
+    Primitive* prim;
+    s16 angle;
+    s32 var_s3;
+    s32 var_s4;
+    s32 var_s5;
+    s32 temp_s6;
+    s32 var_s7;
+    s16 temp_a2;
+    s32 temp_a3;
+    s32 directionsPressed;
+
+    s32 randy;
+
+    s32 xDiff;
+    s32 yDiff;
+
+    s32 selfY;
+    s32 selfX;
+    f32 dumb_temp;
+    u16 playerUnk46;
+
+    s32 magSq;
+    s32 ySq;
+    s32 xSq;
+
+    lowerParams = self->params & 0xFF;
+    upperParams = self->params & 0xFF00;
+    if (lowerParams == 0) {
+        if (self->ext.et_80166784.unkA6 != 0) {
+            self->ext.et_80166784.unkA6--;
+        }
+    }
+    playerUnk46 = g_Player.unk46;
+    if (playerUnk46 == 0) {
+        DestroyEntity(self);
+        return;
+    }
+    if ((self->step == 2) && (playerUnk46 == 2)) {
+        self->step = 1;
+        self->velocityX = 0;
+        self->velocityY = 0;
+        self->palette = 0x8148;
+        if (PLAYER.facingLeft == 0) {
+            self->ext.et_80166784.unk88 = playerUnk46;
+        } else {
+            self->ext.et_80166784.unk88 = 3;
+        }
+        self->ext.et_80166784.unk8C = 0x600;
+    }
+    if (self->step != 0) {
+        xDiff = self->posX.val - self->ext.et_80166784.unk98;
+        yDiff = self->posY.val - self->ext.et_80166784.unk9C;
+        self->ext.et_80166784.unk7C.val += xDiff;
+        self->ext.et_80166784.unk80.val += yDiff;
+    }
+    directionsPressed = g_Player.padPressed & 0xF000;
+    if (directionsPressed == PAD_UP) {
+        var_s4 = D_80155B2C[0][self->ext.et_80166784.unk88];
+    }
+    if (directionsPressed == PAD_DOWN) {
+        var_s4 = D_80155B2C[3][self->ext.et_80166784.unk88];
+    }
+    if (directionsPressed == PAD_RIGHT) {
+        var_s4 = D_80155B2C[1][self->ext.et_80166784.unk88];
+    }
+    if (directionsPressed == PAD_LEFT) {
+        var_s4 = D_80155B2C[2][self->ext.et_80166784.unk88];
+    }
+    if (directionsPressed == (PAD_UP + PAD_RIGHT)) {
+        var_s4 = D_80155B2C[4][self->ext.et_80166784.unk88];
+    }
+    if (directionsPressed == (PAD_UP + PAD_LEFT)) {
+        var_s4 = D_80155B2C[5][self->ext.et_80166784.unk88];
+    }
+    if (directionsPressed == (PAD_DOWN + PAD_RIGHT)) {
+        var_s4 = D_80155B2C[6][self->ext.et_80166784.unk88];
+    }
+    if (directionsPressed == (PAD_DOWN + PAD_LEFT)) {
+        var_s4 = D_80155B2C[7][self->ext.et_80166784.unk88];
+    }
+    if (directionsPressed == 0) {
+        var_s4 = D_80155B2C[8][self->ext.et_80166784.unk88];
+    }
+    self->ext.et_80166784.unk86 = 0;
+    if (lowerParams == 0) {
+        if (self->step == 1) {
+            if (PLAYER.step == 2) {
+                if (PLAYER.facingLeft == 0) {
+                    var_s3 = D_80155A40[var_s4];
+                } else {
+                    var_s3 = D_80155A1C[var_s4].x;
+                }
+                var_s5 = D_80155A1C[var_s4].y;
+            } else {
+                if (PLAYER.step == 0) {
+                    if (PLAYER.facingLeft == 0) {
+                        var_s3 = (&D_80155A08)[var_s4];
+                    } else {
+                        var_s3 = D_801559E4[var_s4].x;
+                    }
+                } else if (PLAYER.facingLeft == 0) {
+                    var_s3 = D_80155A78[var_s4];
+                } else {
+                    var_s3 = D_80155A54[var_s4].x;
+                }
+                var_s5 = D_801559E4[var_s4].y;
+            }
+        } else {
+            if (PLAYER.step == 2) {
+                var_s3 = D_801559C8[PLAYER.animFrameIdx].x;
+                var_s5 = D_801559C8[PLAYER.animFrameIdx].y;
+            } else {
+                var_s3 = D_801559AC[PLAYER.animFrameIdx].x;
+                var_s5 = D_801559AC[PLAYER.animFrameIdx].y;
+            }
+            if (PLAYER.facingLeft == 0) {
+                var_s3 = -var_s3;
+            }
+        }
+        var_s3 <<= 16;
+        var_s5 <<= 16;
+        var_s3 += PLAYER.posX.val;
+        var_s5 += PLAYER.posY.val;
+    } else {
+        self--;
+        var_s3 = self->posX.val;
+        var_s5 = self->posY.val;
+        self++;
+    }
+    switch (self->step) {
+    case 0:
+        self->animSet = -0x7FEE;
+        self->animCurFrame = 0x1E;
+        self->palette = 0x8138;
+        self->unk5A = 0x46;
+        if (lowerParams < 8) {
+            self->hitboxWidth = 3;
+            self->hitboxHeight = 3;
+            self->hitboxOffX = 0;
+            self->hitboxOffY = 0;
+            self->ext.et_80166784.unkB0 = 0xE;
+            func_8015FAB8(self);
+        }
+        if (lowerParams == 0) {
+            self->zPriority = PLAYER.zPriority - 2;
+        }
+        if (lowerParams == 7) {
+            self->animCurFrame = 0x1F;
+            self->hitboxWidth++;
+            self->hitboxHeight++;
+        }
+        self->zPriority = PLAYER.zPriority + 4;
+        self->posX.val = var_s3;
+        self->posY.val = var_s5;
+        self->flags = 0x04070000;
+        self->ext.et_80166784.unk8C = 0x500;
+        self->ext.et_80166784.unk7C = self->posX.val;
+        self->ext.et_80166784.unk80 = self->posY.val;
+        self->ext.et_80166784.unk98 = self->ext.et_80166784.unk7C.val;
+        self->ext.et_80166784.unk9C = self->ext.et_80166784.unk80.val;
+        self->primIndex = g_api.AllocPrimitives(PRIM_LINE_G2, 1);
+        if (self->primIndex != -1) {
+            self->flags |= 0x800000;
+            prim = &g_PrimBuf[self->primIndex];
+            prim->b0 = prim->g0 = prim->r0 = 0x7F;
+            prim->b1 = prim->g1 = prim->r1 = 0x1F;
+            prim->priority = PLAYER.zPriority + 2;
+            prim->drawMode = 0x35;
+        }
+        self->step = 2;
+        func_80166784(self);
+        break;
+    case 1:
+        if (upperParams == 0) {
+            if ((u16)(self->palette + 0x7EC0) < 8) {
+                if (self->ext.et_80166784.unkA4 != 0) {
+                    self->ext.et_80166784.unkA4--;
+                } else {
+                    self->palette++;
+                }
+            }
+        } else if (self->ext.et_80166784.unkA4 != 0) {
+            if (!(lowerParams & 1)) {
+                CreateEntFactoryFromEntity(self, FACTORY(0, 20), 0);
+                self->ext.et_80166784.unkA4 = 0;
+            }
+        }
+        if (lowerParams < 8) {
+            if (self->ext.et_80166784.unk84 != 0) {
+                if (--self->ext.et_80166784.unk84 == 0) {
+                    if (self->velocityY < 0) {
+                        self->velocityY = 0;
+                    }
+                    self->velocityX = 0;
+                }
+            } else {
+                self->velocityX = self->velocityX * 3 / 4;
+            }
+            if (self->ext.et_80166784.unk88 == var_s4) {
+                break;
+            }
+
+            // This block has a really weird extra instruction.
+            //  Required a lot of faking to get it to match. The
+            //  first if-statement really should not matter.
+            if (lowerParams == 0) {
+                s32 a1;
+                Entity* a0;
+                if (upperParams == 0) {
+                    a1 = FACTORY(0, 18);
+                } else {
+                    a0 = self;
+                }
+                if (self->ext.et_80166784.unkA6 == 0) {
+                    g_api.PlaySfx(0x602);
+                    self->ext.et_80166784.unkA6 = 0x20;
+                }
+                if (upperParams == 0) {
+                    a0 = self;
+                    a1 = FACTORY(0, 18);
+                } else {
+                    a0 = self;
+                    a1 = FACTORY(0x100, 18);
+                }
+                CreateEntFactoryFromEntity(a0, a1, 0);
+            }
+
+            self->ext.et_80166784.unk84 = 6;
+            if (var_s4 == 0) {
+                if (PLAYER.facingLeft == 0) {
+                    xDiff = D_80155A08;
+                } else {
+                    xDiff = D_801559E4[0].x;
+                }
+                // This should be 2 simple lines, but I had to be pedantic about
+                // the individual steps to get it to match.
+                selfY = self->posY.val;
+                selfX = self->posX.val;
+                yDiff = D_801559E4[0].y;
+                xDiff += PLAYER.posX.i.hi;
+                yDiff += PLAYER.posY.i.hi;
+                xDiff <<= 16;
+                yDiff <<= 16;
+
+                angle = ratan2(selfY - yDiff, xDiff - selfX);
+                self->velocityX = (rcos(angle) << 7) >> 4;
+                self->velocityY = -(rsin(angle) << 7) >> 4;
+            } else {
+                self->velocityY = D_80155AE4[var_s4].x;
+                self->velocityX = D_80155AE4[var_s4].y;
+            }
+            self->ext.et_80166784.unk88 = var_s4;
+        }
+        break;
+    case 2:
+        if (lowerParams >= 8) {
+            if (self->flags & FLAG_HAS_PRIMS) {
+                if (PLAYER.animFrameIdx == 5) {
+                    g_api.FreePrimitives(self->primIndex);
+                    self->flags &= ~FLAG_HAS_PRIMS;
+                }
+            }
+            if (PLAYER.animFrameIdx == 6) {
+                if ((PLAYER.animFrameDuration >= 0xF) ||
+                    (PLAYER.animFrameDuration == 0)) {
+                    self->palette = 0x8138;
+                } else if ((PLAYER.animFrameDuration == 11 ||
+                            PLAYER.animFrameDuration == 12) ||
+                           (PLAYER.animFrameDuration == 13 ||
+                            PLAYER.animFrameDuration == 14)) {
+                    self->palette = 0x813C;
+                } else {
+                    self->palette = D_80155C70[PLAYER.animFrameDuration % 3];
+                }
+            }
+        } else {
+            self->velocityX = D_80155AAC[PLAYER.animFrameIdx].x;
+            self->velocityY = D_80155AAC[PLAYER.animFrameIdx].y;
+            switch (PLAYER.animFrameIdx) {
+            case 0:
+                angle = (rand() & 0x3FF) - 0x200;
+                self->velocityX = (rcos(angle) << 0xB) >> 4;
+                self->velocityY = -(rsin(angle) << 0xB) >> 4;
+                break;
+            case 1:
+                randy = rand() | ~0x3FF;
+                self->velocityX = (rcos(randy) << 0xB) >> 4;
+                self->velocityY = -(rsin(randy) << 0xB) >> 3;
+                self->ext.et_80166784.unk8C = 0x600;
+                break;
+            case 4:
+                self->ext.et_80166784.unk8C = 0x3C0;
+                if (PLAYER.animFrameDuration == 1) {
+                    self->velocityX = FIX(-8);
+                }
+                break;
+            case 5:
+                self->ext.et_80166784.unk8C += 0xC0;
+                if (self->flags & FLAG_HAS_PRIMS) {
+                    g_api.FreePrimitives(self->primIndex);
+                    self->flags &= ~FLAG_HAS_PRIMS;
+                }
+                break;
+            case 6:
+                if (upperParams != 0) {
+                    if (PLAYER.animFrameDuration == D_80155C78[lowerParams]) {
+                        CreateEntFactoryFromEntity(self, FACTORY(0, 20), 0);
+                    }
+                }
+                if (lowerParams == (0x10 - PLAYER.animFrameDuration)) {
+                    self->ext.et_80166784.unk8C = 0x600;
+                }
+                // This very well might be a switch
+                if (PLAYER.animFrameDuration > 14) {
+                    self->palette = 0x8138;
+                } else if (PLAYER.animFrameDuration == 0) {
+                    self->palette = 0x8138;
+                } else if (PLAYER.animFrameDuration == 11 ||
+                           PLAYER.animFrameDuration == 12) {
+                    self->palette = 0x813C;
+                } else if (PLAYER.animFrameDuration == 13 ||
+                           PLAYER.animFrameDuration == 14) {
+                    self->palette = 0x813C;
+                } else {
+                    self->palette = D_80155C70[PLAYER.animFrameDuration % 3];
+                }
+                break;
+            }
+            if (PLAYER.facingLeft == 0) {
+                self->velocityX = -self->velocityX;
+            }
+            self->velocityX /= 2;
+            self->velocityY /= 2;
+        }
+        break;
+    }
+    temp_a2 = self->ext.et_80166784.unk8C;
+    temp_s6 = self->posX.val;
+    sp38 = self->posY.val;
+    if (lowerParams == 0) {
+        self->posX.val = var_s3;
+        self->posY.val = var_s5;
+    } else if (lowerParams >= 8) {
+        // weird reuse of variables here
+        xDiff = (self - 8)->posX.val;
+        yDiff = (self - 8)->posY.val;
+        temp_a3 = (self - 7)->posX.val;
+        self->posX.val = (xDiff + temp_a3) / 2;
+        var_s7 = (self - 7)->posY.val;
+        self->posY.val = (yDiff + var_s7) / 2;
+    } else {
+        temp_a3 = temp_s6 - self->ext.et_80166784.unk7C.val;
+        var_s7 = sp38 - self->ext.et_80166784.unk80.val;
+        if (self->step == 1) {
+            if (self->velocityY < FIX(0.5)) {
+                self->posX.val = temp_a3 + temp_s6;
+                self->posY.val += var_s7;
+            }
+            if ((u32)(self->velocityY - FIX(0.5)) < FIX(0.75)) {
+                self->posX.val += temp_a3 * 15 / 16;
+                self->posY.val += var_s7 * 15 / 16;
+            }
+            if ((u32)self->velocityY >= FIX(1.25)) {
+                self->posX.val += temp_a3 * 13 / 16;
+                self->posY.val += var_s7 * 13 / 16;
+            }
+        } else {
+            self->posX.val = temp_a3 * 3 / 4 + temp_s6;
+            self->posY.val += var_s7 * 3 / 4;
+        }
+        if ((self->velocityY < FIX(8)) || (self->velocityY < 0)) {
+            self->velocityY += 0xA00;
+        }
+        self->posX.val += self->velocityX;
+        self->posY.val += self->velocityY;
+
+        magSq = ((temp_a2 * temp_a2) >> 0x10);
+        yDiff = self->posY.val - var_s5;
+        ySq = (yDiff * (yDiff >> 0x10)) >> 0x10;
+        xDiff = self->posX.val - var_s3;
+        xSq = (xDiff * (xDiff >> 0x10)) >> 0x10;
+        if (magSq < (ySq + xSq)) {
+            angle = ratan2(-yDiff, xDiff);
+            xDiff = (rcos(angle) * temp_a2) >> 4;
+            yDiff = -(rsin(angle) * temp_a2) >> 4;
+            self->posX.val = var_s3 + xDiff;
+            self->posY.val = var_s5 + yDiff;
+        }
+    }
+    self->ext.et_80166784.unk7C = temp_s6;
+    self->ext.et_80166784.unk80 = sp38;
+    self->ext.et_80166784.unkA0 = var_s7;
+    self->ext.et_80166784.unk98 = self->posX.val;
+    self->ext.et_80166784.unk9C = self->posY.val;
+    if (lowerParams == 0) {
+        D_80175080 = var_s4;
+        D_80175084 = self->ext.et_80166784.unk88;
+    }
+    if ((self->primIndex != -1) && (self->flags & FLAG_HAS_PRIMS)) {
+        prim = &g_PrimBuf[self->primIndex];
+        prim->x0 = self->posX.i.hi;
+        prim->y0 = self->posY.i.hi;
+        // for some reason need this to make it use lh instead of lhu for x1 and
+        // y1.
+        dumb_temp = self->ext.et_80166784.unk7C;
+        prim->x1 = dumb_temp.i.hi;
+        dumb_temp = self->ext.et_80166784.unk80;
+        prim->y1 = dumb_temp.i.hi;
+        if (PLAYER.animFrameIdx == 4) {
+            prim->priority = PLAYER.zPriority + 4;
+        } else {
+            prim->priority = PLAYER.zPriority + 2;
+        }
+    }
+    if (upperParams != 0) {
+        if (lowerParams != 7) {
+            self->unk6C =
+                (rsin(
+                     (s16)((((g_GameTimer / 4) & 7) + D_80155C98[lowerParams]) *
+                           512)) >>
+                 6) -
+                0x80;
+            self->drawMode = 0x30;
+            self->drawFlags |= DRAW_HIDE;
+        }
+        self->palette = 0x8160;
+    }
+}
 
 void func_8016779C(Entity* entity) {
     if (g_Player.unk46 == 0) {
