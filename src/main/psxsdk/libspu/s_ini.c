4b57b133d0d9ca8be16b53b44ae3c444029faf5a
sozud <122322823+sozud@users.noreply.github.com>
2024-02-16 21:19:47 -0800

    _SpuInit

--- src/main/psxsdk/libspu/s_ini.c
+++ src/main/psxsdk/libspu/s_ini.c
@@ -1,7 +1,59 @@
 #include "common.h"
 #include "libspu_internal.h"
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libspu/s_ini", _SpuInit);
+extern s32 _spu_fd;
+extern s32 _spu_trans_mode;
+extern s32 _spu_rev_flag;
+extern s32 _spu_rev_reserve_wa;
+extern s32 _spu_rev_offsetaddr;
+
+struct SpuRevAttr {
+    s32 unk0;
+    s32 unk18;
+    s16 unk1c;
+    s16 unk1e;
+    s32 unk20;
+    s32 unk24;
+};
+
+extern struct SpuRevAttr _spu_rev_attr;
+
+extern s16 _spu_voice_centerNote[];
+
+extern s32 _spu_EVdma;
+
+extern s32 _spu_keystat;
+
+extern s32 _spu_rev_startaddr[];
+
+void _SpuInit(s32 arg0) {
+    u16* var_v0;
+    s32 var_v1;
+    u16 temp;
+
+    ResetCallback();
+    _spu_init(arg0);
+    if (arg0 == 0) {
+        temp = 0xC000;
+        for (var_v1 = 0x17, var_v0 = &_spu_voice_centerNote[0x17]; var_v1 >= 0;
+             var_v1--, var_v0--) {
+            *var_v0 = temp;
+        }
+    }
+    SpuStart();
+    _spu_rev_flag = 0;
+    _spu_rev_reserve_wa = 0;
+    _spu_rev_attr.unk18 = 0;
+    _spu_rev_attr.unk1c = 0;
+    _spu_rev_attr.unk1e = 0;
+    _spu_rev_attr.unk20 = 0;
+    _spu_rev_attr.unk24 = 0;
+    _spu_rev_offsetaddr = _spu_rev_startaddr[0];
+    _spu_FsetRXX(0xD1, _spu_rev_startaddr[0], 0);
+    _spu_trans_mode = 0;
+    _spu_transMode = 0;
+    _spu_keystat = 0;
+}
 
 void SpuStart(void) {
     s32 temp_v0;
--- src/main/psxsdk/libspu/s_sr.c
+++ src/main/psxsdk/libspu/s_sr.c
@@ -6,27 +6,27 @@ s32 SpuSetReverb(s32 on_off) {
     u16 var_v1;
     switch (on_off) {
     case 0:
-        D_800330A0 = 0;
+        _spu_rev_flag = 0;
         var_v1 = _spu_RXX->rxx.spucnt;
         var_v1 = var_v1 & 0xFF7F;
         _spu_RXX->rxx.spucnt = var_v1;
         break;
 
     case 1:
-        if ((D_800330A4 != on_off) &&
-            (_SpuIsInAllocateArea_(D_800330A8) != 0)) {
-            D_800330A0 = 0;
+        if ((_spu_rev_reserve_wa != on_off) &&
+            (_SpuIsInAllocateArea_(_spu_rev_offsetaddr) != 0)) {
+            _spu_rev_flag = 0;
             var_v1 = _spu_RXX->rxx.spucnt;
             var_v1 = var_v1 & 0xFF7F;
             new_var = var_v1;
             _spu_RXX->rxx.spucnt = new_var;
         } else {
-            D_800330A0 = on_off;
+            _spu_rev_flag = on_off;
             var_v1 = (new_var = _spu_RXX->rxx.spucnt) | 0x80;
             _spu_RXX->rxx.spucnt = var_v1;
         }
         break;
     }
 
-    return D_800330A0;
+    return _spu_rev_flag;
 }
