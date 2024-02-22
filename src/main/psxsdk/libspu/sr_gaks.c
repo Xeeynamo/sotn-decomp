e1ba1ee2454488bad52258d08c64fc4219735927
sozud <122322823+sozud@users.noreply.github.com>
2024-02-22 00:38:20 -0800

    SpuGetAllKeysStatus

--- src/main/psxsdk/libspu/sr_gaks.c
+++ src/main/psxsdk/libspu/sr_gaks.c
@@ -1,5 +1,29 @@
 #include "common.h"
+#include "libspu_internal.h"
 
 INCLUDE_ASM("main/nonmatchings/psxsdk/libspu/sr_gaks", SpuRGetAllKeysStatus);
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libspu/sr_gaks", SpuGetAllKeysStatus);
+extern s32 _spu_keystat;
+
+void SpuGetAllKeysStatus(char* status) {
+    s32 voice;
+    u16* new_var;
+    u16 volumex;
+    s32 max;
+    max = 24;
+    for (voice = 0; voice < max; voice++) {
+        new_var = &_spu_RXX->rxx.voice[voice].volumex;
+        volumex = *new_var;
+        if ((1 << voice) & _spu_keystat) {
+            if (volumex != 0) {
+                status[voice] = 1;
+            } else {
+                status[voice] = 3;
+            }
+        } else if (volumex != 0) {
+            status[voice] = 2;
+        } else {
+            status[voice] = 0;
+        }
+    }
+}
