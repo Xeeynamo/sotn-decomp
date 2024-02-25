a47ed09821d2739a2c87d458575337f11d5a9b62
Ross Squires <ross.squires@gmail.com>
2024-02-25 11:18:32 -0500

    Decomp w_001 func_ptr_80170008

--- src/weapon/w_001.c
+++ src/weapon/w_001.c
@@ -8,7 +8,57 @@ INCLUDE_ASM("weapon/nonmatchings/w_001", EntityWeaponAttack);
 
 INCLUDE_ASM("weapon/nonmatchings/w_001", func_ptr_80170004);
 
-INCLUDE_ASM("weapon/nonmatchings/w_001", func_ptr_80170008);
+extern AnimationFrame D_B000_8017AFC8;
+
+void func_ptr_80170008(Entity* self) {
+    u16 new_var;
+    switch (self->step) {
+    case 0:
+        self->flags = 0x08100000;
+        self->animSet = 9;
+        self->unk4C = &D_B000_8017AFC8;
+        self->zPriority = PLAYER.zPriority - 2;
+        self->facingLeft = (PLAYER.facingLeft + 1) & 1;
+        SetSpeedX(-0x48000);
+        self->palette = 0x81B0;
+        self->drawMode = 0x30;
+        self->posX.val += self->velocityX * 6;
+        self->posY.i.hi -= 0xD;
+        do {
+            if (PLAYER.drawFlags & 2) {
+                self->posY.i.hi -= 0x3;
+            }
+            self->drawFlags = 3;
+            self->rotY = 0x60;
+            self->rotX = 0x60;
+            self->ext.factory.unkAE =
+                self->ext.factory.parent->ext.factory.unkAE;
+            SetWeaponProperties(self, 0);
+            new_var = self->ext.factory.parent->enemyId;
+            self->hitboxWidth = 0xA;
+            self->hitboxHeight = 8;
+            self->enemyId = new_var;
+        } while (0);
+        g_api_PlaySfx(0x62C);
+        self->step += 1;
+        return;
+
+    case 1:
+        if (self->hitFlags != 0) {
+            DestroyEntity(self);
+            return;
+        }
+        self->posX.val += self->velocityX;
+        self->rotX += 0x10;
+        if ((self->rotY += 0x10) >= 0x101) {
+            self->rotY = 0x100;
+        }
+        if (self->rotX >= 0x181) {
+            self->rotX = 0x180;
+        }
+        return;
+    }
+}
 
 void func_ptr_8017000C(Entity* self) {}
 
