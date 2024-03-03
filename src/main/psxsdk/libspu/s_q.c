67760e543219cecdb7ea34cbee396725e9f4df09
sozud <122322823+sozud@users.noreply.github.com>
2024-03-03 14:38:42 -0800

    SpuQuit

--- src/main/psxsdk/libspu/s_q.c
+++ src/main/psxsdk/libspu/s_q.c
@@ -1,3 +1,19 @@
 #include "common.h"
+#include "libspu_internal.h"
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libspu/s_q", SpuQuit);
+s32 CloseEvent(u32);
+s32 DisableEvent(u32);
+void _SpuDataCallback(void (*arg0)());
+
+void SpuQuit(void) {
+    if (_spu_isCalled == 1) {
+        _spu_isCalled = 0;
+        EnterCriticalSection();
+        _spu_transferCallback = NULL;
+        _spu_IRQCallback = NULL;
+        _SpuDataCallback(0);
+        CloseEvent(_spu_EVdma);
+        DisableEvent(_spu_EVdma);
+        ExitCriticalSection();
+    }
+}
