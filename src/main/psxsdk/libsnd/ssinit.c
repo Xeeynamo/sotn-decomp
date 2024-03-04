06d3c6aac16ebf7da6b1d694daf3a444ba5b816e
sozud <122322823+sozud@users.noreply.github.com>
2024-03-04 12:31:44 -0800

    _SsInit

--- src/main/psxsdk/libsnd/ssinit.c
+++ src/main/psxsdk/libsnd/ssinit.c
@@ -1,3 +1,36 @@
 #include "common.h"
+#include "libsnd_i.h"
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libsnd/ssinit", _SsInit);
+typedef void (*SndSsMarkCallbackProc)(short seq_no, short sep_no, short data);
+extern SndSsMarkCallbackProc _SsMarkCallback[32][16];
+extern short D_80032EC0[];
+extern u16 D_80032ED0[];
+
+void _SsInit(void) {
+    u16* var_a2;
+    int i, j;
+
+    var_a2 = (u16*)0x1F801C00;
+    for (i = 0; i < 24; i++) {
+        for (j = 0; j < 8; j++) {
+            *var_a2++ = D_80032EC0[j];
+        }
+    }
+
+    var_a2 = (u16*)0x1F801D80;
+    for (i = 0; i < 16; i++) {
+        *var_a2++ = D_80032ED0[i];
+    }
+
+    SpuVmInit(24);
+
+    for (j = 0; j < 32; j++) {
+        for (i = 0; i < 16; i++) {
+            _SsMarkCallback[j][i] = 0;
+        }
+    }
+
+    VBLANK_MINUS = 0x3C;
+    _snd_openflag = 0;
+    _snd_ev_flag = 0;
+}
