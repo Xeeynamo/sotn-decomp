763cd091f41f2e39206cdc651bb80f7d745b8267
sozud <122322823+sozud@users.noreply.github.com>
2024-03-05 12:41:37 -0800

    SpuSetIRQ (thanks to @Mc-muffin)

--- src/main/psxsdk/libspu/s_si.c
+++ src/main/psxsdk/libspu/s_si.c
@@ -1,3 +1,26 @@
 #include "common.h"
+#include "libspu_internal.h"
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libspu/s_si", SpuSetIRQ);
+s32 SpuSetIRQ(s32 arg0) {
+    u32 var_v1;
+
+    if ((arg0 == 0) || (arg0 == 3)) {
+        _spu_RXX->rxx.spucnt &= ~0x40;
+        for (var_v1 = 0; _spu_RXX->rxx.spucnt & 0x40; var_v1++) {
+            if (var_v1 + 1 > 0xF00) {
+                printf("SPU: time out: wait (IRQ/ON)");
+                return -1;
+            }
+        }
+    }
+    if ((arg0 == 1) || (arg0 == 3)) {
+        _spu_RXX->rxx.spucnt |= 0x40;
+        for (var_v1 = 0; !(_spu_RXX->rxx.spucnt & 0x40); var_v1++) {
+            if (var_v1 + 1 > 0xF00) {
+                printf("SPU: time out: wait (IRQ/OFF)");
+                return -1;
+            }
+        }
+    }
+    return arg0;
+}
