a1bd5d2c9ff8865455beed46c99ed29f01cb7677
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-09 16:53:07 +0200

    PSXSDK: Decompile intr_vb.c

--- src/main/psxsdk/libetc/intr_vb.c
+++ src/main/psxsdk/libetc/intr_vb.c
@@ -1,9 +1,49 @@
 #include "common.h"
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libetc/intr_vb", startIntrVSync);
+typedef void (*Callback)();
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libetc/intr_vb", trapIntrVSync);
+void trapIntrVSync(void);
+void setIntrVSync(int index, Callback callback);
+void VSync_memclr(int*, int);
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libetc/intr_vb", setIntrVSync);
+extern void InterruptCallback(int, void*);
+extern Callback D_8002D354[8];
+extern volatile u_long* D_8002D378; /* 0x1F801114 - Timer 1 Counter Mode */
+extern volatile int Vcount;
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libetc/intr_vb", VSync_memclr);
+void* startIntrVSync(void) {
+    *D_8002D378 = 0x107;
+    Vcount = 0;
+    VSync_memclr(&D_8002D354, 8);
+    InterruptCallback(0, &trapIntrVSync);
+    return &setIntrVSync;
+}
+
+void trapIntrVSync(void) {
+    int i;
+
+    Vcount += 1;
+    (void)Vcount;
+
+    for (i = 0; i < 8; i++) {
+        if (D_8002D354[i] != NULL) {
+            D_8002D354[i]();
+        }
+    }
+}
+
+void setIntrVSync(int index, Callback callback) {
+    if (callback != D_8002D354[index]) {
+        D_8002D354[index] = callback;
+    }
+}
+
+void VSync_memclr(int* ptr, int size) {
+    int i;
+    int* e = ptr;
+
+    for (i = size - 1; i != -1; i--) {
+        *e = 0;
+        e++;
+    }
+}
