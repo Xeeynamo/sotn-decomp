9b94a397737305a3a9d4b651dbd2e1b37944c004
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2024-03-09 17:19:01 +0200

    Decompile libetc intr_*.c (#1096)

--- src/main/psxsdk/libetc/intr_dma.c
+++ src/main/psxsdk/libetc/intr_dma.c
@@ -1,9 +1,68 @@
 #include "common.h"
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libetc/intr_dma", startIntrDMA);
+typedef void (*Callback)();
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libetc/intr_dma", trapIntrDMA);
+Callback setIntrDMA(int index, Callback callback);
+void trapIntrDMA(void);
+void DMA_memclr(int* ptr, int size);
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libetc/intr_dma", setIntrDMA);
+extern void InterruptCallback(int, void*);
+extern Callback D_8002D380[8];
+extern volatile u_long* D_8002D37C; /* 0x1F8010F4 - DMA Interrupt Register */
+extern unsigned long* D_8002D3A0;
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libetc/intr_dma", DMA_memclr);
+void* startIntrDMA(void) {
+    DMA_memclr(&D_8002D380, 8);
+    *D_8002D37C = 0;
+    InterruptCallback(3, &trapIntrDMA);
+    return &setIntrDMA;
+}
+
+void trapIntrDMA(void) {
+    u32 mask;
+    int i;
+
+    while ((mask = (*D_8002D37C >> 24) & 0x7f) != 0) {
+        for (i = 0; mask != 0 && i < 7; i++, mask >>= 1) {
+            if (mask & 1) {
+                *D_8002D37C &= (0xffffff | (1 << (i + 24)));
+                if (D_8002D380[i] != NULL) {
+                    D_8002D380[i]();
+                }
+            }
+        }
+    }
+
+    if ((*D_8002D37C & 0xFF000000) == 0x80000000 || *D_8002D37C & 0x8000) {
+        printf("DMA bus error: code=%08x\n", *D_8002D37C);
+        for (i = 0; i < 7; i++) {
+            printf("MADR[%d]=%08x\n", i, D_8002D3A0[4 * i]);
+        }
+    }
+}
+
+Callback setIntrDMA(int index, Callback callback) {
+    Callback prev = D_8002D380[index];
+    if (callback != prev) {
+        if (callback != NULL) {
+            D_8002D380[index] = callback;
+            *D_8002D37C =
+                (*D_8002D37C & 0xFFFFFF) | 0x800000 | ((1 << (index + 16)));
+        } else {
+            D_8002D380[index] = 0;
+            *D_8002D37C =
+                ((*D_8002D37C & 0xFFFFFF) | 0x800000) & ~(1 << (index + 16));
+        }
+    }
+    return prev;
+}
+
+void DMA_memclr(int* ptr, int size) {
+    int i;
+    int* e = ptr;
+
+    for (i = size - 1; i != -1; i--) {
+        *e = 0;
+        e++;
+    }
+}
