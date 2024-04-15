60412395cc41b2c462fcfc28fba57aa96e8abdce
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-04-15 20:15:15 +0100

    ugly psp makefile

--- Makefile.psp.mk
+++ Makefile.psp.mk
@@ -4,7 +4,11 @@ PSP_EU_TARGETS	:= tt_000
 SPLAT_PIP		:= splat split
 MWCPP_APP		:= python3 tools/mwcpp.py
 
-extract_pspeu: $(addprefix $(BUILD_DIR)/,$(addsuffix .ld,$(PSP_EU_TARGETS)))
+define list_o_files_psp
+	$(foreach file,$(call list_src_files,$(1)),$(PSP_BUILD_DIR)/$(file).o)
+endef
+
+extract_pspeu: $(addprefix $(PSP_BUILD_DIR)/,$(addsuffix .ld,$(PSP_EU_TARGETS)))
 
 bin/wibo:
 	wget -O $@ https://github.com/decompals/wibo/releases/download/0.6.13/wibo
@@ -12,13 +16,18 @@ bin/wibo:
 	chmod +x bin/wibo
 bin/mwccpsp.exe: bin/wibo bin/mwccpsp_3.0.1_147
 
-$(PSP_BUILD_DIR)/tt_%.ld: $(CONFIG_DIR)/splat.$(VERSION).tt_%.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).tt_%.txt
-	$(SPLAT_PIP) $<
-
 $(PSP_BUILD_DIR)/%.c.o: %.c bin/mwccpsp.exe
 	mkdir -p $(dir $@)
 	$(MWCPP_APP) $< -o $@.processed
 	$(CCPSP) -gccinc -Iinclude -D_internal_version_hd -O0 -c -g -o $@ $@.processed
 	rm $@.processed
 
-tt_000_psp: $(PSP_BUILD_DIR)/src/servant/tt_000_psp/80.c.o
+#tt_000_psp: $(PSP_BUILD_DIR)/src/servant/tt_000_psp/80.c.o
+tt_000_psp: $(PSP_BUILD_DIR)/tt_000.bin
+
+$(PSP_BUILD_DIR)/tt_%.bin: $(PSP_BUILD_DIR)/tt_%.elf
+	$(OBJCOPY) -O binary $< $@
+$(PSP_BUILD_DIR)/tt_%.ld: $(CONFIG_DIR)/splat.pspeu.tt_%.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.pspeu.tt_%.txt
+	$(SPLAT_PIP) $<
+$(PSP_BUILD_DIR)/tt_%.elf: $(PSP_BUILD_DIR)/tt_%.ld $$(call list_o_files_psp,servant/tt_$$*_psp)
+	$(call link,tt_$*,$@)
