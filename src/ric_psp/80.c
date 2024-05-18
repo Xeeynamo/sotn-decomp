3345c4a7c0eb27efbdc307a52af4ba825f7f49c8
sozud <122322823+sozud@users.noreply.github.com>
2024-05-18 15:19:33 -0700

    fixes?

--- src/ric_psp/80.c
+++ src/ric_psp/80.c
@@ -1,301 +1,314 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A6AF8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A6B80);
+// INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A6280);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A6C80);
+// INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A6290);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A7340);
+// INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A62D8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A73E8);
+// INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A6328);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A79F0);
+// INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A6610);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A7A38);
+// INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A6728);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A7B98);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A6AF8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A8AD8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A6B80);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A8D28);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A6C80);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A94A0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A7340);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A9560);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A73E8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A9670);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A79F0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A96D8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A7A38);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A9738);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A7B98);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A9840);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A8AD8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092A9EA0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A8D28);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AA248);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A94A0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AA388);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A9560);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AA678);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A9670);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AA9F8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A96D8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AAB48);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A9738);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AAD60);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A9840);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AB7A8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092A9EA0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AC2A8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AA248);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092ACE78);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AA388);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092ACF18);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AA678);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092ACFB0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AA9F8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AD108);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AAB48);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AD298);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AAD60);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AD730);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AB7A8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092ADEA0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AC2A8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AE150);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092ACE78);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AE368);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092ACF18);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AF450);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092ACFB0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AF4D0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AD108);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AF5A8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AD298);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AF600);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AD730);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AF680);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092ADEA0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AF738);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AE150);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AF898);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AE368);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AF9D0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AF450);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AFA90);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AF4D0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AFB68);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AF5A8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092AFDA8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AF600);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B00F0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AF680);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B0580);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AF738);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B05A8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AF898);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B0640);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AF9D0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B06F8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AFA90);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B07A8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AFB68);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B0A10);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092AFDA8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B0E38);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B00F0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B0E60);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B0580);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B0EA0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B05A8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B0F40);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B0640);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B1038);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B06F8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B1088);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B07A8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B10D0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B0A10);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B1178);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B0E38);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B1238);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B0E60);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B1270);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B0EA0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B1C60);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B0F40);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B1CD0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B1038);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B2590);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B1088);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B2DD8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B10D0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B3118);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B1178);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B3468);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B1238);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B34F0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B1270);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B3578);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B1C60);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B4030);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B1CD0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B4688);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B2590);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B4758);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B2DD8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B4DE0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B3118);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B5790);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B3468);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B59F0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B34F0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B5EF8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B3578);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B5FC8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B4030);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B6718);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B4688);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B6C58);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B4758);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B6DD8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B4DE0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B7530);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B5790);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B7FF8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B59F0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092B8978);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B5EF8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BA148);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B5FC8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BA218);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B6718);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BA338);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B6C58);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BA540);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B6DD8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BA5F8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B7530);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BAEE8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B7FF8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BB860);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092B8978);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BBD10);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BA148);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BDAC8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BA218);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BDD88);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BA338);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BE4C8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BA540);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BE570);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BA5F8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BE628);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BAEE8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BE6F0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BB860);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BE788);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BBD10);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BE888);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BDAC8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BEA38);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BDD88);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BEC98);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BE4C8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BF050);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BE570);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BF278);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BE628);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BF288);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BE6F0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BF628);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BE788);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BF718);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BE888);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BFDF0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BEA38);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092BFF48);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BEC98);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C00B8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BF050);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C01C8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BF278);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C02B8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BF288);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C09D8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BF628);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C1478);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BF718);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C1868);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BFDF0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C19B8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092BFF48);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C1EF0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C00B8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C2218);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C01C8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C25A0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C02B8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C2B90);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C09D8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C2BA0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C1478);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C2E98);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C1868);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C2F50);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C19B8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C4C00);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C1EF0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C4FF8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C2218);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C5DD8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C25A0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C69A8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C2B90);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C73B0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C2BA0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C77B8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C2E98);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C77D8);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C2F50);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C8228);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C4C00);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C8D48);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C4FF8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C8D90);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C5DD8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C8DA0);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C69A8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C9480);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C73B0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C9988);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C77B8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C9B88);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C77D8);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092C9C98);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C8228);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CA460);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C8D48);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CAD68);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C8D90);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CC700);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C8DA0);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CC930);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C9480);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CCA78);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C9988);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CCA88);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C9B88);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CCA98);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092C9C98);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CD120);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CA460);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CD528);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CAD68);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CDD08);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CC700);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CDF18);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CC930);
 
-INCLUDE_ASM("asm/pspeu/ric/nonmatchings/80", func_psp_092CE170);
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CCA78);
+
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CCA88);
+
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CCA98);
+
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CD120);
+
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CD528);
+
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CDD08);
+
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CDF18);
+
+INCLUDE_ASM("ric/nonmatchings/80", func_psp_092CE170);
