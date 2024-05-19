065a604f6f47f8b9578e80f3203b284a28735cc4
sozud <122322823+sozud@users.noreply.github.com>
2024-05-19 03:22:37 -0700

    PlaySfx syms (#1149)
    Taken from https://decomp.me/scratch/qLAhD
    InitSoundVars2: https://decomp.me/scratch/8sm4N
    InitSoundVars3: https://decomp.me/scratch/wDtJk
    ExecCdSoundCommands: https://decomp.me/scratch/ZUe2l

--- src/dra_psp/63E60.c
+++ src/dra_psp/63E60.c
@@ -1,5 +1,5 @@
 #include "../dra/dra.h"
 
-INCLUDE_ASM("dra/nonmatchings/63E60", func_psp_091407E0);
+INCLUDE_ASM("dra/nonmatchings/63E60", InitSoundVars3);
 
-INCLUDE_ASM("dra/nonmatchings/63E60", func_psp_091408E8);
+INCLUDE_ASM("dra/nonmatchings/63E60", InitSoundVars2);
