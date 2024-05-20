c1c36704be664e5adbaf576f59f45b058e0a09b6
sozud <122322823+sozud@users.noreply.github.com>
2024-05-20 12:12:14 -0700

    More Dra syms (#1152)
    IsRelicActive https://decomp.me/scratch/CaBA3
    func_800FE3C4 https://decomp.me/scratch/mZPXF

--- src/dra_psp/24810.c
+++ src/dra_psp/24810.c
@@ -8,9 +8,9 @@ INCLUDE_ASM("dra/nonmatchings/24810", func_psp_091012A8);
 
 INCLUDE_ASM("dra/nonmatchings/24810", func_800FE044);
 
-INCLUDE_ASM("dra/nonmatchings/24810", func_psp_09101AA8);
+INCLUDE_ASM("dra/nonmatchings/24810", IsRelicActive);
 
-INCLUDE_ASM("dra/nonmatchings/24810", func_psp_09101AF0);
+INCLUDE_ASM("dra/nonmatchings/24810", func_800FE3C4);
 
 INCLUDE_ASM("dra/nonmatchings/24810", GetEquipProperties);
 
