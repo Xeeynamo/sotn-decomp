6fc9172be08f41229aadad04921f8c4d6e56deeb
bismurphy <tjmurphy@mit.edu>
2024-05-22 13:51:15 -0400

    Decompile weapon 23 func_ptr_80170028 (#1160)
    Looks like just another weapon function.
    
    Weird uses of the Weapon struct. Unfortunately Weapons don't seem to
    have any consistency among them. Would be nice if we could define the
    Entity extension in each weapon's `w_0XX.c` file, but I don't think
    that's a possibility in C.

--- src/weapon/w_023.c
+++ src/weapon/w_023.c
@@ -21,8 +21,57 @@ INCLUDE_ASM("weapon/nonmatchings/w_023", EntityWeaponShieldSpell);
 
 INCLUDE_ASM("weapon/nonmatchings/w_023", func_ptr_80170024);
 
-INCLUDE_ASM("weapon/nonmatchings/w_023", func_ptr_80170028);
+extern AnimationFrame D_A5000_8017AB58[];
 
+void func_ptr_80170028(Entity* self) {
+
+    switch (self->step) {
+    case 0:
+        self->palette = self->ext.weapon.parent->palette;
+        self->unk5A = self->ext.weapon.parent->unk5A;
+        self->unk4C = D_A5000_8017AB58;
+        self->flags |= (FLAG_UNK_04000000 | FLAG_UNK_100000);
+        self->drawFlags = FLAG_DRAW_ROTX | FLAG_DRAW_ROTY;
+
+        self->rotX = self->rotY = 0;
+        self->posY.i.hi += 40;
+        self->velocityY = FIX(-1.5);
+        self->ext.weapon.unk7E = 0;
+        // Note: This is a weird use of the lifetime, it's not a lifetime! It's
+        // just caching the initial X position.
+        self->ext.weapon.lifetime = self->posX.i.hi;
+        self->animFrameIdx = rand() % 5;
+        self->animFrameDuration = 2;
+        self->ext.weapon.unk7E = 0x400;
+        self->ext.weapon.unk80 = (self->params & 0xFF) * 4;
+        self->step++;
+        return;
+    case 1:
+        // Actually, looks like it's using unk80 as the life timer.
+        if (--self->ext.weapon.unk80 == 0) {
+            self->animSet = self->ext.weapon.parent->animSet;
+            self->ext.weapon.unk80 = 0x40;
+            self->step++;
+        }
+        return;
+    case 2:
+        if (--self->ext.weapon.unk80 == 0) {
+            DestroyEntity(self);
+            return;
+        }
+        self->posY.val += self->velocityY;
+        self->ext.weapon.unk7E = (self->ext.weapon.unk7E + 0x80) & 0xFFF;
+        self->posX.i.hi = self->ext.weapon.lifetime +
+                          ((rcos(self->ext.weapon.unk7E) * 2) >> 8);
+        if (self->ext.weapon.unk7E < 0x800) {
+            self->zPriority = 0x1B4;
+        } else {
+            self->zPriority = 0x1BC;
+        }
+        self->rotX = self->rotY = -(rsin(self->ext.weapon.unk7E) >> 5) + 0xC0;
+        break;
+    }
+}
 void WeaponUnused2C(void) {}
 
 void WeaponUnused30(void) {}
