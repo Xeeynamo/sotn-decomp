8b89e64f063cb08cfed4e50f46e75ba32691e2c5
sozud <122322823+sozud@users.noreply.github.com>
2024-05-24 12:44:56 -0700

    syms

--- src/dra_psp/4370.c
+++ src/dra_psp/4370.c
@@ -6,7 +6,7 @@ INCLUDE_ASM("dra/nonmatchings/4370", func_800EDB58);
 
 INCLUDE_ASM("dra/nonmatchings/4370", AllocPrimitives);
 
-INCLUDE_ASM("dra/nonmatchings/4370", func_psp_090E0F00);
+INCLUDE_ASM("dra/nonmatchings/4370", func_800EDD9C);
 
 INCLUDE_ASM("dra/nonmatchings/4370", FreePrimitives);
 
