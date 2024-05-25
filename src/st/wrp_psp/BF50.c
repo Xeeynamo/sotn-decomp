14330845e39dc0012d33e7f6f404083e34ac6fb8
sozud <122322823+sozud@users.noreply.github.com>
2024-05-25 14:13:23 -0700

    more

--- src/st/wrp_psp/BF50.c
+++ src/st/wrp_psp/BF50.c
@@ -7,7 +7,10 @@ INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", AnimateEntity);
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", GetSideToPlayer);
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", MoveEntity);
+void MoveEntity() {
+    g_CurrentEntity->posX.val += g_CurrentEntity->velocityX;
+    g_CurrentEntity->posY.val += g_CurrentEntity->velocityY;
+}
 
 void FallEntity(void) {
     if (g_CurrentEntity->velocityY < FALL_TERMINAL_VELOCITY) {
