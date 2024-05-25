ef9f42b421f31c283836fb28c28075d5c21d35ab
sozud <122322823+sozud@users.noreply.github.com>
2024-05-24 23:54:17 -0700

    syms

--- src/dra_psp/24810.c
+++ src/dra_psp/24810.c
@@ -4,7 +4,8 @@ INCLUDE_ASM("dra/nonmatchings/24810", func_psp_09101190);
 
 INCLUDE_ASM("dra/nonmatchings/24810", func_psp_09101288);
 
-INCLUDE_ASM("dra/nonmatchings/24810", func_psp_091012A8);
+// https://decomp.me/scratch/cwWp4
+INCLUDE_ASM("dra/nonmatchings/24810", CheckAndDoLevelUp);
 
 INCLUDE_ASM("dra/nonmatchings/24810", func_800FE044);
 
@@ -14,6 +15,8 @@ INCLUDE_ASM("dra/nonmatchings/24810", func_800FE3C4);
 
 INCLUDE_ASM("dra/nonmatchings/24810", GetEquipProperties);
 
-INCLUDE_ASM("dra/nonmatchings/24810", func_psp_09101F60);
+// https://decomp.me/scratch/qrddz
+INCLUDE_ASM("dra/nonmatchings/24810", HasEnoughMp);
 
-INCLUDE_ASM("dra/nonmatchings/24810", func_psp_09101FC8);
+// https://decomp.me/scratch/BqUrb
+INCLUDE_ASM("dra/nonmatchings/24810", func_800FE8F0);
