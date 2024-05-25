fc8fb9eedadb8d21fcad444d1f6042ec6542edb8
sozud <122322823+sozud@users.noreply.github.com>
2024-05-25 14:08:06 -0700

    more

--- src/st/wrp_psp/C3F0.c
+++ src/st/wrp_psp/C3F0.c
@@ -1,8 +1,14 @@
 #include "common.h"
+#include "game.h"
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/C3F0", func_8018C1E0);
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/C3F0", SetStep);
+void SetStep(u8 step) {
+    g_CurrentEntity->step = step;
+    g_CurrentEntity->step_s = 0;
+    g_CurrentEntity->animFrameIdx = 0;
+    g_CurrentEntity->animFrameDuration = 0;
+}
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/C3F0", InitializeEntity);
 
