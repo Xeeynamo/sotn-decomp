461d3338d957af45a49d5953384468abfacae6ff
sozud <122322823+sozud@users.noreply.github.com>
2024-05-26 05:26:51 -0700

    FallEntity (#1174)
    I get
    ```
    mipsel-linux-gnu-ld: build/pspeu/src/st/wrp_psp/BF50.c.o: in function `FallEntity':
    (.text+0x0): undefined reference to `g_CurrentEntity'
    mipsel-linux-gnu-ld: (.text+0x4): undefined reference to `g_CurrentEntity'
    mipsel-linux-gnu-ld: (.text+0x1c): undefined reference to `g_CurrentEntity'
    mipsel-linux-gnu-ld: (.text+0x20): undefined reference to `g_CurrentEntity'
    ```
    Not sure what the issue is

--- src/st/wrp_psp/BF50.c
+++ src/st/wrp_psp/BF50.c
@@ -1,17 +1,52 @@
 #include "common.h"
+#include "game.h"
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", PreventEntityFromRespawning);
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", AnimateEntity);
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", GetSideToPlayer);
+u8 GetSideToPlayer() {
+    u8 side = 0;
+    Entity* player = &PLAYER;
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", MoveEntity);
+    if (g_CurrentEntity->posX.i.hi > player->posX.i.hi) {
+        side |= 1;
+    }
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", FallEntity);
+    if (g_CurrentEntity->posY.i.hi > player->posY.i.hi) {
+        side |= 2;
+    }
+    return side;
+}
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", AllocEntity);
+void MoveEntity() {
+    g_CurrentEntity->posX.val += g_CurrentEntity->velocityX;
+    g_CurrentEntity->posY.val += g_CurrentEntity->velocityY;
+}
+
+void FallEntity(void) {
+    if (g_CurrentEntity->velocityY < FALL_TERMINAL_VELOCITY) {
+        g_CurrentEntity->velocityY += FALL_GRAVITY;
+    }
+}
+
+Entity* AllocEntity(Entity* start, Entity* end) {
+    Entity* current = start;
+
+    while (current < end) {
+        if (!current->entityId) {
+            DestroyEntity(current);
+            return current;
+        }
+        current++;
+    }
+    return NULL;
+}
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", UnkEntityFunc0);
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BF50", func_8018C160);
+u16 func_8018C160(Entity* a, Entity* b) {
+    s32 diffX = b->posX.i.hi - a->posX.i.hi;
+    s32 diffY = b->posY.i.hi - a->posY.i.hi;
+    return ratan2(diffY, diffX);
+}
