b42fe552f21e321f17cc860efbe479bbe3b071f8
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-05-26 17:16:31 +0100

    EntityRoomForeground matching

--- src/st/wrp_psp/BDA0.c
+++ src/st/wrp_psp/BDA0.c
@@ -1,6 +1,27 @@
 #include "../wrp/wrp.h"
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/BDA0", EntityRoomForeground);
+extern ObjInit2 D_80181420[];
+void EntityRoomForeground(Entity* entity) {
+    ObjInit2* objInit = &D_80181420[entity->params];
+
+    if (!entity->step) {
+        InitializeEntity(g_eInitGeneric2);
+        entity->animSet = objInit->animSet;
+        entity->zPriority = objInit->zPriority;
+        entity->unk5A = objInit->unk4.u;
+        entity->palette = objInit->palette;
+        entity->drawFlags = objInit->drawFlags;
+        entity->drawMode = objInit->drawMode;
+        if (objInit->unkC != 0) {
+            entity->flags = objInit->unkC;
+        }
+        if (entity->params > 4) {
+            entity->drawFlags |= DRAW_COLORS;
+            entity->rotZ = 0x800;
+        }
+    }
+    AnimateEntity(objInit->unk10, entity);
+}
 
 #include "../../destroy_entity.h"
 
