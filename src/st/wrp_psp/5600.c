c0222b4a9222289fa3df59397a49c9634b21e06e
sozud <122322823+sozud@users.noreply.github.com>
2024-05-26 12:44:22 -0700

    CollectHeartVessel (#1179)

--- src/st/wrp_psp/5600.c
+++ src/st/wrp_psp/5600.c
@@ -6,7 +6,20 @@ INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/5600", CollectGold);
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/5600", CollectSubweapon);
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/5600", CollectHeartVessel);
+void CollectHeartVessel(void) {
+    if (g_PlayableCharacter != PLAYER_ALUCARD) {
+        g_api.PlaySfx(NA_SE_PL_COLLECT_HEART);
+        g_Status.hearts += HEART_VESSEL_RICHTER;
+
+        if (g_Status.hearts > g_Status.heartsMax) {
+            g_Status.hearts = g_Status.heartsMax;
+        }
+    } else {
+        g_api.PlaySfx(NA_SE_PL_COLLECT_HEART);
+        g_api.func_800FE044(HEART_VESSEL_INCREASE, 0x4000);
+    }
+    DestroyEntity(g_CurrentEntity);
+}
 
 void CollectLifeVessel(void) {
     g_api.PlaySfx(NA_SE_PL_COLLECT_HEART);
