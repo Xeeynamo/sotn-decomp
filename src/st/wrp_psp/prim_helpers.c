c27205afc121e19a5203925f6e68cefe3dfa89f8
sozud <122322823+sozud@users.noreply.github.com>
2024-05-26 14:05:17 -0700

    PrimResetNext

--- src/st/wrp_psp/prim_helpers.c
+++ src/st/wrp_psp/prim_helpers.c
@@ -1,6 +1,28 @@
 #include "../wrp/wrp.h"
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/prim_helpers", PrimResetNext);
+void PrimResetNext(Primitive* prim) {
+    prim->p1 = 0;
+    prim->p2 = 0;
+    prim->p3 = 0;
+    prim->next->x1 = 0;
+    prim->next->y1 = 0;
+    prim->next->y0 = 0;
+    prim->next->x0 = 0;
+    prim->next->clut = 0;
+    LOHU(prim->next->u0) = 0;
+    LOHU(prim->next->b1) = 0;
+    LOHU(prim->next->r1) = 0;
+    LOHU(prim->next->u1) = 0;
+    prim->next->tpage = 0;
+    LOHU(prim->next->r2) = 0;
+    LOHU(prim->next->b2) = 0;
+    prim->next->u2 = 0;
+    prim->next->v2 = 0;
+    prim->next->r3 = 0;
+    prim->next->b3 = 0;
+    prim->next->x2 = 0;
+    prim->next->y2 = 0;
+}
 
 void UnkPolyFunc2(Primitive* prim) {
     PrimResetNext(prim);
