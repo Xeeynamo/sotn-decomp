df961d35d419adff0066522e26acbf4fb0bd03dd
Joey Murphy <tjmurphy@mit.edu>
2024-05-27 16:02:55 -0400

    Decompile EntityEquipItemDrop ST0 and MAD

--- src/st/st0/st0.h
+++ src/st/st0/st0.h
@@ -154,6 +154,8 @@ extern s32 D_801C2578;
 // *** EntityDracula properties END ***
 
 extern s16 D_80181990[];
+extern u16 g_InitializeData0[];
+extern u16 D_80181D74[];
 extern u32 D_80181D7C[];
 extern u16 D_80181DA4[];
 extern u8* D_80181E28[];
