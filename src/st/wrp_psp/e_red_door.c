3d06a33e15741edd5fd47df69508ae61be35cbde
sozud <122322823+sozud@users.noreply.github.com>
2024-05-26 21:58:22 -0700

    more

--- src/st/wrp_psp/e_red_door.c
+++ src/st/wrp_psp/e_red_door.c
@@ -1,6 +1,27 @@
 #include "../wrp/wrp.h"
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_red_door", EntityIsNearPlayer);
+bool EntityIsNearPlayer(Entity* self) {
+    s16 posX;
+    s16 diffX;
+    s16 posY;
+    s16 diffY;
+
+    diffX = PLAYER.posX.i.hi - self->posX.i.hi;
+    posX = abs(diffX);
+
+    if (posX > 16) {
+        return 0;
+    }
+
+    diffY = PLAYER.posY.i.hi - self->posY.i.hi;
+    posY = abs(diffY);
+
+    if (posY > 32) {
+        return 0;
+    }
+
+    return 1;
+}
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_red_door", EntityRedDoor);
 
