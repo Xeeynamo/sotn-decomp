8f9e7bf878f8b4cca260e1c1af281f4185844b8f
sozud <122322823+sozud@users.noreply.github.com>
2024-05-26 20:13:42 -0700

    wrp funcs

--- src/st/wrp_psp/e_misc.c
+++ src/st/wrp_psp/e_misc.c
@@ -14,4 +14,32 @@ INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_misc", EntityUnkId15);
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_misc", EntityIntenseExplosion);
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_misc", func_801916C4);
+void func_801916C4(s16 sfxId) {
+    s32 posX;
+    s32 posY;
+    s16 arg2;
+    s16 arg1;
+
+    posX = g_CurrentEntity->posX.i.hi - 128;
+    arg2 = (abs(posX) - 32) >> 5;
+    if (arg2 > 8) {
+        arg2 = 8;
+    } else if (arg2 < 0) {
+        arg2 = 0;
+    }
+    if (posX < 0) {
+        arg2 = -arg2;
+    }
+    arg1 = abs(posX) - 96;
+    posY = abs(g_CurrentEntity->posY.i.hi - 128) - 112;
+    if (posY > 0) {
+        arg1 += posY;
+    }
+    if (arg1 < 0) {
+        arg1 = 0;
+    }
+    arg1 = 127 - (arg1 >> 1);
+    if (arg1 > 0) {
+        g_api.func_80134714(sfxId, arg1, arg2);
+    }
+}
--- src/st/wrp_psp/st_update.c
+++ src/st/wrp_psp/st_update.c
@@ -2,4 +2,35 @@
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/st_update", Update);
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/st_update", UpdateStageEntities);
+// INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/st_update", UpdateStageEntities);
+
+extern u16 UNK_Invincibility0[];
+
+void UpdateStageEntities(void) {
+    Entity* entity;
+    s16 temp;
+    for (entity = &g_Entities[STAGE_ENTITY_START];
+         entity < &g_Entities[TOTAL_ENTITY_COUNT]; entity++) {
+        if (!entity->pfnUpdate)
+            continue;
+
+        if (entity->step) {
+            if (!(entity->flags & FLAG_UNK_10000))
+                continue;
+            if (entity->flags & 0xF) {
+                temp = entity->nFramesInvincibility << 1;
+                temp += entity->flags & 1;
+                entity->palette = UNK_Invincibility0[temp];
+                if ((--entity->flags & 0xF) == 0) {
+                    entity->palette = entity->hitEffect;
+                    entity->hitEffect = 0;
+                }
+            }
+        }
+
+        g_CurrentEntity = entity;
+        entity->pfnUpdate(entity);
+        entity->unk44 = 0;
+        entity->hitFlags = 0;
+    }
+}
