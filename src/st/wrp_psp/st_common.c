cd90d5e61443844c6374acedcbd800b8bbc764c6
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-05-27 16:34:02 +0100

    func_8018C55C matching

--- src/st/wrp_psp/st_common.c
+++ src/st/wrp_psp/st_common.c
@@ -99,6 +99,39 @@ void EntityDummy(Entity* arg0) {
     }
 }
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/st_common", func_8018C55C);
+void func_8018C55C(s16* hitSensors, s16 sensorCount) {
+    Collider collider;
+    s32 velocityX;
+    s16 i;
+    s16 x;
+    s16 y;
+
+    velocityX = g_CurrentEntity->velocityX;
+    if (velocityX == 0) {
+        return;
+    }
+
+    x = g_CurrentEntity->posX.i.hi;
+    y = g_CurrentEntity->posY.i.hi;
+    for (i = 0; i < sensorCount; i++) {
+        if (velocityX < 0) {
+            x += *hitSensors++;
+        } else {
+            x -= *hitSensors++;
+        }
+
+        y += *hitSensors++;
+        g_api.CheckCollision(x, y, &collider, 0);
+        if (collider.effects & EFFECT_UNK_0002 &&
+            (!(collider.effects & EFFECT_UNK_8000) || i)) {
+            if (velocityX < 0) {
+                g_CurrentEntity->posX.i.hi += collider.unk1C;
+            } else {
+                g_CurrentEntity->posX.i.hi += collider.unk14;
+            }
+            break;
+        }
+    }
+}
 
 #include "../replace_breakable_with_item_drop.h"
