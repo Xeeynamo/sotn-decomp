e469ad36ce3ad13d46caee4398fe3ae3fd7a0be9
sozud <122322823+sozud@users.noreply.github.com>
2024-05-27 17:06:25 -0700

    func_8018CB34

--- src/st/wrp_psp/e_collect.c
+++ src/st/wrp_psp/e_collect.c
@@ -18,7 +18,44 @@ void func_8018CAB0(void) {
     }
 }
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_collect", func_8018CB34);
+void func_8018CB34(u16 arg0) {
+    Collider collider;
+
+    if (g_CurrentEntity->velocityX < 0) {
+        g_api.CheckCollision(g_CurrentEntity->posX.i.hi,
+                             g_CurrentEntity->posY.i.hi - 7, &collider, 0);
+        if (collider.effects & EFFECT_NOTHROUGH) {
+            g_CurrentEntity->velocityY = 0;
+        }
+    }
+
+    g_api.CheckCollision(g_CurrentEntity->posX.i.hi,
+                         g_CurrentEntity->posY.i.hi + 7, &collider, 0);
+
+    if (arg0) {
+        if (!(collider.effects & EFFECT_NOTHROUGH)) {
+            MoveEntity();
+            FallEntity();
+            return;
+        }
+
+        g_CurrentEntity->velocityX = 0;
+        g_CurrentEntity->velocityY = 0;
+
+        if (collider.effects & EFFECT_QUICKSAND) {
+            g_CurrentEntity->posY.val += FIX(0.125);
+            return;
+        }
+
+        g_CurrentEntity->posY.i.hi += collider.unk18;
+        return;
+    }
+
+    if (!(collider.effects & EFFECT_NOTHROUGH)) {
+        MoveEntity();
+        func_8018CAB0();
+    }
+}
 
 extern s8 c_HeartPrizes[];
 void CollectHeart(u16 arg0) {
