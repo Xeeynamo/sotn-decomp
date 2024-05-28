93fb643b6aec60cd8817501dc5bc33ca6225a3d5
sozud <122322823+sozud@users.noreply.github.com>
2024-05-28 00:07:43 -0700

    more

--- src/st/wrp_psp/e_collect.c
+++ src/st/wrp_psp/e_collect.c
@@ -202,6 +202,26 @@ void EntityExplosion(Entity* entity) {
     }
 }
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_collect", BlinkItem);
+void BlinkItem(Entity* self, u16 renderFlags) {
+    Primitive* prim;
+    s32 temp;
+    prim = &g_PrimBuf[self->primIndex];
+
+    prim->x0 = prim->x2 = self->posX.i.hi - 7;
+    prim->x1 = prim->x3 = prim->x0 + 14;
+
+    prim->y0 = prim->y1 = self->posY.i.hi - 7;
+    prim->y2 = prim->y3 = prim->y0 + 14;
+
+    if (renderFlags & RENDERFLAGS_NOSHADOW) {
+        prim->r0 = prim->r1 = prim->r2 = prim->r3 = prim->g0 = prim->g1 =
+            prim->g2 = prim->g3 = prim->b0 = prim->b1 = prim->b2 = prim->b3 =
+                255;
+    } else {
+        prim->r0 = prim->r1 = prim->r2 = prim->r3 = prim->g0 = prim->g1 =
+            prim->g2 = prim->g3 = prim->b0 = prim->b1 = prim->b2 = prim->b3 =
+                128;
+    }
+}
 
 INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_collect", EntityEquipItemDrop);
