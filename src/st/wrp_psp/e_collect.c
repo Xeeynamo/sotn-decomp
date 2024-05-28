30b5318b1b6ebcdb46572877ef29d02ac9a7615f
sozud <122322823+sozud@users.noreply.github.com>
2024-05-27 18:46:31 -0700

    CollectSubweapon

--- src/st/wrp_psp/e_collect.c
+++ src/st/wrp_psp/e_collect.c
@@ -91,7 +91,63 @@ void CollectGold(u16 goldSize) {
     DestroyEntity(g_CurrentEntity);
 }
 
-INCLUDE_ASM("st/wrp_psp/psp/wrp_psp/e_collect", CollectSubweapon);
+extern u16 D_80180DC4[];
+extern u16 D_80180DF4[];
+extern s32 D_091CF020;
+extern u32 D_091CF6DC;
+extern u32 D_091CF698;
+extern u16 D_psp_09246370[];
+extern u16 D_psp_092462E0[];
+extern u16 D_psp_09246388[];
+extern u16 D_psp_092462F8[];
+void CollectSubweapon(u16 subWeaponIdx) {
+    Entity* player = &PLAYER;
+    u16 subWeapon;
+
+    g_api.PlaySfx(NA_SE_PL_IT_PICKUP);
+
+    if (D_091CF020 == 2) {
+        subWeapon = D_091CF6DC;
+    } else {
+        subWeapon = D_091CF698;
+    }
+
+    if (D_091CF020 == 2) {
+        D_091CF6DC = D_psp_09246370[subWeaponIdx - 0xe];
+        func_90E4C90();
+    } else {
+        D_091CF698 = D_psp_092462E0[subWeaponIdx - 0xe];
+    }
+
+    if (D_091CF020 != 2 && subWeapon == D_091CF698 ||
+        D_091CF020 == 2 && subWeapon == D_091CF6DC) {
+        subWeapon = 1;
+        g_CurrentEntity->unk6D[0] = 0x10;
+    } else {
+        if (D_091CF020 == 2) {
+            subWeapon = D_psp_09246388[subWeapon];
+        } else {
+            subWeapon = D_psp_092462F8[subWeapon];
+        }
+        g_CurrentEntity->unk6D[0] = 0x60;
+    }
+
+    if (subWeapon) {
+        g_CurrentEntity->params = subWeapon;
+        g_CurrentEntity->posY.i.hi = player->posY.i.hi + 12;
+        SetStep(7);
+        g_CurrentEntity->velocityY = FIX(-2.5);
+        g_CurrentEntity->animCurFrame = 0;
+        g_CurrentEntity->ext.generic.unk88.S16.unk2 = 5;
+        if (player->facingLeft ^ 1) {
+            g_CurrentEntity->velocityX = FIX(-2);
+            return;
+        }
+        g_CurrentEntity->velocityX = FIX(2);
+        return;
+    }
+    DestroyEntity(g_CurrentEntity);
+}
 
 void CollectHeartVessel(void) {
     if (g_PlayableCharacter != PLAYER_ALUCARD) {
