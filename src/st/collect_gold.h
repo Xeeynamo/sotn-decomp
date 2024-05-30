8bb4c3d63fb7e1d2e4895c3363949c49a5da1dd3
Joey Murphy <tjmurphy@mit.edu>
2024-05-30 12:25:37 -0400

    Dedupe PSP; remove temp variable that breaks PSP

--- src/st/collect_gold.h
+++ src/st/collect_gold.h
@@ -2,14 +2,13 @@ extern u32 c_GoldPrizes[];
 extern u8* g_goldCollectTexts[];
 
 void CollectGold(u16 goldSize) {
-    u16 goldSizeIndex;
 #ifdef VERSION_BETA
     g_api.PlaySfx(0x69D); // MAD seems to use its own sfx id set.
 #else
     g_api.PlaySfx(NA_SE_PL_COLLECT_GOLD);
 #endif
-    goldSizeIndex = goldSize - 2;
-    g_Status.gold += c_GoldPrizes[goldSizeIndex];
+    goldSize -= 2;
+    g_Status.gold += c_GoldPrizes[goldSize];
     if (g_Status.gold > MAX_GOLD) {
         g_Status.gold = MAX_GOLD;
     }
@@ -19,7 +18,7 @@ void CollectGold(u16 goldSize) {
         g_unkGraphicsStruct.BottomCornerTextTimer = 0;
     }
 
-    BottomCornerText(g_goldCollectTexts[goldSizeIndex], 1);
+    BottomCornerText(g_goldCollectTexts[goldSize], 1);
     DestroyEntity(g_CurrentEntity);
 #endif
 }
