cec8424a09c404074e237adceca4ec5c7d5aa61d
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2024-05-31 00:18:36 +0100

    WRP PSP merge e_room_fg.c (#1221)
    Straightforward PR. Interestingly importing the data doesn't seem to
    result to a match ad there's some `0x10` aligned padding added. Either
    we know too little about importing data with MWCC or the file split was
    wrong and the data belonged together with other functions in another C
    file.
    
    For now I had to use a `#ifdef`. It will probably get forgotten until we
    decide to import the data from WRP PSP.

--- src/st/wrp/e_room_fg.c
+++ src/st/wrp/e_room_fg.c
@@ -1,5 +1,6 @@
 #include "wrp.h"
 
+#if !defined(VERSION_PSP)
 u8 D_80181120[] = {0x40, 0x01, 0xFF, 0x00};
 u8 D_80181124[] = {0x40, 0x02, 0xFF, 0x00};
 u8 D_80181128[] = {0x40, 0x02, 0xFF, 0x00};
@@ -13,22 +14,29 @@ ObjInit2 D_80181134[] = {
     {0x000C, 0x01EC, 0x0000, 0x0000, 0x00, 0x10, 0, D_80181130},
     {0x000C, 0x0080, 0x0000, 0x0000, 0x00, 0x10, 0, D_8018112C},
 };
+
+#else
+extern ObjInit2 D_80181134[];
+
+#endif
+
 void EntityRoomForeground(Entity* entity) {
     ObjInit2* objInit = &D_80181134[entity->params];
-    if (entity->step == 0) {
+
+    if (!entity->step) {
         InitializeEntity(g_eInitGeneric2);
         entity->animSet = objInit->animSet;
         entity->zPriority = objInit->zPriority;
-        entity->unk5A = objInit->unk4.s;
+        entity->unk5A = objInit->unk4.u;
         entity->palette = objInit->palette;
         entity->drawFlags = objInit->drawFlags;
         entity->drawMode = objInit->drawMode;
         if (objInit->unkC != 0) {
             entity->flags = objInit->unkC;
         }
-        if (entity->params >= 5) {
+        if (entity->params > 4) {
+            entity->drawFlags |= DRAW_COLORS;
             entity->rotZ = 0x800;
-            entity->drawFlags |= 4;
         }
     }
     AnimateEntity(objInit->unk10, entity);
