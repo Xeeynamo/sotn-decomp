8947665c9d930dab69b830af14f9791247a31288
bismurphy <tjmurphy@mit.edu>
2024-06-01 11:23:14 -0400

    Decompile Weapon 28 shield spell (#1225)
    This one was a beast, but nice to get it working.
    
    This seemed to match the same entity extension as weapon 29. Weapons 28
    and 29 are both shields, so I decided to rename the Weapon29 struct to
    be Shield instead. We will see if it continues to match for the future
    shields.
    
    This had a ton of FIX(), enums, #defines, etc, so hopefully I got them
    all :)

--- include/entity.h
+++ include/entity.h
@@ -211,15 +211,19 @@ typedef struct {
     /* 0x7E */ s16 unk7E;
     /* 0x80 */ s16 unk80;
     /* 0x82 */ s16 unk82;
-    /* 0x84 */ s32 unk84;
+    /* 0x84 */ s16 unk84;
+    /* 0x86 */ s16 unk86;
     /* 0x88 */ s16 childPalette;
     /* 0x8A */ s16 unk8A;
     /* 0x8C */ struct Entity* parent;
-    /* 0x90 */ s32 unk90;
-    /* 0x94 */ s32 unk94;
-    /* 0x98 */ s32 unk98;
+    /* 0x90 */ s16 unk90;
+    /* 0x92 */ s16 unk92;
+    /* 0x94 */ s16 unk94;
+    /* 0x96 */ s16 unk96;
+    /* 0x98 */ s16 unk98;
+    /* 0x9A */ s16 unk9A;
     /* 0x9C */ s16 unk9C;
-} ET_Weapon29;
+} ET_Shield;
 
 typedef struct {
     s16 timer;
@@ -993,7 +997,7 @@ typedef union { // offset=0x7C
     ET_StagePopup stpopup;
     ET_MessageBox messageBox;
     ET_Weapon weapon;
-    ET_Weapon29 weapon29;
+    ET_Shield shield;
     ET_Food food;
     ET_HitByIce hitbyice;
     ET_HitByLightning hitbylightning;
