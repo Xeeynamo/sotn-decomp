3f24a09be0e3b1475bf12c6432583cb6538f3a79
sozud <122322823+sozud@users.noreply.github.com>
2024-06-02 13:48:16 -0700

    Start importing wrp data

--- src/st/wrp_psp/create_entity.c
+++ src/st/wrp_psp/create_entity.c
@@ -1,4 +1,41 @@
 #include "../wrp/wrp.h"
+
+u8 g_eBreakableHitboxes[] = {
+    /* 5D8 */ 8,
+    /* 5D9 */ 8,
+    /* 5DA */ 0,
+    /* 5DB */ 0,
+    /* 5DC */ 0,
+    /* 5DD */ 0,
+    /* 5DE */ 0,
+    /* 5DF */ 0,
+};
+
+// g_eBreakableExplosionTypes moved
+
+u16 g_eBreakableanimSets[] = {
+    /* 5E8 */ 3,
+    /* 5EA */ 3,
+    /* 5EC */ 0,
+    /* 5EE */ 0,
+    /* 5F0 */ 0,
+    /* 5F2 */ 0,
+    /* 5F4 */ 0,
+    /* 5F6 */ 0,
+};
+
+// shorter than psx version
+u8 g_eBreakableDrawModes[] = {
+    /* 5F8 */ 0x70,
+    /* 5F9 */ 0x30,
+    /* 5FA */ 0x00,
+    /* 5FB */ 0x00,
+    /* 5FC */ 0x00,
+    /* 5FD */ 0x00,
+    /* 5FE */ 0x00,
+    /* 5FF */ 0x00
+};
+
 #include "../st_private.h"
 
 #include "../create_entity_from_layout.h"
