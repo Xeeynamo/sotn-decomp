e62f6c3a649dd1e6be2bd39825fff8b38af1d786
bismurphy <tjmurphy@mit.edu>
2024-06-04 13:30:04 -0400

    Decompile weapon 26 func_ptr_80170024 (#1256)
    Kind of a crazy one with the way the vectors are dealt with, but
    eventually got it to work. Hopefully this can be a nice reference for
    some of those SDK functions.

--- include/entity.h
+++ include/entity.h
@@ -252,6 +252,26 @@ typedef struct {
 } ET_MedusaShieldLaser;
 
 typedef struct {
+    /* 0x7C */ s16 unk7C;
+    /* 0x7E */ s16 unk7E;
+    /* 0x80 */ s16 unk80;
+    /* 0x82 */ s16 unk82;
+    /* 0x84 */ s16 unk84;
+    /* 0x86 */ s16 unk86;
+    /* 0x88 */ s16 childPalette;
+    /* 0x8A */ s16 unk8A;
+    /* 0x8C */ struct Entity* parent;
+    /* 0x90 */ s16 unk90;
+    /* 0x92 */ s16 unk92;
+    /* 0x94 */ s16 unk94;
+    /* 0x96 */ s16 unk96;
+    /* 0x98 */ s16 unk98;
+    /* 0x9A */ s16 unk9A;
+    /* 0x9C */ s16 unk9C;
+    /* 0x9E */ s16 unk9E;
+} ET_ShamanShieldStar;
+
+typedef struct {
     s16 timer;
     s16 unk7E;
     s32 unk80;
@@ -1021,6 +1041,7 @@ typedef union { // offset=0x7C
     ET_Weapon weapon;
     ET_Shield shield;
     ET_MedusaShieldLaser medshieldlaser;
+    ET_ShamanShieldStar shamanshieldstar;
     ET_Food food;
     ET_HitByIce hitbyice;
     ET_HitByLightning hitbylightning;
