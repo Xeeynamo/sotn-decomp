5b3699baedc0de00cff2867692fecad60abc399b
Joey Murphy <tjmurphy@mit.edu>
2024-06-05 13:01:02 -0400

    Decompile DRE func_801A2848

--- src/st/dre/22848.c
+++ src/st/dre/22848.c
@@ -9,8 +9,61 @@ long RotTransPers3(SVECTOR* v0, SVECTOR* v1, SVECTOR* v2, long* sxy0,
                    long* sxy1, long* sxy2, long* p, long* flag);
 long NormalClip(long sxy0, long sxy1, long sxy2);
 
-Primitive* func_801A2848(Primitive* prim, u8* model, u16* arg2);
-INCLUDE_ASM("st/dre/nonmatchings/22848", func_801A2848);
+Primitive* func_801A2848(Primitive* prim, u8* arg1, u16* arg2) {
+    s32 pad0;
+    s32 pad1;
+    s32 pad2;
+    s32 pad3;
+    s32 pad4;
+
+    pad0 = *SPAD(arg1[0]);
+    pad1 = *SPAD(arg1[1]);
+    pad2 = *SPAD(arg1[2]);
+    if (NormalClip(pad0, pad1, pad2) <= 0) {
+        return prim;
+    }
+    pad3 = *SPAD(arg1[3]);
+    pad4 = *SPAD(arg1[4]);
+
+    prim->tpage = 0xF;
+    prim->clut = arg2[0];
+    prim->u0 = prim->u2 = 4;
+    prim->u1 = prim->u3 = 0x7C;
+    prim->v0 = prim->v1 = 3;
+    prim->v2 = prim->v3 = 0x9E;
+    LOW(prim->x0) = pad0;
+    LOW(prim->x1) = pad1;
+    LOW(prim->x2) = pad2;
+    LOW(prim->x3) = pad3;
+    prim->drawMode = DRAW_UNK02 | DRAW_COLORS;
+    prim->r0 = prim->g0 = prim->b0 = arg2[1];
+    LOW(prim->r1) = LOW(prim->r0);
+    LOW(prim->r2) = LOW(prim->r0);
+    LOW(prim->r3) = LOW(prim->r0);
+    prim = prim->next;
+
+    prim->tpage = 0xF;
+    prim->clut = arg2[0];
+    // Some of these values are shared but don't seem to match if combined
+    prim->u0 = 0xFE;
+    prim->u2 = prim->u1 = 0xC2;
+    prim->u3 = 0xFE;
+    prim->v0 = 0xAC;
+    prim->v1 = 0x6C;
+    prim->v3 = prim->v2 = 0xAC;
+    LOW(prim->x0) = pad0;
+    LOW(prim->x1) = pad4;
+    LOW(prim->x3) = pad1;
+    prim->x2 = (prim->x0 + prim->x3) / 2;
+    prim->y2 = (prim->y0 + prim->y3) / 2;
+
+    prim->drawMode = DRAW_UNK02 | DRAW_COLORS;
+    prim->r0 = prim->g0 = prim->b0 = arg2[1];
+    LOW(prim->r1) = LOW(prim->r0);
+    LOW(prim->r2) = LOW(prim->r0);
+    LOW(prim->r3) = LOW(prim->r0);
+    return prim->next;
+}
 
 Primitive* func_801A2A58(Primitive* prim, u8* model, u16* arg2);
 INCLUDE_ASM("st/dre/nonmatchings/22848", func_801A2A58);
