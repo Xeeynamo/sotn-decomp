12c796fbd08f88e41741675bfb95482dffed765b
bismurphy <tjmurphy@mit.edu>
2024-06-05 12:32:06 -0400

    Decompile DRE Entity3DHouseSpawner (#1260)
    This was an old one with a decompme comment in the code that I decided
    to tackle. Turned out to be able to make it relatively straightforward!
    
    I think the naming is reasonable and is motivated by the behavior in the
    code, but happy to adjust if needed.
    
    Strangely, decompme informed me that the function was matched here:
    https://decomp.me/scratch/aEXJO by "NicoVazCan", 3 months ago. Not sure
    who that is, but they never submitted it, so I guess it's abandoned.
    Either way, I think the code in this PR is more readable.

--- src/st/dre/22848.c
+++ src/st/dre/22848.c
@@ -45,8 +45,27 @@ Primitive* func_801A2C9C(Primitive* prim, u8* model, u16* arg2) {
     return prim;
 }
 
-// DECOMP_ME_WIP EntityUnkId17 https://decomp.me/scratch/nNfXk 95.28%
-INCLUDE_ASM("st/dre/nonmatchings/22848", EntityUnkId17);
+// Spawns the set of 3D background houses.
+void Entity3DHouseSpawner(Entity* self) {
+    s16* dataPtr;
+    Entity* newEntity;
+
+    if (self->step == 0) {
+        dataPtr = D_801815E8;
+        // It appears that the data here will hit an FFFF after 5 cycles.
+        while (*dataPtr != -1) {
+            newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
+            if (newEntity == NULL) {
+                break;
+            }
+            CreateEntityFromCurrentEntity(E_ID_BACKGROUND_HOUSE, newEntity);
+            newEntity->posX.i.hi = *dataPtr++ - g_Tilemap.scrollX.i.hi;
+            newEntity->posY.i.hi = *dataPtr++ - g_Tilemap.scrollY.i.hi;
+            newEntity->params = *dataPtr++;
+        }
+        self->step++;
+    }
+}
 
 // 3D house object in background ID 0x16
 INCLUDE_ASM("st/dre/nonmatchings/22848", Entity3DBackgroundHouse);
