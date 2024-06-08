d8cd4816c1a0c026a297c49a00be907614e72f5a
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-06-08 20:05:09 +0100

    Simplify collision.c based on feedback

--- src/st/no3/collision.c
+++ src/st/no3/collision.c
@@ -1,3 +1,1 @@
-#include <common.h>
-
 #include "../collision.h"
--- src/st/rwrp/collision.c
+++ src/st/rwrp/collision.c
@@ -1,12 +1,1 @@
-#include <common.h>
-
-extern u16 g_testCollEnemyLookup[];
-extern u8 g_testCollLuckCutoff[];
-extern u8 g_testColluCoords[];
-extern u8 g_testCollvCoords[];
-extern u16 g_testCollElementLookup[10];
-extern u8 g_testColliFrames[10];
-extern u16 g_testCollPrizeTable[];
-extern u16 g_testCollRandTable[];
-
 #include "../collision.h"
