aa105d653ae6a6e6e03a0782234b8f81b889c0be
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2024-06-11 23:14:50 +0100

    PSP WRP merge warp.c and import bss section (#1303)
    Merging `wrp_psp/warp.c` into `wrp/warp.c`.
    
    I imported all the BSS data into `wrp_psp/bss.c`. I do not feel it is
    yet the right time to split the bss section into the individual files as
    I feel it might have a ripple effect to every other overlay.
    
    I confirm empty arrays on PSX are in data, but on PSP they are in bss.
    This is currently forcing me to do some `#ifdef` here and there. It
    should get solved once the bss is split into their respective original
    files.
    
    I feel very positive about the file split in WRP. So far it seems to be
    accurate. I really hope to spread this new pattern to other overlays
    too. It should make importing new stage overlays in the future much
    easier.

---
+++ src/st/wrp_psp/bss.c
@@ -0,0 +1,49 @@
+#include <stage.h>
+
+// warp.c
+s32 D_80193AA0;
+s32 D_80193AA4;
+s32 D_80193AA8;
+s32 D_80193AAC;
+s32 D_80180648;
+
+// st_debug.c
+u8 D_80180530[8];
+
+// e_misc.c
+u8 D_80180608[8]; // different than the D_80180608 in warp.c ???
+
+// ????
+u8 D_psp_0924BCD0[0x1000];
+
+// e_breakable.c
+u8 g_eBreakableExplosionTypes[8];
+
+// st_init_wrp.c
+s32 D_psp_0924BCC0;
+s32 D_psp_0924BCB8;
+s32 D_psp_0924BCB0;
+s32 D_psp_0924BCA8;
+s32 D_psp_0924BCA0;
+s32 D_psp_0924BC98;
+s32 D_psp_0924BC90;
+
+// ????
+LayerDef D_psp_0924BC80;
+
+// blit_char.c
+u8* D_psp_0924BC78;
+u8* D_psp_0924BC70;
+s32 D_psp_0924BC68;
+
+// e_particles.c
+s32 D_psp_0924BC60;
+
+// st_update.c
+u16 g_ItemIconSlots[0x20];
+
+// create_entity.c
+u16* g_LayoutObjHorizontal;
+u16* g_LayoutObjVertical;
+u8 g_LayoutObjPosHorizontal;
+u8 g_LayoutObjPosVertical;
