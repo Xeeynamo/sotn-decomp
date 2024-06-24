16c0b3f174fe19f5db442f2ff69e8b33f0367c53
Josh Lory <josh.lory@outlook.com>
2024-06-23 22:50:56 -0700

    Extract more DRA data (#1351)
    Continuing the work in
    https://github.com/Xeeynamo/sotn-decomp/pull/1350.

--- src/dra/843B0.c
+++ src/dra/843B0.c
@@ -849,7 +849,7 @@ void EntityHellfireHandler(Entity* self) {
         // I think this is to make the yellow laser beam?
         // it ends up looking like the library card effect.
         LoadImage(&D_800B0788, D_800B06C8);
-        LoadImage(&D_800B0790, &D_800B0728);
+        LoadImage(&D_800B0790, D_800B0728);
         prim = &g_PrimBuf[self->primIndex];
         prim->v0 = prim->v1 = prim->v2 = prim->v3 = 0xF8;
         prim->u0 = prim->u2 = 1;
