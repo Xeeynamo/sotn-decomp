128b0928dafd1e5710d8575d601c1f0034c6f00f
Jonathan Hohle <jon@ttkb.co>
2024-06-24 17:31:37 +0000

    Update Makefile Dependencies/Make 4.4 Compatibility
    This updates the Makefile so that targets which depend on phony targets
    (like `*_dirs`) have direct dependencies on those targets rather than
    relying on transient dependencies from dependant targets.
    
    This also adds colored print statements to each target to help identify
    what is being built/executed.
    
    In some situations splat won't write a new linker script if the existing
    one hasn't changed. However, because the inputs to splat (and it's make
    target) have newer timestamps than the generated linker script, make
    doesn't know it has been updated and will try to rebuild every time.
    These linker script targets now touch the script to ensure they have a
    later timestamp and won't be rebuilt.

--- Makefile.psx.mk
+++ Makefile.psx.mk
@@ -10,33 +10,57 @@ PSX_HD_TARGETS	:= dra tt_000
 # immovable
 PSX_BASE_SYMS	:= $(CONFIG_DIR)/symbols.$(VERSION).txt
 
+print_target = \
+    @printf "\033[0;32m$@\033[0m\n";
+
 extract_us: $(addprefix $(BUILD_DIR)/,$(addsuffix .ld,$(PSX_US_TARGETS)))
+	$(call print_target)
 	$(PNG2S) bdecode config/gfx.game.json disks/us assets/game
 extract_hd: $(addprefix $(BUILD_DIR)/,$(addsuffix .ld,$(PSX_HD_TARGETS)))
 
 extract_disk_us: extract_disk_psxus
 extract_disk_hd: extract_disk_pspeu
 extract_disk_psx%: $(SOTNDISK)
+	$(call print_target)
 	$(SOTNDISK) extract disks/sotn.$*.cue disks/$*
 
-$(BUILD_DIR)/main.ld: $(CONFIG_DIR)/splat.$(VERSION).main.yaml $(SPLAT_APP)
+# todo: these should have an explicit dependency on extract disk
+$(BUILD_DIR)/main.ld: $(CONFIG_DIR)/splat.$(VERSION).main.yaml | main_dirs
+	$(call print_target)
 	$(SPLAT) $<
-$(BUILD_DIR)/dra.ld: $(CONFIG_DIR)/splat.$(VERSION).dra.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).dra.txt $(SPLAT_APP)
+	touch $@
+# todo: these should have an explicit dependency on extract disk
+$(BUILD_DIR)/dra.ld: $(CONFIG_DIR)/splat.$(VERSION).dra.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).dra.txt | dra_dirs
+	$(call print_target)
 	$(SPLAT) $<
-$(BUILD_DIR)/ric.ld: $(CONFIG_DIR)/splat.$(VERSION).ric.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).ric.txt $(SPLAT_APP)
+	touch $@
+# todo: these should have an explicit dependency on extract disk
+$(BUILD_DIR)/ric.ld: $(CONFIG_DIR)/splat.$(VERSION).ric.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).ric.txt | ric_dirs
+	$(call print_target)
 	$(SPLAT) $<
-$(BUILD_DIR)/stmad.ld: $(CONFIG_DIR)/splat.$(VERSION).stmad.yaml $(CONFIG_DIR)/symbols.beta.txt $(CONFIG_DIR)/symbols.stmad.txt $(SPLAT_APP)
+	touch $@
+# todo: these should have an explicit dependency on extract disk
+$(BUILD_DIR)/stmad.ld: $(CONFIG_DIR)/splat.$(VERSION).stmad.yaml $(CONFIG_DIR)/symbols.beta.txt $(CONFIG_DIR)/symbols.stmad.txt | stmad_dirs
+	$(call print_target)
 	$(SPLAT) $<
 	$(GFXSTAGE) d disks/$(VERSION)/ST/MAD/F_MAD.BIN $(ASSETS_DIR)/st/mad
-$(BUILD_DIR)/st%.ld: $(CONFIG_DIR)/splat.$(VERSION).st%.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).st%.txt $(SPLAT_APP)
+
+# todo: these should have an explicit dependency on extract disk
+$(BUILD_DIR)/st%.ld: $(CONFIG_DIR)/splat.$(VERSION).st%.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).st%.txt | st%_dirs
+	$(call print_target)
 	$(SPLAT) $<
 	$(GFXSTAGE) d disks/$(VERSION)/ST/$$(echo '$*' | tr '[:lower:]' '[:upper:]')/F_$$(echo '$*' | tr '[:lower:]' '[:upper:]').BIN $(ASSETS_DIR)/st/$*
-$(BUILD_DIR)/tt_%.ld: $(CONFIG_DIR)/splat.$(VERSION).tt_%.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).tt_%.txt $(SPLAT_APP)
+$(BUILD_DIR)/tt_%.ld: $(CONFIG_DIR)/splat.$(VERSION).tt_%.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).tt_%.txt | tt_%_dirs
+	$(call print_target)
 	$(SPLAT) $<
-$(BUILD_DIR)/weapon.ld: $(CONFIG_DIR)/splat.$(VERSION).weapon.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).weapon.txt $(CONFIG_DIR)/symbols.$(VERSION).weapon.txt.in $(SPLAT_APP)
+	touch $@
+$(BUILD_DIR)/weapon.ld: $(CONFIG_DIR)/splat.$(VERSION).weapon.yaml $(PSX_BASE_SYMS) $(CONFIG_DIR)/symbols.$(VERSION).weapon.txt $(CONFIG_DIR)/symbols.$(VERSION).weapon.txt.in
+	$(call print_target)
 	$(SPLAT) $<
+	touch $@
 
 $(BUILD_DIR)/assets/dra/memcard_%.png.o: assets/dra/memcard_%.png
+	$(call print_target)
 	mkdir -p $(dir $@)
 	$(PNG2S) encode $< \
 		$(BUILD_DIR)/assets/dra/memcard_$*.png.s g_saveIcon$* \
@@ -46,6 +70,7 @@ $(BUILD_DIR)/assets/dra/memcard_%.png.o: assets/dra/memcard_%.png
 	$(AS) $(AS_FLAGS) -o $(BUILD_DIR)/assets/dra/memcard_$*.pal.o $(BUILD_DIR)/assets/dra/memcard_$*.pal.s
 	rm $(BUILD_DIR)/assets/dra/memcard_$*.pal.s
 $(BUILD_DIR)/assets/st/sel/memcard_%.png.o: assets/st/sel/memcard_%.png
+	$(call print_target)
 	mkdir -p $(dir $@)
 	$(PNG2S) encode $< \
 		$(BUILD_DIR)/assets/st/sel/memcard_$*.png.s g_saveIcon$* \
