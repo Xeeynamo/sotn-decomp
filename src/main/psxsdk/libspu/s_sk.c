f5406ebf02408ab0ce41a03048c959aec968860e
sozud <122322823+sozud@users.noreply.github.com>
2024-06-29 18:53:53 -0700

    SpuSetKey

--- src/main/psxsdk/libspu/s_sk.c
+++ src/main/psxsdk/libspu/s_sk.c
@@ -14,12 +14,22 @@ void SpuSetKey(long on_off, unsigned long voice_bit) {
         if (on_off != 1) {
             return;
         }
+#ifndef VERSION_PC
         _spu_RXX->rxx.key_on[0] = new_var;
         _spu_RXX->rxx.key_on[1] = temp_a3;
+#else
+        write_16(0x1F801D88, new_var, __FILE__, __LINE__);
+        write_16(0x1F801D8A, temp_a3, __FILE__, __LINE__);
+#endif
         _spu_keystat = temp_a1 | _spu_keystat;
     } else {
+#ifndef VERSION_PC
         _spu_RXX->rxx.key_off[0] = temp_a1;
         _spu_RXX->rxx.key_off[1] = temp_a3;
+#else
+        write_16(0x1F801D8C, temp_a1, __FILE__, __LINE__);
+        write_16(0x1F801D8E, temp_a3, __FILE__, __LINE__);
+#endif
         _spu_keystat = (~temp_a1) & _spu_keystat;
     }
 }
