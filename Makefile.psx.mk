fb1a3bc0b2a996ba359d2e37b4177c4d00e020fb
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-07-02 13:48:11 +0100

    Migrate NO3 to the asset tool

--- Makefile.psx.mk
+++ Makefile.psx.mk
@@ -60,6 +60,7 @@ extract_assets: $(SOTNASSETS)
 build_assets: $(SOTNASSETS)
 	$(SOTNASSETS) build_all -i assets/st/cen_test -o src/st/cen/
 	$(SOTNASSETS) build_all -i assets/st/dre_test -o src/st/dre/
+	$(SOTNASSETS) build_all -i assets/st/no3_test -o src/st/no3/
 	$(SOTNASSETS) build_all -i assets/st/nz0_test -o src/st/nz0/
 	$(SOTNASSETS) build_all -i assets/st/wrp_test -o src/st/wrp/
 
