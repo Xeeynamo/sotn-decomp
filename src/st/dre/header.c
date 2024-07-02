7eee871f822268ea8bf31e0372f97727f7aef666
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-07-02 13:02:14 +0100

    Migrate DRE to the asset tool

--- src/st/dre/header.c
+++ src/st/dre/header.c
@@ -1,9 +1,9 @@
 #include "dre.h"
 
 extern RoomHeader g_Rooms[];
-extern SpriteParts** SpriteBanks[];
+extern signed short* spriteBanks[];
 extern void* Cluts[];
-extern RoomDef g_TileLayers[];
+extern MyRoomDef rooms[];
 extern GfxBank* g_GfxBanks[];
 void UpdateStageEntities();
 
@@ -13,10 +13,10 @@ static Overlay StageOverlay = {
     .UpdateRoomPosition = UpdateRoomPosition,
     .InitRoomEntities = InitRoomEntities,
     .rooms = g_Rooms,
-    .spriteBanks = SpriteBanks,
+    .spriteBanks = spriteBanks,
     .cluts = Cluts,
     .objLayoutHorizontal = NULL,
-    .tileLayers = g_TileLayers,
+    .tileLayers = rooms,
     .gfxBanks = g_GfxBanks,
     .UpdateStageEntities = UpdateStageEntities,
     .unk2c = NULL,
@@ -26,13 +26,7 @@ static Overlay StageOverlay = {
     .StageEndCutScene = NULL,
 };
 
-extern SpriteParts* D_80190168[];
-
-static SpriteParts** SpriteBanks[] = {
-    NULL, D_80190168, NULL, NULL, NULL, NULL, NULL, NULL,
-    NULL, NULL,       NULL, NULL, NULL, NULL, NULL, NULL,
-    NULL, NULL,       NULL, NULL, NULL, NULL, NULL, NULL,
-};
+#include "sprite_banks.h"
 
 extern u16* D_8018AEA8[0x50];
 extern u16* D_8018AF48[0x80];
@@ -53,3 +47,5 @@ static u16** Clut[] = {
 static void* Cluts[] = {
     Clut,
 };
+
+#include "layers.h"
