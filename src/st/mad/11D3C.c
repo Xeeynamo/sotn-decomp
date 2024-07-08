2220f6c5bde197b4de06fbefbca321e338d33aa3
Jonathan Hohle <jonhohle@gmail.com>
2024-07-08 12:16:47 -0700

    Use PSP InitializeEntity Impl (#1394)
    Update `InitializeEntity` to use the PSP decompilation and remove
    duplicate implmeentations across stages.

--- src/st/mad/11D3C.c
+++ src/st/mad/11D3C.c
@@ -303,35 +303,7 @@ void EntityExplosionSpawn(u16 arg0, u16 arg1) {
     g_CurrentEntity->step_s = 0;
 }
 
-void InitializeEntity(u16 arg0[]) {
-    u16 enemyId;
-    EnemyDef* enemyDef;
-
-    g_CurrentEntity->animSet = *arg0++;
-    g_CurrentEntity->animCurFrame = *arg0++;
-    g_CurrentEntity->unk5A = *arg0++;
-    g_CurrentEntity->palette = *arg0++;
-
-    enemyId = *arg0++;
-    g_CurrentEntity->enemyId = enemyId;
-    enemyDef = &g_api.enemyDefs[enemyId];
-    g_CurrentEntity->hitPoints = enemyDef->hitPoints;
-    g_CurrentEntity->attack = enemyDef->attack;
-    g_CurrentEntity->attackElement = enemyDef->attackElement;
-    g_CurrentEntity->hitboxState = enemyDef->hitboxState;
-    g_CurrentEntity->hitboxWidth = enemyDef->hitboxWidth;
-    g_CurrentEntity->hitboxHeight = enemyDef->hitboxHeight;
-    g_CurrentEntity->flags = enemyDef->flags;
-    g_CurrentEntity->hitboxOffX = 0;
-    g_CurrentEntity->hitboxOffY = 0;
-    g_CurrentEntity->step_s = 0;
-    g_CurrentEntity->step++;
-    if (g_CurrentEntity->zPriority == 0) {
-        g_CurrentEntity->zPriority =
-            g_unkGraphicsStruct.g_zEntityCenter.unk - 0xC;
-    }
-}
-
+#include "../init_entity.h"
 void EntityDummy(Entity* arg0) {
     if (arg0->step == 0) {
         arg0->step++;
