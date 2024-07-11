4f68fb6e1f1b70ea22af6a97c71e14e3d0058091
Josh Lory <josh.lory@outlook.com>
2024-07-11 15:06:36 +0000

    Attempt to fix CI build

--- src/dra/dra.h
+++ src/dra/dra.h
@@ -323,10 +323,6 @@ extern Point16 D_800ACEE0[];
 #if defined(VERSION_HD)
 extern s32 D_800ACEDC_hd;
 #endif
-extern u8 D_800ACF4C[];
-extern u8 D_800ACF54[10];
-extern s16 D_800ACF60[]; // collection of sounds
-extern s16 D_800ACF6C[];
 extern s32 D_800ACF74; // These two might...
 extern s32 D_800ACF78; // ...be an array
 extern s16 D_800ACF7C[4];
