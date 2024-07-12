d09bbe8596f86c6d77d67a9c7c91624657210bc1
bismurphy <tjmurphy@mit.edu>
2024-07-12 14:05:27 -0400

    Decompile DRA EntityPlayerOutline (#1405)
    This was EntityMPReplenished, but then I looked into how it is used and
    found that it is used for many other things besides replenishing MP, so
    I named it more generically.
    
    I think I hunted down every situation where it is used and documented
    them fairly well. In general, I think this is a relatively good-looking
    function and is easy to read.
    
    Looking forward to seeing any feedback!

--- src/pc/stubs.c
+++ src/pc/stubs.c
@@ -581,7 +581,6 @@ u16* func_80106A28(u32 arg0, u16 kind) {
 void func_80115394(s32* arg0, s16 arg1, s16 arg2) { NOT_IMPLEMENTED; }
 
 void EntityNumberMovesToHpMeter(Entity* self) { NOT_IMPLEMENTED; }
-void EntityMpReplenished(Entity* self) { NOT_IMPLEMENTED; }
 void EntityPlayerDissolves(Entity* self) { NOT_IMPLEMENTED; }
 void EntityGuardText(Entity* self) { NOT_IMPLEMENTED; }
 void func_8012F894(Entity* self) { NOT_IMPLEMENTED; }
