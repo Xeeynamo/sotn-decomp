2f42eb037283a595afaf810ef8e85086f329a4ca
sozud <122322823+sozud@users.noreply.github.com>
2024-07-21 23:15:32 -0400

    updates

--- src/main/psxsdk/libcd/cdread2.c
+++ src/main/psxsdk/libcd/cdread2.c
@@ -2,4 +2,8 @@
 
 INCLUDE_ASM("main/nonmatchings/psxsdk/libcd/cdread2", CdRead2);
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libcd/cdread2", StCdInterrupt2);
+void StCdInterrupt();
+
+void StCdInterrupt2(void) {
+    StCdInterrupt();
+}
