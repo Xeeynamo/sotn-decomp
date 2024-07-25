e8f2c45e5d2551e847e3f8e961300db598125210
sozud <122322823+sozud@users.noreply.github.com>
2024-07-25 07:51:50 -0400

    updates

--- src/main/psxsdk/libcd/c_011.c
+++ src/main/psxsdk/libcd/c_011.c
@@ -2,18 +2,10 @@
 
 INCLUDE_ASM("main/nonmatchings/psxsdk/libcd/c_011", StCdInterrupt);
 
-void mem2mem(s32* arg0, s32* arg1, u32 arg2) {
-    s32 temp_v0;
-    s32* var_a0;
-    s32* var_a1;
-    u32 var_v1;
-
-    var_a0 = arg0;
-    var_a1 = arg1;
-
-    for (var_v1 = 0; var_v1 < arg2; var_v1++) {
-        temp_v0 = *var_a1++;
-        *var_a0++ = temp_v0;
+void mem2mem(s32* dst, s32* src, u32 num) {
+    u32 i;
+    for (i = 0; i < num; i++) {
+        *dst++ = *src++;
     }
 }
 
