1e0458ff1023e6a121ad33682a9434d097619751
sozud <122322823+sozud@users.noreply.github.com>
2024-07-25 07:54:53 -0400

    cd funcs (#1431)
    some of these by @joshlory

--- src/main/psxsdk/libcd/c_011.c
+++ src/main/psxsdk/libcd/c_011.c
@@ -2,6 +2,11 @@
 
 INCLUDE_ASM("main/nonmatchings/psxsdk/libcd/c_011", StCdInterrupt);
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libcd/c_011", mem2mem);
+void mem2mem(s32* dst, s32* src, u32 num) {
+    u32 i;
+    for (i = 0; i < num; i++) {
+        *dst++ = *src++;
+    }
+}
 
 INCLUDE_ASM("main/nonmatchings/psxsdk/libcd/c_011", dma_execute);
