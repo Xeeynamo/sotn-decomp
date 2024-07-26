d26060e8f0aa46abf860ea27afab02e9791f81f3
Josh Lory <josh.lory@outlook.com>
2024-07-26 23:21:21 +0000

    Decompile func_ptr_80170008_w_012

--- src/weapon/w_012.c
+++ src/weapon/w_012.c
@@ -7,7 +7,30 @@ INCLUDE_ASM("weapon/nonmatchings/w_012", EntityWeaponAttack);
 
 INCLUDE_ASM("weapon/nonmatchings/w_012", func_ptr_80170004);
 
-INCLUDE_ASM("weapon/nonmatchings/w_012", func_ptr_80170008);
+void func_ptr_80170008(Entity* self) {
+    if (self->step == 0) {
+        if (self->ext.weapon.parent->entityId == 0) {
+            DestroyEntity(self);
+            return;
+        }
+        self->animCurFrame = self->ext.weapon.parent->animCurFrame;
+        self->animSet = self->ext.weapon.parent->animSet;
+        self->facingLeft = self->ext.weapon.parent->facingLeft;
+        self->unk5A = self->ext.weapon.parent->unk5A;
+        self->zPriority = PLAYER.zPriority - 4;
+        self->flags = FLAG_UNK_08000000 | FLAG_UNK_04000000 | FLAG_UNK_20000;
+        self->palette = self->ext.weapon.parent->palette;
+        self->drawFlags = self->ext.weapon.parent->drawFlags + FLAG_DRAW_UNK8;
+        self->rotZ = self->ext.weapon.parent->rotZ;
+        self->unk6C = 0x80;
+        self->step++;
+    }
+    if (self->unk6C >= 48) {
+        self->unk6C -= 8;
+        return;
+    }
+    DestroyEntity(self);
+}
 
 void func_ptr_8017000C(Entity* self) {}
 
