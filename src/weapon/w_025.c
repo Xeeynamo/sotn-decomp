41159bab18dc4f11324c37479a8ac7850702d8d6
bismurphy <tjmurphy@mit.edu>
2024-07-28 20:37:18 -0400

    Decompile Goddess Shield func_ptr_80170024 (#1451)
    Nothing very special here, each shield seems to have different
    func_ptr_80170024 effects and this is one of them.

--- src/weapon/w_025.c
+++ src/weapon/w_025.c
@@ -164,7 +164,134 @@ int GetWeaponId(void) { return 25; }
 
 INCLUDE_ASM("weapon/nonmatchings/w_025", EntityWeaponShieldSpell);
 
-INCLUDE_ASM("weapon/nonmatchings/w_025", func_ptr_80170024);
+// Goddess shield
+void func_ptr_80170024(Entity* self) {
+    Primitive* prim;
+    s16 angle;
+    s16 xSize;
+    s16 ySize;
+    s16 xVar;
+    s16 yVar;
+
+    switch (self->step) {
+    case 0:
+        self->primIndex = g_api.AllocPrimitives(PRIM_GT4, 1);
+        if (self->primIndex == -1) {
+            DestroyEntity(self);
+            return;
+        }
+        SetSpriteBank1(D_B3000_8017A040);
+        if (g_HandId != 0) {
+            self->animSet += 2;
+            self->ext.shield.childPalette = 0x12A;
+            self->ext.shield.unk7D = 0x80;
+        } else {
+            self->ext.shield.childPalette = 0x112;
+            self->ext.shield.unk7D = 0;
+        }
+        self->posX.i.hi =
+            (self->ext.shield.parent->posX.i.hi + (rand() % 160)) - 80;
+        self->posY.i.hi =
+            (self->ext.shield.parent->posY.i.hi + (rand() % 192)) - 64;
+        self->flags |= FLAG_UNK_04000000 | FLAG_HAS_PRIMS;
+        self->facingLeft = PLAYER.facingLeft;
+        self->rotX = self->rotY = 0;
+        self->rotZ = 0;
+
+        prim = &g_PrimBuf[self->primIndex];
+        prim->clut = self->ext.shield.childPalette;
+        prim->tpage = 0x19;
+
+        prim->u0 = prim->u2 = 0;
+        prim->u1 = prim->u3 = 0x5F;
+        prim->v0 = prim->v1 = self->ext.shield.unk7D + 0x28;
+        prim->v2 = prim->v3 = self->ext.shield.unk7D + 0x28 + 0x57;
+        prim->r0 = prim->g0 = prim->b0 = prim->r1 = prim->g1 = prim->b1 =
+            prim->r2 = prim->g2 = prim->b2 = prim->r3 = prim->g3 = prim->b3 =
+                0x80;
+        if (rand() & 3) {
+            prim->priority = 0x1BC;
+        } else {
+            prim->priority = 0x1B6;
+        }
+        prim->drawMode = DRAW_TPAGE2 | DRAW_TPAGE | DRAW_COLORS | DRAW_TRANSP;
+        self->ext.shield.unk80 = (rand() & 7) + 8;
+        self->ext.shield.unk96 = rand() % 16 - 8;
+        if (self->ext.shield.unk96 >= 0) {
+            self->ext.shield.unk96++;
+        }
+        // PSP has rand() & 0x7FFF here, don't know why
+        self->ext.shield.unk98 = rand();
+        self->step++;
+        break;
+    case 1:
+        self->rotX += 8;
+        self->rotY += 8;
+        if (!self->ext.shield.unk80) {
+            self->ext.shield.unk80 = (rand() & 0x3F) + 0x60;
+            self->step++;
+        }
+        break;
+    case 2:
+        self->rotX += 2;
+        self->rotY += 2;
+        if (!self->ext.shield.unk80) {
+            self->step++;
+        }
+        break;
+    case 3:
+        prim = &g_PrimBuf[self->primIndex];
+        if (prim->b3 >= 6) {
+            prim->b3 -= 6;
+        }
+        if (prim->b3 < 7) {
+            DestroyEntity(self);
+            return;
+        }
+        prim->r0 = prim->g0 = prim->b0 = prim->r1 = prim->g1 = prim->b1 =
+            prim->r2 = prim->g2 = prim->b2 = prim->r3 = prim->g3 = prim->b3;
+        break;
+    }
+    self->ext.shield.unk80--;
+    self->ext.shield.unk98 += 0x40;
+    if (!(self->ext.shield.unk80 & 3)) {
+        if (self->ext.shield.parent->entityId) {
+            angle = self->ext.shield.unk98;
+            xVar = self->ext.shield.parent->posX.i.hi;
+            yVar = self->ext.shield.parent->posY.i.hi - 0x20;
+            yVar -= (((rsin(angle) >> 4) * 64) >> 8);
+            xVar += (((rcos(angle) >> 4) * 48) >> 8);
+            angle = ratan2(-(yVar - self->posY.i.hi), xVar - self->posX.i.hi);
+            self->velocityX = rcos(angle) * 8;
+            self->velocityY = -rsin(angle) * 8;
+        } else {
+            self->velocityY = 0;
+            self->velocityX = 0;
+            self->step = 3;
+        }
+    }
+    self->posX.val += self->velocityX;
+    self->posY.val += self->velocityY;
+    self->rotZ += self->ext.shield.unk96;
+    xVar = self->posX.i.hi;
+    yVar = self->posY.i.hi;
+
+    xSize = self->rotX * 0x43 / 256;
+    ySize = self->rotY * 0x43 / 256;
+    prim = &g_PrimBuf[self->primIndex];
+    angle = self->rotZ + 0x639;
+    prim->x0 = xVar + (((rcos(angle) >> 4) * xSize) >> 8);
+    prim->y0 = yVar - (((rsin(angle) >> 4) * ySize) >> 8);
+    angle = self->rotZ + 0x1C7;
+    prim->x1 = xVar + (((rcos(angle) >> 4) * xSize) >> 8);
+    prim->y1 = yVar - (((rsin(angle) >> 4) * ySize) >> 8);
+    angle = self->rotZ + 0x9C7;
+    prim->x2 = xVar + (((rcos(angle) >> 4) * xSize) >> 8);
+    prim->y2 = yVar - (((rsin(angle) >> 4) * ySize) >> 8);
+    angle = self->rotZ + 0xE39;
+    prim->x3 = xVar + (((rcos(angle) >> 4) * xSize) >> 8);
+    prim->y3 = yVar - (((rsin(angle) >> 4) * ySize) >> 8);
+}
 
 void func_ptr_80170028(Entity* self) {}
 
