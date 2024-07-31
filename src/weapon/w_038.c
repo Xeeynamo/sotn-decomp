d011e9eb70237bb32e3d5e19e40905c355bbdcb1
Jonathan Hohle <jon@ttkb.co>
2024-07-31 14:48:19 -0700

    Decompile w_038, w_039 `EntityWeaponAttack` (#1457)
    Decompiles `EntityWeaponAttack` for `w_038` and `w_039`. These match
    several other `EntityWeaponAttack` methods with the addition of a
    conditional call to `CreateEntFactoryFromEntity` at the end.

--- src/weapon/w_038.c
+++ src/weapon/w_038.c
@@ -3,7 +3,66 @@
 #include "weapon_private.h"
 #include "shared.h"
 
-INCLUDE_ASM("weapon/nonmatchings/w_038", EntityWeaponAttack);
+extern WeaponAnimation D_10E000_8017ADC0[2];
+
+void EntityWeaponAttack(Entity* self) {
+    WeaponAnimation* anim;
+    s8 animIndex;
+
+    self->posX.val = PLAYER.posX.val;
+    self->posY.val = PLAYER.posY.val;
+    self->facingLeft = PLAYER.facingLeft;
+    animIndex = (self->params & 0x7fff) >> 0x8;
+    anim = &D_10E000_8017ADC0[animIndex];
+
+    if (PLAYER.ext.weapon.anim < anim->frameStart ||
+        PLAYER.ext.weapon.anim >= (anim->frameStart + 7) || !g_Player.unk46) {
+        DestroyEntity(self);
+        return;
+    }
+
+    if (self->step == 0) {
+        SetSpriteBank1(g_Animset);
+        self->animSet = ANIMSET_OVL(0x10);
+        self->palette = 0x110;
+        self->unk5A = 0x64;
+
+        if (g_HandId != 0) {
+            self->animSet += 2;
+            self->palette += 0x18;
+            self->unk5A += 2;
+        }
+
+        self->palette += anim->palette;
+        self->flags = FLAG_UNK_20000 | FLAG_UNK_40000;
+        self->zPriority = PLAYER.zPriority - 2;
+        SetWeaponProperties(self, 0);
+
+        self->step++;
+    }
+
+    self->ext.weapon.anim = PLAYER.ext.weapon.anim - anim->frameStart;
+
+    if (PLAYER.animFrameDuration == 1 &&
+        PLAYER.animFrameIdx == anim->soundFrame) {
+        g_api.PlaySfx(anim->soundId);
+    }
+
+    if (g_api.UpdateUnarmedAnim(anim->frameProps, anim->frames) < 0) {
+        DestroyEntity(self);
+        return;
+    }
+
+    self->drawFlags = PLAYER.drawFlags;
+    self->rotY = PLAYER.rotY;
+    self->rotPivotY = PLAYER.rotPivotY;
+
+    if (animIndex != 0 && PLAYER.animFrameIdx == 4 &&
+        PLAYER.animFrameDuration == 1) {
+        g_api.CreateEntFactoryFromEntity(
+            self, ((g_HandId + 1) << 0xC) | 0x38, 0);
+    }
+}
 
 INCLUDE_ASM("weapon/nonmatchings/w_038", func_ptr_80170004);
 
