a9863ac2ef1a4c68c6c9b4356f09f2cbed745880
DerpPrincess <nekokagamines@gmail.com>
2024-08-05 20:11:15 -0500

    PlayerOvl Richter data d_0.c imported

---
+++ src/ric/d_0.c
@@ -0,0 +1,20 @@
+#include "ric.h"
+
+void UpdateEntityRichter(void);
+void func_80156F40(s16 arg0);
+void func_801603C4(void);
+void func_8015E7B4(Unkstruct_8010BF64* arg0);
+
+PlayerOvl Richter = {
+    /* 0x00 */ UpdateEntityRichter,
+    /* 0x04 */ func_80156F40,
+    /* 0x08 */ func_801603C4,
+    /* 0x0C */ func_8015E7B4,
+};
+
+s32* D_8013C010[4] = { 
+    0x8013C020,
+    0x8013C020,
+    0x8013C020,
+    0x8013C020
+};
