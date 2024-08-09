1cbad8bddad0a9ce364555959dd47460f1d74daa
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-08-09 12:23:32 +0100

    st_update

---
+++ src/st/nz0/st_update.c
@@ -0,0 +1,16 @@
+#include <stage.h>
+
+static u16 UNK_Invincibility0[] = {
+    PAL_OVL(0x163), PAL_OVL(0x164), PAL_OVL(0x166), PAL_OVL(0x164),
+    PAL_OVL(0x160), PAL_OVL(0x166), PAL_OVL(0x162), PAL_OVL(0x164),
+    PAL_OVL(0x167), PAL_OVL(0x164), PAL_OVL(0x167), PAL_OVL(0x168),
+    PAL_OVL(0x168), PAL_OVL(0x164), PAL_OVL(0x161), PAL_OVL(0x164),
+    PAL_OVL(0x165), PAL_OVL(0x165), PAL_OVL(0x163), PAL_OVL(0x163),
+    PAL_OVL(0x165), PAL_OVL(0x165),
+};
+
+#include "../random.h"
+
+#include "../update.h"
+
+#include "../update_stage_entities.h"
