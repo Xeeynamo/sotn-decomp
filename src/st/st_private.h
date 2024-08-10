412af86347a4b2ec1d78aadc4161ea8ce2bb268f
Jonathan Hohle <jon@ttkb.co>
2024-08-09 21:49:03 -0700

    remove unnecessary forward decls

--- src/st/st_private.h
+++ src/st/st_private.h
@@ -16,22 +16,4 @@
 #define LAYOUT_OBJ_SLOT 3
 #define LAYOUT_OBJ_PARAMS 4
 
-// TODO: are these necessary any longer?
-/*
- * A pointer to the current element in `g_pStObjLayoutHorizontal`
- */
-extern u16* g_LayoutObjHorizontal;
-/*
- * A pointer to the current element in `g_pStObjLayoutVertical`
- */
-extern u16* g_LayoutObjVertical;
-/*
- * The direction last traversed in `g_pStObjLayoutHorizontal`
- */
-extern u8 g_LayoutObjPosHorizontal;
-/*
- * The direction last traversed in `g_pStObjLayoutVertical``
- */
-extern u8 g_LayoutObjPosVertical;
-
 #endif // ST_PRIVATE_H
