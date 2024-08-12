a174457e776e5e88dea9c2829257fcee5bccba14
Joey Murphy <tjmurphy@mit.edu>
2024-08-12 19:52:40 -0400

    Fix PSP

--- src/st/collect_heart.h
+++ src/st/collect_heart.h
@@ -8,7 +8,7 @@ void CollectHeart(u16 arg0) {
     __builtin_memcpy(heartPrizes, c_HeartPrizes[0], 2);
     __builtin_memcpy(mad_unknown, c_HeartPrizes[2], 2);
 #else
-    s8* heartPrizes = c_HeartPrizes;
+#define heartPrizes c_HeartPrizes
 #endif
 
     g_api.PlaySfx(SFX_HEART_PICKUP);
