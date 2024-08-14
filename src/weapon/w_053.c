18b596a679dfa4869058a7a3f42feadf6382c406
Josh Lory <josh.lory@outlook.com>
2024-08-13 23:36:40 -0500

    Decompile func_ptr_80170008_w_053 (#1508)
    https://decomp.me/scratch/jAvbr

--- src/weapon/w_053.c
+++ src/weapon/w_053.c
@@ -86,7 +86,67 @@ void EntityWeaponAttack(Entity* self) {
 
 INCLUDE_ASM("weapon/nonmatchings/w_053", func_ptr_80170004);
 
-INCLUDE_ASM("weapon/nonmatchings/w_053", func_ptr_80170008);
+static void func_ptr_80170008(Entity* self) {
+    switch (self->step) {
+    case 0:
+        self->palette = PAL_OVL(0x100);
+        self->zPriority = PLAYER.zPriority;
+        self->facingLeft = PLAYER.facingLeft;
+        self->animCurFrame = PLAYER.animCurFrame - ANIMSET_OVL_FLAG;
+        self->animSet = 1;
+        self->drawMode = DRAW_TPAGE;
+        self->unk5A = 0;
+        self->flags = FLAG_UNK_04000000 | FLAG_UNK_20000;
+
+        SetSpeedX(FIX(8));
+        self->velocityY = FIX(-0.5);
+        if (self->params & 0x7F00) {
+            g_api.CreateEntFactoryFromEntity(
+                self, ((g_HandId + 1) << 0xC) + FACTORY(0x2800, 0x38), 0);
+        }
+        self->step++;
+        break;
+    case 1:
+        self->animCurFrame = PLAYER.animCurFrame - ANIMSET_OVL_FLAG;
+        self->posX.val += self->velocityX;
+        self->posY.val += self->velocityY;
+        DecelerateX(FIX(15) / 32);
+        if (PLAYER.animFrameIdx == 6) {
+            g_api.CreateEntFactoryFromEntity(
+                self, ((g_HandId + 1) << 0xC) + FACTORY(0x1E00, 0x38), 0);
+            self->step++;
+        }
+        break;
+    case 2:
+        self->drawMode = 0;
+        self->animCurFrame = PLAYER.animCurFrame - ANIMSET_OVL_FLAG;
+        if (PLAYER.animFrameIdx == 8 && PLAYER.animFrameDuration == 1) {
+            g_api.CreateEntFactoryFromEntity(
+                self, ((g_HandId + 1) << 0xC) + FACTORY(0x1E00, 0x38), 0);
+        }
+        if (PLAYER.animFrameIdx == 10) {
+            SetSpeedX(FIX(-8));
+            self->velocityY = FIX(0.5);
+            self->step++;
+        }
+        break;
+    case 3:
+        self->drawMode = DRAW_TPAGE;
+        self->animCurFrame = PLAYER.animCurFrame - ANIMSET_OVL_FLAG;
+        self->posX.val += self->velocityX;
+        self->posY.val += self->velocityY;
+        DecelerateX(FIX(15) / 32);
+        if (PLAYER.animFrameIdx == 15) {
+            DestroyEntity(self);
+            return;
+        }
+        break;
+    }
+
+    self->drawFlags = PLAYER.drawFlags;
+    self->rotY = PLAYER.rotY;
+    self->rotPivotY = PLAYER.rotPivotY;
+}
 
 INCLUDE_ASM("weapon/nonmatchings/w_053", func_ptr_8017000C);
 
