7d0e85f846e21bc94fa5a749e4d459b9baa7056b
Jonathan Hohle <jon@ttkb.co>
2024-08-14 10:08:10 -0700

    Decompile `EntityWeaponAttack` in `w_032`

--- src/weapon/w_032.c
+++ src/weapon/w_032.c
@@ -9,7 +9,75 @@ extern s32 g_HandId;
 #define g_Animset w_032_1
 #define g_Animset2 w_032_2
 
-INCLUDE_ASM("weapon/nonmatchings/w_032", EntityWeaponAttack);
+extern WeaponAnimation D_E4000_8017A8F8[];
+
+void EntityWeaponAttack(Entity* self) {
+    WeaponAnimation* anim;
+    s16 animIndex;
+
+    self->posX.val = PLAYER.posX.val;
+    self->posY.val = PLAYER.posY.val;
+    self->facingLeft = PLAYER.facingLeft;
+    animIndex = (self->params >> 8) & 0x7F;
+    anim = &D_E4000_8017A8F8[animIndex];
+
+    if ((PLAYER.ext.player.anim < anim->frameStart ||
+         PLAYER.ext.player.anim >= (anim->frameStart + 7) ||
+         g_Player.unk46 == 0) &&
+        self->step != 2) {
+        self->step = 2;
+        self->ext.timer.t = 4;
+        self->animCurFrame = 0;
+        self->hitboxState = 0;
+    }
+    if (self->step == 0) {
+        SetSpriteBank1(g_Animset);
+        self->animSet = ANIMSET_OVL(0x10);
+        self->palette = PAL_DRA(0x110);
+        self->unk5A = 0x64;
+        if (g_HandId) {
+            self->palette += 0x18;
+            self->animSet += 2;
+            self->unk5A = 0x66;
+        }
+        self->palette += anim->palette;
+        self->flags = FLAG_UNK_40000 | FLAG_UNK_20000;
+        if (self->params & 0x7F00) {
+            self->zPriority = PLAYER.zPriority - 2;
+        } else {
+            self->zPriority = PLAYER.zPriority + 2;
+        }
+        SetWeaponProperties(self, 0);
+        self->step++;
+    }
+    if (self->step == 2) {
+        self->ext.timer.t--;
+        if (!self->ext.timer.t) {
+            DestroyEntity(self);
+        }
+    } else {
+        self->ext.weapon.anim = PLAYER.ext.player.anim - anim->frameStart;
+        if ((PLAYER.animFrameDuration == 1) &&
+            (PLAYER.animFrameIdx == anim->soundFrame)) {
+            g_api.PlaySfx(anim->soundId);
+            if (anim->soundId == SFX_WEAPON_SWISH_B) {
+                g_api.PlaySfx(SFX_UNK_6EF);
+            }
+        }
+        if (g_api.UpdateUnarmedAnim(anim->frameProps, anim->frames) < 0) {
+            self->step = 2;
+            self->ext.timer.t = 4;
+            self->animCurFrame = 0;
+            self->hitboxState = 0;
+        }
+        if (self->ext.weapon.anim < 4 && PLAYER.animFrameIdx == 6) {
+            self->hitboxState = 4;
+        }
+        self->drawFlags = PLAYER.drawFlags;
+        self->rotY = PLAYER.rotY;
+        self->rotPivotY = PLAYER.rotPivotY;
+    }
+}
 
 s32 func_ptr_80170004(Entity* self) {}
 
