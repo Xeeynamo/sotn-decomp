5428b3399dbc58c7e4bea1c802307e527f69a67c
Max Clark <clark.max.a@gmail.com>
2024-09-11 03:12:37 +0000

    Move ifdef to point sozud pointed out

--- include/entity.h
+++ include/entity.h
@@ -361,6 +361,9 @@ typedef struct {
     s32 : 32;
     s16 : 16;
     struct Entity* parent;
+#ifdef VERSION_PC
+    /* 0xXX */ s32 : 32;
+#endif
     s32 unk90;
     s32 unk94;
     s32 unk98;
@@ -368,9 +371,6 @@ typedef struct {
     s32 accelerationY;
     s32 unkA4;
     s32 unkA8;
-#ifdef VERSION_PC
-    /* 0xXX */ s32 : 32;
-#endif
     u8 anim;
 } ET_WeaponUnk016;
 
