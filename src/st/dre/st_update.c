8c3cf9c23cff7c2bb0e696478174aea571a75cbd
Joey Murphy <tjmurphy@mit.edu>
2024-09-15 17:23:40 -0400

    Standardize DRE st_update

---
+++ src/st/dre/st_update.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <game.h>
+
+// palette
+u16 UNK_Invincibility0[] = {
+    PAL_OVL(0x163), PAL_OVL(0x164), PAL_OVL(0x166), PAL_OVL(0x164),
+    PAL_OVL(0x160), PAL_OVL(0x166), PAL_OVL(0x162), PAL_OVL(0x164),
+    PAL_OVL(0x167), PAL_OVL(0x164), PAL_OVL(0x167), PAL_OVL(0x168),
+    PAL_OVL(0x168), PAL_OVL(0x164), PAL_OVL(0x161), PAL_OVL(0x164),
+    PAL_OVL(0x165), PAL_OVL(0x165), PAL_OVL(0x163), PAL_OVL(0x163),
+    PAL_OVL(0x165), PAL_OVL(0x165),
+};
+#include "../random.h"
+#include "../update.h"
+#include "../update_stage_entities.h"
