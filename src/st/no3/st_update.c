fcd0e1eeced680acc9241d738f3397150c22c4cc
Joey Murphy <tjmurphy@mit.edu>
2024-09-15 17:32:29 -0400

    Deploy to all other overlays

--- src/st/no3/st_update.c
+++ src/st/no3/st_update.c
@@ -1,15 +1,2 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include <stage.h>
-
-static u16 UNK_Invincibility0[] = {
-    PAL_OVL(0x163), PAL_OVL(0x164), PAL_OVL(0x166), PAL_OVL(0x164),
-    PAL_OVL(0x160), PAL_OVL(0x166), PAL_OVL(0x162), PAL_OVL(0x164),
-    PAL_OVL(0x167), PAL_OVL(0x164), PAL_OVL(0x167), PAL_OVL(0x168),
-    PAL_OVL(0x168), PAL_OVL(0x164), PAL_OVL(0x161), PAL_OVL(0x164),
-    PAL_OVL(0x165), PAL_OVL(0x165), PAL_OVL(0x163), PAL_OVL(0x163),
-    PAL_OVL(0x165), PAL_OVL(0x165),
-};
-
-#include "../random.h"
-#include "../update.h"
-#include "../update_stage_entities.h"
+#include "../st_update.h"
--- src/st/st_update.h
+++ src/st/st_update.h
@@ -8,7 +8,9 @@ u16 UNK_Invincibility0[] = {
     PAL_OVL(0x167), PAL_OVL(0x164), PAL_OVL(0x167), PAL_OVL(0x168),
     PAL_OVL(0x168), PAL_OVL(0x164), PAL_OVL(0x161), PAL_OVL(0x164),
     PAL_OVL(0x165), PAL_OVL(0x165), PAL_OVL(0x163), PAL_OVL(0x163),
+#if !(defined VERSION_BETA)
     PAL_OVL(0x165), PAL_OVL(0x165),
+#endif
 };
 #include "random.h"
 #include "update.h"
