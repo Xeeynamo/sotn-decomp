738a55b95be6a573995f5746b506978eae177102
sozud <122322823+sozud@users.noreply.github.com>
2024-09-16 18:57:42 -0700

    file

---
+++ src/st/cutscene_unk1.h
@@ -0,0 +1,9 @@
+void CutsceneUnk1(void) {
+    g_Dialogue.nextLineX = 2;
+    g_Dialogue.nextCharX = 2;
+    g_Dialogue.nextCharY = 0;
+    g_Dialogue.unk12 = 0;
+    g_Dialogue.nextCharTimer = 0;
+    g_Dialogue.unk17 = 8;
+    g_Dialogue.nextLineY = g_Dialogue.startY + 0x14;
+}
