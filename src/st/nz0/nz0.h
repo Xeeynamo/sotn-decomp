39c26fd5ebb041cd7e353176ac1a08845ecfcc83
Joey Murphy <tjmurphy@mit.edu>
2024-09-18 14:22:54 -0400

    Add NZ0

--- src/st/nz0/nz0.h
+++ src/st/nz0/nz0.h
@@ -208,7 +208,7 @@ extern const u8 D_80181378[];
 extern const u8 D_80181388[];
 extern s32 g_ElevatorTarget;
 extern s16 g_ElevatorTargetPos[];
-extern u16 D_801813B0[];
+extern u16 g_HeartDropArray[];
 extern const char* D_8018146C[];
 extern s16 D_80181978[];
 extern u16 D_80181D9C[];
