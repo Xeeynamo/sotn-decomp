20c8880c9044df605fa24bda973e8ad2e4ae0e18
Joey Murphy <tjmurphy@mit.edu>
2024-09-18 15:34:50 -0400

    Add CEN

--- src/st/cen/entity_heart_drop.c
+++ src/st/cen/entity_heart_drop.c
@@ -1,40 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "cen.h"
-// Bizarre variable - u16 here, but u8 in EntityHolyGlassesCutscene
-extern u16 D_801805D8[];
 
-void EntityHeartDrop(Entity* self) {
-    u16 index;
-    u8 value;
-    PfnEntityUpdate update;
-
-    if (!self->step) {
-        index = self->ext.heartDrop.unkB4 = self->params + 0xC0;
-        value = g_CastleFlags[(index >> 3) + HEART_FLAGS_START] >> (index & 7);
-        if (value & 1) {
-            DestroyEntity(self);
-            return;
-        }
-
-        index -= 0xC0;
-        index = D_801805D8[index];
-        if (index < 128) {
-            self->ext.heartDrop.update = EntityPrizeDrop;
-        } else {
-            self->ext.heartDrop.update = EntityEquipItemDrop;
-            index -= 128;
-        }
-        self->params = index + 0x8000;
-    } else {
-        index = self->ext.heartDrop.unkB4;
-        if (self->step < 5) {
-            if (self->hitFlags) {
-                g_CastleFlags[(index >> 3) + HEART_FLAGS_START] |=
-                    1 << (index & 7);
-                self->step = 5;
-            }
-        }
-    }
-    update = self->ext.heartDrop.update;
-    update(self);
-}
+#define HEART_DROP_CASTLE_FLAG 192
+#include "../entity_heart_drop.h"
