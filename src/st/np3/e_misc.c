b67985ad54330d89e3ad0590be4ae4fdfecf7471
Joey Murphy <tjmurphy@mit.edu>
2024-09-19 15:57:46 -0400

    NP3 use e_misc.h

--- src/st/np3/e_misc.c
+++ src/st/np3/e_misc.c
@@ -1,45 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "np3.h"
 
-#include "../entity_relic_orb.h"
-
 #define HEART_DROP_CASTLE_FLAG 0x30
-#include "../entity_heart_drop.h"
+#include "../e_misc.h"
 
-#include "../entity_message_box.h"
-
-#include "../check_coll_offsets.h"
-
-#include "../entity_unkId13.h"
-
-#include "../entity_unkId14_spawner.h"
-
-#include "../entity_unkId15_spawner.h"
-
-#include "../entity_unkId14.h"
-
-#include "../entity_unkId15.h"
-
-#include "../entity_olrox_drool.h"
-
-#include "../unk_collision_func5.h"
-
-#include "../unk_collision_func4.h"
-
-#include "../entity_intense_explosion.h"
-
-#include "../initialize_unk_entity.h"
-
-#include "../make_entity_from_id.h"
-
-#include "../make_explosions.h"
-
-#include "../entity_big_red_fireball.h"
-
-#include "../unk_recursive_primfunc_1.h"
-
-#include "../unk_recursive_primfunc_2.h"
-
-#include "../clut_lerp.h"
-
-#include "../play_sfx_positional.h"
