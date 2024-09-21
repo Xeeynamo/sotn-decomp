faf36a565edac7d5ee3d39b07a34c8ca7d0f29e0
bismurphy <tjmurphy@mit.edu>
2024-09-21 11:54:04 -0400

    Migrate ST0 to use e_misc.h (#1648)
    This completes the migration for all stages into e_misc.h. They are now
    all using it in a unified manner.
    
    It's very satisfying to have everything coming together across the
    stages like this!

--- src/st/st0/39EA8.c
+++ src/st/st0/39EA8.c
@@ -3,44 +3,6 @@
 #include "st0.h"
 #include "sfx.h"
 
-#include "../entity_relic_orb.h"
-
-#include "../check_coll_offsets.h"
-
-#include "../entity_unkId13.h"
-
-#include "../entity_unkId14_spawner.h"
-
-#include "../entity_unkId15_spawner.h"
-
-#include "../entity_unkId14.h"
-
-#include "../entity_unkId15.h"
-
-#include "../entity_olrox_drool.h"
-
-#include "../unk_collision_func5.h"
-
-#include "../unk_collision_func4.h"
-
-#include "../entity_intense_explosion.h"
-
-#include "../initialize_unk_entity.h"
-
-#include "../make_entity_from_id.h"
-
-#include "../make_explosions.h"
-
-#include "../entity_big_red_fireball.h"
-
-#include "../unk_recursive_primfunc_1.h"
-
-#include "../unk_recursive_primfunc_2.h"
-
-#include "../clut_lerp.h"
-
-#include "../play_sfx_positional.h"
-
 #include "../entity_soul_steal_orb.h"
 
 #include "../entity_enemy_blood.h"
@@ -70,11 +32,11 @@ void EntityRoomForeground(Entity* entity) {
 
 #include "../popup.h"
 
-INCLUDE_ASM("st/st0/nonmatchings/37308", EntityClouds);
+INCLUDE_ASM("st/st0/nonmatchings/39EA8", EntityClouds);
 
-INCLUDE_ASM("st/st0/nonmatchings/37308", EntityClockTower3D);
+INCLUDE_ASM("st/st0/nonmatchings/39EA8", EntityClockTower3D);
 
-INCLUDE_ASM("st/st0/nonmatchings/37308", EntityCutscenePhotograph);
+INCLUDE_ASM("st/st0/nonmatchings/39EA8", EntityCutscenePhotograph);
 
 void EntityCutscenePhotographFire(Entity* entity) {
     switch (entity->step) {
@@ -103,4 +65,4 @@ void EntityCutscenePhotographFire(Entity* entity) {
     }
 }
 
-INCLUDE_ASM("st/st0/nonmatchings/37308", func_801BC5C0);
+INCLUDE_ASM("st/st0/nonmatchings/39EA8", func_801BC5C0);
