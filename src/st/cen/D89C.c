c5fea880a3486833760cfe671f87a2af4a801597
Joey Murphy <tjmurphy@mit.edu>
2024-09-21 17:13:55 -0400

    more file shuffling

--- src/st/cen/D89C.c
+++ src/st/cen/D89C.c
@@ -4,35 +4,20 @@
 #include "game.h"
 #include "sfx.h"
 
-static u16 D_80180554[] = {
-    0x0E03, 0x0F04, 0x1004, 0x1104, 0x1204, 0x1304, 0x1403, 0x0000,
-};
+static u8 anim_1[] = {3, 14, 4, 15, 4, 16, 4, 17, 4, 18, 4, 19, 3, 20};
+static u8 anim_2[] = {3, 21, 4, 22, 4, 23, 4, 24, 4, 25, 4, 26, 3, 27};
 
-static u16 D_80180564[] = {
-    0x1503, 0x1604, 0x1704, 0x1804, 0x1904, 0x1A04, 0x1B03, 0x0000,
-};
+static u8* g_eBreakableAnimations[] = {
+    anim_1, anim_2, NULL, NULL, NULL, NULL, NULL, NULL};
+static u8 g_eBreakableHitboxes[] = {12, 20, 0, 0, 0, 0, 0, 0};
 
-u16* D_80180574[] = {
-    D_80180554, D_80180564, NULL, NULL, NULL, NULL, NULL, NULL,
-};
+static u8 g_eBreakableExplosionTypes[] = {3, 3, 0, 0, 0, 0, 0, 0};
 
-// height
-u8 D_80180594[] = {
-    12, 20, 0, 0, 0, 0, 0, 0,
-};
-
-// params
-u8 D_8018059C[] = {
-    0x3, 0x3, 0, 0, 0, 0, 0, 0,
-};
-
-// animset
-u16 D_801805A4[] = {
+static u16 g_eBreakableanimSets[] = {
     ANIMSET_OVL(1), ANIMSET_OVL(1), 0, 0, 0, 0, 0, 0,
 };
 
-// draw mode
-u8 D_801805B4[] = {
+static u8 g_eBreakableDrawModes[] = {
     DRAW_TPAGE | DRAW_TPAGE2,
     DRAW_TPAGE | DRAW_TPAGE2,
     DRAW_DEFAULT,
@@ -43,7 +28,7 @@ u8 D_801805B4[] = {
     DRAW_DEFAULT,
 };
 
-s16 D_801805BC[] = {
+static s16 xyOffsets[] = {
     -4, -4, 3, -6, 2, 9, -4, 12, 0, 2, 0, 15, 0, 31,
 };
 
@@ -52,18 +37,18 @@ void EntityBreakable(Entity* self) {
     u16* ptr;
     s32 j;
     s32 i;
-    u16 params = self->params >> 0xC;
+    u16 breakableType = self->params >> 0xC;
 
     if (self->step == 0) {
         InitializeEntity(D_80180404);
         self->zPriority = 0xB0;
-        self->drawMode = D_801805B4[params];
+        self->drawMode = g_eBreakableDrawModes[breakableType];
         newEntity = &self[1];
-        self->hitboxHeight = D_80180594[params];
-        self->animSet = D_801805A4[params];
+        self->hitboxHeight = g_eBreakableHitboxes[breakableType];
+        self->animSet = g_eBreakableanimSets[breakableType];
         DestroyEntity(newEntity);
         CreateEntityFromEntity(E_BACKGROUND_BLOCK, self, newEntity);
-        if (params != 0) {
+        if (breakableType != 0) {
             self[1].posY.i.hi -= 32;
         } else {
             self[1].posY.i.hi -= 16;
@@ -71,17 +56,17 @@ void EntityBreakable(Entity* self) {
         newEntity->params = 1;
     }
 
-    AnimateEntity(D_80180574[params], self);
+    AnimateEntity(g_eBreakableAnimations[breakableType], self);
 
     if (self->hitParams) {
         g_api.PlaySfx(SFX_CANDLE_HIT);
         newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
         if (newEntity != 0) {
             CreateEntityFromCurrentEntity(E_EXPLOSION, newEntity);
-            newEntity->params = D_8018059C[params] | 0x10;
+            newEntity->params = g_eBreakableExplosionTypes[breakableType] | 0x10;
         }
 
-        for (ptr = &D_801805BC, i = 0; i < 4; i++) {
+        for (ptr = &xyOffsets, i = 0; i < 4; i++) {
             newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
             if (newEntity != 0) {
                 CreateEntityFromEntity(0x80, self, newEntity);
@@ -89,14 +74,14 @@ void EntityBreakable(Entity* self) {
                 ptr++;
                 newEntity->posY.i.hi = newEntity->posY.i.hi + *ptr;
                 ptr++;
-                if (params != 0) {
+                if (breakableType != 0) {
                     newEntity->posY.i.hi -= 20;
                 }
                 newEntity->params = i;
             }
         }
 
-        if (params != 0) {
+        if (breakableType != 0) {
             for (j = 0; j < 3; j++) {
                 newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
                 if (newEntity != 0) {
@@ -111,116 +96,5 @@ void EntityBreakable(Entity* self) {
         }
         ReplaceBreakableWithItemDrop(self);
         DestroyEntity(&self[1]);
-    }
-}
-
-void func_8018DB18(Entity* self) {
-    Entity* newEntity;
-    Collider collider;
-    Primitive* prim;
-    s16 primIndex;
-    s32 facing;
-    s16 temp;
-    s32 temp2;
-    s32 temp3;
-
-    switch (self->step) {
-    case 0:
-        InitializeEntity(D_80180404);
-        self->zPriority = 0xB0;
-        self->drawFlags = FLAG_DRAW_ROTZ;
-        self->animCurFrame = self->params + 28;
-        facing = GetSideToPlayer() & 1;
-
-        temp = (Random() & 30) + 8;
-        self->ext.generic.unk80.modeS16.unk0 = temp;
-        if (self->facingLeft != 0) {
-            self->ext.generic.unk80.modeS16.unk0 = -temp;
-        }
-
-        if (self->params >= 4) {
-            self->ext.generic.unk80.modeS16.unk0 =
-                -self->ext.generic.unk80.modeS16.unk0;
-        }
-
-        if (facing == 0) {
-            self->velocityX = FIX(-1);
-        } else {
-            self->velocityX = FIX(1);
-        }
-
-        temp3 = 0x8000;
-        temp2 = Random() << 8;
-        self->velocityX = self->velocityX + temp3 - temp2;
-        self->velocityY = FIX(-3);
-        self->velocityY = (self->params >> 1) * 0x6000 - 0x30000;
-        if (self->params == 6) {
-            self->velocityX = 0;
-            self->velocityY = 0;
-            self->step = 2;
-        }
-
-        self->primIndex = 0;
-        if (self->params == 0) {
-            primIndex = g_api.AllocPrimitives(PRIM_GT4, 2);
-            if (primIndex != -1) {
-                prim = &g_PrimBuf[primIndex];
-                self->primIndex = primIndex;
-                self->flags |= FLAG_HAS_PRIMS;
-                UnkPolyFunc2(prim);
-                prim->tpage = 0x1A;
-                prim->clut = 0x159;
-                prim->u0 = prim->u2 = 0x40;
-                prim->u1 = prim->u3 = 0x60;
-                prim->v0 = prim->v1 = 0;
-                prim->v2 = prim->v3 = 0x20;
-                prim->next->x1 = self->posX.i.hi + 4;
-                prim->next->y0 = self->posY.i.hi - 8;
-                facing = LOH(prim->next->r2) = 32;
-                LOH(prim->next->b2) = facing;
-                prim->next->b3 = 16;
-                prim->priority = 0xB2;
-                prim->drawMode = DRAW_TPAGE2 | DRAW_TPAGE | DRAW_COLORS |
-                                 DRAW_UNK02 | DRAW_TRANSP;
-            }
-        }
-        break;
-
-    case 1:
-        MoveEntity();
-        self->rotZ += self->ext.generic.unk80.modeS16.unk0;
-        self->velocityY += FIX(0.25);
-        g_api.CheckCollision(
-            self->posX.i.hi, self->posY.i.hi + 6, &collider, 0);
-        if (collider.effects & 1) {
-            self->posY.i.hi += collider.unk18;
-            self->velocityY = -self->velocityY / 2;
-            self->velocityX -= self->velocityX / 3;
-            if (self->velocityY > FIX(-0.625)) {
-                newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
-                if (newEntity != 0) {
-                    CreateEntityFromEntity(
-                        E_INTENSE_EXPLOSION, self, newEntity);
-                    newEntity->params = 16;
-                }
-                DestroyEntity(self);
-                break;
-            }
-        }
-
-        if (self->primIndex != 0) {
-            prim = &g_PrimBuf[self->primIndex];
-            UnkPrimHelper(prim);
-            LOH(prim->next->r2) = LOH(prim->next->b2) += 4;
-            if (LOH(prim->next->r2) > 64) {
-                prim->next->b3 += 252;
-                if (prim->next->b3 == 0) {
-                    g_api.FreePrimitives(self->primIndex);
-                    self->primIndex = 0;
-                    self->flags &= ~FLAG_HAS_PRIMS;
-                }
-            }
-        }
-        break;
     }
 }
