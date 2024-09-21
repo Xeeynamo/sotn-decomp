ad736ee1063231e063525ded9d936bd888cd839e
Joey Murphy <tjmurphy@mit.edu>
2024-09-21 15:06:52 -0400

    Split foreground file

--- src/st/cen/1B274.c
+++ src/st/cen/1B274.c
@@ -4,27 +4,3 @@
 #include "../entity_soul_steal_orb.h"
 
 #include "../entity_enemy_blood.h"
-
-void EntityRoomForeground(Entity* entity) {
-    ObjInit* objInit = &D_8018125C[entity->params];
-
-    if (entity->step == 0) {
-        InitializeEntity(g_eInitGeneric2);
-        entity->animSet = objInit->animSet;
-        entity->zPriority = objInit->zPriority;
-        entity->unk5A = objInit->unk5A;
-        entity->palette = objInit->palette;
-        entity->drawFlags = objInit->drawFlags;
-        entity->drawMode = objInit->drawMode;
-        if (objInit->unkC != 0) {
-            entity->flags = objInit->unkC;
-        }
-        if (entity->params >= 5) {
-            entity->rotZ = 0x800;
-            entity->drawFlags = (u8)(entity->drawFlags | FLAG_DRAW_ROTZ);
-        }
-    }
-    AnimateEntity(objInit->animFrames, entity);
-}
-
-#include "../popup.h"
