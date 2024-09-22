7db7eca350a42eefb7a5140d3f89219f683bae75
Jed Lang <nyxojaele@hotmail.com>
2024-09-22 14:50:43 -0600

    Matched func_801A58D8

--- src/st/chi/254C4.c
+++ src/st/chi/254C4.c
@@ -114,7 +114,31 @@ void EntityOlroxDrool(Entity* self) {
 //TODO: Can't quite use this include due to tiny difference
 //#include "../entity_olrox_drool.h"
 
-INCLUDE_ASM("st/chi/nonmatchings/254C4", func_801A58D8);    // [Duplicate]
+// [Duplicate]
+// func_801A58D8
+bool func_801A58D8(Point16* unk) {
+    Collider collider;
+
+    FallEntity();
+    g_CurrentEntity->posX.val += g_CurrentEntity->velocityX;
+    g_CurrentEntity->posY.val += g_CurrentEntity->velocityY;
+
+    if (g_CurrentEntity->velocityY >= 0) {
+        s16 posX = g_CurrentEntity->posX.i.hi;
+        s16 posY = g_CurrentEntity->posY.i.hi;
+        posX += unk->x;
+        posY += unk->y;
+        g_api.CheckCollision(posX, posY, &collider, 0);
+        if (collider.effects & EFFECT_SOLID) {
+            g_CurrentEntity->posY.i.hi += collider.unk18;
+            g_CurrentEntity->velocityY = -g_CurrentEntity->velocityY / 2;
+            if (g_CurrentEntity->velocityY > FIX(-1.0)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 INCLUDE_ASM("st/chi/nonmatchings/254C4", func_801A59D4);    // [Duplicate]
 
