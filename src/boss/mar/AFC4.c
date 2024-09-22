95d24621c2376dd2adc9d5cc6d4ffef10dc750ff
Kelly MacNeill <kelly@pyromuffin.com>
2024-09-21 18:35:47 -0700

    fix symbols and compare with psp version.

--- src/boss/mar/AFC4.c
+++ src/boss/mar/AFC4.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include <stage.h>
 
+#include "mar.h"
+
 extern Dialogue g_Dialogue;
 
 #include "../../st/cutscene_unk1.h"
@@ -21,50 +23,49 @@ INCLUDE_ASM("boss/mar/nonmatchings/AFC4", func_us_8018B74C);
 
 INCLUDE_ASM("boss/mar/nonmatchings/AFC4", EntityMariaCutscene);
 
-extern s32 D_80097400[];
-extern u16 D_80180A60;
-extern u16 D_us_8019AF2A[];
-
+// func_psp_092467F8
 void func_us_8018C90C(Entity* self) {
     Entity* player;
+    Tilemap* tilemap; // unused but shows up in the psp code.
 
+    tilemap = &g_Tilemap;
     player = &PLAYER;
 
     switch (self->step) {
     case 0:
-        InitializeEntity(&D_80180A60);
+        InitializeEntity(D_80180A60);
         D_8003C8B8 = 0;
-        D_80097400[0] = 1;
+        g_unkGraphicsStruct.unk0 = 1;
         g_Player.padSim = PAD_RIGHT;
         g_Player.D_80072EFC = 1;
-        return;
+        break;
     case 1:
         if ((player->posX.i.hi >= 0x21) && !(g_CutsceneFlags & 1)) {
             g_CutsceneFlags |= 1;
         }
         if (player->posX.i.hi >= 0xB1) {
             g_Player.padSim = PAD_LEFT;
-            self->step += 1;
+            self->step++;
         } else {
             g_Player.padSim = PAD_RIGHT;
         }
         g_Player.D_80072EFC = 1;
-        return;
+        break;
     case 2:
         if (g_CutsceneFlags & 2) {
             D_8003C8B8 = 1;
 
-            if (D_80097400[0]) {
-                D_80097400[0] = 0;
+            if (g_unkGraphicsStruct.unk0) {
+                g_unkGraphicsStruct.unk0 = 0;
             }
 
             DestroyEntity(self);
-
-            D_us_8019AF2A[0] ^= 1;
+            g_Statues[LEFT_STATUE] ^= 1; // suspicious, everywhere else we're
+                                         // treating this like a boolean.
         }
 
         g_Player.padSim = 0;
         g_Player.D_80072EFC = 1;
-        return;
+        break;
     }
 }
