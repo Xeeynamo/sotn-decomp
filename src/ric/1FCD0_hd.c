7db9288950cc4f8edebe383bd1340c39bedc2d05
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-09-23 12:55:33 +0100

    1FCD0

--- src/ric/1FCD0_hd.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("ric/nonmatchings/1FCD0_hd", func_8015BCD0);
-
-INCLUDE_ASM("ric/nonmatchings/1FCD0_hd", RicHandleSlideKick);
-
-INCLUDE_ASM("ric/nonmatchings/1FCD0_hd", RicHandleBladeDash);
