245cb4638bc75ca03f3eb6f80b362668b872db5e
Onenutmcgee <Onenutmcgee@users.noreply.github.com>
2024-09-26 13:23:10 -0500

    Decompile for ServantDesc data for tt_001

--- src/servant/tt_001/F84.c
+++ src/servant/tt_001/F84.c
@@ -4,6 +4,63 @@
 
 extern s32 g_IsServantDestroyed;
 
+#ifndef VERSION_PSP
+/* func_us_80171864 uses pointers by a different naming scheme forcing
+ * a breakout of ServantDesc.  Once that function is decompiled,
+ * these structs should be able to be removed and the declaration
+ * set to a  ServantDesc */
+typedef struct {
+    void (*Init)(s32 arg0);
+    void (*Update)(Entity* self);
+    void (*Unk08)(Entity* self);
+    void (*Unk0C)(/*?*/);
+    void (*Unk10)(/*?*/);
+    void (*Unk14)(/*?*/);
+    void (*Unk18)(/*?*/);
+    void (*Unk1C)(/*?*/);
+    void (*Unk20)(/*?*/);
+    void (*Unk24)(/*?*/);
+} ServantDesc_Brk1;
+
+typedef struct {
+    void (*Unk28)(Entity* self);
+} ServantDesc_Brk2;
+
+typedef struct {
+    void (*Unk2C)(/*?*/);
+    void (*Unk30)(/*?*/);
+    void (*Unk34)(/*?*/);
+    void (*Unk38)(/*?*/);
+    void (*Unk3C)(Entity* self);
+} ServantDesc_Brk3;
+
+void func_us_80171624(s32 arg0);
+void func_us_80171864(Entity* self);
+void func_us_801720A4(Entity* self);
+void func_us_801720AC(void);
+void func_us_801720B4(void);
+void func_us_801720BC(void);
+void func_us_801720C4(void);
+void func_us_801720CC(void);
+void func_us_801720D4(void);
+void func_us_801720DC(void);
+void func_us_801720E4(Entity* self);
+void func_us_8017246C(void);
+void func_us_801728EC(void);
+void func_us_801728F4(void);
+void func_us_801728FC(void);
+void DestroyServantEntity(Entity* self);
+
+ServantDesc_Brk1 g_ServantDesc_Brk1 = {
+    func_us_80171624, func_us_80171864, func_us_801720A4, func_us_801720AC,
+    func_us_801720B4, func_us_801720BC, func_us_801720C4, func_us_801720CC,
+    func_us_801720D4, func_us_801720DC};
+ServantDesc_Brk2 g_ServantDesc_Brk2 = {func_us_801720E4};
+ServantDesc_Brk3 g_ServantDesc_Brk3 = {
+    func_us_8017246C, func_us_801728EC, func_us_801728F4, func_us_801728FC,
+    DestroyServantEntity};
+#endif
+
 INCLUDE_ASM("servant/tt_001/nonmatchings/F84", func_us_80170F84);
 
 INCLUDE_ASM("servant/tt_001/nonmatchings/F84", func_us_80171284);
@@ -18,7 +75,7 @@ INCLUDE_ASM("servant/tt_001/nonmatchings/F84", func_us_80171624);
 
 INCLUDE_ASM("servant/tt_001/nonmatchings/F84", func_us_80171864);
 
-void func_us_801720A4(void) {}
+void func_us_801720A4(Entity* self) {}
 
 void func_us_801720AC(void) {}
 
