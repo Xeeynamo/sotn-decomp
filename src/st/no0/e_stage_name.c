2e4ebb994e590dc8e6a527126f80b31f65769e4a
bismurphy <tjmurphy@mit.edu>
2024-09-29 19:53:54 -0400

    Add NO0 (Marble Gallery) overlay (#1691)
    This adds this overlay to the project.
    
    Importantly, we use a new tool (tools/auto_dedupe_new_overlay) to
    process the new overlay and automatically split its C files according to
    known duplicate files. As of now, this does not do any decompiling (the
    whole overlay is kept as INCLUDE_ASM), but it automatically does all the
    file splits and code copying needed. Hopefully this reduces some amount
    of duplicated work.
    
    To be clear, this new script is not in any build chain, but is meant to
    be used after `make-config` to take the overlay from being a single
    giant lump of `us.c` to being individual files, for the sake of easier
    deduplicating. It will likely need some revision for future overlays,
    but at least it should be a good start toward reducing tedious work.
    
    I'll leave this overlay in place like this for about a week in case
    there are any newcomers who would like to try de-duplicating some of
    these files and decompiling the new functions; after that week I'll get
    into doing things myself.

---
+++ src/st/no0/e_stage_name.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no0/nonmatchings/e_stage_name", StageNamePopupHelper);
+
+INCLUDE_RODATA("st/no0/nonmatchings/e_stage_name", D_us_801C14A8);
+
+INCLUDE_ASM("st/no0/nonmatchings/e_stage_name", EntityStageNamePopup);
