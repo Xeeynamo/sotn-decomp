cf280c66f5ce0463834aaacf1dc56f4991979d80
Aleksandr Shabelnikov <mistermidi@gmail.com>
2024-09-30 22:56:38 +0200

    Overlay NO0: Deduplicate and split st_update data (#1703)
    Co-authored-by: SynaMax <djsynamax@gmail.com>

--- src/st/no0/st_update.c
+++ src/st/no0/st_update.c
@@ -1,8 +1,2 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no0/nonmatchings/st_update", Random);
-
-INCLUDE_ASM("st/no0/nonmatchings/st_update", Update);
-
-INCLUDE_ASM("st/no0/nonmatchings/st_update", UpdateStageEntities);
+#include "../st_update.h"
