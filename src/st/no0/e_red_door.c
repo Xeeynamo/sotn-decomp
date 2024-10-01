5c72a3ba380e53b792e9df00d9592a067a5671fa
Aleksandr Shabelnikov <mistermidi@gmail.com>
2024-10-01 15:03:08 +0200

    NO0: Deduplicate and split e_red_door data

--- src/st/no0/e_red_door.c
+++ src/st/no0/e_red_door.c
@@ -1,6 +1,2 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no0/nonmatchings/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/no0/nonmatchings/e_red_door", EntityRedDoor);
+#include "../e_red_door.h"
