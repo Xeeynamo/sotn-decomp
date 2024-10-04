0121536f35b8e57ffd59749545dd4470bda1b120
Aleksandr Shabelnikov <mistermidi@gmail.com>
2024-10-04 21:05:44 +0200

    NO0: decompile function func_us_801CC750

--- src/st/no0/4C750.c
+++ src/st/no0/4C750.c
@@ -2,7 +2,58 @@
 #include "common.h"
 #include "no0.h"
 
-INCLUDE_ASM("st/no0/nonmatchings/4C750", func_us_801CC750);
+// INCLUDE_ASM("st/no0/nonmatchings/4C750", func_us_801CC750);
+
+extern void func_us_801CC8F8(Entity*);
+extern u16 D_us_80180A88;
+
+void func_us_801CC750(Entity* self) {
+    Entity* entityPtr;
+    s16 i;
+    Primitive* prim;
+    s32 primIndex;
+
+    if (self->step != 0) {
+        return;
+    }
+
+    InitializeEntity(&D_us_80180A88);
+    primIndex = g_api.AllocPrimitives(PRIM_GT4, 9);
+    if (primIndex != -1) {
+        prim = &g_PrimBuf[primIndex];
+        i = -0x10;
+        self->primIndex = primIndex;
+        self->flags |= FLAG_HAS_PRIMS;
+        while (prim != NULL) {
+            prim->tpage = 0xF;
+            prim->clut = 0x2A;
+            prim->u0 = prim->u2 = 0xC0;
+            prim->u1 = prim->u3 = 0xE0;
+            prim->v0 = prim->v1 = 0x80;
+            prim->v2 = prim->v3 = 0xB0;
+            prim->x0 = prim->x2 = i;
+            prim->x1 = prim->x3 = i + 0x20;
+            prim->y0 = prim->y1 = 0x91;
+            prim->y2 = prim->y3 = 0xC1;
+            prim->priority = 0;
+            prim->drawMode = DRAW_DEFAULT;
+            prim = prim->next;
+            i += 0x1E;
+        }
+    }
+
+    entityPtr = (Entity*)((char*)self + 0xBC);
+
+    for (i = -0x10; i < 0x130; i += 0x60) {
+        DestroyEntity(entityPtr);
+        entityPtr->entityId = 0x16;
+        entityPtr->pfnUpdate = func_us_801CC8F8;
+        entityPtr->posY.i.hi = 0x8E;
+        entityPtr->posX.i.hi = i;
+
+        entityPtr = (Entity*)((char*)entityPtr + 0xBC);
+    }
+}
 
 extern s32 D_80097408[];
 
