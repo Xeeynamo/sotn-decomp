330653a09afaaa2b00175bd114f518674486bb8d
Joey Murphy <tjmurphy@mit.edu>
2024-10-07 12:57:59 -0400

    Decompile NO3 func_801BB548

--- src/st/no3/377D4.c
+++ src/st/no3/377D4.c
@@ -1828,7 +1828,120 @@ void EntityFallingRock(Entity* self) {
     }
 }
 
-INCLUDE_ASM("st/no3/nonmatchings/377D4", func_801BB548);
+extern CVECTOR D_8018136C;
+extern s16 D_80181370[];
+
+// See also: UnkRecursivePrimFunc1. Strong overlap, but also some different
+// logic.
+Primitive* func_801BB548(
+    SVECTOR* arg0, SVECTOR* arg1, SVECTOR* arg2, SVECTOR* arg3, Primitive* arg4,
+    s32 arg5, Primitive* arg6, u8* arg7) {
+    s32 i;
+    Primitive* prim1;
+    s32 vecIdx1;
+    s32 vecIdx2;
+    s32 vecIdx3;
+    s32 vecIdx4;
+    s16* vecIndices;
+    SVECTOR* vectors;
+    uvPair* uv_values;
+
+    long unused;
+    long interp;
+    s32 rotTransResult;
+    Primitive* nextArg6;
+
+    if (arg6 == NULL) {
+        return NULL;
+    }
+    prim1 = (Primitive*)arg7;
+    arg7 += sizeof(Primitive);
+    vectors = (SVECTOR*)arg7;
+    arg7 += sizeof(SVECTOR) * 9;
+    uv_values = (uvPair*)arg7;
+    arg7 += sizeof(uvPair) * 10; // This is effectively arg7 = arg7->next, since
+                                 // this covers the full size of arg7.
+
+    vectors[0] = *arg0;
+    vectors[2] = *arg1;
+    vectors[6] = *arg2;
+    vectors[8] = *arg3;
+
+    vectors[1].vx = (vectors[0].vx + vectors[2].vx + 1) >> 1;
+    vectors[1].vy = (vectors[0].vy + vectors[2].vy + 1) >> 1;
+    vectors[1].vz = (vectors[0].vz + vectors[2].vz + 1) >> 1;
+    vectors[7].vx = (vectors[6].vx + vectors[8].vx + 1) >> 1;
+    vectors[7].vy = (vectors[6].vy + vectors[8].vy + 1) >> 1;
+    vectors[7].vz = (vectors[6].vz + vectors[8].vz + 1) >> 1;
+    vectors[3].vx = (vectors[0].vx + vectors[6].vx + 1) >> 1;
+    vectors[3].vy = (vectors[0].vy + vectors[6].vy + 1) >> 1;
+    vectors[3].vz = (vectors[0].vz + vectors[6].vz + 1) >> 1;
+    vectors[5].vx = (vectors[2].vx + vectors[8].vx + 1) >> 1;
+    vectors[5].vy = (vectors[2].vy + vectors[8].vy + 1) >> 1;
+    vectors[5].vz = (vectors[2].vz + vectors[8].vz + 1) >> 1;
+    vectors[4].vx = (vectors[3].vx + vectors[5].vx + 1) >> 1;
+    vectors[4].vy = (vectors[3].vy + vectors[5].vy + 1) >> 1;
+    vectors[4].vz = (vectors[3].vz + vectors[5].vz + 1) >> 1;
+
+    uv_values[0] = *(uvPair*)&arg4->u0;
+    uv_values[2] = *(uvPair*)&arg4->u1;
+    uv_values[6] = *(uvPair*)&arg4->u2;
+    uv_values[8] = *(uvPair*)&arg4->u3;
+    uv_values[1].u = (uv_values[0].u + uv_values[2].u + 1) >> 1;
+    uv_values[1].v = (uv_values[0].v + uv_values[2].v + 1) >> 1;
+    uv_values[7].u = (uv_values[6].u + uv_values[8].u + 1) >> 1;
+    uv_values[7].v = (uv_values[6].v + uv_values[8].v + 1) >> 1;
+    uv_values[3].u = (uv_values[0].u + uv_values[6].u + 1) >> 1;
+    uv_values[3].v = (uv_values[0].v + uv_values[6].v + 1) >> 1;
+    uv_values[5].u = (uv_values[2].u + uv_values[8].u + 1) >> 1;
+    uv_values[5].v = (uv_values[2].v + uv_values[8].v + 1) >> 1;
+    uv_values[4].u = (uv_values[3].u + uv_values[5].u + 1) >> 1;
+    uv_values[4].v = (uv_values[3].v + uv_values[5].v + 1) >> 1;
+
+    *prim1 = *arg4;
+    D_8018136C.cd = arg4->type;
+    vecIndices = &D_80181370[0];
+    for (i = 0; i < 4; i++) {
+        vecIdx1 = *vecIndices++;
+        vecIdx2 = *vecIndices++;
+        vecIdx3 = *vecIndices++;
+        vecIdx4 = *vecIndices++;
+
+        RotTransPers(&vectors[vecIdx1], (long*)&prim1->x0, &interp, &unused);
+        DpqColor(&D_8018136C, interp, &prim1->r0);
+
+        RotTransPers(&vectors[vecIdx2], (long*)&prim1->x1, &interp, &unused);
+        DpqColor(&D_8018136C, interp, &prim1->r1);
+
+        RotTransPers(&vectors[vecIdx3], (long*)&prim1->x2, &interp, &unused);
+        DpqColor(&D_8018136C, interp, &prim1->r2);
+
+        rotTransResult = RotTransPers(
+            &vectors[vecIdx4], (long*)&prim1->x3, &interp, &unused);
+        DpqColor(&D_8018136C, interp, &prim1->r3);
+
+        *(uvPair*)&prim1->u0 = uv_values[vecIdx1];
+        *(uvPair*)&prim1->u1 = uv_values[vecIdx2];
+        *(uvPair*)&prim1->u2 = uv_values[vecIdx3];
+        *(uvPair*)&prim1->u3 = uv_values[vecIdx4];
+        if (arg5 == 1) {
+            if (rotTransResult > 0) {
+                nextArg6 = arg6->next;
+                *arg6 = *prim1;
+                arg6->next = nextArg6;
+                arg6 = arg6->next;
+                if (arg6 == NULL) {
+                    return NULL;
+                }
+            }
+        } else {
+            arg6 = func_801BB548(
+                &vectors[vecIdx1], &vectors[vecIdx2], &vectors[vecIdx3],
+                &vectors[vecIdx4], prim1, arg5 - 1, arg6, arg7);
+        }
+    }
+    return arg6;
+}
 
 // sky animation during death cutscene
 void EntityDeathSkySwirl(Entity* self) {
