cc901f660c5be8726fbda0e57f3fb93a217d7abb
Onenutmcgee <Onenutmcgee@users.noreply.github.com>
2024-10-08 11:53:15 -0500

    Fix for msvc

--- src/servant/tt_000/10E8.c
+++ src/servant/tt_000/10E8.c
@@ -1057,3 +1057,20 @@ void func_80173C24(void) {}
 #endif
 
 #include "../process_event.h"
+
+#include "../create_event_entity.h"
+
+#include "../is_movement_allowed.h"
+
+#ifndef VERSION_PSP
+#include "../check_all_entities_valid.h"
+#endif
+
+#include "../servant_unk0.h"
+
+#ifdef VERSION_PSP
+extern ServantDesc bat_ServantDesc;
+void func_092EC220(void) {
+    memcpy((u8*)&D_8D1DC40, (u8*)&bat_ServantDesc, sizeof(ServantDesc));
+}
+#endif
