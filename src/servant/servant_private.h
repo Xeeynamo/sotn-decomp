86f13ff62897a342b06f12f60bf157861cd7e47b
Onenutmcgee <Onenutmcgee@users.noreply.github.com>
2024-10-08 12:30:54 -0500

    cleaning up servant_private.h static functions

--- src/servant/servant_private.h
+++ src/servant/servant_private.h
@@ -2,43 +2,29 @@
 #include <servant.h>
 
 #ifndef VERSION_PSP
-// all of the shared functions need to be marked static
-static s32 AccumulateTowardZero(s32 arg0, s32 arg1);
-static s16 CalculateAngleToEntity(Entity* entity, s16 targetX, s16 targetY);
-static s32 CalculateDistance(Entity* entity, s32 targetX, s32 targetY);
-static s32 CheckAllEntitiesValid(void);
-static s32 CheckEntityValid(Entity* entity);
-static void CreateEventEntity(Entity* entityParent, s32 entityId, s32 params);
-static void DestroyServantEntity(Entity* self);
-static s16 GetTargetPositionWithDistanceBuffer(
-    s16 currentX, s16 targetX, s16 distanceBuffer);
-static s32 IsMovementAllowed(s32 arg0);
-static void ProcessEvent(Entity* self, bool resetEvent);
-static void ProcessSfxState(Entity* entity);
-static Entity* SearchForEntityInRange(s32 rangeIndex, s32 entityId);
-static void ServantInit(InitializeMode mode);
-static s32 ServantUnk0(void);
-static u32 ServantUpdateAnim(
-    Entity* self, s8* frameProps, AnimationFrame** frames);
-static void SetEntityAnimation(Entity* entity, AnimationFrame* anim);
-static void UpdateServantDefault(Entity* self);
+#define STATIC static
 #else
-s32 AccumulateTowardZero(s32 arg0, s32 arg1);
-s16 CalculateAngleToEntity(Entity* entity, s16 targetX, s16 targetY);
-s32 CalculateDistance(Entity* entity, s32 targetX, s32 targetY);
-s32 CheckAllEntitiesValid(void);
-s32 CheckEntityValid(Entity* entity);
-void CreateEventEntity(Entity* entityParent, s32 entityId, s32 params);
-void DestroyServantEntity(Entity* self);
-s16 GetTargetPositionWithDistanceBuffer(
-    s16 currentX, s16 targetX, s16 distanceBuffer);
-s32 IsMovementAllowed(s32 arg0);
-void ProcessEvent(Entity* self, bool resetEvent);
-void ProcessSfxState(Entity* entity);
-Entity* SearchForEntityInRange(s32 rangeIndex, s32 entityId);
-void ServantInit(InitializeMode mode);
-s32 ServantUnk0(void);
-u32 ServantUpdateAnim(Entity* self, s8* frameProps, AnimationFrame** frames);
-void SetEntityAnimation(Entity* entity, AnimationFrame* anim);
-void UpdateServantDefault(Entity* self);
+#define STATIC
 #endif
+
+// all of the shared functions need to be marked static for PC build due to name
+// collisions
+STATIC s32 AccumulateTowardZero(s32 arg0, s32 arg1);
+STATIC s16 CalculateAngleToEntity(Entity* entity, s16 targetX, s16 targetY);
+STATIC s32 CalculateDistance(Entity* entity, s32 targetX, s32 targetY);
+STATIC s32 CheckAllEntitiesValid(void);
+STATIC s32 CheckEntityValid(Entity* entity);
+STATIC void CreateEventEntity(Entity* entityParent, s32 entityId, s32 params);
+STATIC void DestroyServantEntity(Entity* self);
+STATIC s16 GetTargetPositionWithDistanceBuffer(
+    s16 currentX, s16 targetX, s16 distanceBuffer);
+STATIC s32 IsMovementAllowed(s32 arg0);
+STATIC void ProcessEvent(Entity* self, bool resetEvent);
+STATIC void ProcessSfxState(Entity* entity);
+STATIC Entity* SearchForEntityInRange(s32 rangeIndex, s32 entityId);
+STATIC void ServantInit(InitializeMode mode);
+STATIC s32 ServantUnk0(void);
+STATIC u32 ServantUpdateAnim(
+    Entity* self, s8* frameProps, AnimationFrame** frames);
+STATIC void SetEntityAnimation(Entity* entity, AnimationFrame* anim);
+STATIC void UpdateServantDefault(Entity* self);
