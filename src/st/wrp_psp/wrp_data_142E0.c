3054dae1f888ed5414f2400a7fdee2ed1dca7c84
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-10-10 20:58:22 +0100

    psp fix

--- src/st/wrp_psp/wrp_data_142E0.c
+++ src/st/wrp_psp/wrp_data_142E0.c
@@ -19,7 +19,7 @@ Overlay g_StageOverlay = {
     /* 0x14 */ &g_SpriteBanks,
     /* 0x18 */ &g_Cluts,
     /* 0x1C */ NULL,
-    /* 0x20 */ OVL_EXPORT(rooms_layers),
+    /* 0x20 */ (RoomDef*)OVL_EXPORT(rooms_layers),
     /* 0x24 */ &g_EntityGfxs,
     /* 0x28 */ UpdateStageEntities,
     /* 0x2C */ 0x00000000,
