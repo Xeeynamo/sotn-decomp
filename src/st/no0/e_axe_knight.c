57e85aabaf982598d3f6861764b8ed45e4a41abc
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-10-12 15:06:07 +1100

    Dedupe `no0` `e_axe_knight`

--- src/st/no0/e_axe_knight.c
+++ src/st/no0/e_axe_knight.c
@@ -1,13 +1,17 @@
-#include "common.h"
+#include "no0.h"
 
-INCLUDE_ASM("st/no0/nonmatchings/e_axe_knight", func_801C3F9C);
+extern s16 sensors_move[];
+extern s16 sensors_ground[4][2];
+extern s16 dead_particle_pos[][2];
+extern u16 unused[];
+extern u8 anim_walk[];
+extern u8 anim_throw_duck[];
+extern u8 anim_throw_stand[];
+extern u8 anim_die[];
+extern u8 hitboxes[][4];
+extern u8 hitbox_lookup[];
+extern u8 steps[];
+extern u32 init_velocity_x[];
+extern u32 init_velocity_y[];
 
-INCLUDE_ASM("st/no0/nonmatchings/e_axe_knight", func_801C4198);
-
-INCLUDE_ASM("st/no0/nonmatchings/e_axe_knight", func_801C4550);
-
-INCLUDE_ASM("st/no0/nonmatchings/e_axe_knight", EntityAxeKnight);
-
-INCLUDE_ASM("st/no0/nonmatchings/e_axe_knight", EntityAxeKnightRotateAxe);
-
-INCLUDE_ASM("st/no0/nonmatchings/e_axe_knight", EntityAxeKnightThrowingAxe);
+#include "../entity_axe_knight.h"
