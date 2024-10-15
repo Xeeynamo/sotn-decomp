42e8c4d32c4c065e0eb415fee75cb9cdc274ec49
Jed Lang <nyxojaele@hotmail.com>
2024-10-15 14:25:52 -0600

    Commented on some data usage in ObjInit2

--- include/game.h
+++ include/game.h
@@ -768,7 +768,7 @@ typedef struct {
     /* 0x08 */ u16 drawFlags;
     /* 0x0A */ u16 drawMode;
     /* 0x0C */ u32 unkC;
-    /* 0x10 */ u8* unk10;
+    /* 0x10 */ u8* unk10;   // AnimFrames
 } ObjInit2; // size = 0x14
 
 typedef struct GpuBuffer { // also called 'DB' in the PSY-Q samples
