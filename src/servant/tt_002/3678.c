7472433bb18cd37c89855ab28f3e45516f458740
Onenutmcgee <Onenutmcgee@users.noreply.github.com>
2024-10-17 11:02:06 -0500

    Moving to named c file

---
+++ src/servant/tt_002/3678.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_801739D0);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80173B14);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80173BD0);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80173D60);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_801746E8);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80174998);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80174F0C);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_801753E4);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80175730);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80175A78);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80175DBC);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80176178);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80176504);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80176B6C);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80176C98);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80177380);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80177958);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80177AC4);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80177F64);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80177F84);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_8017865C);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80178804);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80178870);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_801788A8);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80178928);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_8017896C);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_801789E0);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80178A30);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80178C08);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80178FDC);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80179098);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_801791B0);
+
+INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_8017925C);
+
+INCLUDE_RODATA("servant/tt_002/nonmatchings/3678", func_us_80173994);
+
+INCLUDE_RODATA("servant/tt_002/nonmatchings/3678", func_us_801739C8);
