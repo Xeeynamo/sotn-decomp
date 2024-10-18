4214fcc4203d7bc35b125aac492d531682922d9f
H.M. Burger <92937601+Onenutmcgee@users.noreply.github.com>
2024-10-17 19:31:20 -0500

    Adding tt_002 to gamemeta.js (#1803)

--- src/gamemeta.js
+++ src/gamemeta.js
@@ -30,7 +30,7 @@ const gameMetadata = {
             "id": "us",
             "name": "PlayStation 1 US",
             "overlays": [
-                "dra", "ric", "weapon", "stcen", "stdre", "stmad", "stno0", "stno3", "stnp3", "stnz0", "stsel", "stst0", "stwrp", "strwrp", "bomar", "tt_000", "tt_001"
+                "dra", "ric", "weapon", "stcen", "stdre", "stmad", "stno0", "stno3", "stnp3", "stnz0", "stsel", "stst0", "stwrp", "strwrp", "bomar", "tt_000", "tt_001", "tt_002"
             ]
         },
         {
@@ -109,6 +109,10 @@ const gameMetadata = {
         "tt_001": {
             "name": "SERVANT/TT_001.BIN",
             "desc": "Familiar Ghost"
+        },
+        "tt_002": {
+            "name": "SERVANT/TT_002.BIN",
+            "desc": "Familiar Faerie"
         },
     }
 };
