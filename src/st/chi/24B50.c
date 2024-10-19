f6357d746648a04c566d49ef1782893e207c0ae9
Jed Lang <nyxojaele@hotmail.com>
2024-10-19 16:39:04 -0600

    Separated en_heart_drop into its own file

--- src/st/chi/24B50.c
+++ src/st/chi/24B50.c
@@ -1,51 +1,12 @@
 /*
- * File: en_relic_orb.c
+ * File: en_heart_drop.c
  * Overlay: CHI
- * Description: ENTITY - Relic: Spirit Orb. Displays damage
+ * Description: ENTITY - Heart prize
  */
 
 #include "chi.h"
 
 extern u16 g_InitializeData0[];
-extern u16 D_801809E4[];
-
-// [Duplicate]
-// [Almost duplicate of CEN.EntityHeartDrop -- 0xC0 immediate is 0xB0 here]
-// E_HEART_DROP
-// func_801A4A28
-void EntityHeartDrop(Entity* self) {
-    u16 temp_a0;
-    u16 temp_a0_2;
-    u16 var_a0;
-
-    if (self->step == 0) {
-        temp_a0 = self->params + 0xB0;
-        self->ext.generic.unkB4 = temp_a0;
-        if ((D_8003BEEC[temp_a0 >> 3] >> (temp_a0 & 7)) & 1) {
-            DestroyEntity(self);
-            return;
-        }
-        temp_a0_2 = temp_a0 - 0xB0;
-        var_a0 = D_801809E4[temp_a0_2];
-        if (var_a0 < 128) {
-            self->ext.generic.unkB8.unkFuncB8 = EntityPrizeDrop;
-        } else {
-            self->ext.generic.unkB8.unkFuncB8 = EntityEquipItemDrop;
-            var_a0 -= 128;
-        }
-        self->params = var_a0 + 0x8000;
-    } else {
-        temp_a0_2 = self->ext.generic.unkB4;
-        if (self->step < 5) {
-            if (self->hitFlags != 0) {
-                var_a0 = self->ext.generic.unkB4;
-                D_8003BEEC[temp_a0_2 >> 3] |= 1 << (var_a0 & 7);
-                self->step = 5;
-            }
-        }
-    }
-    self->ext.generic.unkB8.unkFuncB8(self);
-}
 
 // [Duplicate]
 // func_801A4B50: [E_SAVE_GAME_POPUP] EntityMessageBox
