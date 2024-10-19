76390954ebb82cce9e8c6a5d79b16309eb82bf39
Jed Lang <nyxojaele@hotmail.com>
2024-10-19 15:31:44 -0600

    Split 1B4DC.c into en_trigger_volume.c and en_breakable.c

--- src/st/chi/en_breakable.c
+++ src/st/chi/en_breakable.c
@@ -1,90 +1,10 @@
 /*
- * File: 1B3FC.c
+ * File: en_breakable.c
  * Overlay: CHI
- * Description: Abandoned Mine
+ * Description: ENTITY - Breakable Object
  */
 
 #include "chi.h"
-
-// D_80180798
-u8 D_80180798[] = {32, 32, 32, 32, 32, 32, 80, 32};
-// D_801807A0
-u8 D_801807A0[] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-// D_801807A8
-u16 D_801807A8[] = {
-    0x0000, 0x0000, 0x0600, 0x0100, 0x0000, 0x0000, 0x0600, 0x01FC,
-    0x0000, 0x00FC, 0x0600, 0x01FC, 0x0000, 0x0000, 0x0600, 0x01FC,
-    0x0000, 0x00FC, 0x0600, 0x0300, 0x0000, 0x00FC, 0x0600, 0x01FC,
-    0x0000, 0x00FC, 0x0600, 0x0300, 0x0000, 0x0200, 0x0600, 0x0300,
-    0x0000, 0x01FC, 0x0500, 0x02FC, 0x0000, 0x0000, 0x0500, 0x0400,
-    0x0000, 0x0000, 0x0500, 0x0400, 0x0000, 0x01FC, 0x0500, 0x02FC,
-    0x00F0, 0x01FC, 0x0310, 0x02FC, 0x00F0, 0x01FC, 0x0310, 0x02FC,
-};
-
-extern u16 EntityInit_80180658;
-extern u8 D_80180798[];     // Trigger long dimension size
-extern u8 D_801807A0[];     // Trigger long dimension is horizontal
-extern u16 D_801807A8[];
-
-// E_TRIGGER_VOLUME
-// func_8019B4DC
-void EntityTriggerVolume(Entity* entity) {
-    s32 temp_v0;
-    u16* temp_v0_2;
-    u16 temp_s1 = entity->params;
-    u16 var_v1;
-    u16 unk;
-
-    entity->unk6D[0] = 0;
-
-    if (entity->step != 0) {
-        switch (temp_s1) {
-        case 4:
-        case 5:
-            if (g_Tilemap.x != 0) {
-                return;
-            }
-            break;
-
-        case 6:
-            if (g_pads->pressed & PAD_TRIANGLE) {
-                g_Tilemap.x = 0;
-                g_Tilemap.width = 1280;
-                entity->step++;
-                return;
-            }
-            break;
-        }
-
-        if (entity->unk44 != 0) {
-            temp_v0 = GetSideToPlayer();
-            var_v1 = entity->ext.generic.unk7C.s;
-            if (var_v1 != 0) {
-                var_v1 = (temp_v0 & 2) * 2;
-            } else {
-                var_v1 = (temp_v0 & 1) * 4;
-            }
-            unk = 8;
-            temp_s1 = (temp_s1 * unk) + var_v1;
-            temp_v0_2 = &D_801807A8[temp_s1];
-            g_Tilemap.x = *(temp_v0_2++);
-            g_Tilemap.y = *(temp_v0_2++);
-            g_Tilemap.width = *(temp_v0_2++);
-            g_Tilemap.height = *(temp_v0_2++);
-        }
-    } else {
-        InitializeEntity(&EntityInit_80180658);
-        entity->ext.generic.unk7C.s = D_801807A0[temp_s1];
-        if (entity->ext.generic.unk7C.s != 0) {
-            entity->hitboxWidth = D_80180798[temp_s1];
-            entity->hitboxHeight = 16;
-        } else {
-            entity->hitboxWidth = 16;
-            entity->hitboxHeight = D_80180798[temp_s1];
-        }
-    }
-}
 
 // D_80180818
 static u16 D_80180818[] = {
@@ -97,31 +17,31 @@ static u16 D_80180828[] = {
 };
 
 // D_80180838
-u16* D_80180838[] = {
+static u16* D_80180838[] = {
     D_80180818, D_80180828, NULL, NULL, NULL, NULL, NULL, NULL,
 };
 
 // [height?]
 // D_80180858
-u8 D_80180858[] = {
+static u8 D_80180858[] = {
     12, 20, 0, 0, 0, 0, 0, 0,
 };
 
 // [params?]
 // D_80180860
-u8 D_80180860[] = {
+static u8 D_80180860[] = {
     3, 3, 0, 0, 0, 0, 0, 0,
 };
 
 // [animset?]
 // D_80180868
-u16 D_80180868[] = {
+static u16 D_80180868[] = {
     ANIMSET_OVL(1), ANIMSET_OVL(1), 0, 0, 0, 0, 0, 0,
 };
 
 // [draw mode?]
 // D_80180878
-u8 D_80180878[] = {
+static u8 D_80180878[] = {
     DRAW_TPAGE | DRAW_TPAGE2,
     DRAW_TPAGE | DRAW_TPAGE2,
     DRAW_DEFAULT,
@@ -133,7 +53,7 @@ u8 D_80180878[] = {
 };
 
 // D_80180880
-s16 D_80180880[] = {
+static s16 D_80180880[] = {
     -4, -4, 3, -6, 2, 9, -4, 12, 0, 2, 0, 15, 0, 31,
 };
 
---
+++ src/st/chi/en_trigger_volume.c
@@ -0,0 +1,87 @@
+/*
+ * File: en_trigger_volume.c
+ * Overlay: CHI
+ * Description: ENTITY - Trigger Volume
+ */
+
+#include "chi.h"
+
+// D_80180798
+static u8 D_80180798[] = {32, 32, 32, 32, 32, 32, 80, 32};
+// D_801807A0
+static u8 D_801807A0[] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+// D_801807A8
+static u16 D_801807A8[] = {
+    0x0000, 0x0000, 0x0600, 0x0100, 0x0000, 0x0000, 0x0600, 0x01FC,
+    0x0000, 0x00FC, 0x0600, 0x01FC, 0x0000, 0x0000, 0x0600, 0x01FC,
+    0x0000, 0x00FC, 0x0600, 0x0300, 0x0000, 0x00FC, 0x0600, 0x01FC,
+    0x0000, 0x00FC, 0x0600, 0x0300, 0x0000, 0x0200, 0x0600, 0x0300,
+    0x0000, 0x01FC, 0x0500, 0x02FC, 0x0000, 0x0000, 0x0500, 0x0400,
+    0x0000, 0x0000, 0x0500, 0x0400, 0x0000, 0x01FC, 0x0500, 0x02FC,
+    0x00F0, 0x01FC, 0x0310, 0x02FC, 0x00F0, 0x01FC, 0x0310, 0x02FC,
+};
+
+extern u16 EntityInit_80180658;
+extern u8 D_80180798[];     // Trigger long dimension size
+extern u8 D_801807A0[];     // Trigger long dimension is horizontal
+extern u16 D_801807A8[];
+
+// E_TRIGGER_VOLUME
+// func_8019B4DC
+void EntityTriggerVolume(Entity* entity) {
+    s32 temp_v0;
+    u16* temp_v0_2;
+    u16 temp_s1 = entity->params;
+    u16 var_v1;
+    u16 unk;
+
+    entity->unk6D[0] = 0;
+
+    if (entity->step != 0) {
+        switch (temp_s1) {
+        case 4:
+        case 5:
+            if (g_Tilemap.x != 0) {
+                return;
+            }
+            break;
+
+        case 6:
+            if (g_pads->pressed & PAD_TRIANGLE) {
+                g_Tilemap.x = 0;
+                g_Tilemap.width = 1280;
+                entity->step++;
+                return;
+            }
+            break;
+        }
+
+        if (entity->unk44 != 0) {
+            temp_v0 = GetSideToPlayer();
+            var_v1 = entity->ext.generic.unk7C.s;
+            if (var_v1 != 0) {
+                var_v1 = (temp_v0 & 2) * 2;
+            } else {
+                var_v1 = (temp_v0 & 1) * 4;
+            }
+            unk = 8;
+            temp_s1 = (temp_s1 * unk) + var_v1;
+            temp_v0_2 = &D_801807A8[temp_s1];
+            g_Tilemap.x = *(temp_v0_2++);
+            g_Tilemap.y = *(temp_v0_2++);
+            g_Tilemap.width = *(temp_v0_2++);
+            g_Tilemap.height = *(temp_v0_2++);
+        }
+    } else {
+        InitializeEntity(&EntityInit_80180658);
+        entity->ext.generic.unk7C.s = D_801807A0[temp_s1];
+        if (entity->ext.generic.unk7C.s != 0) {
+            entity->hitboxWidth = D_80180798[temp_s1];
+            entity->hitboxHeight = 16;
+        } else {
+            entity->hitboxWidth = 16;
+            entity->hitboxHeight = D_80180798[temp_s1];
+        }
+    }
+}
