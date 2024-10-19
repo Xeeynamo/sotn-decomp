b82d82b697929c6537e7ac432f4106445fe04f54
Jed Lang <nyxojaele@hotmail.com>
2024-10-19 15:54:25 -0600

    Separated entity creation functions into ut_create_entity

--- src/st/chi/en_red_door.c
+++ src/st/chi/en_red_door.c
@@ -6,23 +6,6 @@
 
 #include "chi.h"
 
-// func_8019FA64: CreateEntityFromLayout
-// func_8019FB28: CreateEntityWhenInVerticalRange
-// func_8019FC40: CreateEntityWhenInHorizontalRange
-// func_8019FD58: FindFirstEntityToTheRight
-// func_8019FDA4: FindFirstEntityToTheLeft
-// func_8019FDFC: CreateEntitiesToTheRight
-// func_8019FEF8: CreateEntitiesToTheLeft
-// func_801A000C: FindFirstEntityAbove
-// func_801A0058: FindFirstEntityBelow
-// func_801A00B0: CreateEntitiesAbove
-// func_801A01AC: CreateEntitiesBelow
-// func_801A02C0: InitRoomEntities
-// func_801A0438: UpdateRoomPosition
-// func_801A04EC: CreateEntityFromCurrentEntity
-// func_801A0560: CreateEntityFromEntity
-#include "../create_entity.h"
-
 // D_80180E0C: g_eRedDoorUV
 // func_801A05DC: EntityIsNearPlayer
 // func_801A0654: [E_RED_DOOR] EntityRedDoor
---
+++ src/st/chi/ut_create_entity.c
@@ -0,0 +1,24 @@
+/*
+ * File: ut_create_entity.c
+ * Overlay: CHI
+ * Description: UTILITY - Entity creation functions
+ */
+
+#include "chi.h"
+
+// func_8019FA64: CreateEntityFromLayout
+// func_8019FB28: CreateEntityWhenInVerticalRange
+// func_8019FC40: CreateEntityWhenInHorizontalRange
+// func_8019FD58: FindFirstEntityToTheRight
+// func_8019FDA4: FindFirstEntityToTheLeft
+// func_8019FDFC: CreateEntitiesToTheRight
+// func_8019FEF8: CreateEntitiesToTheLeft
+// func_801A000C: FindFirstEntityAbove
+// func_801A0058: FindFirstEntityBelow
+// func_801A00B0: CreateEntitiesAbove
+// func_801A01AC: CreateEntitiesBelow
+// func_801A02C0: InitRoomEntities
+// func_801A0438: UpdateRoomPosition
+// func_801A04EC: CreateEntityFromCurrentEntity
+// func_801A0560: CreateEntityFromEntity
+#include "../create_entity.h"
