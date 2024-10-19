bdc6118090c0d8434cf556a5eeb6c6196da917dc
Jed Lang <nyxojaele@hotmail.com>
2024-10-19 17:11:09 -0600

    Renamed 27D78 -> en_bottom_corner_text

---
+++ src/st/chi/en_bottom_corner_text.c
@@ -0,0 +1,10 @@
+/*
+ * File: en_bottom_corner_text.c
+ * Overlay: CHI
+ * Description: ENTITY - Message that appears in the bottom corner of the screen
+ */
+
+#include "chi.h"
+
+// func_801A7D78: BottomCornerText
+#include "../bottom_corner_text.h"
