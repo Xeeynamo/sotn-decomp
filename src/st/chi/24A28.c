b589cd0c235afe1330f542958507b99863df6581
Jed Lang <nyxojaele@hotmail.com>
2024-10-19 16:20:56 -0600

    Separated en_relic_orb into its own file

--- src/st/chi/24A28.c
+++ src/st/chi/24A28.c
@@ -1,24 +1,12 @@
 /*
- * File: 23F58.c
+ * File: en_relic_orb.c
  * Overlay: CHI
- * Description: Abandoned Mine
+ * Description: ENTITY - Relic: Spirit Orb. Displays damage
  */
 
 #include "chi.h"
 
 extern u16 g_InitializeData0[];
-
-// D_80181314: g_RelicOrbTexts
-// D_80181318: g_RelicOrbTextBg1EY
-// D_80181328: g_RelicOrbTextBg1SY
-// D_80181338: g_RelicOrbTextBg2SY
-// D_80181348: g_RelicOrbTextBg2EY
-// D_80181358: g_RelicOrbSparkleX
-// D_80181368: g_RelicOrbSparkleY
-// [Duplicate]
-// func_801A3F58: [E_RELIC_ORB] EntityRelicOrb
-#include "../entity_relic_orb.h"
-
 extern u16 D_801809E4[];
 
 // [Duplicate]
