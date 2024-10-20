5cd8624f0b499a819a8adbd7c61a2fa9c0b5d6b2
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-10-20 19:04:41 +1100

    Decompile `no0` `UpdateClockHands`

--- src/st/no0/clock_room.c
+++ src/st/no0/clock_room.c
@@ -6,7 +6,16 @@ INCLUDE_ASM("st/no0/nonmatchings/clock_room", func_us_801CCAAC);
 
 INCLUDE_ASM("st/no0/nonmatchings/clock_room", func_us_801CCBE4);
 
-INCLUDE_ASM("st/no0/nonmatchings/clock_room", func_us_801CCC2C);
+void UpdateClockHands(Entity* self, PlayerStatus* status) {
+    // self + 5 is the minute hand
+    self += 5;
+    self->ext.clockRoom.hand = status->timerMinutes * 60;
+
+    // self + 6 is the hour hand
+    self += 1;
+    self->ext.clockRoom.hand =
+        (status->timerHours * 300) + (status->timerMinutes * 5);
+}
 
 INCLUDE_ASM("st/no0/nonmatchings/clock_room", func_us_801CCC74);
 
