3cec7b1f65b934038352cf2d3896b3631696f0e6
Joey Murphy <tjmurphy@mit.edu>
2024-10-21 11:06:16 -0400

    name cen cutscene enum id

--- src/st/cen/cen.h
+++ src/st/cen/cen.h
@@ -32,7 +32,7 @@ typedef enum EntityIDs{
     /* 0x13 */ E_ID_13,
     /* 0x14 */ E_ID_14,
     /* 0x15 */ E_GREY_PUFF,
-    /* 0x16 */ E_ID_16,
+    /* 0x16 */ E_CUTSCENE,
     /* 0x17 */ E_PLATFORM,
     /* 0x18 */ E_MARIA,
     /* 0x19 */ E_ROOM_DARKNESS,
