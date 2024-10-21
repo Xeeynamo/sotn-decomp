fa4a0ad7193d3460050f11f9f5ab3740b89aab52
Jed Lang <nyxojaele@hotmail.com>
2024-10-20 23:23:39 -0600

    Used duplicate for en_heart_drop

--- src/st/chi/en_heart_drop.c
+++ src/st/chi/en_heart_drop.c
@@ -8,46 +8,5 @@
 
 #define HEART_DROP_CASTLE_FLAG 0xB0
 
-extern u16 g_InitializeData0[];
-extern u16 g_HeartDropArray[];
-
-// [Duplicate]
-// [Almost duplicate of CEN.EntityHeartDrop -- 0xC0 immediate is 0xB0 here]
-// E_HEART_DROP
-// func_801A4A28
-void EntityHeartDrop(Entity* self) {
-    u16 index;
-    u8 value;
-    PfnEntityUpdate update;
-
-    if (!self->step) {
-        index = self->ext.heartDrop.unkB4 =
-            self->params + HEART_DROP_CASTLE_FLAG;
-        value = g_CastleFlags[(index >> 3) + HEART_FLAGS_START] >> (index & 7);
-        if (value & 1) {
-            DestroyEntity(self);
-            return;
-        }
-
-        index -= HEART_DROP_CASTLE_FLAG;
-        index = g_HeartDropArray[index];
-        if (index < 128) {
-            self->ext.heartDrop.update = EntityPrizeDrop;
-        } else {
-            self->ext.heartDrop.update = EntityEquipItemDrop;
-            index -= 128;
-        }
-        self->params = index + 0x8000;
-    } else {
-        index = self->ext.heartDrop.unkB4;
-        if (self->step < 5) {
-            if (self->hitFlags) {
-                g_CastleFlags[(index >> 3) + HEART_FLAGS_START] |=
-                    1 << (index & 7);
-                self->step = 5;
-            }
-        }
-    }
-    update = self->ext.heartDrop.update;
-    update(self);
-}
+// func_801A4A28: [E_HEART_DROP] EntityHeartDrop
+#include "../entity_heart_drop.h"
