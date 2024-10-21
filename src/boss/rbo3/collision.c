a0449079b745a77f687d401193fa5c32dac8df44
Aleksandr Shabelnikov <mistermidi@gmail.com>
2024-10-21 20:50:46 +0200

    RBO3 st_update deduplication (#1816)

--- src/boss/rbo3/collision.c
+++ src/boss/rbo3/collision.c
@@ -1,6 +1,2 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("boss/rbo3/nonmatchings/collision", HitDetection);
-
-INCLUDE_ASM("boss/rbo3/nonmatchings/collision", EntityDamageDisplay);
+#include "../../st/collision.h"
