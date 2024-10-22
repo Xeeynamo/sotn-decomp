17c3f30ddbe0aa3d092d06fed3cfdb17331cfa93
Jed Lang <nyxojaele@hotmail.com>
2024-10-22 09:28:37 -0600

    Renamed entity -> self in en_breakable_with_debris

--- src/st/en_breakable_with_debris.h
+++ src/st/en_breakable_with_debris.h
@@ -76,7 +76,12 @@ void EntityBreakableWithDebris(Entity* self) {
 // params: Index of breakable debris to use
 //         (>= 4) Considered a "tall" breakable debris
 //         (== 6) The "last standing" debris, that doesn't move
-void EntityBreakableDebris(Entity* entity) {
+void EntityBreakableDebris(Entity* self) {
+    typedef enum Step {
+        INIT = 0,
+        UPDATE = 1,
+    };
+
     Collider collider;
     Primitive* prim;
     s16 primIndex;
@@ -87,52 +92,52 @@ void EntityBreakableDebris(Entity* entity) {
     Entity* newEntity;
     s32 temp;
 
-    switch (entity->step) {
-    case 0:
+    switch (self->step) {
+    case INIT:
         InitializeEntity(&g_eBreakableWithDebrisInit);
-        entity->zPriority = 0x70;
-        entity->hitboxState = 0;
-        entity->drawFlags = FLAG_DRAW_ROTZ;
-        entity->animCurFrame = entity->params + 28;
+        self->zPriority = 0x70;
+        self->hitboxState = 0;
+        self->drawFlags = FLAG_DRAW_ROTZ;
+        self->animCurFrame = self->params + 28;
         facing = GetSideToPlayer() & 1;
 
         angle = (Random() & 0x1E) + 8;
-        entity->ext.breakableDebris.angle = angle;
-        if (entity->facingLeft) {
-            entity->ext.breakableDebris.angle = -angle;
+        self->ext.breakableDebris.angle = angle;
+        if (self->facingLeft) {
+            self->ext.breakableDebris.angle = -angle;
         }
 
         // Check for upper "tall" debris
-        if (entity->params >= 4) {
-            entity->ext.breakableDebris.angle = -entity->ext.breakableDebris.angle;
+        if (self->params >= 4) {
+            self->ext.breakableDebris.angle = -self->ext.breakableDebris.angle;
         }
 
         if (facing == 0) {
-            entity->velocityX = FIX(-1);
+            self->velocityX = FIX(-1);
         } else {
-            entity->velocityX = FIX(1);
+            self->velocityX = FIX(1);
         }
 
         temp = 0x8000;
         temp_v0_2 = Random() << 8;
-        entity->velocityX = entity->velocityX + temp - temp_v0_2;
-        entity->velocityY = FIX(-3);
-        entity->velocityY = (entity->params >> 1) * 0x6000 - 0x30000;
+        self->velocityX = self->velocityX + temp - temp_v0_2;
+        self->velocityY = FIX(-3);
+        self->velocityY = (self->params >> 1) * 0x6000 - 0x30000;
 
         // "Last standing" debris just sits there (tall breakables only)
-        if (entity->params == 6) {
-            entity->velocityX = 0;
-            entity->velocityY = 0;
-            entity->step = 2;
+        if (self->params == 6) {
+            self->velocityX = 0;
+            self->velocityY = 0;
+            self->step = 2;
         }
 
-        entity->primIndex = 0;
-        if (entity->params == 0) {
+        self->primIndex = 0;
+        if (self->params == 0) {
             primIndex = g_api.AllocPrimitives(PRIM_GT4, 2);
             if (primIndex != -1) {
                 prim = &g_PrimBuf[primIndex];
-                entity->primIndex = primIndex;
-                entity->flags |= FLAG_HAS_PRIMS;
+                self->primIndex = primIndex;
+                self->flags |= FLAG_HAS_PRIMS;
 
                 UnkPolyFunc2(prim, primIndex);
 
@@ -143,8 +148,8 @@ void EntityBreakableDebris(Entity* entity) {
                 prim->v0 = prim->v1 = 0;
                 prim->v2 = prim->v3 = 0x20;
 
-                prim->next->x1 = entity->posX.i.hi + 4;
-                prim->next->y0 = entity->posY.i.hi - 8;
+                prim->next->x1 = self->posX.i.hi + 4;
+                prim->next->y0 = self->posY.i.hi - 8;
                 LOH(prim->next->r2) = 0x20;
                 LOH(prim->next->b2) = 0x20;
                 prim->next->b3 = 0x10;
@@ -154,36 +159,37 @@ void EntityBreakableDebris(Entity* entity) {
             }
         }
         break;
+
     case 1:
         MoveEntity();
-        entity->rotZ += entity->ext.breakableDebris.angle;
-        entity->velocityY += FIX(0.25);
-        g_api.CheckCollision(entity->posX.i.hi, entity->posY.i.hi + 6, &collider, 0);
+        self->rotZ += self->ext.breakableDebris.angle;
+        self->velocityY += FIX(0.25);
+        g_api.CheckCollision(self->posX.i.hi, self->posY.i.hi + 6, &collider, 0);
         if (collider.effects & 1) {
-            entity->posY.i.hi += collider.unk18;
-            entity->velocityY = -entity->velocityY / 2;
-            entity->velocityX -= entity->velocityX / 3;
-            if (entity->velocityY > FIX(-0.625)) {
+            self->posY.i.hi += collider.unk18;
+            self->velocityY = -self->velocityY / 2;
+            self->velocityX -= self->velocityX / 3;
+            if (self->velocityY > FIX(-0.625)) {
                 newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
                 if (newEntity != 0) {
-                    CreateEntityFromEntity(E_INTENSE_EXPLOSION, entity, newEntity);
+                    CreateEntityFromEntity(E_INTENSE_EXPLOSION, self, newEntity);
                     newEntity->params = 16;
                 }
-                DestroyEntity(entity);
+                DestroyEntity(self);
                 break;
             }
         }
 
-        if (entity->primIndex != 0) {
-            prim = &g_PrimBuf[entity->primIndex];
+        if (self->primIndex != 0) {
+            prim = &g_PrimBuf[self->primIndex];
             UnkPrimHelper(prim);
             LOH(prim->next->r2) = LOH(prim->next->b2) += 4;
             if (LOH(prim->next->r2) > 64) {
                 prim->next->b3 += 252;
                 if (prim->next->b3 == 0) {
-                    g_api.FreePrimitives(entity->primIndex);
-                    entity->primIndex = 0;
-                    entity->flags &= ~FLAG_HAS_PRIMS;
+                    g_api.FreePrimitives(self->primIndex);
+                    self->primIndex = 0;
+                    self->flags &= ~FLAG_HAS_PRIMS;
                 }
             }
         }
