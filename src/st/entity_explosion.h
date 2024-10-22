3296fd1a5816afa16da9cb4b6f98c2578aa831ce
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-10-22 12:50:07 +1100

    Remove unused `AnimateEntityMissingProto` code

--- src/st/entity_explosion.h
+++ src/st/entity_explosion.h
@@ -3,11 +3,6 @@ extern u16 g_EInitParticle[];
 extern s32 g_ExplosionYVelocities[];
 extern u8* g_ExplosionAnimations[];
 
-// when `AnimateEntity` is not declared, the default declaration
-// is used which assumes a 32 bit return type. This prototype
-// can be forced by setting MISSING_ANIMATE_ENTITY_PROTOTYPE
-typedef u32 (*AnimateEntityMissingProto)(u8[], Entity*);
-
 void EntityExplosion(Entity* entity) {
     if (!entity->step) {
         InitializeEntity(g_EInitParticle);
@@ -28,12 +23,7 @@ void EntityExplosion(Entity* entity) {
     } else {
         entity->posY.val += entity->velocityY;
 
-#ifdef MISSING_ANIMATE_ENTITY_PROTOTYPE
-        if (!((AnimateEntityMissingProto)AnimateEntity)(
-                g_ExplosionAnimations[entity->params], entity)) {
-#else
         if (!AnimateEntity(g_ExplosionAnimations[entity->params], entity)) {
-#endif
             DestroyEntity(entity);
         }
     }
