f705102345d6e364d1bc41ac522c01931e5ccd7b
Jed Lang <nyxojaele@hotmail.com>
2024-10-22 23:49:51 -0600

    Renamed the palette binary data symbols

--- src/st/chi/dt_header.c
+++ src/st/chi/dt_header.c
@@ -32,28 +32,29 @@ Overlay OVL_EXPORT(Overlay) = {
 // D_80180040: spriteBanks
 #include "sprite_banks.h"
 
-extern u16* D_80189FE0[0x30];   // Unused
-extern u16* D_8018A040[0x100];
-extern u16* D_8018A240[0x60];   // Unused
-extern u16* D_8018A300[0x40];
-extern u16* D_8018A380[0x20];
-extern u16* D_8018A3C0[0x40];
-extern u16* D_8018A440[0x290];
+// Palettes -- These addresses are to the binary data
+extern u16* Palette_80189FE0[0x30];   // Unused (US)
+extern u16* Palette_8018A040[0x100];
+extern u16* Palette_8018A240[0x60];   // Unused (US)
+extern u16* Palette_8018A300[0x40];
+extern u16* Palette_8018A380[0x20];
+extern u16* Palette_8018A3C0[0x40];
+extern u16* Palette_8018A440[0x290];
 
 // D_801800A0
-static u16** D_801800A0[] = {
+static u16** PaletteCommand0[] = {
     MAKE_PAL_OP(PAL_BULK_COPY, 0),
-    PAL_BULK(0x2000, D_8018A300),
-    PAL_BULK(0x2040, D_8018A380),
-    PAL_BULK(0x2060, D_8018A3C0),
-    PAL_BULK(0x20A0, D_8018A440),
-    PAL_BULK(0x2E00, D_8018A040),
+    PAL_BULK(0x2000, Palette_8018A300),
+    PAL_BULK(0x2040, Palette_8018A380),
+    PAL_BULK(0x2060, Palette_8018A3C0),
+    PAL_BULK(0x20A0, Palette_8018A440),
+    PAL_BULK(0x2E00, Palette_8018A040),
     PAL_TERMINATE(),
 };
 
 // D_801800E4
 static void* Cluts[] = {
-    D_801800A0,
+    PaletteCommand0,
 };
 
 // D_801800E8: layers
