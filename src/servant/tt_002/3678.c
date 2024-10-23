8fc83ca7fa8134db929538ab70d36e2ae94babab
= <tyler.hanson88@gmail.com>
2024-10-22 21:58:25 -0500

    Got it most of the way, need to figure something out

--- src/servant/tt_002/3678.c
+++ src/servant/tt_002/3678.c
@@ -2,6 +2,14 @@
 #include "common.h"
 #include <servant.h>
 
+extern s32 D_us_801792DC;
+extern u16 D_us_801722E8[];
+extern Entity thisFamiliar;
+extern s32 D_us_801792E0;
+extern FamiliarStats s_FaerieStats;
+extern s32 D_us_8017931C;
+extern s32 D_us_80179320;
+
 INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_801739D0);
 
 // This is a duplicate CreateEventEntity which is lower in the file, but we need
@@ -36,7 +44,95 @@ INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80173BD0);
 
 INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80173D60);
 
-INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_801746E8);
+void func_us_801746E8(InitializeMode mode)
+{
+    u16* src;
+    u16* dst;
+    RECT rect;
+    s32 i;
+    SpriteParts** spriteBanks;
+    Entity* entity;  
+
+    D_us_801792DC = g_Servant;
+
+    if ((mode == MENU_SWITCH_SERVANT) || (mode == MENU_SAME_SERVANT)) {
+        ProcessEvent(NULL, true);
+        if (mode == MENU_SAME_SERVANT) {
+            return;
+        }
+    }
+    
+    dst = &g_Clut[0x2800];
+    src = D_us_801722E8;
+    for (i = 0; i < 0x100; i++) {
+        *dst++ = *src++;
+    }
+
+    rect.x = 0;
+    rect.w = 0x100;
+    rect.h = 1;
+    rect.y = 0xF4;
+
+    dst = &g_Clut[0x2800];
+    LoadImage(&rect, (u_long*)dst);
+    
+    spriteBanks = g_api.o.spriteBanks;
+    spriteBanks += 20;
+    *spriteBanks = (SpriteParts*)g_ServantSpriteParts;
+
+    entity = &thisFamiliar;
+    
+    DestroyEntity(entity);
+    entity->unk5A = 0x6C;
+    entity->palette = 0x140;
+    entity->animSet = ANIMSET_OVL(20);
+    entity->zPriority = PLAYER.zPriority - 2;
+    entity->facingLeft = PLAYER.facingLeft;
+    entity->params = 0;
+    
+    if (mode == MENU_SWITCH_SERVANT) {
+        if (D_8003C708.flags & 0x20 || D_8003C708.flags & 0x40) {
+            entity->entityId = 0xD1;
+        } else {
+            entity->entityId = 0xD8;
+        }
+        entity->posX.val = FIX(128);
+        entity->posY.val = FIX(-32);
+    } else {
+        entity->entityId = 0xD1;
+        if (D_8003C708.flags & 0x20) {
+            if(ServantUnk0())
+            {
+                entity->posX.val = FIX(192);
+            }
+            else
+            {
+               entity->posX.val = FIX(64);
+            }
+            entity->posY.val = FIX(160);
+        } else {
+            entity->posX.val = PLAYER.posX.val + (PLAYER.facingLeft ? FIX(24) : FIX(-24));
+            entity->posY.val = PLAYER.posY.val + FIX(-32);
+        }
+    } 
+     
+    D_us_801792E0 = (s32) entity->zPriority;
+    g_api.GetServantStats(entity, 0, 0, &s_FaerieStats);
+    // There is a better way to do this, but I don't know what it is
+    entity = (Entity*)((int*)entity + 0x2F);
+    DestroyEntity(entity);
+    entity->entityId = 0xD9;
+    entity->unk5A = 0x6C;
+    entity->palette = 0x140;
+    entity->animSet = -0x7FEC;
+    entity->zPriority = PLAYER.zPriority - 3;
+    entity->facingLeft = PLAYER.facingLeft;
+    entity->params = 0;
+    
+    D_us_8017931C = 0;
+    D_us_80179320 = 0;
+    g_api.GetServantStats(entity, 0, 0, &s_FaerieStats);
+}
 
 INCLUDE_ASM("servant/tt_002/nonmatchings/3678", func_us_80174998);
 
