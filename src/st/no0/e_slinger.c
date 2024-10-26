edcdaa7cab21601850b5f39b2d636f7936921a56
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-10-26 19:26:42 +1100

    Decompile `no0` `EntitySlingerPieces`

--- src/st/no0/e_slinger.c
+++ src/st/no0/e_slinger.c
@@ -1,7 +1,36 @@
-#include "common.h"
+#include "no0.h"
 
 INCLUDE_ASM("st/no0/nonmatchings/e_slinger", func_us_801D75E4);
 
 INCLUDE_ASM("st/no0/nonmatchings/e_slinger", func_us_801D7670);
 
-INCLUDE_ASM("st/no0/nonmatchings/e_slinger", func_us_801D7B40);
+extern u16 g_EInitSlingerPieces[];
+extern u16 anim_bone_rot[];
+
+// Bone parts that rotate and fall down when killed
+// This is a duplicate of EntitySkeletonPieces and EntityBoneScimitarParts
+void EntitySlingerPieces(Entity* self) {
+    if (self->step) {
+        self->ext.skeleton.explosionTimer--;
+        if (self->ext.skeleton.explosionTimer & 0xFF) {
+            self->rotZ += anim_bone_rot[self->params];
+            FallEntity();
+            MoveEntity();
+            return;
+        }
+
+        self->entityId = E_EXPLOSION;
+        self->pfnUpdate = (PfnEntityUpdate)EntityExplosion;
+        self->params = 0;
+        self->step = 0;
+        return;
+    }
+
+    InitializeEntity(g_EInitSlingerPieces);
+    self->drawFlags = FLAG_DRAW_ROTZ;
+    self->animCurFrame = self->params + 15;
+
+    if (self->facingLeft) {
+        self->velocityX = -self->velocityX;
+    }
+}
