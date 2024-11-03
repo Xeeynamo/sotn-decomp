133e32e023237c26bdb775d4e98786431141c26a
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-11-03 17:33:07 +1100

    Decompile `no0` `CheckFieldCollisionY`
    Co-authored-by: Aleksandr Shabelnikov <mistermidi@gmail.com>

--- src/st/no0/e_flea_man.c
+++ src/st/no0/e_flea_man.c
@@ -1,10 +1,37 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
 #include "no0.h"
 
-INCLUDE_ASM("st/no0/nonmatchings/e_flea_man", func_us_801DC64C);
+// Checks for collisions while the entity is moving downward, updating position
+// if collision occurs. This is similar to other function CheckFieldCollision
+void CheckFieldCollisionY(s16 hitSensors[], s16 sensorCount) {
+    Collider collider;
+    s32 velocityY;
+    s16 currentPosX;
+    s16 currentPosY;
+    s16 i;
 
-void func_us_801DC754(void) {
+    velocityY = g_CurrentEntity->velocityY;
+    if (velocityY >= 0) {
+        return;
+    }
+
+    currentPosX = g_CurrentEntity->posX.i.hi;
+    currentPosY = g_CurrentEntity->posY.i.hi;
+
+    for (i = 0; i < sensorCount; i++) {
+        currentPosX += *hitSensors++;
+        currentPosY += *hitSensors++;
+
+        g_api.CheckCollision(currentPosX, currentPosY, &collider, 0);
+
+        if (collider.effects & EFFECT_SOLID) {
+            g_CurrentEntity->posY.i.hi += collider.unk20;
+            break;
+        }
+    }
+}
+
+void SetFacingLeft(void) {
     g_CurrentEntity->facingLeft = (GetSideToPlayer() & 1) ^ 1;
 }
 
