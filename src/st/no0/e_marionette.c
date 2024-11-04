db20f9fbf75b25c153ba5fa049658a7814310066
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-11-04 12:38:57 +1100

    Split Marionette functions into separate file

---
+++ src/st/no0/e_marionette.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+#include "no0.h"
+
+INCLUDE_ASM("st/no0/nonmatchings/e_marionette", func_us_801D6254);
+
+INCLUDE_ASM("st/no0/nonmatchings/e_marionette", func_us_801D6474);
+
+INCLUDE_ASM("st/no0/nonmatchings/e_marionette", func_us_801D66F8);
