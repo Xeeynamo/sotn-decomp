2765a6eb80a85e123952b2468523f12310e7b395
AlChar <charronalexandre@hotmail.com>
2024-11-05 17:22:26 -0500

    Decompile func_us_80177F84 (#1878)
    PSX : https://decomp.me/scratch/9GOEx
    
    I haven't tested if it matches on PSP

--- include/unkstruct.h
+++ include/unkstruct.h
@@ -113,6 +113,21 @@ typedef struct {
 } Unkstruct_80102CD8;
 
 typedef struct {
+    /* 0x00 */ s16 count;
+    /* 0x02 */ s16 unk2;
+    /* 0x04 */ s16 unk4;
+    /* 0x06 */ s16 unk6;
+    /* 0x08 */ s16 w;
+    /* 0x0A */ s16 h;
+    /* 0x0C */ s16 r;
+    /* 0x0E */ s16 g;
+    /* 0x10 */ s16 b;
+    /* 0x12 */ u16 priority;
+    /* 0x14 */ s16 drawMode;
+    /* 0x16 */ u32 flags;
+} Unkstruct_801724CC; // size = 0x1A
+
+typedef struct {
     /* 0x0 */ s16 animSet;
     /* 0x2 */ s16 unk2; // Entity::unk5A
     /* 0x4 */ u16 palette;
