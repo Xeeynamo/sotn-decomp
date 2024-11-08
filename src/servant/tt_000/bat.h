c377cda35a9306965861064455e728e784d341e8
Onenutmcgee <tyler.hanson88@gmail.com>
2024-11-07 21:18:46 -0600

    Adding BatAbilityValues struct

---
+++ src/servant/tt_000/bat.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+#include "servant.h"
+
+typedef struct {
+    s32 delayFrames;
+    s32 maxAngle;
+    s32 additionalBatCount;
+    s32 minimumEnemyHp;
+    s32 makeBadAttacks;
+} BatAbilityValues;
