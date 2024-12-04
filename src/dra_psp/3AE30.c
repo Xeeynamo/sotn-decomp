2e28bdf7f4442c9423478be9ab4025c6def6d08b
Joey Murphy <tjmurphy@mit.edu>
2024-12-03 19:31:03 -0500

    Remove file split

--- src/dra_psp/3AE30.c
+++ src/dra_psp/3AE30.c
@@ -66,3 +66,9 @@ s32 CheckMoveDirection(void) {
     }
     return 0;
 }
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3AE30", func_psp_09117A90);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3AE30", SetSpeedX);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3AE30", func_psp_09117B78);
