83afce7528a5ac0b123d7f5bc6afbc23583ab396
Aleksandr Shabelnikov <mistermidi@gmail.com>
2024-12-05 12:17:29 +0100

    `no1` dedupe has begun (#1973)

--- src/st/no1/collision.c
+++ src/st/no1/collision.c
@@ -1,6 +1,2 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no1/nonmatchings/collision", HitDetection);
-
-INCLUDE_ASM("st/no1/nonmatchings/collision", EntityDamageDisplay);
+#include "../collision.h"
---
+++ src/st/no1/no1.h
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "stage.h"
+
+#define STAGE_IS_NO1
+
+#define OVL_EXPORT(x) NO1_##x
