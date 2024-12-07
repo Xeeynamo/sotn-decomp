2b006442ac6f67baf5f5d63723414de1da3dba9a
Luciano Ciccariello <xeeynamo@hotmail.com>
2024-12-07 23:21:32 +0000

    ST0 header clean-up

--- src/st/st0/st0.h
+++ src/st/st0/st0.h
@@ -52,47 +52,17 @@ void EntityExplosion(Entity*);
 void EntityExplosionVariants(Entity* entity);
 void EntityGreyPuff(Entity* entity);
 
-extern const char* D_801A7984;
-extern const char* D_801A7990;
-extern const char* D_801A799C;
-
-// *** Initializers ***
 extern EInit OVL_EXPORT(EInitBreakable);
 extern EInit g_EInitParticle;
-extern EInit g_EInitCutscene; // Init EntityCutscene
+extern EInit g_EInitCutscene;
 extern EInit g_EInitCommon;
-extern EInit g_EInit3DObject; // Init EntityStageTitleCard
+extern EInit g_EInit3DObject;
 extern EInit g_EInitDracula;
-extern EInit g_EInitDraculaFireball;     // Init EntityDraculaFireball
-extern EInit g_EInitDraculaFinalForm;    // Init EntityDraculaFinalForm
-extern EInit g_EInitDraculaMegaFireball; // Init EntityDraculaMegaFireball
+extern EInit g_EInitDraculaFireball;
+extern EInit g_EInitDraculaFinalForm;
+extern EInit g_EInitDraculaMegaFireball;
 extern EInit g_EInitSecretStairs;
 
-extern ObjInit D_80180638[];
-extern u16 D_80181CAC[];
-extern u16 D_80181CDC[];
-extern u32 D_80181CF0[];
-extern s8 D_801A7B7C;
 extern Point32 D_801BEB64[];
 extern s32 D_801C2578;
-
-// *** EntityDracula properties END ***
-
-extern s16 D_80181990[];
 extern u16 g_EInitObtainable[];
-extern u16 D_80181D74[];
-extern s16 D_80181EB0[];
-extern u32 D_80181EC0[];
-extern u16 D_80181F04[];
-
-extern s8 D_801A7BDC;
-extern Point32 D_801BEB64[];
-extern s32 D_801C2578;
-
-// *** EntitySoulStealOrb properties START ***
-
-extern u16 g_ESoulStealOrbAngles[];
-extern s16 g_ESoulStealOrbSprt[];
-extern u8 g_ESoulStealOrbAnim[];
-
-// *** EntitySoulStealOrb properties END ***
