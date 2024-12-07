bbaab679b4d958fc7dd77ddc087503ca45476bfa
Aleksandr Shabelnikov <mistermidi@gmail.com>
2024-12-07 09:06:25 +0100

    Some `no1` dedupe/split (#1976)

---
+++ src/st/no1/e_collect.c
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", PrizeDropFall);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", PrizeDropFall2);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", CollectHeart);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", CollectGold);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", CollectSubweapon);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", CollectHeartVessel);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", CollectLifeVessel);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", DestroyCurrentEntity);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48A8);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48B0);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48B8);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48C0);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48C8);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48D0);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48D8);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48E0);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48E8);
+
+INCLUDE_RODATA("st/no1/nonmatchings/e_collect", D_us_801B48F0);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", EntityPrizeDrop);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", EntityExplosion);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", BlinkItem);
+
+INCLUDE_ASM("st/no1/nonmatchings/e_collect", EntityEquipItemDrop);
--- src/st/no1/e_init.c
+++ src/st/no1/e_init.c
@@ -1,2 +1,2 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../popup.h"
+#include "no1.h"
