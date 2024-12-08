ca27c9b3a11c57c0d7165820749e371114344e0b
Joey Murphy <tjmurphy@mit.edu>
2024-12-08 17:05:21 -0500

    another unsplit

--- src/dra_psp/23FE0.c
+++ src/dra_psp/23FE0.c
@@ -768,3 +768,59 @@ void func_800FF0B8(void) {
 void func_800FF0F4(s32 arg0) { D_80139828[arg0] = 0x1000; }
 
 s32 func_800FF110(s32 arg0) { return D_80139828[arg0]; }
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", DealDamage);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_091030C8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_09103128);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_091032C8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", RenderTilemap);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", SetRoomForegroundLayer);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", SetRoomBackgroundLayer);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", LoadRoomLayer);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_09103E68);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_800EAEA4);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", ResetPendingGfxLoad);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", LoadGfxAsync);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_091040A0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", DecompressWriteNibble);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", DecompressReadNibble);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", DecompressData);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", LoadPendingGfx);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_800EB4F8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", LoadEquipIcon);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_09104810);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_09104878);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_091048B8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_09104958);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_091049E8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_09104AA0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_09104C80);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_psp_09105200);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/23FE0", func_80109A44);
