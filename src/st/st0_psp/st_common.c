0c25f09d9747a5521f282fead949d4d7ca202ea5
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2024-12-09 21:19:03 +0000

    psp st0 st_common (#1992)
    We can very easily merge files between PSP and US without being worried
    of what PSP symbols are being stripped out of the build. This is just
    the beginning of how we can use that to decompile PSP overlays more
    easily.

--- src/st/st0_psp/st_common.c
+++ src/st/st0_psp/st_common.c
@@ -1,36 +1,3 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_0924FD90);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_0924FE28);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_0924FEB0);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_09250020);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_092500A8);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_092500F8);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", UnkCollisionFunc3);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_092503E8);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_09250458);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_09250500);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_09250580);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_09250670);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_092506C0);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", func_pspeu_09250700);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", EntityDummy);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", CheckFieldCollision);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_common", ReplaceBreakableWithItemDrop);
+#include "../st0/st0.h"
+#include "../st_common.h"
