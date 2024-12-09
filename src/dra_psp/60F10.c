c262db180ba73809393a5c6698395a3e0d3b92ae
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2024-12-09 20:04:00 +0000

    Decompile unused PSP DRA functions (#1990)
    I found these functions in DRA PSP not being called anywhere. Despite
    that they are not being stripped out of the final binary. They were
    small enough to be worth decompiling.
    
    I created a `dra_psp.h` and put there everything that is found in the
    PSP overlay and that we have yet to find the equivalent in the US build
    counterpart.

--- src/dra_psp/60F10.c
+++ src/dra_psp/60F10.c
@@ -3,6 +3,14 @@
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/60F10", func_psp_0913D890);
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/60F10", func_psp_0913D930);
+s32 func_psp_0913D930(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
+    if (arg0 != 0) {
+        return -1;
+    }
+    if (arg1 != 0) {
+        return -1;
+    }
+    return func_89192EC(arg2, arg3);
+}
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/60F10", func_psp_0913D998);
