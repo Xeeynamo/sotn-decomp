03f5ab78cfaf2d7207f9cbc6c9f8643e20e1c393
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-12-17 07:53:57 +1100

    Various `no1` dedupes (#2002)
    * `create_entity`
    * `st_common`
    * `blit_char`
    * `e_room_fg`

--- src/st/no1/create_entity.c
+++ src/st/no1/create_entity.c
@@ -1,33 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
+#include "no1.h"
 
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", CreateEntityFromLayout);
-
-INCLUDE_ASM(
-    "st/no1/nonmatchings/create_entity", CreateEntityWhenInHorizontalRange);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", func_us_801C37BC);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", FindFirstEntityAbove);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", FindFirstEntityBelow);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", CreateEntitiesAbove);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", CreateEntitiesBelow);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", func_us_801C3B88);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", func_us_801C3BD4);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", func_us_801C3C2C);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", func_us_801C3D28);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", InitRoomEntities);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", UpdateRoomPosition);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", CreateEntityFromCurrentEntity);
-
-INCLUDE_ASM("st/no1/nonmatchings/create_entity", CreateEntityFromEntity);
+#include "../pfn_entity_update.h"
+#include "../create_entity.h"
