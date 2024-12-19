f71648dcbaae0bb4a24e30de0da8512eda28fcb2
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-12-19 17:44:39 +1100

    More NO1 dedupes (#2006)
    * e_collect
    * e_misc
    * e_skeleton

--- src/st/no1/e_misc.c
+++ src/st/no1/e_misc.c
@@ -1,46 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
+#include "no1.h"
 
-INCLUDE_RODATA("st/no1/nonmatchings/e_misc", D_us_801B4930);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityRelicOrb);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityHeartDrop);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityMessageBox);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", CheckColliderOffsets);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityUnkId13);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityExplosionVariantsSpawner);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityGreyPuffSpawner);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityExplosionVariants);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityGreyPuff);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityOlroxDrool);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", UnkCollisionFunc5);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", UnkCollisionFunc4);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityIntenseExplosion);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", InitializeUnkEntity);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", MakeEntityFromId);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", MakeExplosions);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", EntityBigRedFireball);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", UnkRecursivePrimFunc1);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", UnkRecursivePrimFunc2);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", ClutLerp);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_misc", PlaySfxPositional);
+#define HEART_DROP_CASTLE_FLAG 0x10
+#include "../e_misc.h"
