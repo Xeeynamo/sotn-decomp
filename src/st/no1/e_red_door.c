eba3a71fa669c896cef87c21a9f0c4a874b25c4b
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-12-20 19:19:12 +1100

    Dedupe `no1` `e_red_door`

--- src/st/no1/e_red_door.c
+++ src/st/no1/e_red_door.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "no1.h"
 
-INCLUDE_ASM("st/no1/nonmatchings/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/no1/nonmatchings/e_red_door", NO1_EntityRedDoor);
+#include "../e_red_door.h"
