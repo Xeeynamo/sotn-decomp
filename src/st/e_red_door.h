03c19d417c550f65fa0bf250f463bc93dcc71e0d
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-12-31 08:41:17 +1100

    Refactor door extensions per feedback

--- src/st/e_red_door.h
+++ src/st/e_red_door.h
@@ -51,14 +51,14 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
     switch (self->step) {
     case 0:
 #ifdef STAGE_IS_NO1
-        self->ext.no1RedDoor.unk88 = self->params & 0x10;
+        self->ext.redDoor.unk88 = self->params & 0x10;
         self->params &= 0xFFEF;
 #endif
         InitializeEntity(g_EInitCommon);
         self->animSet = 7;
         self->animCurFrame = 1;
 #ifdef STAGE_IS_NO1
-        if (self->ext.no1RedDoor.unk88) {
+        if (self->ext.redDoor.unk88) {
             self->zPriority = 0x58;
         } else {
             self->zPriority = PLAYER.zPriority - 0x20;
@@ -70,11 +70,11 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
         self->posY.i.hi += 0x1F;
 
         if (self->params & 0x100) {
-            self->ext.door.unk86 = -4;
+            self->ext.redDoor.unk86 = -4;
         } else {
-            self->ext.door.unk86 = 4;
+            self->ext.redDoor.unk86 = 4;
         }
-        self->posX.i.hi += self->ext.door.unk86;
+        self->posX.i.hi += self->ext.redDoor.unk86;
         self->primIndex = g_api.AllocPrimitives(PRIM_GT4, LEN(g_eRedDoorUV));
         if (self->primIndex == -1) {
             DestroyEntity(self);
@@ -97,7 +97,7 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
             prim->tpage = 0x1F;
             prim->clut = 0x198;
 #ifdef STAGE_IS_NO1
-            if (self->ext.no1RedDoor.unk88) {
+            if (self->ext.redDoor.unk88) {
                 prim->priority = 0x58;
             } else {
                 prim->priority = PLAYER.zPriority - 0x20;
@@ -129,11 +129,11 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
         if (EntityIsNearPlayer(self)) {
             if (!(self->params & 0x100)) {
                 g_api.PlaySfxVolPan(SFX_DOOR_OPEN, 0x60, -6);
-                self->ext.door.angle = 0x1000;
+                self->ext.redDoor.angle = 0x1000;
             }
             if (self->params & 0x100) {
                 g_api.PlaySfxVolPan(SFX_DOOR_OPEN, 0x60, 6);
-                self->ext.door.angle = 0x800;
+                self->ext.redDoor.angle = 0x800;
             }
             self->animCurFrame = 0;
             self->step = 4;
@@ -141,12 +141,12 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
             g_Player.padSim = 0;
             g_Player.D_80072EFC = 0x18;
 #ifdef STAGE_IS_NO1
-            if (self->ext.no1RedDoor.unk88 && self->step == 3) {
+            if (self->ext.redDoor.unk88 && self->step == 3) {
                 PLAYER.zPriority = 0x5C;
             }
 #endif
         } else {
-            self->ext.door.angle = 0xC00;
+            self->ext.redDoor.angle = 0xC00;
             prim = &g_PrimBuf[self->primIndex];
             i = 0;
             while (prim != NULL) {
@@ -201,24 +201,24 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
         g_Player.padSim = 0;
         g_Player.D_80072EFC = 0x18;
         if (!(self->params & 0x100)) {
-            self->ext.door.angle += 0x20;
-            if (self->ext.door.angle >= 0x1000) {
-                self->ext.door.angle = 0x1000;
+            self->ext.redDoor.angle += 0x20;
+            if (self->ext.redDoor.angle >= 0x1000) {
+                self->ext.redDoor.angle = 0x1000;
             }
-            if (self->ext.door.angle == 0x1000) {
+            if (self->ext.redDoor.angle == 0x1000) {
                 self->step++;
             }
         } else {
-            self->ext.door.angle -= 0x20;
-            if (self->ext.door.angle <= 0x800) {
-                self->ext.door.angle = 0x800;
+            self->ext.redDoor.angle -= 0x20;
+            if (self->ext.redDoor.angle <= 0x800) {
+                self->ext.redDoor.angle = 0x800;
             }
-            if (self->ext.door.angle == 0x800) {
+            if (self->ext.redDoor.angle == 0x800) {
                 self->step++;
             }
         }
 #ifdef STAGE_IS_NO1
-        if (self->ext.no1RedDoor.unk88 && self->step == 3) {
+        if (self->ext.redDoor.unk88 && self->step == 3) {
             g_Tilemap.x = 0x98;
             PLAYER.zPriority = 0x5C;
         }
@@ -236,7 +236,7 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
         }
         g_Player.D_80072EFC = 3;
 #ifdef STAGE_IS_NO1
-        if (PLAYER.posX.i.hi < 0x64 && self->ext.no1RedDoor.unk88) {
+        if (PLAYER.posX.i.hi < 0x64 && self->ext.redDoor.unk88) {
             g_Tilemap.left++;
             g_PlayerX -= 0x100;
             g_Tilemap.x = 0x100;
@@ -256,7 +256,7 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
         g_Player.D_80072EFC = 4;
         if (EntityIsNearPlayer(self) == 0) {
 #ifdef STAGE_IS_NO1
-            if (self->ext.no1RedDoor.unk88) {
+            if (self->ext.redDoor.unk88) {
                 PLAYER.zPriority = g_unkGraphicsStruct.g_zEntityCenter;
             }
             self->step++;
@@ -272,17 +272,17 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
         g_Player.padSim = 0;
         g_Player.D_80072EFC = 4;
         if (!(self->params & 0x100)) {
-            self->ext.door.angle -= 0x20;
-            if (self->ext.door.angle < 0xC01) {
-                self->ext.door.angle = 0xC00;
+            self->ext.redDoor.angle -= 0x20;
+            if (self->ext.redDoor.angle < 0xC01) {
+                self->ext.redDoor.angle = 0xC00;
             }
         } else {
-            self->ext.door.angle += 0x20;
-            if (self->ext.door.angle >= 0xC00) {
-                self->ext.door.angle = 0xC00;
+            self->ext.redDoor.angle += 0x20;
+            if (self->ext.redDoor.angle >= 0xC00) {
+                self->ext.redDoor.angle = 0xC00;
             }
         }
-        if (self->ext.door.angle == 0xC00) {
+        if (self->ext.redDoor.angle == 0xC00) {
             prim = &g_PrimBuf[self->primIndex];
             for (i = 0; prim != NULL; i++, prim = prim->next) {
                 prim->drawMode |= DRAW_HIDE;
@@ -307,7 +307,7 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
         g_api.func_8010DFF0(1, 1);
     }
 
-    x = self->posX.i.hi - self->ext.door.unk86;
+    x = self->posX.i.hi - self->ext.redDoor.unk86;
     if (self->params & 0x100) {
         x--;
     } else {
@@ -315,7 +315,7 @@ void OVL_EXPORT(EntityRedDoor)(Entity* self) {
     }
 
     i = 0;
-    angle = self->ext.door.angle;
+    angle = self->ext.redDoor.angle;
     prim = &g_PrimBuf[self->primIndex];
     for (; prim != NULL; i++, prim = prim->next) {
         if (!(prim->drawMode & DRAW_HIDE)) {
