c9fc9c495b9b615ba6e2f9a673e02109cf53d392
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-01-01 22:15:02 +0000

    PSP ST0 EntityPrizeDrop

--- src/st/st0_psp/e_collect.c
+++ src/st/st0_psp/e_collect.c
@@ -1,19 +1,28 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../st0/st0.h"
 
+extern u8* g_SubweaponAnimPrizeDrop[];
+extern u8* g_MariaSubweaponAnimPrizeDrop[];
+extern u16 aluric_subweapons_id[];
+extern u16 maria_subweapons_id[];
+extern s16 D_80180EB8[];
+
 #include "../prize_drop_fall.h"
 #include "../prize_drop_fall2.h"
 
+void CollectHeart(u16 arg0);
 INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_collect", CollectHeart);
 
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_collect", func_pspeu_0924BB08);
+void CollectGold(u16 goldSize);
+INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_collect", CollectGold);
 
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_collect", func_pspeu_0924BBA0);
+void CollectSubweapon(u16 subWeaponIdx);
+INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_collect", CollectSubweapon);
 
+void CollectDummy(u16);
 INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_collect", CollectDummy);
 
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_collect", EntityPrizeDrop);
-
+#include "../entity_prize_drop.h"
 #include "../entity_explosion.h"
 #include "../blink_item.h"
 
