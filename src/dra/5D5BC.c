ec0a4a5ebdead50db567de44ca282d73740e418b
bismurphy <tjmurphy@mit.edu>
2024-12-31 19:17:33 -0500

    partial progress

--- src/dra/5D5BC.c
+++ src/dra/5D5BC.c
@@ -566,21 +566,24 @@ s32 HandleDamage(DamageParam* damage, s32 arg1, s32 amount, s32 arg3) {
     func_800F53A4();
     damage->effects = arg1 & ~0x1F;
     damage->damageKind = arg1 & 0x1F;
-    if (g_Status.defenseElement & damage->effects) {
+    // Damage doubled, weakness
+    if (g_Status.elementsWeakTo & damage->effects) {
         amount *= 2;
     }
-    if (g_Status.D_80097C2A & damage->effects) {
+    // Damage halved, resistance
+    if (g_Status.elementsResist & damage->effects) {
         amount /= 2;
     }
-    if (g_Status.D_80097C2C & damage->effects) {
-        if (g_Status.D_80097C2C & damage->effects & EFFECT_UNK_0200) {
+    // Immune, zero damage
+    if (g_Status.elementsImmune & damage->effects) {
+        if (g_Status.elementsImmune & damage->effects & EFFECT_UNK_0200) {
             damage->effects &= ~EFFECT_UNK_0200;
         } else {
             return 0;
         }
     }
-
-    if (g_Status.D_80097C2E & damage->effects) {
+    // Absorb, hp goes up based on damage amount
+    if (g_Status.elementsAbsorb & damage->effects) {
         if (amount < 1) {
             amount = 1;
         }
