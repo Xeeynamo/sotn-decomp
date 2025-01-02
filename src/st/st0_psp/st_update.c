ce1fdf8849245ee506e1e78a06657493293262fa
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-01-02 17:10:32 +1100

    Dedupe PSP `stst0` `st_update`

--- src/st/st0_psp/st_update.c
+++ src/st/st0_psp/st_update.c
@@ -1,8 +1,2 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_update", Random);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_update", UpdateStageEntities);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/st_update", UpdateStageEntitiesAlt);
+#include "../st_update.h"
