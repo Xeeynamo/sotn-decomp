65d3c52c308c61052e2f0fa34c89f655cba9c81a
Joey Murphy <tjmurphy@mit.edu>
2025-01-03 16:45:07 -0500

    PSP DrawRichterHudSubweapon matching

--- src/dra_psp/61F30.c
+++ src/dra_psp/61F30.c
@@ -7,7 +7,7 @@ INCLUDE_ASM("dra_psp/psp/dra_psp/61F30", func_80106A28);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/61F30", LoadMonsterLibrarianPreview);
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/61F30", func_psp_0913F7F8);
+INCLUDE_ASM("dra_psp/psp/dra_psp/61F30", func_801071CC);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/61F30", func_80107250);
 
