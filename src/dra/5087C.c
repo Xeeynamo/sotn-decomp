7621d6e48a21b76bccf488df6f083e31e5242f58
bismurphy <tjmurphy@mit.edu>
2025-01-04 09:32:31 -0500

    PSP DRA func_800F087C and an important file split (#2028)
    86A0.c is the HUD file I've been doing. This is the next function in
    that file. However, it's completely unrelated, is not the next one in
    the US version, and has different compiler optimization (-O0 versus
    -O4). This heavily indicates that there should be a file split here. So,
    this PR decompiles the function, swaps out that decompilation for the US
    version, and performs the file split to continue preparing for a future
    `hud.c`. Nice to see that we now have the two versions ending the HUD
    file at the same place, now just need to get the starts to align the
    same way.

--- src/dra/5087C.c
+++ src/dra/5087C.c
@@ -243,18 +243,21 @@ extern s32 D_801375C8;
 
 s32 func_800F087C(u32 chunkX, u32 chunkY) {
     RoomBossTeleport* phi_s1;
-    s32 res;
 
-    for (phi_s1 = D_800A297C; true; phi_s1++) {
+    for (phi_s1 = &D_800A297C[0]; true; phi_s1++) {
         if (phi_s1->x == 0x80) {
             return 0;
         }
+        // All must match, otherwise we jump out.
+        if (phi_s1->x != chunkX || phi_s1->y != chunkY ||
+            phi_s1->stageId != g_StageId) {
+            continue;
+        }
 
-        res = phi_s1->x == chunkX;
-        if (res && phi_s1->y == chunkY && phi_s1->stageId == g_StageId &&
-            (phi_s1->eventId == TIMEATTACK_EVENT_INVALID ||
-             TimeAttackController(phi_s1->eventId, TIMEATTACK_GET_RECORD) ==
-                 0)) {
+        if (phi_s1->eventId == TIMEATTACK_EVENT_INVALID) {
+            return phi_s1->unk10 + 2;
+        }
+        if (TimeAttackController(phi_s1->eventId, TIMEATTACK_GET_RECORD) == 0) {
             return phi_s1->unk10 + 2;
         }
     }
