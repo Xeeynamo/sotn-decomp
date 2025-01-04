7be88629e009343ff669ddfcabf293623992fe49
Joey Murphy <tjmurphy@mit.edu>
2025-01-04 09:40:17 -0500

    Decompile PSP version of DRA parallax, unsplit files

--- src/dra_psp/A710.c
+++ src/dra_psp/A710.c
@@ -25,4 +25,83 @@ s32 func_800F087C(u32 chunkX, u32 chunkY) {
     }
 }
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E7150);
+void func_800F0940(void) {
+    switch (g_BgLayers[0].scrollKind) {
+    case 1:
+        g_BgLayers[0].scrollX.i.hi = g_Tilemap.scrollX.i.hi;
+        g_BgLayers[0].scrollY.i.hi = g_Tilemap.scrollY.i.hi;
+        break;
+    case 2:
+        g_BgLayers[0].scrollX.i.hi = (g_Tilemap.scrollX.i.hi / 2);
+        g_BgLayers[0].scrollY.i.hi = (g_Tilemap.scrollY.i.hi / 2) + 0x76;
+        break;
+    case 3:
+        g_BgLayers[0].scrollX.i.hi = g_Tilemap.scrollX.i.hi / 2;
+        g_BgLayers[0].scrollY.i.hi = g_Tilemap.scrollY.i.hi;
+        break;
+    case 4:
+        g_BgLayers[0].scrollX.i.hi = g_Tilemap.scrollX.i.hi;
+        g_BgLayers[0].scrollY.i.hi = g_Tilemap.scrollY.i.hi / 2;
+        if (g_StageId == STAGE_RCHI) {
+            g_BgLayers[0].scrollY.i.hi += 0x80;
+        }
+        break;
+    case 5:
+        g_BgLayers[0].scrollX.i.hi = g_Tilemap.scrollX.i.hi / 2;
+        g_BgLayers[0].scrollY.i.hi = g_Tilemap.scrollY.i.hi / 2;
+        if (g_StageId == STAGE_RDAI) {
+            g_BgLayers[0].scrollX.i.hi += 0x80;
+        }
+        break;
+    case 6:
+        g_BgLayers[0].scrollX.i.hi = g_Tilemap.scrollX.i.hi / 2;
+        g_BgLayers[0].scrollY.i.hi =
+            (g_Tilemap.scrollY.i.hi / 2 - ((g_Tilemap.vSize - 1) * 128)) +
+            (g_BgLayers[0].h * 128);
+        if (g_StageId == STAGE_RDAI) {
+            g_BgLayers[0].scrollX.i.hi += 0x80;
+            g_BgLayers[0].scrollY.i.hi = g_Tilemap.scrollY.i.hi / 2;
+        }
+        break;
+    case 7:
+        g_BgLayers[0].scrollX.i.hi = g_Tilemap.scrollX.i.hi / 2;
+        g_BgLayers[0].scrollY.i.hi = 4;
+        break;
+    default:
+        g_BgLayers[0].scrollX.i.hi = 0;
+        g_BgLayers[0].scrollY.i.hi = 4;
+        break;
+    }
+}
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", SetNextRoomToLoad);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_800F0CD8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E7BC0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E7DA8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E7E38);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E7E90);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E7ED8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E7F38);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E7F98);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E8008);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E80E0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E81A8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E83E8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E8518);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E8620);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_psp_090E8658);
