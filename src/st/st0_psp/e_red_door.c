24262295b805269529e05c1c72f25d5632a0424e
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-01-07 18:27:33 +1100

    Add `st0` `e_red_door` codesharing

--- src/st/st0_psp/e_red_door.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../st0/st0.h"
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/e_red_door", ST0_EntityRedDoor);
