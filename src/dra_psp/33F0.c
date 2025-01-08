69228c677bd9d5d527e9a0a798eea40c41ce79f3
Joey Murphy <tjmurphy@mit.edu>
2025-01-08 12:31:14 -0500

    a further unsplit

--- src/dra_psp/33F0.c
+++ src/dra_psp/33F0.c
@@ -4,3 +4,27 @@
 INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_800EA5E4);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_psp_090DFED8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_psp_090DFFD0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_psp_090E0870);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_psp_090E08D8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_psp_090E0C30);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_psp_090E0C68);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_psp_090E0CA8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_800EDB08);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_800EDB58);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", AllocPrimitives);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", func_800EDD9C);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", FreePrimitives);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/33F0", RenderPrimitives);
