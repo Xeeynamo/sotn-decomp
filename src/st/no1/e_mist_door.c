06489a6435eb0096bd0981c9bd43f4d1f03e1c95
gamezter <gamezter93@gmail.com>
2025-01-13 00:04:01 -0500

    Rename `E_SAVE_GAME_POPUP` to `E_MESSAGE_BOX`

--- src/st/no1/e_mist_door.c
+++ src/st/no1/e_mist_door.c
@@ -44,7 +44,7 @@ void EntityMistDoor(Entity* self) {
     if (self->hitFlags) {
         messageBox = AllocEntity(&g_Entities[224], &g_Entities[256]);
         if (messageBox != NULL) {
-            CreateEntityFromCurrentEntity(E_SAVE_GAME_POPUP, messageBox);
+            CreateEntityFromCurrentEntity(E_MESSAGE_BOX, messageBox);
             messageBox->posX.i.hi = 0x80;
             messageBox->posY.i.hi = 0xB0;
 #ifdef VERSION_PSP
