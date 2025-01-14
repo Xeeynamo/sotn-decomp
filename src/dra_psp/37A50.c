327fd162db56ebe09579f2d2c8db4ff4647e0ad3
bismurphy <tjmurphy@mit.edu>
2025-01-14 11:54:56 -0500

    PSP DRA CheckGravityBootsInput (#2068)
    Got stuck with registers on the -O4 area I was working on, so I'm
    refocusing to this area of code which should be easier. Also simplified
    the selection of compiler optimizations; it seems like DRA is roughly
    50/50 on O4 and O0 so it doesn't make sense to check for the overlay
    first.

--- src/dra_psp/37A50.c
+++ src/dra_psp/37A50.c
@@ -3,4 +3,4 @@
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_091143D0);
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_09114438);
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", DoGravityJump);
