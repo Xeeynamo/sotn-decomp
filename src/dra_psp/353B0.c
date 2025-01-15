e7ac83bd3da24cb132883c8407f417bc20678437
Joey Murphy <tjmurphy@mit.edu>
2025-01-15 16:12:45 -0500

    Unsplit file, probably needs a resplit

--- src/dra_psp/353B0.c
+++ src/dra_psp/353B0.c
@@ -785,3 +785,11 @@ bool CheckSwordBrothersInput(void) {
     }
     return 0;
 }
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/353B0", func_psp_09113C00);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/353B0", func_psp_09113D78);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/353B0", func_psp_09113DE0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/353B0", func_psp_09113E70);
