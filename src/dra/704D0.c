5712ff868630827076744d7d2926e2aed508292d
Joey Murphy <tjmurphy@mit.edu>
2025-01-15 14:57:07 -0500

    PSP DRA CheckQuarterCircleForwardInput

--- src/dra/704D0.c
+++ src/dra/704D0.c
@@ -48,19 +48,21 @@ bool CheckGravityBootsInput(void) {
 }
 
 // Used for wolf charge, and for weapons which have a move using this pattern
+// Used for wolf charge, and for weapons which have a move using this pattern
 bool CheckQuarterCircleForwardInput(void) {
     s32 directionsPressed;
-    s32 down_forward;
     s32 forward;
+    s32 down_forward;
+    s32 unused = 1; // Meaningless, PSP-only
 
     directionsPressed =
         g_Player.padPressed & (PAD_UP | PAD_RIGHT | PAD_DOWN | PAD_LEFT);
-    if (!g_WasFacingLeft) {
+    if (g_WasFacingLeft == 0) {
         forward = PAD_RIGHT;
     } else {
         forward = PAD_LEFT;
     }
-    if (!g_WasFacingLeft) {
+    if (g_WasFacingLeft == 0) {
         down_forward = PAD_DOWN + PAD_RIGHT;
     } else {
         down_forward = PAD_DOWN + PAD_LEFT;
@@ -98,8 +100,8 @@ bool CheckQuarterCircleForwardInput(void) {
         }
         break;
     default:
-        if (g_ButtonCombo[COMBO_QCF].timer != 0) {
-            if (g_Player.unk72 != 0) {
+        if (g_ButtonCombo[COMBO_QCF].timer) {
+            if (g_Player.unk72) {
                 g_ButtonCombo[COMBO_QCF].buttonsCorrect = 0;
                 break;
             }
--- src/dra_psp/353B0.c
+++ src/dra_psp/353B0.c
@@ -45,4 +45,68 @@ bool CheckGravityBootsInput(void) {
     return 0;
 }
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/353B0", func_psp_09111F58);
+// Used for wolf charge, and for weapons which have a move using this pattern
+bool CheckQuarterCircleForwardInput(void) {
+    s32 directionsPressed;
+    s32 forward;
+    s32 down_forward;
+    s32 unused = 1; // Meaningless, PSP-only
+
+    directionsPressed =
+        g_Player.padPressed & (PAD_UP | PAD_RIGHT | PAD_DOWN | PAD_LEFT);
+    if (g_WasFacingLeft == 0) {
+        forward = PAD_RIGHT;
+    } else {
+        forward = PAD_LEFT;
+    }
+    if (g_WasFacingLeft == 0) {
+        down_forward = PAD_DOWN + PAD_RIGHT;
+    } else {
+        down_forward = PAD_DOWN + PAD_LEFT;
+    }
+    switch (g_ButtonCombo[COMBO_QCF].buttonsCorrect) {
+    case 0:
+        g_WasFacingLeft = PLAYER.facingLeft;
+        if (directionsPressed == PAD_DOWN) {
+            g_ButtonCombo[COMBO_QCF].timer = 20;
+            g_ButtonCombo[COMBO_QCF].buttonsCorrect++;
+            break;
+        }
+        if (--g_ButtonCombo[COMBO_QCF].timer == 0) {
+            g_ButtonCombo[COMBO_QCF].buttonsCorrect = 0;
+        }
+        break;
+    case 1:
+        if (directionsPressed == down_forward) {
+            g_ButtonCombo[COMBO_QCF].timer = 20;
+            g_ButtonCombo[COMBO_QCF].buttonsCorrect++;
+            break;
+        }
+        if (--g_ButtonCombo[COMBO_QCF].timer == 0) {
+            g_ButtonCombo[COMBO_QCF].buttonsCorrect = 0;
+        }
+        break;
+    case 2:
+        if (directionsPressed == forward) {
+            g_ButtonCombo[COMBO_QCF].timer = 20;
+            g_ButtonCombo[COMBO_QCF].buttonsCorrect = COMBO_COMPLETE;
+            break;
+        }
+        if (--g_ButtonCombo[COMBO_QCF].timer == 0) {
+            g_ButtonCombo[COMBO_QCF].buttonsCorrect = 0;
+        }
+        break;
+    default:
+        if (g_ButtonCombo[COMBO_QCF].timer) {
+            if (g_Player.unk72) {
+                g_ButtonCombo[COMBO_QCF].buttonsCorrect = 0;
+                break;
+            }
+            if (--g_ButtonCombo[1].timer == 0) {
+                g_ButtonCombo[COMBO_QCF].buttonsCorrect = 0;
+                g_WasFacingLeft = PLAYER.facingLeft;
+            }
+        }
+    }
+    return 0;
+}
