c131210a44f12be0655868e637efbdef74158a02
Joey Murphy <tjmurphy@mit.edu>
2025-01-16 09:43:46 -0500

    rename current firstfunc

--- src/dra_psp/37A50.c
+++ src/dra_psp/37A50.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_091143D0);
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_8010E940);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", DoGravityJump);
 
