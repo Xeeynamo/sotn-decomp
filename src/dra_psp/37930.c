d04a37837c9d3422eaabd3f77e33e6bef81d5fbc
Joey Murphy <tjmurphy@mit.edu>
2025-01-16 09:45:21 -0500

    unsplit start

--- src/dra_psp/37930.c
+++ src/dra_psp/37930.c
@@ -1,4 +1,38 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/37930", func_psp_091142B0);
+INCLUDE_ASM("dra_psp/psp/dra_psp/37930", func_8010E83C);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_8010E940);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", DoGravityJump);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_09114500);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_09114590);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_09114668);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_09114880);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_091149C8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_09114A38);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_8010FAF4);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", PerformHellfire);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", PerformDarkMetamorphosis);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", PerformSoulSteal);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", PerformSummonSpirit);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", PerformTetraSpirit);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", PerformSwordBrothers);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_8010FD88);
+
+// Next func in PSP is SetPlayerStep; next in US is func_8010FDF8. File split.
