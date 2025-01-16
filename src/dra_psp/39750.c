791c4c1e6ed72093df2091eef30d73a0ed48feda
Joey Murphy <tjmurphy@mit.edu>
2025-01-16 09:24:51 -0500

    unsplit some spells

--- src/dra_psp/39750.c
+++ src/dra_psp/39750.c
@@ -2,3 +2,7 @@
 #include "../dra/dra.h"
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformHellfire);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformDarkMetamorphosis);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformSoulSteal);
