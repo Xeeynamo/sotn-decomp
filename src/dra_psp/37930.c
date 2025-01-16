8e1de6af5f8a4c698cfad5eebe02ce1b361bb8b1
bismurphy <tjmurphy@mit.edu>
2025-01-16 16:25:59 -0500

    Update PSP symbols and splits to align files for decomp and code sharing (#2084)
    Lots of merging files on PSP since I found they matched with US. Now I
    have 373F8.c on PSP and 6E42C.c on US, and they start and end at the
    same points, with different stuff before and after. This makes me
    believe they are the same file.
    
    I'm hoping for my next PR to be a full decomp with code sharing on this
    file, but figured changing all the splits would be reasonable to be a PR
    first, so that only the code changes will show up in the next one,
    rather than moved code looking brand new.

--- src/dra_psp/37930.c
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../dra/dra.h"
-
-INCLUDE_ASM("dra_psp/psp/dra_psp/37930", func_psp_091142B0);
---
+++ src/dra_psp/39AA8.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../dra/dra.h"
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39AA8", SetPlayerStep);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39AA8", func_psp_09116450);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39AA8", func_psp_09116720);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39AA8", SetPlayerAnim);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39AA8", func_psp_09116B88);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39AA8", UpdateUnarmedAnim);
