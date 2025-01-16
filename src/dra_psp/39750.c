e4c78f4a153a58f211a54b9409d810e9d918fe69
Joey Murphy <tjmurphy@mit.edu>
2025-01-16 09:25:50 -0500

    Next unsplit

--- src/dra_psp/39750.c
+++ src/dra_psp/39750.c
@@ -6,3 +6,23 @@ INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformHellfire);
 INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformDarkMetamorphosis);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformSoulSteal);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformSummonSpirit);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformTetraSpirit);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", PerformSwordBrothers);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", func_psp_091163A8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", func_psp_09116428);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", func_psp_09116450);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", func_psp_09116720);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", SetPlayerAnim);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", func_psp_09116B88);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/39750", UpdateUnarmedAnim);
