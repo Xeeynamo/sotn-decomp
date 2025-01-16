ef37fd0593641c816e1b61882cab86ea02680f68
Joey Murphy <tjmurphy@mit.edu>
2025-01-16 09:38:32 -0500

    More unsplitting for gravity jump

--- src/dra_psp/37A50.c
+++ src/dra_psp/37A50.c
@@ -4,3 +4,5 @@
 INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_091143D0);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", DoGravityJump);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/37A50", func_psp_09114500);
