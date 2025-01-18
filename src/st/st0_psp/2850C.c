c79cf786def1e4ee3f09aca972bcb6880b6a9f44
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-01-19 04:31:34 +1100

    Update for PSP match on `st0` `2805C` (#2091)

--- src/st/st0_psp/2850C.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/2850C", func_801A805C);
-
-INCLUDE_ASM("st/st0_psp/psp/st0_psp/2850C", func_801A8328);
