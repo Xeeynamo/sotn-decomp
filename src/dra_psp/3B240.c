b55320671f1b3a46587709c1ca412ac230db7652
bismurphy <tjmurphy@mit.edu>
2025-01-19 16:46:00 -0500

    Big PSP decomp with code-sharing (#2092)
    As established in my previous PR, I took a full C file, decompiled every
    function on PSP, backported the changes to US, and then enabled
    code-sharing. Ultimately this increases code quality for US. I also took
    the liberty of improving some symbol names, making an enum for the
    g_Player.timers[], and such. It's nice that this process means a new set
    of eyes is examining every function and generally finding improvements
    to make.

--- src/dra_psp/3B240.c
+++ src/dra_psp/3B240.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/3B240", func_psp_09117BC0);
+INCLUDE_ASM("dra_psp/psp/dra_psp/3B240", func_8010E3E0);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/3B240", func_psp_09117C00);
 
