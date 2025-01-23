c1b8aa6daabc801315a046cdf18013d27ce0c89c
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-01-23 11:39:56 +1100

    Dedupe `lib` `create_entity` for PSX

--- src/st/create_entity_from_layout.h
+++ src/st/create_entity_from_layout.h
@@ -6,7 +6,11 @@ void CreateEntityFromLayout(Entity* entity, LayoutEntity* initDesc) {
 
     DestroyEntity(entity);
     entity->entityId = initDesc->entityId & 0x3FF;
+#ifdef STAGE_IS_LIB
+    entity->pfnUpdate = PfnEntityUpdates[entity->entityId];
+#else
     entity->pfnUpdate = PfnEntityUpdates[entity->entityId - 1];
+#endif
     entity->posX.i.hi = (s16)initDesc->posX - tilemap->scrollX.i.hi;
     entity->posY.i.hi = (s16)initDesc->posY - tilemap->scrollY.i.hi;
     entity->params = initDesc->params;
