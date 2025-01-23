94e9d230a4d2c0c58e44b3c087fe781d8aeeae31
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-01-23 12:10:52 +1100

    Update `lib` `create_entity` for PSP match and codesharing

--- src/st/create_entity_from_entity.h
+++ src/st/create_entity_from_entity.h
@@ -2,11 +2,7 @@
 void CreateEntityFromCurrentEntity(u16 entityId, Entity* entity) {
     DestroyEntity(entity);
     entity->entityId = entityId;
-#ifdef STAGE_IS_LIB
-    entity->pfnUpdate = PfnEntityUpdates[entityId];
-#else
     entity->pfnUpdate = PfnEntityUpdates[entityId - 1];
-#endif
     entity->posX.i.hi = g_CurrentEntity->posX.i.hi;
     entity->posY.i.hi = g_CurrentEntity->posY.i.hi;
 }
@@ -14,11 +10,7 @@ void CreateEntityFromCurrentEntity(u16 entityId, Entity* entity) {
 void CreateEntityFromEntity(u16 entityId, Entity* source, Entity* entity) {
     DestroyEntity(entity);
     entity->entityId = entityId;
-#ifdef STAGE_IS_LIB
-    entity->pfnUpdate = PfnEntityUpdates[entityId];
-#else
     entity->pfnUpdate = PfnEntityUpdates[entityId - 1];
-#endif
     entity->posX.i.hi = source->posX.i.hi;
     entity->posY.i.hi = source->posY.i.hi;
 }
