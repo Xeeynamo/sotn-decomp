b2f360a41002982271e01c40f4bb7209883dcb24
luksy <ser-pounce@users.noreply.github.com>
2025-01-25 22:02:35 +0100

    Add correct signatures

--- src/main/psxsdk/libetc/intr.c
+++ src/main/psxsdk/libetc/intr.c
@@ -14,23 +14,6 @@ static void* stopIntr();
 static void* restartIntr();
 static void memclr(void* ptr, s32 size);
 static void trapIntr();
-s32 setjmp(s32*);
-
-#define JB_PC 0
-#define JB_SP 1
-#define JB_FP 2
-#define JB_S0 3
-#define JB_S1 4
-#define JB_S2 5
-#define JB_S3 6
-#define JB_S4 7
-#define JB_S5 8
-#define JB_S6 9
-#define JB_S7 10
-#define JB_GP 11
-#define JB_SIZE 12
-
-typedef int jmp_buf[JB_SIZE];
 
 struct intrEnv_t {
     u16 interruptsInitialized;
