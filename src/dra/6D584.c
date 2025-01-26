cfd7c65a3139a22232058c18f86145bab61b50b9
Joey Murphy <tjmurphy@mit.edu>
2025-01-25 19:20:19 -0500

    Fix breaking mismatch for ps1

--- src/dra/6D584.c
+++ src/dra/6D584.c
@@ -458,9 +458,15 @@ s32 CheckMoveDirection(void) {
 
 s32 func_8010E334(s32 xStart, s32 xEnd) {
     g_Player.unk7A = 1;
-    if (PLAYER.step == 0 && PLAYER.step_s == 1 && PLAYER.posX.i.hi >= xStart &&
-        PLAYER.posX.i.hi <= xEnd) {
-        return 1;
+    // Interesting - this could have been all && conditions, but if you try,
+    // PS1 optimizes into loading PLAYER.step as a word, rather than doing
+    // two accesses for step and step_s. Breaking it up like this prevents
+    // the optimization and results in a match.
+    if (PLAYER.step == Player_Stand){
+        if(PLAYER.step_s == 1 && PLAYER.posX.i.hi >= xStart &&
+            PLAYER.posX.i.hi <= xEnd) {
+            return 1;
+        }
     }
     return 0;
 }
