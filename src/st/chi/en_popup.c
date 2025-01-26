2968cd542b82bbb650a759c1e4e334bd5b622fd7
Jed Lang <nyxojaele@hotmail.com>
2025-01-25 20:33:29 -0700

    Leveraged duplicate function for popup

--- src/st/chi/en_popup.c
+++ src/st/chi/en_popup.c
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 /*
- * File: en_bottom_corner_text.c
+ * File: en_popup.c
  * Overlay: CHI
  * Description: ENTITY - Message that appears in the bottom corner of the screen
  */
 
 // func_801A7D78: BottomCornerText
-#include "../bottom_corner_text.h"
+#include "../popup.h"
