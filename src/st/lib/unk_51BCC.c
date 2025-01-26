23a02ed53656483c7732c1d7c083448fbe3c658c
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-01-26 18:38:34 +1100

    Dedupe `lib` `prim_helpers`

---
+++ src/st/lib/unk_51BCC.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_us_801D1BCC);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_us_801D1F50);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_us_801D2108);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_us_801D2148);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_801CDC80);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_us_801D2274);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_us_801D2CE0);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_us_801D2DA8);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", func_us_801D35B8);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_51BCC", EntityLifeUpSpawn);
