496a2465c59ec5965c616adf7ffe90933b731868
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-01-27 07:27:46 +1100

    Dedupe `lib` `prim_helpers` (#2130)
    Some rematching done for PSP but overall pretty minimal changes to
    dedupe

---
+++ src/st/lib_psp/19AE8.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09251168);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_092515F8);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09252858);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09252E70);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_092531A8);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09253498);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_092537F8);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09253D58);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09253E20);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09254120);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09254288);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", EntityBreakable);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09254B18);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/19AE8", func_psp_09254C00);
