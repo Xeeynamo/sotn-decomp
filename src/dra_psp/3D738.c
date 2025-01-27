b94ef8e8a148d610285b13292fc437023d28645d
Joey Murphy <tjmurphy@mit.edu>
2025-01-26 20:24:29 -0500

    name next func

--- src/dra_psp/3D738.c
+++ src/dra_psp/3D738.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", func_psp_0911A0B8);
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntitySubwpnThrownDagger);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", func_psp_0911ABF8);
 
