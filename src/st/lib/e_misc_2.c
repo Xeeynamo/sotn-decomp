a2f4400c38f9618a01617e920e9f0d94245bd08e
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-01-28 18:30:14 +1100

    Dedupe `lib` `e_bloody_zombie`

--- src/st/lib/e_misc_2.c
+++ src/st/lib/e_misc_2.c
@@ -6,15 +6,3 @@ INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", EntityRelicOrb);
 INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", EntityHeartDrop);
 
 INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", EntityMessageBox);
-
-INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", EntityBloodSplatter);
-
-INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", func_801C53AC);
-
-INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", EntityBloodyZombie);
-
-INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", func_us_801C9664);
-
-INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", func_us_801C9870);
-
-INCLUDE_ASM("st/lib/nonmatchings/e_misc_2", func_us_801CA090);
