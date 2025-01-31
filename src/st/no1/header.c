b1d44e0df9a22c7aaac1ba33028401fd04268c53
gamezter <gamezter93@gmail.com>
2025-01-30 19:41:05 -0500

    Import `no1` entity_layouts and rooms

---
+++ src/st/no1/header.c
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "no1.h"
+
+extern RoomHeader OVL_EXPORT(rooms)[];
+extern s16** OVL_EXPORT(spriteBanks)[];
+extern u_long* OVL_EXPORT(cluts)[];
+extern LayoutEntity* OVL_EXPORT(pStObjLayoutHorizontal)[];
+extern RoomDef OVL_EXPORT(rooms_layers)[];
+extern u_long* OVL_EXPORT(gfxBanks)[];
+void UpdateStageEntities();
+
+AbbreviatedOverlay OVL_EXPORT(Overlay) = {
+    .Update = Update,
+    .HitDetection = HitDetection,
+    .UpdateRoomPosition = UpdateRoomPosition,
+    .InitRoomEntities = InitRoomEntities,
+    .rooms = OVL_EXPORT(rooms),
+    .spriteBanks = OVL_EXPORT(spriteBanks),
+    .cluts = OVL_EXPORT(cluts),
+    .objLayoutHorizontal = OVL_EXPORT(pStObjLayoutHorizontal),
+    .tileLayers = OVL_EXPORT(rooms_layers),
+    .gfxBanks = OVL_EXPORT(gfxBanks),
+    .UpdateStageEntities = UpdateStageEntities,
+};
+
+//#include "sprite_banks.h"
+//#include "palette_def.h"
+//#include "layers.h"
+//#include "graphics_banks.h"
