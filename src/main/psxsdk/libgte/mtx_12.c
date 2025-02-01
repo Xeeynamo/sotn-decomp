f4073432f699ef085c21cc2523b99ea1e980d7aa
sozud <122322823+sozud@users.noreply.github.com>
2025-02-01 14:07:10 -0800

    Change known gte asm files to asm

--- src/main/psxsdk/libgte/mtx_12.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("main/nonmatchings/psxsdk/libgte/mtx_12", SetTransMatrix);
--- src/main/psxsdk/libgte/smp_03.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("main/nonmatchings/psxsdk/libgte/smp_03", RotTransPers3);
