901534ae684052f6a82dcb77be6c77cd86ccb6df
Joey Murphy <tjmurphy@mit.edu>
2025-01-31 21:58:21 -0500

    first psp unsplit

--- src/dra_psp/3D738.c
+++ src/dra_psp/3D738.c
@@ -6,3 +6,25 @@ INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntitySubwpnThrownDagger);
 INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntitySubwpnThrownAxe);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", func_psp_0911B630);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", func_psp_0911B820);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntityHolyWater);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntityHolyWaterBreakGlass);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntityHolyWaterFlame);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntitySubwpnCrashCross);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntitySubwpnCrashCrossParticles);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntityHellfireHandler);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntityHellfireNormalFireball);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntityBatFireball);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntityHellfireBigFireball);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/3D738", EntityExpandingCircle);
