a7ed022fa9ff99c973a591f442306ffb6f88438a
Joey Murphy <tjmurphy@mit.edu>
2025-02-03 07:51:24 -0500

    stopwatch circle

--- src/dra/8A0A4.c
+++ src/dra/8A0A4.c
@@ -6,11 +6,12 @@
 // expanding circle effect when activating stopwatch
 void EntityStopWatchExpandingCircle(Entity* self) {
     Primitive* prim;
+    s16 angle;
     s32 sine;
     s32 cosine;
     s32 i;
-    u16 selfPosX;
-    u16 selfPosY;
+    s16 selfPosX;
+    s16 selfPosY;
     s16 minus20;
 
     switch (self->step) {
@@ -23,19 +24,20 @@ void EntityStopWatchExpandingCircle(Entity* self) {
         self->flags = FLAG_POS_CAMERA_LOCKED | FLAG_KEEP_ALIVE_OFFCAMERA |
                       FLAG_HAS_PRIMS | FLAG_UNK_20000;
         prim = &g_PrimBuf[self->primIndex];
-        for (i = 0; i < 16; i++, prim = prim->next) {
+        for (i = 0; i < 16; prim = prim->next, i++) {
             prim->tpage = 0x1A;
             prim->clut = 0x15F;
-            self->zPriority = 0xC2;
-            prim->priority = 0xC2;
+            prim->priority = self->zPriority = 0xC2;
             prim->drawMode = DRAW_UNK_400 | DRAW_TPAGE2 | DRAW_TPAGE |
                              DRAW_COLORS | DRAW_TRANSP;
-            prim->u0 = ((rsin((s16)(i << 8)) << 5) >> 0xC) + 0x20;
-            prim->v0 = -((rcos((s16)(i << 8)) << 5) >> 0xC) - 0x21;
-            prim->u1 = ((rsin((s16)(i + 1 << 8)) << 5) >> 0xC) + 0x20;
-            prim->v1 = -((rcos((s16)(i + 1 << 8)) << 5) >> 0xC) - 0x21;
-            prim->v2 = prim->v3 = 0xE0;
+            angle = i << 8;
+            prim->u0 = ((rsin(angle) << 5) >> 0xC) + 0x20;
+            prim->v0 = -((rcos(angle) << 5) >> 0xC) + 0xDF;
+            angle = (i + 1 << 8);
+            prim->u1 = ((rsin(angle) << 5) >> 0xC) + 0x20;
+            prim->v1 = -((rcos(angle) << 5) >> 0xC) + 0xDF;
             prim->u2 = prim->u3 = 0x20;
+            prim->v2 = prim->v3 = 0xE0;
             prim->r0 = prim->r1 = prim->g0 = prim->g1 = prim->b0 = prim->b1 =
                 0x40;
             prim->r2 = prim->r3 = prim->g2 = prim->g3 = 0;
@@ -46,7 +48,8 @@ void EntityStopWatchExpandingCircle(Entity* self) {
         break;
     case 1:
         self->ext.et_stopwatchCircle.size += 0x18;
-        if (++self->ext.et_stopwatchCircle.timer >= 0x1F) {
+        self->ext.et_stopwatchCircle.timer++;
+        if (self->ext.et_stopwatchCircle.timer > 30) {
             DestroyEntity(self);
             return;
         }
@@ -55,7 +58,7 @@ void EntityStopWatchExpandingCircle(Entity* self) {
     selfPosX = self->posX.i.hi;
     selfPosY = self->posY.i.hi;
     prim = &g_PrimBuf[self->primIndex];
-    for (i = 0; i < 16; i++, prim = prim->next) {
+    for (i = 0; i < 16; prim = prim->next, i++) {
         sine = rsin(i << 8);
         cosine = rcos(i << 8);
         minus20 = self->ext.et_stopwatchCircle.size - 0x20;
--- src/dra_psp/45688.c
+++ src/dra_psp/45688.c
@@ -1,7 +1,81 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/45688", EntityStopWatchExpandingCircle);
+// expanding circle effect when activating stopwatch
+void EntityStopWatchExpandingCircle(Entity* self) {
+    Primitive* prim;
+    s16 angle;
+    s32 sine;
+    s32 cosine;
+    s32 i;
+    s16 selfPosX;
+    s16 selfPosY;
+    s16 minus20;
+
+    switch (self->step) {
+    case 0:
+        self->primIndex = AllocPrimitives(PRIM_GT4, 16);
+        if (self->primIndex == -1) {
+            DestroyEntity(self);
+            return;
+        }
+        self->flags = FLAG_POS_CAMERA_LOCKED | FLAG_KEEP_ALIVE_OFFCAMERA |
+                      FLAG_HAS_PRIMS | FLAG_UNK_20000;
+        prim = &g_PrimBuf[self->primIndex];
+        for (i = 0; i < 16; prim = prim->next, i++) {
+            prim->tpage = 0x1A;
+            prim->clut = 0x15F;
+            prim->priority = self->zPriority = 0xC2;
+            prim->drawMode = DRAW_UNK_400 | DRAW_TPAGE2 | DRAW_TPAGE |
+                             DRAW_COLORS | DRAW_TRANSP;
+            angle = i << 8;
+            prim->u0 = ((rsin(angle) << 5) >> 0xC) + 0x20;
+            prim->v0 = -((rcos(angle) << 5) >> 0xC) + 0xDF;
+            angle = (i + 1 << 8);
+            prim->u1 = ((rsin(angle) << 5) >> 0xC) + 0x20;
+            prim->v1 = -((rcos(angle) << 5) >> 0xC) + 0xDF;
+            prim->u2 = prim->u3 = 0x20;
+            prim->v2 = prim->v3 = 0xE0;
+            prim->r0 = prim->r1 = prim->g0 = prim->g1 = prim->b0 = prim->b1 =
+                0x40;
+            prim->r2 = prim->r3 = prim->g2 = prim->g3 = 0;
+            prim->b2 = prim->b3 = 0x20;
+        }
+        self->ext.et_stopwatchCircle.size = 0x20;
+        self->step++;
+        break;
+    case 1:
+        self->ext.et_stopwatchCircle.size += 0x18;
+        self->ext.et_stopwatchCircle.timer++;
+        if (self->ext.et_stopwatchCircle.timer > 30) {
+            DestroyEntity(self);
+            return;
+        }
+        break;
+    }
+    selfPosX = self->posX.i.hi;
+    selfPosY = self->posY.i.hi;
+    prim = &g_PrimBuf[self->primIndex];
+    for (i = 0; i < 16; prim = prim->next, i++) {
+        sine = rsin(i << 8);
+        cosine = rcos(i << 8);
+        minus20 = self->ext.et_stopwatchCircle.size - 0x20;
+        prim->x0 =
+            selfPosX + ((sine * self->ext.et_stopwatchCircle.size) >> 0xC);
+        prim->y0 =
+            selfPosY - ((cosine * self->ext.et_stopwatchCircle.size) >> 0xC);
+        prim->x2 = selfPosX + ((sine * (minus20)) >> 0xC);
+        prim->y2 = selfPosY - ((cosine * minus20) >> 0xC);
+        sine = rsin((i + 1) << 8);
+        cosine = rcos((i + 1) << 8);
+        prim->x1 =
+            selfPosX + ((sine * self->ext.et_stopwatchCircle.size) >> 0xC);
+        prim->y1 =
+            selfPosY - ((cosine * self->ext.et_stopwatchCircle.size) >> 0xC);
+        prim->x3 = selfPosX + ((sine * minus20) >> 0xC);
+        prim->y3 = selfPosY - ((cosine * minus20) >> 0xC);
+    }
+}
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/45688", EntityStopWatch);
 
