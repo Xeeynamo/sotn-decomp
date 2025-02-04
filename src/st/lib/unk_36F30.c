29584feb5c78754267aed292fbb86962a98669c7
gamezter <gamezter93@gmail.com>
2025-02-03 19:33:05 -0500

    Add split

---
+++ src/st/lib/unk_36F30.c
@@ -0,0 +1,213 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "lib.h"
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801B6F30);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801B7C94);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801B7D10);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801B7DF8);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD2C8);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD2DC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD2F0);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD304);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD314);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD324);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD330);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD33C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD348);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD350);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD35C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD360);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD36C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD374);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD37C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD384);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD38C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD394);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD39C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3A4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3AC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3B4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3B8);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3C4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3CC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3D4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3DC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3E4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3EC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3F4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD3FC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD404);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD40C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD414);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD41C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD424);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD42C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD438);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD440);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD44C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD450);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD458);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD464);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD46C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD470);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD474);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD478);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD484);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801B8234);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801B8958);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD4CC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD4DC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD4F0);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD500);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD510);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD524);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD538);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD548);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD55C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD570);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD584);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD598);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD5AC);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD5B8);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD5C8);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD5D8);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD5E4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD5F8);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD608);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD61C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD634);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD64C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD664);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD67C);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD694);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD6A4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD6B4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD6C4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD6D8);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD6E0);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD6F4);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD708);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD714);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801B8A00);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", CutsceneUnk1);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", SetCutsceneScript);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801B9804);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", CutsceneUnk4);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", DrawCutsceneActorName);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", SetCutsceneEnd);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", CutsceneRun);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_80194F14);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", ScaleCutsceneAvatar);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD760);
+
+INCLUDE_RODATA("st/lib/nonmatchings/unk_36F30", D_us_801AD774);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", LIB_EntityCutscene);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801BAF60);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801BB200);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801BB53C);
+
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801BB8DC);
+
+u8 func_us_801BBAB4(void);
+INCLUDE_ASM("st/lib/nonmatchings/unk_36F30", func_us_801BBAB4);
