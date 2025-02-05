354e380e216940891609cb004451b638b0d8fcfa
gamezter <gamezter93@gmail.com>
2025-02-05 17:45:43 -0500

    Decompile `lib` func_us_801B6324

--- src/st/lib/e_shop.c
+++ src/st/lib/e_shop.c
@@ -93,12 +93,6 @@ static InventoryItem D_us_8018134C[] = {
     // clang-format on
 };
 
-typedef struct {
-    /* 0x0 */ u16 category;
-    /* 0x2 */ u16 itemId;
-    /* 0x4 */ u32 price;
-} AvailableInventoryItem;
-
 /// relic index
 u16 D_us_801814D4[] = {16, 0};
 
@@ -148,11 +142,10 @@ extern u32 D_us_801D415C[];
 extern s32 D_us_801D425C[];
 extern AvailableInventoryItem D_us_801D4364[];
 extern s32 g_CutsceneFlags;
-extern u16 g_EInitCommon[];
 extern s32 D_psp_08B42050;
 extern s32 D_psp_08B42054;
 
-void func_us_801B245C(Primitive* arg0, u16 arg1, u16 arg2, s32 arg3, u16 arg4,
+void func_us_801B245C(Primitive* arg0, u16 arg1, u16 arg2, u16 arg3, u16 arg4,
                       s32 arg5, s32 arg6);
 INCLUDE_ASM("st/lib/nonmatchings/e_shop", func_us_801B245C);
 
