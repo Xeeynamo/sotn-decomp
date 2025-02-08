fcb2d736f281abcc6039251a7cbd31c478e28043
Joey Murphy <tjmurphy@mit.edu>
2025-02-07 22:12:49 -0500

    unsplit psp file

--- src/dra_psp/47EA8.c
+++ src/dra_psp/47EA8.c
@@ -2,3 +2,21 @@
 #include "../dra/dra.h"
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", GetFreeEntity);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_091248D0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118894);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118970);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118B18);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118C28);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_09124FB8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityNumberMovesToHpMeter);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_09125DB8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityGuardText);
