492da0c68a1e210d1122bf672362b5022b2367f9
Joey Murphy <tjmurphy@mit.edu>
2025-02-09 11:14:27 -0500

    first few funcs

--- src/dra_psp/47EA8.c
+++ src/dra_psp/47EA8.c
@@ -1,11 +1,80 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
+#include "../dra/dra_bss.h"
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", GetFreeEntity);
+// Given a range of start and end values, finds an unused entity
+// slot in g_Entities to fill in. Starts at start, and goes to
+// end-1. If none in the range are available, returns NULL.
+Entity* GetFreeEntity(s16 start, s16 end) {
+    Entity* entity = &g_Entities[start];
+    s16 i;
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_091248D0);
+    for (i = start; i < end; i++, entity++) {
+        if (entity->entityId == E_NONE) {
+            return entity;
+        }
+    }
+    return NULL;
+}
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118894);
+// Like GetFreeEntity, but searches for an open spot
+// starting from the end and working backward
+Entity* GetFreeEntityReverse(s16 start, s16 end) {
+    Entity* entity = &g_Entities[end - 1];
+    s16 i;
+
+    for (i = end - 1; i >= start; i--, entity--) {
+        if (entity->entityId == E_NONE) {
+            return entity;
+        }
+    }
+    return NULL;
+}
+
+void func_80118894(Entity* self) {
+    s32 i;
+    s32 search_value;
+
+    if (self == &g_Entities[E_WEAPON]) {
+        if (!(self->params & 0x8000)) {
+            self->enemyId = 1;
+        } else {
+            self->enemyId = 2;
+        }
+        return;
+    }
+    // It appears we're looping over elements of the 8013800C array.
+    // If the pointer to arg0 comes before the 32nd (0x20th) g_Entities,
+    // we iterate through the 8013800C array, starting from element 3 and going
+    // as high as 7, searching for our enemy ID. Otherwise we do the same, but
+    // starting from element 7 and going up to 11. 8013800C therefore must have
+    // 11 elements. It may be possible to refactor this code to remove the
+    // duplication.
+    // D_8013800C looks to be keeping track of how many abilities/attacks are
+    // targeting a specific enemy selecting the lowest targeted entity from 3-6
+    // or 7-11
+    if (self < &g_Entities[UNK_ENTITY_20]) {
+        for (search_value = 0; true; search_value++) {
+            for (i = 3; i < 7; i++) {
+                if (D_8013800C[i] == search_value) {
+                    D_8013800C[i]++;
+                    self->enemyId = i;
+                    return;
+                }
+            }
+        }
+    } else {
+        for (search_value = 0; true; search_value++) {
+            for (i = 7; i < 11; i++) {
+                if (D_8013800C[i] == search_value) {
+                    D_8013800C[i]++;
+                    self->enemyId = i;
+                    return;
+                }
+            }
+        }
+    }
+}
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118970);
 
