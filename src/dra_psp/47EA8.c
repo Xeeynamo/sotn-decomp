ccdf02f0384e75c86ed85d78b37e94de3761e4e6
bismurphy <tjmurphy@mit.edu>
2025-02-09 09:46:29 -0500

    File re-splitting for next PSP DRA code share (#2192)
    It looks like the two versions continue to align for a while after this
    still, but this file started to get a little big and this seems like a
    good stopping point, at least until we learn more.

--- src/dra_psp/47EA8.c
+++ src/dra_psp/47EA8.c
@@ -2,3 +2,69 @@
 #include "../dra/dra.h"
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", GetFreeEntity);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_091248D0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118894);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118970);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118B18);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80118C28);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_09124FB8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityNumberMovesToHpMeter);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_09125DB8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityGuardText);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntitySmallRisingHeart);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80119E78);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_80119F70);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_8011A290);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_8011A328);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_09127348);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_8011A4C8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_8011A4D0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_091279A0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_09127B50);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", CreateEntFactoryFromEntity);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityEntFactory);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityUnarmedAttack);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityDiveKickAttack);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_8011B480);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_8011B530);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_8011B5A4);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityUnkId24);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_psp_091295F0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityPlayerBlinkWhite);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityPlayerOutline);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", func_8011E0E4);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityGravityBootBeam);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/47EA8", EntityWingSmashTrail);
---
+++ src/dra_psp/507F0.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../dra/dra.h"
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/507F0", func_8011E4BC);
+
+void func_psp_0912DDA0(void) {}
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/507F0", func_8011EDA8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/507F0", func_8011F074);
