ad0d270a6cd7f698e9b8a558d4b5a1e80bc9cc19
sozud <122322823+sozud@users.noreply.github.com>
2025-02-08 23:27:37 -0500

    Change known gte asm files to asm (#2160)
    We know all of these are handwritten asm from dumping the libs. Script +
    results here:
    https://gist.github.com/sozud/6f5d111c49b69fd7b1c0c812067b2eeb
    
    Change these to asm so people don't waste time trying to match them. The
    changes to src/main/psxsdk/libgte/reg03.c may be controversial since
    someone found a C match for it, but it's not going to match the original
    object. @JoshSchreuder Seems to have done most of the work on that file

--- src/main/psxsdk/libgte/msc02.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("main/nonmatchings/psxsdk/libgte/msc02", InvSquareRoot);
-
-INCLUDE_ASM("main/nonmatchings/psxsdk/libgte/msc02", func_80017008);
-
-INCLUDE_ASM("main/nonmatchings/psxsdk/libgte/msc02", func_80017078);
-
-INCLUDE_ASM("main/nonmatchings/psxsdk/libgte/msc02", MatrixNormal);
