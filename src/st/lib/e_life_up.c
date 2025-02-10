5531d6ec8ca8a9f0caf07b183006f39c035a517f
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-02-11 10:42:21 +1100

    Dedupe `lib` PSX e_mist_door, e_life_up, e_flea_man

--- src/st/lib/e_life_up.c
+++ src/st/lib/e_life_up.c
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "lib.h"
 
-INCLUDE_ASM("st/lib/nonmatchings/e_life_up", EntityLifeUpSpawn);
+#include "../e_life_up.h"
--- src/st/lib/e_mist_door.c
+++ src/st/lib/e_mist_door.c
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "lib.h"
 
-// Mist door
-INCLUDE_ASM("st/lib/nonmatchings/e_mist_door", EntityMistDoor);
+#include "../e_mist_door.h"
