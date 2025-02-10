a98657086802c647718fa12347e3b02c6d06337e
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-02-10 21:26:31 +1100

    Update `lib` PSP splits

---
+++ src/st/lib_psp/e_mist_door.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_mist_door", func_psp_0924B2E0);
+
+INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_mist_door", EntityMistDoor);
