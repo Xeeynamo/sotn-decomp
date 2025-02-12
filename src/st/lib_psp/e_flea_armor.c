9eb053cb73c286b1f9151ffacd27a80b937141c1
Jonathan Hohle <jonhohle@gmail.com>
2025-02-12 14:57:15 -0700

    Decompile EntityFleaArmor
    For PS1 and PSP

--- src/st/lib_psp/e_flea_armor.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-// Flea armor
-INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_flea_armor", func_psp_0924D598);
-
-// Flea armor death pieces?
-INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_flea_armor", func_psp_0924DF08);
