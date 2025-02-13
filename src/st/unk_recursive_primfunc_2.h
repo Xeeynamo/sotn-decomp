454206277f00b75b1741b4e6c1827175e1d264ec
gamezter <gamezter93@gmail.com>
2025-02-13 12:04:35 -0500

    Refactor UnkRecursivePrimFunc2 for clarity

--- src/st/unk_recursive_primfunc_2.h
+++ src/st/unk_recursive_primfunc_2.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-static s16 g_UnkRecursPrim2Inds[] = {
+static s16 g_UnkRecursPrimInds[] = {
     0, 1, 3, 4, 1, 2, 4, 5, 3, 4, 6, 7, 4, 5, 7, 8,
 #if !defined(STAGE_IS_NZ0) && !defined(STAGE_IS_NO1) &&                        \
     !defined(STAGE_IS_CHI) && STAGE != STAGE_ST0 && !defined(STAGE_IS_LIB)
@@ -10,47 +10,37 @@ static s16 g_UnkRecursPrim2Inds[] = {
 #endif
 };
 
-// arg3 holds, in order:
-// A Primitive, an array of 9 rgb_sets, an array of 10 Point16s, an array of 10
-// uvPairs, and a pointer to another of arg3. Pointer type on arg3 is fake, but
-// needed for Windows.
+// dataPtr holds, in order:
+// A Primitive, an array of 9 CVECTORs, an array of 9 Point16s, an array of 10
+// uvPairs, and a pointer to another of dataPtr. Pointer type on dataPtr is
+// fake, but needed for Windows.
 
 Primitive* UnkRecursivePrimFunc2(
-    Primitive* arg0, s32 arg1, Primitive* arg2, u8* arg3) {
+    Primitive* prim, s32 iterations, Primitive* prim2, u8* dataPtr) {
     s32 i;
-    Primitive* prim1;
-    Primitive* prim2;
-    Primitive* nextArg2;
+    Primitive* tempPrim;
     Point16* points;
-    rgb_set* colors;
+    CVECTOR* colors;
     uvPair* uv_values;
     s16* indices;
-    s32 idx1;
-    s32 idx2;
-    s32 idx3;
-    s32 idx4;
 
-    if (arg2 == NULL) {
+    if (prim2 == NULL) {
         return NULL;
     }
-    prim2 = (Primitive*)arg3;
-#ifdef VERSION_PSP
-    arg3 += 0x38;
-#else
-    arg3 += 0x34;
-#endif
-    colors = (rgb_set*)arg3;
-    arg3 = arg3 + 0x24;
+    tempPrim = (Primitive*)dataPtr;
+    dataPtr += sizeof(Primitive);
+    colors = (CVECTOR*)dataPtr;
+    dataPtr += sizeof(CVECTOR) * 9;
 
-    points = (Point16*)arg3;
-    arg3 += 0x24;
-    uv_values = (uvPair*)arg3;
-    arg3 += 0x14;
+    points = (Point16*)dataPtr;
+    dataPtr += sizeof(Point16) * 9;
+    uv_values = (uvPair*)dataPtr;
+    dataPtr += sizeof(uvPair) * 10;
 
-    colors[0] = *(rgb_set*)&arg0->r0;
-    colors[2] = *(rgb_set*)&arg0->r1;
-    colors[6] = *(rgb_set*)&arg0->r2;
-    colors[8] = *(rgb_set*)&arg0->r3;
+    colors[0] = *(CVECTOR*)&prim->r0;
+    colors[2] = *(CVECTOR*)&prim->r1;
+    colors[6] = *(CVECTOR*)&prim->r2;
+    colors[8] = *(CVECTOR*)&prim->r3;
 
     colors[1].r = ((colors[0].r + colors[2].r + 1) >> 1);
     colors[1].g = ((colors[0].g + colors[2].g + 1) >> 1);
@@ -72,10 +62,10 @@ Primitive* UnkRecursivePrimFunc2(
     colors[4].g = ((colors[3].g + colors[5].g + 1) >> 1);
     colors[4].b = ((colors[3].b + colors[5].b + 1) >> 1);
 
-    uv_values[0] = *(uvPair*)&arg0->u0;
-    uv_values[2] = *(uvPair*)&arg0->u1;
-    uv_values[6] = *(uvPair*)&arg0->u2;
-    uv_values[8] = *(uvPair*)&arg0->u3;
+    uv_values[0] = *(uvPair*)&prim->u0;
+    uv_values[2] = *(uvPair*)&prim->u1;
+    uv_values[6] = *(uvPair*)&prim->u2;
+    uv_values[8] = *(uvPair*)&prim->u3;
     uv_values[1].u = (uv_values[0].u + uv_values[2].u + 1) >> 1;
     uv_values[1].v = (uv_values[0].v + uv_values[2].v + 1) >> 1;
     uv_values[7].u = (uv_values[6].u + uv_values[8].u + 1) >> 1;
@@ -87,10 +77,10 @@ Primitive* UnkRecursivePrimFunc2(
     uv_values[4].u = (uv_values[3].u + uv_values[5].u + 1) >> 1;
     uv_values[4].v = (uv_values[3].v + uv_values[5].v + 1) >> 1;
 
-    points[0] = *(Point16*)&arg0->x0;
-    points[2] = *(Point16*)&arg0->x1;
-    points[6] = *(Point16*)&arg0->x2;
-    points[8] = *(Point16*)&arg0->x3;
+    points[0] = *(Point16*)&prim->x0;
+    points[2] = *(Point16*)&prim->x1;
+    points[6] = *(Point16*)&prim->x2;
+    points[8] = *(Point16*)&prim->x3;
     points[1].x = ((points[0].x + points[2].x + 1) >> 1);
     points[1].y = ((points[0].y + points[2].y + 1) >> 1);
     points[7].x = ((points[6].x + points[8].x + 1) >> 1);
@@ -102,38 +92,39 @@ Primitive* UnkRecursivePrimFunc2(
     points[4].x = ((points[3].x + points[5].x + 1) >> 1);
     points[4].y = ((points[3].y + points[5].y + 1) >> 1);
 
-    *prim2 = *arg0;
-    indices = g_UnkRecursPrim2Inds;
+    *tempPrim = *prim;
+    indices = g_UnkRecursPrimInds;
     for (i = 0; i < 4; i++) {
-        idx1 = *indices++;
-        idx2 = *indices++;
-        idx3 = *indices++;
-        idx4 = *indices++;
-        *(uvPair*)&prim2->u0 = uv_values[idx1];
-        *(uvPair*)&prim2->u1 = uv_values[idx2];
-        *(uvPair*)&prim2->u2 = uv_values[idx3];
-        *(uvPair*)&prim2->u3 = uv_values[idx4];
-        *(Point16*)&prim2->x0 = points[idx1];
-        *(Point16*)&prim2->x1 = points[idx2];
-        *(Point16*)&prim2->x2 = points[idx3];
-        *(Point16*)&prim2->x3 = points[idx4];
-        *(rgb_set*)&prim2->r0 = colors[idx1];
-        *(rgb_set*)&prim2->r1 = colors[idx2];
-        *(rgb_set*)&prim2->r2 = colors[idx3];
-        *(rgb_set*)&prim2->r3 = colors[idx4];
+        s32 idx1 = *indices++;
+        s32 idx2 = *indices++;
+        s32 idx3 = *indices++;
+        s32 idx4 = *indices++;
+        *(uvPair*)&tempPrim->u0 = uv_values[idx1];
+        *(uvPair*)&tempPrim->u1 = uv_values[idx2];
+        *(uvPair*)&tempPrim->u2 = uv_values[idx3];
+        *(uvPair*)&tempPrim->u3 = uv_values[idx4];
+        *(Point16*)&tempPrim->x0 = points[idx1];
+        *(Point16*)&tempPrim->x1 = points[idx2];
+        *(Point16*)&tempPrim->x2 = points[idx3];
+        *(Point16*)&tempPrim->x3 = points[idx4];
+        *(CVECTOR*)&tempPrim->r0 = colors[idx1];
+        *(CVECTOR*)&tempPrim->r1 = colors[idx2];
+        *(CVECTOR*)&tempPrim->r2 = colors[idx3];
+        *(CVECTOR*)&tempPrim->r3 = colors[idx4];
 
-        prim2->type = arg0->type;
-        if (arg1 == 1) {
-            nextArg2 = arg2->next;
-            *arg2 = *prim2;
-            arg2->next = nextArg2;
-            arg2 = arg2->next;
-            if (arg2 == NULL) {
+        tempPrim->type = prim->type;
+        if (iterations == 1) {
+            Primitive* swap = prim2->next;
+            *prim2 = *tempPrim;
+            prim2->next = swap;
+            prim2 = prim2->next;
+            if (prim2 == NULL) {
                 return NULL;
             }
         } else {
-            arg2 = UnkRecursivePrimFunc2(prim2, arg1 - 1, arg2, arg3);
+            prim2 =
+                UnkRecursivePrimFunc2(tempPrim, iterations - 1, prim2, dataPtr);
         }
     }
-    return arg2;
+    return prim2;
 }
