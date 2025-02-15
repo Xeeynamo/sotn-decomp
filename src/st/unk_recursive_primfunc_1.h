a8a569838e2b2de18269eb9e4a565345d37ab532
gamezter <charronalexandre@hotmail.com>
2025-02-15 16:38:12 -0500

    Refactor UnkRecursivePrimFunctions for clarity (#2209)
    I came across `UnkRecursivePrimFunction2` and tried to clean it up a bit
    for clarity. got rid of `rgb_set` since it seems to be a copy of
    `CVECTOR`.
    I also moved some variable declarations to the top of their scope which
    makes the code more readable to me, but I wouldnt mind reverting that.
    
    Here's my PSP scratch showing it is still a match :
    https://decomp.me/scratch/UQMqY
    
    Edit: I refactored `UnkRecursivePrimFunc1` and `func_801BB548` since
    they are very similar. These functions seem to split srcPrim into 4
    smaller prims (quads) n = `iterations` times

--- src/st/unk_recursive_primfunc_1.h
+++ src/st/unk_recursive_primfunc_1.h
@@ -1,68 +1,77 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-static u16 g_UnkRecursPrimVecOrder[] = {
-    0, 1, 3, 4, 1, 2, 4, 5, 3, 4, 6, 7, 4, 5, 7, 8};
 
-// arg7 holds, in order:
+// 0------1------2
+// |             |
+// |             |
+// 3      4      5
+// |             |
+// |             |
+// 6------7------8
+
+// clang-format off
+static s16 g_QuadIndices1[] = {
+    0, 1, 3, 4, //top left quad
+    1, 2, 4, 5, //top right quad
+    3, 4, 6, 7, //bottom left quad
+    4, 5, 7, 8, //bottom right quad
+};
+// clang-format on
+
+// dataPtr holds, in order:
 // a Primitive, an array of 9 SVECTORs, an array of 10 uvPairs, and a pointer to
-// another of arg7.
-// arg7 is not actually u8* but windows doesn't like void* I guess?
+// another of dataPtr. Pointer type on dataPtr is
+// fake, but needed for Windows.
+
+// dataPtr gets filled up with scratch data used to split srcPrim into 4 smaller
+// quads
 
 Primitive* UnkRecursivePrimFunc1(
-    SVECTOR* arg0, SVECTOR* arg1, SVECTOR* arg2, SVECTOR* arg3, Primitive* arg4,
-    s32 arg5, Primitive* arg6, u8* arg7) {
-    s32 unused1;
-    s32 unused2;
+    SVECTOR* p0, SVECTOR* p1, SVECTOR* p2, SVECTOR* p3, Primitive* srcPrim,
+    s32 iterations, Primitive* dstPrim, u8* dataPtr) {
+    long p, flag;
     s32 i;
-    Primitive* prim1;
-    Primitive* prim2;
-    s32 vecIdx1;
-    s32 vecIdx2;
-    s32 vecIdx3;
-    s32 vecIdx4;
-    s16* vecIndices;
+    Primitive* tempPrim;
+    s16* indices;
     s32 rotTransResult;
-    SVECTOR* vectors;
+    SVECTOR* points;
     uvPair* uv_values;
-    Primitive* nextArg6;
 
-    if (arg6 == NULL) {
+    if (dstPrim == NULL) {
         return NULL;
     }
-    prim2 = (Primitive*)arg7;
-    arg7 += 0x34;
-    vectors = (SVECTOR*)arg7;
-    arg7 += 0x48;
+    tempPrim = (Primitive*)dataPtr;
+    dataPtr += sizeof(Primitive);
+    points = (SVECTOR*)dataPtr;
+    dataPtr += sizeof(SVECTOR) * 9;
 
-    vectors[0] = *arg0;
-    vectors[2] = *arg1;
-    vectors[6] = *arg2;
-    vectors[8] = *arg3;
+    points[0] = *p0;
+    points[2] = *p1;
+    points[6] = *p2;
+    points[8] = *p3;
 
-    uv_values = (uvPair*)arg7;
-    arg7 += 0x14; // This is effectively arg7 = arg7->next, since this covers
-                  // the full size of arg7.
+    uv_values = (uvPair*)dataPtr;
+    dataPtr += sizeof(uvPair) * 10;
 
-    prim1 = prim2;
-    vectors[1].vx = (vectors[0].vx + vectors[2].vx + 1) >> 1;
-    vectors[1].vy = (vectors[0].vy + vectors[2].vy + 1) >> 1;
-    vectors[1].vz = (vectors[0].vz + vectors[2].vz + 1) >> 1;
-    vectors[7].vx = (vectors[6].vx + vectors[8].vx + 1) >> 1;
-    vectors[7].vy = (vectors[6].vy + vectors[8].vy + 1) >> 1;
-    vectors[7].vz = (vectors[6].vz + vectors[8].vz + 1) >> 1;
-    vectors[3].vx = (vectors[0].vx + vectors[6].vx + 1) >> 1;
-    vectors[3].vy = (vectors[0].vy + vectors[6].vy + 1) >> 1;
-    vectors[3].vz = (vectors[0].vz + vectors[6].vz + 1) >> 1;
-    vectors[5].vx = (vectors[2].vx + vectors[8].vx + 1) >> 1;
-    vectors[5].vy = (vectors[2].vy + vectors[8].vy + 1) >> 1;
-    vectors[5].vz = (vectors[2].vz + vectors[8].vz + 1) >> 1;
-    vectors[4].vx = (vectors[3].vx + vectors[5].vx + 1) >> 1;
-    vectors[4].vy = (vectors[3].vy + vectors[5].vy + 1) >> 1;
-    vectors[4].vz = (vectors[3].vz + vectors[5].vz + 1) >> 1;
+    points[1].vx = (points[0].vx + points[2].vx + 1) >> 1;
+    points[1].vy = (points[0].vy + points[2].vy + 1) >> 1;
+    points[1].vz = (points[0].vz + points[2].vz + 1) >> 1;
+    points[7].vx = (points[6].vx + points[8].vx + 1) >> 1;
+    points[7].vy = (points[6].vy + points[8].vy + 1) >> 1;
+    points[7].vz = (points[6].vz + points[8].vz + 1) >> 1;
+    points[3].vx = (points[0].vx + points[6].vx + 1) >> 1;
+    points[3].vy = (points[0].vy + points[6].vy + 1) >> 1;
+    points[3].vz = (points[0].vz + points[6].vz + 1) >> 1;
+    points[5].vx = (points[2].vx + points[8].vx + 1) >> 1;
+    points[5].vy = (points[2].vy + points[8].vy + 1) >> 1;
+    points[5].vz = (points[2].vz + points[8].vz + 1) >> 1;
+    points[4].vx = (points[3].vx + points[5].vx + 1) >> 1;
+    points[4].vy = (points[3].vy + points[5].vy + 1) >> 1;
+    points[4].vz = (points[3].vz + points[5].vz + 1) >> 1;
 
-    uv_values[0] = *(uvPair*)&arg4->u0;
-    uv_values[2] = *(uvPair*)&arg4->u1;
-    uv_values[6] = *(uvPair*)&arg4->u2;
-    uv_values[8] = *(uvPair*)&arg4->u3;
+    uv_values[0] = *(uvPair*)&srcPrim->u0;
+    uv_values[2] = *(uvPair*)&srcPrim->u1;
+    uv_values[6] = *(uvPair*)&srcPrim->u2;
+    uv_values[8] = *(uvPair*)&srcPrim->u3;
     uv_values[1].u = (uv_values[0].u + uv_values[2].u + 1) >> 1;
     uv_values[1].v = (uv_values[0].v + uv_values[2].v + 1) >> 1;
     uv_values[7].u = (uv_values[6].u + uv_values[8].u + 1) >> 1;
@@ -74,38 +83,36 @@ Primitive* UnkRecursivePrimFunc1(
     uv_values[4].u = (uv_values[3].u + uv_values[5].u + 1) >> 1;
     uv_values[4].v = (uv_values[3].v + uv_values[5].v + 1) >> 1;
 
-    *prim1 = *arg4;
-    vecIndices = (s16*)&g_UnkRecursPrimVecOrder;
+    *tempPrim = *srcPrim;
+    indices = g_QuadIndices1;
     for (i = 0; i < 4; i++) {
-        vecIdx1 = *vecIndices++;
-        vecIdx2 = *vecIndices++;
-        vecIdx3 = *vecIndices++;
-        vecIdx4 = *vecIndices++;
+        s32 idx1 = *indices++;
+        s32 idx2 = *indices++;
+        s32 idx3 = *indices++;
+        s32 idx4 = *indices++;
         rotTransResult = RotTransPers4(
-            &vectors[vecIdx1], &vectors[vecIdx2], &vectors[vecIdx3],
-            &vectors[vecIdx4], (long*)&prim2->x0, (long*)&prim2->x1,
-            (long*)&prim2->x2, (long*)&prim2->x3, (long*)&unused1,
-            (long*)&unused2);
-        *(uvPair*)&prim2->u0 = uv_values[vecIdx1];
-        *(uvPair*)&prim2->u1 = uv_values[vecIdx2];
-        *(uvPair*)&prim2->u2 = uv_values[vecIdx3];
-        *(uvPair*)&prim2->u3 = uv_values[vecIdx4];
-        if (arg5 == 1) {
-            prim1 = arg6;
+            &points[idx1], &points[idx2], &points[idx3], &points[idx4],
+            (long*)&tempPrim->x0, (long*)&tempPrim->x1, (long*)&tempPrim->x2,
+            (long*)&tempPrim->x3, &p, &flag);
+        *(uvPair*)&tempPrim->u0 = uv_values[idx1];
+        *(uvPair*)&tempPrim->u1 = uv_values[idx2];
+        *(uvPair*)&tempPrim->u2 = uv_values[idx3];
+        *(uvPair*)&tempPrim->u3 = uv_values[idx4];
+        if (iterations == 1) {
             if (rotTransResult > 0) {
-                nextArg6 = arg6->next;
-                *prim1 = *prim2;
-                arg6->next = nextArg6;
-                arg6 = nextArg6;
-                if (arg6 == NULL) {
+                Primitive* origNext = dstPrim->next;
+                *dstPrim = *tempPrim;
+                dstPrim->next = origNext;
+                dstPrim = dstPrim->next;
+                if (dstPrim == NULL) {
                     return NULL;
                 }
             }
         } else {
-            arg6 = UnkRecursivePrimFunc1(
-                &vectors[vecIdx1], &vectors[vecIdx2], &vectors[vecIdx3],
-                &vectors[vecIdx4], prim2, arg5 - 1, arg6, arg7);
+            dstPrim = UnkRecursivePrimFunc1(
+                &points[idx1], &points[idx2], &points[idx3], &points[idx4],
+                tempPrim, iterations - 1, dstPrim, dataPtr);
         }
     }
-    return arg6;
+    return dstPrim;
 }
