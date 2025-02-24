1c8873a0f1fd61c9807c18ab8fd1bfdc9bdca208
ProjectOblivion <ProjectOblivion>
2025-02-24 14:04:47 +0000

    Review fixes

--- src/st/lib/e_lesser_demon.c
+++ src/st/lib/e_lesser_demon.c
@@ -38,15 +38,10 @@ void func_us_801BB8DC(s16* unkArg) {
         if (g_CurrentEntity->ext.lesserDemon.unkB0 % 2) {
             g_CurrentEntity->palette = g_CurrentEntity->ext.lesserDemon.unkB2;
         } else {
-            g_CurrentEntity->palette = 0x819F;
+            g_CurrentEntity->palette = PAL_DRA(0x819F);
         }
 
-#ifdef VERSION_PSP
         if (!(--g_CurrentEntity->ext.lesserDemon.unkB0)) {
-#else
-        if (!(--g_CurrentEntity->ext.lesserDemon.unkB0 << 16)) {
-#endif
-
             g_CurrentEntity->palette = g_CurrentEntity->ext.lesserDemon.unkB2;
             g_CurrentEntity->hitboxState = 3;
             SetStep(1);
