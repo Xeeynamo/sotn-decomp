dcfe9b6fc7430eed70df0f64a475ce6035175ccf
Joey Murphy <tjmurphy@mit.edu>
2025-02-28 07:29:46 -0500

    Unsplit PSP to match

---
+++ src/dra_psp/59E20.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../dra/dra.h"
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_091367A0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09136BF8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09136D18);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09136E18);
