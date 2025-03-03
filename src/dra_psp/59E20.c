17694f1a505b97bccf56219486c0120cc1c9f0e7
bismurphy <tjmurphy@mit.edu>
2025-03-03 09:17:53 -0500

    Prepare file alignments for next PSP DRA codeshare (#2277)
    Same as usual, just changing file splits to make them the same between
    versions. Decompiled a couple on PSP to make sure I was in the right
    place. As usual, PSP file will end up deleted once code is shared
    between versions.
    
    Looks like this file is just a whole bunch of helper functions, possibly
    all for transformation-related code.

--- src/dra_psp/59E20.c
+++ src/dra_psp/59E20.c
@@ -1,10 +1,125 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
+#include "../dra/dra_bss.h"
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_091367A0);
+void func_8012C600(void) {
+    s32 x, y;
+    s32 i;
+    s32 t0 = PLAYER.posX.i.hi;
+    s32 t1 = PLAYER.posY.i.hi;
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09136BF8);
+    x = g_Entities[20].posX.i.hi + 8;
+    for (i = 0; i < 9; i++) {
+        if (x <= g_Entities[i + 21].posX.i.hi + 4) {
+            x = g_Entities[i + 21].posX.i.hi + 4;
+        }
+    }
+    D_8013AEBC[0] = x - t0;
 
+    x = g_Entities[20].posX.i.hi - 8;
+    for (i = 0; i < 9; i++) {
+        if (x >= g_Entities[i + 21].posX.i.hi - 4) {
+            x = g_Entities[i + 21].posX.i.hi - 4;
+        }
+    }
+    D_8013AEBC[2] = x - t0;
+
+    y = g_Entities[21].posY.i.hi;
+    for (i = 0; i < 9; i++) {
+        if (y >= g_Entities[i + 21].posY.i.hi) {
+            y = g_Entities[i + 21].posY.i.hi;
+        }
+    }
+    y = y - 4;
+    if (y >= g_Entities[30].posY.i.hi - 12) {
+        y = g_Entities[30].posY.i.hi - 12;
+    }
+    if (y >= g_Entities[20].posY.i.hi - 8) {
+        y = g_Entities[20].posY.i.hi - 8;
+    }
+    D_8013AEBC[3] = y - t1;
+    if (D_8013AEBC[3] < -24) {
+        D_8013AEBC[3] = -24;
+    }
+
+    y = g_Entities[21].posY.i.hi;
+    for (i = 0; i < 9; i++) {
+        if (y <= g_Entities[i + 21].posY.i.hi) {
+            y = g_Entities[i + 21].posY.i.hi;
+        }
+    }
+    y = y + 4;
+
+    if (y <= PLAYER.posY.i.hi + 28) {
+        y = PLAYER.posY.i.hi + 28;
+    }
+    if (y <= g_Entities[17].posY.i.hi + 28) {
+        y = g_Entities[17].posY.i.hi + 28;
+    }
+    D_8013AEBC[1] = y - t1;
+    if (D_8013AEBC[1] > 0x27) {
+        D_8013AEBC[1] = 0x27;
+    }
+
+    if (!PLAYER.facingLeft) {
+        if (D_8013AEBC[2] < -0x2C) {
+            D_8013AEBC[2] = -0x2C;
+        }
+        if (D_8013AEBC[0] > 0x13) {
+            D_8013AEBC[0] = 0x13;
+        }
+    } else {
+        if (D_8013AEBC[2] < -0x13) {
+            D_8013AEBC[2] = -0x13;
+        }
+        if (D_8013AEBC[0] > 0x2C) {
+            D_8013AEBC[0] = 0x2C;
+        }
+    }
+}
+
+bool WolfFormFinished(void) {
+    if (PLAYER.step_s == 0 || PLAYER.step_s == 8) {
+        return false;
+    }
+    if (D_80097448[1] != 0 && !IsRelicActive(RELIC_HOLY_SYMBOL) ||
+        g_Player.padTapped & PAD_L1 ||
+        HandleTransformationMP(FORM_WOLF, REDUCE) < 0) {
+        SetPlayerStep(Player_UnmorphWolf);
+        SetPlayerAnim(0xCA);
+        D_800AFDA4[1] = 1;
+        PLAYER.palette = 0x810D;
+        g_Player.unk66 = 0;
+        g_Player.unk68 = 0;
+        // Create factory for EntityPlayerBlinkWhite
+        CreateEntFactoryFromEntity(g_CurrentEntity, FACTORY(44, 0x24), 0);
+        PLAYER.velocityY >>= 1;
+        return true;
+    }
+    return false;
+}
+
+// func_8012C97C
 INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09136D18);
-
+// func_8012CA64
 INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09136E18);
+// func_8012CB0C
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09136EE0);
+// func_8012CB4C
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09136F30);
+// func_8012CC30
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09137038);
+// func_8012CCE4
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09137110);
+// func_8012CED4
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09137358);
+// func_8012CFA8
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09137448);
+// func_8012CFF0
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09137498);
+// func_8012D024
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_091374D0);
+// func_8012D178
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09137668);
+// func_8012D28C (confirmed)
+INCLUDE_ASM("dra_psp/psp/dra_psp/59E20", func_psp_09137748);
