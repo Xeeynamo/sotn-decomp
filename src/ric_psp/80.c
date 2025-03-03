11d145c24c8d5636017be02306cce40bfcab48fc
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-03-03 13:31:39 +0000

    Add most function symbols

--- src/ric_psp/80.c
+++ src/ric_psp/80.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A6280);
@@ -8,91 +9,91 @@ INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A62D8);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A6328);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A6610);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80158B04);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A6728);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleStand);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A6AF8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleWalk);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A6B80);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleRun);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A6C80);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleJump);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A7340);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleFall);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A73E8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleCrouch);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A79F0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80159BC8);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A7A38);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80159C04);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A7B98);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleHit);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A8AD8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleBossGrab);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A8D28);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleDead);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A94A0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleStandInAir);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A9560);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleEnableFlameWhip);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A9670);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleHydrostorm);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A96D8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleGenericSubwpnCrash);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A9738);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleThrowDaggers);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A9840);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleDeadPrologue);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092A9EA0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleSlide);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AA248);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AA388);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_8015BCD0);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AA678);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleSlideKick);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AA9F8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleBladeDash);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AAB48);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicHandleHighJump);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AAD60);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnAxe);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AB7A8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashAxe);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AC2A8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnDagger);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092ACE78);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092ACF18);
+INCLUDE_ASM("ric_psp/nonmatchings/80", DestroyEntity);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092ACFB0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", GetTeleportToOtherCastle);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AD108);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80156DE4);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AD298);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicInit);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AD730);
+INCLUDE_ASM("ric_psp/nonmatchings/80", CheckStageCollision);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092ADEA0);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AE150);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AE368);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicMain);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AF450);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_8015CC70);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AF4D0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetCrouch);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AF5A8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetStand);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AF600);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AF680);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetWalk);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AF738);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetFall);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AF898);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetJump);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AF9D0);
 
@@ -102,9 +103,9 @@ INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AFB68);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092AFDA8);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B00F0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicDoCrash);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B0580);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetDeadPrologue);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B05A8);
 
@@ -112,31 +113,31 @@ INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B0640);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B06F8);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B07A8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_8015C4AC);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B0A10);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B0E38);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetStep);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B0E60);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetAnimation);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B0EA0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicDecelerateX);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B0F40);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicCheckFacing);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B1038);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetSpeedX);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B1088);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_8015CAAC);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B10D0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetInvincibilityFrames);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B1178);
+INCLUDE_ASM("ric_psp/nonmatchings/80", DisableAfterImage);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B1238);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_8015CC28);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B1270);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicCheckInput);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B1C60);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicGetPlayerSensor);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B1CD0);
 
@@ -146,59 +147,59 @@ INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B2DD8);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B3118);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B3468);
+INCLUDE_ASM("ric_psp/nonmatchings/80", ReboundStoneBounce1);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B34F0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", ReboundStoneBounce2);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B3578);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnReboundStone);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B4030);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnThrownVibhuti);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B4688);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B4758);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnAgunea);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B4DE0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityAguneaHitEnemy);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B5790);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityVibhutiCrashCloud);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B59F0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashVibhuti);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B5EF8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashReboundStoneParticles);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B5FC8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_8016D9C4);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B6718);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashReboundStoneExplosion);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B6C58);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashReboundStone);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B6DD8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashBibleBeam);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B7530);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashBible);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B7FF8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_8016F198);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092B8978);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashStopwatchDoneSparkle);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BA148);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityStopwatchCrashLightning);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BA218);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashStopwatch);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BA338);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BA540);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BA5F8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityAguneaLightning);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BAEE8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityAguneaCircle);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BB860);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnStopwatchCircle);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BBD10);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnStopwatch);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BDAC8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnBibleTrail);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BDD88);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnBible);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BE4C8);
 
@@ -208,106 +209,106 @@ INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BE628);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BE6F0);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BE788);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicSetSubweaponParams);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BE888);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BEA38);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BEC98);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityHitByHoly);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BF050);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityHitByDark);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BF278);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityDummy);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BF288);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicUpdatePlayerEntities);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BF628);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicCreateEntFactoryFromEntity);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BF718);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityFactory);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BFDF0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySlideKick);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092BFF48);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80160D2C);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C00B8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityBladeDash);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C01C8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80160F0C);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C02B8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySmokePuff);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C09D8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityHitByCutBlood);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C1478);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80161C2C);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C1868);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80161EF8);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C19B8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityApplyMariaPowerAnim);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C1EF0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_801623E0);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C2218);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80162604);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C25A0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityMariaPowers);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C2B90);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityNotImplemented4);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C2BA0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityMaria);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C2E98);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C2F50);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityPlayerBlinkWhite);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C4C00);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_801641A0);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C4FF8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityShrinkingPowerUpRing);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C5DD8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityHitByIce);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C69A8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityHitByLightning);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C73B0);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C77B8);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C77D8);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityTeleport);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C8228);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashCrossBeam);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C8D48);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C8D90);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityNotImplemented3);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C8DA0);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80167A70);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C9480);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityCrashHydroStorm);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C9988);
 
 INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C9B88);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092C9C98);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnHolyWater);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CA460);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnHolyWaterFlame);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CAD68);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityWhip);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CC700);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityArmBrandishWhip);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CC930);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80167964);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CCA78);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityNotImplemented1);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CCA88);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityNotImplemented2);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CCA98);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnCrashCross);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CD120);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntityRevivalColumn);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CD528);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnCross);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CDD08);
+INCLUDE_ASM("ric_psp/nonmatchings/80", func_80169C10);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CDF18);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnCrossTrail);
 
-INCLUDE_ASM("ric_psp/nonmatchings/80", func_pspeu_092CE170);
+INCLUDE_ASM("ric_psp/nonmatchings/80", RicEntitySubwpnCrashCrossParticles);
