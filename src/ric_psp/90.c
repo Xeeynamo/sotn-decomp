323faf026556ffbb4e13d4f729a0a005b81568a5
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-03-03 15:08:58 +0000

    RicDebugEnter, RicDebugExit

--- src/ric_psp/90.c
+++ src/ric_psp/90.c
@@ -2,8 +2,19 @@
 #include "../ric/ric.h"
 #include <player.h>
 
-INCLUDE_ASM("ric_psp/nonmatchings/90", RicDebugEnter);
+static void RicDebugEnter(void) {
+    g_IsRicDebugEnter = true;
+    g_RicDebugCurFrame = PLAYER.animCurFrame;
+    g_RicDebugDrawFlags = PLAYER.drawFlags;
+    g_RicDebugPalette = PLAYER.palette;
+}
 
-INCLUDE_ASM("ric_psp/nonmatchings/90", RicDebugExit);
+void RicDebugExit(void) {
+    g_IsRicDebugEnter = false;
+    PLAYER.animCurFrame = g_RicDebugCurFrame;
+    PLAYER.drawFlags = g_RicDebugDrawFlags;
+    PLAYER.palette = g_RicDebugPalette;
+    PLAYER.hitParams = 0;
+}
 
 INCLUDE_ASM("ric_psp/nonmatchings/90", RicDebug);
