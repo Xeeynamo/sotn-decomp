24126e9fa0aef1fc52bc557f9266e43957191ce2
ProjectOblivion <ProjectOblivion>
2025-03-04 01:39:37 +0000

    Added get_functions and get_merged_functions functions, and consolidated psp targets

--- Makefile.psp.mk
+++ Makefile.psp.mk
@@ -57,59 +57,6 @@ $(addprefix $(BUILD_DIR)/%,.BIN .bin _raw.bin .exe): $(BUILD_DIR)/$$(call get_fi
 $(BUILD_DIR)/%.ld: $(CONFIG_DIR)/splat.$(VERSION).%.yaml $(BASE_SYMBOLS) $(CONFIG_DIR)/symbols.$(VERSION).%.txt
 	$(SPLAT) $<
 
-# This isn't ideal, but it works for now.
-ST_DRA_MERGE = $(shell $(INLINE_PYTHON) $$'import yaml;\
-import os;\
-yaml_file=open(os.path.join(os.getcwd(),"config/splat.pspeu.dra.yaml"));\
-config = yaml.safe_load(yaml_file);\
-yaml_file.close();\
-print(" ".join([x[2].split("/")[1] for x in config["segments"][1]["subsegments"] if type(x) == list and x[1] == "c" and x[2].startswith("dra/")]))')
-$(BUILD_DIR)/dra.elf: $(BUILD_DIR)/dra.ld $(addprefix $(BUILD_DIR)/src/dra/,$(addsuffix .c.o,$(ST_DRA_MERGE))) $$(call list_o_files,dra_psp)
-	$(call link,dra,$@)
-$(BUILD_DIR)/tt_%.elf: $(BUILD_DIR)/tt_%.ld $$(call list_o_files,servant/tt_$$*) $(BUILD_DIR)/assets/servant/tt_%/mwo_header.bin.o
-	$(call link,tt_$*,$@)
-
-# This isn't ideal, but it works for now.
-ST_LIB_MERGE = $(shell $(INLINE_PYTHON) $$'import yaml;\
-import os;\
-yaml_file=open(os.path.join(os.getcwd(),"config/splat.pspeu.stlib.yaml"));\
-config = yaml.safe_load(yaml_file);\
-yaml_file.close();\
-print(" ".join([x[2].split("/")[1] for x in config["segments"][1]["subsegments"] if type(x) == list and x[1] == "c" and x[2].startswith("lib/")]))')
-$(BUILD_DIR)/stlib.elf: $(BUILD_DIR)/stlib.ld $(addprefix $(BUILD_DIR)/src/st/lib/,$(addsuffix .c.o,$(ST_LIB_MERGE))) $$(call list_o_files,st/lib_psp) $(BUILD_DIR)/assets/st/lib/mwo_header.bin.o
-	$(call link,stlib,$@)
-
-# This isn't ideal, but it works for now.
-ST_NO4_MERGE = $(shell $(INLINE_PYTHON) $$'import yaml;\
-import os;\
-yaml_file=open(os.path.join(os.getcwd(),"config/splat.pspeu.stno4.yaml"));\
-config = yaml.safe_load(yaml_file);\
-yaml_file.close();\
-print(" ".join([x[2].split("/")[1] for x in config["segments"][1]["subsegments"] if type(x) == list and x[1] == "c" and x[2].startswith("no4/")]))')
-$(BUILD_DIR)/stno4.elf: $(BUILD_DIR)/stno4.ld $(addprefix $(BUILD_DIR)/src/st/no4/,$(addsuffix .c.o,$(ST_NO4_MERGE))) $$(call list_o_files,st/no4_psp) $(BUILD_DIR)/assets/st/no4/mwo_header.bin.o
-	$(call link,stno4,$@)
-
-# This isn't ideal, but it works for now.
-ST_ST0_MERGE = $(shell $(INLINE_PYTHON) $$'import yaml;\
-import os;\
-yaml_file=open(os.path.join(os.getcwd(),"config/splat.pspeu.stst0.yaml"));\
-config = yaml.safe_load(yaml_file);\
-yaml_file.close();\
-print(" ".join([x[2].split("/")[1] for x in config["segments"][1]["subsegments"] if type(x) == list and x[1] == "c" and x[2].startswith("st0/")]))')
-$(BUILD_DIR)/stst0.elf: $(BUILD_DIR)/stst0.ld $(addprefix $(BUILD_DIR)/src/st/st0/,$(addsuffix .c.o,$(ST_ST0_MERGE))) $$(call list_o_files,st/st0_psp) $(BUILD_DIR)/assets/st/st0/mwo_header.bin.o
-	$(call link,stst0,$@)
-
-# This isn't ideal, but it works for now.
-ST_WRP_MERGE = $(shell $(INLINE_PYTHON) $$'import yaml;\
-import os;\
-yaml_file=open(os.path.join(os.getcwd(),"config/splat.pspeu.stwrp.yaml"));\
-config = yaml.safe_load(yaml_file);\
-yaml_file.close();\
-print(" ".join([x[2].split("/")[1] for x in config["segments"][1]["subsegments"] if type(x) == list and x[1] == "c" and x[2].startswith("wrp/")]))')
-$(BUILD_DIR)/stwrp.elf: $(BUILD_DIR)/stwrp.ld $(addprefix $(BUILD_DIR)/src/st/wrp/,$(addsuffix .c.o,$(ST_WRP_MERGE))) $$(call list_o_files,st/wrp_psp) $(BUILD_DIR)/assets/st/wrp/mwo_header.bin.o
-	$(call link,stwrp,$@)
-
-# Recipes
 $(BUILD_DIR)/%.s.o: %.s
 	@mkdir -p $(dir $@)
 	$(AS) $(AS_FLAGS) -o $@ $<
@@ -122,3 +69,21 @@ $(BUILD_DIR)/%.c.o: %.c $(MWCCPSP) $(MWCCGAP_APP)
 $(BUILD_DIR)/assets/%/mwo_header.bin.o: assets/%/mwo_header.bin
 	@mkdir -p $(dir $@)
 	$(LD) -r -b binary -o $@ $<
+
+define get_merged_functions 
+$(shell $(INLINE_PYTHON) 'import yaml;\
+import os;\
+yaml_file=open(os.path.join(os.getcwd(),"config/splat.$(VERSION).$(2)$(1).yaml"));\
+config = yaml.safe_load(yaml_file);\
+yaml_file.close();\
+print(" ".join([x[2].split("/")[1] for x in config["segments"][1]["subsegments"] if type(x) == list and x[1] == "c" and x[2].startswith("$(1)/")]))')
+endef
+
+get_functions = $(addprefix $(BUILD_DIR)/src/$(2)/$(1)/,$(addsuffix .c.o,$(call get_merged_functions,$(1),$(2))))
+
+$(addprefix $(BUILD_DIR)/,$(addsuffix .elf,$(filter-out main,$(GAME)))): $(BUILD_DIR)/%.elf: $(BUILD_DIR)/%.ld $$(call get_functions,%) $$(call list_o_files,%_psp)
+	$(call link,$*,$@)
+$(BUILD_DIR)/st%.elf: $(BUILD_DIR)/st%.ld $$(call get_functions,%,st) $$(call list_o_files,st/%_psp) $(BUILD_DIR)/assets/st/%/mwo_header.bin.o
+	$(call link,st$*,$@)
+$(BUILD_DIR)/tt_%.elf: $(BUILD_DIR)/tt_%.ld $$(call list_o_files,servant/tt_$$*) $(BUILD_DIR)/assets/servant/tt_%/mwo_header.bin.o
+	$(call link,tt_$*,$@)
