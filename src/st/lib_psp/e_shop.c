5a37aba48accbc651d23d2f1d78b7aa64e91d5fd
gamezter <gamezter93@gmail.com>
2025-03-05 12:33:37 -0500

    Decompile func_us_801B245C

--- src/st/lib_psp/e_shop.c
+++ src/st/lib_psp/e_shop.c
@@ -1,7 +1,162 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../lib/lib.h"
 
-INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_shop", func_us_801B245C);
+void func_us_801B245C(Primitive* arg0, u16 arg1, u16 arg2, u16 arg3, u16 arg4,
+                      s32 arg5, s32 arg6) {
+    Primitive* prim;
+    s16 i;
+    s16 offset;
+
+    prim = arg0;
+    for (i = 0; i < 6; i++) {
+        arg0 = arg0->next;
+    }
+    for (i = 0; i < 2; i++) {
+        offset = (rsin(arg1) * arg3) / 0x1000;
+        if (i) {
+            arg0->x1 = arg0->x3 = arg0->x0 + offset;
+            if (arg5 & 1) {
+                arg0->r0 = arg0->r1 = (arg2 / 2) + 0x30;
+            } else {
+                arg0->r0 = arg0->r1 = 0;
+            }
+            if (arg5 & 2) {
+                arg0->r2 = arg0->r3 = (arg2 / 2) + 0x30;
+            } else {
+                arg0->r2 = arg0->r3 = 0;
+            }
+            if (arg5 & 4) {
+                arg0->g0 = arg0->g1 = (arg2 / 2) + 0x30;
+            } else {
+                arg0->g0 = arg0->g1 = 0;
+            }
+            if (arg5 & 8) {
+                arg0->g2 = arg0->g3 = (arg2 / 2) + 0x30;
+            } else {
+                arg0->g2 = arg0->g3 = 0;
+            }
+            if (arg5 & 0x10) {
+                arg0->b0 = arg0->b1 = (arg2 / 2) + 0x30;
+            } else {
+                arg0->b0 = arg0->b1 = 0;
+            }
+            if (arg5 & 0x20) {
+                arg0->b2 = arg0->b3 = (arg2 / 2) + 0x30;
+            } else {
+                arg0->b2 = arg0->b3 = 0;
+            }
+        } else {
+            arg0->x0 = arg0->x2 = arg0->x1 - offset;
+            if (arg5 & 1) {
+                arg0->r0 = arg0->r1 = arg2 + 0x20;
+            } else {
+                arg0->r0 = arg0->r1 = 0;
+            }
+            if (arg5 & 2) {
+                arg0->r2 = arg0->r3 = arg2 + 0x20;
+            } else {
+                arg0->r2 = arg0->r3 = 0;
+            }
+            if (arg5 & 4) {
+                arg0->g0 = arg0->g1 = arg2 + 0x20;
+            } else {
+                arg0->g0 = arg0->g1 = 0;
+            }
+            if (arg5 & 8) {
+                arg0->g2 = arg0->g3 = arg2 + 0x20;
+            } else {
+                arg0->g2 = arg0->g3 = 0;
+            }
+            if (arg5 & 0x10) {
+                arg0->b0 = arg0->b1 = arg2 + 0x20;
+            } else {
+                arg0->b0 = arg0->b1 = 0;
+            }
+            if (arg5 & 0x20) {
+                arg0->b2 = arg0->b3 = arg2 + 0x20;
+            } else {
+                arg0->b2 = arg0->b3 = 0;
+            }
+        }
+        offset = (arg4 * rcos(g_CurrentEntity->ext.et_801B6F30.unk7E)) / 0x1000;
+        if (i) {
+            arg0->y1 = arg0->y0 - offset;
+            arg0->y3 = arg0->y2 + offset;
+            prim->x0 = arg0->x1;
+            prim->y0 = arg0->y1;
+            prim->x1 = arg0->x3;
+            prim->y1 = arg0->y3;
+            prim->drawMode = DRAW_DEFAULT;
+            prim = prim->next;
+        } else {
+            arg0->y0 = arg0->y1 - offset;
+            arg0->y2 = arg0->y3 + offset;
+            prim->x0 = arg0->x0;
+            prim->y0 = arg0->y0;
+            prim->x1 = arg0->x2;
+            prim->y1 = arg0->y2;
+            prim->drawMode = DRAW_DEFAULT;
+            prim = prim->next;
+        }
+        prim->x0 = arg0->x0;
+        prim->y0 = arg0->y0;
+        prim->x1 = arg0->x1;
+        prim->y1 = arg0->y1;
+        prim->drawMode = DRAW_DEFAULT;
+        prim = prim->next;
+
+        prim->x0 = arg0->x2;
+        prim->y0 = arg0->y2;
+        prim->x1 = arg0->x3;
+        prim->y1 = arg0->y3;
+        prim->drawMode = DRAW_DEFAULT;
+        prim = prim->next;
+
+        if (arg6) {
+            arg0->drawMode =
+                DRAW_UNK_400 | DRAW_TPAGE | DRAW_COLORS | DRAW_TRANSP;
+        } else {
+            arg0->drawMode = DRAW_UNK_400 | DRAW_COLORS;
+        }
+        arg0 = arg0->next;
+    }
+    for (i = 0; i < 2; i++) {
+        offset = (rsin(arg1) * (arg3 - 8)) / 0x1000;
+        if (i) {
+            arg0->x1 = arg0->x3 = arg0->x0 + offset;
+        } else {
+            arg0->x0 = arg0->x2 = arg0->x1 - offset;
+        }
+        offset = (arg4 * rcos(arg1) * 3) / 0x4000;
+        if (i) {
+            arg0->y1 = arg0->y0 - offset;
+            arg0->y3 = arg0->y2 + offset;
+            PRED(arg0) = arg2 + 0x60;
+            PGRN(arg0) = arg2 + 0x60;
+            PBLU(arg0) = arg2 + 0x60;
+        } else {
+            arg0->y0 = arg0->y1 - offset;
+            arg0->y2 = arg0->y3 + offset;
+            PRED(arg0) = (arg2 * 2) + 0x40;
+            PGRN(arg0) = (arg2 * 2) + 0x40;
+            PBLU(arg0) = (arg2 * 2) + 0x40;
+        }
+#ifdef VERSION_PSP
+        if (arg1 == 0x400) {
+            if (i) {
+                arg0->x1 = arg0->x3 = arg0->x0 + (arg0->u1 - arg0->u0);
+            } else {
+                arg0->x0 = arg0->x2 = arg0->x1 - (arg0->u1 - arg0->u0);
+            }
+            arg0->y2 = arg0->y3 = arg0->y0 + (arg0->v2 - arg0->v0);
+        }
+        arg0->drawMode = DRAW_DEFAULT;
+#else
+        arg0->drawMode = DRAW_COLORS;
+#endif
+        arg0 = arg0->next;
+    }
+}
 
 // func_us_801B29C4
 INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_shop", func_psp_09261338);
