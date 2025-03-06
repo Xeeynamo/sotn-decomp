a828b9fd65eee2fbb797285b664c63b3ff5308af
ProjectOblivion <ProjectOblivion>
2025-03-06 01:26:58 +0000

    psp.mk fully cleaned up and organized

--- Makefile.psp.mk
+++ Makefile.psp.mk
@@ -4,17 +4,11 @@
 # Reverse stage OVL options: rare rcat rcen rchi rdai rlib rno0 rno1 rno2 rno3 rno4 rnz0 rnz1 rtop rwrp
 # Boss OVL options: bo0 bo1 bo2 bo3 bo4 bo5 bo6 bo7 mar rbo0 rbo1 rbo2 rbo3 rbo4 rbo5 rbo6 rbo7 rbo8
 # Servant OVL options: tt_000 tt_001 tt_002 tt_003 tt_004 tt_005 tt_006
-
 GAME		:= dra
 STAGES		:= lib no4 st0 wrp
 STAGES		+= 
 BOSSES		:= 
 SERVANTS	:= tt_000
-
-# Flags
-AS_FLAGS        += -EL -I include/ -G0 -march=allegrex -mabi=eabi
-MWCCPSP_FLAGS   := -gccinc -Iinclude -D_internal_version_$(VERSION) -c -lang c -sdatathreshold 0 -char unsigned -fl divbyzerocheck
-MWLDPSP_FLAGS   := -partial -nostdlib -msgstyle gcc -sym full,elf -g
 
 # Tools
 ALLEGREX_AS     := $(BIN_DIR)/allegrex-as
@@ -22,44 +16,52 @@ AS              := $(ALLEGREX_AS)
 WIBO            := $(BIN_DIR)/wibo
 MWCCPSP         := $(BIN_DIR)/mwccpsp.exe
 CCPSP           := MWCIncludes=$(BIN_DIR) $(WIBO) $(MWCCPSP)
-
 MWASPSP         := $(WIBO) $(BIN_DIR)/asm_psp_elf.exe -gnu
 MWLDPSP         := $(WIBO) $(BIN_DIR)/mwldpsp.exe
 
-MWCCGAP_DIR     := $(TOOLS_DIR)/mwccgap
-MWCCGAP_APP     := $(MWCCGAP_DIR)/mwccgap.py
-MWCCGAP         := $(PYTHON) $(MWCCGAP_APP)
+# Flags
+AS_FLAGS        += -EL -I include/ -G0 -march=allegrex -mabi=eabi
+MWCCPSP_FLAGS   := -gccinc -Iinclude -D_internal_version_$(VERSION) -c -lang c -sdatathreshold 0 -char unsigned -fl divbyzerocheck
+MWLDPSP_FLAGS   := -partial -nostdlib -msgstyle gcc -sym full,elf -g
 
-# Any file in this list will get -O4,p instead of -Op
-OPT_HI_FUNCS = $(addsuffix .c.o,33F0 A710 C0B0 EC60 186E8 61F30 624DC 628AC 63C90 64EE0)
-OPT_LEVEL = $(if $(filter $(notdir $@),$(OPT_HI_FUNCS)),-O4$(comma)p,-Op)
+# Compiler
+OPT_HI_FUNCS	:= $(addsuffix .c.o,33F0 A710 C0B0 EC60 186E8 61F30 624DC 628AC 63C90 64EE0) # These objects will get -O4,p instead of -Op
+COMPILER		:= $(MWCCGAP)
+COMPILER_ARGS	 = --mwcc-path $(MWCCPSP) --use-wibo --wibo-path $(WIBO) --as-path $(AS) --asm-dir-prefix asm/pspeu --macro-inc-path include/macro.inc $(MWCCPSP_FLAGS) $(OPT_LEVEL)
+COMPILER_REQS	:= $(MWCCPSP) $(MWCCGAP_APP)
+OPT_LEVEL		 = $(if $(filter $(notdir $@),$(OPT_HI_FUNCS)),-O4$(comma)p,-Op)
 
-build_pspeu: $(call get_targets)
-extract_pspeu: $(addprefix $(BUILD_DIR)/,$(addsuffix .ld,$(call get_targets,prefixed)))
-$(call get_targets): %: $(BUILD_DIR)/%.bin
-
-$(addprefix $(BUILD_DIR)/%,.BIN .bin _raw.bin .exe): $(BUILD_DIR)/$$(call get_filename,%,st,bo).elf
-	$(OBJCOPY) -O binary $< $@
-
-$(BUILD_DIR)/%.ld: $(CONFIG_DIR)/splat.$(VERSION).%.yaml $(BASE_SYMBOLS) $(CONFIG_DIR)/symbols.$(VERSION).%.txt
+# Step 1/2 of extract
+$(BUILD_DIR:$(VERSION)=pspeu)/%.ld: $(CONFIG_DIR)/splat.$(VERSION).%.yaml $(BASE_SYMBOLS) $(CONFIG_DIR)/symbols.$(VERSION).%.txt
 	$(SPLAT) $<
 
-$(BUILD_DIR)/%.s.o: %.s
-	@mkdir -p $(dir $@)
-	$(AS) $(AS_FLAGS) -o $@ $<
+# Step 2/2 of extract
+extract_pspeu: $(addprefix $(BUILD_DIR)/,$(addsuffix .ld,$(call get_targets,prefixed)))
 
-
-$(BUILD_DIR)/%.c.o: %.c $(MWCCPSP) $(MWCCGAP_APP)
-	@mkdir -p $(dir $@)
-	$(MWCCGAP) $< $@ --mwcc-path $(MWCCPSP) --use-wibo --wibo-path $(WIBO) --as-path $(AS) --asm-dir-prefix asm/pspeu --macro-inc-path include/macro.inc $(MWCCPSP_FLAGS) $(OPT_LEVEL)
-
+# Step 1/5 of build
+$(BUILD_DIR)/%.s.o: %.s # Shared
+	mkdir -p $(dir $@); $(AS) $(AS_FLAGS) -o $@ $<
+$(BUILD_DIR)/%.c.o: %.c $(COMPILER_REQS) # Shared
+	mkdir -p $(dir $@); $(COMPILER) $< $(call if_version,pspeu,$@) $(COMPILER_ARGS) $(call if_version,us hd,$@)
 $(BUILD_DIR)/assets/%/mwo_header.bin.o: assets/%/mwo_header.bin
-	@mkdir -p $(dir $@)
-	$(LD) -r -b binary -o $@ $<
+	mkdir -p $(dir $@); $(LD) -r -b binary -o $@ $<
 
-$(addprefix $(BUILD_DIR)/,$(addsuffix .elf,$(filter-out main,$(GAME)))): $(BUILD_DIR)/%.elf: $(BUILD_DIR)/%.ld $$(call get_functions,%) $$(call list_o_files,%_psp)
+# Step 2/5 of build
+$(addprefix $(BUILD_DIR:$(VERSION)=pspeu)/,$(addsuffix .elf,$(filter-out main,$(GAME)))): $(BUILD_DIR)/%.elf: $(BUILD_DIR)/%.ld $$(call get_functions,%) $$(call list_o_files,%_psp)
 	$(call link,$*,$@)
-$(BUILD_DIR)/st%.elf: $(BUILD_DIR)/st%.ld $$(call get_functions,%,st) $$(call list_o_files,st/%_psp) $(BUILD_DIR)/assets/st/%/mwo_header.bin.o
+$(BUILD_DIR:$(VERSION)=pspeu)/st%.elf: $(BUILD_DIR)/st%.ld $$(call get_functions,%,st) $$(call list_o_files,st/%_psp) $(BUILD_DIR)/assets/st/%/mwo_header.bin.o
 	$(call link,st$*,$@)
-$(BUILD_DIR)/tt_%.elf: $(BUILD_DIR)/tt_%.ld $$(call list_o_files,servant/tt_$$*) $(BUILD_DIR)/assets/servant/tt_%/mwo_header.bin.o
+$(BUILD_DIR:$(VERSION)=pspeu)/bo%.elf: $(BUILD_DIR)/bo%.ld $$(call get_functions,%,bo) $$(call list_o_files,boss/%_psp) $(BUILD_DIR)/assets/boss/%/mwo_header.bin.o
+	$(call link,st$*,$@)
+$(BUILD_DIR:$(VERSION)=pspeu)/tt_%.elf: $(BUILD_DIR)/tt_%.ld $$(call list_o_files,servant/tt_$$*) $(BUILD_DIR)/assets/servant/tt_%/mwo_header.bin.o
 	$(call link,tt_$*,$@)
+
+# Step 3/5 of build
+$(addprefix $(BUILD_DIR)/%,.BIN .bin _raw.bin .exe): $(BUILD_DIR)/$$(call get_filename,%,st,bo).elf # Shared
+	$(OBJCOPY) -O binary $< $@
+
+# Step 4/5 of build
+$(addsuffix _psp,$(call get_targets)): %_psp: $(BUILD_DIR)/%.bin
+
+# Step 5/5 of build
+build_pspeu: $(addsuffix _psp,$(call get_targets))
