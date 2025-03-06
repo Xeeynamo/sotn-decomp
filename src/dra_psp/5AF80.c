3d56dba537deea33bb3448b75e67705aaed0a423
Joey Murphy <tjmurphy@mit.edu>
2025-03-05 20:49:58 -0500

    format

--- src/dra_psp/5AF80.c
+++ src/dra_psp/5AF80.c
@@ -2,29 +2,29 @@
 #include "../dra/dra.h"
 #include "../dra/dra_bss.h"
 
-//func_8012D3E8
+// func_8012D3E8
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_09137900);
-//func_8012DBBC
+// func_8012DBBC
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_09138240);
-//func_8012DF04
+// func_8012DF04
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_09138600);
-//func_8012E040
+// func_8012E040
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_09138778);
-//func_8012E550
+// func_8012E550
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_09138DA0);
-//func_8012E7A4
+// func_8012E7A4
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_09139068);
-//func_8012E9C0
+// func_8012E9C0
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_091392D8);
-//func_8012EAD0
+// func_8012EAD0
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_09139428);
-//func_8012ED30
+// func_8012ED30
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_091396C8);
-//func_8012EF2C (confirmed)
+// func_8012EF2C (confirmed)
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_091398F0);
-//func_8012F178
+// func_8012F178
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_09139B50);
-//func_8012F83C
+// func_8012F83C
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_psp_0913A3A0);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/5AF80", func_8012F894);
