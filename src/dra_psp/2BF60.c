bdab8755b73bd5e71c4d3d1469edf21d7dcf9a96
Joey Murphy <tjmurphy@mit.edu>
2025-03-09 08:56:51 -0400

    Start of files aligned

--- src/dra_psp/2BF60.c
+++ src/dra_psp/2BF60.c
@@ -1,10 +1,68 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
-
+// func_80111928
 INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_091088E0);
-
+// func_80111938
 INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_091088F0);
-
+// func_8011197C
 INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_09108938);
-
+// func_801119C4
 INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_09108988);
+// func_80111CC0
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_80111CC0);
+// func_80111D24
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_09108D40);
+// func_80111DE8
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_09108E48);
+// func_8011203C
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_8011203C);
+// PlayerStepStand
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_09109200);
+// PlayerStepWalk
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910A138);
+// PlayerStepJump
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910A198);
+// PlayerStepFall
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910A9D0);
+// PlayerStepCrouch
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910AA60);
+// func_801139CC
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910B660);
+// PlayerStepHighJump
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910B768);
+// func_80113D7C
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910BAC8);
+// func_80113E68
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910BC00);
+// func_80113EE0
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910BC78);
+// func_80113F7C
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910BD28);
+// AlucardHandleDamage
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910BE88);
+// PlayerStepStoned
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910D1F0);
+// PlayerStepKill
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910D8F8);
+// PlayerStepUnk17
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910E2D0);
+// func_80115C50
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910E378);
+// PlayerStepTeleport
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910E4B8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910E758);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910EA68);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910ECC0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910EFE0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910F0D8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_psp_0910F128);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", BatFormFinished);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2BF60", func_8011690C);
