24decff67b9931784f74aad1cda23f3e3607fb1d
Joey Murphy <tjmurphy@mit.edu>
2025-03-09 08:52:59 -0400

    Unsplit PSP

--- src/dra_psp/2C350.c
+++ src/dra_psp/2C350.c
@@ -14,3 +14,47 @@ INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_09109200);
 INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910A138);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910A198);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910A9D0);
+// PlayerStepCrouch
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910AA60);
+// func_801139CC
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910B660);
+// PlayerStepHighJump
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910B768);
+// func_80113D7C
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910BAC8);
+// func_80113E68
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910BC00);
+// func_80113EE0
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910BC78);
+// func_80113F7C
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910BD28);
+// AlucardHandleDamage
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910BE88);
+// PlayerStepStoned
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910D1F0);
+// PlayerStepKill
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910D8F8);
+// PlayerStepUnk17
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910E2D0);
+// func_80115C50
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910E378);
+// PlayerStepTeleport
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910E4B8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910E758);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910EA68);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910ECC0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910EFE0);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910F0D8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_psp_0910F128);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", BatFormFinished);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/2C350", func_8011690C);
