07af6664cd77c124f43e75ce87665c4af897ef53
ProjectOblivion <ProjectOblivion>
2025-03-10 00:37:16 +0000

    Format

--- tools/sotn_permuter/permuter_loader.py
+++ tools/sotn_permuter/permuter_loader.py
@@ -15,7 +15,8 @@ import decomp_permuter.src.objdump
 import decomp_permuter.src.main as permuter
 import importer
 
-PERMUTE_PATH = os.path.join(os.getcwd(),"tools/sotn_permuter/permute.me/")
+PERMUTE_PATH = os.path.join(os.getcwd(), "tools/sotn_permuter/permute.me/")
+
 
 # These functions exist solely to overwrite some of the undesirable hardcoded behavior in decomp-permuter import.py
 # They intentionally retain as much of the original code and naming as possible to avoid unexpected incompatibility
@@ -25,7 +26,7 @@ def sotn_create_directory(func_name: str) -> str:
     while True:
         ctr += 1
         dirname = f"{func_name}-{ctr}" if ctr > 1 else func_name
-        dirname = os.path.join(PERMUTE_PATH,dirname)
+        dirname = os.path.join(PERMUTE_PATH, dirname)
         try:
             os.mkdir(dirname)
             return dirname
@@ -63,9 +64,13 @@ if __name__ == "__main__":
         default=os.getenv("VERSION") or "us",
         help="Sets game version and overrides VERSION environment variable",
     )
-    
-    subparsers = parser.add_subparsers(dest="action", help="Whether to import or permute the function")
-    parser_import = subparsers.add_parser("import", help="Import a function to be permuted")
+
+    subparsers = parser.add_subparsers(
+        dest="action", help="Whether to import or permute the function"
+    )
+    parser_import = subparsers.add_parser(
+        "import", help="Import a function to be permuted"
+    )
     parser_import.add_argument(
         "source",
         type=str,
@@ -96,24 +101,32 @@ if __name__ == "__main__":
         type=str,
         help="The function to be permuted",
     )
-    parser_clean = subparsers.add_parser("clean", help="Clean function folders from permuter")
+    parser_clean = subparsers.add_parser(
+        "clean", help="Clean function folders from permuter"
+    )
     parser_clean.add_argument(
         "function",
         type=str,
-        help="The function to be cleaned, or use \"all\" to clean all folders",
+        help='The function to be cleaned, or use "all" to clean all folders',
     )
     # This will keep the loader from choking on args intended for decomp-permuter
     args, unknown = parser.parse_known_args()
     # Removes args intended only for the loader
     if "-o" in sys.argv:
-        sys.argv.pop(sys.argv.index("-o")+1)
-    sys.argv = [a for a in sys.argv if a not in ["-o","-p"] and a not in actions]
-    sys.argv = [a for a in sys.argv if not a.startswith("--v") and not a.startswith("--o") and not a.startswith("--p")]
-    
+        sys.argv.pop(sys.argv.index("-o") + 1)
+    sys.argv = [a for a in sys.argv if a not in ["-o", "-p"] and a not in actions]
+    sys.argv = [
+        a
+        for a in sys.argv
+        if not a.startswith("--v")
+        and not a.startswith("--o")
+        and not a.startswith("--p")
+    ]
+
     # Function overwrites
     importer.create_directory = sotn_create_directory
     importer.finalize_compile_command = sotn_finalize_compile_command
-    
+
     # Actual loader logic
     if args.version == "us":
         importer.DEFAULT_AS_CMDLINE = [
@@ -152,9 +165,7 @@ if __name__ == "__main__":
             if not any(i.startswith("--settings") for i in sys.argv)
             else []
         )
-        decomp_permuter.src.objdump.MIPS_SETTINGS.executable = [
-            "bin/allegrex-objdump"
-        ]
+        decomp_permuter.src.objdump.MIPS_SETTINGS.executable = ["bin/allegrex-objdump"]
         decomp_permuter.src.objdump.MIPS_SETTINGS.arguments = [
             "-drz",
             "-m",
@@ -173,22 +184,32 @@ if __name__ == "__main__":
                 )
             ) as yamlFile:
                 splatConfig = yaml.safe_load(yamlFile)
-            if "options" in splatConfig and "nonmatchings_path" in splatConfig["options"]:
+            if (
+                "options" in splatConfig
+                and "nonmatchings_path" in splatConfig["options"]
+            ):
                 if args.version == "pspeu":
-                    nonmatchings_path = os.path.join(splatConfig["options"]["nonmatchings_path"],f"{args.overlay[-3:]}_psp")
+                    nonmatchings_path = os.path.join(
+                        splatConfig["options"]["nonmatchings_path"],
+                        f"{args.overlay[-3:]}_psp",
+                    )
                 else:
                     nonmatchings_path = splatConfig["options"]["nonmatchings_path"]
             else:
                 nonmatchings_path = "nonmatchings"
 
             if args.version == "pspeu":
-                src_path = os.path.join(os.getcwd(),splatConfig["options"]["src_path"],f"{args.overlay[-3:]}_psp")
+                src_path = os.path.join(
+                    os.getcwd(),
+                    splatConfig["options"]["src_path"],
+                    f"{args.overlay[-3:]}_psp",
+                )
             else:
-                src_path = os.path.join(os.getcwd(),splatConfig["options"]["src_path"])
-            
+                src_path = os.path.join(os.getcwd(), splatConfig["options"]["src_path"])
+
             cFilename = os.path.join(src_path, f"{args.source}.c")
             sys.argv = [cFilename if a == args.source else a for a in sys.argv]
-            
+
             sFilename = os.path.join(
                 os.getcwd(),
                 splatConfig["options"]["asm_path"],
@@ -197,7 +218,7 @@ if __name__ == "__main__":
                 f"{args.function}.s",
             )
             sys.argv = [sFilename if a == args.function else a for a in sys.argv]
-            funcPath = os.path.join(PERMUTE_PATH,args.function)
+            funcPath = os.path.join(PERMUTE_PATH, args.function)
             if os.path.isdir(funcPath):
                 try:
                     shutil.rmtree(funcPath)
@@ -211,22 +232,35 @@ if __name__ == "__main__":
         print(sys.argv)
         importer.main(sys.argv[1:])
         if args.permute and args.overlay:
-            sys.argv = [a for a in sys.argv if not a.startswith("--settings") and not a.endswith(".c")]
+            sys.argv = [
+                a
+                for a in sys.argv
+                if not a.startswith("--settings") and not a.endswith(".c")
+            ]
             sys.argv[1] = sys.argv[1].split("/")[-1].rstrip(".s")
             args.function = args.function.split("/")[-1].rstrip(".s")
 
-    if args.action == "permute" or (args.action == "import" and args.permute and args.overlay):
+    if args.action == "permute" or (
+        args.action == "import" and args.permute and args.overlay
+    ):
         print(f"Permuting {args.version} function {args.function}")
-        sys.argv = [os.path.join(PERMUTE_PATH, args.function) if x == args.function else x for x in sys.argv]
+        sys.argv = [
+            os.path.join(PERMUTE_PATH, args.function) if x == args.function else x
+            for x in sys.argv
+        ]
         addArgs = [a for a in permuteArgs if not any(i.startswith(a) for i in sys.argv)]
-        addArgs = [a + f"{multiprocessing.cpu_count()}" if a == "-j" else a for a in addArgs]
+        addArgs = [
+            a + f"{multiprocessing.cpu_count()}" if a == "-j" else a for a in addArgs
+        ]
         sys.argv.extend(addArgs)
         permuter.main()
 
     if args.action == "clean":
         for dirName in os.listdir(PERMUTE_PATH):
             dirPath = os.path.join(PERMUTE_PATH, dirName)
-            if os.path.isdir(dirPath) and (dirName.startswith(args.function) or args.function == "all"):
+            if os.path.isdir(dirPath) and (
+                dirName.startswith(args.function) or args.function == "all"
+            ):
                 try:
                     shutil.rmtree(dirPath)
                     print(f"{dirPath} and its contents deleted successfully.")
