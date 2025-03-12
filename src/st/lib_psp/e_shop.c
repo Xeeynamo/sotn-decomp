d34daead215e6d512c0d72c27790b65f146b7fb5
gamezter <gamezter93@gmail.com>
2025-03-12 11:17:10 -0400

    Decompile `lib_psp` func_us_801B8234

--- src/st/lib_psp/e_shop.c
+++ src/st/lib_psp/e_shop.c
@@ -36,6 +36,7 @@ extern u16 D_psp_092A4A78[];
 extern s16 D_psp_092A4A88[];
 extern u16 D_psp_092A4AA0[];
 extern s16 D_psp_092A49B8[];
+extern u8** D_psp_092A5F40;
 extern u8** D_psp_092A5F48;
 extern u8** D_psp_092A5F98;
 extern const char* D_psp_092A4CA8[];
@@ -67,6 +68,7 @@ extern u8* D_us_80181660;
 extern char* D_us_80181668;
 extern char** D_us_80181674;
 extern u8* D_psp_092A5D38;
+extern u16 D_psp_09298988[];
 
 INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_shop", func_psp_0925D430);
 
@@ -1708,6 +1710,7 @@ INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_shop", func_us_801B6F30);
 
 INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_shop", func_us_801B7C94);
 
+void func_us_801B7DF8(Primitive* prim, Entity* arg1, s16 enemyId);
 INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_shop", func_us_801B7DF8);
 
 u8* func_psp_09269FF0(char* str, u8* pix, s16 x, s16 y) {
@@ -1738,7 +1741,294 @@ loop:
 
 INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_shop", func_us_801B7D10);
 
-INCLUDE_ASM("st/lib_psp/psp/lib_psp/e_shop", func_us_801B8234);
+void func_us_801B8234(Entity* self) {
+    Primitive* prim;
+    s32 primIndex;
+    s32 i;
+    s16 enemyIndex;
+    u16 enemyId;
+    u16 pads;
+    u8* pix;
+    u8* s3;
+    EnemyDef* enemyDef;
+    s32 pad[12];
+
+    enemyId = D_psp_09298988[self->params];
+    switch (self->step) {
+    case 0:
+#ifdef VERSION_PSP
+        primIndex = g_api.AllocPrimitives(PRIM_SPRT, 0xAE);
+#else
+        primIndex = g_api.AllocPrimitives(PRIM_SPRT, 0x86);
+#endif
+        if (primIndex != -1) {
+            InitializeEntity(g_EInitCommon);
+            i = 0;
+            self->flags |= FLAG_HAS_PRIMS;
+            self->primIndex = primIndex;
+            prim = &g_PrimBuf[primIndex];
+            for (; prim != NULL; i++) {
+                if (i < 6) {
+                    prim->type = PRIM_LINE_G2;
+                    prim->r0 = prim->r1 = 0x80;
+                    prim->g0 = prim->g1 = 0x80;
+                    prim->b0 = prim->b1 = 0x80;
+                    prim->priority = 0x1FA;
+                    prim->drawMode = DRAW_HIDE;
+                } else if (i < 8) {
+                    prim->type = PRIM_G4;
+                    prim->x0 = prim->x1 = prim->x2 = prim->x3 = 0x80;
+                    prim->y0 = prim->y1 = 0x1E;
+                    prim->y2 = prim->y3 = 0xDE;
+                    prim->priority = 0x1FA;
+                    prim->drawMode = DRAW_HIDE;
+#ifdef VERSION_PSP
+                } else if (i < 10) {
+                    prim->type = PRIM_GT4;
+                    prim->tpage = 0x110;
+                    prim->u0 = prim->u2 = (i - 9) * 0x78 + 0x80;
+                    prim->u1 = prim->u3 = prim->u0 + 0x78;
+                    prim->v0 = prim->v1 = 0x26;
+                    prim->v2 = prim->v3 = prim->v0 + 0xB0;
+                    prim->x0 = prim->x1 = prim->x2 = prim->x3 = 0x80;
+                    prim->y0 = prim->y1 = 0x26;
+                    prim->y2 = prim->y3 = 0xD6;
+                    prim->priority = 0x1FC;
+                    prim->drawMode = DRAW_HIDE;
+                } else if (i == 10) {
+                    prim->type = PRIM_GT4;
+                    prim->tpage = 0x2110;
+                    prim->u0 = prim->u2 = 0x20;
+                    prim->u1 = prim->u3 = prim->u0 + 0x5F;
+                    prim->v0 = prim->v1 = 0;
+                    prim->v2 = prim->v3 = prim->v0 + 0x6F;
+                    prim->x0 = prim->x2 = 0x1C;
+                    prim->x1 = prim->x3 = prim->x0 + (prim->u1 - prim->u0);
+                    prim->y0 = prim->y1 = 0x3C;
+                    prim->y2 = prim->y3 = prim->y0 + (prim->v2 - prim->v0);
+                    prim->priority = 0x11;
+                    prim->drawMode = DRAW_HIDE;
+                } else if (i == 11) {
+                    prim->tpage = 0x10;
+                    prim->clut = 0x1A1;
+                    prim->u0 = 0;
+                    prim->v0 = 0x80;
+                    prim->u1 = 0x80;
+                    prim->v1 = 0x40;
+                    prim->x0 = 0x84;
+                    prim->y0 = 0x38;
+                    prim->priority = 0x12;
+                    prim->drawMode = DRAW_HIDE;
+                } else if (i == 12) {
+                    prim->tpage = 0x10;
+                    prim->clut = 0x1A1;
+                    prim->u0 = 0;
+                    prim->v0 = 0xC0;
+                    prim->v1 = 0x20;
+                    prim->u1 = 0xF0;
+                    prim->x0 = 0xA;
+                    prim->y0 = 0xB4;
+                    prim->priority = 0x12;
+                    prim->drawMode = DRAW_HIDE;
+#else
+                } else if (i < 10) {
+                    prim->type = PRIM_GT4;
+                    prim->tpage = 0x114;
+                    prim->u0 = prim->u2 = (i - 9) * 0x70 + 0x80;
+                    prim->u1 = prim->u3 = prim->u0 + 0x70;
+                    prim->v0 = prim->v1 = 0x26;
+                    prim->v2 = prim->v3 = prim->v0 + 0xB0;
+                    prim->x0 = prim->x1 = prim->x2 = prim->x3 = 0x80;
+                    prim->y0 = prim->y1 = 0x26;
+                    prim->y2 = prim->y3 = 0xD6;
+                    prim->priority = 0x1FC;
+                    prim->drawMode = DRAW_HIDE;
+                } else if (i == 10) {
+                    prim->tpage = 0x110;
+                    prim->u0 = 0x20;
+                    prim->v0 = 0;
+                    prim->u1 = 0x60;
+                    prim->v1 = 0x70;
+                    prim->x0 = 0x18;
+                    prim->y0 = 0x3C;
+                    prim->priority = 0x11;
+                    prim->drawMode = DRAW_HIDE;
+                } else if (i == 11) {
+                    prim->tpage = 0x10;
+                    prim->clut = 0x1A1;
+                    prim->u0 = 0;
+                    prim->v0 = 0xC0;
+                    prim->v1 = 0x20;
+                    prim->u1 = 0xF0;
+                    prim->x0 = 0x18;
+                    prim->y0 = 0xB4;
+                    prim->priority = 0x12;
+                    prim->drawMode = DRAW_HIDE;
+#endif
+                } else {
+                    prim->clut = 0x196;
+                    prim->tpage = 0x1E;
+                    prim->u1 = prim->v1 = 8;
+                    prim->priority = 0x12;
+                    prim->drawMode = DRAW_HIDE;
+                }
+                prim = prim->next;
+            }
+        }
+        break;
+
+    case 1:
+        switch (self->step_s) {
+        case 0:
+            self->step_s++;
+            break;
+
+        case 1:
+            if (g_api.LoadMonsterLibrarianPreview(self->params)) {
+                self->step_s++;
+            }
+            break;
+
+        case 2:
+            if (!g_IsUsingCd) {
+                self->step_s++;
+            }
+            break;
+
+        case 3:
+            func_us_801B11A0(0, 0x180, 0x100, 0x60);
+            pix = g_Pix[0];
+#ifdef VERSION_PSP
+            for (i = 0; i < 4; i++) {
+                s3 = D_psp_092A5F98[i];
+            }
+            enemyDef = &g_api.enemyDefs[enemyId];
+            s3 = D_psp_092A5F48[self->params];
+            func_us_801B0C40(pix, s3, 0, 0x1C0, 0x6E);
+            s3 = D_psp_092A5F40[self->params];
+            func_us_801B0C40(pix, s3, 0, 0x1D0, 0x6E);
+#else
+            s3 = D_us_80180FB4[self->params];
+            func_us_801B0C40(pix, s3, 0, 0x1C0, 0x6E);
+#endif
+            self->step_s++;
+            break;
+
+        case 4:
+            prim = &g_PrimBuf[self->primIndex];
+            for (i = 0; i < 10; i++) {
+                prim = prim->next;
+            }
+            func_us_801B7DF8(prim, self, enemyId);
+            self->ext.et_801B6F30.unk7C = 0;
+            self->ext.et_801B6F30.unk7E = 0;
+            SetStep(2);
+        }
+        break;
+
+    case 2:
+        self->ext.et_801B6F30.unk7C++;
+        self->ext.et_801B6F30.unk7E += 0x40;
+        prim = &g_PrimBuf[self->primIndex];
+#ifdef VERSION_PSP
+        func_us_801B245C(prim, self->ext.et_801B6F30.unk7E,
+                         self->ext.et_801B6F30.unk7C * 2, 0x80, 0x20, 7, 0);
+#else
+        func_us_801B245C(prim, self->ext.et_801B6F30.unk7E,
+                         self->ext.et_801B6F30.unk7C * 2, 0x78, 0x20, 7, 0);
+#endif
+        if (self->ext.et_801B6F30.unk7C == 0x10) {
+            SetStep(3);
+        }
+        break;
+
+    case 3:
+        pads = g_pads[0].repeat;
+        if (pads & PAD_RIGHT) {
+#ifdef VERSION_PSP
+            for (enemyIndex = self->params + 1; enemyIndex < 0x94;
+                 enemyIndex++) {
+#else
+            for (enemyIndex = self->params + 1; enemyIndex < 0x92;
+                 enemyIndex++) {
+#endif
+                if (g_CastleFlags[(enemyIndex >> 3) + ENEMY_LIST_190] &
+                        (1 << (enemyIndex & 7)) &&
+                    D_psp_09298988[self->params] != 0xFFFF) {
+                    self->params = enemyIndex;
+                    SetStep(4);
+                    break;
+                }
+            }
+        } else if (pads & PAD_LEFT) {
+            for (enemyIndex = self->params - 1; enemyIndex >= 0; enemyIndex--) {
+                if (g_CastleFlags[(enemyIndex >> 3) + ENEMY_LIST_190] &
+                        (1 << (enemyIndex & 7)) &&
+                    D_psp_09298988[self->params] != 0xFFFF) {
+                    self->params = enemyIndex;
+                    SetStep(4);
+                    break;
+                }
+            }
+        }
+        pads = g_pads[0].tapped;
+#ifdef VERSION_PSP
+        if (pads & D_psp_08B42054) {
+#else
+        if (pads & PAD_TRIANGLE) {
+#endif
+            SetStep(7);
+        }
+        break;
+
+    case 4:
+        self->ext.et_801B6F30.unk7E = 0x400;
+        self->ext.et_801B6F30.unk7C = 0x10;
+        SetStep(5);
+        break;
+
+    case 5:
+        self->ext.et_801B6F30.unk7C--;
+        self->ext.et_801B6F30.unk7E -= 0x40;
+        prim = &g_PrimBuf[self->primIndex];
+#ifdef VERSION_PSP
+        func_us_801B245C(prim, self->ext.et_801B6F30.unk7E,
+                         self->ext.et_801B6F30.unk7C, 0x80, 0x20, 7, 0);
+#else
+        func_us_801B245C(prim, self->ext.et_801B6F30.unk7E,
+                         self->ext.et_801B6F30.unk7C, 0x74, 0x20, 7, 0);
+#endif
+        if (!self->ext.et_801B6F30.unk7C) {
+            SetStep(1);
+        }
+        break;
+
+    case 7:
+        if ((g_CutsceneFlags & 0x200) == 0) {
+            self->ext.et_801B6F30.unk7E = 0x400;
+            self->ext.et_801B6F30.unk7C = 0x10;
+            self->step++;
+        }
+        break;
+
+    case 8:
+        self->ext.et_801B6F30.unk7C--;
+        self->ext.et_801B6F30.unk7E -= 0x40;
+        prim = &g_PrimBuf[self->primIndex];
+#ifdef VERSION_PSP
+        func_us_801B245C(prim, self->ext.et_801B6F30.unk7E,
+                         self->ext.et_801B6F30.unk7C * 2, 0x80, 0x20, 7, 0);
+#else
+        func_us_801B245C(prim, self->ext.et_801B6F30.unk7E,
+                         self->ext.et_801B6F30.unk7C * 2, 0x74, 0x20, 7, 0);
+#endif
+        if (!self->ext.et_801B6F30.unk7C) {
+            g_CutsceneFlags |= 0x400;
+            DestroyEntity(self);
+        }
+        break;
+    }
+}
 
 void func_psp_0926ADD8(Primitive* prim, Entity* self) {
     s16 x, y;
