11551e2559b9769b19c8afe84c94e5aa4ce704c6
Joey Murphy <tjmurphy@mit.edu>
2025-03-14 07:48:38 -0400

    func_800EA538

--- src/dra_psp/1E50.c
+++ src/dra_psp/1E50.c
@@ -505,6 +505,25 @@ INCLUDE_ASM("dra_psp/psp/dra_psp/1E50", func_psp_090DFC68);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/1E50", func_psp_090DFC80);
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/1E50", func_800EA538);
+void func_800EA538(s32 arg0) {
+    u16 v1;
+    s32 i,j;
+    u16 unk8;
+
+    v1 = (0x8000 >> (arg0 - 1));
+    if (arg0) {
+        for (i = 0; i < LEN(D_8006C3C4); i++) {
+            unk8 = D_8006C3C4[i].unk8;
+            if (unk8 & v1) {
+                D_8006C3C4[i].unk8 = 0;
+            }
+        }
+    } else {
+        D_8003C0F8 = 0;
+        for (j = 0; j < LEN(D_8006C3C4); j++) {
+            D_8006C3C4[j].unk8 = 0;
+        }
+    }
+}
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/1E50", func_800EA5AC);
