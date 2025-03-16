6063ea037ac65aad6a9374e06fb9c336700cf80a
Robin Eberhard <robin.eberhard@mpq.mpg.de>
2025-03-16 14:36:37 +0100

    decomp func_us_801C5430, func_us_801C5990, func_us_801C59DC, func_us_801C7340, D_us_801D433C

--- src/boss/bo4/unk_45354.c
+++ src/boss/bo4/unk_45354.c
@@ -14,7 +14,13 @@ void func_8010E0B8(void) {
     g_Entities[STAGE_ENTITY_START + UNK_ENTITY_1].ext.entSlot1.unk0 = 0;
 }
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_45354", func_us_801C5430);
+extern s16 D_us_801D4D6C[];
+
+void func_us_801C5430(s16 a0, s16 a1) {
+    if (D_us_801D4D6C[0] <= a1) {
+        D_us_801D4D6C[0] = a1;
+    }
+}
 
 #include "../../decelerate.h"
 
@@ -64,9 +70,35 @@ INCLUDE_ASM("boss/bo4/nonmatchings/unk_45354", func_8010E6AC);
 
 INCLUDE_ASM("boss/bo4/nonmatchings/unk_45354", func_us_801C58E4);
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_45354", func_us_801C5990);
+void SetPlayerAnim(u8 anim);                                 /* extern */
+extern s16 DOPPLEGANGER_step_s;
+extern s32 DOPPLEGANGER_velocityY;
+extern s16 g_Dop_unk44[];
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_45354", func_us_801C59DC);
+void func_us_801C5990(void) {
+    DOPPLEGANGER_velocityY = 0xFFFBC000;
+    *g_Dop_unk44 |= 0x21;
+    SetPlayerAnim(0x20);
+    DOPPLEGANGER_step_s = 0;
+}
+
+extern s32 D_800762E0;
+extern s16 D_us_801D4D5A;
+extern s16 D_us_801D4D5C;
+extern u16 D_us_801D4DC0;
+
+void func_us_801C59DC(void) {
+    SetPlayerStep(4);
+    if (D_us_801D4DC0 != 2) {
+        SetPlayerAnim(0x1C);
+    }
+    DOPPLEGANGER_velocityY = 0x20000;
+    D_us_801D4D5A = 8;
+    D_us_801D4D5C = 8;
+    D_800762E0 = 0;
+    *g_Dop_unk44 = 0x10;
+}
+
 
 INCLUDE_ASM("boss/bo4/nonmatchings/unk_45354", func_us_801C5A4C);
 
--- src/boss/bo4/unk_46E7C.c
+++ src/boss/bo4/unk_46E7C.c
@@ -114,7 +114,18 @@ INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_80113E68);
 
 INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801C72BC);
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801C7340);
+extern s16 DOPPLEGANGER_posX_i_hi;
+extern s16 D_8007630A;
+extern s16 PLAYER_posX_i_hi;
+
+void func_us_801C7340(void) {
+    if (DOPPLEGANGER_posX_i_hi <= PLAYER_posX_i_hi) {
+        D_8007630A = 0;
+        return;
+    }
+    D_8007630A = 1;
+}
+
 
 INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801C7380);
 
