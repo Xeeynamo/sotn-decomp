c21815645f6452f9ab36ab8893b7471f99b34655
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2025-03-17 05:24:03 +0000

    PSP RIC pl_debug (#2319)
    Easy one. Simply finish to decompile the file and split it to be
    coherent between US, HD and PSPEU.

--- Makefile.psp.mk
+++ Makefile.psp.mk
@@ -85,7 +85,7 @@ ST_DRA_MERGE = 624DC 628AC 6BF64 alu_anim 6DF70 6E42C 6FDF8 704D0 7879C 7E4BC 84
 $(BUILD_DIR)/dra.elf: $(BUILD_DIR)/dra.ld $(addprefix $(BUILD_DIR)/src/dra/,$(addsuffix .c.o,$(ST_DRA_MERGE))) $$(call list_o_files_psp,dra_psp)
 	$(call link_with_deadstrip,dra,$@)
 
-RIC_MERGE = pl_handlers
+RIC_MERGE = pl_debug pl_handlers
 $(BUILD_DIR)/ric.elf: $(BUILD_DIR)/ric.ld $(addprefix $(BUILD_DIR)/src/ric/,$(addsuffix .c.o,$(RIC_MERGE))) $$(call list_o_files_psp,ric_psp) $(BUILD_DIR)/assets/ric/mwo_header.bin.o
 	$(call link_with_deadstrip,ric,$@)
 
