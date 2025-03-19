13529c729a12e21af787628ea7a562b36192c252
Joey Murphy <tjmurphy@mit.edu>
2025-03-18 22:31:31 -0400

    MemcardInit

--- src/dra_psp/60C90.c
+++ src/dra_psp/60C90.c
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
+#include "../dra/dra_bss.h"
+
 
 void func_psp_0913D610(void) {}
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/60C90", MemcardInit);
+void MemcardInit(void) { g_MemcardStep = 0; }
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/60C90", func_psp_0913D628);
 
