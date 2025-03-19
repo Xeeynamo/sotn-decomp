d4a491af7eadf80a7b0ff6c2b68d9069448815df
Joey Murphy <tjmurphy@mit.edu>
2025-03-18 22:29:29 -0400

    func_801083BC

--- src/dra_psp/60C28.c
+++ src/dra_psp/60C28.c
@@ -7,4 +7,10 @@ void func_801073C0(void) {
     CdDataCallback(NULL);
 }
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/60C28", func_psp_0913D5D8);
+CdFile D_800ACC64 = {0x0000, 3, 0x00000, 0, 0, 0xFF, 0};
+void func_801083BC(void) {
+    g_CdStep = CdStep_LoadInit;
+    g_LoadFile = CdFile_25;
+    D_8013AE9C = 10;
+    D_800ACC64.loc = 0;
+}
