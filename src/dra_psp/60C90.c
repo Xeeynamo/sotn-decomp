dae69ed83ba7dfa548aa7a0493723e969fda7d5e
Joey Murphy <tjmurphy@mit.edu>
2025-03-18 22:40:40 -0400

    MemcardInfoInit

--- src/dra_psp/60C90.c
+++ src/dra_psp/60C90.c
@@ -2,13 +2,25 @@
 #include "../dra/dra.h"
 #include "../dra/dra_bss.h"
 
+extern MemcardInfo g_MemcardInfo[2];
 
 void func_psp_0913D610(void) {}
 
 void MemcardInit(void) { g_MemcardStep = 0; }
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/60C90", func_psp_0913D628);
+void MemcardInfoInit(void) {
+    g_MemcardInfo[0].nBlockUsed = 0;
+    g_MemcardInfo[1].nBlockUsed = 0;
+}
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/60C90", MemcardParse);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/60C90", GetMemcardFreeBlockCount);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/60C90", MemcardDetectSave);
+
+s32 func_psp_0913D798(s32 cardNum, s32 blockNum) {
+    return g_MemcardInfo[cardNum].blocks[blockNum];
+}
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/60C90", func_psp_0913D7D8);
