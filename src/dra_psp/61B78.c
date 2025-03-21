00cff8657b93d936366a2ef3d5f9b47aea1e8cc0
Joey Murphy <tjmurphy@mit.edu>
2025-03-21 18:30:13 -0400

    ResetPadsRepeat

--- src/dra_psp/61B78.c
+++ src/dra_psp/61B78.c
@@ -1,7 +1,20 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../dra/dra.h"
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", func_psp_0913E4F8);
+// This could have been BUTTON_COUNT * PAD_COUNT but nope, 16 on psp
+extern u8 g_PadsRepeatTimer[16];
+
+void ResetPadsRepeat(void) {
+    u8* ptr;
+    s32 i;
+
+    g_pads[0].repeat = 0;
+    ptr = g_PadsRepeatTimer;
+
+    for (i = 0; i < LEN(g_PadsRepeatTimer); i++) {
+        *ptr++ = 0x10;
+    }
+}
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", func_psp_0913E558);
 
