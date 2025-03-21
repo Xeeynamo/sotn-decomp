93c50e4b0d24a3e2e660e54ca51c40c66a66ac7b
Joey Murphy <tjmurphy@mit.edu>
2025-03-21 18:14:58 -0400

    failed file split?

--- src/dra_psp/61B78.c
+++ src/dra_psp/61B78.c
@@ -8,7 +8,3 @@ INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", func_psp_0913E558);
 INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", func_psp_0913E658);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", func_psp_0913E6D0);
-
-INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", DestroyEntity);
-
-INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", DestroyEntitiesFromIndex);
