33fd61e032a52daeb2f4d3fda57a9938f77a0aef
bismurphy <tjmurphy@mit.edu>
2025-03-21 18:10:49 -0400

    Decompile CD and Memory Card functions for DRA PSP (#2347)
    Pretty well-contained in their files. Lots of functions here are very
    different from their original versions, so no code sharing for this PR.
    Some functions are the same but there are enough differences to make it
    reasonable to have these as separate files, in my opinion.

--- src/dra_psp/60C28.c
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../dra/dra.h"
-#include "../dra/dra_bss.h"
-
-INCLUDE_ASM("dra_psp/psp/dra_psp/60C28", func_psp_0913D5A8);
-
-INCLUDE_ASM("dra_psp/psp/dra_psp/60C28", func_psp_0913D5D8);
--- src/dra_psp/dra_psp.h
+++ src/dra_psp/dra_psp.h
@@ -5,7 +5,6 @@ extern s32 D_psp_091474B8;
 extern s32 D_psp_091893B8[];
 extern s32 D_psp_091CDC80;
 extern s32 D_psp_091CDC88;
-extern unsigned char D_psp_09236780[][0x20];
 
 s32 func_89192EC(s32, s32);
 void func_892A620(s32, s32);
