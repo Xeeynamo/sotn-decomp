764e510a757a40c05e5bef0202a1fb2e14142ea1
Joey Murphy <tjmurphy@mit.edu>
2025-03-21 18:34:08 -0400

    UpdatePadsRepeat

--- src/dra_psp/61B78.c
+++ src/dra_psp/61B78.c
@@ -16,7 +16,32 @@ void ResetPadsRepeat(void) {
     }
 }
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", func_psp_0913E558);
+void UpdatePadsRepeat(void) {
+    u16 button = 1;
+    u16 unk = g_pads[0].tapped;
+    u16 pressed = g_pads[0].pressed;
+    u16 repeat = 0;
+    u8* timers = g_PadsRepeatTimer;
+    s32 i = 0;
+
+    while (i < 0x10) {
+        if (pressed & button) {
+            if (unk & button) {
+                repeat |= button;
+                timers[0] = 0x10;
+            } else {
+                if (!timers[0]--) {
+                    repeat |= button;
+                    timers[0] = 5;
+                }
+            }
+        }
+        i++;
+        timers++;
+        button <<= 1;
+    }
+    g_pads[0].repeat = repeat;
+}
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/61B78", func_psp_0913E658);
 
