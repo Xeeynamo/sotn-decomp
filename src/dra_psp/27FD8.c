0e264acc5c0d1a78118073330c287273dc73821e
Joey Murphy <tjmurphy@mit.edu>
2025-03-22 22:19:59 -0400

    func_8010A234

--- src/dra_psp/27FD8.c
+++ src/dra_psp/27FD8.c
@@ -394,7 +394,67 @@ static void CheckStageCollision(s32 isTransformed) {
     }
 }
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/27FD8", func_8010A234);
+void func_8010A234(s32 arg0) {
+    s32 weaponID;
+    s32 (*weapon)(void);
+    s32 i;
+    Entity* ent;
+
+    g_CurrentEntity = &PLAYER;
+    weapon = D_8017A000.GetWeaponId;
+    weaponID = weapon();
+    // Wearing Axe Lord Armor! This is probably when you initially put it on.
+    if ((weaponID == 0x2D) && CheckEquipmentItemCount(ITEM_AXE_LORD_ARMOR, EQUIP_ARMOR)) {
+        // If we already have the flag set, exit.
+        if (g_Player.status & PLAYER_STATUS_AXEARMOR) {
+            return;
+        }
+        // Alucard says "WHAT?!" when first putting on Axe Lord Armor
+        PlaySfx(SFX_VO_ALU_WHAT);
+        g_Player.padSim = 0;
+        g_Player.demo_timer = 32;
+        func_8010FAF4();
+        weapon = (int (*)())D_8017A000.EntityWeaponAttack;
+        weapon();
+        g_Player.status |= PLAYER_STATUS_AXEARMOR;
+        func_8010DFF0(1, 0xA);
+        func_80109328();
+        if (arg0 == 0) {
+            return;
+        }
+        PlayAnimation(D_800B0130, D_800B01B8);
+        return;
+    }
+    // Detect opposite case: If we're not wearing it, but status is set
+    // Means we need to run a routine to get back into normal mode.
+    if (g_Player.status & PLAYER_STATUS_AXEARMOR) {
+        PLAYER.palette = 0x8100;
+        PLAYER.animSet = 1;
+        PLAYER.unk5A = 0;
+        PLAYER.rotZ = 0;
+        PLAYER.drawFlags &=
+            (FLAG_DRAW_UNK10 | FLAG_DRAW_UNK20 | FLAG_DRAW_UNK40 | FLAG_BLINK |
+             FLAG_DRAW_ROTY | FLAG_DRAW_ROTX);
+        func_8010FAF4();
+        PLAYER.rotPivotY = 0;
+        PLAYER.rotPivotX = 0;
+        if (g_Player.vram_flag & 1) {
+            func_8010E570(0);
+        } else {
+            func_8010E7AC();
+        }
+        g_Player.status &= ~PLAYER_STATUS_AXEARMOR;
+        func_80111CC0();
+        if (arg0 != 0) {
+            PlayAnimation(D_800B0130, D_800B01B8);
+        }
+        for(i = 0, ent = &g_Entities[0]; i < TOTAL_ENTITY_COUNT; i++, ent++ ){
+            if(ent->palette == 0x110 && ent->unk5A == 100){
+                ent->animSet = 0;
+            }
+        }
+    }
+}
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/27FD8", func_8010A3F0);
 
