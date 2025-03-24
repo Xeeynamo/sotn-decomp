44619ec7d916fd45162d0b765b664530f7237531
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-03-25 10:51:08 +1100

    Fix a couple of small permuter issues related to sotn_str changes

--- include/game.h
+++ include/game.h
@@ -396,7 +396,7 @@ typedef enum {
 #define PAL_OVL(x) ((x) | PAL_OVL_FLAG)
 
 #ifndef SOTN_STR
-// Decorator to re-encode strings with tools/sotn_str/sotn_str.py when building
+// Decorator to re-encode strings with ./tools/sotn_str when building
 // the game. Certain strings in SOTN do not follow the ASCII encoding and each
 // character is offseted by 0x20. This is only for strings that use the 8x8
 // font. e.g. _S("I am a Symphony of the Night encoded string")
