6974a53b57cf92599805307ee07bd27b9a0a8bda
gamezter <charronalexandre@hotmail.com>
2025-03-25 12:21:18 -0400

    Decompile `dra_psp` func_psp_0913FA28 (#2359)
    PSP: https://decomp.me/scratch/Boq55
    
    There is still a small mismatch on PSX that I can't figure out:
    https://decomp.me/scratch/Ng6Jv
    
    I managed to decompile this function with the help of many other people
    with in-progress scratches on decomp.me, thanks to everyone who
    contributed and made progress on a closer match!
    
    edit:
    @JoshSchreuder helped to get a match on PSX for `func_80106A28`

--- src/dra/63ED4.c
+++ src/dra/63ED4.c
@@ -1563,7 +1563,217 @@ void DrawEntitiesHitbox(s32 drawMode) {
     }
 }
 
-INCLUDE_ASM("dra/nonmatchings/63ED4", func_80106A28);
+extern u16 D_800AC910[];
+#ifdef VERSION_HD
+extern u8 D_800AC914[10][30];
+#else
+extern u8 D_800AC914[1][30];
+#endif
+extern u16 D_800AC934[]; // LUT of ceil(index / 2)
+extern u16 D_80137EF8[];
+
+#define ExtractBit(x)                                                          \
+    x & 1;                                                                     \
+    x >>= 1
+
+#ifndef VERSION_PC
+
+u16* func_80106A28(u16 ch, u16 kind) {
+    u8* bitmap;
+    u16* srcPtr;
+    u16* destPtr;
+    u16 i, j, k;
+    u16 val0, val1, val2;
+    u16 b0, b1, b2, b3;
+    u16 dest, dest2;
+    u8 row;
+
+    // Krom2RawAdd returns a pointer to 30 bytes: 15 rows of 2 bytes for a
+    // bitmap of size 16x15.
+    bitmap = (u8*)Krom2RawAdd(ch);
+    if (bitmap == (u8*)-1) {
+        srcPtr = D_800AC910;
+        for (i = 0; i < LEN(D_800AC914); i++) {
+            if (ch == *srcPtr++) {
+                bitmap = D_800AC914[i];
+            }
+        }
+        if (bitmap == (u8*)-1) {
+            ch = 0x819A;
+            bitmap = (u8*)Krom2RawAdd(ch);
+        }
+    }
+
+    srcPtr = D_80137EF8;
+    for (i = 0; i < 15; i++) {
+        val0 = val2 = val1 = 0;
+
+        row = *bitmap;
+
+        b0 = ExtractBit(row);
+        b1 = ExtractBit(row);
+        b2 = ExtractBit(row);
+        b3 = ExtractBit(row);
+
+        val1 |= (b0 * 3 + b1) << 0x4;
+        val1 |= (b1 * 2 + b2 * 2);
+        val0 |= (b2 + b3 * 3) << 0xC;
+
+        b0 = ExtractBit(row);
+        b1 = ExtractBit(row);
+        b2 = ExtractBit(row);
+        b3 = ExtractBit(row);
+
+        val0 |= (b0 * 3 + b1) << 0x8;
+        val0 |= (b1 * 2 + b2 * 2) << 0x4;
+        val0 |= b2 + b3 * 3;
+
+        bitmap++;
+
+        row = *bitmap;
+
+        b0 = ExtractBit(row);
+        b1 = ExtractBit(row);
+        b2 = ExtractBit(row);
+        b3 = ExtractBit(row);
+
+        val2 |= (b0 * 3 + b1) << 0xC;
+        val2 |= (b1 * 2 + b2 * 2) << 0x8;
+        val2 |= (b2 + b3 * 3) << 0x4;
+
+        b0 = ExtractBit(row);
+        b1 = ExtractBit(row);
+        b2 = ExtractBit(row);
+        b3 = ExtractBit(row);
+
+        val2 |= b0 * 3 + b1;
+        val1 |= (b1 * 2 + b2 * 2) << 0xC;
+        val1 |= (b2 + b3 * 3) << 0x8;
+        dest = val1; // fake
+
+        *srcPtr++ = val0;
+        *srcPtr++ = val1;
+        *srcPtr++ = val2;
+        bitmap++;
+    }
+
+    destPtr = D_80137EF8;
+    srcPtr = D_80137EF8;
+    switch (kind) {
+    case 0:
+    case 3:
+        for (i = 0; i < 45; i++) {
+            val0 = *srcPtr++;
+            dest = D_800AC934[(val0 >> 0xC & 0xF) * 4] << 0xC;
+            dest |= D_800AC934[(val0 >> 0x8 & 0xF) * 4] << 0x8;
+            dest |= D_800AC934[(val0 >> 0x4 & 0xF) * 4] << 0x4;
+            dest |= D_800AC934[(val0 >> 0x0 & 0xF) * 4] << 0x0;
+            *destPtr++ = dest;
+        }
+        for (i = 0; i < 3; i++) {
+            *destPtr++ = 0;
+        }
+        break;
+
+    case 1:
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 3; j++) {
+                for (k = 7; k > 0; k--) {
+                    val0 = *srcPtr;
+                    srcPtr += 3;
+                    val2 = *srcPtr;
+                    dest = D_800AC934[((val0 >> 0xC & 0xF) * k +
+                                       (val2 >> 0xC & 0xF) * (8 - k)) /
+                                      2]
+                           << 0xC;
+                    dest |= D_800AC934[((val0 >> 0x8 & 0xF) * k +
+                                        (val2 >> 0x8 & 0xF) * (8 - k)) /
+                                       2]
+                            << 0x8;
+                    dest |= D_800AC934[((val0 >> 0x4 & 0xF) * k +
+                                        (val2 >> 0x4 & 0xF) * (8 - k)) /
+                                       2]
+                            << 0x4;
+                    dest |= D_800AC934[((val0 >> 0x0 & 0xF) * k +
+                                        (val2 >> 0x0 & 0xF) * (8 - k)) /
+                                       2]
+                            << 0x0;
+                    *destPtr = dest;
+                    destPtr += 3;
+                }
+                srcPtr -= 20;
+                destPtr -= 20;
+            }
+            srcPtr += 21;
+            destPtr += 18;
+        }
+        for (i = 0; i < 6; i++) {
+            *destPtr++ = 0;
+        }
+        break;
+
+    case 2:
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 3; j++) {
+                for (k = 3; k > 0; k--) {
+                    val0 = *srcPtr;
+                    srcPtr += 3;
+                    val2 = *srcPtr;
+                    dest = D_800AC934[(val0 >> 0xC & 0xF) * k +
+                                      (val2 >> 0xC & 0xF) * (4 - k)]
+                           << 0xC;
+                    dest |= D_800AC934[(val0 >> 0x8 & 0xF) * k +
+                                       (val2 >> 0x8 & 0xF) * (4 - k)]
+                            << 0x8;
+                    dest |= D_800AC934[(val0 >> 0x4 & 0xF) * k +
+                                       (val2 >> 0x4 & 0xF) * (4 - k)]
+                            << 0x4;
+                    dest |= D_800AC934[(val0 >> 0x0 & 0xF) * k +
+                                       (val2 >> 0x0 & 0xF) * (4 - k)]
+                            << 0x0;
+                    *destPtr = dest;
+                    destPtr += 3;
+                }
+                srcPtr -= 8;
+                destPtr -= 8;
+            }
+            srcPtr += 9;
+            destPtr += 6;
+        }
+        for (i = 0; i < 12; i++) {
+            *destPtr++ = 0;
+        }
+        break;
+    }
+    if (kind == 3) {
+        for (i = 0; i < 0x42; i++) {
+            dest2 = D_80137EF8[i];
+            if (dest2 & 4) {
+                if ((D_80137EF8[i] & 0xF0) == 0) {
+                    D_80137EF8[i] |= 0x10;
+                }
+            }
+            if (dest2 & 0x40) {
+                if ((D_80137EF8[i] & 0xF00) == 0) {
+                    D_80137EF8[i] |= 0x100;
+                }
+            }
+            if (dest2 & 0x400) {
+                if ((D_80137EF8[i] & 0xF000) == 0) {
+                    D_80137EF8[i] |= 0x1000;
+                }
+            }
+            if ((dest2 & 0x4000) && (i < 0x41)) {
+                if ((D_80137EF8[i + 1] & 0xF) == 0) {
+                    D_80137EF8[i + 1] |= 1;
+                }
+            }
+        }
+    }
+    return D_80137EF8;
+}
+
+#endif
 
 bool LoadMonsterLibrarianPreview(s32 monsterId) {
     if (g_IsUsingCd)
