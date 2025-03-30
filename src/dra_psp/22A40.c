bbb424f1867236f491a363d472790f1396734c20
Joey Murphy <tjmurphy@mit.edu>
2025-03-30 00:01:41 -0400

    split out timeattack on psp too

--- src/dra_psp/22A40.c
+++ src/dra_psp/22A40.c
@@ -15,4 +15,3 @@ INCLUDE_ASM("dra_psp/psp/dra_psp/22A40", func_psp_090FFAB8);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/22A40", EntityBatEcho);
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/22A40", TimeAttackController);
