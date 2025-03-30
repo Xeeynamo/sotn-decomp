ad52563593dc70ccac9d29a4ad2823b75a19181e
bismurphy <tjmurphy@mit.edu>
2025-03-30 10:00:39 -0400

    Prepare file alignments for DRA menu.c on PSP (#2384)
    Some function decomp, but mostly just reworking splits for a future
    decomp and codeshare of menu.c.

--- src/dra/dra.h
+++ src/dra/dra.h
@@ -41,6 +41,11 @@ typedef enum {
     MENU_DG_TIME_ATTACK,
     MENU_DG_EQUIP_SORT,
     MENU_DG_FAMILIARS,
+#if defined(VERSION_PSP)
+    MENU_PSP_EXTRA_1,
+    MENU_PSP_EXTRA_2,
+    MENU_PSP_EXTRA_3,
+#endif
     NUM_MENU,
 } MenuDialogue;
 
@@ -309,17 +314,18 @@ extern SubweaponDef g_SubwpnDefs[13];
 #if defined(VERSION_PSP)
 extern RelicDesc* g_RelicDefs;
 extern SpellDef* g_SpellDefs;
+extern char** g_MenuStr;
 extern EnemyDef* g_EnemyDefs;
 extern Accessory* g_AccessoryDefs;
 extern Equipment* g_EquipDefs;
 #else
 extern RelicDesc g_RelicDefs[30];
 extern SpellDef g_SpellDefs[28];
+extern const char* g_MenuStr[110];
 extern EnemyDef g_EnemyDefs[400];
 extern Accessory g_AccessoryDefs[90];
 extern Equipment g_EquipDefs[217];
 #endif
-extern const char* g_MenuStr[110];
 extern s32 g_ExpNext[];
 extern u16 D_800AC958[];
 extern CdFile* D_800ACC74[];
--- src/dra_psp/12E00.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../dra/dra.h"
-
-INCLUDE_ASM("dra_psp/psp/dra_psp/12E00", func_psp_090EF780);
-
-INCLUDE_ASM("dra_psp/psp/dra_psp/12E00", func_psp_090EF8F0);
