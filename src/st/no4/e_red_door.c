fc57c67c765a9abc3006f146670d7b9f6d2ac041
gamezter <gamezter93@gmail.com>
2025-03-31 10:51:45 -0400

    Import `no4` e_particles, e_red_door, e_room_fg and e_stage_name

--- src/st/no4/e_red_door.c
+++ src/st/no4/e_red_door.c
@@ -1,6 +1,3 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no4/nonmatchings/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/no4/nonmatchings/e_red_door", NO4_EntityRedDoor);
+#include "no4.h"
+#include "../e_red_door.h"
