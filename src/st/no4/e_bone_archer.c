ace5fc588094f44e4dcd0bf0eacd21a1e5d0fb12
gamezter <gamezter93@gmail.com>
2025-04-02 11:09:07 -0400

    Split `no4`

---
+++ src/st/no4/e_bone_archer.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no4/nonmatchings/e_bone_archer", func_us_801DCA9C);
+
+INCLUDE_ASM("st/no4/nonmatchings/e_bone_archer", func_us_801DD410);
---
+++ src/st/no4/e_skeleton_ape.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no4/nonmatchings/e_skeleton_ape", func_us_801DD688);
+
+INCLUDE_ASM("st/no4/nonmatchings/e_skeleton_ape", func_us_801DDBBC);
+
+INCLUDE_ASM("st/no4/nonmatchings/e_skeleton_ape", func_us_801DDCAC);
+
+INCLUDE_ASM("st/no4/nonmatchings/e_skeleton_ape", func_us_801DDCE4);
+
+INCLUDE_ASM("st/no4/nonmatchings/e_skeleton_ape", func_us_801DE0F0);
