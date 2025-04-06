3b1e9955b2f9625b0c7077ba0ffc96c1068ef9c0
sozud <122322823+sozud@users.noreply.github.com>
2025-04-06 10:18:48 -0700

    match func_060668D4 (#2413)

--- src/saturn/game.c
+++ src/saturn/game.c
@@ -13,7 +13,31 @@ INCLUDE_ASM("asm/saturn/game/f_nonmat", f60664E0, func_060664E0);
 INCLUDE_ASM("asm/saturn/game/f_nonmat", f60665BC, func_060665BC);
 INCLUDE_ASM("asm/saturn/game/f_nonmat", f60666A4, func_060666A4);
 INCLUDE_ASM("asm/saturn/game/f_nonmat", f6066854, func_06066854);
-INCLUDE_ASM("asm/saturn/game/f_nonmat", f60668D4, func_060668D4);
+
+#define FLAG_UNK_20000 0x20000
+#define STAGE_ENTITY_START 64
+#define TOTAL_ENTITY_COUNT 256
+
+void (*func_06064684)();
+
+// func_060668D4
+// similar to func_8011A9D8
+void func_8011A9D8(void) {
+    Entity* entity;
+    s32 i;
+
+    func_06064684();
+
+    entity = &g_Entities[STAGE_ENTITY_START];
+    for (i = STAGE_ENTITY_START; i < TOTAL_ENTITY_COUNT; i++, entity++) {
+        if (!(entity->flags & FLAG_UNK_20000)) {
+            DestroyEntity(entity);
+        }
+    }
+}
+
+INCLUDE_ASM("asm/saturn/game/data", d606692C, d_0606692C);
+
 INCLUDE_ASM("asm/saturn/game/f_nonmat", f6066B30, func_06066B30);
 INCLUDE_ASM("asm/saturn/game/f_nonmat", f6066B74, func_06066B74);
 INCLUDE_ASM("asm/saturn/game/f_nonmat", f6066CE0, func_06066CE0);
