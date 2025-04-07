1d9a0364243581ebe01a7e184a675b184831536a
gamezter <charronalexandre@hotmail.com>
2025-04-07 13:56:19 -0400

    Code-share `no4_psp` collision, create_entity and st-update (#2416)

---
+++ src/st/no4_psp/unk_10C30.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_092482B0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_092483A0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_09248CB8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_09248D90);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_09249030);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_09249560);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_09249760);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_09249FA0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_10C30", func_pspeu_0924A260);
---
+++ src/st/no4_psp/unk_1B2B0.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_1B2B0", func_pspeu_09252930);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_1B2B0", func_pspeu_09252B00);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_1B2B0", func_pspeu_09253B00);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_1B2B0", func_pspeu_09254690);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_1B2B0", func_pspeu_09254DE8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_1B2B0", func_pspeu_09255310);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_1B2B0", func_pspeu_09255640);
