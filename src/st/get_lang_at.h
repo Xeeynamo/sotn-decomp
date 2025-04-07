3512d1049115cfd0006a116bf27b484e3320872c
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-07 17:12:12 +1000

    Decompile PSP `lib` init and stage name functions (#2414)
    Decompiled some PSP exclusive LIB functions

---
+++ src/st/get_lang_at.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <game.h>
+
+extern s32 g_UserLanguage;
+
+u8* GetLangAt(s32 idx, u8* en, u8* fr, u8* sp, u8* ge, u8* it) {
+    switch (g_UserLanguage) {
+    default:
+    case LANG_EN:
+        return en + idx;
+    case LANG_FR:
+        return fr + idx;
+    case LANG_SP:
+        return sp + idx;
+    case LANG_GE:
+        return ge + idx;
+    case LANG_IT:
+        return it + idx;
+    }
+}
