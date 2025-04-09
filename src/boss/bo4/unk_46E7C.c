87317ecee8c3562e5ff1b41c4cb57edf8c904553
Jonathan Hohle <jonhohle@gmail.com>
2025-04-09 12:31:03 -0700

    More BO4 & RBO5 functions
    Another round of duplictes between `BO4` and `RBO5`.
    
    I found that `BO4` and `RBO5` needed `g_pfn_800EA5AC` to have a default
    signature. Instead of casting, I updated the struct to match. This works
    on PS and PSP, however, may not be desirable for portability later.

--- src/boss/bo4/unk_46E7C.c
+++ src/boss/bo4/unk_46E7C.c
@@ -189,10 +189,71 @@ static s32 func_us_801C820C(void) {
 
 INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801C8224);
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801C8CE0);
+extern s16 D_us_8018126C[];
+extern s16 D_us_80181274[];
+extern s16 D_us_8018128C[][2];
+extern s16 D_us_8018129C[][2];
+extern s16 D_us_80183B0C[][2];
+
+void func_us_801C8CE0(void) {
+    s32 i;
+    s32 count;
+    u8 _pad[40]; // must be between 33 & 40
+
+    DOPPLEGANGER.drawFlags = FLAG_DRAW_ROTZ;
+    DecelerateX(FIX(1.0 / 8.0));
+    if (g_Dop.vram_flag & 3) {
+        DOPPLEGANGER.velocityY = 0;
+    }
+    DecelerateY(FIX(1.0 / 8.0));
+    func_8011690C(0);
+    count = 0;
+
+    switch (DOPPLEGANGER.step_s) {
+    case 0:
+        for (i = 0; i < 4; i++) {
+            if (D_us_8018129C[i][1] < D_us_80181274[i]) {
+                D_us_8018129C[i][1]++;
+            } else {
+                count++;
+            }
+
+            if (D_us_8018128C[i][1] > D_us_8018126C[i]) {
+                D_us_8018128C[i][1]--;
+            } else {
+                count++;
+            }
+
+            if (i == 0 && (g_Dop.vram_flag & 0x8000)) {
+                DOPPLEGANGER.posY.i.hi--;
+            }
+        }
+
+        if (count == 8) {
+            DOPPLEGANGER.animSet = ANIMSET_OVL(1);
+            DOPPLEGANGER.drawFlags = FLAG_DRAW_DEFAULT;
+            DOPPLEGANGER.rotZ = 0;
+            g_Dop.unk66 = 1;
+            DOPPLEGANGER.step_s = 1;
+            D_us_80183B0C[0][1] = 0x5F;
+        }
+        break;
+
+    case 1:
+        if (g_Dop.unk66 == 3) {
+            func_us_801C58E4();
+            if (!(g_Dop.vram_flag & 0x8000)) {
+                DOPPLEGANGER.velocityY = 0xFFFF0000;
+            }
+            DOPPLEGANGER.palette = 0x8200;
+            func_80111CC0();
+        }
+        break;
+    }
+}
 
 s32 func_us_801C8EE4(void) {
-    if ((DOPPLEGANGER.step_s == 0) || !(g_Dop.padTapped & PAD_R2)) {
+    if (DOPPLEGANGER.step_s == 0 || !(g_Dop.padTapped & PAD_R2)) {
         return false;
     }
     CheckMoveDirection();
@@ -202,7 +263,62 @@ s32 func_us_801C8EE4(void) {
 
 INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801C8F3C);
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801C93C4);
+void func_us_801C93C4(void) {
+    s32 i;
+    u8 _pad[40];
+    s32 count;
+
+    if ((g_Dop.vram_flag & 1) && DOPPLEGANGER.velocityY > 0) {
+        DOPPLEGANGER.velocityY = 0;
+    }
+    if ((g_Dop.vram_flag & 2) && DOPPLEGANGER.velocityY < 0) {
+        DOPPLEGANGER.velocityY = 0;
+    }
+
+    DecelerateX(FIX(1.0 / 128.0));
+    DecelerateY(FIX(1.0 / 128.0));
+    count = 0;
+
+    for (i = 0; i < 4; i++) {
+
+        if (D_us_8018129C[i][1] < D_us_80181274[i]) {
+            D_us_8018129C[i][1]++;
+        } else {
+            count += 1;
+        }
+        if (D_us_8018128C[i][1] > D_us_8018126C[i]) {
+            D_us_8018128C[i][1]--;
+        } else {
+            count += 1;
+        }
+        if (i == 0 && (g_Dop.vram_flag & 0x8000)) {
+            DOPPLEGANGER.posY.i.hi--;
+        }
+    }
+
+    if (count == 8) {
+        DOPPLEGANGER.animSet = ANIMSET_OVL(1);
+        SetPlayerAnim(0xCB);
+        if (DOPPLEGANGER.step_s != 0) {
+            SetPlayerAnim(0xCC);
+        }
+
+        if (g_Entities[E_ID_50].step < 3) {
+            g_Entities[E_ID_50].step = 3;
+            return;
+        }
+        if (g_Entities[E_ID_50].step == 5) {
+            DOPPLEGANGER.palette = PAL_OVL(0x200);
+            func_8010FAF4();
+            CreateEntFactoryFromEntity(g_CurrentEntity, FACTORY(0x2C, 0x5B), 0);
+            func_us_801C58E4();
+            if (!(g_Dop.vram_flag & 0x8000)) {
+                DOPPLEGANGER.velocityY = FIX(-1);
+            }
+            func_80111CC0();
+        }
+    }
+}
 
 extern s32 D_us_801D3D44;
 
@@ -368,11 +484,67 @@ void DopEntityHitByHoly(Entity* self) {
 
 void func_us_801C9FEC(void) { FntPrint("dummy set\n"); }
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801CA014);
+extern PfnEntityUpdate D_us_801813D0[];
+// some kind of timer
+extern u8 D_us_801D3D4C;
+extern u8 D_us_801D3D50;
+extern u8 D_us_801D3D54;
+extern u8 D_us_801D3D58;
+
+void func_us_801CA014(void) {
+    Entity* entity;
+    PfnEntityUpdate entityUpdate;
+    s32 i;
+
+    entity = g_CurrentEntity = &g_Entities[E_ID_44];
+
+    for (i = E_ID_44; i < E_ID_90; i++, g_CurrentEntity++, entity++) {
+        if (i == 16 && entity->entityId == 0) {
+            g_Dop.unk48 = 0;
+        }
+
+        if (entity->entityId == 0) {
+            continue;
+        }
+
+        entityUpdate = D_us_801813D0[entity->entityId];
+        entityUpdate(entity);
+
+        entity = g_CurrentEntity;
+        if (entity->entityId != 0) {
+            if (!(entity->flags & FLAG_UNK_10000000) &&
+                (entity->posX.i.hi > 288 || entity->posX.i.hi < -32 ||
+                 entity->posY.i.hi > 256 || entity->posY.i.hi < -16)) {
+                DestroyEntity(g_CurrentEntity);
+            } else {
+                if (entity->flags & FLAG_UNK_20000000) {
+                    UpdateAnim(0, &D_us_8018136C[0].af);
+                }
+                entity->flags |= FLAG_NOT_AN_ENEMY;
+            }
+        }
+    }
+
+    if (D_us_801D3D4C) {
+        D_us_801D3D4C--;
+        if (D_us_801D3D4C & 1) {
+            g_api.g_pfn_800EA5AC(
+                1, D_us_801D3D50, D_us_801D3D54, D_us_801D3D58);
+        }
+    }
+
+    if (g_Dop.status & (PLAYER_STATUS_DEAD | PLAYER_STATUS_UNK80000)) {
+        FntPrint("dead boss\n");
+        entity = &g_Entities[E_ID_44];
+        for (i = E_ID_44; i < E_ID_90; i++, entity++) {
+            entity->hitboxState = 0;
+        }
+    }
+}
 
 Entity* CreateEntFactoryFromEntity(
     Entity* source, u32 factoryParams, s16 arg2) {
-    Entity* newFactory = GetFreeEntity(0x44, 0x50);
+    Entity* newFactory = GetFreeEntity(E_ID_44, E_ID_50);
 
     if (newFactory == NULL) {
         return NULL;
@@ -395,7 +567,8 @@ INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801CA2AC);
 extern EInit D_us_80180434;
 
 void func_us_801CA748(Entity* self) {
-    if (DOPPLEGANGER.step != 6 || DOPPLEGANGER.step_s != 3) {
+    if (DOPPLEGANGER.step != Dop_MorphBat ||
+        DOPPLEGANGER.step_s != Dop_Crouch) {
         DestroyEntity(self);
         return;
     }
@@ -461,13 +634,28 @@ void DopEntityHitByDark(Entity* self) {
     }
 }
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801CB020);
+// Does any entity with the same ID and params already
+// exist in the index range [0x50, 0x90)
+static bool func_8011BD48(Entity* entity) {
+    s16 objId = entity->entityId;
+    s16 params = entity->params;
+    Entity* e;
+    s32 i;
 
-INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", func_us_801CB07C);
+    for (e = &g_Entities[E_ID_50], i = E_ID_50; i < E_ID_90; e++, i++) {
+        if (objId == e->entityId && params == e->params && e != entity) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+INCLUDE_ASM("boss/bo4/nonmatchings/unk_46E7C", EntityPlayerBlinkWhite);
 
 extern s16* D_us_801B0A78[];
 extern s16 D_us_80181B74[24][5];
-extern u8* D_us_8018435C[];
+extern u8* g_SpriteSheet[];
 
 void EntityPlayerOutline(Entity* self) {
     s16* animFramePtr;
@@ -505,7 +693,7 @@ void EntityPlayerOutline(Entity* self) {
     spriteIdx &= INT16_MAX;
     selfX = self->posX.i.hi;
     selfY = self->posY.i.hi;
-    spritesheetPtr = ((u8**)D_us_8018435C)[spriteIdx];
+    spritesheetPtr = ((u8**)g_SpriteSheet)[spriteIdx];
     four = 4;
     one = 1;
     spriteX = four + spritesheetPtr[0];
@@ -858,7 +1046,7 @@ void DopEntityHitByLightning(Entity* self) {
         if ((++self->ext.hitbylightning.unk9C) > 0xA8) {
             var_s0 = true;
         }
-    } else if (DOPPLEGANGER.step != 11) {
+    } else if (DOPPLEGANGER.step != Dop_Hit) {
         var_s0 = true;
     }
 
@@ -888,7 +1076,7 @@ void DopEntityHitByLightning(Entity* self) {
                 DRAW_TPAGE2 | DRAW_TPAGE | DRAW_UNK02 | DRAW_TRANSP;
             prim = prim->next;
         }
-        if ((DOPPLEGANGER.velocityY != 0) && (DOPPLEGANGER.step != 17)) {
+        if ((DOPPLEGANGER.velocityY != 0) && (DOPPLEGANGER.step != Dop_Kill)) {
             self->ext.hitbylightning.unk92 = 1;
         }
         self->ext.hitbylightning.unk94 = 16;
--- src/boss/rbo5/doors.c
+++ src/boss/rbo5/doors.c
@@ -136,7 +136,8 @@ void func_us_801B3690(Entity* self) {
         offsetX = 0x100 - g_Tilemap.scrollX.i.hi;
         offsetY = 0x80 - g_Tilemap.scrollY.i.hi;
         for (i = 0; i < 2; i++) {
-            dop = AllocEntity(&g_Entities[224], &g_Entities[256]);
+            dop =
+                AllocEntity(g_Entities + 224, g_Entities + TOTAL_ENTITY_COUNT);
             if (dop != NULL) {
                 CreateEntityFromCurrentEntity(E_ID(ID_1C), dop);
                 dop->posX.i.hi = offsetX - 64 + (i * 128);
@@ -155,7 +156,7 @@ void func_us_801B3690(Entity* self) {
         break;
 
     case 9:
-        dop = AllocEntity(&g_Entities[160], &g_Entities[192]);
+        dop = AllocEntity(g_Entities + 160, g_Entities + 192);
         if (dop != NULL) {
             CreateEntityFromEntity(E_ID(ID_1D), self, dop);
             dop->posX.i.hi = 0x100 - g_Tilemap.scrollX.i.hi;
