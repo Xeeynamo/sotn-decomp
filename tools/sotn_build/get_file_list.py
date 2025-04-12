8f6e8540040847d30dde4d370fd9073d6352e0e7
ProjectOblivion <ProjectOblivion>
2025-04-12 20:13:15 +0000

    Converted to using Path

--- tools/sotn_build/get_file_list.py
+++ tools/sotn_build/get_file_list.py
@@ -6,16 +6,16 @@ import sys
 import os
 import re
 from string import Template
+from pathlib import Path
 
 # I intend things like this to be stored in a conf file when this functionality is more established in the project
 # Since it has a high likelihood of changing and isn't very complex, keeping it here is easier for now
 CONFIG_BASE_DIR: str = "config"
 CONFIG_PATTERNS: dict[Template] = {
-    "splat": Template("splat.$version.(?:st|bo)?$ovl.yaml")
+    "splat": Template("splat.$version.(?:st|bo)?$ovl.yaml"),
+    "saturn": Template("$ovl.(?:prg|bin).yaml"),
+    "sotn": Template("sotn.$version.(?:st|bo)?$ovl.yaml"),# For when an overlay is fully decompiled
 }
-# I would prefer os.path.join(path,filename.ext) because it is the safer method, but it
-# further muddies set comprehensions that are already difficult to read in some cases
-FILENAME_TEMPLATE: Template = Template("$path/$filename.$ext")
 SEG_TYPES: dict[tuple] = {
     "asm": ("asm", "header"),
     "data": ("data", "rodata", "bss", "sbss"),
@@ -35,22 +35,21 @@ SEG_TYPES: dict[tuple] = {
 # Shared source files seem to be an artifact in make that is no longer needed. Shared src files are now handled by includes in ovl src files.
 def get_config(args: argparse.Namespace) -> dict:
     """Uses the supplied parameters to retrieve and return a dict of config values with version and ovl added"""
-    config_dir: str = os.path.realpath(CONFIG_BASE_DIR)
-    config_pattern: re.Pattern = re.compile(
+    config_pattern = re.compile(
         CONFIG_PATTERNS[args.config_type].substitute(version=args.version, ovl=args.ovl)
     )
 
     # We only care about the first match we find since there shouldn't be multiple matches
-    config_path: str = next(
-        (file for file in os.listdir(config_dir) if config_pattern.match(file)), None
+    config_path = next(
+        (file for file in Path(CONFIG_BASE_DIR).iterdir() if config_pattern.match(file.name)), None
     )
 
     if config_path:
-        config_path = os.path.realpath(os.path.join(config_dir, config_path))
         with open(config_path) as config_file:
-            config: dict = yaml.safe_load(config_file)
+            config = yaml.safe_load(config_file)
 
-        extra_options: dict = {
+        # So that only one object needs to be passed around
+        extra_options = {
             "version": args.version,
             "ovl": args.ovl,
             "ignore_hidden": args.ignore_hidden,
@@ -75,7 +74,7 @@ def get_file_segments(segments: list) -> list[list]:
         if isinstance(seg, dict) and "subsegments" in seg
         for sub in seg["subsegments"]
     ]
-    # Converts docts that contain segment information into lists
+    # Converts dicts that contain segment information into lists
     segs.extend(
         [seg.get("start", 0x0), seg["type"].replace("code", "c"), seg["name"]]
         for seg in segments + subsegs
@@ -85,82 +84,58 @@ def get_file_segments(segments: list) -> list[list]:
     return [seg for seg in segs + subsegs if isinstance(seg, list)]
 
 
-def normalize_args(arg: list[str]) -> list[str]:
-    """Takes a list of items that are either a string of comma separated items or an individual item and normalizes it into a list of individual items"""
-    if arg:
-        return ",".join(arg).split(",")
-    else:
-        return []
-
-
 def get_asm_files(options: dict, segments: list[list]) -> set[str]:
     """Constructs a list of asm files from a given segment list"""
-    asm_path: str = options["asm_path"].rstrip("/")
-    data_path: str = os.path.join(options["asm_path"], "data").rstrip("/")
+    asm_path = Path(options["asm_path"])
+    data_path = asm_path.joinpath("data")
 
-    asm_files: set = {
-        FILENAME_TEMPLATE.substitute(
-            path=asm_path, filename=seg[2] if len(seg) >= 3 else seg[1], ext="s"
-        )
+    asm_files = {
+        asm_path.joinpath(f"{seg[2]}" if len(seg) >= 3 else seg[1]).with_suffix(".s")
         for seg in segments
         if len(seg) >= 2 and seg[1] in SEG_TYPES["asm"]
     }
-
-    # A seg type is added with a leading . for each existing data seg type
+    
+    data_seg_types = SEG_TYPES["data"]
+    # A seg type is added with a leading . for each existing data seg type if ignore_hidden is not set
     if not options["ignore_hidden"]:
-        data_seg_types = SEG_TYPES["data"] + tuple(
+        data_seg_types += tuple(
             f".{type}" for type in SEG_TYPES["data"]
         )
 
     data_files: set = {
-        FILENAME_TEMPLATE.substitute(
-            path=data_path,
-            # If a segment has 3 or more elements, then it has a name
-            # If it only has 1 or 2 elements, we use the memory address as the name
-            filename=seg[2] if len(seg) >= 3 else f"{seg[0]:X}",
-            # If it is only 1 element, then we assume it is bss and that the element is a memory address
-            ext=f"{seg[1].lstrip('.')}.s" if len(seg) >= 2 else "bss.s",
-        )
+        data_path.joinpath(f"{seg[2]}" if len(seg) >= 3 else f"{seg[0]:X}").with_suffix(f".{seg[1].lstrip('.')}.s" if len(seg) >= 2 else ".bss.s")
         for seg in segments
         if (len(seg) >= 2 and seg[1] in (data_seg_types)) or len(seg) == 1
     }
 
-    return {file for file in data_files | asm_files if os.path.exists(file)}
+    return {file for file in data_files | asm_files if file.exists()}
 
 
 def get_src_files(options: dict, segments: list[list]) -> set[str]:
     """Constructs a list of source files from splat segments"""
-    src_path: str = options["src_path"].rstrip("/")
+    src_path = Path(options["src_path"])
 
     # We assume that if there is a data file, then there should be a c file for it
     # This is safe since we're using sets and we validate existence on the final set
     extra_files: set = {
-        FILENAME_TEMPLATE.substitute(path=src_path, filename=seg[2], ext="c")
+        src_path.joinpath(f"{seg[2]}").with_suffix(".c")
         for seg in segments
         if len(seg) >= 3 and seg[1].startswith(".")
     }
 
     c_files: set = {
-        FILENAME_TEMPLATE.substitute(
-            path=src_path,
-            filename=seg[2] if len(seg) >= 3 else f"{seg[0]:X}",
-            ext=seg[1],
-        )
+        src_path.joinpath(f"{seg[2]}" if len(seg) >= 3 else f"{seg[0]:X}").with_suffix(".c")
         for seg in segments
         if len(seg) >= 2 and seg[1] in SEG_TYPES["src"]
     }
 
     if options["include_shared"]:
-        src_path = src_path.replace(f"/{options["ovl"]}", "")
-        c_files.update(
-            {
-                os.path.join(src_path, file)
-                for file in os.listdir(src_path)
-                if file.endswith(".c")
-            }
-        )
+        if options["ovl"] in src_path.parts:
+            c_files.update(src_path.parent.glob(".c"))
+        else:
+            c_files.update(src_path.glob(".c"))
 
-    return {file for file in c_files | extra_files if os.path.exists(file)}
+    return {file for file in c_files | extra_files if file.exists()}
 
 
 # This function still needs a bit of refinement, but it is serviceable for now
@@ -168,53 +143,34 @@ def get_asset_files(
     options: dict, segments: list[list], asset_filters: tuple[tuple]
 ) -> set[str]:
     """Constructs a list of asset files using a directory listing or the segments"""
-    asset_path = options["asset_path"].rstrip("/")
+    asset_path = Path(options["asset_path"])
 
-    # This conditional needs to be cleaned up and probably relocated for easier code maintenance
-    if (
-        options["platform"] == "psx"
-        and ("st/" in asset_path or "boss/" in asset_path)
-        and options["ovl"] != "sel"
-        and asset_filters == (("", ""),)
-    ):
-        asset_filters = tuple(
-            tuple(f.split("*"))
-            for f in ("D_801*.bin", "*.gfxbin", "*.palbin", "cutscene_*.bin")
-        )
+    # Stages and bosses, excluding sel, use these as default glob patterns instead of *
+    if (("st" in asset_path.parts or "boss" in asset_path.parts) and options["ovl"] != "sel"):
+        asset_filters = tuple(("D_801*.bin", "*.gfxbin", "*.palbin", "cutscene_*.bin"))
 
     # The file listing is really the only one used currently because some asset segment files are built by splat extensions
     # and the resulting files aren't directly specified in the config
-    if os.path.isdir(asset_path):
-        asset_files = {
-            os.path.join(asset_path, file)
-            for file in os.listdir(asset_path)
-            if any(
-                file.startswith(start_filter) and file.endswith(end_filter)
-                for start_filter, end_filter in asset_filters
-            )
-        }
+    if asset_path.exists():
+        asset_files = {path for asset_filter in asset_filters for path in asset_path.glob(asset_filter)}
     else:  # This method is included as something of a proof of concept/work in progress
         # Splat configs have different segment type names than what these files are saved as, so we translate them with this dict
         # dict.get() can have a default value specified, so we use extension.get(seg_type,seg_type) to get the translation
         # If there is no translation in the dict, then it just uses seg_type
         extension = {
-            "cmp": "dec",
-            "raw": "bin",
-            "ci4": "png",
-            "palette": "png",
-            "assets": "json",
+            "cmp": ".dec",
+            "raw": ".bin",
+            "ci4": ".png",
+            "palette": ".png",
+            "assets": ".json",
         }
         asset_files = {
-            FILENAME_TEMPLATE.substitute(
-                path=asset_path, filename=seg[2], ext=extension.get(seg[1], seg[1])
-            )
+            asset_path.joinpath(f"{seg[2]}").with_suffix(extension.get(seg[1], f".{seg[1]}"))
             for seg in segments
             if len(seg) >= 3 and seg[1] in SEG_TYPES["asset"]
         }
-        # Add a .bin file for each .dec file and supplement asset_files with a directory listing
-        asset_files.update(
-            {file.replace("dec", "bin") for file in asset_files if ".dec" in file}
-        )
+        # Add a .bin file for each .dec file 
+        asset_files.update({file.with_suffix(".bin") for file in asset_files if file.suffix == ".dec"})
     return asset_files
 
 
@@ -268,7 +224,7 @@ def parse_args(args: list):
         type=str,
         default="*",
         required=False,
-        help="Filters to apply to asset_path files",
+        help="A glob pattern to apply to asset_path files, specify multiple by repeating the option",
     )
 
     return parser.parse_args(args)
@@ -279,13 +235,10 @@ def main(args: list) -> None:
     args = parse_args(args)
     config = get_config(args)
 
-    asset_filters: tuple = tuple(
-        tuple(filter.split("*")) for filter in normalize_args(args.asset_filter)
-    )
-    file_segments: list = get_file_segments(config["segments"])
+    file_segments = get_file_segments(config["segments"])
     asm_files: set = get_asm_files(config["options"], file_segments)
     src_files: set = get_src_files(config["options"], file_segments)
-    asset_files: set = get_asset_files(config["options"], file_segments, asset_filters)
+    asset_files: set = get_asset_files(config["options"], file_segments, tuple(args.asset_filter))
 
     # These are currently sorted for ease of comparison/debugging, but it isn't relevant for actual function
     match args.list_type:
@@ -302,11 +255,17 @@ def main(args: list) -> None:
                 )
             )
 
+def get_repo_root(current_dir: Path = Path(__file__).resolve().parent) -> Path:
+    """Steps backward from the file location to infer the root of the repo"""
+    if next(current_dir.glob("src"), None) or current_dir.name == "/":
+        return current_dir
+    else:
+        return get_repo_root(current_dir.parent)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         main(sys.argv[1:])
     else:
-        # This is for ensuring proper path and args when running in atypical ways during debugging
-        os.chdir(os.path.join(os.getenv("HOME"), "github/sotn-decomp"))
-        main(["--version=pspeu", "list", "dra", "o_files", "-c", "splat", "-H"])
+        # This is for development and debugging purposes and isn't intended to be used in the final tool
+        os.chdir(get_repo_root())
+        main(["--version=pspeu", "list", "lib", "o_files", "-c", "splat", "-H", "-s"])
