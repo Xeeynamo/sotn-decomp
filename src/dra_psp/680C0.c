88605e95637a289b8d9f80cde3731d902f7b206d
gamezter <charronalexandre@hotmail.com>
2025-04-14 19:11:05 -0400

    Code-share `dra` CheckCollision (#2439)

--- src/dra_psp/680C0.c
+++ src/dra_psp/680C0.c
@@ -6,5 +6,3 @@ INCLUDE_ASM("dra_psp/psp/dra_psp/680C0", func_psp_09144A40);
 INCLUDE_ASM("dra_psp/psp/dra_psp/680C0", func_psp_09144BD8);
 
 INCLUDE_ASM("dra_psp/psp/dra_psp/680C0", func_psp_09144C80);
-
-INCLUDE_ASM("dra_psp/psp/dra_psp/680C0", CheckCollision);
