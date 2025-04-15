0cb385d6cc59aa60376be5d15c4335f78464a4b5
gamezter <charronalexandre@hotmail.com>
2025-04-15 11:27:22 -0400

    Align `dra/953A0.c` to `dra_psp/67558.c` (#2441)
    Depends on https://github.com/Xeeynamo/sotn-decomp/pull/2439

---
+++ src/dra_psp/67558.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../dra/dra.h"
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", ExecCdSoundCommands);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", PlaySfxScript);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", KeyOn12_19);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", func_8013572C);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", IncrementRingBufferPos);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", ExecSfx);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", func_80135D8C);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", func_80136010);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", func_801361F8);
+
+INCLUDE_ASM("dra_psp/psp/dra_psp/67558", func_psp_09144C80);
