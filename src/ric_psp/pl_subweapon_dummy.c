b625a523ebd00c6ab01e65b1c8f1b689aed50b1d
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-04-18 00:43:21 +0100

    RicEntityNotImplemented

--- src/ric_psp/pl_subweapon_dummy.c
+++ src/ric_psp/pl_subweapon_dummy.c
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-// SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../ric/ric.h"
 
-// clang-format off
-INCLUDE_ASM("ric_psp/nonmatchings/ric_psp/pl_subweapon_dummy", RicEntityNotImplemented1);
+void RicEntityNotImplemented1(Entity* self) {}
 
-INCLUDE_ASM("ric_psp/nonmatchings/ric_psp/pl_subweapon_dummy", RicEntityNotImplemented2);
+void RicEntityNotImplemented2(Entity* self) {}
