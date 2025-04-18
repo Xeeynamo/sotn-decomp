67d159db10071b5c34425ac88cca1ac0b9d9060a
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-18 19:55:06 +1000

    Merge `chi` PSP `e_collect`

--- src/st/chi_psp/e_misc.c
+++ src/st/chi_psp/e_misc.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_misc", EntityRelicOrb);
-
 INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_misc", EntityHeartDrop);
 
 INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_misc", EntityMessageBox);
