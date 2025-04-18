c07abf74e11d63b78d166feec02b1e71d79ce40a
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-18 18:08:30 +1000

    Merge `chi` PSP `en_red_door`

--- src/st/chi_psp/e_red_door.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_red_door", CHI_EntityRedDoor);
