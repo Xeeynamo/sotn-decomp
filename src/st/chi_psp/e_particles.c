21f44a2a763a4e8fa3a8ef643407c1df33314448
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-18 17:23:19 +1000

    Merge `chi` PSP `blit_char`

--- src/st/chi_psp/e_particles.c
+++ src/st/chi_psp/e_particles.c
@@ -8,7 +8,3 @@ INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_particles", func_psp_0923B2F0);
 INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_particles", func_psp_0923F500);
 
 INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_particles", EntityEnemyBlood);
-
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_particles", func_psp_0923C2F8);
-
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/e_particles", func_psp_0923C390);
