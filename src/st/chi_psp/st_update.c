f3282ff6a9a490fb68f5322753610f90ce1470d1
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-18 17:43:02 +1000

    Merge `chi` PSP `st_update`

--- src/st/chi_psp/st_update.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/st_update", Random);
-
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/st_update", Update);
-
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/st_update", UpdateStageEntities);
