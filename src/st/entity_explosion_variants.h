259749c92cc19e07d2aee2f0d9988c792e1b8275
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-19 10:35:08 +1000

    Merge `chi` PSP `e_misc`

--- src/st/entity_explosion_variants.h
+++ src/st/entity_explosion_variants.h
@@ -1,7 +1,26 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-extern s32 explode_yVel[];
-extern u8 explode_startFrame[];
-extern u16 explode_lifetime[];
+static s32 explode_yVel[] = {
+    /* 1020 */ FIX(4.0 / 128),
+    /* 1024 */ FIX(20.0 / 128),
+    /* 1028 */ FIX(36.0 / 128),
+    /* 102C */ FIX(56.0 / 128),
+    /* 1030 */ FIX(112.0 / 128),
+    /* 1034 */ FIX(144.0 / 128),
+};
+
+static u8 explode_startFrame[] = {
+    /* 1038 */ 1,
+    /* 1039 */ 9,
+    /* 103A */ 21,
+    /* 103B */ 43,
+};
+
+static u16 explode_lifetime[] = {
+    /* 103C */ 0x0010,
+    /* 103E */ 0x0018,
+    /* 1040 */ 0x002A,
+    /* 1042 */ 0x002F,
+};
 
 // ID is 0x14.
 
