a251bd3f6ca1c6663b149a582dc1a9bd3db57d17
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-19 10:48:03 +1000

    Merge `chi` PSP `en_thornweed_corpseweed`

---
+++ src/st/chi_psp/13DD8.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+// clang-format off
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924B458);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_092469A0);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_psp_092447C0);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924BD20);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924C060);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_psp_0923C0C0);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924C620);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_psp_0924D488);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", EntityLockCamera);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924D7F8);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924D948);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924E4D8);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924EAC0);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924EC10);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924ED28);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924F300);
+
+INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924F6D0);
+// clang-format on
