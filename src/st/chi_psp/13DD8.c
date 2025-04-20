82e59e5c7b83c0e718938fd27e86d4689621954c
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-20 11:17:00 +1000

    Merge `chi` PSP `st_debug`

--- src/st/chi_psp/13DD8.c
+++ src/st/chi_psp/13DD8.c
@@ -2,10 +2,6 @@
 #include "../chi/chi.h"
 
 // clang-format off
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", EntityBackgroundBlock);
-
-INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", EntityLockCamera);
-
 INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", func_pspeu_0924D7F8);
 
 INCLUDE_ASM("st/chi_psp/nonmatchings/chi_psp/13DD8", EntityFallingStairs);
