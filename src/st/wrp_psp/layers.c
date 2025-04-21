6b117db44bdc14ecf0d5e72011ff1a607f15bcab
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-04-22 06:54:34 +1000

    Do some misc PSP tidy ups (#2466)
    A few miscellaneous things I noticed while working on CHI PSP that I
    wanted to fix up before I forgot.

---
+++ src/st/wrp_psp/layers.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+// Just a stub for the BSS currently. This should be generated
+// in future from the assets tool
+LayerDef emptyLayer = {NULL, NULL, 0, 0, 0};
