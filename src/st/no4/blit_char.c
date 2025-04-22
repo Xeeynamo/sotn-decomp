df6073710d640f9df3212a4151fd301391acdbbd
gamezter <gamezter93@gmail.com>
2025-04-22 16:48:27 -0400

    Code-Share `no4` blit_char

--- src/st/no4/blit_char.c
+++ src/st/no4/blit_char.c
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+#include "no4.h"
+
+#ifdef VERSION_PSP
+#include "../blit_char_psp.h"
+#else
 #include "../blit_char.h"
+#endif
+
--- src/st/no4_psp/blit_char.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no4_psp/psp/no4_psp/blit_char", func_psp_0923C2F8);
-
-INCLUDE_ASM("st/no4_psp/psp/no4_psp/blit_char", func_psp_0923C390);
-
-INCLUDE_ASM("st/no4_psp/psp/no4_psp/blit_char", BlitChar);
