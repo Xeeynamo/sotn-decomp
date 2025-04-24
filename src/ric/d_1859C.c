b3e8834119f92d0cb7a60a40bb77751b209f62ce
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-04-24 10:48:52 +0100

    d_1859C

--- src/ric/d_1859C.c
+++ src/ric/d_1859C.c
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "ric.h"
 
+#if defined(VERSION_PSP)
+extern s32 D_8015459C;
+extern s16 g_DeadPrologueTimer;
+extern s16 D_801545AA;
+extern s16 D_801545AC;
+#else
 s32 D_8015459C = 0;
 RECT D_801545A0 = {512, 256, 32, 80};
 s16 g_DeadPrologueTimer = 0;
 s16 D_801545AA = 0;
 s16 D_801545AC = 0;
-u8 D_801545B0[] = {10, 8, 8, 6, 6, 4, 4,   4,   4, 4,
-                   4,  4, 4, 4, 4, 4, 255, 255, 0, 0};
+#endif
+u8 D_801545B0[] = {10, 8, 8, 6, 6, 4, 4,  4,  4, 4,
+                   4,  4, 4, 4, 4, 4, -1, -1, 0, 0};
 u8 D_801545C4[] = {4, 4, 4, 4, 6, 6, 6, 6, 8, 8, 16, 16, 16, 16, 16, 16};
 u8 D_801545D4[] = {8,  12, 16, 20, 24, 28, 32, 32,
                    32, 32, 32, 32, 32, 32, 32, 32};
@@ -30,11 +37,18 @@ Point16 g_RicSensorsWall[NUM_VERTICAL_SENSORS * 2] = {
 s16 g_RicSensorsCeilingDefault[NUM_HORIZONTAL_SENSORS] = {-16, -16, -16, -16};
 s16 g_RicSensorsFloorDefault[NUM_HORIZONTAL_SENSORS] = {28, 24, 24, 24};
 s16 g_RicSensorsWallDefault[NUM_VERTICAL_SENSORS] = {23, 16, 8, 0, -8, -8, -15};
+#if defined(VERSION_PSP)
+extern s16 g_RicSensorsCeilingCrouch[NUM_HORIZONTAL_SENSORS]; // on BSS
+#else
 s16 g_RicSensorsCeilingCrouch[NUM_HORIZONTAL_SENSORS] = {0, 0, 0, 0};
+#endif
 s16 g_RicSensorsWallCrouch[NUM_VERTICAL_SENSORS] = {23, 16, 8, 4, 4, 1, 1};
+
+#if !defined(VERSION_PSP)
 AnimationFrame* g_RicEmptyAnimGroup[ZERO_LEN] = {};
 u8 D_80154674[][4] = {{16, 127, 63, 0},
                       {16, 127, 0, 0},
                       {16, 63, 63, 127},
                       {16, 63, 127, 63},
                       {16, 47, 63, 127}};
+#endif
