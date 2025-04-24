97ee43d9cfd9cd9b42b0c3c4f56c28eb9ff0cdc4
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-04-24 16:28:02 +0100

    Extract subweapons def as an asset file

--- tools/sotn-assets/util/utils.go
+++ tools/sotn-assets/util/utils.go
@@ -18,6 +18,14 @@ func JoinMapKeys[T any](m map[string]T, sep string) string {
 	return strings.Join(keys, sep)
 }
 
+func ReverseMap[K comparable, V comparable](m map[K]V) map[V]K {
+	reversed := make(map[V]K, len(m))
+	for k, v := range m {
+		reversed[v] = k
+	}
+	return reversed
+}
+
 func MinBy[T any](slice []T, getter func(T) int) (max int) {
 	if len(slice) == 0 {
 		return max
