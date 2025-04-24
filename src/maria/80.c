46336e058b70d8cd22880a6a14d8f8709be2f705
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-04-24 20:35:15 +0100

    Add maria overlay

---
+++ src/maria/80.c
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "maria.h"
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A6280);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A6740);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A6958);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A6A08);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A6E50);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A7560);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A7950);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A7B80);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A7F20);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A7F78);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A81C0);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A82E0);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A8AE8);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A9250);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A9288);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A95A8);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A9920);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A9978);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A9C28);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A9D48);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A9DE8);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092A9E88);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092AAA38);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092AAC80);
+
+INCLUDE_ASM("maria_psp/nonmatchings/80", func_pspeu_092AB1C0);
---
+++ src/maria/pl_blueprints.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "maria.h"
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarGetFreeEntity);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarGetFreeEntityReverse);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_8015F9F0);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_8015FA5C);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_pspeu_092B5C90);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_8015FB84);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_8015FDB0);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityDummy);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarUpdatePlayerEntities);
+
+// clang-format off
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarCreateEntFactoryFromEntity);
+// clang-format on
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityFactory);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntitySlideKick);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityBladeDash);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_80160F0C);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntitySmokePuff);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityHitByCutBlood);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_80161C2C);
+
+// clang-format off
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityApplyMariaPowerAnim);
+// clang-format on
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_801623E0);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_80162604);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_pspeu_092B91A8);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_pspeu_092B91B8);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_pspeu_092B9298);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_pspeu_092B92F0);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_80162E9C);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityPlayerBlinkWhite);
+
+// clang-format off
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityShrinkingPowerUpRing);
+// clang-format on
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityHitByIce);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityHitByLightning);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_80165DD8);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", func_80166024);
+
+INCLUDE_ASM("maria_psp/nonmatchings/pl_blueprints", MarEntityTeleport);
