fc3e3e5a6ea9015f6f5cc46071bd7ef5307e98f2
ProjectOblivion <31639802+ProjectOblivion@users.noreply.github.com>
2025-04-25 14:01:18 -0400

    Fixed file listing for fully merged psp overlays (#2484)
    I also took the opportunity to pull the python out of Makefile and put
    it in it's own .py file.

---
+++ tools/list_ovl_files.py
@@ -0,0 +1,26 @@
+"""
+This script is not expected to be user friendly.
+It is used during builds by Makefile.psp.mk.
+"""
+
+import yaml
+import sys
+
+with open(sys.argv[1]) as yaml_file:
+    config = yaml.safe_load(yaml_file)
+c_subsegments = [
+    seg
+    for seg in config["segments"][1]["subsegments"]
+    if type(seg) == list and (seg[1] == "c" or seg[1] == ".data")
+]
+merged_functions = []
+for seg in c_subsegments:
+    func = f"{seg[2]}"
+    if "/" in func:
+        ovl, func = func.split("/")
+        if ovl == sys.argv[2]:
+            merged_functions.append(func)
+    else:
+        merged_functions.append(func)
+
+print(" ".join(merged_functions))
