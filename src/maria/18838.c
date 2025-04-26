a0f98f84ab362d831e46389d9edb0fbef30670f6
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2025-04-27 00:21:14 +0100

    PSP: Add maria overlay (#2493)
    As decompiling the Richter overlay for both PSX and PSP gave us precious
    hints about differences, more accurate matches and file splits, I am now
    kick-starting this exclusive PSP overlay with a good amount of functions
    and splits already documented. Hopefully having the file pre-split will
    stimulate collaboration.

---
+++ src/maria/18838.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "maria.h"
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BEA38);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BEAB0);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BEB40);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BF8B8);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BF950);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BFD30);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BFEB0);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BFF78);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092BFFB8);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092C0040);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092C0138);
+
+INCLUDE_ASM("maria_psp/nonmatchings/18838", func_pspeu_092C0248);
