da204cba11f06b63a80ac2e28b7f3c9c5d27804c
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2025-04-27 21:35:50 +0100

    PSP MAR: MarInit with localized data imported (#2498)
    Code and data differences with RIC suggests Maria is just a shameless
    copy&paste

--- src/maria/pl_main.c
+++ src/maria/pl_main.c
@@ -76,7 +76,113 @@ static s16 func_80156DE4(void) {
     return 0;
 }
 
-INCLUDE_ASM("maria_psp/nonmatchings/pl_main", MarInit);
+// Similar to of DRA func_80109594
+extern s32 D_pspeu_092E5F20;
+extern u8 hud_fr[];
+extern u8 hud_sp[];
+extern u8 hud_ge[];
+extern u8 hud_it[];
+extern LangImg g_FontImage;
+extern LangImg g_HudImage;
+void MarInit(s16 initParam) {
+    Entity* e;
+    s16 radius;
+    s16 intensity;
+    Primitive* prim;
+    s32 i;
+    SpriteParts** spriteptr;
+    s32* memset_ptr;
+    s32 memset_len;
+#ifndef VERSION_PSP
+    Entity* g_CurrentEntity;
+#endif
+
+    g_CurrentEntity = &PLAYER;
+    DestroyEntity(g_CurrentEntity);
+    PLAYER.posX.val = FIX(32);
+    PLAYER.posY.val = FIX(32);
+    PLAYER.animSet = ANIMSET_OVL(0x10);
+    PLAYER.zPriority = g_unkGraphicsStruct.g_zEntityCenter;
+    PLAYER.facingLeft = 0;
+    PLAYER.palette = 0x8114;
+    PLAYER.rotY = 0x100;
+    PLAYER.rotX = 0x100;
+    PLAYER.hitboxOffX = 0;
+    PLAYER.hitboxOffY = 0;
+    PLAYER.rotPivotY = 0x18;
+    memset_len = sizeof(PlayerState) / sizeof(s32);
+    memset_ptr = (s32*)&g_Player;
+    for (i = 0; i < memset_len; i++) {
+        *memset_ptr++ = 0;
+    }
+    g_Player.vram_flag = g_Player.unk04 = 1;
+    MarSetStand(0);
+    PLAYER.anim = mar_anim_stand_relax;
+    g_PlayerDraw->enableColorBlend = 0;
+    MarDebugOff();
+    for (i = 0; i < LEN(mar_80175958); i++) {
+        radius = (rand() & 0x3FF) + 0x100;
+        intensity = (rand() & 0xFF) + 0x100;
+        mar_80175958[i] = +(((rcos(radius) << 4) * intensity) >> 8);
+        mar_801759D8[i] = -(((rsin(radius) << 4) * intensity) >> 7);
+    }
+    spriteptr = g_api.o.spriteBanks;
+    spriteptr += 0x10;
+    *spriteptr = (SpriteParts*)mar_801530AC;
+    spriteptr++;
+    *spriteptr = (SpriteParts*)D_pspeu_092C6A50;
+    spriteptr++;
+    *spriteptr = (SpriteParts*)D_pspeu_092C8CE0;
+    spriteptr++;
+    *spriteptr = (SpriteParts*)D_pspeu_092CC558;
+    spriteptr++;
+    *spriteptr = (SpriteParts*)D_pspeu_092CA590;
+    spriteptr++;
+    *spriteptr = (SpriteParts*)D_pspeu_092CDC48;
+    spriteptr++;
+    *spriteptr = (SpriteParts*)D_pspeu_092C7AA0;
+    for (e = &g_Entities[1], i = 0; i < 3; i++, e++) {
+        DestroyEntity(e);
+        e->animSet = ANIMSET_OVL(0x10);
+        e->unk5A = i + 1;
+        e->palette = PAL_OVL(0x114);
+        e->flags = FLAG_UNK_20000 | FLAG_POS_CAMERA_LOCKED;
+    }
+    g_Entities[1].primIndex = g_api.AllocPrimitives(PRIM_TILE, 6);
+    g_Entities[1].flags |= FLAG_HAS_PRIMS;
+    for (prim = &g_PrimBuf[g_Entities[1].primIndex], i = 0; prim != NULL; i++,
+        prim = prim->next) {
+        prim->drawMode = DRAW_UNK_100 | DRAW_UNK02 | DRAW_HIDE;
+    }
+    if (D_80097C98 == 6) {
+        MarCreateEntFactoryFromEntity(
+            g_CurrentEntity, FACTORY(BP_TELEPORT, 1), 0);
+        MarSetInit(1);
+    }
+    if (D_80097C98 == 4) {
+        MarCreateEntFactoryFromEntity(
+            g_CurrentEntity, FACTORY(BP_TELEPORT, 3), 0);
+        MarSetInit(3);
+    }
+    if (D_80097C98 == 5) {
+        MarCreateEntFactoryFromEntity(
+            g_CurrentEntity, FACTORY(BP_TELEPORT, 5), 0);
+        MarSetInit(5);
+    }
+    func_pspeu_092C0040();
+    func_pspeu_092C0248();
+#ifdef VERSION_PSP
+    // ifdef'fed for the same reason in RicInit
+    D_pspeu_092E5F20 = 30;
+
+    func_91040A0(&g_FontImage);
+    g_HudImage.imgData = GetLang(NULL, hud_fr, hud_sp, hud_ge, hud_it);
+    if (g_HudImage.imgData) {
+        func_91040A0(&g_HudImage);
+    }
+#endif
+    func_90E4C68();
+}
 
 INCLUDE_ASM("maria_psp/nonmatchings/pl_main", CheckStageCollision);
 
