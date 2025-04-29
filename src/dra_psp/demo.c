070f3f56d14d3a5d7614544657a8d53293ccb3d0
gamezter <gamezter93@gmail.com>
2025-04-29 17:17:34 -0400

    Decompile `dra_psp` demo.c

--- src/dra_psp/demo.c
+++ src/dra_psp/demo.c
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "dra.h"
+#include "../dra/dra.h"
 
 // BSS
+extern s32 D_8C630D4;
+extern s32 D_psp_08C630DC;
+extern u8 D_8D95C40[];
 extern s32 g_DemoKeyIdx;
-extern u8* g_DemoPtr;
 
+#undef DEMO_KEY_PTR
+#define DEMO_KEY_PTR D_8D95C40
+
+static u8* g_DemoPtr = DEMO_KEY_PTR;
 static u8 D_800A243C[] = {
     STAGE_ST0,       STAGE_BO0,      STAGE_BO4,      STAGE_BO1,  STAGE_BO2,
     STAGE_BO3,       STAGE_NZ0_DEMO, STAGE_BO5,      STAGE_RBO1, STAGE_DRE,
     STAGE_NZ1_DEMO,  STAGE_RBO0,     STAGE_RBO2,     STAGE_BO7,  STAGE_BO6,
     STAGE_RBO3,      STAGE_RBO4,     STAGE_LIB_DEMO, STAGE_RBO5, STAGE_RBO7,
     STAGE_RNZ1_DEMO, STAGE_RBO8,     STAGE_NO0,      STAGE_NO0,  STAGE_NO0,
-    STAGE_NO0,       STAGE_NO0,      STAGE_RCEN,     STAGE_RBO6, STAGE_NO0,
+    STAGE_NO0,       STAGE_CEN,      STAGE_RCEN,     STAGE_RBO6, STAGE_NO0,
     STAGE_NO0,       STAGE_NO0,
 };
 
@@ -19,17 +25,17 @@ void DemoGameInit(s32 arg0) {
     s32 i;
 
     if (arg0 != 2) {
+        g_DemoKeyIdx = D_80097C98 & STAGE_INVERTEDCASTLE_MASK;
         if (g_StageId == STAGE_NZ0) {
             g_StageId = STAGE_NZ0_DEMO;
-        }
-        if (g_StageId == STAGE_NZ1) {
+        } else if (g_StageId == STAGE_NZ1) {
             g_StageId = STAGE_NZ1_DEMO;
-        }
-        if (g_StageId == STAGE_LIB) {
+        } else if (g_StageId == STAGE_LIB) {
             g_StageId = STAGE_LIB_DEMO;
-        }
-        if (g_StageId == STAGE_RNZ1) {
+        } else if (g_StageId == STAGE_RNZ1) {
             g_StageId = STAGE_RNZ1_DEMO;
+        } else {
+            g_StageId = D_800A243C[g_DemoKeyIdx];
         }
     } else {
         g_DemoKeyIdx = D_80097C98 & STAGE_INVERTEDCASTLE_MASK;
@@ -93,6 +99,7 @@ void DemoGameInit(s32 arg0) {
             g_Status.hearts = 60;
             g_Status.subWeapon = 2;
             g_Status.equipment[LEFT_HAND_SLOT] = ITEM_CLAYMORE;
+            g_Status.equipment[RIGHT_HAND_SLOT] = ITEM_CLAYMORE;
             g_Status.mpMax = 50;
             g_Status.mp = g_Status.mpMax;
             g_Status.statsBase[0] = 20;
@@ -122,6 +129,14 @@ void DemoGameInit(s32 arg0) {
             g_Status.equipment[RIGHT_HAND_SLOT] = ITEM_IRON_SHIELD;
             g_Status.subWeapon = 9;
             g_Status.statsBase[0] = 10;
+        }
+        if (g_StageId == STAGE_CEN) {
+            g_Status.equipment[LEFT_HAND_SLOT] = ITEM_BASTARD_SWORD;
+            g_Status.equipment[RIGHT_HAND_SLOT] = ITEM_LEATHER_SHIELD;
+            g_Status.subWeapon = 2;
+            g_Status.hearts = 30;
+            g_Status.statsBase[0] = 20;
+            g_Status.statsBase[2] = 20;
         }
         if (g_StageId == STAGE_RBO6) {
             g_Status.mpMax = 80;
@@ -158,6 +173,7 @@ void DemoGameInit(s32 arg0) {
         }
         if (g_StageId == STAGE_RBO2) {
             g_Status.equipment[LEFT_HAND_SLOT] = ITEM_CLAYMORE;
+            g_Status.equipment[RIGHT_HAND_SLOT] = ITEM_CLAYMORE;
             g_Status.statsBase[0] = 25;
             g_Status.statsBase[2] = 40;
             g_Status.equipment[ARMOR_SLOT] = ITEM_ALUCARD_MAIL;
@@ -209,51 +225,33 @@ void DemoGameInit(s32 arg0) {
 
 void DemoOpenFile(s32 arg0) {
     char fileName[0x100];
-    long fd;
+    s32 var_s0;
 
-    if (g_UseDisk) {
-        g_CdStep = CdStep_LoadInit;
-        g_LoadFile = CdFile_DemoKey;
-        g_LoadOvlIdx = g_DemoKeyIdx;
-        return;
-    }
-    if (arg0 == 0) {
-        __builtin_memcpy(fileName, "sim:c:\\bin\\demo_key.bin",
-                         sizeof("sim:c:\\bin\\demo_key.bin"));
+    func_890A3C0(DEMO_KEY_PTR, 0, 0x2000);
+    if ((D_8C630D4 == 2) && (arg0 == 0)) {
+        sprintf(fileName, "BIN/DEMOKEY%c%c.BIN;1",
+                ((D_psp_08C630DC / 10) % 10) + '0',
+                (D_psp_08C630DC % 10) + '0');
+        var_s0 = 0;
     } else {
-        __builtin_memcpy(fileName, "sim:c:\\bin\\dk_000.bin",
-                         sizeof("sim:c:\\bin\\dk_000.bin"));
-        fileName[15] = '0' + (g_DemoKeyIdx / 10 % 10);
-        fileName[16] = '0' + (g_DemoKeyIdx % 10);
+        sprintf(fileName, "BIN/DEMOKEY.BIN;1");
+        var_s0 = g_DemoKeyIdx << 0xD;
     }
-    fd = open(fileName, O_RDONLY);
-    if (fd < 0) {
-        return;
+    while (func_890FBEC(fileName, DEMO_KEY_PTR, var_s0, 0x2000) == 0) {
+        VSync(30);
     }
-    read(fd, DEMO_KEY_PTR, 0x2000);
-    close(fd);
 }
 
 void DemoSaveFile(void) {
     char buf[0x100];
     long fd;
-
-    __builtin_memcpy(
-        buf, "sim:c:\\bin\\demo_key.bin", sizeof("sim:c:\\bin\\demo_key.bin"));
-    fd = open(buf, O_CREAT);
-    if (fd < 0) {
-        return;
-    }
-    if (write(fd, DEMO_KEY_PTR, 0x2000) < 0) {
-        close(fd);
-        return;
-    }
-    if (close(fd) < 0) {
-        return;
-    }
-
-    __builtin_memcpy(buf, "  OK", sizeof("  OK"));
-    DebugInputWait(buf);
+    // Castlevania X
+    sprintf(buf,
+            "host0:c:/psp/悪魔城ドラキュラＸ/PRODUCTS/DISK/BIN/DEMOKEY%c%c.BIN",
+            ((D_psp_08C630DC / 10) % 10) + '0', (D_psp_08C630DC % 10) + '0');
+    fd = func_8939D68(buf, 0x602, 0x1FF);
+    func_8939D20(fd, DEMO_KEY_PTR, 0x2000);
+    func_8939D40(fd);
 }
 
 void DemoInit(s32 arg0) {
@@ -282,11 +280,84 @@ void DemoInit(s32 arg0) {
     }
 }
 
+typedef struct {
+    u16 psx;
+    u16 psp;
+} ButtonMap;
+
+// mapping between PSX and PSP buttons
+static ButtonMap D_psp_09160AA0[] = {
+    {0x1000, PAD_UP},     {0x4000, PAD_DOWN},        {0x8000, PAD_LEFT},
+    {0x2000, PAD_RIGHT},  {0x0010, PAD_TRIANGLE},    {0x0040, PAD_CROSS},
+    {0x0080, PAD_SQUARE}, {0x0020, PAD_CIRCLE},      {0x0100, PAD_SELECT},
+    {0x0800, PAD_START},  {0x0004, PAD_L1 | PAD_R1}, {0x0008, PAD_R1},
+    {0x0002, PAD_L1}};
+static ButtonMap D_psp_09160AD8[] = {
+    {0x1000, PAD_UP},     {0x4000, PAD_DOWN},     {0x8000, PAD_LEFT},
+    {0x2000, PAD_RIGHT},  {0x0010, PAD_TRIANGLE}, {0x0040, PAD_CROSS},
+    {0x0080, PAD_SQUARE}, {0x0020, PAD_CIRCLE},   {0x0100, PAD_SELECT},
+    {0x0008, PAD_R1},     {0x0004, PAD_L1},       {0x0800, PAD_START}};
+
+u16 func_psp_090FF548(u16 psx) {
+    ButtonMap* ptr;
+    s32 i;
+    u16 ret;
+    s32 count;
+
+    ret = 0;
+    if (g_PlayableCharacter == PLAYER_ALUCARD) {
+        ptr = D_psp_09160AA0;
+        count = LEN(D_psp_09160AA0);
+    } else {
+        ptr = D_psp_09160AD8;
+        count = LEN(D_psp_09160AD8);
+    }
+    for (i = 0; i < count; i++) {
+        if (psx & ptr[i].psx) {
+            ret |= ptr[i].psp;
+        }
+    }
+    return ret;
+}
+
+u16 func_psp_090FF608(u16 psp) {
+    s32 i;
+    s32 count;
+    ButtonMap* ptr;
+
+    u16 ret = 0;
+    if (g_PlayableCharacter == PLAYER_ALUCARD) {
+        ptr = D_psp_09160AA0;
+        count = LEN(D_psp_09160AA0);
+        for (i = 0; i < count; i++) {
+            if ((psp & ptr[i].psp) != ptr[i].psp) {
+                continue;
+            }
+            ret |= ptr[i].psx;
+
+            if (i >= 10) {
+                break;
+            }
+        }
+    } else {
+        ptr = D_psp_09160AD8;
+        count = LEN(D_psp_09160AD8);
+        for (i = 0; i < count; i++) {
+            if (psp & ptr[i].psp) {
+                ret |= ptr[i].psx;
+            }
+        }
+    }
+    return ret;
+}
+
 void DemoUpdate(void) {
     u8 curBtnLo;
     u8 curBtnHi;
+    u16 curBtn;
     u8 btnLo;
     u8 btnHi;
+    u32 btn;
     u8 frameCount;
     s32 demoOffset;
 
@@ -300,6 +371,7 @@ void DemoUpdate(void) {
 
     case Demo_PlaybackInit:
     case Demo_Playback:
+        func_8926498();
         FntPrint("demonstration\n");
         if (!frameCount) {
             g_DemoPtr += DEMO_KEY_LEN;
@@ -316,7 +388,9 @@ void DemoUpdate(void) {
                 g_DemoMode = Demo_None;
             }
         } else {
-            g_pads[0].pressed = btnLo + (btnHi << 8);
+            btn = btnLo + (btnHi << 8);
+            g_pads[0].pressed = 0;
+            g_pads[0].pressed = func_psp_090FF548(btn);
             g_pads[0].tapped = 0;
             g_DemoPtr[2]--;
         }
@@ -330,15 +404,16 @@ void DemoUpdate(void) {
             return;
         }
 
-        if (g_pads[1].tapped & PAD_CIRCLE) {
+        if (g_pads[0].tapped & PAD_SELECT) {
             g_DemoPtr += DEMO_KEY_LEN;
             g_DemoPtr[0] = 0xFF;
             g_DemoPtr[1] = 0xFF;
             DemoSaveFile();
             g_DemoMode = Demo_None;
         }
-        curBtnLo = g_pads[0].pressed;
-        curBtnHi = g_pads[0].pressed >> 8;
+        curBtn = func_psp_090FF608(g_pads[0].pressed);
+        curBtnLo = curBtn & 0xFF;
+        curBtnHi = curBtn >> 8;
         if (frameCount != 0xFF && btnLo == curBtnLo && btnHi == curBtnHi) {
             g_DemoPtr[2]++;
         } else {
