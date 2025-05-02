8fb70adbdca6545f5da15d73ce84ed9c044cdad0
Joey Murphy <tjmurphy@mit.edu>
2025-05-01 20:49:50 -0400

    func_800F2860

--- src/dra_psp/A710.c
+++ src/dra_psp/A710.c
@@ -924,7 +924,57 @@ bool func_800F27F4(s32 arg0) {
     D_801375C8 = 8;
 }
 
-INCLUDE_ASM("dra_psp/psp/dra_psp/A710", func_800F2860);
+void func_800F2860(void) {
+    s32 var_v0;
+
+    switch (D_801375C8) {
+    case 1:
+        PlaySfx(0x80);
+        D_801375C8++;
+        break;
+
+    case 2:
+        if (func_80131F68() == false) {
+            D_801375C8++;
+            break;
+        }
+        break;
+    case 3:
+#if defined(VERSION_US)
+        PlaySfx(0x33F);
+#else
+        PlaySfx(0x33C);
+#endif
+        D_801375C8++;
+        break;
+    case 4:
+        if (func_80131F68() != false) {
+            D_801375C8++;
+            break;
+        }
+        break;
+    case 5:
+        if (func_80131F68() == false) {
+            D_801375C8++;
+            break;
+        }
+        break;
+    case 6:
+        PlaySfx(D_80097910);
+        D_801375C8 = 0;
+        return;
+    case 7:
+        if (func_80131F68() != 0) {
+            return;
+        }
+        D_801375C8--;
+        break;
+    case 8:
+        PlaySfx(0x80);
+        D_801375C8--;
+        break;
+    }
+}
 
 extern bool D_80137598;
 extern s32 D_8013759C;
