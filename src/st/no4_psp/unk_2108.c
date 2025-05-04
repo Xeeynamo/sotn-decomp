7f335d9a99c3835ce432f4000dab8dac272a3460
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-05-05 03:40:11 +1000

    Import data for `EntityBreakableCrystalFloor` (#2526)
    For @SestrenExsis to show an example
    
    I also split the file up - it appears that the 2 functions after this
    are related to another secret wall in `no4` based on PSP splits.

---
+++ src/st/no4_psp/unk_2108.c
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09239788);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_092398B8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09239B00);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923B288);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923B378);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923BAB0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923BB30);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923C460);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923D0B8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923D0C8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923D180);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923D428);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923D4A0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923D568);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923D9E0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923DD38);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923E368);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923E690);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923EA80);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923EED0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923F390);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923F860);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923FB88);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_0923FF60);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09240350);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09240680);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_092409A8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09240CD0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09241098);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09241410);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09241870);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09241B28);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09241E38);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09242200);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_092423C8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_092427C8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_092428E0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09242940);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09242AD0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09242D08);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09242E30);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09242FB8);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09243260);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_092432C0);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09243320);
+
+INCLUDE_ASM("st/no4_psp/psp/no4_psp/unk_2108", func_pspeu_09243478);
