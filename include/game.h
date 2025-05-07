b7cdda582c23475674c380547c197f000bf94901
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2025-05-08 00:28:17 +0100

    Remove LangImg struct (#2542)
    I noticed the struct `LangImg` is useless as the pattern for loading
    image data was already figured out

--- include/game.h
+++ include/game.h
@@ -2070,16 +2070,7 @@ typedef enum {
 } Language;
 u8* GetLangAt(s32 idx, u8* en, u8* fr, u8* sp, u8* ge, u8* it);
 
-typedef struct {
-    s16 unk00;
-    s16 unk02;
-    RECT rect;
-    u8* imgData;
-    s32 unk10;
-    s32 unk14;
-} LangImg;
-
 void func_892667C(s32 paletteID, u16* palette);
-void func_91040A0(LangImg* img);
+void func_91040A0(u_long** gfxBank);
 
 #endif
