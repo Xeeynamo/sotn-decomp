f32673a4c43e1590fa4a55ec571636dcdab70e73
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-05-08 00:17:33 +0100

    remove struct

--- include/game.h
+++ include/game.h
@@ -2070,15 +2070,6 @@ typedef enum {
 } Language;
 u8* GetLangAt(s32 idx, u8* en, u8* fr, u8* sp, u8* ge, u8* it);
 
-typedef struct {
-    s16 unk00;
-    s16 unk02;
-    RECT rect;
-    u8* imgData;
-    s32 unk10;
-    s32 unk14;
-} LangImg;
-
 void func_892667C(s32 paletteID, u16* palette);
 void func_91040A0(u_long** gfxBank);
 
