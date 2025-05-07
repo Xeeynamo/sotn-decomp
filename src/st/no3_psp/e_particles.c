1c4081d31aeca35a48377ffc99882d372bbb84d4
bismurphy <tjmurphy@mit.edu>
2025-05-07 02:29:49 -0400

    Add NO3 overlay to PSP version (#2533)
    Took a bit of fighting to get this to work, but now we have a working
    NO3 overlay for PSP.
    
    I intend to chip away at this slowly when I get time to do decomp stuff.
    Should be a nice quiet corner :)

---
+++ src/st/no3_psp/e_particles.c
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", EntitySoulStealOrb);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", EntityEnemyBlood);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09252C78);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09252CD8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09252E60);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", EntityWarg);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", EntityUnkId4B);
+
+// clumsy thing, linter splits the line which breaks INCLUDE_ASM
+// clang-format off
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", EntityWargExplosionPuffTransparent);
+// clang-format on
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_092546E0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09254778);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_092547B8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09254D70);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09254E48);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09254FC8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09255068);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09255168);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09255400);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_092554C0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_09255518);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", EntityPrizeDrop);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", EntityExplosion);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", func_pspeu_092564C0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_particles", EntityEquipItemDrop);
---
+++ src/st/no3_psp/e_room_fg.c
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", EntityRoomForeground);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925F8D8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925F970);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925F9F8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925FB68);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925FBD8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925FC30);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925FCB8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925FD08);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925FD40);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_0925FFF8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_092602D8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09260348);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_092603F0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09260470);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09260568);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_092605B8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_092605F8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", EntityDummy);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_092607D8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09260928);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09260AB8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09260F90);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09261088);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09261588);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_092615D8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_092616E0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", func_pspeu_09261748);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", EntityZombie);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_room_fg", EntityZombieSpawner);
