0c4f968145e1950087c9eb8e70a4d8a6bcec845e
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-05-09 21:54:13 +0100

    licenzz

--- src/maria/pl_anims.c
+++ src/maria/pl_anims.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 #include "maria.h"
 
 AnimationFrame mar_anim_stand_relax[] = {
--- src/maria/pl_header.c
+++ src/maria/pl_header.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 #include "maria.h"
 
 extern unsigned char* maria_sprites[];
