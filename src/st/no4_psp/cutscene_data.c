848c06b3a905745184b6dd0596ec6609f5a893c9
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-05-10 17:02:53 +1000

    Decompile `no4` PSP `st_init_no4`

--- src/st/no4_psp/cutscene_data.c
+++ src/st/no4_psp/cutscene_data.c
@@ -3,43 +3,43 @@
 #include <cutscene.h>
 
 // Played when the ferryman takes the player right across the river
-u8 D_pspeu_09267DA8[] = {
+u8 cutscene_data_ferryman_right_it[] = {
 #include "gen_cutscene_data_ferryman_right_it.h"
 };
 
-u8 D_pspeu_09267DF0[] = {
+u8 cutscene_data_ferryman_right_sp[] = {
 #include "gen_cutscene_data_ferryman_right_sp.h"
 };
 
-u8 D_pspeu_09267E30[] = {
+u8 cutscene_data_ferryman_right_fr[] = {
 #include "gen_cutscene_data_ferryman_right_fr.h"
 };
 
-u8 D_pspeu_09267E70[] = {
+u8 cutscene_data_ferryman_right_ge[] = {
 #include "gen_cutscene_data_ferryman_right_ge.h"
 };
 
-u8 D_pspeu_09267EB8[] = {
+u8 cutscene_data_ferryman_right_en[] = {
 #include "gen_cutscene_data_ferryman_right_en.h"
 };
 
 // Played when the ferryman takes the player left across the river
-u8 D_pspeu_09267EF8[] = {
+u8 cutscene_data_ferryman_left_it[] = {
 #include "gen_cutscene_data_ferryman_left_it.h"
 };
 
-u8 D_pspeu_09267F50[] = {
+u8 cutscene_data_ferryman_left_sp[] = {
 #include "gen_cutscene_data_ferryman_left_sp.h"
 };
 
-u8 D_pspeu_09267FB8[] = {
+u8 cutscene_data_ferryman_left_fr[] = {
 #include "gen_cutscene_data_ferryman_left_fr.h"
 };
 
-u8 D_pspeu_09268020[] = {
+u8 cutscene_data_ferryman_left_ge[] = {
 #include "gen_cutscene_data_ferryman_left_ge.h"
 };
 
-u8 D_pspeu_09268088[] = {
+u8 cutscene_data_ferryman_left_en[] = {
 #include "gen_cutscene_data_ferryman_left_en.h"
 };
