196e3b72b35baafe370403e76bacaa9b7d25ebc7
Joey Murphy <tjmurphy@mit.edu>
2025-05-11 09:37:33 -0400

    Good start to NO3, got entry funcs matching

---
+++ src/st/no3_psp/cutscene_data.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../no3/no3.h"
+#include <cutscene.h>
+
+u8 cutscene_data_it[] = {
+#include "gen_cutscene_data_it.h"
+};
+
+u8 cutscene_data_sp[] = {
+#include "gen_cutscene_data_sp.h"
+};
+
+u8 cutscene_data_fr[] = {
+#include "gen_cutscene_data_fr.h"
+};
+
+u8 cutscene_data_ge[] = {
+#include "gen_cutscene_data_ge.h"
+};
+
+u8 cutscene_data_en[] = {
+#include "gen_cutscene_data_en.h"
+};
