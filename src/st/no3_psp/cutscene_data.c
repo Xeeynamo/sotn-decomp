11f57d99ce72a6c3840dea0db48d0e74f9cdc834
Joey Murphy <tjmurphy@mit.edu>
2025-05-11 19:02:01 -0400

    gen path swap

--- src/st/no3_psp/cutscene_data.c
+++ src/st/no3_psp/cutscene_data.c
@@ -3,21 +3,21 @@
 #include <cutscene.h>
 
 u8 cutscene_data_it[] = {
-#include "gen_cutscene_data_it.h"
+#include "gen/cutscene_data_it.h"
 };
 
 u8 cutscene_data_sp[] = {
-#include "gen_cutscene_data_sp.h"
+#include "gen/cutscene_data_sp.h"
 };
 
 u8 cutscene_data_fr[] = {
-#include "gen_cutscene_data_fr.h"
+#include "gen/cutscene_data_fr.h"
 };
 
 u8 cutscene_data_ge[] = {
-#include "gen_cutscene_data_ge.h"
+#include "gen/cutscene_data_ge.h"
 };
 
 u8 cutscene_data_en[] = {
-#include "gen_cutscene_data_en.h"
+#include "gen/cutscene_data_en.h"
 };
