611ea618e7474e27584f3d301a7acde30771c709
gamezter <charronalexandre@hotmail.com>
2025-05-12 13:19:48 -0400

    Import `dra_psp` lba_stage.c (#2559)

--- include/game.h
+++ include/game.h
@@ -724,8 +724,7 @@ typedef struct {
     /* 0x24 */ char* name;
     /* 0x28 */ u8 unk28;
     /* 0x29 */ s8 seqIdx; // index of D_800ACCF8
-    /* 0x2A */ u16 unk2A;
-} Lba; /* size=0x2C */
+} Lba;                    /* size=0x2A */
 
 typedef struct {
     /* 0x00 */ s16 cursorX;
