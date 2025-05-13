153152029d2707825c05e428b26a58a32a198eee
Joey Murphy <tjmurphy@mit.edu>
2025-05-13 07:53:23 -0400

    PSP split weights file

---
+++ src/st/no3_psp/entrance_weights.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../no3/no3.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/entrance_weights", func_pspeu_09239020);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/entrance_weights", EntityWeightsSwitch);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/entrance_weights", EntityPathBlockSmallWeight);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/entrance_weights", EntityPathBlockTallWeight);
