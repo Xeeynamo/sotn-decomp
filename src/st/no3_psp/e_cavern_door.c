e3a3442de2159e827d56ecbf7ebdeb9c1f56b841
Joey Murphy <tjmurphy@mit.edu>
2025-05-13 18:31:21 -0400

    psp cavern door to own file

---
+++ src/st/no3_psp/e_cavern_door.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../no3/no3.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_cavern_door", DoorCascadePhysics);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_cavern_door", EntityCavernDoorLever);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_cavern_door", EntityCavernDoorPlatform);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_cavern_door", EntityCavernDoor);
