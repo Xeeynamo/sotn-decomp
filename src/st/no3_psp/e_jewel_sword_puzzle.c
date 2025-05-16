5585d9b07771064c5fd9fd613f514283e006233a
Joey Murphy <tjmurphy@mit.edu>
2025-05-16 08:26:28 -0400

    data maybe starting to come together

--- src/st/no3_psp/e_jewel_sword_puzzle.c
+++ src/st/no3_psp/e_jewel_sword_puzzle.c
@@ -14,13 +14,12 @@ static u16 initRockTiles[] = {
     0x0332, 0x033A, 0x0351, 0x0000, 0x0000, 0x0000,
     0x0332, 0x033A, 0x0351, 0x0350, 0x032F, 0x034E,
 };
-static u16 mermanRockTiles2[] = {
-    0x02A7, 0x0287, 0x02B4, 0x02A8, 
-    0x02AE, 0x02B5, 0x06DB, 0x06DC, 
-    0x06DD, 0x06B7, 0x0000, 0x06C6,
+static u16 rockTiles2[] = {
+    0x02A7, 0x0287, 0x02B4, 0x02A8, 0x02AE, 0x02B5, 
+    0x06DB, 0x06DC, 0x06DD, 0x06B7, 0x0000, 0x06C6,
     // Index 12
-    0x06B6, 0x06B8, 0x06B9, 0x06B7, 
-    0x0000, 0x06C6, 0,           0,
+    0x06B6, 0x06B8, 0x06B9, 0x06B7, 0x0000, 0x06C6};
+static u16 rockTiles3[] = {
     0x0000, 0x0000, 0x0000, 0x0350, 
     0x032F, 0x034E, 
     // Index 26
@@ -158,7 +157,7 @@ void EntityMermanRockRightSide(Entity* self) {
         self->hitboxWidth = 16;
         self->hitboxHeight = 24;
 
-        tileLayoutPtr = &mermanRockTiles2[26];
+        tileLayoutPtr = &rockTiles3[6];
         tilePos = 0x1FD;
         for (i = 0; i < 3; i++, tileLayoutPtr++) {
             g_BgLayers[0].layout[tilePos] = *tileLayoutPtr;
@@ -168,7 +167,7 @@ void EntityMermanRockRightSide(Entity* self) {
 
         if (g_CastleFlags[JEWEL_SWORD_ROOM_STEPS] & rockBroken) {
             tilePos = 0x1FD;
-            tileLayoutPtr = &mermanRockTiles2[12];
+            tileLayoutPtr = &rockTiles2[12];
             for (i = 0; i < 3; i++, tileLayoutPtr++) {
                 g_Tilemap.fg[tilePos] = *tileLayoutPtr;
                 *(&g_Tilemap.fg[tilePos] + 1) = *(tileLayoutPtr + 3);
@@ -181,7 +180,7 @@ void EntityMermanRockRightSide(Entity* self) {
 
     case 1:
         if (self->hitFlags) {
-            tileLayoutPtr = &mermanRockTiles2[self->ext.mermanRock.unk84 * 6] + 6;
+            tileLayoutPtr = &rockTiles2[self->ext.mermanRock.unk84 * 6] + 6;
             tilePos = 0x1FD;
             for (i = 0; i < 3; i++, tileLayoutPtr++) {
                 g_Tilemap.fg[tilePos] = *tileLayoutPtr;
@@ -255,14 +254,14 @@ void EntityJewelSwordDoor(Entity* self) {
         break;
 
     case 2:
-        for (tileLayoutPtr = &mermanRockTiles2[47], i = 0; i < 3; i++) {
+        for (tileLayoutPtr = &rockTiles2[47], i = 0; i < 3; i++) {
             tileLayoutPos = 0x420 + i;
             for (j = 0; j < 5; tileLayoutPos += 0x30, j++, tileLayoutPtr++) {
                 g_Tilemap.fg[tileLayoutPos] = *tileLayoutPtr;
             }
         }
 
-        for (tileLayoutPtr = &mermanRockTiles2[79], i = 0; i < 3; i++) {
+        for (tileLayoutPtr = &rockTiles2[79], i = 0; i < 3; i++) {
             tileLayoutPos = 0x420 + i;
             for (j = 0; j < 5; j++, tileLayoutPtr++) {
                 g_BgLayers[0].layout[tileLayoutPos] = *tileLayoutPtr;
