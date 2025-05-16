e92d7978bf2f93f5e165532874e26e1d0e037480
Joey Murphy <tjmurphy@mit.edu>
2025-05-15 20:12:39 -0400

    Split out for psp, ready for decomp

---
+++ src/st/no3_psp/e_jewel_sword_puzzle.c
@@ -0,0 +1,9 @@
+#include "../no3/no3.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_jewel_sword_puzzle", EntityMermanRockLeftSide);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_jewel_sword_puzzle", EntityMermanRockRightSide);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_jewel_sword_puzzle", EntityJewelSwordDoor);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_jewel_sword_puzzle", EntityFallingRock2);
