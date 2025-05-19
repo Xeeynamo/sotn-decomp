b5d9bd733aae41d9a1cafc29ebdcb8207e2aa64a
Joey Murphy <tjmurphy@mit.edu>
2025-05-18 20:06:49 -0400

    Split for explosion puff opaque next

---
+++ src/st/no3_psp/e_explosion_puff_opaque.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include "../no3/no3.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_explosion_puff_opaque", func_pspeu_0923D348);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_explosion_puff_opaque", EntityExplosionPuffOpaque);
