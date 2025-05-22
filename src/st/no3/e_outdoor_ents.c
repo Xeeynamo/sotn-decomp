39746cfa227da374aa8c5dad8c89cf83f17841ae
Joey Murphy <tjmurphy@mit.edu>
2025-05-22 17:40:00 -0400

    Some good data

--- src/st/no3/e_outdoor_ents.c
+++ src/st/no3/e_outdoor_ents.c
@@ -37,8 +37,8 @@ void EntityPushAlucard(Entity* self) {
         player->posX.val += FIX(8.5);
         g_unkGraphicsStruct.unkC += 4;
         if (g_unkGraphicsStruct.unkC == 192) {
-            self->ext.alucardController.unk80 = FIX(4.5);
             self->step++;
+            self->ext.alucardController.unk80 = FIX(4.5);
         }
         g_Player.demo_timer = 1;
         g_api.func_8010E0A8();
@@ -72,22 +72,22 @@ void EntityPushAlucard(Entity* self) {
         break;
 
     case 5:
-        if ((player->velocityY > 0) &&
-            (self->ext.alucardController.unk7C == false)) {
+        if ((player->velocityY > 0) && !self->ext.alucardController.unk7C) {
             g_Player.padSim = PAD_CROSS;
             self->ext.alucardController.unk7C = true;
         } else {
             g_Player.padSim = PAD_RIGHT | PAD_CROSS;
         }
         g_api.func_8010E0A8();
-        g_Player.demo_timer = 1;
         player->posX.val += FIX(4.5);
+        g_Player.demo_timer = 1;
     }
 }
 
 // Pushes Alucard through the castle door at the entrance
 void EntityCastleDoorTransition(Entity* self) {
     Entity* player = &PLAYER;
+    Tilemap* tilemap = &g_Tilemap;
 
     switch (self->step) {
     case 0:
@@ -100,13 +100,13 @@ void EntityCastleDoorTransition(Entity* self) {
         g_Player.padSim = PAD_RIGHT;
         g_Player.demo_timer = 255;
         player->posX.i.hi = 8;
-        self->ext.castleDoorTransition.playerVelocity = 0x28000;
+        self->ext.castleDoorTransition.playerVelocity = FIX(2.5);
         break;
 
     case 1:
         player->posX.val += self->ext.castleDoorTransition.playerVelocity;
         g_Player.demo_timer = 1;
-        if ((player->posX.i.hi + g_Tilemap.scrollX.i.hi) > 120) {
+        if ((player->posX.i.hi + tilemap->scrollX.i.hi) > 120) {
             g_Player.padSim = 0;
             self->step++;
         }
@@ -114,19 +114,39 @@ void EntityCastleDoorTransition(Entity* self) {
 
     case 2:
         if (self->ext.castleDoorTransition.playerVelocity != 0) {
-            self->ext.castleDoorTransition.playerVelocity -= 0x2800;
+            self->ext.castleDoorTransition.playerVelocity -= FIX(5.0 / 32);
             EntityExplosionVariantsSpawner(
-                &PLAYER, 1, 1, 4, 0x18, (Random() & 3) + 1, -4);
+                player, 1, 1, 4, 0x18, (Random() & 3) + 1, -4);
         } else {
-            g_PauseAllowed = true;
             self->step++;
+            g_PauseAllowed = true;
         }
         player->posX.val += self->ext.castleDoorTransition.playerVelocity;
         g_Player.demo_timer = 1;
         break;
+    case 3:
+        break;
     }
 }
 
+static u16 D_801813DC[] = {
+    0x0080, 0x0290, 0x0130, 0x0498, 0x01E0, 0x0480, 0x0290, 0x028A, 0x0340,
+    0x0298, 0x03F8, 0x0290, 0x04A0, 0x0480, 0x0554, 0x0288, 0x0608, 0x0498,
+    0x06B6, 0x0494, 0x0760, 0x0284, 0x0810, 0x0290, 0x08C0, 0x0498, 0x0970,
+    0x028E, 0x0A20, 0x0298, 0x0AD0, 0x0482, 0x0B88, 0x0288, 0x0C38, 0x0280,
+    0x0CE0, 0x0498, 0x0D90, 0x0286, 0x0E50, 0x0286, 0x0F10, 0x0286, 0x0FD8,
+    0x0286, 0x10A0, 0x0286, 0x1160, 0x0286, 0x1240, 0x0286, 0x1320, 0x0286,
+    0x1400, 0x0286, 0x14E8, 0x0286, 0x15D0, 0x0286, 0x16C0, 0x0286, 0x17C0,
+    0x0286, 0x18D0, 0x0286, 0x19A0, 0x0286, 0xFFFF, 0x0290};
+static u16 D_80181468[] = {
+    0x0040, 0x0290, 0x00C0, 0x0498, 0x0140, 0x0480, 0x01C0, 0x028A, 0x0240,
+    0x0298, 0x02C8, 0x0290, 0x0348, 0x0480, 0x03C0, 0x0288, 0x0440, 0x0498,
+    0x04C8, 0x0494, 0x0548, 0x0284, 0x05C0, 0x0290, 0x0640, 0x0498, 0x06C0,
+    0x028E, 0x0740, 0x0298, 0x07C0, 0x0482, 0x0848, 0x0288, 0x08C8, 0x0280,
+    0x0940, 0x0498, 0x09C0, 0x0286, 0x0A48, 0x0286, 0x0AD0, 0x0290, 0x0B58,
+    0x0482, 0x0BE0, 0x0298, 0x0C70, 0x0288, 0x0D00, 0x0286, 0x0D90, 0x0480,
+    0x0E30, 0x0292, 0x0EE0, 0x0296, 0x0FC0, 0x0488, 0x10C0, 0x0294, 0x1200,
+    0x0482, 0xFFFF, 0x0290};
 // large foreground tree during intro
 void EntityForegroundTree(Entity* self) {
     Tilemap* tilemap = &g_Tilemap;
