b8aeae0b5e4ba22eb2ab6c3de031a3c5c56415fc
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-05-24 10:04:36 +0100

    hotfix: fix CI by using builds gen with a venv

--- Makefile.psx.mk
+++ Makefile.psx.mk
@@ -1,15 +1,15 @@
 .PHONY: ninja
 ninja: bin/cc1-psx-26 $(MASPSX_APP) $(SOTNASSETS)
-	VERSION=$(VERSION) python3 tools/builds/gen.py
+	VERSION=$(VERSION) .venv/bin/python3 tools/builds/gen.py
 	ninja
 
 .PHONY: extract_us
 extract_us:
-	VERSION=us python3 tools/builds/gen.py
+	VERSION=us .venv/bin/python3 tools/builds/gen.py
 	ninja build/us/dra.ld
 .PHONY: extract_hd
 extract_hd:
-	VERSION=hd python3 tools/builds/gen.py
+	VERSION=hd .venv/bin/python3 tools/builds/gen.py
 	ninja build/hd/dra.ld
 
 extract_disk_us: extract_disk_psxus
