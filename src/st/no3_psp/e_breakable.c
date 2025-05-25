db76eaf479c857c494ab93e6b1b4f4cf8e38fcfa
Joey Murphy <tjmurphy@mit.edu>
2025-05-25 09:15:49 -0400

    Working on PSP

--- src/st/no3_psp/e_breakable.c
+++ src/st/no3_psp/e_breakable.c
@@ -2,4 +2,33 @@
 
 #include "../no3/no3.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_breakable", EntityBreakable);
+static u8 anim_1[] = {4, 1, 4, 2, 0, 0, 0, 0};
+static u8 anim_2[] = {4, 0, 4, 0, 0, 0, 0, 0};
+static u8* g_eBreakableAnimations[] = {
+    anim_1, anim_2, NULL, NULL, NULL, NULL, NULL, NULL};
+static u8 g_eBreakableHitboxes[] = {8, 8, 0, 0, 0, 0, 0, 0};
+
+#ifndef VERSION_PSP // on PSP this is in the BSS section, not data
+static u8 g_eBreakableExplosionTypes[] = {0, 0, 0, 0, 0, 0, 0, 0};
+#else
+static u8 g_eBreakableExplosionTypes[8];
+#endif
+
+static u16 g_eBreakableanimSets[] = {
+    ANIMSET_DRA(3), ANIMSET_DRA(3), 0, 0, 0, 0, 0, 0};
+static u8 g_eBreakableDrawModes[] = {
+    DRAW_TPAGE | DRAW_TPAGE2 | DRAW_UNK_40,
+    DRAW_TPAGE | DRAW_TPAGE2,
+    DRAW_DEFAULT,
+    DRAW_DEFAULT,
+    DRAW_DEFAULT,
+    DRAW_DEFAULT,
+    DRAW_DEFAULT,
+    DRAW_DEFAULT};
+
+#ifndef VERSION_PSP
+// on PSP this might be either optimised out to BSS or completely removed
+static u8 unused[] = {0, 0, 0, 0, 0, 0, 0, 0};
+#endif
+
+#include "../e_breakable.h"
