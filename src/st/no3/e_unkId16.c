fa13d6304831087e0466b196f7234925ae6b6058
bismurphy <tjmurphy@mit.edu>
2025-05-25 15:39:35 -0400

    NO3 EntityBackgroundBlock improvements (#2605)
    I've been making a bit of a personal rule to not do `extern` for data
    members on PSP, and it seems to be going well. The way I see it, I
    either have to add the data to the symbols file or pull it into the
    function, and it's better to have it in the function anyway. So here we
    pull in the ObjInit2 data for this function, on both versions.
    
    Also some nice file splitting to prepare things for decompiling the
    cutscene information on PSP, which I expect to be a bit of a hefty task.

--- src/st/no3/e_unkId16.c
+++ src/st/no3/e_unkId16.c
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "no3.h"
 
-#include "../entrance_stage_entities.h"
+#include "../entity_unkId16.h"
---
+++ src/st/no3_psp/cutscene.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include "../no3/no3.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_09242C00);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_09242CA0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_09242FB8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_09243040);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_09243108);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_09243168);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_09243378);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_092433B0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", func_pspeu_09243748);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/cutscene", NO3_EntityCutscene);
