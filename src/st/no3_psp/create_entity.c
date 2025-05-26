8aab0569eb2e9e4183c35993d808faf92fbacbc0
Joey Murphy <tjmurphy@mit.edu>
2025-05-26 18:48:47 -0400

    one func down

--- src/st/no3_psp/create_entity.c
+++ src/st/no3_psp/create_entity.c
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 #include "../no3/no3.h"
+#include "../pfn_entity_update.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09245A58);
+//CreateEntityFromLayout
+void func_pspeu_09245A58(Entity* entity, LayoutEntity* initDesc) {
+    Tilemap* tilemap;
+
+    tilemap = &g_Tilemap;
+
+    DestroyEntity(entity);
+    entity->entityId = initDesc->entityId & 0x3FF;
+    entity->pfnUpdate = PfnEntityUpdates[entity->entityId - 1];
+    entity->posX.i.hi = (s16)initDesc->posX - tilemap->scrollX.i.hi;
+    entity->posY.i.hi = (s16)initDesc->posY - tilemap->scrollY.i.hi;
+    entity->params = initDesc->params;
+    entity->entityRoomIndex = initDesc->entityRoomIndex >> 8;
+    entity->unk68 = (initDesc->entityId >> 0xA) & 7;
+}
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09245B78);
 
