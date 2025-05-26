15c8bb94a42460b24b47d9768158946764aae90a
Joey Murphy <tjmurphy@mit.edu>
2025-05-26 18:40:56 -0400

    Pull out create entity

---
+++ src/st/no3_psp/create_entity.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include "../no3/no3.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09245A58);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09245B78);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09245D10);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09245EA8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09245F18);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09245FA0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_092460E0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09246238);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_092462B0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09246338);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", func_pspeu_09246480);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", InitRoomEntities);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", UpdateRoomPosition);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", CreateEntityFromCurrentEntity);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/create_entity", CreateEntityFromEntity);
--- src/st/no3_psp/working_on.c
+++ src/st/no3_psp/working_on.c
@@ -4,36 +4,6 @@
 
 #include "../no3/no3.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09245A58);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09245B78);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09245D10);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09245EA8);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09245F18);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09245FA0);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_092460E0);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09246238);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_092462B0);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09246338);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09246480);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", InitRoomEntities);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", UpdateRoomPosition);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", CreateEntityFromCurrentEntity);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", CreateEntityFromEntity);
-
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityBat);
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityStageNamePopup);
