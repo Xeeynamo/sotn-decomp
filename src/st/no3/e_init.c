a578705dbd3bd4f2906c15b048e74ced3ec46c7b
Joey Murphy <tjmurphy@mit.edu>
2025-05-27 19:10:15 -0400

    e_init for psp maybe

--- src/st/no3/e_init.c
+++ src/st/no3/e_init.c
@@ -203,15 +203,15 @@ EInit OVL_EXPORT(EInitBreakable) = {ANIMSET_DRA(3), 0, 0x00, 0x000, 0};
 #include "../e_init_common.h"
 EInit g_EInitStInteractable = {0x8001, 0, 0x0, 0x0, 5};
 EInit D_80180B24 = {0x8006, 1, 0x48, 0x2cb, 148};
-EInit D_80180B30 = {0x8006, 1, 0x48, 0x2cb, 151, 0, 0, 0};
-EInit D_80180B3C = {0x0, 0, 0x48, 0x2cb, 150, 0, 0, 0};
-EInit g_EInitWaterObject = {0x8003, 1, 0x48, 0x28c, 29, 0, 0, 0};
-EInit g_EInitWaterSplash = {0x8003, 0, 0x48, 0x28c, 30, 0, 0, 0};
-EInit D_80180B60 = {0x8004, 1, 0x4a, 0x2b2, 27, 0, 0, 0};
-EInit D_80180B6C = {0x8004, 0, 0x4a, 0x2b2, 28, 0, 0, 0};
-EInit g_EInitBoneScimitar = {0x8002, 1, 0x48, 0x2c5, 105, 0, 0, 0};
-EInit g_EInitScimitarParts = {0x8002, 1, 0x48, 0x2c5, 2, 0, 0, 0};
-EInit g_EInitBat = {0x8005, 0, 0x4c, 0x2c8, 64, 0, 0, 0};
-EInit D_80180B9C = {0x8006, 7, 0x48, 0x2d0, 175, 0, 0, 0};
-EInit D_80180BA8 = {0x8007, 0, 0x4d, 0x2d3, 110, 0, 0, 0};
+EInit D_80180B30 = {0x8006, 1, 0x48, 0x2cb, 151};
+EInit D_80180B3C = {0x0, 0, 0x48, 0x2cb, 150};
+EInit g_EInitWaterObject = {0x8003, 1, 0x48, 0x28c, 29};
+EInit g_EInitWaterSplash = {0x8003, 0, 0x48, 0x28c, 30};
+EInit D_80180B60 = {0x8004, 1, 0x4a, 0x2b2, 27};
+EInit D_80180B6C = {0x8004, 0, 0x4a, 0x2b2, 28};
+EInit g_EInitBoneScimitar = {0x8002, 1, 0x48, 0x2c5, 105};
+EInit g_EInitScimitarParts = {0x8002, 1, 0x48, 0x2c5, 2};
+EInit g_EInitBat = {0x8005, 0, 0x4c, 0x2c8, 64};
+EInit D_80180B9C = {0x8006, 7, 0x48, 0x2d0, 175};
+EInit D_80180BA8 = {0x8007, 0, 0x4d, 0x2d3, 110};
 
---
+++ src/st/no3_psp/e_init.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../no3/no3.h"
+
+EInit OVL_EXPORT(EInitBreakable) = {ANIMSET_DRA(3), 0, 0x00, 0x000, 0};
+#include "../e_init_common.h"
+EInit g_EInitStInteractable = {0x8001, 0, 0x0, 0x0, 5};
+EInit D_80180B24 = {0x8006, 1, 0x48, 0x2cb, 148};
+EInit D_80180B30 = {0x8006, 1, 0x48, 0x2cb, 151};
+EInit D_80180B3C = {0x0, 0, 0x48, 0x2cb, 150};
+EInit g_EInitWaterObject = {0x8003, 1, 0x48, 0x28c, 29};
+EInit g_EInitWaterSplash = {0x8003, 0, 0x48, 0x28c, 30};
+EInit D_80180B60 = {0x8004, 1, 0x4a, 0x2b2, 27};
+EInit D_80180B6C = {0x8004, 0, 0x4a, 0x2b2, 28};
+EInit g_EInitBoneScimitar = {0x8002, 1, 0x48, 0x2c5, 105};
+EInit g_EInitScimitarParts = {0x8002, 1, 0x48, 0x2c5, 2};
+EInit g_EInitBat = {0x8005, 0, 0x4c, 0x2c8, 64};
+EInit D_80180B9C = {0x8006, 7, 0x48, 0x2d0, 175};
+EInit D_80180BA8 = {0x8007, 0, 0x4d, 0x2d3, 110};
