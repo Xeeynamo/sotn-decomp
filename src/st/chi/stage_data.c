d596d8873ce321d38a6ddfb6566243df021bf137
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-05-27 22:13:47 +0100

    Migrate CHI assets

---
+++ src/st/chi/stage_data.c
@@ -0,0 +1,80 @@
+#include <stage.h>
+
+u8 bn_gfx_stage_name_jp_sm[] = {
+#include "gen/bn_gfx_stage_name_jp_sm.h"
+};
+u8 bn_gfx_stage_name_jp_lg[] = {
+#include "gen/bn_gfx_stage_name_jp_lg.h"
+};
+u8 bn_gfx_gremlin[] = {
+#include "gen/bn_gfx_gremlin.h"
+};
+u8 bn_gfx_salem_witch_1[] = {
+#include "gen/bn_gfx_salem_witch_1.h"
+};
+u8 bn_gfx_salem_witch_2[] = {
+#include "gen/bn_gfx_salem_witch_2.h"
+};
+u8 bn_gfx_salem_witch_3[] = {
+#include "gen/bn_gfx_salem_witch_3.h"
+};
+u8 bn_gfx_salem_witch_4[] = {
+#include "gen/bn_gfx_salem_witch_4.h"
+};
+u8 bn_gfx_salem_witch_5[] = {
+#include "gen/bn_gfx_salem_witch_5.h"
+};
+u8 bn_gfx_thornweed_corpseweed[] = {
+#include "gen/bn_gfx_thornweed_corpseweed.h"
+};
+u8 bn_gfx_venus_weed_1[] = {
+#include "gen/bn_gfx_venus_weed_1.h"
+};
+u8 bn_gfx_venus_weed_2[] = {
+#include "gen/bn_gfx_venus_weed_2.h"
+};
+u8 bn_gfx_stage_name_en[] = {
+#include "gen/bn_gfx_stage_name_en.h"
+};
+
+u16 bn_pal_unused_0[] = {
+#include "gen/bn_pal_unused_0.h"
+};
+u16 bn_pal_salem_witch_projectiles[] = {
+#include "gen/bn_pal_salem_witch_projectiles.h"
+};
+u16 bn_pal_unused_1[] = {
+#include "gen/bn_pal_unused_1.h"
+};
+u16 bn_pal_gremlin[] = {
+#include "gen/bn_pal_gremlin.h"
+};
+u16 bn_pal_salem_witch[] = {
+#include "gen/bn_pal_salem_witch.h"
+};
+u16 bn_pal_thornweed_corpseweed[] = {
+#include "gen/bn_pal_thornweed_corpseweed.h"
+};
+u16 bn_pal_venus_weed[] = {
+#include "gen/bn_pal_venus_weed.h"
+};
+
+#include "gen/tilemap_0A960.h"
+#include "gen/tilemap_0B160.h"
+#include "gen/tilemap_0B560.h"
+#include "gen/tilemap_0B960.h"
+#include "gen/tilemap_0C160.h"
+#include "gen/tilemap_0C760.h"
+#include "gen/tilemap_0CF60.h"
+#include "gen/tilemap_0D360.h"
+#include "gen/tilemap_0D560.h"
+#include "gen/tilemap_0DB60.h"
+#include "gen/tilemap_0E360.h"
+#include "gen/tilemap_0E960.h"
+#include "gen/tilemap_0EB60.h"
+#include "gen/tilemap_0EF60.h"
+#include "gen/tilemap_0F360.h"
+#include "gen/tilemap_0F560.h"
+#include "gen/tiledef_0FB60.h"
+#include "gen/tiledef_13B70.h"
+#include "gen/tiledef_17B80.h"
