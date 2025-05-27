e3dc18882d00b070ded26ae9da5c46699cf152c0
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-05-27 22:36:25 +0100

    Migrate RWRP assets

--- src/st/rwrp/stage_data.c
+++ src/st/rwrp/stage_data.c
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include <stage.h>
 
+u8 stage_title_jp[] = {
+#include "gen/stage_title_jp.h"
+};
+u8 stage_placeholder[] = {
+#include "gen/stage_placeholder.h"
+};
+u16 D_80181D08[] = {
+#include "gen/D_80181D08.h"
+};
+u16 D_80181F08[] = {
+#include "gen/D_80181F08.h"
+};
+
 #include "gen/tilemap_01FC8.h"
 #include "gen/tilemap_021C8.h"
 #include "gen/tilemap_023C8.h"
