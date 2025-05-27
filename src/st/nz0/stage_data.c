010d00f7ee1906bfc0d91de8ec87951abfb43260
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-05-27 22:29:15 +0100

    Migrate NZ0 data

---
+++ src/st/nz0/stage_data.c
@@ -0,0 +1,183 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+u8 stage_title_nz0_jp[] = {
+#include "gen/stage_title_nz0_jp.h"
+};
+u8 e_blood_skeleton_g[] = {
+#include "gen/e_blood_skeleton_g.h"
+};
+u8 e_slinger_g[] = {
+#include "gen/e_slinger_g.h"
+};
+u8 e_axe_knight_g0[] = {
+#include "gen/e_axe_knight_g0.h"
+};
+u8 e_axe_knight_g1[] = {
+#include "gen/e_axe_knight_g1.h"
+};
+u8 e_bloody_zombie_g[] = {
+#include "gen/e_bloody_zombie_g.h"
+};
+u8 e_skeleton_g[] = {
+#include "gen/e_skeleton_g.h"
+};
+u8 e_skelerang_g0[] = {
+#include "gen/e_skelerang_g0.h"
+};
+u8 e_skelerang_g1[] = {
+#include "gen/e_skelerang_g1.h"
+};
+u8 e_table_with_globe_g[] = {
+#include "gen/e_table_with_globe_g.h"
+};
+u8 e_table_with_alembic_g[] = {
+#include "gen/e_table_with_alembic_g.h"
+};
+u8 e_table_blue_flame_g[] = {
+#include "gen/e_table_blue_flame_g.h"
+};
+u8 e_explosion_g[] = {
+#include "gen/e_explosion_g.h"
+};
+u8 e_cloche_g[] = {
+#include "gen/e_cloche_g.h"
+};
+u8 e_slogra_g0[] = {
+#include "gen/e_slogra_g0.h"
+};
+u8 e_slogra_g1[] = {
+#include "gen/e_slogra_g1.h"
+};
+u8 e_slogra_g2[] = {
+#include "gen/e_slogra_g2.h"
+};
+u8 e_gaibon_g0[] = {
+#include "gen/e_gaibon_g0.h"
+};
+u8 e_gaibon_g1[] = {
+#include "gen/e_gaibon_g1.h"
+};
+u8 e_sealed_door_g[] = {
+#include "gen/e_sealed_door_g.h"
+};
+u8 e_maria_nz0_g0[] = {
+#include "gen/e_maria_nz0_g0.h"
+};
+u8 e_maria_nz0_g1[] = {
+#include "gen/e_maria_nz0_g1.h"
+};
+u8 cutscene_maria[] = {
+#include "gen/cutscene_maria.h"
+};
+u8 cutscene_alucard[] = {
+#include "gen/cutscene_alucard.h"
+};
+u8 stage_title_nz0_us[] = {
+#include "gen/stage_title_nz0_us.h"
+};
+u16 e_blood_skeleton_p[] = {
+#include "gen/e_blood_skeleton_p.h"
+};
+u16 D_80195CBC[] = {
+#include "gen/D_80195CBC.h"
+};
+u16 e_slinger_p[] = {
+#include "gen/e_slinger_p.h"
+};
+u16 D_80195CFC[] = {
+#include "gen/D_80195CFC.h"
+};
+u16 e_axe_knight_p[] = {
+#include "gen/e_axe_knight_p.h"
+};
+u16 e_bloody_zombie_p[] = {
+#include "gen/e_bloody_zombie_p.h"
+};
+u16 e_skeleton_p[] = {
+#include "gen/e_skeleton_p.h"
+};
+u16 e_skelerang_p[] = {
+#include "gen/e_skelerang_p.h"
+};
+u16 e_table_with_globe_p[] = {
+#include "gen/e_table_with_globe_p.h"
+};
+u16 e_table_with_alembic_p[] = {
+#include "gen/e_table_with_alembic_p.h"
+};
+u16 e_table_blue_flame_p[] = {
+#include "gen/e_table_blue_flame_p.h"
+};
+u16 e_explosion_p[] = {
+#include "gen/e_explosion_p.h"
+};
+u16 D_8019621C[] = {
+#include "gen/D_8019621C.h"
+};
+u16 e_cloche_p[] = {
+#include "gen/e_cloche_p.h"
+};
+u16 e_slogra_p[] = {
+#include "gen/e_slogra_p.h"
+};
+u16 e_gaibon_p[] = {
+#include "gen/e_gaibon_p.h"
+};
+u16 e_sealed_door_p[] = {
+#include "gen/e_sealed_door_p.h"
+};
+u16 D_8019661C[] = {
+#include "gen/D_8019661C.h"
+};
+u16 e_maria_nz0_p[] = {
+#include "gen/e_maria_nz0_p.h"
+};
+u16 D_8019665C[] = {
+#include "gen/D_8019665C.h"
+};
+u16 D_8019675C[] = {
+#include "gen/D_8019675C.h"
+};
+u16 D_8019685C[] = {
+#include "gen/D_8019685C.h"
+};
+u16 D_8019695C[] = {
+#include "gen/D_8019695C.h"
+};
+
+#include "gen/tilemap_16A5C.h"
+#include "gen/tilemap_16C5C.h"
+#include "gen/tilemap_16E5C.h"
+#include "gen/tilemap_1725C.h"
+#include "gen/tilemap_17A5C.h"
+#include "gen/tilemap_17C5C.h"
+#include "gen/tilemap_17E5C.h"
+#include "gen/tilemap_1805C.h"
+#include "gen/tilemap_1845C.h"
+#include "gen/tilemap_1865C.h"
+#include "gen/tilemap_1885C.h"
+#include "gen/tilemap_18A5C.h"
+#include "gen/tilemap_1905C.h"
+#include "gen/tilemap_1925C.h"
+#include "gen/tilemap_19C5C.h"
+#include "gen/tilemap_1A05C.h"
+#include "gen/tilemap_1A45C.h"
+#include "gen/tilemap_1AA5C.h"
+#include "gen/tilemap_1AE5C.h"
+#include "gen/tilemap_1BE5C.h"
+#include "gen/tilemap_1C65C.h"
+#include "gen/tilemap_1C85C.h"
+#include "gen/tilemap_1D45C.h"
+#include "gen/tilemap_1D65C.h"
+#include "gen/tilemap_1E25C.h"
+#include "gen/tilemap_1EE5C.h"
+#include "gen/tilemap_1F25C.h"
+#include "gen/tilemap_2005C.h"
+#include "gen/tilemap_2025C.h"
+#include "gen/tilemap_2045C.h"
+#include "gen/tilemap_2065C.h"
+#include "gen/tilemap_2085C.h"
+#include "gen/tiledef_20E5C.h"
+#include "gen/tiledef_24E6C.h"
+#include "gen/tiledef_26E7C.h"
