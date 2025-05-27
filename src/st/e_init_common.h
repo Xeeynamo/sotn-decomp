360b182248869a133817d4c0b4e1761f258dd789
Joey Murphy <tjmurphy@mit.edu>
2025-05-27 19:03:31 -0400

    Pull last member out of e_init_common.h

--- src/st/e_init_common.h
+++ src/st/e_init_common.h
@@ -9,5 +9,4 @@ EInit g_EInitUnkId13 = {ANIMSET_DRA(0), 0, 0x00, 0x000, 2};
 EInit g_EInitLockCamera = {ANIMSET_DRA(0), 0, 0x00, 0x000, 1};
 EInit g_EInitCommon = {ANIMSET_DRA(0), 0, 0x00, 0x000, 3};
 EInit g_EInitDamageNum = {ANIMSET_DRA(0), 0, 0x00, 0x000, 3};
-EInit g_EInitSecret = {ANIMSET_OVL(1), 0, 0x00, 0x000, 3};
 #endif
