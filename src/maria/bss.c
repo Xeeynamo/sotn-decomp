dae1c1409493799a044e0e417909aaf9ee2aed76
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-05-30 23:49:30 +0100

    Fix BSS types

--- src/maria/bss.c
+++ src/maria/bss.c
@@ -9,8 +9,8 @@ u8 D_80174FB8;
 Point16 D_80175000[32];
 s32 D_80154F7C[16];
 AnimationFrame g_MarEmptyAnimGroup[ZERO_LEN];
-s16 g_MarSensorsCeilingCrouch[NUM_HORIZONTAL_SENSORS]; // TODO pl_sensors
-s32 mar_hit_stun_timer;                                // TODO move to pl_steps
+s16 g_MarSensorsCeilingCrouch[NUM_HORIZONTAL_SENSORS];
+s32 mar_hit_stun_timer; // TODO move to pl_steps?
 DebugInfo g_MarDebug;
 u8 dead_dissolve_bmp[0x1400];
 s16 D_80174F68;
@@ -22,10 +22,10 @@ s32 mar_801759D8[32];
 ButtonComboState g_MarComboButtons[2];
 s32 D_pspeu_092E5F28;
 s32 D_pspeu_092E5F20;
-u8 D_pspeu_092E5F18[0x08];  // TODO unknown type
-u8 D_pspeu_092E5D18[0x200]; // TODO unknown type
+s32 D_pspeu_092E5F18;
+s32 D_pspeu_092E5D18[0x80];
 Point16 D_pspeu_092E5B18[0x80];
-u8 D_pspeu_092E5B10[0x08]; // TODO unknown type
+s32 D_pspeu_092E5B10;
 u8 D_pspeu_092E5B08[0x08];
-u8 D_pspeu_092E5908[0x200]; // TODO unknown type
+s32 D_pspeu_092E5908[0x80];
 s32 D_pspeu_092E5900;
