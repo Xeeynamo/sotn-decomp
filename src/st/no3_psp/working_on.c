a21835857dcf6876b115ce665e11e6267ff03bb8
Joey Murphy <tjmurphy@mit.edu>
2025-05-30 19:41:39 -0400

    first helper

--- src/st/no3_psp/working_on.c
+++ src/st/no3_psp/working_on.c
@@ -4,7 +4,38 @@
 
 #include "../no3/no3.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_0924D9C0);
+// Detects if the merman is splashing into water.
+// If so, creates a splash effect, and sets merman underwater to true.
+
+// CheckMermanEnteringWater
+bool func_pspeu_0924D9C0(s16 yOffset) {
+    Collider collider;
+    Entity* newEntity;
+    s32 res = false;
+
+    s16 x = g_CurrentEntity->posX.i.hi;
+    s16 y = g_CurrentEntity->posY.i.hi + yOffset;
+    g_api.CheckCollision(x, y, &collider, 0);
+
+    if (!(collider.effects & EFFECT_SOLID)) {
+        res = true;
+    }
+
+    if (collider.effects & EFFECT_WATER) {
+        if (!g_CurrentEntity->ext.merman.isUnderwater) {
+            newEntity = AllocEntity(&g_Entities[232], &g_Entities[256]);
+            if (newEntity != NULL) {
+                CreateEntityFromEntity(
+                    E_MERMAN_WATER_SPLASH, g_CurrentEntity, newEntity);
+                newEntity->posY.i.hi += yOffset;
+                newEntity->zPriority = g_CurrentEntity->zPriority;
+            }
+            g_api.PlaySfx(NA_SE_EV_WATER_SPLASH);
+            g_CurrentEntity->ext.merman.isUnderwater = true;
+        }
+    }
+    return res;
+}
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityMerman2);
 
