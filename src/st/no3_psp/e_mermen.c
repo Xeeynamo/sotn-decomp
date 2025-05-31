ede3814828d51d02e85eb7df4b45a483f1653f04
Joey Murphy <tjmurphy@mit.edu>
2025-05-31 15:51:15 -0400

    pull enums into psp temporarily

--- src/st/no3_psp/e_mermen.c
+++ src/st/no3_psp/e_mermen.c
@@ -927,7 +927,47 @@ void EntityMermanSpawner(Entity* self) {
     }
 }
 
-#include "../e_merman.h"
+typedef enum {
+    MERMAN_INIT,
+    MERMAN_SWIMMING_UP,
+    MERMAN_SWIMMING = 3,
+    MERMAN_JUMPING,
+    MERMAN_WALKING_TOWARDS_PLAYER,
+    MERMAN_SPIT_FIRE,
+    MERMAN_LUNGE,
+    MERMAN_FALLING,
+    MERMAN_DYING,
+} MermanSteps;
+
+typedef enum {
+    MERMAN_JUMPING_SETUP,
+    MERMAN_JUMPING_UNDERWATER,
+    MERMAN_JUMPING_IN_AIR,
+    MERMAN_JUMPING_LANDING,
+} MermanJumpingSubSteps;
+
+typedef enum {
+    MERMAN_WALKING_TOWARDS_START,
+    MERMAN_WALKING_TOWARDS_PLAYER_WALKING,
+} MermanWalkingTowardsPlayerSubSteps;
+
+typedef enum {
+    MERMAN_SPIT_FIRE_FACE_PLAYER,
+    MERMAN_SPIT_FIRE_ATTACK,
+} MermanSpitFireSubSteps;
+
+typedef enum {
+    MERMAN_LUNGE_START,
+    MERMAN_LUNGE_SETUP,
+    MERMAN_LUNGE_TOWARDS_PLAYER,
+    MERMAN_LUNGE_STANDING,
+} MermanLungeSubSteps;
+
+typedef enum {
+    MERMAN_DYING_SETUP,
+    MERMAN_DYING_KNOCKEDBACK,
+    MERMAN_DYING_END,
+} MermanDyingSubSteps;
 
 static Point32 D_80183A28[] = {XY(FIX(0.5), FIX(-0.5)), XY(FIX(-0.5), FIX(-0.5)), XY(FIX(0.5), FIX(0.5)), XY(FIX(-0.5), FIX(0.5))};
 static u8 D_80183A48[] = {0x40, 0x30, 0x50, 0x40};
