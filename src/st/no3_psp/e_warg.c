5bde0bdb70d1e178a2adeeb7e43e99ae9bd83ab1
Joey Murphy <tjmurphy@mit.edu>
2025-06-01 14:02:58 -0400

    Second helper

--- src/st/no3_psp/e_warg.c
+++ src/st/no3_psp/e_warg.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../no3/no3.h"
 
-//func_801CF58C
+// func_801CF58C
 static void func_pspeu_09252C78(Entity* self) {
     self->velocityX = 0;
     self->ext.warg.unk86 = 0x100;
@@ -10,7 +10,50 @@ static void func_pspeu_09252C78(Entity* self) {
     self->ext.warg.unk80 = 0x20;
 }
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_warg", func_pspeu_09252CD8);
+// func_801CF5E0
+static void func_pspeu_09252CD8(Entity* self) {
+    Tilemap* tilemap = &g_Tilemap;
+    s16 temp_v0;
+
+    // I think this could be a mistake. The == gets
+    // evaluated before the & 1. In other places they use
+    // GetSideToPlayer() & 1 but I think if they wanted to
+    // do that here they should have put it in parentheses.
+    // Makes no difference to ultimate behavior, but does mean
+    // we get some weird bonus insructions (xor, sltiu) on PSP.
+    if (self->facingLeft == GetSideToPlayer() & 1) {
+        SetStep(5);
+        return;
+    }
+
+    if (!self->ext.warg.unk86) {
+        func_pspeu_09252C78(self);
+        return;
+    }
+
+    temp_v0 = self->ext.warg.unk84 - self->posX.i.hi - tilemap->scrollX.i.hi;
+
+    if (temp_v0 > 16) {
+        SetStep(3);
+        if (self->facingLeft) {
+            self->ext.warg.unk7C = 0;
+        } else {
+            self->ext.warg.unk7C = 1;
+        }
+    } else if (temp_v0 < -16) {
+        SetStep(3);
+        if (self->facingLeft) {
+            self->ext.warg.unk7C = 1;
+        } else {
+            self->ext.warg.unk7C = 0;
+        }
+    } else {
+        SetStep(7);
+    }
+
+    self->ext.warg.unk80 = 0;
+    self->ext.warg.unk82 = 32;
+}
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_warg", func_pspeu_09252E60);
 
