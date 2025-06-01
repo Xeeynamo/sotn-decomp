2fc9577054a7d955837b9c31b69f3fdf2ef7da84
Joey Murphy <tjmurphy@mit.edu>
2025-06-01 13:54:22 -0400

    first warg helper

--- src/st/no3_psp/e_warg.c
+++ src/st/no3_psp/e_warg.c
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
+#include "../no3/no3.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_warg", func_pspeu_09252C78);
+//func_801CF58C
+static void func_pspeu_09252C78(Entity* self) {
+    self->velocityX = 0;
+    self->ext.warg.unk86 = 0x100;
+    SetStep(6);
+    g_api.PlaySfx(SFX_WARG_GROWL);
+    self->ext.warg.unk80 = 0x20;
+}
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_warg", func_pspeu_09252CD8);
 
