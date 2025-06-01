fc8817f31457d4741fb6fe92e7d28c5c5569192b
Joey Murphy <tjmurphy@mit.edu>
2025-06-01 10:21:43 -0400

    Set up Warg file splits for next PR

--- src/st/no3_psp/working_on.c
+++ src/st/no3_psp/working_on.c
@@ -1,21 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09252C78);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09252CD8);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09252E60);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityWarg);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityUnkId4B);
-
-// clumsy thing, linter splits the line which breaks INCLUDE_ASM
-// clang-format off
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityWargExplosionPuffTransparent);
-// clang-format on
-
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_092546E0);
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09254778);
