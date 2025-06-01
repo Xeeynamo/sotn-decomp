ee19eaadf875c45bba8a2243086870519c0ea203
Joey Murphy <tjmurphy@mit.edu>
2025-06-01 10:11:28 -0400

    Split data after e_particles

---
+++ src/st/no3_psp/working_on.c
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09252C78);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09252CD8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09252E60);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityWarg);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityUnkId4B);
+
+// clumsy thing, linter splits the line which breaks INCLUDE_ASM
+// clang-format off
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityWargExplosionPuffTransparent);
+// clang-format on
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_092546E0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09254778);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_092547B8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09254D70);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09254E48);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09254FC8);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09255068);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09255168);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09255400);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_092554C0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_09255518);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityPrizeDrop);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityExplosion);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_092564C0);
+
+INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityEquipItemDrop);
