42883e2bbb47fd133e9ad34f51e790fd393b9724
ProjectOblivion <ProjectOblivion>
2025-06-02 17:46:16 +0000

    Added decompiled st_debug and added e_init_common symbols to the symbols file for us

--- src/st/dai/st_debug.c
+++ src/st/dai/st_debug.c
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "dai.h"
 
-INCLUDE_ASM("st/dai/nonmatchings/st_debug", DAI_EntityBackgroundBlock);
+extern ObjInit2 OVL_EXPORT(BackgroundBlockInit)[];
+extern u16 g_EInitCommon[];
 
-INCLUDE_ASM("st/dai/nonmatchings/st_debug", DAI_EntityLockCamera);
+// params: Index of ObjInit to use
+//         (== 1) Use a different hardcoded scaleY and scaleX value
+void OVL_EXPORT(EntityBackgroundBlock)(Entity* self) {
+    ObjInit2* objInit = &OVL_EXPORT(BackgroundBlockInit)[self->params];
+    if (!self->step) {
+        InitializeEntity(g_EInitCommon);
+        self->animSet = objInit->animSet;
+        self->zPriority = objInit->zPriority;
+#ifdef VERSION_PSP
+        self->unk5A = LOHU(objInit->facingLeft);
+#else
+        self->unk5A = LOH(objInit->facingLeft);
+#endif
+        self->palette = objInit->palette;
+        self->drawFlags = objInit->drawFlags;
+        self->drawMode = objInit->drawMode;
+        if (objInit->flags) {
+            self->flags = objInit->flags;
+        }
+    }
+    AnimateEntity(objInit->animFrames, self);
+}
+
+#include "../entity_lock_camera.h"
