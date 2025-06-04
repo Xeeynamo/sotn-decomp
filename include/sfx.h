761b437723eee8170cd26ba2847aa5189e947c35
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-06-04 18:17:46 +0100

    Passively enable all SFXs

--- include/sfx.h
+++ include/sfx.h
@@ -650,7 +650,7 @@ enum Sfx {
     SFX_CLOCK_ROOM_TICK = 1961,
     SFX_UNK_7AF = 0x7Af,
 
-#if defined(VERSION_PSP) || defined(VERSION_PC)
+    // Exclusive to Maria PSP
     /* 0x8E6 */ SFX_VO_MAR_8E6 = 0x8E6,
     /* 0x8E7 */ SFX_VO_MAR_ATTACK_C,
     /* 0x8E8 */ SFX_VO_MAR_8E8,
@@ -667,7 +667,6 @@ enum Sfx {
     /* 0x8F3 */ SFX_VO_MAR_PAIN_C,
     /* 0x8F4 */ SFX_VO_MAR_PAIN_D,
     /* 0x8F5 */ SFX_VO_MAR_DEATH,
-#endif
 };
 
 #endif
