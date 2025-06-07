11bc5269a07736b2d5437aa1f2f90a402a022e47
ProjectOblivion <ProjectOblivion>
2025-06-07 14:36:02 +0000

    Renamed magically sealed door to sealed door

---
+++ src/st/dai_psp/e_sealed_door.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../dai/dai.h"
+// clang-format off
+INCLUDE_ASM("st/dai_psp/nonmatchings/dai_psp/e_sealed_door", GetLang_09245408);
+
+INCLUDE_ASM("st/dai_psp/nonmatchings/dai_psp/e_sealed_door", SealedDoorIsNearPlayer);
+
+INCLUDE_ASM("st/dai_psp/nonmatchings/dai_psp/e_sealed_door", EntitySealedDoor);
