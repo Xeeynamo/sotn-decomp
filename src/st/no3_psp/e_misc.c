ec9ccffd4c17bce474ebf1f8210e2d3461504f1f
Joey Murphy <tjmurphy@mit.edu>
2025-06-07 22:38:46 -0400

    NO3 PSP fixed

--- src/st/no3_psp/e_misc.c
+++ src/st/no3_psp/e_misc.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../no3/no3.h"
 
-#include "../blit_char_psp.h"
-
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_misc", EntityRelicOrb);
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/e_misc", EntityHeartDrop);
