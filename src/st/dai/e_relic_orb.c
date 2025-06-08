77c8a96161dc858e0d9c23d5f8f001b7085c8958
Joey Murphy <tjmurphy@mit.edu>
2025-06-08 15:16:10 -0400

    fix wrp/rwrp mismatch, add include_asm for dai stuff

--- src/st/dai/e_relic_orb.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "dai.h"
-
-INCLUDE_RODATA("st/dai/nonmatchings/e_relic_orb", D_us_801C0080);
-
-INCLUDE_ASM("st/dai/nonmatchings/e_relic_orb", EntityRelicOrb);
