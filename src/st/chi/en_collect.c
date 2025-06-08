2cb40713487d42cea15e65aa2b0448de607ea6a3
Joey Murphy <tjmurphy@mit.edu>
2025-06-08 15:38:39 -0400

    no more dedicated collect psp

--- src/st/chi/en_collect.c
+++ src/st/chi/en_collect.c
@@ -7,8 +7,6 @@
 #define GOLD_COLLECT_TEXT                                                      \
     _S("$1"), _S("$25"), _S("$50"), _S("$100"), _S("$250"), _S("$400"),        \
         _S("$700"), _S("$1000"), _S("$2000"), _S("$5000"),
-#include "../e_collect_psp.h"
-
-#else
-#include "../e_collect.h"
 #endif
+
+#include "../e_collect.h"
