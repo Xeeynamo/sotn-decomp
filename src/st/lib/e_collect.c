0c0a3915a8d081d9b46b669d77e8dfb1ef9ce46f
Joey Murphy <tjmurphy@mit.edu>
2025-06-08 15:23:33 -0400

    US all matching

--- src/st/lib/e_collect.c
+++ src/st/lib/e_collect.c
@@ -1,2 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+#include "lib.h"
+
+#define HEART_DROP_CASTLE_FLAG 0x70
 #include "../e_collect.h"
--- src/st/no4/e_collect.c
+++ src/st/no4/e_collect.c
@@ -1,2 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+#include "no4.h"
+
+#define HEART_DROP_CASTLE_FLAG 0x40
 #include "../e_collect.h"
