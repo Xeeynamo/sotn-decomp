7fcd28fb531e08323f5362d7175422d170378802
ProjectOblivion <ProjectOblivion>
2025-06-09 13:00:37 +0000

    Moved AnimFrames, BackgroundBlockInit, and RedDoorTiles to st_debug

--- src/st/dai/e_init.c
+++ src/st/dai/e_init.c
@@ -179,21 +179,3 @@ EInit D_us_80180A94 = {ANIMSET_OVL(12), 0, 84, PAL_DRA(562), 227};
 EInit D_us_80180AA0 = {ANIMSET_OVL(13), 0, 81, PAL_DRA(564), 136};
 EInit D_us_80180AAC = {ANIMSET_OVL(13), 0, 81, PAL_DRA(564), 2};
 EInit D_us_80180AB8 = {ANIMSET_OVL(13), 0, 81, PAL_DRA(565), 137};
-
-static u8 AnimFrames_80180AC4[] = {0x40, 0x01, 0xFF, 0x00};
-
-ObjInit2 OVL_EXPORT(BackgroundBlockInit)[] = {{
-    .animSet = ANIMSET_DRA(6),
-    .zPriority = 0x1FA,
-    .unk5A = 0,
-    .palette = PAL_DRA(0),
-    .drawFlags = DRAW_DEFAULT,
-    .drawMode = DRAW_TPAGE,
-    .animFrames = &AnimFrames_80180AC4,
-}};
-
-u16 OVL_EXPORT(RedDoorTiles)[][8] = {
-    {0x04B3, 0x04B3, 0x04B3, 0x04B3, 0x0000, 0x0000, 0x0000, 0x0000},
-    {0x000D, 0x000E, 0x000F, 0x000E, 0x01F0, 0x01F6, 0x020E, 0x01F6},
-    {0x000A, 0x000B, 0x000C, 0x000B, 0x0086, 0x0088, 0x00A1, 0x0088},
-};
--- src/st/dai/st_debug.c
+++ src/st/dai/st_debug.c
@@ -1,7 +1,24 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "dai.h"
 
-extern ObjInit2 OVL_EXPORT(BackgroundBlockInit)[];
+static u8 AnimFrames_80180AC4[] = {0x40, 0x01, 0xFF, 0x00};
+
+ObjInit2 OVL_EXPORT(BackgroundBlockInit)[] = {{
+    .animSet = ANIMSET_DRA(6),
+    .zPriority = 0x1FA,
+    .unk5A = 0,
+    .palette = PAL_DRA(0),
+    .drawFlags = DRAW_DEFAULT,
+    .drawMode = DRAW_TPAGE,
+    .animFrames = &AnimFrames_80180AC4,
+}};
+
+u16 OVL_EXPORT(RedDoorTiles)[][8] = {
+    {0x04B3, 0x04B3, 0x04B3, 0x04B3, 0x0000, 0x0000, 0x0000, 0x0000},
+    {0x000D, 0x000E, 0x000F, 0x000E, 0x01F0, 0x01F6, 0x020E, 0x01F6},
+    {0x000A, 0x000B, 0x000C, 0x000B, 0x0086, 0x0088, 0x00A1, 0x0088},
+};
+
 extern u16 g_EInitCommon[];
 
 void OVL_EXPORT(EntityBackgroundBlock)(Entity* self) {
