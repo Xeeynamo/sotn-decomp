c22b7511afae1819ade2c2fa566aeb50a30a089d
Joey Murphy <tjmurphy@mit.edu>
2025-06-10 21:40:15 -0400

    Split NO3 PSP for st_common

--- src/st/no3_psp/working_on.c
+++ src/st/no3_psp/working_on.c
@@ -1,50 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", DestroyEntity);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", PreventEntityFromRespawning);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", AnimateEntity);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", GetDistanceToPlayerX);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", GetDistanceToPlayerY);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", GetSideToPlayer);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", MoveEntity);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", FallEntity);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", UnkCollisionFunc3);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", UnkCollisionFunc2);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", AllocEntity);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", UnkEntityFunc0);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", GetAngleBetweenEntities);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", GetNormalizedAngle);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", SetStep);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", SetSubStep);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", InitializeEntity);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityDummy);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", UnkCollisionFunc);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", CheckFieldCollision);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", GetPlayerCollisionWith);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", ReplaceBreakableWithItemDrop);
-
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", UnkPrimHelper);
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", FindFirstUnkPrim);
