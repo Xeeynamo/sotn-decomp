df8a75fdf77a1744dd12b836d7d153885bca2f54
Joey Murphy <tjmurphy@mit.edu>
2025-06-10 21:25:40 -0400

    Water effects all good

--- src/st/no3_psp/working_on.c
+++ src/st/no3_psp/working_on.c
@@ -1,18 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../no3/no3.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_0925C158);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityAlucardWaterEffect);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntitySplashWater);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntitySurfacingWater);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntitySideWaterSplash);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntitySmallWaterDrop);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityWaterDrop);
-
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", BottomCornerText);
