196d743094f8a9250856790290057e1fac0b6c83
Joey Murphy <tjmurphy@mit.edu>
2025-06-10 21:54:59 -0400

    PSP NO3 prim helpers

--- src/st/no3_psp/working_on.c
+++ src/st/no3_psp/working_on.c
@@ -1,16 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", UnkPrimHelper);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", FindFirstUnkPrim);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", func_pspeu_092615D8);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", UnkPolyFunc2);
-
-INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", PrimDecreaseBrightness);
-
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityZombie);
 
 INCLUDE_ASM("st/no3_psp/psp/no3_psp/working_on", EntityZombieSpawner);
