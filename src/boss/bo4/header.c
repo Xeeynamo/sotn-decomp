c8ed0e5ef0f252c606153bd836c9c544a6fa439a
Jonathan Hohle <jonhohle@gmail.com>
2025-06-15 14:12:20 -0700

    Partial configuration for BO4 assets (#2666)
    BO4 is the first of several overlays which use yet another `Overlay`
    struct. This follows the same layout as `Overlay` and
    `AbbreviatedOverlay`, but contains only two sprite banks and no cutscene
    handler. There are a few other overlays that use this structure that
    haven't been started yet.
    
    `sotn-assets` is having trouble with the sprite banks (possibly because
    of this new overlay struct), as well as the palette definitions, so
    those are imported in code. Everything else is extracted as assets.

---
+++ src/boss/bo4/header.c
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "bo4.h"
+
+extern RoomHeader OVL_EXPORT(rooms)[];
+extern s16** OVL_EXPORT(spriteBanks)[];
+extern u_long* OVL_EXPORT(cluts)[];
+extern LayoutEntity* OVL_EXPORT(pStObjLayoutHorizontal)[];
+extern RoomDef OVL_EXPORT(rooms_layers)[];
+extern u_long* OVL_EXPORT(gfxBanks)[];
+void UpdateStageEntities();
+
+extern u8* g_SpriteSheet[0x34C];
+extern u8* g_PlOvlDopBatSpritesheet[6];
+
+AbbreviatedOverlay2 OVL_EXPORT(Overlay) = {
+    .Update = Update,
+    .HitDetection = HitDetection,
+    .UpdateRoomPosition = UpdateRoomPosition,
+    .InitRoomEntities = InitRoomEntities,
+    .rooms = OVL_EXPORT(rooms),
+    .spriteBanks = OVL_EXPORT(spriteBanks),
+    .cluts = OVL_EXPORT(cluts),
+    .objLayoutHorizontal = BO4_pStObjLayoutHorizontal,
+    .tileLayers = OVL_EXPORT(rooms_layers),
+    .gfxBanks = OVL_EXPORT(gfxBanks),
+    .UpdateStageEntities = UpdateStageEntities,
+    .unk2C = g_SpriteSheet,
+    .unk30 = g_PlOvlDopBatSpritesheet,
+};
+
+extern s16* D_us_801B0A78[];
+extern s16* D_us_801B159C[];
+extern s16* D_us_801B1664[];
+extern s16* D_us_801B2068[];
+extern s16* D_us_801B252C[];
+
+s16** OVL_EXPORT(spriteBanks)[] = {
+    NULL, D_us_801B0A78, D_us_801B159C, D_us_801B1664, D_us_801B2068, NULL,
+    NULL, NULL,          NULL,          NULL,          NULL,          NULL,
+    NULL, NULL,          NULL,          D_us_801B252C, NULL,          NULL,
+    NULL, NULL,          NULL,          NULL,          NULL,          NULL,
+};
+
+extern s16* D_us_801AC288[0x180];
+
+static u_long* D_us_80180094[] = {
+    MAKE_PAL_OP(PAL_BULK_COPY, 0),
+    PAL_BULK(0x2000, D_us_801AC288),
+    PAL_TERMINATE(),
+};
+
+static u_long* D_us_801800A8[] = {
+    PAL_COPY_INFO(),
+    PAL_COPY_DATA_(0x20F0, g_Clut + 0x20A0, 16),
+};
+
+static u_long* D_us_801800B8[] = {
+    MAKE_PAL_OP(PAL_UNK_OP2, 2),
+    PAL_COPY_DATA_(0x20F0, g_Clut + 0x20C0, 16),
+    PAL_TERMINATE(),
+};
+
+static u_long* D_us_801800CC[] = {
+    MAKE_PAL_OP(PAL_UNK_OP2, 2),
+    PAL_COPY_DATA_(0x20F0, g_Clut + 0x2000, 16),
+    PAL_TERMINATE(),
+};
+
+u_long* OVL_EXPORT(cluts)[] = {
+    D_us_80180094,
+    D_us_801800B8,
+    D_us_801800CC,
+    D_us_801800A8,
+};
+
+#include "gen/layers.h"
+#include "gen/graphics_banks.h"
