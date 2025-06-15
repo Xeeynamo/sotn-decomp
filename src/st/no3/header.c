995b181b2c51eb32d605025e72fbd072eede1aab
Joey Murphy <tjmurphy@mit.edu>
2025-06-15 19:50:51 -0400

    PSP use header.c

--- src/st/no3/header.c
+++ src/st/no3/header.c
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "no3.h"
+#include "../pfn_entity_update.h"
 
 extern RoomHeader OVL_EXPORT(rooms)[];
 extern SpriteParts* OVL_EXPORT(spriteBanks)[];
 extern u_long* OVL_EXPORT(cluts)[];
-extern LayoutEntity* OVL_EXPORT(pStObjLayoutHorizontal)[];
 extern RoomDef OVL_EXPORT(rooms_layers)[];
 extern GfxBank* OVL_EXPORT(gfxBanks)[];
 void UpdateStageEntities();
@@ -17,7 +17,7 @@ AbbreviatedOverlay OVL_EXPORT(Overlay) = {
     .rooms = OVL_EXPORT(rooms),
     .spriteBanks = OVL_EXPORT(spriteBanks),
     .cluts = OVL_EXPORT(cluts),
-    .objLayoutHorizontal = OVL_EXPORT(pStObjLayoutHorizontal),
+    .objLayoutHorizontal = &OBJ_LAYOUT_HORIZONTAL,
     .tileLayers = OVL_EXPORT(rooms_layers),
     .gfxBanks = OVL_EXPORT(gfxBanks),
     .UpdateStageEntities = UpdateStageEntities,
