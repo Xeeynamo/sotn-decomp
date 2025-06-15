c2cad8be1716c64a588005f87f61755f5e899e8a
Joey Murphy <tjmurphy@mit.edu>
2025-06-15 19:15:25 -0400

    Move water data to main NO3 directory

--- src/st/no3/d_water_data.c
+++ src/st/no3/d_water_data.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-#include "../no3/no3.h"
+#include "no3.h"
 
 // g_WaterXTbl
 s16 g_WaterXTbl[] = {0,   768, 0, 440, 512, 7,  7, 0,   0,   150, 0, 184,
