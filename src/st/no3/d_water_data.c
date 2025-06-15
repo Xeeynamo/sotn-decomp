f2d4b6fc88b3ec4f15fdfc0c08dd5f4282f89e90
Joey Murphy <tjmurphy@mit.edu>
2025-06-15 19:20:48 -0400

    Fix water sounds to be array of 2 instead of individual vars

--- src/st/no3/d_water_data.c
+++ src/st/no3/d_water_data.c
@@ -2,6 +2,8 @@
 
 #include "no3.h"
 
-// g_WaterXTbl
+// 7C3 needs a name!
+u16 g_WaterSounds[] = {NA_SE_EV_WATER_SPLASH, 0x7C3};
+
 s16 g_WaterXTbl[] = {0,   768, 0, 440, 512, 7,  7, 0,   0,   150, 0, 184,
                      256, 7,   7, 0,   48,  68, 0, 468, 512, 7,   7, 0};
