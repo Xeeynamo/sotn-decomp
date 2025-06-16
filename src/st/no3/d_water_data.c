11fbbb90af81ccf7c294e39c4de628ff5c0a37f3
Joey Murphy <tjmurphy@mit.edu>
2025-06-15 23:42:58 -0400

    name for 7c3

--- src/st/no3/d_water_data.c
+++ src/st/no3/d_water_data.c
@@ -2,8 +2,7 @@
 
 #include "no3.h"
 
-// 7C3 needs a name!
-u16 g_WaterSounds[] = {NA_SE_EV_WATER_SPLASH, 0x7C3};
+u16 g_WaterSounds[] = {NA_SE_EV_WATER_SPLASH, SFX_WATER_SURFACING};
 
 s16 g_WaterXTbl[] = {0,   768, 0, 440, 512, 7,  7, 0,   0,   150, 0, 184,
                      256, 7,   7, 0,   48,  68, 0, 468, 512, 7,   7, 0};
