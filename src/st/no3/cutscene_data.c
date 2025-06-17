d873b77edfcc2436b65380fe02a7f3f9587c33d0
Joey Murphy <tjmurphy@mit.edu>
2025-06-17 17:13:49 -0400

    Format

--- src/st/no3/cutscene_data.c
+++ src/st/no3/cutscene_data.c
@@ -6,4 +6,5 @@ u8 OVL_EXPORT(cutscene_data)[] = {
 #include "gen/cutscene_data.h"
 };
 
-static u8 unused[] = {32, 88, 8, 128, 8, 128, 0, 0, 0, 33, 90, 10, 160, 7, 120, 255, 255, 0};
+static u8 unused[] = {32, 88, 8,  128, 8, 128, 0,   0,   0,
+                      33, 90, 10, 160, 7, 120, 255, 255, 0};
