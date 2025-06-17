f6bf17e11df2d2ae86607f719823d948bb2258a4
Joey Murphy <tjmurphy@mit.edu>
2025-06-16 20:55:59 -0400

    PSP NO3 sprites/spritebanks

--- src/st/no3/header2.c
+++ src/st/no3/header2.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "no3.h"
 
-#include "gen/sprite_banks.h"
 #include "gen/palette_def.h"
 #include "gen/layers.h"
 #include "gen/graphics_banks.h"
