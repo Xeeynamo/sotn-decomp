b8fb8842f8e608273ef9141f06b0feab2516b22e
ProjectOblivion <ProjectOblivion>
2025-06-18 21:57:02 +0000

    Merged st_collision

--- src/st/dai/st_collision.c
+++ src/st/dai/st_collision.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "dai.h"
 
-INCLUDE_ASM("st/dai/nonmatchings/st_collision", HitDetection);
-
-INCLUDE_ASM("st/dai/nonmatchings/st_collision", EntityDamageDisplay);
+#include "../collision.h"
