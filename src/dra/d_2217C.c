cc5b6c88182617c19c26b1d1b4b11b33b9f91c06
gamezter <charronalexandre@hotmail.com>
2025-06-19 11:50:48 -0400

    Share `dra_psp` data (#2683)

---
+++ src/dra/d_2217C.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <game.h>
+
+u8 text_item_up[] = {
+#include "gen/text_item_up.h"
+};
+u8 load_room_obj[] = {
+#include "gen/load_room_obj.h"
+};
+u8 save_room_obj[] = {
+#include "gen/save_room_obj.h"
+};
+u8 load_room_tile[] = {
+#include "gen/load_room_tile.h"
+};
+u8 save_room_tile[] = {
+#include "gen/save_room_tile.h"
+};
--- src/dra/d_24CEC.c
+++ src/dra/d_24CEC.c
@@ -1,68 +1,56 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include <game.h>
 
-u8 text_item_up[] = {
-#include "gen/text_item_up.h"
-};
-u8 load_room_obj[] = {
-#include "gen/load_room_obj.h"
-};
-u8 save_room_obj[] = {
-#include "gen/save_room_obj.h"
-};
-u8 load_room_tile[] = {
-#include "gen/load_room_tile.h"
-};
-u8 save_room_tile[] = {
-#include "gen/save_room_tile.h"
-};
-
 u8 D_800C4CEC[] = {
-#include "../dra/gen/D_800C4CEC.h"
+#include "gen/D_800C4CEC.h"
 };
 
 u8 D_800C4D70[] = {
-#include "../dra/gen/D_800C4D70.h"
+#include "gen/D_800C4D70.h"
 };
 
 u8 D_800C4DF4[] = {
-#include "../dra/gen/D_800C4DF4.h"
+#include "gen/D_800C4DF4.h"
 };
 
 u8 D_800C4E78[] = {
-#include "../dra/gen/D_800C4E78.h"
+#include "gen/D_800C4E78.h"
 };
 
 u8 D_800C4EFC[] = {
-#include "../dra/gen/D_800C4EFC.h"
+#include "gen/D_800C4EFC.h"
 };
 
 u8 D_800C4F80[] = {
-#include "../dra/gen/D_800C4F80.h"
+#include "gen/D_800C4F80.h"
 };
 
 u8 D_800C5004[] = {
-#include "../dra/gen/D_800C5004.h"
+#include "gen/D_800C5004.h"
 };
 
 u8 D_800C5088[] = {
-#include "../dra/gen/D_800C5088.h"
+#include "gen/D_800C5088.h"
 };
 
 u8 D_800C510C[] = {
-#include "../dra/gen/D_800C510C.h"
+#include "gen/D_800C510C.h"
 };
 
 u8 D_800C5190[] = {
-#include "../dra/gen/D_800C5190.h"
+#include "gen/D_800C5190.h"
 };
 
 u8 D_800C5214[] = {
-#include "../dra/gen/D_800C5214.h"
+#include "gen/D_800C5214.h"
 };
 
 u8* D_800C52F8[] = {
     // subweapon graphics?
     D_800C4CEC, D_800C4D70, D_800C4DF4, D_800C4E78, D_800C4EFC, D_800C4F80,
     D_800C5004, D_800C5088, D_800C510C, D_800C5190, D_800C5214,
+};
+
+u8 g_GfxEquipIcon[] = {
+#include "gen/g_GfxEquipIcon.h"
 };
