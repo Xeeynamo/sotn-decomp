1f4abd1f2f6bf1ea0ed9432d8e7b95881700878c
Jonathan Hohle <jonhohle@gmail.com>
2025-06-20 08:21:41 -0700

    Match Remaining PSP Bat Familiar Functions
    Getting `UpdateBatAttackMode` to work required changing the logic
    slightly. The GCC optimized version is interesting in that it will
    ignore comparisons that would not produce any code execution. It seems
    like it wouldn't be logically equivalent (and if you were relying on
    side effects from reads, it wouldn't be), but ends up working out to the
    same thing.
    
    `UpdateServantDefault` needed `SetEntityAnimation` to be `static`.

--- src/servant/set_entity_animation.h
+++ src/servant/set_entity_animation.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-void SetEntityAnimation(Entity* entity, AnimationFrame* anim) {
+static void SetEntityAnimation(Entity* entity, AnimationFrame* anim) {
     if (entity->anim != anim) {
         entity->anim = anim;
         entity->pose = 0;
