9dd1e77fe72c74c064989c9d6875a37bef11c309
Joey Murphy <tjmurphy@mit.edu>
2025-06-25 17:21:12 -0400

    Split first file

---
+++ src/st/np3_psp/st_init_np3.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/st_init_np3", InitEntityIds1);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/st_init_np3", InitEntityIds2);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/st_init_np3", NP3_Load);
