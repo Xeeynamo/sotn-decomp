53e7649443d9d6dd63af81a30d252d8069876d64
ProjectOblivion <31639802+ProjectOblivion@users.noreply.github.com>
2025-06-29 09:48:30 -0400

    Unified `pspeu` and `us` `dai` segments and renamed some segments (#2715)

---
+++ src/st/dai/e_priest.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "dai.h"
+
+// Likely the priest
+INCLUDE_ASM("st/dai/nonmatchings/e_priest", func_us_801C2534);
+
+// The blades from the bad priest
+INCLUDE_ASM("st/dai/nonmatchings/e_priest", func_us_801C2CD4);
+
+// The debris from the blades
+INCLUDE_ASM("st/dai/nonmatchings/e_priest", func_us_801C2FF0);
---
+++ src/st/dai/e_stained_glass.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "dai.h"
+
+// This is fairly similar to the same name from ST0 file 2DAC8.c
+INCLUDE_ASM("st/dai/nonmatchings/e_stained_glass", func_801ADB10);
+
+INCLUDE_ASM("st/dai/nonmatchings/e_stained_glass", func_us_801D9394);
+
+INCLUDE_ASM("st/dai/nonmatchings/e_stained_glass", func_us_801D97D0);
+
+INCLUDE_ASM("st/dai/nonmatchings/e_stained_glass", func_us_801D9F5C);
