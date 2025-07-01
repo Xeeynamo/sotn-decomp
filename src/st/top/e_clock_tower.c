5d030555633f2684fea221f7b04aeef081c85f7a
Jonathan Hohle <jonhohle@gmail.com>
2025-06-30 21:00:51 -0700

    Initial Import of TOP

---
+++ src/st/top/e_clock_tower.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+// entity clouds?
+
+INCLUDE_ASM("st/top/nonmatchings/e_clock_tower", func_us_801BABF8);
+
+INCLUDE_ASM("st/top/nonmatchings/e_clock_tower", EntityClockTower3D);
+
+INCLUDE_RODATA("st/top/nonmatchings/e_clock_tower", D_us_801A8C30);
---
+++ src/st/top/prim_helpers.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", UnkPrimHelper);
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", UpdateAnimation);
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", FindFirstUnkPrim);
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", FindFirstUnkPrim2);
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", PrimToggleVisibility);
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", PrimResetNext);
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", UnkPolyFunc2);
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", UnkPolyFunc0);
+
+INCLUDE_ASM("st/top/nonmatchings/prim_helpers", PrimDecreaseBrightness);
