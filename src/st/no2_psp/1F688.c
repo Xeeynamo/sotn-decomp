e9ba48c36e4f3e62803ba9ccb6a62121afeeb0f0
gamezter <gamezter93@gmail.com>
2025-07-03 09:34:22 -0400

    Import `no2_psp` st_init_no2

---
+++ src/st/no2_psp/1F688.c
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09256D08);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_092571F8);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_092575D8);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09257810);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09257B10);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09257B78);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09257C00);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09257DF0);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09257ED8);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_092592A0);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09259668);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09259AA8);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09259D40);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_09259E40);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925A248);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925A8A8);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925A988);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925B018);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925B278);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925B730);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925BAD0);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925BBA8);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925BE88);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925C018);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925C0F8);
+
+INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/1F688", func_pspeu_0925C380);
