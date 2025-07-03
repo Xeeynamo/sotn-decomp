08c721787049a2c596b12151b550f68516a8cdd6
gamezter <gamezter93@gmail.com>
2025-07-03 09:58:18 -0400

    Import `no2_psp` e_init.c

---
+++ src/st/no2_psp/e_init.c
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../no2/no2.h"
+
+void func_pspeu_09256D08(Entity* self);
+void func_pspeu_092412F0(Entity* self);
+void func_pspeu_092404D8(Entity* self);
+void func_pspeu_09245DB8(Entity* self);
+void func_pspeu_0923BA30(Entity* self);
+void func_pspeu_09243F80(Entity* self);
+void func_pspeu_0923EB00(Entity* self);
+void func_pspeu_09246DB8(Entity* self);
+void func_pspeu_0923AA98(Entity* self);
+void func_pspeu_092415F0(Entity* self);
+void func_pspeu_09241E48(Entity* self);
+void func_pspeu_09242F80(Entity* self);
+void func_pspeu_0923F060(Entity* self);
+void func_pspeu_09243150(Entity* self);
+void func_pspeu_0924D810(Entity* self);
+void func_pspeu_0924D810(Entity* self);
+void func_pspeu_0925A8A8(Entity* self);
+void func_pspeu_0925B018(Entity* self);
+void func_pspeu_09243A10(Entity* self);
+void func_pspeu_09243C88(Entity* self);
+void func_pspeu_09243E00(Entity* self);
+void func_pspeu_09239B28(Entity* self);
+void func_pspeu_0923A620(Entity* self);
+void func_pspeu_0923A9D8(Entity* self);
+void func_pspeu_092575D8(Entity* self);
+void func_pspeu_09257810(Entity* self);
+void func_pspeu_09257B10(Entity* self);
+void func_pspeu_09257B78(Entity* self);
+void func_pspeu_09257C00(Entity* self);
+void func_pspeu_09259E40(Entity* self);
+void func_pspeu_09259D40(Entity* self);
+void func_pspeu_0925A248(Entity* self);
+void func_pspeu_0925C0F8(Entity* self);
+void func_pspeu_0925BBA8(Entity* self);
+void func_pspeu_0925BAD0(Entity* self);
+void func_pspeu_092556A0(Entity* self);
+void func_pspeu_09256348(Entity* self);
+void func_pspeu_09255EF0(Entity* self);
+void func_pspeu_092441F8(Entity* self);
+void func_pspeu_0925A988(Entity* self);
+void func_pspeu_09248B38(Entity* self);
+void func_pspeu_09246ED0(Entity* self);
+void func_pspeu_0924A940(Entity* self);
+void func_pspeu_0924AA78(Entity* self);
+void func_pspeu_09238E80(Entity* self);
+void func_pspeu_09239898(Entity* self);
+void func_pspeu_092386A8(Entity* self);
+void func_pspeu_0925B730(Entity* self);
+void func_pspeu_0925C380(Entity* self);
+void func_pspeu_0925B278(Entity* self);
+void func_pspeu_09257ED8(Entity* self);
+void func_pspeu_0923D188(Entity* self);
+void func_pspeu_0923DCC0(Entity* self);
+void func_pspeu_0923DE98(Entity* self);
+void func_pspeu_09253010(Entity* self);
+void func_pspeu_09254098(Entity* self);
+void func_pspeu_09254520(Entity* self);
+void func_pspeu_092515D0(Entity* self);
+void func_pspeu_092529B0(Entity* self);
+void func_pspeu_0924F990(Entity* self);
+void func_pspeu_09250ED0(Entity* self);
+void func_pspeu_0924AB98(Entity* self);
+void func_pspeu_0924BE50(Entity* self);
+void func_pspeu_0924BF68(Entity* self);
+void func_pspeu_092571F8(Entity* self);
+
+PfnEntityUpdate OVL_EXPORT(EntityUpdates)[] = {
+    /* 0x00 */ func_pspeu_09256D08,
+    /* 0x01 */ func_pspeu_092412F0,
+    /* 0x02 */ func_pspeu_092404D8,
+    /* 0x03 */ func_pspeu_09245DB8,
+    /* 0x04 */ func_pspeu_0923BA30,
+    /* 0x05 */ func_pspeu_09243F80,
+    /* 0x06 */ func_pspeu_0923EB00,
+    /* 0x07 */ func_pspeu_09246DB8,
+    /* 0x08 */ func_pspeu_0923AA98,
+    /* 0x09 */ func_pspeu_092415F0,
+    /* 0x0A */ func_pspeu_09241E48,
+    /* 0x0B */ func_pspeu_09242F80,
+    /* 0x0C */ func_pspeu_0923F060,
+    /* 0x0D */ func_pspeu_09243150,
+    /* 0x0E */ func_pspeu_0924D810,
+    /* 0x0F */ func_pspeu_0924D810,
+    /* 0x10 */ func_pspeu_0925A8A8,
+    /* 0x11 */ func_pspeu_0925B018,
+    /* 0x12 */ func_pspeu_09243A10,
+    /* 0x13 */ func_pspeu_09243C88,
+    /* 0x14 */ func_pspeu_09243E00,
+    /* 0x15 */ func_pspeu_09239B28,
+    /* 0x16 */ func_pspeu_0923A620,
+    /* 0x17 */ func_pspeu_0923A9D8,
+    /* 0x18 */ func_pspeu_092575D8,
+    /* 0x19 */ func_pspeu_09257810,
+    /* 0x1A */ func_pspeu_09257B10,
+    /* 0x1B */ func_pspeu_09257B78,
+    /* 0x1C */ func_pspeu_09257C00,
+    /* 0x1D */ func_pspeu_09259E40,
+    /* 0x1E */ func_pspeu_09259D40,
+    /* 0x1F */ func_pspeu_0925A248,
+    /* 0x20 */ func_pspeu_0925C0F8,
+    /* 0x21 */ func_pspeu_0925BBA8,
+    /* 0x22 */ func_pspeu_0925BAD0,
+    /* 0x23 */ func_pspeu_092556A0,
+    /* 0x24 */ func_pspeu_09256348,
+    /* 0x25 */ func_pspeu_09255EF0,
+    /* 0x26 */ func_pspeu_092441F8,
+    /* 0x27 */ func_pspeu_0925A988,
+    /* 0x28 */ func_pspeu_09248B38,
+    /* 0x29 */ func_pspeu_09246ED0,
+    /* 0x2A */ func_pspeu_0924A940,
+    /* 0x2B */ func_pspeu_0924AA78,
+    /* 0x2C */ func_pspeu_09238E80,
+    /* 0x2D */ func_pspeu_09239898,
+    /* 0x2E */ func_pspeu_092386A8,
+    /* 0x2F */ func_pspeu_0925B730,
+    /* 0x30 */ func_pspeu_0925C380,
+    /* 0x31 */ func_pspeu_0925B278,
+    /* 0x32 */ func_pspeu_09257ED8,
+    /* 0x33 */ func_pspeu_0923D188,
+    /* 0x34 */ func_pspeu_0923DCC0,
+    /* 0x35 */ func_pspeu_0923DE98,
+    /* 0x36 */ func_pspeu_09253010,
+    /* 0x37 */ func_pspeu_09254098,
+    /* 0x38 */ func_pspeu_09254520,
+    /* 0x39 */ func_pspeu_092515D0,
+    /* 0x3A */ func_pspeu_092529B0,
+    /* 0x3B */ func_pspeu_0924F990,
+    /* 0x3C */ func_pspeu_09250ED0,
+    /* 0x3D */ func_pspeu_0924AB98,
+    /* 0x3E */ func_pspeu_0924BE50,
+    /* 0x3F */ func_pspeu_0924BF68,
+    /* 0x40 */ func_pspeu_092571F8,
+};
+
+EInit D_pspeu_09266618 = {ANIMSET_DRA(0x03), 0x00, 0x00, 0x000, 0x000};
+EInit g_EInitObtainable = {ANIMSET_DRA(0x03), 0x00, 0x00, 0x000, 0x001};
+EInit g_EInitParticle = {ANIMSET_DRA(0x03), 0x00, 0x00, 0x000, 0x002};
+EInit D_pspeu_09266648 = {ANIMSET_DRA(0x00), 0x00, 0x00, 0x000, 0x004};
+EInit g_EInitInteractable = {ANIMSET_DRA(0x00), 0x00, 0x00, 0x000, 0x005};
+EInit g_EInitUnkId13 = {ANIMSET_DRA(0x00), 0x00, 0x00, 0x000, 0x002};
+EInit g_EInitLockCamera = {ANIMSET_DRA(0x00), 0x00, 0x00, 0x000, 0x001};
+EInit g_EInitCommon = {ANIMSET_DRA(0x00), 0x00, 0x00, 0x000, 0x003};
+EInit g_EInitDamageNum = {ANIMSET_DRA(0x00), 0x00, 0x00, 0x000, 0x003};
+EInit g_EInitSkelerang = {ANIMSET_OVL(0x01), 0x01, 0x48, 0x200, 0x00B};
+EInit g_EInitSkelerangBoomerang = {ANIMSET_OVL(0x01), 0x2B, 0x48, 0x200, 0x00C};
+EInit D_pspeu_092666C8 = {ANIMSET_OVL(0x02), 0x0B, 0x00, 0x000, 0x003};
+EInit D_pspeu_092666D8 = {ANIMSET_OVL(0x03), 0x01, 0x49, 0x203, 0x003};
+EInit g_EInitOwlKnight = {ANIMSET_OVL(0x08), 0x10, 0x49, 0x21C, 0x014};
+EInit g_EInitOwlKnightSword = {ANIMSET_OVL(0x08), 0x00, 0x49, 0x2CB, 0x015};
+EInit g_EInitOwl = {ANIMSET_OVL(0x08), 0x01, 0x49, 0x21C, 0x016};
+EInit g_EInitBloodyZombie = {ANIMSET_OVL(0x09), 0x01, 0x4A, 0x223, 0x00D};
+EInit D_pspeu_09266728 = {ANIMSET_OVL(0x02), 0x0E, 0x00, 0x000, 0x005};
+EInit D_pspeu_09266738 = {ANIMSET_OVL(0x05), 0x01, 0x4C, 0x207, 0x085};
+EInit D_pspeu_09266748 = {ANIMSET_OVL(0x05), 0x01, 0x4C, 0x207, 0x003};
+EInit D_pspeu_09266758 = {ANIMSET_OVL(0x00), 0x00, 0x00, 0x000, 0x086};
+EInit D_pspeu_09266768 = {ANIMSET_OVL(0x00), 0x00, 0x00, 0x000, 0x087};
+EInit D_pspeu_09266778 = {ANIMSET_OVL(0x06), 0x02, 0x4C, 0x23E, 0x0BA};
+EInit D_pspeu_09266788 = {ANIMSET_OVL(0x06), 0x12, 0x4C, 0x23E, 0x0BB};
+EInit D_pspeu_09266798 = {ANIMSET_OVL(0x06), 0x02, 0x4C, 0x23B, 0x0BC};
+EInit D_pspeu_092667A8 = {ANIMSET_OVL(0x06), 0x14, 0x4C, 0x23B, 0x0BD};
+EInit D_pspeu_092667B8 = {ANIMSET_OVL(0x06), 0x02, 0x4C, 0x238, 0x0BE};
+EInit D_pspeu_092667C8 = {ANIMSET_OVL(0x06), 0x13, 0x4C, 0x238, 0x0BF};
+EInit D_pspeu_092667D8 = {ANIMSET_OVL(0x04), 0x00, 0x50, 0x241, 0x081};
+EInit D_pspeu_092667E8 = {ANIMSET_OVL(0x04), 0x00, 0x50, 0x241, 0x002};
+EInit D_pspeu_092667F8 = {ANIMSET_OVL(0x04), 0x00, 0x50, 0x241, 0x082};
