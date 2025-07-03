40385ca1c9bb93899a4b4b60b2b0d78e9d410296
bismurphy <tjmurphy@mit.edu>
2025-07-03 09:43:33 -0400

    PSP NP3 Gaibon + codeshare files  (#2728)
    Mostly just cleanup to make PSP match, but also some general
    improvements like making the data types correct, and documenting the
    weird Gaibon Leg entity.

--- src/st/np3/np3.h
+++ src/st/np3/np3.h
@@ -89,7 +89,7 @@ typedef enum EntityIDs {
     /* 0x4F */ E_SLOGRA_SPEAR,
     /* 0x50 */ E_SLOGRA_SPEAR_PROJECTILE,
     /* 0x51 */ E_GAIBON,
-    /* 0x52 */ E_GAIBON_IDLE,
+    /* 0x52 */ E_GAIBON_LEG,
     /* 0x53 */ E_GAIBON_SMALL_FIREBALL,
     /* 0x54 */ E_GAIBON_BIG_FIREBALL,
     /* 0x55 */ E_HAMMER, // the enemy named "Hammer", Gurkha's friend
@@ -137,6 +137,8 @@ extern EInit g_EInitGurkhaType2;
 extern EInit g_EInitGurkhaBlade;
 
 extern void ReplaceBreakableWithItemDrop(Entity*);
+extern Primitive* FindFirstUnkPrim(Primitive* poly);
+
 extern void func_801BAFA0(s16);
 extern void func_801BB044(s16);
 extern void func_801BB140(s16);
