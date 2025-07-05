e8b6424b7d56bd18c20e892ab201057d665846de
Joey Murphy <tjmurphy@mit.edu>
2025-07-05 13:23:23 -0400

    bat done

--- src/st/np3_psp/working_on.c
+++ src/st/np3_psp/working_on.c
@@ -3,8 +3,6 @@
 
 // clang-format off
 
-INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_09242ED0);
-
 INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", EntityStageNamePopup);
 
 // clang-format on
