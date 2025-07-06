6c2ece7b5162ca4096910ed395a1f504d1ed79b8
ProjectOblivion <31639802+ProjectOblivion@users.noreply.github.com>
2025-07-06 11:08:05 -0400

    Did some `dai` cleanup and fixed some oversights (#2732)

--- src/st/dai/e_collect.c
+++ src/st/dai/e_collect.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "dai.h"
 
-#define HEART_DROP_CASTLE_FLAG 0x98
-
 #ifdef VERSION_PSP
 #define GOLD_COLLECT_TEXT                                                      \
     _S("$1"), _S("$25"), _S("$50"), _S("$100"), _S("$250"), _S("$400"),        \
