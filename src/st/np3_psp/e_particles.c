e0840ea5a0103b8bab1ec79685ce0c03fe11acb1
Joey Murphy <tjmurphy@mit.edu>
2025-07-05 21:35:46 -0400

    resplit e_particles

--- src/st/np3_psp/e_particles.c
+++ src/st/np3_psp/e_particles.c
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/e_particles", func_psp_0923AD68);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/e_particles", func_psp_0923B2F0);
+
 INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/e_particles", EntitySoulStealOrb);
 
 INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/e_particles", EntityEnemyBlood);
