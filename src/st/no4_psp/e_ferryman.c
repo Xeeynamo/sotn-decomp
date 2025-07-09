ce70b6fe1b39dcb2745559d69ce17c12c7687f4a
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-07-09 18:19:10 +1000

    Rename PSP split and name data sections

---
+++ src/st/no4_psp/e_ferryman.c
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../no4/no4.h"
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_09239788);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_092398B8);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_09239B00);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923B288);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923B378);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923BAB0);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923BB30);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923C460);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923D0B8);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923D0C8);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923D180);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923D428);
+
+INCLUDE_ASM("st/no4_psp/nonmatchings/no4_psp/e_ferryman", func_pspeu_0923D4A0);
