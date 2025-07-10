d1ac00c31d10ecb867269171dc99b8352185559d
John Dunlap <johndunlapdev@gmail.com>
2025-07-10 10:49:41 -0400

    reformatted array access

--- src/st/no2/unk_33A94.c
+++ src/st/no2/unk_33A94.c
@@ -9,6 +9,7 @@ INCLUDE_ASM("st/no2/nonmatchings/unk_33A94", func_us_801B3F30);
 
 INCLUDE_ASM("st/no2/nonmatchings/unk_33A94", EntityFrozenShadeCrystal);
 
+//INCLUDE_ASM("st/no2/nonmatchings/unk_33A94", func_us_801B41A4);
 extern s8 g_GpuBuffers_0_draw_b0;
 extern s8 g_GpuBuffers_0_draw_g0;
 extern s8 g_GpuBuffers_0_draw_r0;
@@ -20,12 +21,12 @@ void func_us_801B41A4(void) {
     if (g_CurrentEntity->step == 0) {
         g_CurrentEntity->step += 1;
     }
-    g_GpuBuffers_0_draw_r0 = 0x20;
-    g_GpuBuffers_0_draw_g0 = 0x18;
-    g_GpuBuffers_0_draw_b0 = 0x28;
-    g_GpuBuffers_1_draw_r0 = 0x20;
-    g_GpuBuffers_1_draw_g0 = 0x18;
-    g_GpuBuffers_1_draw_b0 = 0x28;
+    g_GpuBuffers[0].draw.r0 = 0x20;
+    g_GpuBuffers[0].draw.g0 = 0x18;
+    g_GpuBuffers[0].draw.b0 = 0x28;
+    g_GpuBuffers[1].draw.r0 = 0x20;
+    g_GpuBuffers[1].draw.g0 = 0x18;
+    g_GpuBuffers[1].draw.b0 = 0x28;
 }
 
 INCLUDE_ASM("st/no2/nonmatchings/unk_33A94", func_us_801B4210);
