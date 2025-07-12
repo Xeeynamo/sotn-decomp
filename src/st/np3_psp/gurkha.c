e71aea17f6b7cdbeb1de70b7a78079b69cc7d1b7
Joey Murphy <tjmurphy@mit.edu>
2025-07-12 15:08:09 -0400

    fix psp

--- src/st/np3_psp/gurkha.c
+++ src/st/np3_psp/gurkha.c
@@ -4,14 +4,14 @@
 // clang-format off
 
 // func_801CF778
-INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_0925DD30);
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/gurkha", func_pspeu_0925DD30);
 // func_801CF7A0
-INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_0925DD78);
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/gurkha", func_pspeu_0925DD78);
 
 // EntityGurkha
-INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_0925DF90);
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/gurkha", func_pspeu_0925DF90);
 
 // EntityGurkhaSword
-INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_0925F370);
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/gurkha", func_pspeu_0925F370);
 
 // clang-format on
