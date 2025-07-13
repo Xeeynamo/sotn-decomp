2ff5004633330effa1eed952038e4c34dd686f4e
bismurphy <tjmurphy@mit.edu>
2025-07-13 10:02:12 -0400

    NP3 PSP Blade enemy (#2740)
    Mostly just taking US stuff and porting it over with tweaks to be
    compatible with PSP, but also pulling in data, and cleaning up some
    struct stuff.
    
    Still lots to learn here, but once we have the code cleaned up the
    research will become easier.
    
    Prepping for next PR which should be Gurkha.

---
+++ src/st/np3_psp/working_on.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../np3/np3.h"
+
+// clang-format off
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_0925F770);
+
+// Hammer
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_0925F9D0);
+
+// EntityGurkhaBodyParts
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_09260A58);
+
+// EntityHammerWeapon
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_09260EE0);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_801CE04C);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_801CE120);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_801CE1E8);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_801CE228);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", polarPlacePartsList);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_pspeu_092614A8);
+
+INCLUDE_ASM("st/np3_psp/nonmatchings/np3_psp/working_on", func_801CE3FC);
+
+// clang-format on
