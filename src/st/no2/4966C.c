7227bd0c2ea9b80ff819eb752cc6dd8f176995a8
gamezter <gamezter93@gmail.com>
2025-07-14 15:25:36 -0400

    Code-Share giantbro_helpers

---
+++ src/st/no2/4966C.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "no2.h"
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CE04C);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CE120);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CE1E8);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CE228);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", polarPlacePartsList);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CE2CC);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CE3FC);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CE4CC);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", EntityHammer);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", EntityGurkhaBodyParts);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", EntityHammerWeapon);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CF778);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", func_801CF7A0);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", EntityGurkha);
+
+INCLUDE_ASM("st/no2/nonmatchings/4966C", EntityGurkhaSword);
