f78e1010b850372a9897289b41a1a049cbd4deb6
gamezter <gamezter93@gmail.com>
2025-07-16 17:48:37 -0400

    Fix PSP

--- src/st/no2/header.c
+++ src/st/no2/header.c
@@ -22,7 +22,9 @@ AbbreviatedOverlay OVL_EXPORT(Overlay) = {
     .UpdateStageEntities = UpdateStageEntities,
 };
 
+#ifndef VERSION_PSP
 #include "gen/sprite_banks.h"
 #include "gen/palette_def.h"
 #include "gen/layers.h"
 #include "gen/graphics_banks.h"
+#endif
