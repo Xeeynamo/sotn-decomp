b5b627de270ab141f52a4eb91baa4ad2d12b873a
gamezter <charronalexandre@hotmail.com>
2025-07-17 07:49:01 -0400

    Import `no2` data (#2755)

---
+++ src/st/no2/background_block_init.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "no2.h"
+
+static u8 AnimFrames_80180994[] = {0x40, 0x01, 0xFF, 0x00};
+
+ObjInit2 OVL_EXPORT(BackgroundBlockInit)[] = {
+    {.animSet = ANIMSET_DRA(6),
+     .zPriority = 0x01FA,
+     .unk5A = 0x0000,
+     .palette = PAL_DRA(0),
+     .drawFlags = FLAG_DRAW_DEFAULT,
+     .drawMode = DRAW_TPAGE,
+     .flags = 0,
+     .animFrames = AnimFrames_80180994},
+};
