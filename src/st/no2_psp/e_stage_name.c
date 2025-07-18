5d363c0155c9437d3b63eb27cd3f189b2d43a172
gamezter <gamezter93@gmail.com>
2025-07-18 11:41:31 -0400

    Decompile `no2_psp` e_stage_name

--- src/st/no2_psp/e_stage_name.c
+++ src/st/no2_psp/e_stage_name.c
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../no2/no2.h"
 
-INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/e_stage_name", func_psp_0923C0C0);
-// clang-format off
-INCLUDE_ASM("st/no2_psp/nonmatchings/no2_psp/e_stage_name", EntityStageNamePopup);
+extern s32 g_UserLanguage;
+extern u_long* D_pspeu_0925D1E8[];
+extern u8* D_pspeu_0925D1F4;
+extern u8* D_pspeu_0925D200[];
+
+void func_psp_0923C0C0(void) {
+    D_pspeu_0925D1F4 = D_pspeu_0925D200[g_UserLanguage - 1];
+    func_91040A0(D_pspeu_0925D1E8);
+}
+
+#include "../entity_stage_name_popup_jp.h"
