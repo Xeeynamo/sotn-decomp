f64f492023d481756610f33e942c922067400f2a
gamezter <gamezter93@gmail.com>
2025-07-22 09:56:39 -0400

    Decompile `no2` func_us_801B6E34

---
+++ src/st/no2/372E8.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "no2.h"
+
+INCLUDE_ASM("st/no2/nonmatchings/372E8", func_us_801B72E8);
+
+INCLUDE_ASM("st/no2/nonmatchings/372E8", func_us_801B7580);
