b6b43231b184f4ed3953a3142d78fbb0d2e7e3cc
TomoshibiAkira <yumesaki_toriko@yahoo.co.jp>
2025-07-23 01:35:40 +0800

    follow other implementations; lint

--- src/st/no2_psp/22BC8.c
+++ src/st/no2_psp/22BC8.c
@@ -10,7 +10,7 @@ void func_pspeu_0925A248(Entity* self) {
     DRAWENV draw;
     RECT rect;
     DR_ENV* dr_env;
-    Prim_801B4DA4* prim;
+    Primitive* prim;
     s32 i;
     s32 primIndex;
     u8 flag;
@@ -19,17 +19,17 @@ void func_pspeu_0925A248(Entity* self) {
     switch (self->step) {
     case 0:
         InitializeEntity(D_pspeu_09266648);
-        primIndex = g_api.func_800EDB58(4, 8);
+        primIndex = g_api.func_800EDB58(PRIM_GT4, 8);
         if (primIndex != -1) {
             self->flags |= FLAG_HAS_PRIMS;
             self->primIndex = primIndex;
-            prim = (Prim_801B4DA4*)&g_PrimBuf[primIndex];
+            prim = &g_PrimBuf[primIndex];
             dr_env = g_api.func_800EDB08((POLY_GT4*)prim);
             if (dr_env == NULL) {
                 DestroyEntity(self);
                 return;
             }
-            prim->union0.rgb.type = 7;
+            prim->type = PRIM_ENV;
             prim->priority = 0x13F;
             prim->drawMode = DRAW_HIDE;
             prim = prim->next;
@@ -38,20 +38,21 @@ void func_pspeu_0925A248(Entity* self) {
                 DestroyEntity(self);
                 return;
             }
-            prim->union0.rgb.type = 7;
+            prim->type = PRIM_ENV;
             prim->priority = 0x141;
             prim->drawMode = DRAW_UNK_800;
             prim = prim->next;
             self->ext.prim = (Primitive*)prim;
-            for (; prim != NULL; prim = prim->next) {
+            while (prim != NULL) {
                 prim->drawMode = DRAW_HIDE;
+                prim = prim->next;
             }
             return;
         }
         DestroyEntity(self);
         return;
     case 1:
-        prim = (Prim_801B4DA4*)&g_PrimBuf[self->primIndex];
+        prim = &g_PrimBuf[self->primIndex];
         draw = g_CurrentBuffer->draw;
         if (draw.ofs[0] == 0) {
             flag = 0;
@@ -66,13 +67,13 @@ void func_pspeu_0925A248(Entity* self) {
         draw.clip = rect;
         draw.ofs[0] = 0;
         draw.ofs[1] = 0x100;
-        dr_env = prim->union1.drenv1;
+        dr_env = *(DR_ENV**)&prim->r1;
         SetDrawEnv(dr_env, &draw);
         prim->drawMode = DRAW_DEFAULT;
         prim = prim->next;
-        prim = self->ext.et_801B4DA4.prim;
+        prim = self->ext.prim;
         for (i = 2; i >= 0; i--) {
-            prim->union0.rgb.type = 4;
+            prim->type = PRIM_GT4;
             prim->tpage = 0x1110;
             prim->u0 = prim->u2 = 0;
             prim->u1 = prim->u3 = prim->u0 + 0x28;
@@ -82,11 +83,10 @@ void func_pspeu_0925A248(Entity* self) {
             prim->x1 = prim->x3 = prim->x0 + (0x50 - (i * 0x14));
             prim->y0 = prim->y1 = (i * 0xA) + 0x40;
             prim->y2 = prim->y3 = prim->y0 + (0x50 - (i * 0x14));
-            prim->union0.rgb.r0 = prim->union0.rgb.g0 = prim->union0.rgb.b0 =
-                0xFF;
-            prim->union1.drenv1 = prim->union0.drenv0;
-            prim->union2.drenv2 = prim->union0.drenv0;
-            prim->union3.drenv3 = prim->union0.drenv0;
+            prim->r0 = prim->g0 = prim->b0 = 0xFF;
+            LOW(prim->r1) = LOW(prim->r0);
+            LOW(prim->r2) = LOW(prim->r0);
+            LOW(prim->r3) = LOW(prim->r0);
             prim->priority = 0x140;
             prim->drawMode = DRAW_TPAGE2 | DRAW_TPAGE | DRAW_TRANSP;
             if (i == 0) {
@@ -95,7 +95,7 @@ void func_pspeu_0925A248(Entity* self) {
             prim->drawMode |= DRAW_COLORS;
             prim = prim->next;
         }
-        prim->union0.rgb.type = 4;
+        prim->type = PRIM_GT4;
         prim->tpage = 0x101A;
         prim->clut = 0x15F;
         prim->u0 = prim->u2 = 0x10;
@@ -106,16 +106,16 @@ void func_pspeu_0925A248(Entity* self) {
         prim->x1 = prim->x3 = 0x28;
         prim->y0 = prim->y1 = 0;
         prim->y2 = prim->y3 = 0x28;
-        prim->union0.rgb.r0 = 0x40;
-        prim->union0.rgb.g0 = 0x38;
-        prim->union0.rgb.b0 = 0x38;
-        prim->union1.drenv1 = prim->union0.drenv0;
-        prim->union2.drenv2 = prim->union0.drenv0;
-        prim->union3.drenv3 = prim->union0.drenv0;
+        prim->r0 = 0x40;
+        prim->g0 = 0x38;
+        prim->b0 = 0x38;
+        LOW(prim->r1) = LOW(prim->r0);
+        LOW(prim->r2) = LOW(prim->r0);
+        LOW(prim->r3) = LOW(prim->r0);
         prim->priority = 0x140;
         prim->drawMode = DRAW_UNK_40 | DRAW_TPAGE | DRAW_COLORS | DRAW_TRANSP;
         prim = prim->next;
-        prim->union0.rgb.type = 4;
+        prim->type = PRIM_GT4;
         prim->tpage = 0x104;
         prim->u0 = prim->u2 = 0x8B;
         prim->u1 = prim->u3 = 0xB3;
@@ -128,7 +128,7 @@ void func_pspeu_0925A248(Entity* self) {
         prim->priority = 0x140;
         prim->drawMode = DRAW_DEFAULT;
         prim = prim->next;
-        prim->union0.rgb.type = 4;
+        prim->type = PRIM_GT4;
         prim->tpage = 0x1110;
         prim->u0 = prim->u2 = 4;
         prim->u1 = prim->u3 = 0x4C;
@@ -138,12 +138,12 @@ void func_pspeu_0925A248(Entity* self) {
         prim->x1 = prim->x3 = 0xC3;
         prim->y0 = prim->y1 = 0x27;
         prim->y2 = prim->y3 = 0x6F;
-        prim->union0.rgb.r0 = 0x20;
-        prim->union0.rgb.g0 = 0x10;
-        prim->union0.rgb.b0 = 0x20;
-        prim->union1.drenv1 = prim->union0.drenv0;
-        prim->union2.drenv2 = prim->union0.drenv0;
-        prim->union3.drenv3 = prim->union0.drenv0;
+        prim->r0 = 0x20;
+        prim->g0 = 0x10;
+        prim->b0 = 0x20;
+        LOW(prim->r1) = LOW(prim->r0);
+        LOW(prim->r2) = LOW(prim->r0);
+        LOW(prim->r3) = LOW(prim->r0);
         prim->priority = 0x142;
         prim->drawMode =
             DRAW_UNK_400 | DRAW_TPAGE2 | DRAW_TPAGE | DRAW_COLORS | DRAW_TRANSP;
