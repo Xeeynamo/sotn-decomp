6c4445d3e379b5d4b27a6857213ef80c7d2b9da6
gamezter <charronalexandre@hotmail.com>
2025-07-24 07:26:15 -0400

    Codeshare `no2` gurkha (#2766)
    I renamed:
    EntityGurkhaSword -> EntityGurkhaWeapon
    EntityBladeSword -> EntityBladeWeapon
    and their related `EInit`s and `E_ID`s since the next giantbrother
    entity to be code-shared is hammer, whos secondary entity is
    `EntityHammerWeapon`. I figured we should keep the naming consistent
    across all giantbrothers.
    
    I also tried to format the data in `e_blade.h` and the new `e_gurkha.h`
    to make it more readable and consistent.
    I know @bismurphy is working on related code right now so this PR can
    wait while we get their OK

--- src/st/e_blade.h
+++ src/st/e_blade.h
@@ -122,6 +122,7 @@ static s32 func_801D0B78(Entity* ent) {
 // We should make enums for each of them (12 might be BLADE_LEFT_THIGH)
 // to make these giant data sets more meaningful.
 
+// clang-format off
 static s16 D_801833E4[] = {0, 9, 0, 4, 4, -4, -8, 0};
 static giantBroBodyPartsInit D_801833F4[] = {
     {9, 18, 16, 7, 1},    {10, 9, 14, 8, 2},    {11, 10, 0, 9, 3},
@@ -141,10 +142,14 @@ static s16 D_80183548[] = {12, 13, 9, 10, 14, 11, 2, 7, 8, 4, 5, 0};
 static s16 D_80183560[] = {9, 10, 12, 13, 11, 14, 0};
 static s16 D_80183570[] = {12, 13, 9, 10, 14, 11, 0};
 static s16 D_80183580[] = {2, 4, 5, 15, 0};
-static s16 D_8018358C[] = {
-    0x000,  0x000,  0x080, -0x100, 0x000, 0x000,  0x000,
-    -0x100, -0x040, 0x100, 0x000,  0x200, -0x500, -0x400};
-static unkStr_801CDD80 D_801835A8[] = {{16, D_8018358C}, {0, 0}};
+
+static s16 D_8018358C[] = {0x000,  0x000, 0x080, -0x100, 0x000, 0x000,  0x000,
+                          -0x100, -0x040, 0x100, 0x000,  0x200, -0x500, -0x400};
+static unkStr_801CDD80 D_801835A8[] = {
+    {16, D_8018358C},
+    {0, 0}
+};
+
 static s16 D_801835B8[] = {0x040, -0x020, 0x020,  -0x220, 0x000, -0x100,
                            0x000, -0x0C0, -0x0C0, 0x020,  0x100, 0x000};
 static s16 D_801835D0[] = {-0x090, -0x090, 0x158, 0x000,  0x000,  -0x020,
@@ -152,11 +157,21 @@ static s16 D_801835D0[] = {-0x090, -0x090, 0x158, 0x000,  0x000,  -0x020,
 static s16 D_801835E8[] = {-0x100, -0x180, 0x100, 0x0C0,  0x000,  0x040,
                            0x000,  0x100,  0x280, -0x140, -0x140, 0x000};
 static unkStr_801CDD80 D_80183600[] = {
-    {14, D_801835B8}, {14, D_801835D0}, {14, D_801835E8}, {0, 0}};
+    {14, D_801835B8},
+    {14, D_801835D0},
+    {14, D_801835E8},
+    {0, 0}
+};
+
 static s16 D_80183620[] = {0x200, 0x0C0,  -0x100, -0x180, 0x000, -0x040,
                            0x000, -0x140, -0x140, 0x100,  0x280, 0x000};
 static unkStr_801CDD80 D_80183638[] = {
-    {14, D_801835D0}, {14, D_801835B8}, {14, D_80183620}, {0, 0}};
+    {14, D_801835D0},
+    {14, D_801835B8},
+    {14, D_80183620},
+    {0, 0}
+};
+
 static s16 D_80183658[] = {0x100,  0x040,  0x080, -0x400, 0x000, -0x380,
                            -0x0C0, -0x0C0, 0x080, 0x000,  0x300, 0x000};
 static s16 D_80183670[] = {0x0C0,  -0x100, 0x100, -0x480, 0x000, -0x400,
@@ -172,21 +187,30 @@ static s16 D_801836D0[] = {0x020,  -0x380, 0x1C0, 0x000,  -0x240, 0x100,
 static s16 D_801836E8[] = {0x080,  -0x400, 0x100, 0x040,  -0x380, 0x100,
                            -0x0C0, 0x000,  0x300, -0x0C0, 0x080,  0x000};
 static unkStr_801CDD80 D_80183700[] = {
-    {5, D_80183670}, {5, D_80183688}, {4, D_801836A0}, {0, 0}};
+    {5, D_80183670},
+    {5, D_80183688},
+    {4, D_801836A0},
+    {0, 0}
+};
 static unkStr_801CDD80 D_80183720[] = {
     {4, D_801836B8},
     {4, D_801836D0},
     {4, D_801836E8},
     {0x60, D_801836E8},
-    {0, 0}};
-static s16 D_80183748[] = {0, 0, 0x80, -0x80, 0, 0, 0, 0, 0, 0, 0, 0};
-static unkStr_801CDD80 D_80183760[] = {{32, D_80183748}, {0, 0}};
-static s16 D_80183770[] = {
-    0x100,  -0x240, 0x200, -0x100, 0x000, 0x000,  -0x1C0,
-    -0x100, -0x300, 0x100, 0x000,  0x180, -0x500, -0x480};
-static s16 D_8018378C[] = {
-    0x180,  -0x240, 0x280, -0x100, 0x000, 0x000,  -0x1C0,
-    -0x100, -0x300, 0x100, 0x000,  0x180, -0x500, -0x480};
+    {0, 0}
+};
+
+static s16 D_80183748[] = {0x000, 0x000, 0x080, -0x080, 0x000, 0x000,
+                           0x000, 0x000, 0x000, 0x000, 0x000, 0x000};
+static unkStr_801CDD80 D_80183760[] = {
+    {32, D_80183748},
+    {0, 0}
+};
+
+static s16 D_80183770[] = {0x100,  -0x240, 0x200, -0x100, 0x000, 0x000,  -0x1C0,
+                           -0x100, -0x300, 0x100, 0x000,  0x180, -0x500, -0x480};
+static s16 D_8018378C[] = {0x180,  -0x240, 0x280, -0x100, 0x000, 0x000,  -0x1C0,
+                           -0x100, -0x300, 0x100, 0x000,  0x180, -0x500, -0x480};
 static s16 D_801837A8[] = {0x040,  -0x180, 0x1C0, -0x180, 0x000, 0x000,  -0x020,
                            -0x040, -0x300, 0x100, 0x300,  0x500, -0x500, 0x000};
 static s16 D_801837C4[] = {0x000, -0x100, 0x100, -0x100, 0x000, 0x000,  0x080,
@@ -202,15 +226,24 @@ static s16 D_80183834[] = {-0x240, -0x240, 0x300, -0x100, 0x000, 0x000, -0x200,
 static s16 D_80183850[] = {-0x280, -0x280, 0x380, -0x140, 0x000, 0x000, -0x180,
                            -0x080, 0x340,  0x4C0, -0x200, 0x200, 0x500, -0x300};
 static unkStr_801CDD80 D_8018386C[] = {
-    {16, D_80183770}, {24, D_8018378C}, {4, D_801837A8},
-    {8, D_801837C4},  {18, D_801837E0}, {0, 0}};
+    {16, D_80183770},
+    {24, D_8018378C},
+    {4, D_801837A8},
+    {8, D_801837C4},
+    {18, D_801837E0},
+    {0, 0}
+};
 static unkStr_801CDD80 D_8018389C[] = {
     {4, D_801837FC},
     {6, D_80183818},
     {4, D_80183834},
     {32, D_80183850},
     {0, 0}};
-static unkStr_801CDD80 D_801838C4[] = {{16, D_80183770}, {0, 0}};
+static unkStr_801CDD80 D_801838C4[] = {
+    {16, D_80183770},
+    {0, 0}
+};
+
 static s16 D_801838D4[] = {0x080, -0x040, 0x300,  -0x200, 0x000, -0x200,
                            0x100, -0x080, -0x100, 0x300,  0x100, 0x180};
 static s16 D_801838EC[] = {0x0A0, -0x040, 0x400, 0x300, 0x000,  0x300,
@@ -220,21 +253,30 @@ static s16 D_80183904[] = {0x080, -0x040, 0x500, 0x380, 0x000,  0x400,
 static s16 D_8018391C[] = {0x080, -0x040, 0x480, 0x300, 0x000,  0x300,
                            0x180, -0x080, 0x080, 0x300, -0x100, 0x080};
 static unkStr_801CDD80 D_80183934[] = {
-    {12, D_8018358C}, {12, D_80183770}, {4, D_801838D4},  {4, D_801838EC},
-    {6, D_80183904},  {24, D_8018391C}, {16, D_80183770}, {0, 0}};
+    {12, D_8018358C},
+    {12, D_80183770},
+    {4, D_801838D4},
+    {4, D_801838EC},
+    {6, D_80183904},
+    {24, D_8018391C},
+    {16, D_80183770},
+    {0, 0}
+};
+
 static s16 D_80183974[] = {0x0C0, -0x080, 0x180, -0x200, 0x000, -0x200, 0x100,
                            0x180, 0x400,  0x600, 0x300,  0x500, 0x000,  -0x100};
 static unkStr_801CDD80 D_80183990[] = {
-    {16, D_80183974}, {32, D_8018358C}, {0, 0}};
-static s16 D_801839A8[] = {
-    0x000,  -0x200, 0x080, -0x200, -0x200, -0x200, 0x000,
-    -0x100, -0x040, 0x100, 0x000,  0x200,  -0x500, -0x400};
-static s16 D_801839C4[] = {
-    -0x100, -0x400, -0x140, -0x400, -0x400, -0x400, -0x200,
-    -0x100, -0x100, 0x200,  0x200,  0x300,  -0x300, -0x100};
-static s16 D_801839E0[] = {
-    0x000,  -0x400, 0x080, -0x400, -0x400, -0x400, -0x100,
-    -0x100, 0x200,  0x400, 0x200,  0x280,  -0x300, -0x100};
+    {16, D_80183974},
+    {32, D_8018358C},
+    {0, 0}
+};
+
+static s16 D_801839A8[] = {0x000,  -0x200, 0x080, -0x200, -0x200, -0x200, 0x000,
+                           -0x100, -0x040, 0x100, 0x000,  0x200,  -0x500, -0x400};
+static s16 D_801839C4[] = {-0x100, -0x400, -0x140, -0x400, -0x400, -0x400, -0x200,
+                           -0x100, -0x100, 0x200,  0x200,  0x300,  -0x300, -0x100};
+static s16 D_801839E0[] = {0x000,  -0x400, 0x080, -0x400, -0x400, -0x400, -0x100,
+                           -0x100, 0x200,  0x400, 0x200,  0x280,  -0x300, -0x100};
 static s16 D_801839FC[] = {0x000, 0x400, 0x600, 0x300};
 static s16 D_80183A04[] = {0x040, 0x700, 0xB00, 0x800};
 static s16 D_80183A0C[] = {0x000, 0x780, 0xB80, 0xC00};
@@ -247,8 +289,14 @@ static unkStr_801CDD80 D_80183A24[] = {
     {56, D_801839E0},
     {0, 0}};
 static unkStr_801CDD80 D_80183A4C[] = {
-    {6, D_801839FC}, {8, D_80183A04}, {32, D_80183A0C},
-    {4, D_80183A14}, {3, D_80183A1C}, {0, 0}};
+    {6, D_801839FC},
+    {8, D_80183A04},
+    {32, D_80183A0C},
+    {4, D_80183A14},
+    {3, D_80183A1C},
+    {0, 0}
+};
+// clang-format on
 
 void EntityBlade(Entity* self) {
     Collider collider;
@@ -259,7 +307,7 @@ void EntityBlade(Entity* self) {
     Entity* ent_s0;
 
     if ((self->step & 1) && (self->hitFlags & 3)) {
-        func_801CE1E8(0xE);
+        func_801CE1E8(14);
     }
     if ((self->flags & FLAG_DEAD) && (self->step < 0x18)) {
         ent_s0 = self + 13;
@@ -267,7 +315,7 @@ void EntityBlade(Entity* self) {
 
         if (ent_s0->ext.GH_Props.unk88 || ent_s4->ext.GH_Props.unk88) {
             PlaySfxPositional(0x750);
-            func_801CE1E8(0x18);
+            func_801CE1E8(24);
         }
     }
     switch (self->step) {
@@ -303,14 +351,14 @@ void EntityBlade(Entity* self) {
         self->unk5C = NULL;
 
         ent_s0 = self + 15;
-        CreateEntityFromCurrentEntity(E_BLADE_SWORD, ent_s0);
+        CreateEntityFromCurrentEntity(E_BLADE_WEAPON, ent_s0);
         ent_s0->ext.GH_Props.length = 12;
         ent_s0->ext.GH_Props.parent = self + 5;
         ent_s0->zPriority = self->zPriority + 3;
         ent_s0->params = 19;
 
         ent_s0 = self + 16;
-        CreateEntityFromCurrentEntity(E_BLADE_SWORD, ent_s0);
+        CreateEntityFromCurrentEntity(E_BLADE_WEAPON, ent_s0);
         ent_s0->ext.GH_Props.length = 12;
         ent_s0->ext.GH_Props.parent = self + 8;
         ent_s0->zPriority = self->zPriority - 4;
@@ -327,11 +375,11 @@ void EntityBlade(Entity* self) {
         } else {
             ptr = D_80183548;
         }
-        func_801CDF1C(ptr, &D_80183600, 0);
+        func_801CDF1C(ptr, D_80183600, 0);
         func_801CDE10(ptr);
         func_801D0A00(ptr);
         func_801D0B40();
-        polarPlacePartsList(&D_80183494);
+        polarPlacePartsList(D_80183494);
         if (self->ext.GH_Props.unkB0[0] > 1) {
             collider.unk18 = 9;
             ent_s0 = self + ptr[3];
@@ -360,11 +408,11 @@ void EntityBlade(Entity* self) {
         } else {
             ptr = D_80183570;
         }
-        func_801CDF1C(ptr, &D_80183638, 0);
+        func_801CDF1C(ptr, D_80183638, 0);
         func_801CDE10(ptr);
         func_801D0A00(ptr);
         func_801D0B40();
-        polarPlacePartsList(&D_80183494);
+        polarPlacePartsList(D_80183494);
         if (self->ext.GH_Props.unkB0[0] > 1) {
             collider.unk18 = 9;
             ent_s0 = self + ptr[3];
@@ -401,7 +449,7 @@ void EntityBlade(Entity* self) {
             } else {
                 ptr = D_801834D8;
             }
-            func_801CDF1C(ptr, &D_801838C4, 0);
+            func_801CDF1C(ptr, D_801838C4, 0);
             func_801CDE10(ptr);
             func_801D0A00(ptr);
             func_801D0B40();
@@ -411,7 +459,7 @@ void EntityBlade(Entity* self) {
             self->step_s++;
             break;
         case 1:
-            func_801CDF1C(ptr, &D_80183700, 0);
+            func_801CDF1C(ptr, D_80183700, 0);
             func_801CDE10(ptr);
             func_801D0A00(ptr);
             func_801D0B40();
@@ -432,7 +480,7 @@ void EntityBlade(Entity* self) {
         case 3:
             MoveEntity();
             self->velocityY += FIX(11.0 / 128);
-            func_801CDF1C(ptr, &D_80183720, 0);
+            func_801CDF1C(ptr, D_80183720, 0);
             func_801CDE10(ptr);
             func_801CE3FC(ptr);
             func_801D0B40();
@@ -456,7 +504,7 @@ void EntityBlade(Entity* self) {
             }
             break;
         }
-        polarPlacePartsList(&D_80183494);
+        polarPlacePartsList(D_80183494);
         break;
     case 8:
         if (self->ext.GH_Props.unk84 == 1) {
@@ -474,10 +522,10 @@ void EntityBlade(Entity* self) {
             self->step_s++;
             /* fallthrough */
         case 1:
-            func_801CDF1C(ptr, &D_8018386C, 0);
+            func_801CDF1C(ptr, D_8018386C, 0);
             func_801CDE10(ptr);
             func_801D0A00(ptr);
-            polarPlacePartsList(&D_80183494);
+            polarPlacePartsList(D_80183494);
             ent_s0 = self + 16;
             if (ent_s0->hitFlags) {
                 self->ext.GH_Props.unk8D = 1;
@@ -491,14 +539,14 @@ void EntityBlade(Entity* self) {
             }
             break;
         case 2:
-            func_801CDF1C(ptr, &D_8018389C, 0);
+            func_801CDF1C(ptr, D_8018389C, 0);
             func_801CDE10(ptr);
             func_801D0A00(ptr);
             if ((self->ext.GH_Props.unkB0[0] == 1) &&
                 !self->ext.GH_Props.unkB4[0]) {
                 PlaySfxPositional(SFX_BONE_SWORD_SWISH_C);
             }
-            polarPlacePartsList(&D_80183494);
+            polarPlacePartsList(D_80183494);
             if (!self->ext.GH_Props.unkB0[0] && !self->ext.GH_Props.unkB4[0]) {
                 ent_s0 = self + 15;
                 ent_s0->ext.GH_Props.unk8D = 0;
@@ -508,10 +556,10 @@ void EntityBlade(Entity* self) {
             }
             break;
         case 3:
-            func_801CDF1C(ptr, &D_801835A8, 0);
+            func_801CDF1C(ptr, D_801835A8, 0);
             func_801CDE10(ptr);
             func_801D0A00(ptr);
-            polarPlacePartsList(&D_80183494);
+            polarPlacePartsList(D_80183494);
             if (!self->ext.GH_Props.unkB0[0] && !self->ext.GH_Props.unkB4[0]) {
                 var_s3 = func_801D0B78(self);
                 if (var_s3) {
@@ -533,11 +581,11 @@ void EntityBlade(Entity* self) {
         } else {
             ptr = D_80183514;
         }
-        func_801CDF1C(ptr, &D_80183934, 0);
+        func_801CDF1C(ptr, D_80183934, 0);
         func_801CDE10(ptr);
         func_801D0A00(ptr);
         func_801D0B40();
-        polarPlacePartsList(&D_80183494);
+        polarPlacePartsList(D_80183494);
         ent_s0 = self + ptr[5];
         ent_s0->ext.GH_Props.unk8D = 1;
         if (!self->ext.GH_Props.unkB0[0] && !self->ext.GH_Props.unkB4[0]) {
@@ -558,11 +606,11 @@ void EntityBlade(Entity* self) {
         } else {
             ptr = D_80183548;
         }
-        func_801CDF1C(ptr, &D_80183760, 0);
+        func_801CDF1C(ptr, D_80183760, 0);
         func_801CDE10(ptr);
         func_801D0A00(ptr);
         func_801D0B40();
-        polarPlacePartsList(&D_80183494);
+        polarPlacePartsList(D_80183494);
         if (!self->ext.GH_Props.unkB4[0]) {
             self->facingLeft ^= 1;
             self->ext.GH_Props.unk88 = 0;
@@ -584,10 +632,10 @@ void EntityBlade(Entity* self) {
         } else {
             ptr = D_801834D8;
         }
-        func_801CDF1C(ptr, &D_80183990, 0);
+        func_801CDF1C(ptr, D_80183990, 0);
         func_801CDE10(ptr);
         func_801D0A00(ptr);
-        polarPlacePartsList(&D_80183494);
+        polarPlacePartsList(D_80183494);
         if (!self->ext.GH_Props.unkB0[0] && !self->ext.GH_Props.unkB4[0]) {
             var_s3 = func_801D0B78(self);
             if (var_s3) {
@@ -613,10 +661,10 @@ void EntityBlade(Entity* self) {
             } else {
                 ptr = D_801834D8;
             }
-            func_801CDF1C(ptr, &D_80183A24, 0);
+            func_801CDF1C(ptr, D_80183A24, 0);
             func_801CDE10(ptr);
             func_801D0A00(ptr);
-            polarPlacePartsList(&D_80183494);
+            polarPlacePartsList(D_80183494);
             if (self->ext.GH_Props.unkB0[0] == 3 &&
                 !self->ext.GH_Props.unkB4[0]) {
                 PlaySfxPositional(SFX_STOMP_HARD_A);
@@ -627,9 +675,9 @@ void EntityBlade(Entity* self) {
             break;
         case 2:
             ptr = D_80183580;
-            func_801CDF1C(ptr, &D_80183A4C, 0);
+            func_801CDF1C(ptr, D_80183A4C, 0);
             polarPlacePartsWithAngvel(ptr);
-            polarPlacePartsList(&D_80183494);
+            polarPlacePartsList(D_80183494);
             if (!self->ext.GH_Props.unkB0[0] && !self->ext.GH_Props.unkB4[0]) {
                 PlaySfxPositional(SFX_ARROW_SHOT_A);
                 ent_s0 = self + 15;
@@ -678,7 +726,7 @@ void EntityBlade(Entity* self) {
     D_8006C38C.y = self->ext.GH_Props.unkB4[0];
 }
 
-void EntityBladeSword(Entity* self) {
+void EntityBladeWeapon(Entity* self) {
     Primitive *prim, *prim2;
     s32 x0, y0, x1, y1;
     s32 primIndex;
@@ -691,7 +739,7 @@ void EntityBladeSword(Entity* self) {
 
     switch (self->step) {
     case 0:
-        InitializeEntity(g_EInitBladeSword);
+        InitializeEntity(g_EInitBladeWeapon);
         self->drawFlags |= FLAG_DRAW_ROTATE;
         self->hitboxWidth = 6;
         self->hitboxHeight = 6;
