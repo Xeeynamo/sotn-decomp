1a7707ade0fad6afc6a1eca2be4acaacdd7b5782
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-08-04 10:58:51 +1000

    Fix PSP `e_collect`

--- src/st/cat_psp/e_collect.c
+++ src/st/cat_psp/e_collect.c
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../cat/cat.h"
 
+INCLUDE_ASM("st/cat_psp/nonmatchings/cat_psp/e_collect", func_psp_0923C2F8);
+
+INCLUDE_ASM("st/cat_psp/nonmatchings/cat_psp/e_collect", func_psp_0923C390);
+
+INCLUDE_ASM("st/cat_psp/nonmatchings/cat_psp/e_collect", BlitChar);
+
 INCLUDE_ASM("st/cat_psp/nonmatchings/cat_psp/e_collect", PrizeDropFall);
 
 INCLUDE_ASM("st/cat_psp/nonmatchings/cat_psp/e_collect", PrizeDropFall2);
