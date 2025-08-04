8e4d65f8edf8ba33ff452dd9de1af9fa530b625f
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-08-04 13:32:36 +1000

    Dedupe `cat` `popup`

--- src/st/cat/popup.c
+++ src/st/cat/popup.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "cat.h"
 
-INCLUDE_ASM("st/cat/nonmatchings/popup", BottomCornerText);
-
-INCLUDE_RODATA("st/cat/nonmatchings/popup", D_us_801B6BB0);
+#include "../popup.h"
