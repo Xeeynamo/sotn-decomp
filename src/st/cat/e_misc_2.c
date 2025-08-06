284370f7e680047acc7829c45810a976183c22c2
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-08-06 20:54:55 +1000

    More `cat` dedupes (#2784)
    Mostly standard, a couple of functions in `e_misc` on PSP side which had
    been deadstripped out of earlier overlays but are used in CAT, so needed
    minor updates.

--- src/st/cat/e_misc_2.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "cat.h"
-
-INCLUDE_ASM("st/cat/nonmatchings/e_misc_2", EntityHeartDrop);
-
-INCLUDE_ASM("st/cat/nonmatchings/e_misc_2", EntityMessageBox);
--- src/st/cat_psp/e_misc_2.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../cat/cat.h"
-
-INCLUDE_ASM("st/cat_psp/nonmatchings/cat_psp/e_misc_2", EntityHeartDrop);
-
-INCLUDE_ASM("st/cat_psp/nonmatchings/cat_psp/e_misc_2", EntityMessageBox);
