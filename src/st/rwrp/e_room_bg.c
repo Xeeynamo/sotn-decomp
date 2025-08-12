011d447e1d29886cbc771e0528e0da5498a4a5d2
Jonathan Hohle <jonhohle@gmail.com>
2025-08-11 21:24:40 -0700

    Shared e_room_bg
    Most stages use one of a few variants of `e_room_bg`. Those differences
    can be enabled via macros depending on the implementation.

--- src/st/rwrp/e_room_bg.c
+++ src/st/rwrp/e_room_bg.c
@@ -1,30 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rwrp.h"
-
-extern ObjInit2 OVL_EXPORT(BackgroundBlockInit)[];
-extern u16 g_EInitCommon[];
-
-void OVL_EXPORT(EntityBackgroundBlock)(Entity* self) {
-    ObjInit2* objInit = &OVL_EXPORT(BackgroundBlockInit)[self->params];
-    if (!self->step) {
-        InitializeEntity(g_EInitCommon);
-        self->animSet = objInit->animSet;
-        self->zPriority = objInit->zPriority;
-#ifdef VERSION_PSP
-        self->unk5A = LOHU(objInit->facingLeft);
-#else
-        self->unk5A = LOH(objInit->facingLeft);
-#endif
-        self->palette = objInit->palette;
-        self->drawFlags = objInit->drawFlags;
-        self->drawMode = objInit->drawMode;
-        if (objInit->flags) {
-            self->flags = objInit->flags;
-        }
-
-        if (self->params == 1) {
-            self->scaleX = self->scaleY = 0x0200;
-        }
-    }
-    AnimateEntity(objInit->animFrames, self);
-}
+#define BG_BLOCK_NEEDS_SCALE
+#include "../e_room_bg.h"
