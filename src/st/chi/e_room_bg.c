f1c178ef54ca774791f7d7a410119a060183ee02
Jonathan Hohle <jonhohle@gmail.com>
2025-08-12 13:44:11 -0700

    Shared `e_room_bg` (#2790)
    Most stages use one of a few variants of `e_room_bg`. Those differences
    can be enabled via macros depending on the implementation.
    
    The first variation is how `facingLeft` is assigned. Many overlays
    assign that to `unk5A`, but others assign it to `facingLeft` and then
    have an additional copy of `unk5A`. There's a possibility that future
    PSP versions will also need `BG_FACING_LEFT_FIX` along with an unsigned
    load, but the current overlays do not.
    
    The other variation is whether or not `scaleX` is set. So far, any
    overlay that sets the scale uses the same value.

--- src/st/chi/e_room_bg.c
+++ src/st/chi/e_room_bg.c
@@ -7,31 +7,6 @@
 
 #include "chi.h"
 
-extern ObjInit2 OVL_EXPORT(BackgroundBlockInit)[];
-extern u16 g_EInitCommon[];
-
 // func_8019B3FC: [E_BACKGROUND_BLOCK] EntityBackgroundBlock
-void OVL_EXPORT(EntityBackgroundBlock)(Entity* self) {
-    ObjInit2* objInit = &OVL_EXPORT(BackgroundBlockInit)[self->params];
-    if (!self->step) {
-        InitializeEntity(g_EInitCommon);
-        self->animSet = objInit->animSet;
-        self->zPriority = objInit->zPriority;
-#ifdef VERSION_PSP
-        self->unk5A = LOHU(objInit->facingLeft);
-#else
-        self->unk5A = LOH(objInit->facingLeft);
-#endif
-        self->palette = objInit->palette;
-        self->drawFlags = objInit->drawFlags;
-        self->drawMode = objInit->drawMode;
-        if (objInit->flags) {
-            self->flags = objInit->flags;
-        }
-
-        if (self->params == 1) {
-            self->scaleX = self->scaleY = 0x0200;
-        }
-    }
-    AnimateEntity(objInit->animFrames, self);
-}
+#define BG_BLOCK_NEEDS_SCALE
+#include "../e_room_bg.h"
