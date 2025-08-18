7ab9f1222fd85ec54803a82e9fedd887ea04be1a
ProjectOblivion <ProjectOblivion>
2025-08-18 19:09:47 +0000

    Decompiled, merged, and imported dai/unk_43644

--- src/st/dai/unk_43644.c
+++ src/st/dai/unk_43644.c
@@ -1,4 +1,40 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "dai.h"
 
-INCLUDE_ASM("st/dai/nonmatchings/unk_43644", func_us_801C3644);
+// bss on pspeu
+// currently used by e_priest/func_us_801C2534
+#ifdef VERSION_PSP
+s32 D_us_80180EA0; // D_pspeu_092970A0
+#else
+s32 D_us_80180EA0 = {0};
+#endif
+
+void func_us_801C3644(Entity* self) {
+    if (!D_us_80180EA0) {
+        DestroyEntity(self);
+        return;
+    }
+    switch (self->step) {
+    case 0:
+        InitializeEntity(g_EInitInteractable);
+        g_api.PlaySfx(SET_STOP_SEQ);
+        D_80097928 = 1;
+        self->step = 2;
+        if (g_api.func_80131F68()) {
+            g_api.PlaySfx(SET_UNK_80);
+            self->step++;
+        }
+        break;
+    case 1:
+        if (!g_api.func_80131F68()) {
+            self->step++;
+        }
+        break;
+    case 2:
+        g_api.PlaySfx(D_80097910);
+        D_80097928 = 0;
+        D_us_80180EA0 = 0;
+        self->step++;
+        break;
+    }
+}
