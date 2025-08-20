5c48485dc29489d1ec9c31e9388e47c061e4dae9
gamezter <gamezter93@gmail.com>
2025-08-20 09:56:25 -0400

    Run linter

--- src/main/psxsdk/libcd/c_005.c
+++ src/main/psxsdk/libcd/c_005.c
@@ -11,7 +11,8 @@ extern s32 D_8006BAF0;
 extern s32 D_80072BCC;
 extern s32 D_800987A8;
 
-void StSetStream(u_long mode, u_long start_frame, u_long end_frame, void (*func1)(), void (*func2)()) {
+void StSetStream(u_long mode, u_long start_frame, u_long end_frame,
+                 void (*func1)(), void (*func2)()) {
     StSetMask(1U, start_frame, end_frame);
     D_800987A8 = 0;
     D_8003C8BC = func1;
@@ -22,4 +23,3 @@ void StSetStream(u_long mode, u_long start_frame, u_long end_frame, void (*func1
     D_8003C0FC = 0;
     D_8003C8C0 = func2;
 }
-
