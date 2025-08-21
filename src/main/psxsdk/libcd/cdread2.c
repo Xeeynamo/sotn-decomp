764e95c685c24b96cf3ff7a67070046a950531bd
gamezter <gamezter93@gmail.com>
2025-08-20 21:03:37 -0400

    Decompile more `libcd` functions + cleanup

--- src/main/psxsdk/libcd/cdread2.c
+++ src/main/psxsdk/libcd/cdread2.c
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: MIT
 #include "common.h"
+#include "psxsdk/libcd.h"
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libcd/cdread2", CdRead2);
+void data_ready_callback();
+extern s32 D_8003C768;
 
-void StCdInterrupt();
+static inline void StCdInterrupt2(u_char intr, Result_t* result) {
+    StCdInterrupt();
+}
 
-void StCdInterrupt2(void) { StCdInterrupt(); }
+int CdRead2(long mode) {
+    u8 param = mode;
+    CdControl(CdlSetmode, &param, NULL);
+    if (mode & CdlModeStream) {
+        if (mode & CdlModeSize1) {
+            D_8003C768 = 0;
+        } else {
+            D_8003C768 = 1;
+        }
+        CdDataCallback(data_ready_callback);
+        CdReadyCallback(StCdInterrupt2);
+    }
+    return CdControl(CdlReadS, NULL, NULL);
+}
