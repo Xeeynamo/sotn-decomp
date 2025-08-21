3f16fb4b1030dc995e100fb340ffd71692471e17
gamezter <gamezter93@gmail.com>
2025-08-20 21:14:06 -0400

    decompile `init_ring_status`

--- src/main/psxsdk/libcd/c_008.c
+++ src/main/psxsdk/libcd/c_008.c
@@ -1,5 +1,14 @@
 // SPDX-License-Identifier: MIT
 #include "common.h"
 #include "registers.h"
+#include <psxsdk/libcd.h>
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libcd/c_008", init_ring_status);
+extern StHEADER* D_800987C8;
+
+int init_ring_status(s32 arg0, u32 arg1) {
+    s32 i;
+
+    for (i = 0; i < arg1; i++) {
+        *((s32*)&D_800987C8[i + arg0]) = 0;
+    }
+}
