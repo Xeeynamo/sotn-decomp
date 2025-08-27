7ca02055816888aed3483747e24f0356029b58e1
gamezter <gamezter93@gmail.com>
2025-08-27 09:47:22 -0400

    add comments

--- src/main/psxsdk/libspu/s_n2p.c
+++ src/main/psxsdk/libspu/s_n2p.c
@@ -33,33 +33,41 @@ s32 _spu_note2pitch(s32 center_note, u16 center_note_frac, s32 offset_note,
 s32 _spu_pitch2note(u16 arg0, u16 arg1, u16 arg2) {
     s32 temp_a0;
     s32 note_high;
-    s32 temp_a1_2;
+    s32 lsb_mask;
     s32 temp_v1;
-    u32 var_a3;
+    s32 lsb;
     s32 note_low;
-    s32 var_v1;
+    s32 i;
 
-    var_a3 = 0;
-    for (var_v1 = 0; var_v1 < 16; var_v1++) {
-        if ((arg2 >> var_v1) & 1) {
-            var_a3 = var_v1;
-        }        
+    // Find least significant bit of arg2
+    lsb = 0;
+    for (i = 0; i < 16; i++) {
+        if ((arg2 >> i) & 1) {
+            lsb = i;
+        }
     }
-    
-    temp_a1_2 = (1 << (var_a3 + 1)) - (1 << var_a3);
-    temp_a0 = arg2 - (1 << var_a3);
-    temp_v1 = arg1 + ((temp_a0 - (temp_a1_2 * ((temp_a0 * 12) / temp_a1_2)) / 12) * 0x600) / temp_a1_2;
-    note_high = arg0 + ((var_a3 - 12) * 12) + (temp_a0 * 12) / temp_a1_2;
+
+    // Equivalent to `lsb_mask = 1 << lsb;`
+    lsb_mask = (1 << (lsb + 1)) - (1 << lsb);
+
+    // Mask out lsb of arg2
+    temp_a0 = arg2 - (1 << lsb);
+
+    temp_v1 =
+        arg1 +
+        ((temp_a0 - (lsb_mask * ((temp_a0 * 12) / lsb_mask)) / 12) * 0x600) /
+            lsb_mask;
+    note_high = arg0 + ((lsb - 12) * 12) + (temp_a0 * 12) / lsb_mask;
+
     note_low = temp_v1 % 0x80;
     note_high += (temp_v1 < 0x80) ? 0 : 1;
-    
+
     if (note_high < 0) {
         return -1;
     }
     if (note_high >= 0x80) {
         return -1;
     }
-    
+
     return (note_high << 8) | note_low;
 }
-
