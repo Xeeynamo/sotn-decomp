1ce27b55fec8c3e8d79875a0c2a5ed6b997ec294
gamezter <charronalexandre@hotmail.com>
2025-08-27 23:37:01 -0400

    Decompile `_spu_pitch2note` (#2837)
    Copied from @Mc-muffin's scratch : https://decomp.me/scratch/k3ClY
    
    EDIT: also decompiled `_spu_t` based off @Mc-muffin's scratch :
    https://decomp.me/scratch/EfAgN + code cleanup

--- src/main/psxsdk/libspu/s_n2p.c
+++ src/main/psxsdk/libspu/s_n2p.c
@@ -30,4 +30,44 @@ s32 _spu_note2pitch(s32 center_note, u16 center_note_frac, s32 offset_note,
     return sample_rate;
 }
 
-INCLUDE_ASM("main/nonmatchings/psxsdk/libspu/s_n2p", _spu_pitch2note);
+s32 _spu_pitch2note(u16 arg0, u16 arg1, u16 arg2) {
+    s32 temp_a0;
+    s32 note_high;
+    s32 lsb_mask;
+    s32 temp_v1;
+    s32 lsb;
+    s32 note_low;
+    s32 i;
+
+    // Find least significant bit of arg2
+    lsb = 0;
+    for (i = 0; i < 16; i++) {
+        if ((arg2 >> i) & 1) {
+            lsb = i;
+        }
+    }
+
+    // Equivalent to `lsb_mask = 1 << lsb;`
+    lsb_mask = (1 << (lsb + 1)) - (1 << lsb);
+
+    // Mask out lsb of arg2
+    temp_a0 = arg2 - (1 << lsb);
+
+    temp_v1 =
+        arg1 +
+        ((temp_a0 - (lsb_mask * ((temp_a0 * 12) / lsb_mask)) / 12) * 0x600) /
+            lsb_mask;
+    note_high = arg0 + ((lsb - 12) * 12) + (temp_a0 * 12) / lsb_mask;
+
+    note_low = temp_v1 % 0x80;
+    note_high += (temp_v1 < 0x80) ? 0 : 1;
+
+    if (note_high < 0) {
+        return -1;
+    }
+    if (note_high >= 0x80) {
+        return -1;
+    }
+
+    return (note_high << 8) | note_low;
+}
