5d7791b096a9f9106056a169be3a6fc1abd61ba5
Jonathan Hohle <jonhohle@gmail.com>
2025-09-03 05:57:53 -0700

    BO6 Richter Import (#2841)
    Import BO6 and dedup most of the shared code.
    
    There are a lot of implementation similiarities between boss Richter and
    player Richter, but also both Dopplegangers.

---
+++ src/boss/bo6/richter.c
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "bo6.h"
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", func_us_801B4BD0);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", func_us_801B4EAC);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_CheckBladeDashInput);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_CheckHighJumpInput);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicMain);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", func_us_801B5A14);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", RichterThinking);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", func_us_801B6998);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", EntityRichter);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepStand);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepWalk);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepRun);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepJump);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepFall);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepCrouch);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicResetPose);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", func_us_801B77D8);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepHit);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepDead);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepStandInAir);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepEnableFlameWhip);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepHydrostorm);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepGenericSubwpnCrash);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepThrowDaggers);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepSlide);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepSlideKick);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepBladeDash);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", func_us_801B8E80);
+
+INCLUDE_ASM("boss/bo6/nonmatchings/richter", BO6_RicStepHighJump);
