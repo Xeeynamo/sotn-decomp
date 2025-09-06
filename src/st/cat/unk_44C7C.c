015b01b2971576798439a7243ed43303cb0473ca
neki-or <somaschini.marco2@gmail.com>
2025-09-06 18:03:33 +0000

    Added matching decomp of 801C5600

--- src/st/cat/unk_44C7C.c
+++ src/st/cat/unk_44C7C.c
@@ -3,7 +3,24 @@
 
 INCLUDE_ASM("st/cat/nonmatchings/unk_44C7C", func_us_801C4C7C);
 
-INCLUDE_ASM("st/cat/nonmatchings/unk_44C7C", func_us_801C5600);
+void func_us_801C5600(Primitive* arg0)
+{
+    if ((g_Timer % arg0->u3) != 0) {
+        return;
+    }
+    arg0->y0 -= 1;
+
+    if (arg0->y0 < arg0->u2 + 0x90) {
+        arg0->r0 -= 8;
+        arg0->g0 -= 4;
+        arg0->b0 -= 4;
+    }
+    if (arg0->r0 < 8) {
+        arg0->p3 = 0;
+        arg0->drawMode = 8;
+    }
+
+}
 
 INCLUDE_ASM("st/cat/nonmatchings/unk_44C7C", func_us_801C56A0);
 
