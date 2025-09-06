31bd283a0719074e03a6a7acc106c6be8d0eb422
neki-or <somaschini.marco2@gmail.com>
2025-09-06 18:05:11 +0000

    Renamed arg0 to prim

--- src/st/cat/unk_44C7C.c
+++ src/st/cat/unk_44C7C.c
@@ -3,21 +3,21 @@
 
 INCLUDE_ASM("st/cat/nonmatchings/unk_44C7C", func_us_801C4C7C);
 
-void func_us_801C5600(Primitive* arg0)
+void func_us_801C5600(Primitive* prim)
 {
-    if ((g_Timer % arg0->u3) != 0) {
+    if ((g_Timer % prim->u3) != 0) {
         return;
     }
-    arg0->y0 -= 1;
+    prim->y0 -= 1;
 
-    if (arg0->y0 < arg0->u2 + 0x90) {
-        arg0->r0 -= 8;
-        arg0->g0 -= 4;
-        arg0->b0 -= 4;
+    if (prim->y0 < prim->u2 + 0x90) {
+        prim->r0 -= 8;
+        prim->g0 -= 4;
+        prim->b0 -= 4;
     }
-    if (arg0->r0 < 8) {
-        arg0->p3 = 0;
-        arg0->drawMode = 8;
+    if (prim->r0 < 8) {
+        prim->p3 = 0;
+        prim->drawMode = 8;
     }
 
 }
