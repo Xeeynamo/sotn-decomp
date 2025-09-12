8002886903b01782678d681d32e17c282b84ef14
Jonathan Hohle <jonhohle@gmail.com>
2025-09-11 22:49:16 -0700

    much of w0_000

--- src/weapon/80.c
+++ src/weapon/80.c
@@ -1,14 +1,50 @@
-#include "common.h"
+#include "weapon_private.h"
 
-INCLUDE_ASM("weapon/w0_000/nonmatchings/80", func_pspeu_092F2E80);
+#include "../destroy_entity.h"
 
-INCLUDE_ASM("weapon/w0_000/nonmatchings/80", func_pspeu_092F2F18);
+INCLUDE_ASM("weapon/w0_000/nonmatchings/80", LoadWeaponPalette);
 
-INCLUDE_ASM("weapon/w0_000/nonmatchings/80", func_pspeu_092F3000);
+extern s32 g_HandId;
 
-INCLUDE_ASM("weapon/w0_000/nonmatchings/80", func_pspeu_092F3048);
+static void SetSpriteBank1(SpriteParts* animset) {
+    SpriteParts** spriteBankDst = (SpriteParts**) g_api.o.spriteBanks;
 
-INCLUDE_ASM("weapon/w0_000/nonmatchings/80", EnttiyWeaponAttack);
+    spriteBankDst += 0x10;
+    if (g_HandId != 0) {
+        spriteBankDst += 2;
+    }
+    *spriteBankDst = animset;
+}
+
+static void SetWeaponProperties(Entity* self, s32 kind) {
+    Equipment equip;
+    s32 equipId;
+
+    equipId = self->ext.weapon.equipId;
+    g_api.GetEquipProperties(g_HandId, &equip, equipId);
+    switch (kind) {
+    case 0:
+    case 1:
+        self->attack = equip.attack;
+        self->attackElement = equip.element;
+        self->hitboxState = equip.hitType;
+        self->nFramesInvincibility = equip.enemyInvincibilityFrames;
+        self->stunFrames = equip.stunFrames;
+        self->hitEffect = equip.hitEffect;
+        self->entityRoomIndex = equip.criticalRate;
+        g_api.func_80118894(self);
+        break;
+    case 2:
+        self->attack = equip.attack;
+        break;
+    }
+    if (self->hitboxState == 4) {
+        self->attack = 0xFF;
+    }
+}
+
+
+INCLUDE_ASM("weapon/w0_000/nonmatchings/80", EntityWeaponAttack);
 
 void func_pspeu_092F3420(void) {
 }
@@ -25,7 +61,7 @@ void func_pspeu_092F3438(void) {
 void func_pspeu_092F3440(void) {
 }
 
-INCLUDE_ASM("weapon/w0_000/nonmatchings/80", func_pspeu_092F3448);
+INCLUDE_ASM("weapon/w0_000/nonmatchings/80", GetWeaponId);
 
 void func_pspeu_092F3458(void) {
 }
@@ -51,6 +87,13 @@ void func_pspeu_092F3488(void) {
 void func_pspeu_092F3490(void) {
 }
 
-INCLUDE_ASM("weapon/w0_000/nonmatchings/80", w0_000_Load);
+// TODO: this should be WEAPON0_PTR
+extern void D_8017A000[];
+extern Weapon w0_000_Overlay;
+
+void w0_000_Load(void) {
+    memcpy(&D_8017A000, &w0_000_Overlay, sizeof(Weapon));
+}
+
 
 // INCLUDE_RODATA("weapon/w0_000/nonmatchings/80", D_pspeu_092F4708);
