25d9efcf935bcd5f1aa4634ec732f46ed649633a
gamezter <gamezter93@gmail.com>
2025-09-25 09:47:26 -0400

    Fix test failure

--- src/main/psxsdk/libsnd/vmanager.c
+++ src/main/psxsdk/libsnd/vmanager.c
@@ -8,7 +8,11 @@ struct _ss_spu_vm_rec_struct {
 };
 
 static s16 _svm_sreg_buf[NUM_SPU_CHANNELS * 8];
-static unsigned char _svm_sreg_dirty[NUM_SPU_CHANNELS];
+#ifdef VERSION_PC
+char _svm_sreg_dirty[NUM_SPU_CHANNELS];
+#else
+static char _svm_sreg_dirty[NUM_SPU_CHANNELS];
+#endif
 static struct SpuVoice _svm_voice[NUM_SPU_CHANNELS];
 static s32 D_8003BD08;
 
