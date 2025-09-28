c1284f808baa5e0fec70ba16696324e9c1f0b928
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-09-29 01:25:03 +1000

    Decompile `cat` `e_lossoth` (#2887)
    `DrawNapalmEffect`
    PSX: https://decomp.me/scratch/uE3Bo
    PSP: https://decomp.me/scratch/XwKnm
    
    `EntityLossoth`
    PSX: https://decomp.me/scratch/viY4h
    PSP: https://decomp.me/scratch/CI3hJ
    
    `EntityLossothEffects`
    PSX: https://decomp.me/scratch/Ap1yN
    PSP: https://decomp.me/scratch/sMgzM
    
    `EntityLossothFireball`
    PSX: https://decomp.me/scratch/vd8Xd
    PSP: https://decomp.me/scratch/rT7Cx
    
    `EntityLossothNapalmFlare`
    PSX: https://decomp.me/scratch/0j4Bw
    PSP: https://decomp.me/scratch/HXOmm
    
    `EntityLossothFlames`
    PSX: https://decomp.me/scratch/aljfW
    PSP: https://decomp.me/scratch/NXkhz

--- include/entity.h
+++ include/entity.h
@@ -3512,6 +3512,38 @@ typedef struct {
     /* 0x85 */ u8 resetColliderEffects;
 } ET_GraveKeeper;
 
+typedef struct {
+    /* 0x7C */ struct Primitive* prim;
+    /* 0x80 */ s16 timer;
+    /* 0x82 */ u16 : 16;
+    /* 0x84 */ u32 : 32;
+    /* 0x88 */ u32 : 32;
+    /* 0x8C */ u32 : 32;
+    /* 0x90 */ u32 : 32;
+    /* 0x94 */ u32 : 32;
+    /* 0x98 */ u32 : 32;
+    /* 0x9C */ struct Entity* unk9C;
+    /* 0xA0 */ struct Primitive* unkA0;
+    /* 0xA4 */ s16 unkA4;
+    /* 0xA6 */ u16 : 16;
+    /* 0xA8 */ struct Entity* lossothEntity;
+} ET_Lossoth;
+
+typedef struct {
+    /* 0x7C */ struct Primitive* prim;
+    /* 0x80 */ u32 : 32;
+    /* 0x84 */ u32 : 32;
+    /* 0x88 */ u32 : 32;
+    /* 0x8C */ u32 : 32;
+    /* 0x90 */ u32 : 32;
+    /* 0x94 */ u32 : 32;
+    /* 0x98 */ u32 : 32;
+    /* 0x9C */ struct Primitive* unk9C;
+    /* 0xA0 */ f32 unkA0;
+    /* 0xA4 */ u8 paletteOffset;
+    /* 0xA5 */ u8 unkA5;
+} ET_LossothNapalmFlare;
+
 typedef union { // offset=0x7C
     struct Primitive* prim;
     ET_Placeholder ILLEGAL;
@@ -3816,6 +3848,8 @@ typedef union { // offset=0x7C
     ET_StainedGlass stainedGlass;
     ET_DiscusLord discusLord;
     ET_GraveKeeper graveKeeper;
+    ET_Lossoth lossoth;
+    ET_LossothNapalmFlare lossothNapalm;
 } Ext;
 
 #define SYNC_FIELD(struct1, struct2, field)                                    \
