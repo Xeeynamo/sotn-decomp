4caca931608a8554a096cd5d51d83a6214c18e87
ProjectOblivion <ProjectOblivion>
2025-10-03 20:23:01 +0000

    Merged pspeu ib, no2, and no4 e_stage_name with us

--- src/st/no4_psp/cutscene_data.c
+++ src/st/no4_psp/cutscene_data.c
@@ -4,42 +4,42 @@
 
 // Played when the ferryman takes the player right across the river
 u8 cutscene_data_ferryman_right_it[] = {
-#include "gen/cutscene_data_ferryman_right_it.h"
+#include "../no4/gen/cutscene_data_ferryman_right_it.h"
 };
 
 u8 cutscene_data_ferryman_right_sp[] = {
-#include "gen/cutscene_data_ferryman_right_sp.h"
+#include "../no4/gen/cutscene_data_ferryman_right_sp.h"
 };
 
 u8 cutscene_data_ferryman_right_fr[] = {
-#include "gen/cutscene_data_ferryman_right_fr.h"
+#include "../no4/gen/cutscene_data_ferryman_right_fr.h"
 };
 
 u8 cutscene_data_ferryman_right_ge[] = {
-#include "gen/cutscene_data_ferryman_right_ge.h"
+#include "../no4/gen/cutscene_data_ferryman_right_ge.h"
 };
 
 u8 cutscene_data_ferryman_right_en[] = {
-#include "gen/cutscene_data_ferryman_right_en.h"
+#include "../no4/gen/cutscene_data_ferryman_right_en.h"
 };
 
 // Played when the ferryman takes the player left across the river
 u8 cutscene_data_ferryman_left_it[] = {
-#include "gen/cutscene_data_ferryman_left_it.h"
+#include "../no4/gen/cutscene_data_ferryman_left_it.h"
 };
 
 u8 cutscene_data_ferryman_left_sp[] = {
-#include "gen/cutscene_data_ferryman_left_sp.h"
+#include "../no4/gen/cutscene_data_ferryman_left_sp.h"
 };
 
 u8 cutscene_data_ferryman_left_fr[] = {
-#include "gen/cutscene_data_ferryman_left_fr.h"
+#include "../no4/gen/cutscene_data_ferryman_left_fr.h"
 };
 
 u8 cutscene_data_ferryman_left_ge[] = {
-#include "gen/cutscene_data_ferryman_left_ge.h"
+#include "../no4/gen/cutscene_data_ferryman_left_ge.h"
 };
 
 u8 cutscene_data_ferryman_left_en[] = {
-#include "gen/cutscene_data_ferryman_left_en.h"
+#include "../no4/gen/cutscene_data_ferryman_left_en.h"
 };
