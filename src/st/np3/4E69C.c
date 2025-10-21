82125150a2d327cb0ff2c5e70ddc0b307b6770a9
SynaMax <djsynamax@gmail.com>
2025-10-21 20:02:51 +0000

    Hammer

--- src/st/np3/4E69C.c
+++ src/st/np3/4E69C.c
@@ -16,7 +16,7 @@ void EntityHammer(Entity* self) {
         func_801CE1E8(12);
     }
     if ((self->flags & FLAG_DEAD) && (self->step < 24)) {
-        PlaySfxPositional(0x745);
+        PlaySfxPositional(SFX_HAMMER_DEATH);
         func_801CE1E8(24);
     }
     switch (self->step) {
@@ -196,7 +196,7 @@ void EntityHammer(Entity* self) {
             polarPlacePartsList(D_80182A20);
             if ((self->ext.GH_Props.unkB0[0] == 0) &&
                 (self->ext.GH_Props.unkB4[0] == 0)) {
-                PlaySfxPositional(0x743);
+                PlaySfxPositional(SFX_HAMMER_ATTACK);
                 self->step_s++;
             }
             break;
@@ -269,7 +269,7 @@ void EntityHammer(Entity* self) {
         break;
     case 12:
         if (self->step_s == 0) {
-            PlaySfxPositional(0x744);
+            PlaySfxPositional(SFX_HAMMER_PAIN);
             self->step_s++;
         }
         if (self->ext.GH_Props.unk84 == 1) {
