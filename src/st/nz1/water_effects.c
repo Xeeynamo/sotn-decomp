f685b72df4ff6f4ebc4ed989cb734f1ed2b02a66
Jonathan Hohle <jonhohle@gmail.com>
2025-10-28 10:25:43 -0700

    Import NZ1 for US and PSPEU (#2936)
    The `make-config` output for PSPEU was off by 0x80 so most of the
    remaining symbols were manually mapped.

---
+++ src/st/nz1/water_effects.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <common.h>
+
+INCLUDE_ASM("st/nz1/nonmatchings/water_effects", func_801C4144);
+
+INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntityAlucardWaterEffect);
+
+INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntitySplashWater);
+
+INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntitySurfacingWater);
+
+INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntitySideWaterSplash);
+
+INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntitySmallWaterDrop);
+
+INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntityWaterDrop);
---
+++ src/st/nz1_psp/1DA88.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <common.h>
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", NZ1_EntityBreakableDebris);
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", func_pspeu_092554E8);
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", func_pspeu_09256C18);
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", func_pspeu_09256E08);
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", func_pspeu_09257108);
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", func_pspeu_09257610);
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", func_pspeu_09257860);
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", func_pspeu_09257A88);
+
+INCLUDE_ASM("st/nz1_psp/nonmatchings/nz1_psp/1DA88", func_pspeu_09257C20);
