073934fe2825e5fb73e0203f1c7af29b750b5fcf
gamezter <gamezter93@gmail.com>
2025-10-29 17:22:11 -0400

    Name more psp sdk functions

--- src/main_psp/29F0.c
+++ src/main_psp/29F0.c
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", func_psp_089012FC);
+INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", sceGuPatchDivide);
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", func_psp_08901328);
+INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", sceGuColorMaterial);
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", func_psp_08901350);
+INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", sceGuSpecular);
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", func_psp_0890137C);
+INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", sceGuTexScale);
 
 INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", func_psp_089013C0);
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", func_psp_0890142C);
+INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", sceGuTerm);
 
 INCLUDE_ASM("main_psp/nonmatchings/main_psp/29F0", func_psp_089014A4);
 
