99e3c5198323c2036abfcf62c919a03f737b07ec
gamezter <gamezter93@gmail.com>
2025-10-30 08:49:33 -0400

    Name more functions

--- src/main_psp/3030.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/3030", func_psp_0890193C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/3030", func_psp_08901990);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/3030", func_psp_089019E8);
