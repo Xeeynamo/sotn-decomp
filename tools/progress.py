55415b4ad9efd0b8514fa57706f8c7090d8c6583
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-11-01 17:42:36 +0000

    fix negative progress report on Discord

--- tools/progress.py
+++ tools/progress.py
@@ -383,6 +383,8 @@ def report_discord(progresses: dict[str, DecompProgressStats]):
             funcs_diff = funcs - (stat.functions_prev / stat.functions_total)
             data = stat.data_imported / stat.data_total
             data_diff = (stat.data_imported - stat.data_prev) / stat.data_total
+            if coverage == 1.0 and coverage_diff < 0 and data == 1.0 and data_diff < 0:
+                continue  # ignore flukes of negative reports with 100% progress on refactor commits
 
             report += f"**{overlay.upper()} ({args.version})**:"
             if stat.code_matching != stat.code_matching_prev:
