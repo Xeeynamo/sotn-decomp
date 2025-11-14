05e936f5073e61f0f2801e1040d41b4b710176c9
gamezter <charronalexandre@hotmail.com>
2025-11-13 19:31:09 -0500

    Decompile more functions in 11320.c (#2997)

--- src/main_psp/11320.c
+++ src/main_psp/11320.c
@@ -15,6 +15,15 @@ typedef struct {
     s32 unk18;
 } Unk08B1F140;
 
+extern float D_psp_0893CCFC;
+extern float D_psp_0893CD00;
+extern float D_psp_0893CD04;
+extern float D_psp_0893CD08;
+extern float D_psp_0893CD0C;
+extern float D_psp_0893CD10;
+extern float D_psp_0893CD14;
+extern float D_psp_0893CD18;
+extern float D_psp_0893CD1C;
 extern u8 D_psp_089B6540[0x400];
 extern s32 D_psp_089B6940[2][0x100];
 extern u8 D_psp_089B7140[2][0x34000];
@@ -29,6 +38,10 @@ extern s32 D_psp_08B1F1A8;
 extern s32 D_psp_08B1F1AC;
 extern s32 D_psp_08B1F1B0;
 extern s32 D_psp_08B1F1B4;
+extern s32 D_psp_08B1F1B8;
+extern s32 D_psp_08B1F1BC;
+extern s32 D_psp_08B1F1C0;
+extern s32 D_psp_08B1F1D0[][0x20];
 extern s32 D_psp_08B41FC0;
 
 void func_psp_08910088(void);
@@ -332,218 +345,337 @@ void func_psp_08910A20(s32 arg0, s32 arg1) {
     *D_psp_08B1F19C++ = 0x9D000000 | ((arg0 >> 24) & 0xF) << 16 | arg1;
 }
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08910A80);
+s32 func_psp_08910A80(s32* arg0, u32 arg1, u32 arg2, s32 arg3, s32 arg4) {
+    s32* temp_s1;
+    s32* temp_s2;
+    s32* temp_v0;
+    s32* temp_a1;
+    s32 i;
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08910C74);
+    *D_psp_08B1F19C++ = 0x12000000 | arg4 & 0x00FFFFFF;
+    temp_s2 = func_psp_08910498(2);
+    *D_psp_08B1F19C++ = 0x04000000 | (arg3 << 16 | arg1);
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08910D28);
+    temp_s1 = func_psp_08910498(2);
+    temp_v0 = func_psp_08910498((arg2 * arg1) / 4);
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08910D44);
+    *temp_s1++ = 0x10000000 | ((u32)D_psp_08B1F19C & 0xFF000000) >> 8;
+    *temp_s1++ = 0x08000000 | (u32)D_psp_08B1F19C & 0x00FFFFFF;
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08910FD8);
+    temp_a1 = temp_v0;
+    for (i = (arg2 * arg1) / 4; i > 0; i--) {
+        *temp_a1++ = *arg0++;
+    }
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089111C0);
+    *temp_s2++ = 0x10000000 | ((u32)temp_v0 & 0xFF000000) >> 8;
+    *temp_s2++ = 0x01000000 | (u32)temp_v0 & 0x00FFFFFF;
+    return 2;
+}
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089113A8);
+s32 func_psp_08910C74(s32 arg0, s32 arg1, s32 arg3, s32 arg4, s32 arg5) {
+    *D_psp_08B1F19C++ = 0x12000000 | arg5 & 0x00FFFFFF;
+    *D_psp_08B1F19C++ = 0x10000000 | (arg0 & 0xFF000000) >> 8;
+    *D_psp_08B1F19C++ = 0x01000000 | arg0 & 0x00FFFFFF;
+    *D_psp_08B1F19C++ = 0x04000000 | (arg4 << 16 | arg1);
+    return 2;
+}
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891149C);
+void func_psp_08910D28(void) {
+    D_psp_08B1F1B8 = 0;
+    D_psp_08B1F1BC = 0;
+    D_psp_08B1F1C0 = 0;
+}
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089117F4);
+s32 func_psp_08910D44(s32 arg0, s32 arg1, s32 arg2) {
+    float sp24[3];
+    float sp18[3];
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08911990);
+    sp24[0] = D_psp_0893CCFC;
+    sp24[1] = D_psp_0893CD00;
+    sp24[2] = D_psp_0893CD04;
+    sp18[0] = D_psp_0893CD08;
+    sp18[1] = D_psp_0893CD0C;
+    sp18[2] = D_psp_0893CD10;
+    if ((D_psp_08B1F1B8 == arg0) && (D_psp_08B1F1BC == arg2)) {
+        arg0 = 0;
+    }
+    if (D_psp_08B1F1C0 == arg1) {
+        arg1 = 0;
+    }
+    if (arg1 != 0) {
+        *D_psp_08B1F19C++ = 0xC500FF01;
+        *D_psp_08B1F19C++ = 0xB0000000 | arg1 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ = 0xB1000000 | (arg1 & 0xFF000000) >> 8;
+        if (arg2 == 1) {
+            *D_psp_08B1F19C = 0xC4000010;
+        } else {
+            *D_psp_08B1F19C = 0xC4000001;
+        }
+        D_psp_08B1F19C++;
+        D_psp_08B1F1C0 = arg1;
+    }
+    if (arg0 != 0) {
+        *D_psp_08B1F19C++ = 0xC2000000;
+        *D_psp_08B1F19C++ = 0xC3000000 | *((u32*)&sp24[arg2]);
+        *D_psp_08B1F19C++ = 0xA0000000 | arg0 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ =
+            0xA8000000 | ((arg0 & 0xFF000000) >> 8 | *((u32*)&sp18[arg2]));
+        *D_psp_08B1F19C++ = 0xB8000900 | (8 - arg2);
+        D_psp_08B1F1B8 = arg0;
+        D_psp_08B1F1BC = arg2;
+    }
+    if (arg0 != 0) {
+        *D_psp_08B1F19C++ = 0xCB000000;
+        ResetGraph(1);
+    }
+    return arg1 | arg0;
+}
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08911AB8);
+s32 func_psp_08910FD8(
+    s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5) {
+    if (arg1 != 0) {
+        *D_psp_08B1F19C++ = 0xC500FF01;
+        *D_psp_08B1F19C++ = 0xB0000000 | arg1 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ = 0xB1000000 | (arg1 & 0xFF000000) >> 8;
+        *D_psp_08B1F19C++ = 0xC4000010;
+        D_psp_08B1F1C0 = arg1;
+    }
+    if (arg0 != 0) {
+        *D_psp_08B1F19C++ = 0xC2000000;
+        *D_psp_08B1F19C++ = 0xC3000000 | arg2;
+        *D_psp_08B1F19C++ = 0xA0000000 | arg0 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ = 0xA8000000 | ((arg0 & 0xFF000000) >> 8 | arg3);
+        *D_psp_08B1F19C++ = 0xB8000000 | arg5 << 8 | arg4;
+        D_psp_08B1F1B8 = arg0;
+        D_psp_08B1F1BC = arg2;
+    }
+    if (arg0 != 0) {
+        *D_psp_08B1F19C++ = 0xCB000000;
+        ResetGraph(1);
+    }
+    return arg1 | arg0;
+}
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08911B7C);
+s32 func_psp_089111C0(
+    s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5) {
+    if (arg1 != 0) {
+        *D_psp_08B1F19C++ = 0xC500FF03;
+        *D_psp_08B1F19C++ = 0xB0000000 | arg1 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ = 0xB1000000 | (arg1 & 0xFF000000) >> 8;
+        *D_psp_08B1F19C++ = 0xC4000010;
+        D_psp_08B1F1C0 = arg1;
+    }
+    if (arg0 != 0) {
+        *D_psp_08B1F19C++ = 0xC2000000;
+        *D_psp_08B1F19C++ = 0xC3000000 | arg2;
+        *D_psp_08B1F19C++ = 0xA0000000 | arg0 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ = 0xA8000000 | ((arg0 & 0xFF000000) >> 8 | arg3);
+        *D_psp_08B1F19C++ = 0xB8000000 | arg5 << 8 | arg4;
+        D_psp_08B1F1B8 = arg0;
+        D_psp_08B1F1BC = arg2;
+    }
+    if (arg0 != 0) {
+        *D_psp_08B1F19C++ = 0xCB000000;
+        ResetGraph(1);
+    }
+    return arg1 | arg0;
+}
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08911B84);
+void func_psp_089113A8(s32 arg0) {
+    switch (arg0) {
+    case 0:
+        func_psp_08910660(1);
+        func_psp_08910810(0, 0xA, 0xA, 0xFF808080, 0xFF808080);
+        break;
+    case 1:
+        func_psp_08910660(1);
+        func_psp_08910810(0, 0xA, 0xA, 0xFFFFFFFF, 0xFFFFFFFF);
+        break;
+    case 2:
+        func_psp_08910660(1);
+        func_psp_08910810(2, 0xA, 0xA, 0xFFFFFFFF, 0xFFFFFFFF);
+        break;
+    case 3:
+        func_psp_08910660(1);
+        func_psp_08910810(0, 0xA, 0xA, 0xFF404040, 0xFFFFFFFF);
+        break;
+    default:
+        func_psp_08910660(0);
+        break;
+    }
+}
+
+s32 func_psp_0891149C(s32 arg0, u32 arg1, u32 arg2, s32 arg3, s32 arg4) {
+    float sp14[3];
+    s32 var_a1_2;
+    s32 var_v0;
+    s32 var_v1_2;
+    s32 var_v0_2;
+
+    sp14[0] = D_psp_0893CD14;
+    sp14[1] = D_psp_0893CD18;
+    sp14[2] = D_psp_0893CD1C;
+    var_v0 = 0;
+    if ((arg3 != 0) && (D_psp_08B1F1C0 != arg3)) {
+        *D_psp_08B1F19C++ = 0xC500FF01;
+        *D_psp_08B1F19C++ = 0xB0000000 | arg3 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ = 0xB1000000 | (arg3 & 0xFF000000) >> 8;
+        if (arg4 == 1) {
+            *D_psp_08B1F19C = 0xC4000010;
+        } else {
+            *D_psp_08B1F19C = 0xC4000001;
+        }
+        D_psp_08B1F19C++;
+        D_psp_08B1F1C0 = arg3;
+    }
+    if ((arg0 != 0) && (D_psp_08B1F1B8 != arg0)) {
+        var_v1_2 = 0;
+        switch (arg4) {
+        case 0:
+            arg1 = (arg1 + 0x1F) & ~0x1F;
+            var_v1_2 = arg1;
+            break;
+        case 1:
+            arg1 = (arg1 + 0xF) & ~0xF;
+            var_v1_2 = arg1;
+            break;
+        case 2:
+            arg1 = (arg1 + 7) & ~7;
+            var_v1_2 = arg1;
+            break;
+        }
+
+        if (arg1 > 0x100) {
+            var_v0_2 = 9;
+        } else if (arg1 > 0x80) {
+            var_v0_2 = 8;
+        } else if (arg1 > 0x40) {
+            var_v0_2 = 7;
+        } else if (arg1 > 0x20) {
+            var_v0_2 = 6;
+        } else if (arg1 > 0x10) {
+            var_v0_2 = 5;
+        } else if (arg1 > 8) {
+            var_v0_2 = 4;
+        } else {
+            var_v0_2 = 3;
+        }
+
+        if (arg2 > 0x100) {
+            var_a1_2 = 9;
+        } else if (arg2 > 0x80) {
+            var_a1_2 = 8;
+        } else if (arg2 > 0x40) {
+            var_a1_2 = 7;
+        } else if (arg2 > 0x20) {
+            var_a1_2 = 6;
+        } else if (arg2 > 0x10) {
+            var_a1_2 = 5;
+        } else if (arg2 > 8) {
+            var_a1_2 = 4;
+        } else {
+            var_a1_2 = 3;
+        }
+        *D_psp_08B1F19C++ = 0xC2000000;
+        *D_psp_08B1F19C++ = 0xC3000000 | *((u32*)&sp14[arg4]);
+        *D_psp_08B1F19C++ = 0xA0000000 | arg0 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ = 0xA8000000 | ((arg0 & 0xFF000000) >> 8 | var_v1_2);
+        *D_psp_08B1F19C++ = 0xB8000000 | var_a1_2 << 8 | var_v0_2;
+        D_psp_08B1F1B8 = arg0;
+        D_psp_08B1F1BC = arg4;
+        var_v0 = 1;
+    }
+    if (var_v0 != 0) {
+        *D_psp_08B1F19C++ = 0xCB000000;
+        ResetGraph(1);
+    }
+    return 0;
+}
+
+void func_psp_089117F4(s32 mode, s32 sx, s32 sy, s32 height, s32 width, s32 sbw,
+                       s32 sbp, s32 dx, s32 dy, s32 dbw, s32 dbp) {
+    *D_psp_08B1F19C++ = 0xCC000000;
+    *D_psp_08B1F19C++ = 0xB2000000 | sbp & 0x00FFFFFF;
+    *D_psp_08B1F19C++ = 0xB3000000 | ((sbp >> 24) & 0xF) << 16 | sbw;
+    *D_psp_08B1F19C++ = 0xEB000000 | sy << 10 | sx;
+    *D_psp_08B1F19C++ = 0xB4000000 | dbp & 0x00FFFFFF;
+    *D_psp_08B1F19C++ = 0xB5000000 | ((dbp >> 24) & 0xF) << 16 | dbw;
+    *D_psp_08B1F19C++ = 0xEC000000 | dy << 10 | dx;
+    *D_psp_08B1F19C++ = 0xEE000000 | (width - 1) << 10 | (height - 1);
+    *D_psp_08B1F19C++ = 0xEA000000 | (mode == 3);
+    *D_psp_08B1F19C++ = 0xCC000000;
+}
+
+s32 func_psp_08911990(s32 arg0, s32 arg1) {
+
+    s32 var_v0 = (arg1 == 0x100) ? 8 : 9;
+
+    *D_psp_08B1F19C++ = 0xC2000000;
+    *D_psp_08B1F19C++ = 0xC3000001;
+    *D_psp_08B1F19C++ = 0xA0000000 | arg0 & 0x00FFFFFF;
+    *D_psp_08B1F19C++ = 0xA8000000 | ((arg0 & 0xFF000000) >> 8 | arg1);
+    *D_psp_08B1F19C++ = 0xB8000900 | var_v0;
+    *D_psp_08B1F19C++ = 0xCB000000;
+    ResetGraph(1);
+    D_psp_08B1F1B8 = 0;
+    D_psp_08B1F1BC = 0;
+    D_psp_08B1F1C0 = 0;
+    return 0;
+}
+
+void func_psp_08911AB8(u8 arg0, u8 arg1, u8 arg2) {
+    func_psp_08910778(1);
+    *D_psp_08B1F19C++ = 0xD9000000 | arg2 << 16 | arg1 << 8 | arg0;
+    *D_psp_08B1F19C++ = 0xDAFFFFFF;
+    *D_psp_08B1F19C++ = 0xD8000003;
+}
+
+void func_psp_08911B7C(void) { func_psp_08910778(0); }
+
+void func_psp_08911B84(
+    s32 arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5) {
+    *D_psp_08B1F19C++ = 0x21000000 | arg0;
+    if (arg0 == 1) {
+        *D_psp_08B1F19C++ = 0xDF000000 | arg1 << 8 | arg3 << 4 | arg2;
+        *D_psp_08B1F19C++ = 0xE0000000 | arg4 & 0x00FFFFFF;
+        *D_psp_08B1F19C++ = 0xE1000000 | arg5 & 0x00FFFFFF;
+    }
+}
 
 INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08911C3C);
 
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08911F24);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08911FA0);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08912008);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08912070);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089120E4);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891214C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089121BC);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", DVDUMDSampleStart);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08912398);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089123B8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891249C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08912530);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089125F8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08912608);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08912640);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891269C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891273C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891274C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891275C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089127D8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089127E8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08912814);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08912820);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089128C4);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089132C8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913314);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089133D4);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913A04);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913B48);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913B98);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913C24);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913C58);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913CA0);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913CF0);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913D20);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913DD4);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913E60);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08913F5C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089140DC);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089144BC);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914638);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914854);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891490C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891499C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914A18);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914AB8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914AE8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914B08);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914B58);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914BA4);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914BF4);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914C20);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", dispbuf_func);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914E44);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914E74);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914EE0);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914F18);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914FA8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08914FFC);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891504C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915070);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915088);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915228);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915250);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891527C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915294);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915324);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915340);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891535C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891539C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915454);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915470);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", read_func);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915C70);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915F5C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08915FD0);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916094);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916110);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916188);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089161D8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916278);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089162A8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089162C8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089162F0);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916310);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", soundbuf_func);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916550);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916630);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916724);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916830);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891689C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_089168E8);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_0891692C);
-
-INCLUDE_ASM("main_psp/nonmatchings/main_psp/11320", func_psp_08916984);
+void func_psp_08911F24(s32 arg0, s32 arg1) {
+    *D_psp_08B1F19C++ =
+        0x10000000 |
+        ((u32)D_psp_08B1F1D0[arg1 + ((arg0 * 2) + 5)] & 0xFF000000) >> 8;
+    *D_psp_08B1F19C++ =
+        0x0A000000 | (u32)D_psp_08B1F1D0[arg1 + ((arg0 * 2) + 5)] & 0x00FFFFFF;
+}
+
+void func_psp_08911FA0(void) {
+    *D_psp_08B1F19C++ = 0x10000000 | ((u32)D_psp_08B1F1D0[9] & 0xFF000000) >> 8;
+    *D_psp_08B1F19C++ = 0x0A000000 | (u32)D_psp_08B1F1D0[9] & 0x00FFFFFF;
+}
+
+void func_psp_08912008(void) {
+    *D_psp_08B1F19C++ =
+        0x10000000 | ((u32)D_psp_08B1F1D0[10] & 0xFF000000) >> 8;
+    *D_psp_08B1F19C++ = 0x0A000000 | (u32)D_psp_08B1F1D0[10] & 0x00FFFFFF;
+}
+
+void func_psp_08912070(s32 arg0) {
+    *D_psp_08B1F19C++ =
+        0x10000000 | ((u32)D_psp_08B1F1D0[arg0 + 2] & 0xFF000000) >> 8;
+    *D_psp_08B1F19C++ = 0x0A000000 | (u32)D_psp_08B1F1D0[arg0 + 2] & 0x00FFFFFF;
+}
+
+void func_psp_089120E4(void) {
+    *D_psp_08B1F19C++ = 0x10000000 | ((u32)D_psp_08B1F1D0[4] & 0xFF000000) >> 8;
+    *D_psp_08B1F19C++ = 0x0A000000 | (u32)D_psp_08B1F1D0[4] & 0x00FFFFFF;
+}
+
+void func_psp_0891214C(s32 arg0) {
+    *D_psp_08B1F19C++ =
+        0x10000000 | ((u32)D_psp_08B1F1D0[arg0] & 0xFF000000) >> 8;
+    *D_psp_08B1F19C++ = 0x0A000000 | (u32)D_psp_08B1F1D0[arg0] & 0x00FFFFFF;
+}
