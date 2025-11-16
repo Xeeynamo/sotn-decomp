0538e3c6760978615146b25118a10abb1efc9d3e
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2025-11-16 18:09:25 +0000

    Reinforce sotn-assets CLI parser (#3003)
    Replace the custom CLI args parser with the well-established spf13/cobra
    module.
    
    This PR has no practical functional changes, other than a better
    `--help` handling and validation of the CLI arguments. But it is an
    important step to continue expanding the tool without keep feeding the
    previous hack-y solution.

--- tools/sotn-assets/go.mod
+++ tools/sotn-assets/go.mod
@@ -4,6 +4,7 @@ go 1.24
 
 require (
 	github.com/goccy/go-yaml v1.18.0
+	github.com/spf13/cobra v1.10.1
 	github.com/stretchr/testify v1.9.0
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b
 	golang.org/x/sync v0.15.0
@@ -13,5 +14,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
 )
--- tools/sotn-assets/objdiffbin.go
+++ tools/sotn-assets/objdiffbin.go
@@ -2,44 +2,31 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/xeeynamo/sotn-decomp/tools/sotn-assets/deps"
 )
 
-func handleObjdiffGUI(_ []string) error {
-	if os.Getenv("VERSION") == "" {
-		return fmt.Errorf("VERSION not set")
-	}
-	configPath := fmt.Sprintf("config/assets.%s.yaml", os.Getenv("VERSION"))
+func handleObjdiffGUI(version string) error {
+	configPath := fmt.Sprintf("config/assets.%s.yaml", version)
 	if err := objdiffgen(configPath); err != nil {
 		return err
 	}
 	return deps.ExecObjdiffGUI("--project-dir", ".")
 }
 
-func handlerObjdiffCLI(args []string) error {
-	if os.Getenv("VERSION") == "" {
-		return fmt.Errorf("VERSION not set")
-	}
-	configPath := fmt.Sprintf("config/assets.%s.yaml", os.Getenv("VERSION"))
-	if err := objdiffgen(configPath); err != nil {
+func handlerObjdiffCLI(version, srcPath, symbol string) error {
+	if err := objdiffgen(version); err != nil {
 		return err
 	}
-	if len(args) < 2 {
-		return fmt.Errorf("usage: sotn-assets objdiff <src_path> <symbol>\n\t(e.g. objdiff dra/4A538 func_800EAD0C)")
-	}
-	srcPath := args[0]
-	symbol := args[1]
 	if !strings.HasSuffix(srcPath, ".c") {
 		srcPath += ".c"
 	}
 	if !strings.HasPrefix(srcPath, "src/") {
 		srcPath = "src/" + srcPath
 	}
-	basePath := filepath.Join("build", "us", srcPath+".o")
+	basePath := filepath.Join("build", version, srcPath+".o")
 	targetPath := filepath.Join("expected", basePath)
 	return deps.ExecObjdiffCLI("diff", "-1", basePath, "-2", targetPath, symbol)
 }
