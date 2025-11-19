0dee7ceda1cd38b089a530c957462791229a4d94
SynaMax <djsynamax@gmail.com>
2025-11-19 02:28:27 +0000

    Succubus, Dracula, Maria

--- include/sfx.h
+++ include/sfx.h
@@ -260,23 +260,6 @@ enum {
 // Unknown SFX related to the priest
 #define SFX_UNK_4E5 0x4E5
 
-// STAGE DRE
-#define NA_VO_SU_LAUGH 0x86E
-// Blank, may be a leftover from the Jap version.
-#define NA_VO_SU_BLANK 0x86F
-#define NA_VO_SU_GRUNT_1 0x870
-#define NA_VO_SU_GRUNT_2 0x872
-#define NA_VO_SU_GRUNT_3 0x874
-#define NA_VO_SU_HURT_1 0x879
-#define NA_VO_SU_HURT_2 0x87A
-#define NA_VO_SU_CRYSTAL_1 0x87C
-#define NA_VO_SU_SUCK_YOU_DRY 0x876
-#define NA_VO_SU_NO_SCREAM 0x87B
-#define NA_VO_SU_DELICIOUS 0x8D1
-
-// UNUSED SOUNDS
-#define NA_VO_MAR_AIM_HEAD 0x87F
-
 // The VAB IDs appear in large chunks so all sounds proceeding
 // a vabid label comment will belong in that VAB group unless noted.
 //
@@ -1001,10 +984,36 @@ enum Sfx {
     /* 0x86B */ SFX_DRACULA_BEHOLD_MY_TRUE_FORM, // "...and despair!"
     /* 0x86C */ SFX_UNUSED_86C,
     /* 0x86D */ SFX_UNUSED_86D,
+    /* 0x86E */ SFX_SUCCUBUS_LAUGH,   // used for taunt and petal attack
+    /* 0x86F */ SFX_UNK_SUCCUBUS_86F, // muted, related to charge attack
+    /* 0x870 */ SFX_SUCCUBUS_DUPLICATES,
+    /* 0x871 */ SFX_UNUSED_SUCCUBUS_ATTACK_A,
+    /* 0x872 */ SFX_SUCCUBUS_HOMING_ORB_ATTACK,
+    /* 0x873 */ SFX_UNUSED_SUCCUBUS_ATTACK_B,
+    /* 0x874 */ SFX_SUCCUBUS_WING_ATTACK,
+    /* 0x875 */ SFX_UNUSED_SUCCUBUS_AH,
+    /* 0x876 */ SFX_SUCCUBUS_ILL_SUCK_YOU_DRY,
+    /* 0x877 */ SFX_UNUSED_SUCCUBUS_OH_A,
+    /* 0x878 */ SFX_UNUSED_SUCCUBUS_OH_B, // duplicate of 0x877
+    /* 0x879 */ SFX_SUCCUBUS_PAIN_A,
+    /* 0x87A */ SFX_SUCCUBUS_PAIN_B,
+    /* 0x87B */ SFX_SUCCUBUS_DEFEAT, // "Nooooo!"
+    /* 0x87C */ SFX_SUCCUBUS_HOMING_ORB,
+
+    // vabid 2
+    /* 0x87D */ SFX_MARIA_RICHTER, // "Richter!"
+    /* 0x87E */ SFX_MARIA_GRANT_HIM_YOUR_STRENGTH,
+    /* 0x87F */ SFX_UNUSED_MARIA_AIM_FOR_HIS_HEAD,
+    /* 0x880 */ SFX_DRACULA_MONSTER_SILHOUETTE_APPEAR,
+    /* 0x881 */ SFX_MARIA_ANIMALS_APPEAR,
+    /* 0x882 */ SFX_MARIA_FOOTSTEPS_A,
+    /* 0x883 */ SFX_MARIA_FOOTSTEPS_B,
+    /* 0x884 */ SFX_UNUSED_884,
     SFX_TELEPORT_SYNTH_UP = 0x8BA,
     SFX_TELEPORT_SYNTH_DOWN,
     SFX_BURNING_PHOTOGRAPH = 0x8BE,
     SFX_UI_NAME_ENTRY = 0x8CD,
+    NA_VO_SU_DELICIOUS = 0x8D1,
 
     // Exclusive to Maria PSP
     /* 0x8E6 */ SFX_VO_MAR_8E6 = 0x8E6,
