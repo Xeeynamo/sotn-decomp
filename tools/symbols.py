47296e77166034fa9c7b811851333fc206f29638
ProjectOblivion <ProjectOblivion>
2025-11-22 18:19:52 +0000

    Added _binary_assets to excluded_starts

--- tools/symbols.py
+++ tools/symbols.py
@@ -438,7 +438,7 @@ def clean(config_files):
 
 
 def get_symbols(file_path, excluded_starts=[], excluded_ends=[], excluded_comments=[]):
-    excluded_starts = {"LM", "__pad"} | set(excluded_starts)
+    excluded_starts = {"LM", "__pad", "_binary_assets"} | set(excluded_starts)
     excluded_ends = {"_START", "_END", "_VRAM", "_s", "_c"} | set(excluded_ends)
     file_path = Path(file_path)
     match file_path.suffix:
