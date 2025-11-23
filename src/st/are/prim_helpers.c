3d96c00ff9966b28b2f2257f0f0fa40e534c46aa
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-11-23 12:16:45 +1100

    Dedupe `prim_helpers`

--- src/st/are/prim_helpers.c
+++ src/st/are/prim_helpers.c
@@ -1,22 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "are.h"
 
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", UnkPrimHelper);
-
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", UpdateAnimation);
-
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", FindFirstUnkPrim);
-
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", FindFirstUnkPrim2);
-
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", PrimToggleVisibility);
-
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", PrimResetNext);
-
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", UnkPolyFunc2);
-
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", UnkPolyFunc0);
-
-INCLUDE_ASM("st/are/nonmatchings/prim_helpers", PrimDecreaseBrightness);
-
-INCLUDE_RODATA("st/are/nonmatchings/prim_helpers", g_UnkPrimHelperRot);
+#include "../prim_helpers.h"
