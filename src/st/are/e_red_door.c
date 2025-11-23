94148aa931dfd12f980428ef90a8eb17bc3d7a4b
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-11-23 12:09:57 +1100

    Dedupe `e_red_door`

--- src/st/are/e_red_door.c
+++ src/st/are/e_red_door.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "are.h"
 
-INCLUDE_ASM("st/are/nonmatchings/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/are/nonmatchings/e_red_door", ARE_EntityRedDoor);
+#include "../e_red_door.h"
--- src/st/are_psp/e_red_door.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../are/are.h"
-
-INCLUDE_ASM("st/are_psp/nonmatchings/are_psp/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/are_psp/nonmatchings/are_psp/e_red_door", ARE_EntityRedDoor);
