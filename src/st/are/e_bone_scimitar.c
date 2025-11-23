962ff835d51312132bc0a298469e7f5b5f82fe87
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-11-23 14:38:52 +1100

    Dedupe `e_bone_scimitar`

--- src/st/are/e_bone_scimitar.c
+++ src/st/are/e_bone_scimitar.c
@@ -1,8 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "are.h"
 
-INCLUDE_ASM("st/are/nonmatchings/e_bone_scimitar", BoneScimitarAttackCheck);
-
-INCLUDE_ASM("st/are/nonmatchings/e_bone_scimitar", EntityBoneScimitar);
-
-INCLUDE_ASM("st/are/nonmatchings/e_bone_scimitar", EntityBoneScimitarParts);
+#include "../e_bone_scimitar.h"
--- src/st/are_psp/e_bone_scimitar.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../are/are.h"
-
-INCLUDE_ASM("st/are_psp/nonmatchings/are_psp/e_bone_scimitar", BoneScimitarAttackCheck);
-
-INCLUDE_ASM("st/are_psp/nonmatchings/are_psp/e_bone_scimitar", EntityBoneScimitar);
-
-INCLUDE_ASM("st/are_psp/nonmatchings/are_psp/e_bone_scimitar", EntityBoneScimitarParts);
