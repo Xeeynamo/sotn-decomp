4fb6cdd1b22ba8d02f9432cc2bbb1480df375843
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-11-23 13:40:46 +1100

    Dedupe `e_mist_door`

--- src/st/are_psp/e_mist_door.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../are/are.h"
-
-INCLUDE_ASM("st/are_psp/nonmatchings/are_psp/e_mist_door", GetLang);
-
-INCLUDE_ASM("st/are_psp/nonmatchings/are_psp/e_mist_door", EntityMistDoor);
