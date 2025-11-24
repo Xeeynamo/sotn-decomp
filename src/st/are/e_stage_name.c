0d641048a8551fe7c0ef9ca3bcb52f7e41916734
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-11-24 20:10:55 +1100

    Dedupe `are` `e_stage_name`

--- src/st/are/e_stage_name.c
+++ src/st/are/e_stage_name.c
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "are.h"
 
-INCLUDE_ASM("st/are/nonmatchings/e_stage_name", StageNamePopupDissolver);
+#define STAGE_FLAG OVL_EXPORT(STAGE_FLAG)
 
-INCLUDE_ASM("st/are/nonmatchings/e_stage_name", EntityStageNamePopup);
+#ifdef VERSION_US
+#include "../e_stage_name_us.h"
+#endif
+
+#ifdef VERSION_PSP
+static u8 stage_name_en[] = {
+#include "gen/stage_name_en.h"
+};
+static u8 stage_name_it[] = {
+#include "gen/stage_name_it.h"
+};
+static u8 stage_name_sp[] = {
+#include "gen/stage_name_sp.h"
+};
+// nb. For some reason this is ordered differently to other overlays
+static u8 stage_name_fr[] = {
+#include "gen/stage_name_fr.h"
+};
+static u8 stage_name_ge[] = {
+#include "gen/stage_name_ge.h"
+};
+
+#define STAGE_NAME_LOAD_GFX
+#include "../e_stage_name_jp.h"
+#endif
