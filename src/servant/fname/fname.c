eab3b84f5cf029374af3964823c6e7c446ea8dab
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-12-06 21:05:52 +0000

    Add fname overlay

---
+++ src/servant/fname/fname.c
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <servant.h>
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170714);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170780);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_801707C8);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170810);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170838);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_8017085C);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_801708A0);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170914);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170B64);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170BCC);
+
+void func_80170CF8(void) {}
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170D00);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170DA4);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80170EA4);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80171038);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80171178);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_801716C4);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80171780);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_801718F0);
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80172204);
+
+void func_801728D4(void) {}
+
+void func_801728DC(void) {}
+
+void func_801728E4(void) {}
+
+void func_801728EC(void) {}
+
+void func_801728F4(void) {}
+
+void func_801728FC(void) {}
+
+void func_80172904(void) {}
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_8017290C);
+
+void func_80173144(void) {}
+
+void func_8017314C(void) {}
+
+void func_80173154(void) {}
+
+void func_8017315C(void) {}
+
+INCLUDE_ASM("servant/fname/nonmatchings/fname", func_80173164);
