838e601fc4b565560bc63a192d77289ad57b02f7
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-12-18 14:00:21 +1100

    Import most of `are` PSX data (#3070)

---
+++ src/st/are/gfx_data.c
@@ -0,0 +1,189 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "are.h"
+
+u8 gfx_stage_name_jp_sm[] = {
+#include "gen/gfx_stage_name_jp_sm.h"
+};
+
+u8 gfx_stage_name_jp_lg[] = {
+#include "gen/gfx_stage_name_jp_lg.h"
+};
+
+u8 gfx_cavern_door[] = {
+#include "gen/gfx_cavern_door.h"
+};
+
+u8 gfx_stage_name_en[] = {
+#include "gen/gfx_stage_name_en.h"
+};
+
+u8 gfx_blade_master_1[] = {
+#include "gen/gfx_blade_master_1.h"
+};
+
+u8 gfx_blade_master_2[] = {
+#include "gen/gfx_blade_master_2.h"
+};
+
+u8 gfx_blade_master_3[] = {
+#include "gen/gfx_blade_master_3.h"
+};
+
+u8 gfx_blade_soldier[] = {
+#include "gen/gfx_blade_soldier.h"
+};
+
+u8 gfx_bone_musket[] = {
+#include "gen/gfx_bone_musket.h"
+};
+
+u8 gfx_owl_knight_owl[] = {
+#include "gen/gfx_owl_knight_owl.h"
+};
+
+u8 gfx_owl_knight_1[] = {
+#include "gen/gfx_owl_knight_1.h"
+};
+
+u8 gfx_owl_knight_2[] = {
+#include "gen/gfx_owl_knight_2.h"
+};
+
+u8 gfx_valhalla_knight_1[] = {
+#include "gen/gfx_valhalla_knight_1.h"
+};
+
+u8 gfx_valhalla_knight_2[] = {
+#include "gen/gfx_valhalla_knight_2.h"
+};
+
+u8 gfx_valhalla_knight_3[] = {
+#include "gen/gfx_valhalla_knight_3.h"
+};
+
+u8 gfx_axe_knight_1[] = {
+#include "gen/gfx_axe_knight_1.h"
+};
+
+u8 gfx_axe_knight_2[] = {
+#include "gen/gfx_axe_knight_2.h"
+};
+
+u8 gfx_armor_lord_1[] = {
+#include "gen/gfx_armor_lord_1.h"
+};
+
+u8 gfx_armor_lord_2[] = {
+#include "gen/gfx_armor_lord_2.h"
+};
+
+u8 gfx_armor_lord_3[] = {
+#include "gen/gfx_armor_lord_3.h"
+};
+
+u8 gfx_armor_lord_4[] = {
+#include "gen/gfx_armor_lord_4.h"
+};
+
+u8 gfx_hunting_girl[] = {
+#include "gen/gfx_hunting_girl.h"
+};
+
+u8 gfx_paranthropus_1[] = {
+#include "gen/gfx_paranthropus_1.h"
+};
+
+u8 gfx_paranthropus_2[] = {
+#include "gen/gfx_paranthropus_2.h"
+};
+
+u8 gfx_flea_man_armor[] = {
+#include "gen/gfx_flea_man_armor.h"
+};
+
+u8 gfx_bone_scimitar[] = {
+#include "gen/gfx_bone_scimitar.h"
+};
+
+u8 e_explosion_unk_1[] = {
+#include "gen/e_explosion_unk_1.h"
+};
+
+u8 e_explosion_unk_2[] = {
+#include "gen/e_explosion_unk_2.h"
+};
+
+u8 gfx_plate_lord[] = {
+#include "gen/gfx_plate_lord.h"
+};
+
+u8 gfx_grave_keeper[] = {
+#include "gen/gfx_grave_keeper.h"
+};
+
+u8 gfx_grave_keeper_tombstone[] = {
+#include "gen/gfx_grave_keeper_tombstone.h"
+};
+
+// Palettes
+// These can't be imported correctly until #2401 is fixed
+
+// u16 pal_puff_opaque[] = {
+// #include "gen/pal_puff_opaque.h"
+// };
+
+// u16 pal_cavern_door[] = {
+// #include "gen/pal_cavern_door.h"
+// };
+
+// u16 pal_blade_master[] = {
+// #include "gen/pal_blade_master.h"
+// };
+
+// u16 pal_blade_soldier[] = {
+// #include "gen/pal_blade_soldier.h"
+// };
+
+// u16 pal_bone_musket[] = {
+// #include "gen/pal_bone_musket.h"
+// };
+
+// u16 pal_owl_knight[] = {
+// #include "gen/pal_owl_knight.h"
+// };
+
+// u16 pal_valhalla_knight[] = {
+// #include "gen/pal_valhalla_knight.h"
+// };
+
+// u16 pal_armor_lord[] = {
+// #include "gen/pal_armor_lord.h"
+// };
+
+// u16 pal_blue_axe_knight[] = {
+// #include "gen/pal_blue_axe_knight.h"
+// };
+
+// u16 pal_hunting_girl[] = {
+// #include "gen/pal_hunting_girl.h"
+// };
+
+// u16 pal_paranthropus[] = {
+// #include "gen/pal_paranthropus.h"
+// };
+
+// u16 pal_flea_man_armor[] = {
+// #include "gen/pal_flea_man_armor.h"
+// };
+
+// u16 pal_bone_scimitar[] = {
+// #include "gen/pal_bone_scimitar.h"
+// };
+
+// u16 pal_plate_lord[] = {
+// #include "gen/pal_plate_lord.h"
+// };
+
+// u16 pal_grave_keeper_tombstone[] = {
+// #include "gen/pal_grave_keeper_tombstone.h"
+// };
--- src/st/top/e_blue_axe_knight.c
+++ src/st/top/e_blue_axe_knight.c
@@ -1,3 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "top.h"
+
+#define SPRITE_BANK sprites_3
 #include "../e_blue_axe_knight.h"
