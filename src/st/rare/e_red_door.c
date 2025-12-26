30605a6a04be11db0575353bf8a89b0af0241cbc
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-12-27 10:31:19 +1100

    Dedupe `e_red_door`

--- src/st/rare/e_red_door.c
+++ src/st/rare/e_red_door.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rare.h"
 
-INCLUDE_ASM("st/rare/nonmatchings/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_red_door", RARE_EntityRedDoor);
+#include "../e_red_door.h"
