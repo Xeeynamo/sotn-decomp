e1c00e3dd1954e9dc5c211f0c8c89f27ea68f747
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-12-27 09:36:10 +1100

    Dedupe `collision`

--- src/st/rare/collision.c
+++ src/st/rare/collision.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rare.h"
 
-INCLUDE_ASM("st/rare/nonmatchings/collision", HitDetection);
-
-INCLUDE_ASM("st/rare/nonmatchings/collision", EntityDamageDisplay);
+#include "../collision.h"
--- src/st/rare_psp/collision.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rare/rare.h"
-
-INCLUDE_ASM("st/rare_psp/nonmatchings/rare_psp/collision", HitDetection);
-
-INCLUDE_ASM("st/rare_psp/nonmatchings/rare_psp/collision", EntityDamageDisplay);
