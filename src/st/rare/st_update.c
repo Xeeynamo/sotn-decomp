6b83dba4c4f3e0723bb4a8c909ee0ed5aa2395d1
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-12-27 17:19:48 +1100

    Dedupe `st_update`

--- src/st/rare/st_update.c
+++ src/st/rare/st_update.c
@@ -1,8 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rare.h"
 
-INCLUDE_ASM("st/rare/nonmatchings/st_update", Random);
-
-INCLUDE_ASM("st/rare/nonmatchings/st_update", Update);
-
-INCLUDE_ASM("st/rare/nonmatchings/st_update", UpdateStageEntities);
+#include "../st_update.h"
