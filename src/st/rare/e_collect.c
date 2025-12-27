262e5b540d34ea3eea913fb0d23561806c575558
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2025-12-27 17:31:39 +1100

    Dedupe `e_collect`

--- src/st/rare/e_collect.c
+++ src/st/rare/e_collect.c
@@ -1,56 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rare.h"
 
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", PrizeDropFall);
+#define HEART_DROP_CASTLE_FLAG 0x278
 
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", PrizeDropFall2);
+#ifdef VERSION_PSP
+#define GOLD_COLLECT_TEXT                                                      \
+    _S("$1"), _S("$25"), _S("$50"), _S("$100"), _S("$250"), _S("$400"),        \
+        _S("$700"), _S("$1000"), _S("$2000"), _S("$5000"),
+#endif
 
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", CollectHeart);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", CollectGold);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", CollectSubweapon);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", CollectHeartVessel);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", CollectLifeVessel);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", CollectDummy);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A52D8);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A52E0);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A52E8);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A52F0);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A52F8);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A5300);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A5308);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A5310);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A5318);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A5320);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", EntityPrizeDrop);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", EntityExplosion);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", BlinkItem);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", EntityEquipItemDrop);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", BlitChar);
-
-INCLUDE_RODATA("st/rare/nonmatchings/e_collect", D_us_801A5360);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", EntityRelicOrb);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", EntityHeartDrop);
-
-INCLUDE_ASM("st/rare/nonmatchings/e_collect", EntityMessageBox);
+#include "../e_collect.h"
--- src/st/rare_psp/unk_9DE8.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rare/rare.h"
-
-INCLUDE_ASM("st/rare_psp/nonmatchings/rare_psp/unk_9DE8", func_psp_0923C2F8);
-
-INCLUDE_ASM("st/rare_psp/nonmatchings/rare_psp/unk_9DE8", func_psp_0923C390);
