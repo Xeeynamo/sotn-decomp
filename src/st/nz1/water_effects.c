d9cf5acc99f9bd921e28983b46049e6945c5ab7a
Matt Kelly <matthew.ryan.kelly@gmail.com>
2026-01-04 22:55:15 -0500

    Use shared water_effects.h header
    Replace 7 INCLUDE_ASM stubs with the shared decompiled header,
    matching the pattern used in no3/no4/np3.
    
    PSP build verified matching.

--- src/st/nz1/water_effects.c
+++ src/st/nz1/water_effects.c
@@ -1,16 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "nz1.h"
 
-INCLUDE_ASM("st/nz1/nonmatchings/water_effects", func_801C4144);
-
-INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntityAlucardWaterEffect);
-
-INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntitySplashWater);
-
-INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntitySurfacingWater);
-
-INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntitySideWaterSplash);
-
-INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntitySmallWaterDrop);
-
-INCLUDE_ASM("st/nz1/nonmatchings/water_effects", EntityWaterDrop);
+#include "../water_effects.h"
