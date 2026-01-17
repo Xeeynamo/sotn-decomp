c417222a7793a2f2944b456d894db63e2c0b2220
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-01-17 13:56:44 +1100

    Decompile PSP `st_init`

--- src/st/rcat_psp/unk_70B0.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rcat/rcat.h"
-
-INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/unk_70B0", func_psp_0923C2F8);
-
-INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/unk_70B0", func_psp_0923C390);
