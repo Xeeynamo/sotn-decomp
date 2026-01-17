fafa37d581c928592dea737cf60b417e60dbb5f8
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-01-17 13:24:25 +1100

    Dedupe `st_update`

--- src/st/rcat_psp/st_update.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rcat/rcat.h"
-
-INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/st_update", Random);
-
-INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/st_update", Update);
-
-INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/st_update", UpdateStageEntities);
