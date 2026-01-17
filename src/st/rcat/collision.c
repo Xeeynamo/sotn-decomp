9c3b227b1e06b65882c9bc2426faaf425e7d7236
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-01-17 12:10:40 +1100

    Dedupe `collision`

--- src/st/rcat/collision.c
+++ src/st/rcat/collision.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rcat.h"
 
-INCLUDE_ASM("st/rcat/nonmatchings/collision", HitDetection);
-
-INCLUDE_ASM("st/rcat/nonmatchings/collision", EntityDamageDisplay);
+#include "../collision.h"
--- src/st/rcat_psp/collision.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rcat/rcat.h"
-
-INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/collision", HitDetection);
-
-INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/collision", EntityDamageDisplay);
