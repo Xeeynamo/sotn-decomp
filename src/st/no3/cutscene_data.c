def8a2182e290ca210433215a826b98a37a8d446
Jonathan Hohle <jonhohle@gmail.com>
2026-01-20 20:57:08 -0700

    Merge US & PSP NO3
    This moves all source files for `no3` into the common directory and
    merges `EntityCutscene` and related data in `cutscene.c`. Several
    changes were made to the US version of closely mirror the PSP version's
    source and any real changes are `#ifdef`ed.

--- src/st/no3/cutscene_data.c
+++ src/st/no3/cutscene_data.c
@@ -2,9 +2,34 @@
 #include "no3.h"
 #include <cutscene.h>
 
+#ifdef VERSION_PSP
+
+u8 cutscene_data_it[] = {
+#include "gen/cutscene_data_it.h"
+};
+
+u8 cutscene_data_sp[] = {
+#include "gen/cutscene_data_sp.h"
+};
+
+u8 cutscene_data_fr[] = {
+#include "gen/cutscene_data_fr.h"
+};
+
+u8 cutscene_data_ge[] = {
+#include "gen/cutscene_data_ge.h"
+};
+
+u8 cutscene_data_en[] = {
+#include "gen/cutscene_data_en.h"
+};
+
+#else
 u8 OVL_EXPORT(cutscene_data)[] = {
 #include "gen/cutscene_data.h"
 };
 
-static u8 unused[] = {32, 88, 8,  128, 8, 128, 0,   0,   0,
-                      33, 90, 10, 160, 7, 120, 255, 255, 0};
+#endif
+
+static u8 unused[] UNUSED = {
+    32, 88, 8, 128, 8, 128, 0, 0, 0, 33, 90, 10, 160, 7, 120, 255, 255, 0};
