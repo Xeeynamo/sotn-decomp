c0bac5ef6ad63fd39ea97d6e679c342440fbbb8c
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-01-31 08:54:16 +1100

    Fix secrets splits

---
+++ src/st/rcat/e_secrets.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "rcat.h"
+
+INCLUDE_ASM("st/rcat/nonmatchings/e_secrets", EntityBreakableWallDebris);
+
+INCLUDE_ASM("st/rcat/nonmatchings/e_secrets", EntityBreakableWallParticles);
+
+INCLUDE_ASM("st/rcat/nonmatchings/e_secrets", func_us_801B3248);
+
+INCLUDE_ASM("st/rcat/nonmatchings/e_secrets", EntityBreakableWallSegment);
+
+INCLUDE_ASM("st/rcat/nonmatchings/e_secrets", EntitySecretWall);
---
+++ src/st/rcat_psp/e_secrets.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../rcat/rcat.h"
+
+INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/e_secrets", EntityBreakableWallDebris);
+
+INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/e_secrets", EntityBreakableWallParticles);
+
+INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/e_secrets", func_us_801B3248);
+
+INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/e_secrets", EntityBreakableWallSegment);
+
+INCLUDE_ASM("st/rcat_psp/nonmatchings/rcat_psp/e_secrets", EntitySecretWall);
