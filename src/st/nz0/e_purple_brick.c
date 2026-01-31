44d9c8424d35bef8cef933c108ac7adfb663b3c4
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-01-31 17:49:09 +0000

    merge EntityPurpleBrickScrollingBackground

--- src/st/nz0/e_purple_brick.c
+++ src/st/nz0/e_purple_brick.c
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "nz0.h"
-#include "sfx.h"
 
 // A purplish-red brick background that scrolls behind the foreground layer
 void EntityPurpleBrickScrollingBackground(Entity* self) {
