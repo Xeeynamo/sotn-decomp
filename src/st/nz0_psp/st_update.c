1d02e112df51e27074cc931671cb849525543657
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-01-31 17:59:03 +0000

    Merge st_update

--- src/st/nz0_psp/st_update.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../nz0/nz0.h"
-
-INCLUDE_ASM("st/nz0_psp/nonmatchings/nz0_psp/st_update", Random);
-
-INCLUDE_ASM("st/nz0_psp/nonmatchings/nz0_psp/st_update", Update);
-
-INCLUDE_ASM("st/nz0_psp/nonmatchings/nz0_psp/st_update", UpdateStageEntities);
