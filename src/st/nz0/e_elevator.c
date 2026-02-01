b9fd95f379b1531c2a6719f224fdcd5ae9e6108d
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-02-01 15:21:05 +0000

    Merge e_elevator

--- src/st/nz0/e_elevator.c
+++ src/st/nz0/e_elevator.c
@@ -6,19 +6,19 @@ static s32 elevator_target = 0;
 static s16 elevator_target_pos[] = {0x0000, 0x0060, 0x0360, 0x0660};
 
 void func_801B6DE4(Entity* self) {
-    s32 temp_s1;
-    s16 primIndex;
     Primitive* prim;
+    s32 primIndex;
+    s32 hitFlags;
     s32 y;
 
-    temp_s1 = self->hitFlags;
+    hitFlags = self->hitFlags;
     switch (self->step) {
     case 0:
         InitializeEntity(g_EInitInteractable);
         self->ext.elevator.yPos = self->posY.i.hi + g_Tilemap.scrollY.i.hi;
+        self->hitboxWidth = 6;
         self->hitboxHeight = 8;
         self->hitboxOffY = -22;
-        self->hitboxWidth = 6;
         self->hitboxState = 1;
 
         primIndex = g_api.AllocPrimitives(PRIM_GT4, 1);
@@ -26,10 +26,12 @@ void func_801B6DE4(Entity* self) {
             DestroyEntity(self);
             return;
         }
-        prim = &g_PrimBuf[primIndex];
-        self->primIndex = primIndex;
-        self->ext.prim = prim;
         self->flags |= FLAG_HAS_PRIMS;
+        self->primIndex = primIndex;
+        prim = &g_PrimBuf[primIndex];
+        self->ext.prim = prim;
+
+        prim->type = PRIM_SPRT;
         prim->tpage = 0xF;
         prim->clut = 9;
         prim->u0 = 72;
@@ -37,15 +39,14 @@ void func_801B6DE4(Entity* self) {
         prim->u1 = 16;
         prim->v1 = 16;
         prim->priority = 0x5F;
-        prim->type = PRIM_SPRT;
         prim->drawMode = DRAW_UNK02;
     case 1:
-        if (temp_s1) {
+        if (hitFlags) {
             self->posY.val += FIX(1.0);
             y = g_Tilemap.scrollY.i.hi + self->posY.i.hi;
-            if ((self->ext.elevator.yPos + 4) < y) {
-                self->posY.i.hi = (u16)(self->ext.elevator.yPos -
-                                        (g_Tilemap.scrollY.i.hi - 4));
+            if (self->ext.elevator.yPos + 4 < y) {
+                self->posY.i.hi =
+                    self->ext.elevator.yPos + 4 - g_Tilemap.scrollY.i.hi;
                 self->step++;
                 PlaySfxPositional(SFX_SWITCH_CLICK);
                 elevator_target = self->params;
@@ -53,12 +54,12 @@ void func_801B6DE4(Entity* self) {
         }
         break;
     case 2:
-        if (!temp_s1) {
+        if (!hitFlags) {
             self->posY.val += 0xFFFF0000;
             y = g_Tilemap.scrollY.i.hi + self->posY.i.hi;
             if (y < self->ext.elevator.yPos) {
                 self->posY.i.hi =
-                    (u16)(self->ext.elevator.yPos) - g_Tilemap.scrollY.i.hi;
+                    self->ext.elevator.yPos - g_Tilemap.scrollY.i.hi;
                 self->step = 1;
             }
         }
@@ -66,8 +67,7 @@ void func_801B6DE4(Entity* self) {
     }
     prim = self->ext.prim;
     prim->x0 = self->posX.i.hi - 8;
-    prim->y0 = self->posY.i.hi;
-    prim->y0 = prim->y0 - 8;
+    prim->y0 = self->posY.i.hi - 8;
 }
 
 typedef enum ElevatorSubSteps {
@@ -79,21 +79,18 @@ typedef enum ElevatorSubSteps {
 
 void EntityElevator(Entity* self) {
     Primitive* prim;
-    s16 primIndex;
-    s32 player;
+    s32 primIndex;
+    s32 isPlayerTouching;
+    Entity* player;
     s32 x, y;
-    s32 temp;
 
     FntPrint("elevator:%x\n", elevator_target);
-
-    if (g_Player.status & (PLAYER_STATUS_BAT_FORM | PLAYER_STATUS_MIST_FORM)) {
-        temp = 0;
+    if (!(g_Player.status &
+          (PLAYER_STATUS_BAT_FORM | PLAYER_STATUS_MIST_FORM))) {
+        isPlayerTouching = GetPlayerCollisionWith(self, 16, 5, 4);
     } else {
-        temp = GetPlayerCollisionWith(self, 16, 5, 4);
+        isPlayerTouching = 0;
     }
-
-    //! FAKE:
-    player = PLAYER_CHARACTER;
 
     switch (self->step) {
     case ELEVATOR_INIT:
@@ -104,7 +101,7 @@ void EntityElevator(Entity* self) {
         self->hitboxHeight = 5;
         self->hitboxState = 1;
 
-        if (elevator_target != 0) {
+        if (elevator_target) {
             y = elevator_target_pos[elevator_target];
             self->posY.i.hi = y - g_Tilemap.scrollY.i.hi;
         } else {
@@ -116,10 +113,10 @@ void EntityElevator(Entity* self) {
             DestroyEntity(self);
             return;
         }
-        prim = &g_PrimBuf[primIndex];
-        self->primIndex = primIndex;
-        self->ext.elevator.prim = prim;
         self->flags |= FLAG_HAS_PRIMS;
+        self->primIndex = primIndex;
+        prim = &g_PrimBuf[primIndex];
+        self->ext.elevator.prim = prim;
         prim->type = PRIM_SPRT;
         prim->tpage = 0xF;
         prim->clut = 9;
@@ -130,7 +127,7 @@ void EntityElevator(Entity* self) {
         prim->priority = 0x72;
         prim->drawMode = DRAW_UNK02;
         prim = prim->next;
-        while (prim != NULL) {
+        while (prim) {
             prim->type = PRIM_SPRT;
             prim->tpage = 0xF;
             prim->clut = 9;
@@ -143,50 +140,50 @@ void EntityElevator(Entity* self) {
             prim = prim->next;
         }
         self->ext.elevator.elevatorTarget = elevator_target;
-
     case ELEVATOR_IDLE:
-        if (temp != 0) {
+        if (isPlayerTouching) {
             if (g_pads[0].pressed & PAD_UP) {
-                if (--elevator_target < 1) {
+                elevator_target--;
+                if (elevator_target < 1) {
                     elevator_target = 1;
                 }
             }
             if (g_pads[0].pressed & PAD_DOWN) {
-                if (++elevator_target > 3) {
+                elevator_target++;
+                if (elevator_target > 3) {
                     elevator_target = 3;
                 }
             }
         }
 
         if (elevator_target != self->ext.elevator.elevatorTarget) {
-            y = elevator_target_pos[elevator_target];
-            y -= g_Tilemap.scrollY.i.hi;
-            if (y >= self->posY.i.hi) {
-                self->step = ELEVATOR_DESCEND;
-            } else {
+            y = elevator_target_pos[elevator_target] - g_Tilemap.scrollY.i.hi;
+            if (y < self->posY.i.hi) {
                 self->step = ELEVATOR_ASCEND;
+                PlaySfxPositional(SFX_LOW_SYNTH_BUBBLES);
+            } else {
+                self->step = ELEVATOR_DESCEND;
+                PlaySfxPositional(SFX_LOW_SYNTH_BUBBLES);
             }
-            PlaySfxPositional(SFX_LOW_SYNTH_BUBBLES);
         }
         self->ext.elevator.elevatorTarget = elevator_target;
         break;
-
     case ELEVATOR_ASCEND:
         self->posY.i.hi--;
         y = elevator_target_pos[self->ext.elevator.elevatorTarget] -
             g_Tilemap.scrollY.i.hi;
-        if (self->posY.i.hi >= y) {
-            if (temp != 0) {
-                g_api.func_8010DFF0(0, 1);
-                g_Entities[player].posY.i.hi--;
-                D_80097488.y.i.hi--;
-            }
-        } else {
+        if (self->posY.i.hi < y) {
             self->posY.i.hi = y;
             self->step = ELEVATOR_IDLE;
+        } else {
+            if (isPlayerTouching) {
+                g_api.func_8010DFF0(0, 1);
+                player = &PLAYER;
+                player->posY.i.hi--;
+                D_80097488.y.i.hi--;
+            }
         }
         break;
-
     case ELEVATOR_DESCEND:
         self->posY.i.hi++;
         y = elevator_target_pos[self->ext.elevator.elevatorTarget] -
@@ -196,9 +193,10 @@ void EntityElevator(Entity* self) {
             self->step = ELEVATOR_IDLE;
             break;
         }
-        if (temp != 0) {
+        if (isPlayerTouching) {
             g_api.func_8010DFF0(0, 1);
-            g_Entities[player].posY.i.hi++;
+            player = &PLAYER;
+            player->posY.i.hi++;
             D_80097488.y.i.hi++;
         }
         break;
@@ -208,9 +206,8 @@ void EntityElevator(Entity* self) {
     prim->y0 = self->posY.i.hi;
 
     prim = prim->next;
-    x = self->posX.i.hi;
+    x = self->posX.i.hi - 3;
     y = self->posY.i.hi - 56;
-    x -= 3;
     while (prim != NULL) {
         prim->x0 = x;
         prim->y0 = y;
