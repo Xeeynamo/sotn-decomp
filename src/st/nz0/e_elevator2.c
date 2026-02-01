c3f74f246db4d21b8b68286a10a7034f3c9b06f4
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-02-01 15:44:22 +0000

    Merge e_elevator2

--- src/st/nz0/e_elevator2.c
+++ src/st/nz0/e_elevator2.c
@@ -2,33 +2,37 @@
 #include "nz0.h"
 #include "sfx.h"
 
-static bool g_CallElevator = false;
+static bool g_CallElevator = 0;
 
 void EntityBloodSkeleElevButton(Entity* self) {
+    Entity* newEntity;
     Primitive* prim;
-    s16 primIndex;
-    s32 var_a0;
-    s32 temp;
+    s32 primIndex;
+    s32 hitFlags;
+    s32 buttonY;
+    s32 distance;
 
     switch (self->step) {
     case 0:
         InitializeEntity(g_EInitInteractable);
+        self->hitboxWidth = 6;
         self->hitboxHeight = 8;
         self->hitboxOffY = -22;
-        self->hitboxWidth = 6;
         self->hitboxState = 1;
-        CreateEntityFromEntity(E_BLOOD_SKELETON, self, &self[-1]);
-        self[-1].posY.i.hi = 344 - g_Tilemap.scrollY.i.hi;
+        newEntity = &self[-1];
+        CreateEntityFromEntity(E_ID(BLOOD_SKELETON), self, newEntity);
+        newEntity->posY.i.hi = 344 - g_Tilemap.scrollY.i.hi;
 
         primIndex = g_api.AllocPrimitives(PRIM_GT4, 1);
         if (primIndex == -1) {
             DestroyEntity(self);
             return;
         }
-        prim = &g_PrimBuf[primIndex];
-        self->primIndex = primIndex;
-        self->ext.prim = prim;
         self->flags |= FLAG_HAS_PRIMS;
+        self->primIndex = primIndex;
+        prim = &g_PrimBuf[primIndex];
+        self->ext.prim = prim;
+        prim->type = PRIM_SPRT;
         prim->tpage = 0xF;
         prim->clut = 9;
         prim->u0 = 72;
@@ -36,64 +40,60 @@ void EntityBloodSkeleElevButton(Entity* self) {
         prim->u1 = 16;
         prim->v1 = 16;
         prim->priority = 0x5F;
-        prim->type = PRIM_SPRT;
         prim->drawMode = DRAW_UNK02;
-
     case 1:
-        var_a0 = self->hitFlags;
-
-        if (abs(self->posX.i.hi - self[-1].posX.i.hi) < 8) {
-            var_a0 |= 0x8000;
+        hitFlags = self->hitFlags;
+        newEntity = &self[-1];
+        distance = self->posX.i.hi - newEntity->posX.i.hi;
+        if (abs(distance) < 8) {
+            hitFlags |= 0x8000;
         }
-
-        if (var_a0) {
+        if (hitFlags) {
             self->posY.val += FIX(1.0);
-            temp = g_Tilemap.scrollY.i.hi + self->posY.i.hi;
-            if (temp > 376) {
+            buttonY = g_Tilemap.scrollY.i.hi + self->posY.i.hi;
+            if (buttonY > 376) {
                 self->posY.i.hi = 376 - g_Tilemap.scrollY.i.hi;
             }
-            g_CallElevator = true;
+            g_CallElevator = 1;
         } else {
             self->posY.val -= FIX(1);
-            temp = g_Tilemap.scrollY.i.hi + self->posY.i.hi;
-            if (temp < 372) {
+            buttonY = g_Tilemap.scrollY.i.hi + self->posY.i.hi;
+            if (buttonY < 372) {
                 self->posY.i.hi = 372 - g_Tilemap.scrollY.i.hi;
             }
-            g_CallElevator = false;
+            g_CallElevator = 0;
         }
-
-    default:
-        prim = self->ext.prim;
-        prim->x0 = self->posX.i.hi - 8;
-        prim->y0 = self->posY.i.hi - 8;
+        break;
     }
+    prim = self->ext.prim;
+    prim->x0 = self->posX.i.hi - 8;
+    prim->y0 = self->posY.i.hi - 8;
 }
 
 void EntityElevator2(Entity* self) {
-    s32 temp = GetPlayerCollisionWith(self, 16, 5, 4);
-    volatile int pad[3];
-    Primitive* prim;
-    s16 primIndex;
-    s32 player;
+    s32 isPlayerColliding;
+    s32 primIndex;
     s32 camY;
+    Entity* player;
     s32 i;
+    Primitive* prim;
 
+    isPlayerColliding = GetPlayerCollisionWith(self, 16, 5, 4);
     switch (self->step) {
     case 0:
         InitializeEntity(g_EInitInteractable);
         self->hitboxOffX = 0;
         self->hitboxOffY = 68;
-        g_CallElevator = false;
-
+        g_CallElevator = 0;
         primIndex = g_api.AllocPrimitives(PRIM_GT4, 3);
         if (primIndex == -1) {
             DestroyEntity(self);
             return;
         }
-        prim = &g_PrimBuf[primIndex];
-        self->primIndex = primIndex;
-        self->ext.prim = prim;
         self->flags |= FLAG_HAS_PRIMS;
+        self->primIndex = primIndex;
+        prim = &g_PrimBuf[primIndex];
+        self->ext.prim = prim;
         prim->type = PRIM_SPRT;
         prim->tpage = 0xF;
         prim->clut = 9;
@@ -117,26 +117,25 @@ void EntityElevator2(Entity* self) {
             prim->drawMode = DRAW_UNK02;
             prim = prim->next;
         }
-
     case 1:
-        //! FAKE:
-        player = PLAYER_CHARACTER;
         if (g_CallElevator) {
             self->posY.i.hi--;
-            camY = g_Tilemap.scrollY.i.hi;
-            if ((self->posY.i.hi + camY) < 96) {
-                self->posY.i.hi = 96 - camY;
-            } else if (temp != 0) {
-                g_Entities[player].posY.i.hi--;
+            camY = self->posY.i.hi + g_Tilemap.scrollY.i.hi;
+            if (camY < 96) {
+                self->posY.i.hi = 96 - g_Tilemap.scrollY.i.hi;
+            } else if (isPlayerColliding) {
+                player = &PLAYER;
+                player->posY.i.hi--;
                 D_80097488.y.i.hi--;
             }
         } else {
             self->posY.i.hi++;
-            camY = g_Tilemap.scrollY.i.hi;
-            if ((self->posY.i.hi + camY) > 216) {
-                self->posY.i.hi = 216 - camY;
-            } else if (temp != 0) {
-                g_Entities[player].posY.i.hi++;
+            camY = self->posY.i.hi + g_Tilemap.scrollY.i.hi;
+            if (camY > 216) {
+                self->posY.i.hi = 216 - g_Tilemap.scrollY.i.hi;
+            } else if (isPlayerColliding) {
+                player = &PLAYER;
+                player->posY.i.hi++;
                 D_80097488.y.i.hi++;
             }
         }
--- src/st/nz0/nz0.h
+++ src/st/nz0/nz0.h
@@ -91,6 +91,7 @@ typedef enum EntityIDs {
 extern s32 E_ID(EXPLOSION);
 extern s32 E_ID(CANNON_SHOT);
 extern s32 E_ID(WALL_DEBRIS);
+extern s32 E_ID(BLOOD_SKELETON);
 extern s32 E_ID(RELIC_CONTAINER);
 extern s32 E_ID(BOSS_ROOM_BLOCK);
 extern s32 E_ID(SLOGRA);
