e961a0028b0ecc37e5cef9a049fc91e96c2395f3
Joey Murphy <tjmurphy@mit.edu>
2026-03-06 18:17:50 -0500

    Automatic file split

---
+++ src/st/rnz0/e_collect.c
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", PrizeDropFall);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", PrizeDropFall2);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", CollectHeart);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", CollectGold);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", CollectSubweapon);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", CollectHeartVessel);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", CollectLifeVessel);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", CollectDummy);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC1C);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC24);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC2C);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC34);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC3C);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC44);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC4C);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC54);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC5C);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_collect", D_us_801AAC64);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", EntityPrizeDrop);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", EntityExplosion);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", BlinkItem);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_collect", EntityEquipItemDrop);
