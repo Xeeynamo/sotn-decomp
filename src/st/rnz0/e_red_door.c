8de24ceee3e0aafc9adb12742e1f06abd0f766e4
Joey Murphy <tjmurphy@mit.edu>
2026-03-06 20:32:03 -0500

    red door good

--- src/st/rnz0/e_red_door.c
+++ src/st/rnz0/e_red_door.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
+#include "rnz0.h"
 
-INCLUDE_ASM("st/rnz0/nonmatchings/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/rnz0/nonmatchings/e_red_door", RNZ0_EntityRedDoor);
+#include "../e_red_door.h"
