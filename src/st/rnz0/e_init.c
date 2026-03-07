5cf62210997dee4ea50bdefd84206b76fd3fabc9
Joey Murphy <tjmurphy@mit.edu>
2026-03-06 19:39:39 -0500

    e_particles dedupe

--- src/st/rnz0/e_init.c
+++ src/st/rnz0/e_init.c
@@ -126,7 +126,7 @@ PfnEntityUpdate OVL_EXPORT(EntityUpdates)[] = {
 };
 
 EInit D_us_80180984 = {ANIMSET_OVL(1), 0,  0, PAL_NULL, 0x000};
-EInit D_us_80180990 = {ANIMSET_DRA(3), 0,  0, PAL_NULL, 0x001};
+EInit g_EInitObtainable = {ANIMSET_DRA(3), 0,  0, PAL_NULL, 0x001};
 EInit D_us_8018099C = {ANIMSET_DRA(3), 0,  0, PAL_NULL, 0x002};
 EInit D_us_801809A8 = {ANIMSET_DRA(0), 0,  0, PAL_NULL, 0x004};
 EInit D_us_801809B4 = {ANIMSET_DRA(0), 0,  0, PAL_NULL, 0x005};
--- src/st/rnz0/e_particles.c
+++ src/st/rnz0/e_particles.c
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 
-INCLUDE_ASM("st/rnz0/nonmatchings/e_particles", EntitySoulStealOrb);
+#include "../e_particles.h"
 
-INCLUDE_ASM("st/rnz0/nonmatchings/e_particles", EntityEnemyBlood);
