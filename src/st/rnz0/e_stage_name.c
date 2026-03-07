67a598b8aeb6f0205998936bdf3bc6d47cc3758a
Joey Murphy <tjmurphy@mit.edu>
2026-03-06 21:00:22 -0500

    proper rename e_stage_name

---
+++ src/st/rnz0/e_stage_name.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_stage_name", StageNamePopupDissolver);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_stage_name", EntityStageNamePopup);
