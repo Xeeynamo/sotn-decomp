0e4a6ecc705f3d298b672f014adb646810c30935
Joey Murphy <tjmurphy@mit.edu>
2026-03-07 16:25:17 -0500

    Working PSP RNZ0

---
+++ src/st/rnz0/rnz0_psp/1F530.c
@@ -0,0 +1,23 @@
+#include "../rnz0.h"
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09256BB0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09256F98);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_092570E8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_092573E8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", RNZ0_EntityBackgroundBlock);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", RNZ0_EntityLockCamera);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_092578A8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09257A60);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09257B98);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09257F10);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_092581F8);
---
+++ src/st/rnz0/rnz0_psp/80.c
@@ -0,0 +1,253 @@
+#include "../rnz0.h"
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09237700);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09238F80);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09239298);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09239C80);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09239DB0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09239ED0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A068);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A200);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A270);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A2F8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A438);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A590);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A608);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A690);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A7D8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A938);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923ABA8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923AC88);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923AD10);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", SalemWitchTrySpawnShadow);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntitySalemWitch);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntitySalemWitchGlow);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntitySalemWitchCurse);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntitySalemWitchTriboltLaunch);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntitySalemWitchTriboltProjectile);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923CEE0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923D100);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923D3F8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923E290);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923E448);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923E620);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923EDC8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09240468);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityStageNamePopup);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityIsNearPlayer);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", RNZ0_EntityRedDoor);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", Random);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", Update);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", UpdateStageEntities);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_psp_0923AD68);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_psp_0923B2F0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntitySoulStealOrb);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityEnemyBlood);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092445D8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092457C0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_psp_0923C2F8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_psp_0923C390);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", BlitChar);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", PrizeDropFall);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", PrizeDropFall2);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", CollectHeart);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", CollectGold);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", CollectSubweapon);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", CollectHeartVessel);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", CollectLifeVessel);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", CollectDummy);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityPrizeDrop);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityExplosion);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", BlinkItem);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityEquipItemDrop);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityRelicOrb);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityHeartDrop);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityMessageBox);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09249B18);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09249C60);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09249DA8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09249ED8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924A050);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924A1D0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924A340);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", HitDetection);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityDamageDisplay);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924C670);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924C710);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924C7F8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924DBD0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924DDF0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924E280);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924E7D0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924EB18);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924EE10);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924F3D8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924F908);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0924FE10);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09250678);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09250790);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252198);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", BottomCornerText);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", EntityRoomForeground);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252920);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092529B8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252A40);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252BB0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252C20);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252C78);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252D00);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252D50);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09252D88);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253040);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253320);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253390);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253438);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092534B8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092535B0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253600);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253640);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092537E8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253820);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092539B0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253E88);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09253F80);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09254480);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09254700);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09254750);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09254838);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09254940);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092549A8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09254A78);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09255288);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09255A00);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09255BF0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09255DE0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09256060);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092562F0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09256678);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_092567B8);
