df890592e6ca045d061f79c10b27a150f6cd7ed5
Joey Murphy <tjmurphy@mit.edu>
2026-03-07 17:59:24 -0500

    give symbols to align functions with US version

--- src/st/rnz0/rnz0_psp/1F530.c
+++ src/st/rnz0/rnz0_psp/1F530.c
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rnz0.h"
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09256BB0);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntityBreakableNZ0);
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09256F98);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntityRedEyeBust);
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_092570E8);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntityRelicContainer);
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_092573E8);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntityBlueFlameTable);
 
 INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", RNZ0_EntityBackgroundBlock);
 
 INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", RNZ0_EntityLockCamera);
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_092578A8);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntityLifeMaxTank);
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09257A60);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntityTableWithGlobe);
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09257B98);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntityLeftSecretRoomWall);
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_09257F10);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntityBottomSecretRoomFloor);
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1F530", func_pspeu_092581F8);
+INCLUDE_ASM("st/rnz0_psp/matchings/rnz0_psp/1F530", EntitySecretWallDebris);
