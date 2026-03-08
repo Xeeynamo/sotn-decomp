518ba2ce14d07ff032dfe2055ce9f47a719dd4e1
Joey Murphy <tjmurphy@mit.edu>
2026-03-08 10:44:09 -0400

    Files aligned for entity 1A

---
+++ src/st/rnz0/unk_38238.c
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801B8238);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801B9430);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801B965C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801B9D78);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801B9E64);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801B9FC8);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BA21C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BAD1C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BAE9C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BB000);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/unk_38238", D_us_801AAD84);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BB5D8);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BC51C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BC6CC);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BCAB0);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BCE4C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BD13C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BD3D4);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BD7D0);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BDB94);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BDE28);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BE550);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BE600);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801BF7B0);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", SealedDoorIsNearPlayer);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", EntitySealedDoor);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801C060C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801C09E8);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801C1668);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801C1758);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801C1CC0);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", func_us_801C1E14);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", SalemWitchTrySpawnShadow);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", EntitySalemWitch);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", EntitySalemWitchGlow);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", EntitySalemWitchCurse);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", EntitySalemWitchTriboltLaunch);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_38238", EntitySalemWitchTriboltProjectile);
