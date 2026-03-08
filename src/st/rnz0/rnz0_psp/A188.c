d540dda7c6246eadf588c1ed13d31d398ebcc84b
Joey Murphy <tjmurphy@mit.edu>
2026-03-07 21:55:39 -0500

    prepare st_update splits

--- src/st/rnz0/rnz0_psp/A188.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rnz0.h"
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", RNZ0_EntityRedDoor);
---
+++ src/st/rnz0/rnz0_psp/B9C8.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../rnz0.h"
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B9C8", func_psp_0923AD68);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B9C8", func_psp_0923B2F0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B9C8", EntitySoulStealOrb);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B9C8", EntityEnemyBlood);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B9C8", func_us_801C09E8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B9C8", func_us_801C1668);
