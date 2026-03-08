25290bade56784fbaf5b094c3c8196092bcbdc94
Joey Murphy <tjmurphy@mit.edu>
2026-03-08 12:32:22 -0400

    split out ents 1f to 2c

---
+++ src/st/rnz0/e_unk1F-2C.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_unk1F-2C", func_us_801B9E64);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_unk1F-2C", func_us_801B9FC8);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_unk1F-2C", func_us_801BA21C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_unk1F-2C", func_us_801BAD1C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_unk1F-2C", func_us_801BAE9C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_unk1F-2C", func_us_801BB000);
+
+INCLUDE_RODATA("st/rnz0/nonmatchings/e_unk1F-2C", D_us_801AAD84);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_unk1F-2C", func_us_801BB5D8);
---
+++ src/st/rnz0/unk_3C51C.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BC51C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BC6CC);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BCAB0);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BCE4C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BD13C);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BD3D4);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BD7D0);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BDB94);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BDE28);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BE550);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BE600);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", func_us_801BF7B0);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", SealedDoorIsNearPlayer);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/unk_3C51C", EntitySealedDoor);
