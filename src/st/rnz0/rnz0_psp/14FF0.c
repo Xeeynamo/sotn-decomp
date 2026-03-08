53ee68ba5b63b6c9e0f863184e8b1e0eace8eba4
Joey Murphy <tjmurphy@mit.edu>
2026-03-07 19:56:16 -0500

    split out collision

---
+++ src/st/rnz0/rnz0_psp/14FF0.c
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../rnz0.h"
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924C670);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924C710);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", EntitySealedDoor);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924DBD0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924DDF0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801BCAB0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924E7D0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924EB18);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924EE10);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801BD7D0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924F908);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_0924FE10);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09250678);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801BE600);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801BF7B0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", BottomCornerText);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", EntityRoomForeground);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", DestroyEntity);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", PreventEntityFromRespawning);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09252A40);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09252BB0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09252C20);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09252C78);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09252D00);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09252D50);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09252D88);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09253040);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", AllocEntity);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09253390);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09253438);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_092534B8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_092535B0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09253600);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", InitializeEntity);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", EntityDummy);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09253820);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_092539B0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09253E88);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09253F80);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09254480);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09254700);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09254750);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09254838);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_09254940);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_pspeu_092549A8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801B7C8C);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801C1758);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801C1CC0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801C1E14);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", EntityPurpleBrickScrollingBackground);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", EntityCannonLever);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801AC758);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", EntityCannonShot);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/14FF0", func_us_801ACAD0);
