2c1de3d85bdb277b71837671a83b8ed790227f47
Joey Murphy <tjmurphy@mit.edu>
2026-03-08 16:11:49 -0400

    Align purple brick

---
+++ src/st/rnz0/2B7EC.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityLeftSecretRoomWall);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityBottomSecretRoomFloor);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntitySecretWallDebris);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityTableWithGlobe);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityLifeMaxTank);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityRelicContainer);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityBlueFlameTable);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityCannonLever);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", func_us_801AC758);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityCannonShot);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", func_us_801ACAD0);
--- src/st/rnz0/rnz0_psp/1E760.c
+++ src/st/rnz0/rnz0_psp/1E760.c
@@ -1,12 +1,3 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rnz0.h"
+#include "common.h"
 
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", EntityPurpleBrickScrollingBackground);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", EntityCannonLever);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", func_us_801AC758);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", EntityCannonShot);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", func_us_801ACAD0);
+INCLUDE_ASM("asm/pspeu/st/rnz0_psp/nonmatchings/rnz0_psp/1E760", EntityPurpleBrickScrollingBackground);
