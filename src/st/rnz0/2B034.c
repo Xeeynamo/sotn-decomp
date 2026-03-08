b2b5d86239f271ae8a3887d7c1a4220c01497f74
Joey Murphy <tjmurphy@mit.edu>
2026-03-08 16:23:31 -0400

    Align e_room_bg

---
+++ src/st/rnz0/2B034.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B034", RNZ0_EntityLockCamera);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B034", EntityBreakableNZ0);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/2B034", EntityRedEyeBust);
