1479a1ea9b5784bf33ec7af7b89593a8789c3913
Joey Murphy <tjmurphy@mit.edu>
2026-03-07 21:33:53 -0500

    start preparing stage name

--- src/st/rnz0/e_stage_name.c
+++ src/st/rnz0/e_stage_name.c
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rnz0.h"
 
+#ifdef VERSION_US
 #include "../e_stage_name_us.h"
+#endif
+
+#ifdef VERSION_PSP
+static u8 stage_name_en[] = {
+#include "gen/stage_name_en.h"
+};
+static u8 stage_name_it[] = {
+#include "gen/stage_name_it.h"
+};
+static u8 stage_name_sp[] = {
+#include "gen/stage_name_sp.h"
+};
+static u8 stage_name_ge[] = {
+#include "gen/stage_name_ge.h"
+};
+static u8 stage_name_fr[] = {
+#include "gen/stage_name_fr.h"
+};
+#endif
+
+#if defined(VERSION_PSP) || defined(VERSION_HD)
+#include "../e_stage_name_jp.h"
+#endif
