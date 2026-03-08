32a1cf5a4d621c0bb76fb7ae687346bc33fe6eec
Joey Murphy <tjmurphy@mit.edu>
2026-03-08 16:02:02 -0400

    Align entities 34-36

---
+++ src/st/rnz0/rnz0_psp/1E760.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../rnz0.h"
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", EntityPurpleBrickScrollingBackground);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", EntityCannonLever);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", func_us_801AC758);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", EntityCannonShot);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/1E760", func_us_801ACAD0);
