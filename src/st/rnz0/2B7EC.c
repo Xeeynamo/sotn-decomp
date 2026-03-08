4778ef7629a70dc8cae003c9c207ed13e03f8727
Joey Murphy <tjmurphy@mit.edu>
2026-03-08 16:50:19 -0400

    Rename left secret room wall

--- src/st/rnz0/2B7EC.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityLeftSecretRoomWall);
-
-INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntityBottomSecretRoomFloor);
-
-INCLUDE_ASM("st/rnz0/nonmatchings/2B7EC", EntitySecretWallDebris);
---
+++ src/st/rnz0/e_left_secret_room_wall.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_left_secret_room_wall", EntityLeftSecretRoomWall);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_left_secret_room_wall", EntityBottomSecretRoomFloor);
+
+INCLUDE_ASM("st/rnz0/nonmatchings/e_left_secret_room_wall", EntitySecretWallDebris);
