84d404003d8abe09f8a33780040d40c55dea787a
Joey Murphy <tjmurphy@mit.edu>
2026-03-08 15:42:49 -0400

    align entities 32+33

--- src/st/rnz0/rnz0_psp/CF58.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rnz0.h"
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/CF58", func_us_801C09E8);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/CF58", func_us_801C1668);
