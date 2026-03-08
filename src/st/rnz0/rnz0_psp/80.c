11931b4c3e40fc35879b7535bad46a31942471dd
Joey Murphy <tjmurphy@mit.edu>
2026-03-07 21:03:10 -0500

    split for create_entity

--- src/st/rnz0/rnz0_psp/80.c
+++ src/st/rnz0/rnz0_psp/80.c
@@ -8,33 +8,3 @@ INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_us_801B9430);
 INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_us_801B965C);
 
 INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_us_801B9D78);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09239DB0);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_09239ED0);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A068);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A200);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A270);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A2F8);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A438);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", FindFirstEntityAbove);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A608);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A690);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923A7D8);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", InitRoomEntities);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", UpdateRoomPosition);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", func_pspeu_0923AC88);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/80", CreateEntityFromEntity);
