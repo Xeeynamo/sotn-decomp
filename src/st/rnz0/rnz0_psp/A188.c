a2d23f5d1b5b7226a5facc1471812db2ad69fdc4
Joey Murphy <tjmurphy@mit.edu>
2026-03-07 21:49:54 -0500

    resplit

--- src/st/rnz0/rnz0_psp/A188.c
+++ src/st/rnz0/rnz0_psp/A188.c
@@ -4,21 +4,3 @@
 INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", EntityIsNearPlayer);
 
 INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", RNZ0_EntityRedDoor);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", Random);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", Update);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", UpdateStageEntities);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", func_psp_0923AD68);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", func_psp_0923B2F0);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", EntitySoulStealOrb);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", EntityEnemyBlood);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", func_us_801C09E8);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/A188", func_us_801C1668);
---
+++ src/st/rnz0/rnz0_psp/B490.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../rnz0.h"
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", Random);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", Update);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", UpdateStageEntities);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", func_psp_0923AD68);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", func_psp_0923B2F0);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", EntitySoulStealOrb);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", EntityEnemyBlood);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", func_us_801C09E8);
+
+INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/B490", func_us_801C1668);
