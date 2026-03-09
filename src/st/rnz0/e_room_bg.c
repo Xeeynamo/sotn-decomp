cd044eb7f81903477990656182a1a410731407d3
Joey Murphy <tjmurphy@mit.edu>
2026-03-09 08:29:37 -0400

    Decomp US e_room_bg

--- src/st/rnz0/e_room_bg.c
+++ src/st/rnz0/e_room_bg.c
@@ -1,4 +1,31 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
+#include "rnz0.h"
 
-INCLUDE_ASM("st/rnz0/nonmatchings/e_room_bg", RNZ0_EntityBackgroundBlock);
+static u32 D_us_80180B10[] = {0x00FF0140};
+static u32 D_us_80180B14[] = {0x26022502, 0x26022702, 0x00000000};
+
+ObjInit2 OVL_EXPORT(BackgroundBlockInit)[] = {
+    {
+        .animSet = ANIMSET_DRA(6),
+        .zPriority = 0x01FA,
+        .unk5A = 0,
+        .palette = 0,
+        .drawFlags = ENTITY_DEFAULT,
+        .blendMode = BLEND_TRANSP,
+        .flags = 0,
+        .animFrames = D_us_80180B10,
+    },
+    {
+        .animSet = ANIMSET_OVL(1),
+        .zPriority = 0x00C0,
+        .unk5A = 0,
+        .palette = 0,
+        .drawFlags = ENTITY_SCALEY | ENTITY_SCALEX,
+        .blendMode = BLEND_TRANSP | BLEND_ADD,
+        .flags = 0,
+        .animFrames = D_us_80180B14,
+    }};
+
+#define BG_BLOCK_NEEDS_SCALE
+
+#include "../e_room_bg.h"
