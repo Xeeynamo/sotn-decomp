3fb6d7f255570502744401c9ce1077626b64f346
Joey Murphy <tjmurphy@mit.edu>
2026-03-10 08:35:47 -0400

    e_red_eye_bust

--- src/st/rnz0/e_red_eye_bust.c
+++ src/st/rnz0/e_red_eye_bust.c
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
+#include "rnz0.h"
+#include "sfx.h"
 
-INCLUDE_ASM("st/rnz0/nonmatchings/e_red_eye_bust", EntityRedEyeBust);
+extern EInit g_EInitEnemy3;
+
+// bust with red eyes that can have a candle on it
+void EntityRedEyeBust(Entity* self) {
+    switch (self->step) {
+    case 0:
+        InitializeEntity(g_EInitEnemy3);
+        self->animCurFrame = 7;
+        self->zPriority = 0x70;
+        break;
+
+    case 1:
+        break;
+
+    case 2:
+#include "../pad2_anim_debug.h"
+    }
+}
