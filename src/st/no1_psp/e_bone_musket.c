9b305011e560c8009bb2c25a69a92baf3b471871
gamezter <gamezter93@gmail.com>
2026-03-10 21:05:46 -0400

    Remove files that were re-added with merge

--- src/st/no1_psp/e_bone_musket.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no1_psp/nonmatchings/no1_psp/e_bone_musket", EntityBoneMusket);
-
-INCLUDE_ASM("st/no1_psp/nonmatchings/no1_psp/e_bone_musket", func_us_801CF298);
--- src/st/no1_psp/e_mist_door.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no1_psp/nonmatchings/no1_psp/e_mist_door", func_pspeu_092490A8);
-
-INCLUDE_ASM("st/no1_psp/nonmatchings/no1_psp/e_mist_door", EntityMistDoor);
