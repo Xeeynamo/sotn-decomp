ce5c8c156c2feb9dca98dd3369666beb4f1f3daf
Joey Murphy <tjmurphy@mit.edu>
2026-03-11 08:03:59 -0400

    e_blue_flame_table

--- src/st/rnz0/e_blue_flame_table.c
+++ src/st/rnz0/e_blue_flame_table.c
@@ -1,6 +1,103 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
+#include "rnz0.h"
 
-INCLUDE_ASM("st/rnz0/nonmatchings/e_blue_flame_table", EntityRelicContainer);
+extern EInit g_EInitPrizeContainer;
 
-INCLUDE_ASM("st/rnz0/nonmatchings/e_blue_flame_table", EntityBlueFlameTable);
+#ifdef VERSION_PSP
+extern s32 E_ID(RELIC_CONTAINER);
+#endif
+
+// Breakable container holding Skill of Wolf, Bat Card, maybe others
+static u8 anim_relic_container[] = {
+    0x10, 0x02, 0x03, 0x01, 0x09, 0x02, 0x03, 0x01, 0x10, 0x02, 0x03, 0x01,
+    0x05, 0x02, 0x03, 0x01, 0x40, 0x02, 0x03, 0x03, 0x10, 0x02, 0x03, 0x03,
+    0x08, 0x02, 0x03, 0x01, 0x14, 0x02, 0x03, 0x03, 0x40, 0x02, 0x00, 0x00};
+static u8 anim_relic_container_alt[] = {
+    0x01, 0x05, 0x03, 0x09, 0x01, 0x06, 0x03, 0x09, 0x01, 0x07,
+    0x03, 0x09, 0x01, 0x08, 0x03, 0x09, 0x00, 0x00, 0x00, 0x00};
+static u8 anim_blue_flame_table[] = {
+    0x01, 0x0A, 0x01, 0x0B, 0x01, 0x0C, 0x01, 0x0D, 0x01, 0x0E,
+    0x01, 0x0F, 0x01, 0x10, 0x01, 0x11, 0x00, 0x00, 0x00, 0x00};
+void EntityRelicContainer(Entity* self) {
+    Entity* newEntity;
+
+    switch (self->step) {
+    case 0:
+        InitializeEntity(g_EInitPrizeContainer);
+        if (self->params & 0x100) {
+            self->blendMode = BLEND_TRANSP | BLEND_ADD;
+        } else {
+            self->zPriority = 0x6A;
+            self->hitboxWidth = 8;
+            self->hitboxHeight = 12;
+            self->hitboxOffX = 0;
+            self->hitboxOffY = 10;
+            self->hitboxState = 2;
+            newEntity = self + 1;
+            CreateEntityFromEntity(E_ID(RELIC_CONTAINER), self, newEntity);
+            newEntity->params = 0x100;
+        }
+    case 1:
+        if (self->params & 0x100) {
+            AnimateEntity(anim_relic_container_alt, self);
+            break;
+        }
+        AnimateEntity(anim_relic_container, self);
+        if (self->hitFlags) {
+            self->hitboxState = 0;
+            SetStep(2);
+        }
+        break;
+    case 2:
+
+        newEntity = self + 1;
+        CreateEntityFromEntity(E_HEART_DROP, self, newEntity);
+        newEntity->params = self->params;
+
+        newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
+        if (newEntity != NULL) {
+            CreateEntityFromEntity(E_EXPLOSION, self, newEntity);
+            newEntity->posY.i.hi -= 8;
+            newEntity->params = 2;
+        }
+        PlaySfxPositional(SFX_GLASS_BREAK_E);
+        self->step++;
+    case 3:
+        self->animCurFrame = 4;
+        break;
+    case 255:
+#include "../pad2_anim_debug.h"
+    }
+}
+
+// Table in room with bone-throwing skeleton. Drops a Resist Thunder.
+void EntityBlueFlameTable(Entity* self) {
+    Entity* newEntity;
+
+    switch (self->step) {
+    case 0:
+        InitializeEntity(g_EInitPrizeContainer);
+        self->zPriority = 0x6A;
+        self->hitboxWidth = 8;
+        self->hitboxHeight = 16;
+        self->hitboxOffX = 0;
+        self->hitboxOffY = 10;
+        self->hitboxState = 2;
+    case 1:
+        AnimateEntity(anim_blue_flame_table, self);
+        if (self->hitFlags) {
+            g_api.PlaySfx(SFX_CANDLE_HIT);
+            self->hitboxState = 0;
+            SetStep(2);
+        }
+        break;
+    case 2:
+        newEntity = &self[1];
+        CreateEntityFromEntity(E_HEART_DROP, self, newEntity);
+        newEntity->params = self->params;
+        self->step++;
+    case 3:
+        self->animCurFrame = 18;
+        break;
+    }
+}
