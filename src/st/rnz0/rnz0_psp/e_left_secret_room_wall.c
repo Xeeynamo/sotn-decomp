6eef1d99c736649682a57e29b8ea3848d6fde502
bismurphy <tjmurphy@mit.edu>
2026-03-11 07:43:54 -0400

    Next batch of RNZ0 files (#3184)
    These are the next 3 files from RNZ0. Mostly copies from the normal NZ0,
    with small patches to account for being inverted. No real surprises
    beyond that. One weird thing is that all the calls in NZ0 for
    PlaySfxPositional have been changed to g_api.PlaySfx, but that's it.

--- src/st/rnz0/rnz0_psp/e_left_secret_room_wall.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rnz0.h"
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/e_left_secret_room_wall", EntityLeftSecretRoomWall);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/e_left_secret_room_wall", EntityBottomSecretRoomFloor);
-
-INCLUDE_ASM("st/rnz0_psp/nonmatchings/rnz0_psp/e_left_secret_room_wall", EntitySecretWallDebris);
