94071c143117f6326096fc94d0069291b7345a69
gamezter <gamezter93@gmail.com>
2026-03-23 19:29:42 -0400

    codeshare `st_update`

--- src/st/no0_psp/st_update.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no0_psp/nonmatchings/no0_psp/st_update", func_pspeu_0923FC78);
-
-INCLUDE_ASM("st/no0_psp/nonmatchings/no0_psp/st_update", Update);
-
-INCLUDE_ASM("st/no0_psp/nonmatchings/no0_psp/st_update", UpdateStageEntities);
