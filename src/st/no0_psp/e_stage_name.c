1a79ee01dc22fe2f14c63673815f8aeda08a116a
gamezter <gamezter93@gmail.com>
2026-03-30 15:23:19 -0400

    Codeshare rest of `no0_psp`

--- src/st/no0_psp/e_stage_name.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "common.h"
-
-INCLUDE_ASM("st/no0_psp/nonmatchings/no0_psp/e_stage_name", func_pspeu_0923BDA8);
-
-INCLUDE_ASM("st/no0_psp/nonmatchings/no0_psp/e_stage_name", EntityStageNamePopup);
