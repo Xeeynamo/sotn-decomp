99726fc32a8cca553f2d3f3e98fae5f470692dd4
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-06-16 14:25:07 +1000

    Dedupe `stcen` `st_update`

--- src/st/cen_psp/st_update.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../cen/cen.h"
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/st_update", Random);
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/st_update", Update);
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/st_update", UpdateStageEntities);
