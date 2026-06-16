eb2b5ab8befeb527af9825791dc109c6350d59bc
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-06-16 14:45:04 +1000

    Cleanup some duplication in `header`

--- src/st/cen/header.c
+++ src/st/cen/header.c
@@ -1,47 +1,28 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "cen.h"
-#ifdef VERSION_PSP
-
-// common
-extern RoomHeader OVL_EXPORT(rooms)[];
-extern SpriteParts* OVL_EXPORT(spriteBanks)[];
-extern u_long* OVL_EXPORT(cluts)[];
-extern RoomDef OVL_EXPORT(rooms_layers)[];
-extern GfxBank* OVL_EXPORT(gfxBanks)[];
-void UpdateStageEntities(void);
-// overlay
-u8* D_pspeu_09293958[];
-
-Overlay OVL_EXPORT(Overlay) = {
-    .Update = Update,
-    .HitDetection = HitDetection,
-    .UpdateRoomPosition = UpdateRoomPosition,
-    .InitRoomEntities = InitRoomEntities,
-    .rooms = OVL_EXPORT(rooms),
-    .spriteBanks = OVL_EXPORT(spriteBanks),
-    .cluts = OVL_EXPORT(cluts),
-    .objLayoutHorizontal = NULL,
-    .tileLayers = OVL_EXPORT(rooms_layers),
-    .gfxBanks = OVL_EXPORT(gfxBanks),
-    .UpdateStageEntities = UpdateStageEntities,
-    .unk2C = NULL,
-    .unk30 = D_pspeu_09293958,
-    .unk34 = NULL,
-    .unk38 = NULL,
-    .StageEndCutScene = NULL,
-};
-#else
 
 void Update(void);
 void HitDetection(void);
 void UpdateRoomPosition(void);
 void InitRoomEntities(s32 objLayoutId);
+void UpdateStageEntities(void);
 
 extern RoomHeader OVL_EXPORT(rooms)[];
-extern s16** OVL_EXPORT(spriteBanks)[];
 extern u_long* OVL_EXPORT(cluts)[];
 extern RoomDef OVL_EXPORT(rooms_layers)[];
+
+#ifdef VERSION_PSP
+extern SpriteParts* OVL_EXPORT(spriteBanks)[];
+extern GfxBank* OVL_EXPORT(gfxBanks)[];
+#else
+extern s16** OVL_EXPORT(spriteBanks)[];
 extern u_long** OVL_EXPORT(gfxBanks)[];
+#endif
+
+#ifdef VERSION_PSP
+// overlay
+u8* D_pspeu_09293958[];
+#endif
 
 Overlay OVL_EXPORT(Overlay) = {
     .Update = Update,
@@ -55,8 +36,16 @@ Overlay OVL_EXPORT(Overlay) = {
     .tileLayers = OVL_EXPORT(rooms_layers),
     .gfxBanks = OVL_EXPORT(gfxBanks),
     .UpdateStageEntities = UpdateStageEntities,
+#ifdef VERSION_PSP
+    .unk2C = NULL,
+    .unk30 = D_pspeu_09293958,
+    .unk34 = NULL,
+    .unk38 = NULL,
+    .StageEndCutScene = NULL,
+#endif
 };
 
+#ifndef VERSION_PSP
 #include "gen/sprite_banks.h"
 #include "gen/palette_def.h"
 #include "gen/layers.h"
