dd5cd5b3d14d1a0e375790893bf032b4a9f83f9e
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-06-16 13:47:39 +1000

    Dedupe `stcen` `e_collect`

--- src/st/cen_psp/unk_207E8.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../cen/cen.h"
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/unk_207E8", func_psp_0923C2F8);
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/unk_207E8", func_psp_0923C390);
