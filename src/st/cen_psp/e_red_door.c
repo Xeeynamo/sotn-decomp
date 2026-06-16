1081a380bb96fd07df7bbab6caf4ce3a4d5b6cbc
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-06-16 13:55:43 +1000

    Dedupe `stcen` `e_red_door`

--- src/st/cen_psp/e_red_door.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../cen/cen.h"
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/e_red_door", CEN_EntityRedDoor);
