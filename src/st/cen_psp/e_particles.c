22abf6360d2c4a28baa28c02727b8e6ccb7c026f
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-06-16 13:52:12 +1000

    Dedupe `stcen` `e_particles`

--- src/st/cen_psp/e_particles.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../cen/cen.h"
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/e_particles", EntitySoulStealOrb);
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/e_particles", EntityEnemyBlood);
