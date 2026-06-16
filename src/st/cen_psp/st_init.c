347fbdac4d7d7e58b944fcff0625848f8a8de388
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-06-16 14:22:28 +1000

    Clean up `st_init` splits

--- src/st/cen_psp/st_init.c
+++ src/st/cen_psp/st_init.c
@@ -2,3 +2,7 @@
 #include "../cen/cen.h"
 
 INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/st_init", GetLangAt);
+
+INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/st_init", func_pspeu_092608E0);
+
+INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/st_init", CEN_Load);
--- src/st/cen_psp/unk_29260.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../cen/cen.h"
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/unk_29260", func_pspeu_092608E0);
-
-INCLUDE_ASM("st/cen_psp/nonmatchings/cen_psp/unk_29260", CEN_Load);
