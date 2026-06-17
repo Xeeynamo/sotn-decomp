659295ed30a2c260a6118869ca74b77381ed1043
Joey Murphy <tjmurphy@protonmail.com>
2026-06-16 20:48:49 -0400

    i guess this works for now but ugh

--- src/st/rno3/e_castle_door.c
+++ src/st/rno3/e_castle_door.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno3.h"
 
-extern EInit g_EInitInteractable;
-
-#include "../e_castle_door.h"
+INCLUDE_ASM("st/rno3/nonmatchings/e_castle_door", EntityCastleDoor);
---
+++ src/st/rno3_psp/e_castle_door.c
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../rno3/rno3.h"
+
+extern EInit g_EInitInteractable;
+#include "../e_castle_door.h"
