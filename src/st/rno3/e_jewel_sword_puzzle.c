45672bc49bee275a4c466122ed33cf89bfe786f9
Joey Murphy <tjmurphy@protonmail.com>
2026-06-21 18:21:08 -0400

    nearly unified

--- src/st/rno3/e_jewel_sword_puzzle.c
+++ src/st/rno3/e_jewel_sword_puzzle.c
@@ -9,17 +9,24 @@ extern EInit g_EInitInteractable;
 extern EInit D_us_80180A34;
 
 #if defined(INVERTED_STAGE)
-#define CASTLE_FLAG JEWEL_ROOM_STEPS
+#define CF_STEPS JEWEL_ROOM_STEPS
+#define CF_OPEN JEWEL_ROOM_OPEN
 #define PLUSMINUS -
 #define PME -=
+#define MPE +=
 #define LEFT_TILESTART 0x40E
+#define RIGHT_TILESTART 0x402
+#define JEWELROOM_TILESTART 0x1DF
 #else
-#define CASTLE_FLAG JEWEL_SWORD_ROOM_STEPS
+#define CF_STEPS JEWEL_SWORD_ROOM_STEPS
+#define CF_OPEN JEWEL_SWORD_ROOM_OPEN
 #define PLUSMINUS +
 #define PME +=
+#define MPE -=
 #define LEFT_TILESTART 0x1F1
+#define RIGHT_TILESTART 0x1FD
+#define JEWELROOM_TILESTART 0x420
 #endif
-
 
 // why devs why
 #ifdef STAGE_IS_NO3
@@ -92,7 +99,7 @@ void EntityMermanRockLeftSide(Entity* self) {
             tilePos PME 0x30;
         }
 
-        if (g_CastleFlags[CASTLE_FLAG] & rockBroken) {
+        if (g_CastleFlags[CF_STEPS] & rockBroken) {
             tilePos = LEFT_TILESTART;
             tileLayoutPtr = &leftRockTiles[12];
             for (i = 0; i < 3; i++, tileLayoutPtr++) {
@@ -157,7 +164,7 @@ void EntityMermanRockLeftSide(Entity* self) {
                 CreateEntityFromEntity(E_EQUIP_ITEM_DROP, self, newEntity);
                 newEntity->params = ITEM_POT_ROAST;
             }
-            g_CastleFlags[JEWEL_ROOM_STEPS] |= rockBroken;
+            g_CastleFlags[CF_STEPS] |= rockBroken;
             self->hitboxState = 1;
             self->step++;
         }
@@ -165,12 +172,13 @@ void EntityMermanRockLeftSide(Entity* self) {
 
     case 2:
         if ((self->hitFlags) && (g_Player.status & PLAYER_STATUS_WOLF_FORM)) {
-            g_CastleFlags[JEWEL_ROOM_STEPS] |= wolfFlag;
+            g_CastleFlags[CF_STEPS] |= wolfFlag;
         }
         break;
     }
 }
 
+// right side of the merman room rock, breaks when hit
 void EntityMermanRockRightSide(Entity* self) {
     const int rockBroken = (1 << 1);
     const int batFlag = (1 << 3);
@@ -188,20 +196,20 @@ void EntityMermanRockRightSide(Entity* self) {
         self->hitboxHeight = 24;
 
         tileLayoutPtr = &rockTiles3[6];
-        tilePos = 0x402;
+        tilePos = RIGHT_TILESTART;
         for (i = 0; i < 3; i++, tileLayoutPtr++) {
             g_BgLayers[0].layout[tilePos] = *tileLayoutPtr;
-            *(&g_BgLayers[0].layout[tilePos] - 1) = *(tileLayoutPtr + 3);
-            tilePos -= 0x30;
+            *(&g_BgLayers[0].layout[tilePos] PLUSMINUS 1) = *(tileLayoutPtr + 3);
+            tilePos PME 0x30;
         }
 
-        if (g_CastleFlags[JEWEL_ROOM_STEPS] & rockBroken) {
-            tilePos = 0x402;
+        if (g_CastleFlags[CF_STEPS] & rockBroken) {
+            tilePos = RIGHT_TILESTART;
             tileLayoutPtr = &rightRockTiles[12];
             for (i = 0; i < 3; i++, tileLayoutPtr++) {
                 g_Tilemap.fg[tilePos] = *tileLayoutPtr;
-                *(&g_Tilemap.fg[tilePos] - 1) = *(tileLayoutPtr + 3);
-                tilePos -= 0x30;
+                *(&g_Tilemap.fg[tilePos] PLUSMINUS 1) = *(tileLayoutPtr + 3);
+                tilePos PME 0x30;
             }
             self->hitboxState = 1;
             self->step = 2;
@@ -211,11 +219,11 @@ void EntityMermanRockRightSide(Entity* self) {
     case 1:
         if (self->hitFlags) {
             tileLayoutPtr = &rightRockTiles[self->ext.mermanRock.unk84 * 6] + 6;
-            tilePos = 0x402;
+            tilePos = RIGHT_TILESTART;
             for (i = 0; i < 3; i++, tileLayoutPtr++) {
                 g_Tilemap.fg[tilePos] = *tileLayoutPtr;
-                *(&g_Tilemap.fg[tilePos] - 1) = *(tileLayoutPtr + 3);
-                tilePos -= 0x30;
+                *(&g_Tilemap.fg[tilePos] PLUSMINUS 1) = *(tileLayoutPtr + 3);
+                tilePos PME 0x30;
             }
 
             g_api.PlaySfx(SFX_WALL_DEBRIS_B);
@@ -225,7 +233,7 @@ void EntityMermanRockRightSide(Entity* self) {
                 CreateEntityFromEntity(E_EXPLOSION, self, newEntity);
                 newEntity->params = 0x13;
                 newEntity->zPriority = 0xA9;
-                newEntity->posX.i.hi += self->ext.mermanRock.unk84 * 16;
+                newEntity->posX.i.hi MPE self->ext.mermanRock.unk84 * 16;
                 newEntity->posY.i.hi += 16;
             }
 
@@ -237,17 +245,24 @@ void EntityMermanRockRightSide(Entity* self) {
                     CreateEntityFromEntity(
                         E_ID(FALLING_ROCK_2), self, newEntity);
                     newEntity->params = *params++;
+                    #if defined(INVERTED_STAGE)
                     newEntity->velocityX = (-Random() << 8) - 0x10000;
                     newEntity->velocityY = -Random() * 0x100;
                     newEntity->posY.i.hi -= -16 + (i * 16);
                     newEntity->facingLeft = 0;
+                    #else
+                    newEntity->velocityX = (Random() << 8) + 0x8000;
+                    newEntity->velocityY = -Random() * 0x100;
+                    newEntity->posY.i.hi += -16 + (i * 16);
+                    newEntity->facingLeft = 1;
+                    #endif
                 }
             }
             self->ext.mermanRock.unk84++;
         }
 
         if (self->ext.mermanRock.unk84 > 1) {
-            g_CastleFlags[JEWEL_ROOM_STEPS] |= rockBroken;
+            g_CastleFlags[CF_STEPS] |= rockBroken;
             self->hitboxState = 1;
             self->step++;
         }
@@ -255,7 +270,7 @@ void EntityMermanRockRightSide(Entity* self) {
 
     case 2:
         if ((self->hitFlags) && (g_Player.status & PLAYER_STATUS_BAT_FORM)) {
-            g_CastleFlags[JEWEL_ROOM_STEPS] |= batFlag;
+            g_CastleFlags[CF_STEPS] |= batFlag;
         }
         break;
     }
@@ -270,14 +285,14 @@ void EntityJewelSwordDoor(Entity* self) {
     switch (self->step) {
     case 0:
         InitializeEntity(g_EInitInteractable);
-        if (g_CastleFlags[JEWEL_ROOM_OPEN]) {
+        if (g_CastleFlags[CF_OPEN]) {
             self->step = 2;
         }
         break;
 
     case 1:
-        if ((g_CastleFlags[JEWEL_ROOM_STEPS] & 4) &&
-            (g_CastleFlags[JEWEL_ROOM_STEPS] & 8)) {
+        if ((g_CastleFlags[CF_STEPS] & 4) &&
+            (g_CastleFlags[CF_STEPS] & 8)) {
             PlaySfxPositional(SFX_WALL_DEBRIS_B);
             self->step++;
         }
@@ -285,14 +300,14 @@ void EntityJewelSwordDoor(Entity* self) {
 
     case 2:
         for (tileLayoutPtr = &rockTiles3[27], i = 0; i < 3; i++) {
-            tileLayoutPos = 0x1DF - i;
+            tileLayoutPos = JEWELROOM_TILESTART - i;
             for (j = 0; j < 5; tileLayoutPos -= 0x30, j++, tileLayoutPtr++) {
                 g_Tilemap.fg[tileLayoutPos] = *tileLayoutPtr;
             }
         }
 
         for (tileLayoutPtr = &rockTiles4[11], i = 0; i < 3; i++) {
-            tileLayoutPos = 0x1DF - i;
+            tileLayoutPos = JEWELROOM_TILESTART - i;
             for (j = 0; j < 5; j++, tileLayoutPtr++) {
                 g_BgLayers[0].layout[tileLayoutPos] = *tileLayoutPtr;
                 tileLayoutPos -= 0x30;
