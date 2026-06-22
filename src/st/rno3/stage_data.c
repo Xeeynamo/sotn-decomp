896c87a38e3deb4b1ecd8e786771f3201234bf5a
Joey Murphy <tjmurphy@protonmail.com>
2026-06-22 08:19:59 -0400

    Palettes

---
+++ src/st/rno3/stage_data.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+u16 D_8018FF08[] = {
+#include "gen/D_8018FF08.h"
+};
+u16 D_80190008[] = {
+#include "gen/D_80190008.h"
+};
+u16 D_801902C8[] = {
+#include "gen/D_801902C8.h"
+};
+u16 D_80190308[] = {
+#include "gen/D_80190308.h"
+};
+u16 D_801903A8[] = {
+#include "gen/D_801903A8.h"
+};
+u16 D_80190948[] = {
+#include "gen/D_80190948.h"
+};
+u16 D_80190E68[] = {
+#include "gen/D_80190E68.h"
+};
