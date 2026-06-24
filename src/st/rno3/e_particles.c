6456ab0b24749ddb1b6d99deb04ad4083335e8be
Joey Murphy <tjmurphy@protonmail.com>
2026-06-24 19:42:57 -0400

    particles all good

--- src/st/rno3/e_particles.c
+++ src/st/rno3/e_particles.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno3.h"
 
-INCLUDE_ASM("st/rno3/nonmatchings/e_particles", EntitySoulStealOrb);
-
-INCLUDE_ASM("st/rno3/nonmatchings/e_particles", EntityEnemyBlood);
+#include "../e_particles.h"
