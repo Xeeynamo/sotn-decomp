764444133a9b2d16d0c42541beaab3054f7f26c5
Joey Murphy <tjmurphy@protonmail.com>
2026-06-24 19:48:03 -0400

    room fg all good

--- src/st/rno3_psp/e_particles.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rno3/rno3.h"
-
-INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_particles", EntitySoulStealOrb);
-
-INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_particles", EntityEnemyBlood);
