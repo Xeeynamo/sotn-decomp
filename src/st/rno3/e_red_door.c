393ee5c30ecbac15dac6861badebb1764942d0bf
Joey Murphy <tjmurphy@protonmail.com>
2026-06-24 19:20:19 -0400

    Red door all good

--- src/st/rno3/e_red_door.c
+++ src/st/rno3/e_red_door.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno3.h"
 
-INCLUDE_ASM("st/rno3/nonmatchings/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/rno3/nonmatchings/e_red_door", RNO3_EntityRedDoor);
+#include "../e_red_door.h"
