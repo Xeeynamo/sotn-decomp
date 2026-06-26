0df064057027c5c0e1b199f5b98640edda3656b6
Joey Murphy <tjmurphy@protonmail.com>
2026-06-26 07:53:16 -0400

    split US on nova skeleton

---
+++ src/st/rno3/e_nova_skeleton.c
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "rno3.h"
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C2740);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C27E0);
+
+INCLUDE_RODATA("st/rno3/nonmatchings/e_nova_skeleton", D_us_801B013C);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", EntityNovaSkeleton);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", EntityBladeSoldierDeathParts);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C2FF0);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C34A0);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C35F8);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C3DE0);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C406C);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C4178);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C4334);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C4468);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C48D8);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C4B44);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_nova_skeleton", func_us_801C4C50);
--- src/st/rno3/unk_41DCC.c
+++ src/st/rno3/unk_41DCC.c
@@ -9,34 +9,3 @@ INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C2380);
 
 INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C247C);
 
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C2740);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C27E0);
-
-INCLUDE_RODATA("st/rno3/nonmatchings/unk_41DCC", D_us_801B013C);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", EntityNovaSkeleton);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", EntityBladeSoldierDeathParts);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C2FF0);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C34A0);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C35F8);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C3DE0);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C406C);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C4178);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C4334);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C4468);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C48D8);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C4B44);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_41DCC", func_us_801C4C50);
