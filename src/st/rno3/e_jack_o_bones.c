583c2b19924cf3d1d5a11b5d0b99f847edb069e6
Joey Murphy <tjmurphy@protonmail.com>
2026-06-26 07:55:56 -0400

    pull out jackobones

---
+++ src/st/rno3/e_jack_o_bones.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "rno3.h"
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_jack_o_bones", func_us_801C1DCC);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_jack_o_bones", EntityJackOBones);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_jack_o_bones", func_us_801C2380);
+
+INCLUDE_ASM("st/rno3/nonmatchings/e_jack_o_bones", func_us_801C247C);
+
