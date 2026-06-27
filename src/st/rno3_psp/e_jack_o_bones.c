fe9d2a40d54d7217de5a14c9b0a6a8a7317131dd
Joey Murphy <tjmurphy@protonmail.com>
2026-06-27 07:54:40 -0400

    format

--- src/st/rno3_psp/e_jack_o_bones.c
+++ src/st/rno3_psp/e_jack_o_bones.c
@@ -1,7 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rno3/rno3.h"
 
-typedef enum{
+extern u16 D_us_80180980;
+extern u16 D_us_8018098C;
+extern u16 D_pspeu_09259080[];
+
+extern s32 D_pspeu_09259028;
+extern s32 D_pspeu_09259038;
+extern s32 D_pspeu_09259048;
+extern s32 D_pspeu_09259060;
+extern s32 D_pspeu_09259070;
+extern u8 D_pspeu_09259090[];
+extern s32 D_pspeu_09259098[];
+extern s32 D_pspeu_092590B8[];
+extern s16 D_pspeu_092590D8[];
+extern s16 D_pspeu_092590E8[];
+extern u8 D_pspeu_092590F8[][4];
+extern s32 D_pspeu_09259100;
+extern u16 g_EInitJackOBones;
+
+typedef enum {
     JACKO_INIT,
     JACKO_1,
     JACKO_2,
@@ -31,21 +49,6 @@ void func_pspeu_0923DEB0(void) {
     g_CurrentEntity->ext.ILLEGAL.u8[0] -= 1;
 }
 
-
-extern s32 D_pspeu_09259028;
-extern s32 D_pspeu_09259038;
-extern s32 D_pspeu_09259048;
-extern s32 D_pspeu_09259060;
-extern s32 D_pspeu_09259070;
-extern u8 D_pspeu_09259090[];
-extern s32 D_pspeu_09259098[];
-extern s32 D_pspeu_092590B8[];
-extern s16 D_pspeu_092590D8[];
-extern s16 D_pspeu_092590E8[];
-extern u8 D_pspeu_092590F8[][4];
-extern s32 D_pspeu_09259100;
-extern u16 g_EInitJackOBones;
-
 void EntityJackOBones(Entity* self) {
     s32 xShift;
     u8 var_s2;
@@ -55,8 +58,8 @@ void EntityJackOBones(Entity* self) {
     if (self->flags & FLAG_DEAD) {
         self->step = JACKO_DEAD;
     }
-    switch (self->step) {                              
-    case JACKO_INIT:                                         
+    switch (self->step) {
+    case JACKO_INIT:
         InitializeEntity(&g_EInitJackOBones);
         if (self->params) {
             self->palette++;
@@ -66,13 +69,13 @@ void EntityJackOBones(Entity* self) {
         self->ext.ILLEGAL.u8[4] = 0;
         self->ext.ILLEGAL.u8[8] = 0;
         break;
-    case JACKO_1:                                         
+    case JACKO_1:
         if (UnkCollisionFunc3(&D_pspeu_09259100) == 0) {
             break;
         }
         self->step++;
         break;
-    case JACKO_2:                                         
+    case JACKO_2:
         if (AnimateEntity(&D_pspeu_09259028, self) == 0) {
             self->facingLeft = (GetSideToPlayer() & 1) ^ 1;
         }
@@ -87,7 +90,7 @@ void EntityJackOBones(Entity* self) {
         }
         func_pspeu_0923DEB0();
         break;
-    case JACKO_3:                                         
+    case JACKO_3:
         if (AnimateEntity(&D_pspeu_09259038, self) == 0) {
             self->facingLeft = (GetSideToPlayer() & 1) ^ 1;
         }
@@ -102,7 +105,7 @@ void EntityJackOBones(Entity* self) {
         }
         func_pspeu_0923DEB0();
         break;
-    case JACKO_4:                                         
+    case JACKO_4:
         var_s2 = AnimateEntity(&D_pspeu_09259048, self);
         // We set a value here but it is never used.
         if (self->params) {
@@ -110,17 +113,18 @@ void EntityJackOBones(Entity* self) {
         } else {
             i = 10;
         }
-        if (!var_s2){
+        if (!var_s2) {
             SetStep(JACKO_3);
             var_s2 = ++self->ext.ILLEGAL.u8[8] & 3;
-            self->ext.ILLEGAL.u8[0] = D_pspeu_092590F8[self->params & 1][var_s2];
+            self->ext.ILLEGAL.u8[0] =
+                D_pspeu_092590F8[self->params & 1][var_s2];
             break;
         }
         if ((var_s2 & 0x80) && (self->animCurFrame == 0xB)) {
             other = AllocEntity(&g_Entities[160], &g_Entities[192]);
             if (other != NULL) {
-                PlaySfxPositional(0x6C8);
-                CreateEntityFromCurrentEntity(0x26, other);
+                PlaySfxPositional(SFX_BONE_THROW);
+                CreateEntityFromCurrentEntity(E_UNK_26, other);
                 if (self->params) {
                     xShift = -0x10;
                 } else {
@@ -137,9 +141,9 @@ void EntityJackOBones(Entity* self) {
             }
         }
         break;
-    case JACKO_5:                                         
+    case JACKO_5:
         switch (self->step_s) {
-        case 0:                                     
+        case 0:
             if (!(AnimateEntity(&D_pspeu_09259060, self) & 1)) {
                 var_s2 = self->ext.ILLEGAL.u8[4];
                 if (!(Random() & 3)) {
@@ -156,26 +160,26 @@ void EntityJackOBones(Entity* self) {
                 self->step_s++;
             }
             break;
-        case 1:                                     
+        case 1:
             if (UnkCollisionFunc3(&D_pspeu_09259100)) {
                 self->step_s++;
             }
             CheckFieldCollision(D_pspeu_09259118, 2);
             break;
-        case 2:                                     
+        case 2:
             if (AnimateEntity(&D_pspeu_09259070, self) == 0) {
                 SetStep(JACKO_3);
             }
         }
         break;
-    case 6:                                         
-        PlaySfxPositional(0x62A);
-        for(i = 0; i < 6; i++){
+    case 6:
+        PlaySfxPositional(SFX_SKELETON_DEATH_B);
+        for (i = 0; i < 6; i++) {
             other = AllocEntity(&g_Entities[224], &g_Entities[256]);
             if (other == NULL) {
                 break;
             }
-            CreateEntityFromCurrentEntity(0x25, other);
+            CreateEntityFromCurrentEntity(E_UNK_25, other);
             other->facingLeft = self->facingLeft;
             other->params = i;
             other->params |= (self->params << 8);
@@ -188,15 +192,11 @@ void EntityJackOBones(Entity* self) {
             other->posY.i.hi += D_pspeu_092590E8[i];
             other->velocityX = D_pspeu_09259098[i];
             other->velocityY = D_pspeu_092590B8[i];
-            
         }
         DestroyEntity(self);
         break;
     }
 }
-
-extern u16 D_us_80180980;
-extern u16 D_pspeu_09259080[];
 
 void func_us_801C2380(Entity* self) {
     if (self->step) {
@@ -217,13 +217,11 @@ void func_us_801C2380(Entity* self) {
     if (self->params & 0x100) {
         self->palette += 1;
     }
-    self->drawFlags = 4;
+    self->drawFlags = ENTITY_ROTATE;
     if (self->facingLeft) {
         self->velocityX = -self->velocityX;
     }
 }
-
-extern u16 D_us_8018098C;
 
 void func_us_801C247C(Entity* self) {
     Collider sp10;
@@ -237,7 +235,7 @@ void func_us_801C247C(Entity* self) {
             self->palette += 1;
         }
         self->animCurFrame = 0x15;
-        self->drawFlags |= 4;
+        self->drawFlags |= ENTITY_ROTATE;
         if (self->params) {
             yVar = 0x40000;
             xVar = 0x10000;
@@ -253,24 +251,24 @@ void func_us_801C247C(Entity* self) {
         self->velocityY = yVar;
     }
     MoveEntity();
-    self->velocityY += 0x3000;
+    self->velocityY += FIX(0.1875);
     self->rotate -= 0x40;
     xVar = self->posX.i.hi;
     yVar = self->posY.i.hi + 5;
     g_api.CheckCollision(xVar, yVar, &sp10, 0);
-    if (sp10.effects & 1) {
-        PlaySfxPositional(0x6A5);
+    if (sp10.effects & EFFECT_SOLID) {
+        PlaySfxPositional(SFX_SKULL_KNOCK_A);
         self->ext.ILLEGAL.s16[8] += 1;
         temp = sp10.unk18;
         xVar = self->posX.i.hi;
-        yVar = self->posY.i.hi -3;
+        yVar = self->posY.i.hi - 3;
         g_api.CheckCollision(xVar, yVar, &sp10, 0);
-        if (sp10.effects & 1) {
+        if (sp10.effects & EFFECT_SOLID) {
             self->velocityX = -self->velocityX;
         } else {
             self->posY.i.hi += temp;
             self->velocityY = -self->velocityY;
-            
+
             if (self->params) {
                 self->velocityY = FIX(-7) / self->ext.ILLEGAL.s16[8];
             } else {
@@ -280,21 +278,21 @@ void func_us_801C247C(Entity* self) {
         xVar = self->posX.i.hi + self->velocityX;
         yVar = self->posY.i.hi;
     }
-    if(self->params){
+    if (self->params) {
         xVar = self->posX.i.hi;
         yVar = self->posY.i.hi - 5;
         g_api.CheckCollision(xVar, yVar, &sp10, 0);
-        if (sp10.effects & 1) {
+        if (sp10.effects & EFFECT_SOLID) {
             self->posY.i.hi += sp10.unk20;
             self->velocityY = abs(self->velocityY);
         }
         if (self->ext.ILLEGAL.s16[8] > 8) {
-            self->flags |= 0x100;
+            self->flags |= FLAG_DEAD;
         }
     }
-    
-    if (self->flags & 0x100) {
-        self->drawFlags = 0;
+
+    if (self->flags & FLAG_DEAD) {
+        self->drawFlags = ENTITY_DEFAULT;
         self->entityId = 2;
         self->pfnUpdate = EntityExplosion;
         self->params = 0;
