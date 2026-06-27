c8dc6e2eb55244e05ff31f656a915ebc4663848b
Joey Murphy <tjmurphy@protonmail.com>
2026-06-27 07:25:12 -0400

    Initial jacko decomp

--- src/st/rno3_psp/e_jack_o_bones.c
+++ src/st/rno3_psp/e_jack_o_bones.c
@@ -22,7 +22,178 @@ void func_pspeu_0923DEB0(void) {
 }
 
 
-INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_jack_o_bones", EntityJackOBones);
+extern s32 D_pspeu_09259028;
+extern s32 D_pspeu_09259038;
+extern s32 D_pspeu_09259048;
+extern s32 D_pspeu_09259060;
+extern s32 D_pspeu_09259070;
+extern u8 D_pspeu_09259090[];
+extern s32 D_pspeu_09259098[];
+extern s32 D_pspeu_092590B8[];
+extern s16 D_pspeu_092590D8[];
+extern s16 D_pspeu_092590E8[];
+extern u8 D_pspeu_092590F8[][4];
+extern s32 D_pspeu_09259100;
+extern u16 g_EInitJackOBones;
+
+typedef enum{
+    JACKO_INIT,
+    JACKO_1,
+    JACKO_2,
+    JACKO_3,
+    JACKO_4,
+    JACKO_5,
+    JACKO_DEAD
+} JackOBonesSteps;
+
+void EntityJackOBones(Entity* self) {
+    s32 xShift;
+    u8 var_s2;
+    s32 i;
+    Entity* other;
+
+    if (self->flags & FLAG_DEAD) {
+        self->step = JACKO_DEAD;
+    }
+    switch (self->step) {                              
+    case JACKO_INIT:                                         
+        InitializeEntity(&g_EInitJackOBones);
+        if (self->params) {
+            self->palette++;
+        }
+        self->facingLeft = (GetSideToPlayer() & 1) ^ 1;
+        self->ext.ILLEGAL.u8[0] = 0x50;
+        self->ext.ILLEGAL.u8[4] = 0;
+        self->ext.ILLEGAL.u8[8] = 0;
+        break;
+    case JACKO_1:                                         
+        if (UnkCollisionFunc3(&D_pspeu_09259100) == 0) {
+            break;
+        }
+        self->step++;
+        break;
+    case JACKO_2:                                         
+        if (AnimateEntity(&D_pspeu_09259028, self) == 0) {
+            self->facingLeft = (GetSideToPlayer() & 1) ^ 1;
+        }
+        self->ext.ILLEGAL.u8[4] = self->facingLeft;
+        if (self->ext.ILLEGAL.u8[4]) {
+            self->velocityX = FIX(0.5);
+        } else {
+            self->velocityX = FIX(-0.5);
+        }
+        if (GetDistanceToPlayerX() < 76) {
+            self->step = JACKO_3;
+        }
+        func_pspeu_0923DEB0();
+        break;
+    case JACKO_3:                                         
+        if (AnimateEntity(&D_pspeu_09259038, self) == 0) {
+            self->facingLeft = (GetSideToPlayer() & 1) ^ 1;
+        }
+        self->ext.ILLEGAL.u8[4] = self->facingLeft ^ 1;
+        if (self->ext.ILLEGAL.u8[4]) {
+            self->velocityX = FIX(0.5);
+        } else {
+            self->velocityX = FIX(-0.5);
+        }
+        if (GetDistanceToPlayerX() > 92) {
+            self->step = JACKO_2;
+        }
+        func_pspeu_0923DEB0();
+        break;
+    case JACKO_4:                                         
+        var_s2 = AnimateEntity(&D_pspeu_09259048, self);
+        // We set a value here but it is never used.
+        if (self->params) {
+            i = 11;
+        } else {
+            i = 10;
+        }
+        if (!var_s2){
+            SetStep(JACKO_3);
+            var_s2 = ++self->ext.ILLEGAL.u8[8] & 3;
+            self->ext.ILLEGAL.u8[0] = D_pspeu_092590F8[self->params & 1][var_s2];
+            break;
+        }
+        if ((var_s2 & 0x80) && (self->animCurFrame == 0xB)) {
+            other = AllocEntity(&g_Entities[160], &g_Entities[192]);
+            if (other != NULL) {
+                PlaySfxPositional(0x6C8);
+                CreateEntityFromCurrentEntity(0x26, other);
+                if (self->params) {
+                    xShift = -0x10;
+                } else {
+                    xShift = 8;
+                }
+                if (self->facingLeft) {
+                    other->posX.i.hi -= xShift;
+                } else {
+                    other->posX.i.hi += xShift;
+                }
+                other->posY.i.hi -= 0x10;
+                other->params = self->params;
+                other->facingLeft = self->facingLeft;
+            }
+        }
+        break;
+    case JACKO_5:                                         
+        switch (self->step_s) {
+        case 0:                                     
+            if (!(AnimateEntity(&D_pspeu_09259060, self) & 1)) {
+                var_s2 = self->ext.ILLEGAL.u8[4];
+                if (!(Random() & 3)) {
+                    var_s2 ^= 1;
+                }
+                if (var_s2) {
+                    self->velocityX = FIX(2);
+                } else {
+                    self->velocityX = FIX(-2);
+                }
+                self->velocityY = FIX(-3);
+                self->pose = 0;
+                self->poseTimer = 0;
+                self->step_s++;
+            }
+            break;
+        case 1:                                     
+            if (UnkCollisionFunc3(&D_pspeu_09259100)) {
+                self->step_s++;
+            }
+            CheckFieldCollision(D_pspeu_09259118, 2);
+            break;
+        case 2:                                     
+            if (AnimateEntity(&D_pspeu_09259070, self) == 0) {
+                SetStep(JACKO_3);
+            }
+        }
+        break;
+    case 6:                                         
+        PlaySfxPositional(0x62A);
+        for(i = 0; i < 6; i++){
+            other = AllocEntity(&g_Entities[224], &g_Entities[256]);
+            if (other == NULL) {
+                break;
+            }
+            CreateEntityFromCurrentEntity(0x25, other);
+            other->facingLeft = self->facingLeft;
+            other->params = i;
+            other->params |= (self->params << 8);
+            other->ext.ILLEGAL.u8[0xC] = D_pspeu_09259090[i];
+            if (self->facingLeft) {
+                other->posX.i.hi -= D_pspeu_092590D8[i];
+            } else {
+                other->posX.i.hi += D_pspeu_092590D8[i];
+            }
+            other->posY.i.hi += D_pspeu_092590E8[i];
+            other->velocityX = D_pspeu_09259098[i];
+            other->velocityY = D_pspeu_092590B8[i];
+            
+        }
+        DestroyEntity(self);
+        break;
+    }
+}
 
 INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_jack_o_bones", func_us_801C2380);
 
