af8bf982b6823e4189dd942e6aa2a9dc43f189bd
Joey Murphy <tjmurphy@protonmail.com>
2026-07-02 07:18:07 -0400

    unsplit psp file

--- src/st/rno3_psp/e_blue_venus_weed.c
+++ src/st/rno3_psp/e_blue_venus_weed.c
@@ -4,3 +4,11 @@
 INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_blue_venus_weed", SetupPrimsForEntitySpriteParts);
 
 INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_blue_venus_weed", EntityVenusWeed);
+
+INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_blue_venus_weed", func_us_801C5850);
+
+INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_blue_venus_weed", func_us_801C5F40);
+
+INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_blue_venus_weed", EntityVenusWeedDart);
+
+INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/e_blue_venus_weed", EntityVenusWeedSpike);
