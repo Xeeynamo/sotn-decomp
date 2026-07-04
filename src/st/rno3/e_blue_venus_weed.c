fffd8699df8678a634e93af602cef592e477b2de
bismurphy <tjmurphy@protonmail.com>
2026-07-03 21:18:15 -0400

    Decompile RNO3 Blue Venus Weed (#3297)
    This one was interesting because we have the Venus Weed (non-blue)
    already in CHI, but this one is the Blue Venus Weed.
    
    The code is extremely similar. I decided for this one to make a #define
    BLUE to switch between the two versions. It became a little clunky in
    places, but I think it worked out okay.

--- src/st/rno3/e_blue_venus_weed.c
+++ src/st/rno3/e_blue_venus_weed.c
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno3.h"
+#define BLUE
+#define SPIKE_SPRITES sprites_rno3_6
+#define TENDRIL_COUNT 8
+#define DART_CLUT_START 0x240
+#define DEATH_CLUT 0x246
+#define PLANT_CLUT 0x250
+#define SPIKE_CLUT_START 0x25A
+#define SPIKE_CLUT_END 0x268
+#define GROWTH_SPEED 2
 
-INCLUDE_ASM("st/rno3/nonmatchings/e_blue_venus_weed", SetupPrimsForEntitySpriteParts);
-
-INCLUDE_ASM("st/rno3/nonmatchings/e_blue_venus_weed", EntityVenusWeed);
-
-INCLUDE_ASM("st/rno3/nonmatchings/e_blue_venus_weed", func_us_801C5850);
-
-INCLUDE_ASM("st/rno3/nonmatchings/e_blue_venus_weed", func_us_801C5F40);
-
-INCLUDE_ASM("st/rno3/nonmatchings/e_blue_venus_weed", EntityVenusWeedDart);
-
-INCLUDE_ASM("st/rno3/nonmatchings/e_blue_venus_weed", EntityVenusWeedSpike);
+#include "../e_venus_weed.h"
