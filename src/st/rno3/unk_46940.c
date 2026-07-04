ebe5750b67b8457299dc67195b10478efd3253a6
Joey Murphy <tjmurphy@protonmail.com>
2026-07-04 14:42:51 -0400

    split out file

--- src/st/rno3/unk_46940.c
+++ src/st/rno3/unk_46940.c
@@ -14,5 +14,3 @@ INCLUDE_ASM("st/rno3/nonmatchings/unk_46940", EntitySideWaterSplash);
 INCLUDE_ASM("st/rno3/nonmatchings/unk_46940", EntitySmallWaterDrop);
 
 INCLUDE_ASM("st/rno3/nonmatchings/unk_46940", EntityWaterDrop);
-
-INCLUDE_ASM("st/rno3/nonmatchings/unk_46940", func_us_801C8B8C);
