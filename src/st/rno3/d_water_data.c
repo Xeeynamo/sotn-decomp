972f548e56a3636c45eca52cd0cb56de1ed4d5c2
Joey Murphy <tjmurphy@protonmail.com>
2026-07-04 15:46:23 -0400

    this is gonna be complicated, first decomp on psp

---
+++ src/st/rno3/d_water_data.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include "rno3.h"
+
+u16 g_WaterSounds[] = {SFX_WATER_SPLASH_JUMP, SFX_WATER_SPLASH_MOVE};
+
+s16 g_WaterXTbl[] = {0, 768, 0, 440, 512, 7, 7, 0, 0, 150, 0, 184, 256, 7, 7, 0, 48, 68, 0, 468, 512, 7, 7, 0, };
--- src/st/rno3_psp/unk_13E00.c
+++ src/st/rno3_psp/unk_13E00.c
@@ -1,7 +1,50 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rno3/rno3.h"
 
-INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/unk_13E00", func_pspeu_0924B480);
+extern EInit g_EInitCommon;
+
+extern u16 g_WaterSounds[];
+extern s16 g_WaterXTbl[];
+
+static s16 g_splashAspects[] = {0, 1, 2, 4, -1, -2, -4, 0};
+
+static u16 func_pspeu_0924B480(s16 arg0, s16 arg1, s16 arg2, s16* arg3) {
+    s16 temp_s2;
+    s16 temp;
+    s16* ptr;
+
+    ptr = &g_WaterXTbl[arg0 * 8];
+    arg1 -= (g_Tilemap.width - *ptr++);
+    temp_s2 = *ptr++;
+    arg1 += temp_s2;
+    if (arg1 < 0) {
+        return 0;
+    }
+    *arg3++ = arg1;
+
+    temp = temp_s2 - arg1;
+    if (temp <= 0) {
+        return 0;
+    }
+    temp_s2 = temp;
+    *arg3 = temp;
+
+    temp = g_splashAspects[*ptr++];
+    if (temp) {
+        temp = temp_s2 / temp;
+    } else {
+        temp = 0;
+    }
+
+    temp = temp + (g_Tilemap.height - *ptr++);
+    if (temp < arg2) {
+        return 0;
+    }
+    if (arg2 <= (g_Tilemap.height - *ptr++)) {
+        return 0;
+    }
+    return ((temp + 0x7FFF) + 1) - arg2;
+}
 
 INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/unk_13E00", EntityAlucardWaterEffect);
 
