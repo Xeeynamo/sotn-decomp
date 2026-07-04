73bf393849f3b18c9e96b4c596e94f25d97a9110
Joey Murphy <tjmurphy@protonmail.com>
2026-07-04 16:10:08 -0400

    port to US

--- src/st/rno3/d_water_data.c
+++ src/st/rno3/d_water_data.c
@@ -5,3 +5,5 @@
 u16 g_WaterSounds[] = {SFX_WATER_SPLASH_JUMP, SFX_WATER_SPLASH_MOVE};
 
 s16 g_WaterXTbl[] = {0, 768, 0, 440, 512, 7, 7, 0, 0, 150, 0, 184, 256, 7, 7, 0, 48, 68, 0, 468, 512, 7, 7, 0, };
+
+s16 unused[] = {0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 257};
