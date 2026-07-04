bf20c9fc52d4a4e1369138f44519ec4ba4e1c3f1
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2026-07-04 01:23:11 +0100

    BO4 macOS CI fix (#3296)

---
+++ src/boss/bo4/sprite_banks.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "bo4.h"
+
+extern s16* D_us_801B0A78[];
+extern s16* D_us_801B159C[];
+extern s16* D_us_801B1664[];
+extern s16* D_us_801B2068[];
+extern s16* D_us_801B252C[];
+
+s16** OVL_EXPORT(spriteBanks)[] = {
+    NULL, D_us_801B0A78, D_us_801B159C, D_us_801B1664, D_us_801B2068, NULL,
+    NULL, NULL,          NULL,          NULL,          NULL,          NULL,
+    NULL, NULL,          NULL,          D_us_801B252C, NULL,          NULL,
+    NULL, NULL,          NULL,          NULL,          NULL,          NULL,
+};
