8d09de3d5b77676ce34ae4693a2b5d880b70cffe
Joey Murphy <tjmurphy@protonmail.com>
2026-07-04 15:53:17 -0400

    next one

--- src/st/rno3_psp/unk_13E00.c
+++ src/st/rno3_psp/unk_13E00.c
@@ -304,7 +304,202 @@ void EntityAlucardWaterEffect(Entity* self) {
     self->ext.aluwater.unk8E = sp10[1];
 }
 
-INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/unk_13E00", EntitySplashWater);
+static s16 unused[] = {
+    0x0000, -0x1000, -0x0C00, -0x0800, 0x1000, 0x0C00, 0x0800, 0x0000};
+static s32 g_SplashYMovement[] = {
+    FIX(-6.5), FIX(9.0 / 32), FIX(-5.5), FIX(17.0 / 64),
+    FIX(-4.5), FIX(0.25),     FIX(-3.5), FIX(15.0 / 64)};
+
+void EntitySplashWater(Entity* self) {
+    s32 primIndex;
+    Tilemap* tilemap = &g_Tilemap;
+    u16 params;
+    u16 index1;
+    u16 width;
+    s16 aspect; // sets y size based on x size
+    u16 index2;
+
+    Primitive *prim, *prim2;
+    Entity* newEntity;
+    s16 selfY;
+    s16 selfX;
+    s32 i;
+
+    params = self->params;
+    width = params / 0x800;
+    index1 = (params >> 8) & 7;
+    index2 = (params >> 5) & 7;
+    params &= 0xF;
+
+    switch (self->step) {
+    case 0:
+        InitializeEntity(g_EInitCommon);
+        if (width && index2 != 7) {
+            primIndex = g_api.AllocPrimitives(PRIM_GT4, 4);
+        } else {
+            primIndex = g_api.AllocPrimitives(PRIM_GT4, 2);
+        }
+        if (primIndex == -1) {
+            DestroyEntity(self);
+            return;
+        }
+        self->flags |= FLAG_HAS_PRIMS;
+        self->primIndex = primIndex;
+        prim = &g_PrimBuf[primIndex];
+        i = 0;
+        selfX = self->posX.i.hi;
+        selfY = self->posY.i.hi;
+        self->ext.waterEffects.unk82 = selfY + tilemap->scrollY.i.hi;
+        for (; prim != NULL; i++, prim = prim->next) {
+            if (i & 1) {
+                prim->u0 = prim->u2 = prim2->u0;
+                prim->u1 = prim->u3 = prim2->u1;
+                prim->v0 = prim->v1 = prim2->v0;
+                prim->v2 = prim->v3 = prim2->v2;
+                prim->y2 = prim2->y2;
+                prim->y3 = prim2->y3;
+                prim->x2 = prim->x0 = prim2->x0;
+                prim->x3 = prim->x1 = prim2->x1;
+            } else {
+                prim->u0 = prim->u2 = 0;
+                prim->u1 = prim->u3 = 0x20;
+                prim->v0 = prim->v1 = 96;
+                prim->v2 = prim->v3 = 0x7C;
+                prim->y2 = prim->y3 = selfY;
+                prim->x2 = prim->x0 = selfX - 0xE;
+                prim->x3 = prim->x1 = selfX + 0xE;
+                if (i > 1) {
+                    aspect = g_splashAspects[index2];
+                    if (width > 14) {
+                        prim->u0 = prim->u2 = prim2->u1;
+                        prim->x0 = prim->x2 = prim2->x1;
+                        prim->y2 = prim2->y3;
+                        if (aspect) {
+                            prim->y3 =
+                                prim->y2 - (prim->x1 - prim->x0) / aspect;
+                        } else {
+                            prim->y3 = prim->y2;
+                        }
+                    } else {
+                        prim->u1 = prim->u3 = prim2->u0;
+                        prim->x1 = prim->x3 = prim2->x0;
+                        prim->y3 = prim2->y2;
+                        if (aspect) {
+                            prim->y2 =
+                                prim->y3 + (prim->x1 - prim->x0) / aspect;
+                        } else {
+                            prim->y2 = prim->y3;
+                        }
+                    }
+                } else {
+                    if (width) {
+                        if (width > 14) {
+                            prim->u1 = prim->u3 = prim->u0 + (width << 5) / 28;
+                            prim->x1 = prim->x3 = prim->x0 + width;
+                        } else {
+                            prim->u0 = prim->u2 += (width << 5) / 28;
+                            prim->x0 = prim->x2 += width;
+                        }
+                    }
+                    if (index1) {
+                        aspect = g_splashAspects[index1];
+                        if (aspect < 0) {
+                            if (selfX >= prim->x1) {
+                                prim->y2 += (prim->x1 - prim->x0) / aspect;
+                            } else {
+                                prim->y2 += (selfX - prim->x0) / aspect;
+                                prim->y3 -= (prim->x1 - selfX) / aspect;
+                            }
+                        } else if (prim->x0 >= selfX) {
+                            prim->y3 -= (prim->x1 - prim->x0) / aspect;
+                        } else {
+                            prim->y2 += (selfX - prim->x0) / aspect;
+                            prim->y3 -= (prim->x1 - selfX) / aspect;
+                        }
+                    }
+                }
+            }
+            PGREY(prim, 0) = PGREY(prim, 1) = 0xFF;
+            PGREY(prim, 2) = PGREY(prim, 3) = 0x80;
+            prim->clut = PAL_CC_MAGIC_HUD_EFFECT;
+            prim->tpage = 0x1A;
+            prim->priority = self->zPriority + 2;
+            prim->drawMode = DRAW_UNK_40 | DRAW_TPAGE2 | DRAW_TPAGE |
+                             DRAW_COLORS | DRAW_UNK02 | DRAW_TRANSP;
+            if (i & 1) {
+                PGREY(prim, 0) = PGREY(prim, 1) = 0x00;
+                PGREY(prim, 2) = PGREY(prim, 3) = 0x60;
+                prim->clut = PAL_FILL_WHITE;
+                prim->priority += 2;
+                prim->drawMode = DRAW_TPAGE2 | DRAW_TPAGE | DRAW_COLORS |
+                                 DRAW_UNK02 | DRAW_TRANSP;
+            }
+            prim2 = prim;
+        }
+
+        aspect = (self->posX.i.hi - 120) >> 4;
+        if (aspect < -8) {
+            aspect = -8;
+        }
+        if (aspect > 8) {
+            aspect = 8;
+        }
+
+        g_api.PlaySfxVolPan(g_WaterSounds[0], 0x7F, aspect);
+
+        self->velocityY = -g_SplashYMovement[params * 2];
+        self->ext.waterEffects.accelY = g_SplashYMovement[params * 2 + 1];
+
+        newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
+        if (newEntity != NULL) {
+            CreateEntityFromCurrentEntity(E_WATER_DROP, newEntity);
+            newEntity->velocityY = self->velocityY;
+        }
+        break;
+
+    case 1:
+        MoveEntity(self);
+        self->velocityY -= self->ext.waterEffects.accelY;
+        if (self->velocityY < FIX(-2.5)) {
+            self->step++;
+        }
+        break;
+
+    case 2:
+        MoveEntity(self);
+        prim = &g_PrimBuf[self->primIndex];
+        if (prim->r0 < 9) {
+            DestroyEntity(self);
+            return;
+        }
+        break;
+    }
+
+    selfY =
+        self->ext.waterEffects.unk82 - self->posY.i.hi - tilemap->scrollY.i.hi;
+    for (i = 0, prim = &g_PrimBuf[self->primIndex]; prim != NULL; i++,
+        prim = prim->next) {
+        prim->y0 = prim->y2 - selfY;
+        prim->y1 = prim->y3 - selfY;
+        if (i & 1) {
+            if (prim->b3 >= 4) {
+                prim->b3 -= 4;
+            }
+            PGREY(prim, 2) = PGREY(prim, 3);
+        } else {
+            if (prim->b3 >= 8) {
+                prim->b3 -= 4;
+            }
+            PGREY(prim, 2) = PGREY(prim, 3);
+            if (prim->b3 <= 8) {
+                if (prim->b1 >= 8) {
+                    prim->b1 -= 8;
+                }
+                PGREY(prim, 0) = PGREY(prim, 1);
+            }
+        }
+    }
+}
 
 INCLUDE_ASM("st/rno3_psp/nonmatchings/rno3_psp/unk_13E00", EntitySurfacingWater);
 
