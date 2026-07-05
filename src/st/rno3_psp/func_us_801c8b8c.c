1e9fce4e564381fc64558007f8a41eab4a6ed3ab
Joey Murphy <tjmurphy@protonmail.com>
2026-07-04 20:41:58 -0400

    Initial format

--- src/st/rno3_psp/func_us_801c8b8c.c
+++ src/st/rno3_psp/func_us_801c8b8c.c
@@ -1,20 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rno3/rno3.h"
 
+extern EInit g_EInitDodoBird;
+
 extern s32 D_pspeu_09258FB0;
 extern s32 D_pspeu_09258FB8;
 extern s32 D_pspeu_09258FC8;
 extern s32 D_pspeu_09258FD8;
-extern u16 g_EInitDodoBird;
 
-typedef enum {
-    DODO_INIT,
-    DODO_1,
-    DODO_2,
-    DODO_3,
-    DODO_4,
-    DODO_DEAD
-} DodoSteps;
+typedef enum { DODO_INIT, DODO_1, DODO_2, DODO_3, DODO_4, DODO_DEAD } DodoSteps;
 
 void func_us_801C8B8C(Entity* self) {
     Entity* other;
@@ -27,12 +21,12 @@ void func_us_801C8B8C(Entity* self) {
     }
     switch (self->step) {
     case 0:
-        InitializeEntity(&g_EInitDodoBird);
+        InitializeEntity(g_EInitDodoBird);
         self->facingLeft = GetSideToPlayer() & 1;
         if (self->params & 0x100) {
-            self->drawFlags |= 4;
+            self->drawFlags |= ENTITY_ROTATE;
             self->animCurFrame = 9;
-            self->step = 4;
+            self->step = DODO_4;
         }
         break;
     case DODO_1:
@@ -43,24 +37,24 @@ void func_us_801C8B8C(Entity* self) {
     case DODO_2:
         AnimateEntity(&D_pspeu_09258FC8, self);
         if (!(g_Timer & 7)) {
-            PlaySfxPositional(0x6BD);
+            PlaySfxPositional(SFX_QUIET_STEPS);
         }
         temp_v0 = UnkCollisionFunc2(&D_pspeu_09258FB0);
         if (temp_v0 == 0) {
-            self->velocityY += 0x4000;
+            self->velocityY += FIX(0.25);
         } else {
             self->velocityY = 0;
         }
         if (self->facingLeft) {
-            self->velocityX = -0xC000;
+            self->velocityX = FIX(-0.75);
         } else {
-            self->velocityX = 0xC000;
+            self->velocityX = FIX(0.75);
         }
         if (self->ext.ILLEGAL.s16[4]) {
             self->velocityX *= 2;
         }
         if (temp_v0 == 0x80) {
-            self->velocityY = -0x40000;
+            self->velocityY = FIX(-4.0);
             SetStep(DODO_1);
         }
         if (GetDistanceToPlayerX() < 0x40) {
@@ -83,10 +77,10 @@ void func_us_801C8B8C(Entity* self) {
         MoveEntity();
         switch (self->step_s) {
         case 0:
-            self->velocityY += 0x2000;
+            self->velocityY += FIX(0.125);
             if (self->velocityY > 0) {
                 self->velocityY = 0;
-                self->flags |= 0xC0000000;
+                self->flags |= FLAG_DESTROY_IF_OUT_OF_CAMERA | FLAG_DESTROY_IF_BARELY_OUT_OF_CAMERA;
                 self->step_s += 1;
             }
             break;
@@ -106,14 +100,14 @@ void func_us_801C8B8C(Entity* self) {
         case 0:
             self->ext.ILLEGAL.s16[2] = 0x10;
             self->step_s += 1;
-            PlaySfxPositional(0x6D0);
-            self->drawFlags |= 4;
+            PlaySfxPositional(SFX_FLEA_RIDER_EXPLODE);
+            self->drawFlags |= ENTITY_ROTATE;
             /* fallthrough */
         case 1:
             AnimateEntity(&D_pspeu_09258FD8, self);
             self->rotate -= 0x80;
             if (!(self->ext.ILLEGAL.s16[2] & 3)) {
-                for(i = 0; i < 5; i++) {
+                for (i = 0; i < 5; i++) {
                     other = AllocEntity(&g_Entities[224], &g_Entities[256]);
                     if (other != NULL) {
                         CreateEntityFromEntity(0x4EU, self, other);
