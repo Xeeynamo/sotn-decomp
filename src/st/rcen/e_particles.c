fe0bbadc85261b39353a07ef5013d1de003a960f
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-08 11:21:04 +1000

    Dedupe `e_particles`

--- src/st/rcen/e_particles.c
+++ src/st/rcen/e_particles.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rcen.h"
 
-INCLUDE_ASM("st/rcen/nonmatchings/e_particles", EntitySoulStealOrb);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_particles", EntityEnemyBlood);
+#include "../e_particles.h"
