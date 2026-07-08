515c4a4e2cb6d490f86cfab8f25930475150db32
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-08 10:30:00 +1000

    Dedupe `collision`

--- src/st/rcen/collision.c
+++ src/st/rcen/collision.c
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rcen.h"
 
-INCLUDE_ASM("st/rcen/nonmatchings/collision", HitDetection);
-
-INCLUDE_ASM("st/rcen/nonmatchings/collision", EntityDamageDisplay);
+#include "../collision.h"
--- src/st/rcen_psp/collision.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rcen/rcen.h"
-
-INCLUDE_ASM("st/rcen_psp/nonmatchings/rcen_psp/collision", HitDetection);
-
-INCLUDE_ASM("st/rcen_psp/nonmatchings/rcen_psp/collision", EntityDamageDisplay);
