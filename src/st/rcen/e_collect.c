0cc9eac6e2193e0eede4c536923229cf16b9c0f4
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-08 12:19:19 +1000

    Dedupe `e_collect`

--- src/st/rcen/e_collect.c
+++ src/st/rcen/e_collect.c
@@ -1,56 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rcen.h"
 
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", PrizeDropFall);
+#define HEART_DROP_CASTLE_FLAG 0x258
 
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", PrizeDropFall2);
+#ifdef VERSION_PSP
+#define GOLD_COLLECT_TEXT                                                      \
+    _S("$1"), _S("$25"), _S("$50"), _S("$100"), _S("$250"), _S("$400"),        \
+        _S("$700"), _S("$1000"), _S("$2000"), _S("$5000"),
+#endif
 
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", CollectHeart);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", CollectGold);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", CollectSubweapon);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", CollectHeartVessel);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", CollectLifeVessel);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", CollectDummy);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A19C);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1A4);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1AC);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1B4);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1BC);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1C4);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1CC);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1D4);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1DC);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A1E4);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", EntityPrizeDrop);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", EntityExplosion);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", BlinkItem);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", EntityEquipItemDrop);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", BlitChar);
-
-INCLUDE_RODATA("st/rcen/nonmatchings/e_collect", D_us_8019A224);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", EntityRelicOrb);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", EntityHeartDrop);
-
-INCLUDE_ASM("st/rcen/nonmatchings/e_collect", EntityMessageBox);
+#include "../e_collect.h"
