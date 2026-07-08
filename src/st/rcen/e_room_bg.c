da3707d45cb0bf964fba2e15e3ee64ea712870c0
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-08 11:45:50 +1000

    Dedupe `e_room_bg`

--- src/st/rcen/e_room_bg.c
+++ src/st/rcen/e_room_bg.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rcen.h"
 
-INCLUDE_ASM("st/rcen/nonmatchings/e_room_bg", RCEN_EntityBackgroundBlock);
+#define BG_BLOCK_NEEDS_SCALE
+#include "../e_room_bg.h"
