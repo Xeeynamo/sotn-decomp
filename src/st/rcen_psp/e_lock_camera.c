830c6b4357bf568cd2f614c96e1172b7eace0503
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-08 11:05:33 +1000

    Dedupe `e_lock_camera`

--- src/st/rcen_psp/e_lock_camera.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rcen/rcen.h"
-
-INCLUDE_ASM("st/rcen_psp/nonmatchings/rcen_psp/e_lock_camera", PlayerIsWithinHitbox);
-
-INCLUDE_ASM("st/rcen_psp/nonmatchings/rcen_psp/e_lock_camera", RCEN_EntityLockCamera);
