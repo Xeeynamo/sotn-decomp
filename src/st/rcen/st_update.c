0161279db94a35c3a6029c55f4a285d0fd544e2e
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-08 12:08:21 +1000

    Dedupe `st_update`

--- src/st/rcen/st_update.c
+++ src/st/rcen/st_update.c
@@ -1,8 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rcen.h"
 
-INCLUDE_ASM("st/rcen/nonmatchings/st_update", Random);
-
-INCLUDE_ASM("st/rcen/nonmatchings/st_update", Update);
-
-INCLUDE_ASM("st/rcen/nonmatchings/st_update", UpdateStageEntities);
+#include "../st_update.h"
--- src/st/rcen_psp/st_update.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rcen/rcen.h"
-
-INCLUDE_ASM("st/rcen_psp/nonmatchings/rcen_psp/st_update", Random);
-
-INCLUDE_ASM("st/rcen_psp/nonmatchings/rcen_psp/st_update", Update);
-
-INCLUDE_ASM("st/rcen_psp/nonmatchings/rcen_psp/st_update", UpdateStageEntities);
