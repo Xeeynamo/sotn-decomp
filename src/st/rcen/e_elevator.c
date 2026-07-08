74395edd133853dde216636b729a498120f671d7
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-08 15:03:14 +1000

    Name elevator segment correctly

--- src/st/rcen/e_elevator.c
+++ src/st/rcen/e_elevator.c
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rcen.h"
 
+// Unused on PSP, see UnusedPrimFunction in CEN
 INCLUDE_ASM("st/rcen/nonmatchings/e_elevator", func_us_8019FD4C);
+
+INCLUDE_ASM("st/rcen/nonmatchings/e_elevator", func_801904B8);
+
+INCLUDE_ASM("st/rcen/nonmatchings/e_elevator", func_us_8019FE9C);
+
+INCLUDE_ASM("st/rcen/nonmatchings/e_elevator", EntityUnkId1B);
