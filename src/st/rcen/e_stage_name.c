01fa9638116fc26ebf57e7606ab6f641b88d8314
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-08 11:50:33 +1000

    Dedupe `e_stage_name`

--- src/st/rcen/e_stage_name.c
+++ src/st/rcen/e_stage_name.c
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rcen.h"
 
-INCLUDE_ASM("st/rcen/nonmatchings/e_stage_name", StageNamePopupDissolver);
+#ifdef VERSION_US
+#include "../e_stage_name_us.h"
+#endif
 
-INCLUDE_ASM("st/rcen/nonmatchings/e_stage_name", EntityStageNamePopup);
+#ifdef VERSION_PSP
+#include "../e_stage_name_jp.h"
+#endif
