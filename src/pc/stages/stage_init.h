c8c716a8c432dd9b93ab3cc8accbc0a15b08c85a
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2026-07-11 23:09:33 +0100

    pc: convert strings on stage loading (#3330)
    Follow-up to
    https://github.com/Xeeynamo/sotn-decomp/pull/3328#discussion_r3564598290
    . Currently it doesn't work as well as stages still load entities from
    other stages. It should be fixed as we keep migrating to `OVL_EXPORT`.
    
    EDIT: confirmed working with the integration of #3333

---
+++ src/pc/stages/stage_init.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+extern const char* OVL_EXPORT(GoldCollectTexts)[10];
+static void InitOnce() {
+    static bool is_initialized = 0;
+    if (is_initialized) {
+        return;
+    }
+    is_initialized = 1;
+
+    for (int i = 0; i < LEN(OVL_EXPORT(GoldCollectTexts)); i++) {
+        OVL_EXPORT(GoldCollectTexts)
+        [i] = AnsiToSotnMenuString(OVL_EXPORT(GoldCollectTexts)[i]);
+    }
+}
--- src/pc/stages/stage_nz0.c
+++ src/pc/stages/stage_nz0.c
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include <game.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "stage_loader.h"
 #include "../st/nz0/nz0.h"
+#include "stage_loader.h"
+#include "stage_init.h"
 
 extern AbbreviatedOverlay OVL_EXPORT(Overlay);
 extern PfnEntityUpdate OVL_EXPORT(EntityUpdates)[];
@@ -15,6 +14,7 @@ extern LayoutEntity** g_pStObjLayoutHorizontal;
 extern LayoutEntity** g_pStObjLayoutVertical;
 void InitStageNZ0(Overlay* o) {
     LoadReset();
+    InitOnce();
     memcpy(o, &OVL_EXPORT(Overlay), sizeof(AbbreviatedOverlay));
     PfnEntityUpdates = OVL_EXPORT(EntityUpdates);
     g_pStObjLayoutHorizontal = OVL_EXPORT(pStObjLayoutHorizontal);
