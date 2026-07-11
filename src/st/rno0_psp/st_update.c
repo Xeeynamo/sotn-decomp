1cface5446d7c5756b68cc512b45404a16c09650
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-07-11 17:04:18 +0100

    RNO0 ovl export

--- src/st/rno0_psp/st_update.c
+++ src/st/rno0_psp/st_update.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rno0/rno0.h"
 
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/st_update", Random);
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/st_update", RNO0_Random);
 
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/st_update", Update);
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/st_update", RNO0_Update);
 
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/st_update", UpdateStageEntities);
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/st_update", RNO0_UpdateStageEntities);
