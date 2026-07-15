b2a1acd9d739f0a6978a108d76cf0619a230da92
Joey Murphy <tjmurphy@protonmail.com>
2026-07-15 18:37:03 -0400

    Palettes

--- src/st/rtop/header.c
+++ src/st/rtop/header.c
@@ -9,7 +9,7 @@ extern RoomDef OVL_EXPORT(rooms_layers)[];
 extern GfxBank* OVL_EXPORT(gfxBanks)[];
 extern u8* D_us_80182E44[];
 
-Overlay OVL_EXPORT(Overaly) = {
+Overlay OVL_EXPORT(Overlay) = {
     .Update = Update,
     .HitDetection = HitDetection,
     .UpdateRoomPosition = UpdateRoomPosition,
---
+++ src/st/rtop/stage_data.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+u16 D_8018AC34[] = {
+#include "gen/D_8018AC34.h"
+};
+u16 D_8018B054[] = {
+#include "gen/D_8018B054.h"
+};
+u16 D_8018B154[] = {
+#include "gen/D_8018B154.h"
+};
+u16 D_8018B174[] = {
+#include "gen/D_8018B174.h"
+};
+u16 D_8018B1B4[] = {
+#include "gen/D_8018B1B4.h"
+};
