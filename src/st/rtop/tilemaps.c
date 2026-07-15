f42e9a13fbe8390962bba807a728dbd110364bb9
Joey Murphy <tjmurphy@protonmail.com>
2026-07-15 19:09:15 -0400

    layers

---
+++ src/st/rtop/tilemaps.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+#include "gen/rtop_tilemap_0.h"
+#include "gen/rtop_tilemap_1.h"
+#include "gen/rtop_tilemap_2.h"
+#include "gen/rtop_tilemap_3.h"
+#include "gen/rtop_tilemap_4.h"
+#include "gen/rtop_tilemap_5.h"
+#include "gen/rtop_tilemap_6.h"
+#include "gen/rtop_tilemap_7.h"
+#include "gen/rtop_tilemap_8.h"
+#include "gen/rtop_tilemap_9.h"
+#include "gen/rtop_tilemap_10.h"
+#include "gen/rtop_tiledef_0.h"
+#include "gen/rtop_tiledef_1.h"
+#include "gen/rtop_tiledef_2.h"
