cac3ae3480cfb2fd0a9ce0d6ec9cb8115161124c
Joey Murphy <tjmurphy@protonmail.com>
2026-07-15 19:12:38 -0400

    gfxbanks

--- src/st/rtop/stage_data.c
+++ src/st/rtop/stage_data.c
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include <stage.h>
 
+u8 D_801822FC[] = {
+#include "gen/D_801822FC.h"
+};
+u8 D_80182640[] = {
+#include "gen/D_80182640.h"
+};
+u8 D_801858E0[] = {
+#include "gen/D_801858E0.h"
+};
+u8 D_80186294[] = {
+#include "gen/D_80186294.h"
+};
+u8 D_80186EA0[] = {
+#include "gen/D_80186EA0.h"
+};
+u8 D_80187B04[] = {
+#include "gen/D_80187B04.h"
+};
+u8 D_801883F8[] = {
+#include "gen/D_801883F8.h"
+};
+u8 D_80189878[] = {
+#include "gen/D_80189878.h"
+};
+
 u16 D_8018AC34[] = {
 #include "gen/D_8018AC34.h"
 };
