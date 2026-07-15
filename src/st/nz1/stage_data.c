bb56c73ade4c6f41fe7ce7de887eee73ed9cd0e9
Joey Murphy <tjmurphy@protonmail.com>
2026-07-15 07:59:48 -0400

    cmpgfx

---
+++ src/st/nz1/stage_data.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+u8 D_80183AF0[] = {
+#include "gen/D_80183AF0.h"
+};
+u8 D_80183FA8[] = {
+#include "gen/D_80183FA8.h"
+};
+u8 D_801858F4[] = {
+#include "gen/D_801858F4.h"
+};
+u8 D_80186530[] = {
+#include "gen/D_80186530.h"
+};
+u8 D_8018684C[] = {
+#include "gen/D_8018684C.h"
+};
+u8 D_80187CCC[] = {
+#include "gen/D_80187CCC.h"
+};
+u8 D_80189088[] = {
+#include "gen/D_80189088.h"
+};
+u8 D_8018A0CC[] = {
+#include "gen/D_8018A0CC.h"
+};
+u8 D_8018A618[] = {
+#include "gen/D_8018A618.h"
+};
+u8 D_8018AEE0[] = {
+#include "gen/D_8018AEE0.h"
+};
+u8 D_8018BCF0[] = {
+#include "gen/D_8018BCF0.h"
+};
+u8 D_8018C984[] = {
+#include "gen/D_8018C984.h"
+};
+u8 D_8018D054[] = {
+#include "gen/D_8018D054.h"
+};
+u8 D_8018D510[] = {
+#include "gen/D_8018D510.h"
+};
+u8 D_8018DF40[] = {
+#include "gen/D_8018DF40.h"
+};
+u8 D_8018E664[] = {
+#include "gen/D_8018E664.h"
+};
+u8 D_8018F130[] = {
+#include "gen/D_8018F130.h"
+};
+u8 D_8018F6F8[] = {
+#include "gen/D_8018F6F8.h"
+};
+u8 D_801900AC[] = {
+#include "gen/D_801900AC.h"
+};
+u8 D_80190AFC[] = {
+#include "gen/D_80190AFC.h"
+};
+u8 D_80191678[] = {
+#include "gen/D_80191678.h"
+};
+u8 D_8019222C[] = {
+#include "gen/D_8019222C.h"
+};
+u8 D_80192B68[] = {
+#include "gen/D_80192B68.h"
+};
