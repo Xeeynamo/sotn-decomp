2e18284d4d213ab14307a9d093d41d6ac7fc391d
Joey Murphy <tjmurphy@protonmail.com>
2026-07-15 20:32:05 -0400

    i am the king of the clock tower

--- src/st/e_clock_tower.h
+++ src/st/e_clock_tower.h
@@ -255,12 +255,131 @@ void EntityClouds(Entity* self) {
     }
 }
 
-extern ClockTowerData s_RoofTextureData[];
-extern ClockTowerData2 s_TowerTextureData[];
+static ClockTowerData s_RoofTextureData[] = {
+    {0x10, 0x29, 0x02, 0x44, 0x1E, 0x44, 0x40},
+    {0x30, 0x29, 0x22, 0x44, 0x3E, 0x44, 0x40},
+    {0x15, 0x4C, 0x03, 0x6F, 0x26, 0x6F, 0x3E},
+};
 
-extern u8 s_ClockRoofScript[];
-extern u8 s_ClockTowerScript[];
-extern SVECTOR* s_ClockVertexSets[];
+static ClockTowerData2 s_TowerTextureData[] = {
+    {0x02, 0x02, 0x3C, 0x24, 0x3E}, {0x41, 0x01, 0x3D, 0x75, 0x44},
+    {0x81, 0x01, 0x3D, 0x75, 0x44}, {0x01, 0x71, 0x3D, 0x0D, 0x44},
+    {0x01, 0x71, 0x3D, 0x0D, 0x45}, {0xC1, 0x01, 0x25, 0x3D, 0x44},
+    {0xC1, 0x41, 0x25, 0x3D, 0x44},
+};
+
+// these are unique sets of vertexes which are used to build
+// the clock tower. each contains all of the points needed to
+// build one "feature" of the clock tower (one of 5 roof sections,
+// clock section, tower, etc.)
+static SVECTOR D_801821C8[] = {
+    {0, -182, 65},  {0, -182, -65}, {-65, -99, -65},
+    {-65, -99, 65}, {65, -99, 65},  {65, -99, -65},
+};
+static SVECTOR D_801821F8[] = { //1d70
+    {83, -163, -82}, {65, -99, -99}, {65, -99, -65},
+    {99, -99, -99},  {99, -99, -65},
+};
+static SVECTOR D_80182220[] = {
+    {83, -163, 82}, {65, -99, 66}, {65, -99, 99}, {99, -99, 66}, {99, -99, 99},
+};
+static SVECTOR D_80182248[] = {
+    {-83, -163, -82}, {-99, -99, -99}, {-99, -99, -65},
+    {-65, -99, -99},  {-65, -99, -65},
+};
+static SVECTOR D_80182270[] = {
+    {-82, -163, 82}, {-99, -99, 65}, {-99, -99, 99},
+    {-65, -99, 65},  {-65, -99, 99},
+};
+static SVECTOR D_80182298[] = {
+    {-99, 83, 99},   {-99, -100, 99}, {-99, -100, -99}, {-99, 83, -99},
+    {99, -100, -99}, {99, 83, -99},   {99, -100, 99},   {99, 83, 99},
+};
+static SVECTOR D_801822D8[] = {
+    {-99, 83, 99},  {-99, 83, -99},  {99, 83, -99},  {99, 83, 99},
+    {-65, 116, 65}, {-65, 116, -65}, {65, 116, -65}, {65, 116, 65},
+};
+static SVECTOR D_80182318[] = {
+    {-65, 283, 65}, {-65, 116, 65}, {-65, 116, -65}, {-65, 283, -65},
+    {65, 116, -65}, {65, 283, -65}, {65, 116, 65},   {65, 283, 65},
+};
+static SVECTOR D_80182358[] = {
+    {-65, 450, 65}, {-65, 283, 65}, {-65, 283, -65}, {-65, 450, -65},
+    {65, 283, -65}, {65, 450, -65}, {65, 283, 65},   {65, 450, 65},
+};
+
+static SVECTOR D_801818C8[] = {
+    {-65, 616, 65}, {-65, 450, 65}, {-65, 450, -65}, {-65, 616, -65}, 
+    {65, 450, -65}, {65, 616, -65}, {65, 450, 65}, {65, 616, 65}
+};
+
+static SVECTOR D_80181908[] = {
+    {-65, 783, 65}, {-65, 616, 65}, {-65, 616, -65}, {-65, 783, -65}, 
+    {65, 616, -65}, {65, 783, -65}, {65, 616, 65}, {65, 783, 65}
+};
+
+static SVECTOR D_80181948[] = {
+    {-65, 983, 65}, {-65, 783, 65}, {-65, 783, -65}, {-65, 983, -65}, 
+    {65, 783, -65}, {65, 983, -65}, {65, 783, 65}, {65, 983, 65}
+};
+
+// clocktower roof
+static u8 s_ClockRoofScript[] = {
+    // clang-format off
+    VINDEX(1),
+    DRAW_TRIANGLE(1, 0, 3, 1, 1),
+    DRAW_TRIANGLE(0, 0, 4, 3, 0),
+    VINDEX(2),
+    DRAW_TRIANGLE(1, 0, 3, 1, 1),
+    DRAW_TRIANGLE(0, 0, 4, 3, 0),
+    VINDEX(3),
+    DRAW_TRIANGLE(1, 0, 3, 1, 4),
+    DRAW_TRIANGLE(0, 0, 1, 2, 3),
+    VINDEX(4),
+    DRAW_TRIANGLE(1, 0, 3, 1, 0),
+    DRAW_TRIANGLE(0, 0, 1, 2, 0),
+    VINDEX(0),
+    DRAW_TRIANGLE(2, 1, 5, 2, 2),
+    OBJ_END,
+    // clang-format on
+};
+
+// clocktower clock and base
+static u8 s_ClockTowerScript[] = {
+    // clang-format off
+    VINDEX(0),
+    DRAW_QUAD(0, 0, 1, 3, 2, 2),
+    VINDEX(5),
+    DRAW_QUAD(2, 2, 4, 3, 5, 5),
+    DRAW_QUAD(1, 1, 2, 0, 3, 5),
+    DRAW_QUAD(2, 1, 6, 2, 4, 0),
+    VINDEX(6),
+    DRAW_QUAD(3, 0, 1, 4, 5, 5),
+    DRAW_QUAD(4, 1, 2, 5, 6, 5),
+    VINDEX(7),
+    DRAW_QUAD(6, 2, 4, 3, 5, 5),
+    DRAW_QUAD(5, 1, 2, 0, 3, 5),
+    VINDEX(8),
+    DRAW_QUAD(6, 2, 4, 3, 5, 5),
+    DRAW_QUAD(5, 1, 2, 0, 3, 5),
+    VINDEX(9),
+    DRAW_QUAD(6, 2, 4, 3, 5, 5),
+    DRAW_QUAD(5, 1, 2, 0, 3, 5),
+    VINDEX(10),
+    DRAW_QUAD(6, 2, 4, 3, 5, 5),
+    DRAW_QUAD(5, 1, 2, 0, 3, 5),
+    VINDEX(11),
+    DRAW_QUAD(6, 2, 4, 3, 5, 5),
+    DRAW_QUAD(5, 1, 2, 0, 3, 5),
+    OBJ_END,
+    // clang-format on
+};
+
+SVECTOR* s_ClockVertexSets[] = {
+    D_801821C8, D_801821F8, D_80182220, D_80182248, D_80182270,
+    D_80182298, D_801822D8, D_80182318, D_80182358, D_801818C8,
+    D_80181908, D_80181948
+};
 
 void EntityClockTower3D(Entity* self) {
     SVECTOR rotVector;
