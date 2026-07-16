c190ce2e4f8368aa50b0efb0c47e76a5e4c41583
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-16 11:28:26 +1000

    Dedupe `e_collect`

--- src/st/top_psp/e_breakable.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../top/top.h"
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/e_breakable", TOP_EntityBreakable);
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/e_breakable", TOP_EntityBreakableDebris);
--- src/st/top_psp/unk_70A8.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../top/top.h"
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_70A8", func_psp_0923C2F8);
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_70A8", func_psp_0923C390);
