80af1a85e19d3d297262f64276dfc0a9c60211b5
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-18 15:38:02 +1000

    Reformat item drops

--- src/boss/rbo3/d_prize_drops.c
+++ src/boss/rbo3/d_prize_drops.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rbo3.h"
 
-u16 OVL_EXPORT(PrizeDrops)[] = {ITEMDROP_RUNESWORD, 0x020C, 0x0307, 0x0404, 0x0520, ITEMDROP_GREAT_SWORD};
+u16 OVL_EXPORT(PrizeDrops)[] = {
+    ITEMDROP_RUNESWORD, 0x020C, 0x0307, 0x0404, 0x0520, ITEMDROP_GREAT_SWORD};
