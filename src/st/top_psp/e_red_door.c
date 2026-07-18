3fb9cee7ba550cb1ffdfb83f2f290dcd5843bc9a
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-18 10:52:10 +1000

    Dedupe `e_red_door`

--- src/st/top_psp/e_red_door.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../top/top.h"
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/e_red_door", EntityIsNearPlayer);
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/e_red_door", TOP_EntityRedDoor);
