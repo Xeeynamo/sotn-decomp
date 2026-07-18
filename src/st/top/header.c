e4f2e1c038b9b5b0463d3ead52f7ef018971c1e6
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-18 13:59:05 +1000

    Fix header and cutscene splits

--- src/st/top/header.c
+++ src/st/top/header.c
@@ -1,41 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "top.h"
-#ifdef VERSION_PSP
 #include "../pfn_entity_update.h"
 
 // common
-extern RoomHeader OVL_EXPORT(rooms)[];
-extern SpriteParts* OVL_EXPORT(spriteBanks)[];
-extern u_long* OVL_EXPORT(cluts)[];
-extern RoomDef OVL_EXPORT(rooms_layers)[];
-extern GfxBank* OVL_EXPORT(gfxBanks)[];
-void UpdateStageEntities(void);
-// overlay
-u8* D_pspeu_092580F0[];
-
-AbbreviatedOverlay2 OVL_EXPORT(Overlay) = {
-    .Update = Update,
-    .HitDetection = HitDetection,
-    .UpdateRoomPosition = UpdateRoomPosition,
-    .InitRoomEntities = InitRoomEntities,
-    .rooms = OVL_EXPORT(rooms),
-    .spriteBanks = OVL_EXPORT(spriteBanks),
-    .cluts = OVL_EXPORT(cluts),
-    .objLayoutHorizontal = &OBJ_LAYOUT_HORIZONTAL,
-    .tileLayers = OVL_EXPORT(rooms_layers),
-    .gfxBanks = OVL_EXPORT(gfxBanks),
-    .UpdateStageEntities = UpdateStageEntities,
-    .unk2C = NULL,
-    .unk30 = D_pspeu_092580F0,
-};
-#else
-#include "../pfn_entity_update.h"
-
 extern RoomHeader rooms[];
 extern SpriteParts* spriteBanks[];
 extern u_long* cluts[];
 extern RoomDef rooms_layers[];
 extern GfxBank* gfxBanks[];
+void UpdateStageEntities(void);
 extern u8* D_us_80182E44[];
 
 AbbreviatedOverlay2 OVL_EXPORT(Overlay) = {
@@ -46,11 +19,10 @@ AbbreviatedOverlay2 OVL_EXPORT(Overlay) = {
     .rooms = rooms,
     .spriteBanks = spriteBanks,
     .cluts = cluts,
-    .objLayoutHorizontal = OBJ_LAYOUT_HORIZONTAL,
+    .objLayoutHorizontal = &OBJ_LAYOUT_HORIZONTAL,
     .tileLayers = rooms_layers,
     .gfxBanks = gfxBanks,
     .UpdateStageEntities = UpdateStageEntities,
     .unk2C = NULL,
     .unk30 = D_us_80182E44,
 };
-#endif
