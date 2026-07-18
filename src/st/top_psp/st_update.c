bf9e70795d925ad69cbf71cba9b22e4f2f9e10cf
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-18 11:23:30 +1000

    Dedupe `st_update`

--- src/st/top_psp/st_update.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../top/top.h"
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/st_update", Random);
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/st_update", Update);
-
-INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/st_update", UpdateStageEntities);
