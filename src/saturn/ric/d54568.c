20f1d283a8209b8d9c7c1f3ab2fb8136f6751aad
sozud <122322823+sozud@users.noreply.github.com>
2026-07-18 12:52:54 +0700

    lint

--- src/saturn/ric/d54568.c
+++ src/saturn/ric/d54568.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 #include "sattypes.h"
 
 s32 D_80154568[] = {0, FIX(-8), FIX(-16)};
--- src/saturn/ric/rictail.c
+++ src/saturn/ric/rictail.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
 #include "inc_asm.h"
 
 INCLUDE_ASM("asm/saturn/richter/data", d60BED7C, d_060BED7C);
