30fc601a17612c79868dcf469ff7e46a78e35bc1
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-19 16:13:04 +1000

    Dedupe `post_richter_cutscene`

---
+++ src/st/top_psp/unk_12048.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../top/top.h"
+
+INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_12048", func_pspeu_09249B38);
+
+INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_12048", func_pspeu_0924A3B0);
+
+INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_12048", EntitySecretButton);
+
+INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_12048", func_pspeu_0924B1E8);
+
+INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_12048", EntitySecretStairs);
+
+INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_12048", EntityRedEyeBust);
+
+INCLUDE_ASM("st/top_psp/nonmatchings/top_psp/unk_12048", func_pspeu_0924BCD0);
