3fb5a8243e8730fb45391ae76b8ed0b72402651b
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-07-19 16:13:02 +0100

    RWRP: reconcile header

--- src/st/rwrp/header.c
+++ src/st/rwrp/header.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rwrp.h"
-#ifdef VERSION_PSP
 
-// common
 extern RoomHeader rooms[];
 extern SpriteParts* spriteBanks[];
 extern u_long* cluts[];
@@ -28,30 +26,3 @@ Overlay OVL_EXPORT(Overlay) = {
     .unk38 = NULL,
     .StageEndCutScene = NULL,
 };
-#else
-
-extern RoomHeader rooms[];
-extern s16** spriteBanks[];
-extern u_long* cluts[];
-extern RoomDef rooms_layers[];
-extern u_long** gfxBanks[];
-
-static Overlay OVL_EXPORT(Overlay) = {
-    .Update = Update,
-    .HitDetection = HitDetection,
-    .UpdateRoomPosition = UpdateRoomPosition,
-    .InitRoomEntities = InitRoomEntities,
-    .rooms = rooms,
-    .spriteBanks = spriteBanks,
-    .cluts = cluts,
-    .objLayoutHorizontal = NULL,
-    .tileLayers = rooms_layers,
-    .gfxBanks = gfxBanks,
-    .UpdateStageEntities = UpdateStageEntities,
-};
-
-#include "gen/sprite_banks.h"
-#include "gen/palette_def.h"
-#include "gen/layers.h"
-#include "gen/graphics_banks.h"
-#endif
