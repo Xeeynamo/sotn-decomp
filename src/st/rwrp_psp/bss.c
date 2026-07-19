cb1fed64126527c480ce2733d685d66ba93e58e7
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-07-19 11:36:29 +0100

    RWRP: import BSS

---
+++ src/st/rwrp_psp/bss.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <common.h>
+
+u8 D_pspeu_0924F310[0x11F0];
+u8 D_pspeu_0924F308[0x08];
+u8 D_pspeu_0924F300[0x08];
+u8 D_pspeu_0924F2F8[0x08];
+u8 D_pspeu_0924F2F0[0x08];
+u8 D_pspeu_0924F2E8[0x08];
+u8 D_pspeu_0924F2E0[0x08];
+u8 D_pspeu_0924E2E0[0x1000];
+u8 D_pspeu_0924E2D8[0x08];
+u8 D_pspeu_0924E2D0[0x08];
+u8 D_pspeu_0924E2C8[0x08];
+u8 D_pspeu_0924E2C0[0x08];
+u8 D_pspeu_0924E2B8[0x08];
+u8 D_pspeu_0924E2B0[0x08];
+u8 D_pspeu_0924E2A8[0x08];
+u8 D_pspeu_0924E2A0[0x08];
+u8 D_pspeu_0924E290[0x10];
+u8 D_pspeu_0924E288[0x08];
+u8 D_pspeu_0924E280[0x08];
+u8 D_pspeu_0924E278[0x08];
+u8 D_pspeu_0924E270[0x08];
+u8 D_pspeu_0924E268[0x08];
+u8 D_pspeu_0924E260[0x08];
+u8 g_ItemIconSlots[0x40];
+u8 g_LayoutObjHorizontal[0x08];
+u8 g_LayoutObjVertical[0x08];
+u8 g_LayoutObjPosHorizontal[0x08];
+u8 g_LayoutObjPosVertical[0x08];
