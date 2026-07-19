3cdd5fec63c321452500425c8058c432399ecfe8
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-07-19 11:50:51 +0100

    RWRP: reconcile e_particles

--- src/st/rwrp_psp/e_particles.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rwrp/rwrp.h"
-
-INCLUDE_ASM("st/rwrp_psp/nonmatchings/rwrp_psp/e_particles", EntitySoulStealOrb);
-
-INCLUDE_ASM("st/rwrp_psp/nonmatchings/rwrp_psp/e_particles", EntityEnemyBlood);
