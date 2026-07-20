bac8d75f4139ae07bc6f4ea39b45b761aee80d07
sozud <122322823+sozud@users.noreply.github.com>
2026-07-20 10:59:05 +0700

    sat: import zero data (#3385)
    This is another round of saturn data, specifically for 0.BIN. This
    should not be as bad as the previous one to review

---
+++ src/saturn/zero/stgfiles.c
@@ -0,0 +1,309 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "sattypes.h"
+
+extern const char D_0601A0E8[];
+extern const char D_0601A0F8[];
+extern const char D_0601A108[];
+extern const char D_0601A118[];
+extern const char D_0601A128[];
+extern const char D_0601A138[];
+extern const char D_0601A148[];
+extern const char D_0601A158[];
+extern const char D_0601A168[];
+extern const char D_0601A178[];
+extern const char D_0601A188[];
+extern const char D_0601A198[];
+extern const char D_0601A1A8[];
+extern const char D_0601A1B8[];
+extern const char D_0601A1C8[];
+extern const char D_0601A1D4[];
+extern const char D_0601A1E0[];
+extern const char D_0601A1EC[];
+extern const char D_0601A1FC[];
+extern const char D_0601A20C[];
+extern const char D_0601A21C[];
+extern const char D_0601A22C[];
+extern const char D_0601A23C[];
+extern const char D_0601A24C[];
+extern const char D_0601A25C[];
+extern const char D_0601A26C[];
+extern const char D_0601A27C[];
+extern const char D_0601A28C[];
+extern const char D_0601A29C[];
+extern const char D_0601A2AC[];
+extern const char D_0601A2BC[];
+extern const char D_0601A2CC[];
+extern const char D_0601A2DC[];
+extern const char D_0601A2EC[];
+extern const char D_0601A2FC[];
+extern const char D_0601A30C[];
+extern const char D_0601A31C[];
+extern const char D_0601A32C[];
+extern const char D_0601A33C[];
+extern const char D_0601A34C[];
+extern const char D_0601A35C[];
+extern const char D_0601A36C[];
+extern const char D_0601A37C[];
+extern const char D_0601A38C[];
+extern const char D_0601A39C[];
+extern const char D_0601A3AC[];
+extern const char D_0601A3BC[];
+extern const char D_0601A3C8[];
+extern const char D_0601A3D4[];
+extern const char D_0601A3E0[];
+extern const char D_0601A3F0[];
+extern const char D_0601A400[];
+extern const char D_0601A410[];
+extern const char D_0601A420[];
+extern const char D_0601A430[];
+extern const char D_0601A440[];
+extern const char D_0601A450[];
+extern const char D_0601A460[];
+extern const char D_0601A470[];
+extern const char D_0601A480[];
+extern const char D_0601A490[];
+extern const char D_0601A4A0[];
+extern const char D_0601A4B0[];
+extern const char D_0601A4C0[];
+extern const char D_0601A4D0[];
+extern const char D_0601A4E0[];
+extern const char D_0601A4F0[];
+extern const char D_0601A500[];
+extern const char D_0601A510[];
+extern const char D_0601A520[];
+extern const char D_0601A530[];
+extern const char D_0601A540[];
+extern const char D_0601A550[];
+extern const char D_0601A560[];
+extern const char D_0601A570[];
+extern const char D_0601A580[];
+extern const char D_0601A590[];
+extern const char D_0601A5A0[];
+extern const char D_0601A5B0[];
+extern const char D_0601A5C0[];
+extern const char D_0601A5D0[];
+extern const char D_0601A5E0[];
+extern const char D_0601A5F0[];
+extern const char D_0601A600[];
+extern const char D_0601A610[];
+extern const char D_0601A620[];
+extern const char D_0601A630[];
+extern const char D_0601A640[];
+extern const char D_0601A650[];
+extern const char D_0601A660[];
+extern const char D_0601A670[];
+extern const char D_0601A680[];
+extern const char D_0601A690[];
+extern const char D_0601A6A0[];
+extern const char D_0601A6B0[];
+extern const char D_0601A6BC[];
+extern const char D_0601A6C8[];
+extern const char D_0601A6D4[];
+extern const char D_0601A6E0[];
+extern const char D_0601A6EC[];
+extern const char D_0601A6F8[];
+extern const char D_0601A704[];
+extern const char D_0601A710[];
+extern const char D_0601A71C[];
+extern const char D_0601A728[];
+extern const char D_0601A734[];
+extern const char D_0601A740[];
+extern const char D_0601A74C[];
+extern const char D_0601A758[];
+extern const char D_0601A764[];
+extern const char D_0601A770[];
+extern const char D_0601A77C[];
+extern const char D_0601A788[];
+extern const char D_0601A794[];
+extern const char D_0601A7A0[];
+extern const char D_0601A7AC[];
+extern const char D_0601A7BC[];
+extern const char D_0601A7CC[];
+extern const char D_0601A7DC[];
+extern const char D_0601A7E8[];
+extern const char D_0601A7F4[];
+extern const char D_0601A800[];
+extern const char D_0601A80C[];
+extern const char D_0601A818[];
+extern const char D_0601A824[];
+extern const char D_0601A830[];
+extern const char D_0601A83C[];
+extern const char D_0601A848[];
+extern const char D_0601A858[];
+extern const char D_0601A868[];
+extern const char D_0601A878[];
+extern const char D_0601A884[];
+extern const char D_0601A890[];
+extern const char D_0601A89C[];
+extern const char D_0601A8A8[];
+extern const char D_0601A8B4[];
+extern const char D_0601A8C0[];
+extern const char D_0601A8D0[];
+extern const char D_0601A8E0[];
+extern const char D_0601A8F0[];
+extern const char D_0601A900[];
+extern const char D_0601A910[];
+extern const char D_0601A920[];
+extern const char D_0601A930[];
+extern const char D_0601A940[];
+extern const char D_0601A950[];
+extern const char D_0601A960[];
+extern const char D_0601A970[];
+extern const char D_0601A980[];
+extern const char D_0601A990[];
+extern const char D_0601A9A0[];
+extern const char D_0601A9B0[];
+extern const char D_0601A9C0[];
+extern const char D_0601A9D0[];
+extern const char D_0601A9E0[];
+extern const char D_0601A9F0[];
+extern const char D_0601AA00[];
+extern const char D_0601AA10[];
+extern const char D_0601AA20[];
+extern const char D_0601AA30[];
+extern const char D_0601AA40[];
+extern const char D_0601AA50[];
+extern const char D_0601AA60[];
+extern const char D_0601AA70[];
+extern const char D_0601AA80[];
+extern const char D_0601AA90[];
+extern const char D_0601AAA0[];
+extern const char D_0601AAB0[];
+extern const char D_0601AAC0[];
+extern const char D_0601AAD0[];
+extern const char D_0601AAE0[];
+extern const char D_0601AAF0[];
+extern const char D_0601AB00[];
+extern const char D_0601AB10[];
+extern const char D_0601AB20[];
+extern const char D_0601AB30[];
+extern const char D_0601AB40[];
+extern const char D_0601AB50[];
+extern const char D_0601AB60[];
+extern const char D_0601AB70[];
+extern const char D_0601AB80[];
+extern const char D_0601AB90[];
+extern const char D_0601ABA0[];
+extern const char D_0601ABB0[];
+extern const char D_0601ABC0[];
+extern const char D_0601ABD0[];
+extern const char D_0601ABE0[];
+extern const char D_0601ABF0[];
+
+typedef struct {
+    const char* prg;
+    const char* chr;
+    const char* map;
+    u32 unkC;
+    u32 unk10;
+    u32 unk14;
+} SaturnStageFileRecord;
+
+SaturnStageFileRecord g_StageFileRecords[100] = {
+    {D_0601AB50, D_0601AB40, D_0601AB30, 0x00000B06, 0xE0000109, 0x04000000},
+    {D_0601AB20, D_0601AB10, D_0601AB00, 0x00000B0A, 0xE000010C, 0x00000000},
+    {D_0601AAF0, D_0601AAE0, D_0601AAD0, 0x00000B0C, 0xE0000106, 0x00000000},
+    {D_0601AAC0, D_0601AAB0, D_0601AAA0, 0x00000B14, 0xE0000105, 0x00000000},
+    {D_0601AA90, D_0601AA80, D_0601AA70, 0x00000B18, 0xE0000102, 0x00000000},
+    {D_0601AA60, D_0601AA50, D_0601AA40, 0x00000B12, 0xE0000103, 0x00000000},
+    {D_0601AA30, D_0601AA20, D_0601AA10, 0x00000B0E, 0xE0000101, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B04, 0xE000010A, 0x01000000},
+    {D_0601A9D0, D_0601A9C0, D_0601A9B0, 0x00000B32, 0xE000010E, 0x00000000},
+    {D_0601A9A0, D_0601A990, D_0601A980, 0x00000B10, 0xE0000107, 0x00000000},
+    {D_0601A970, D_0601A960, D_0601A950, 0x00000B16, 0xE000010D, 0x00000000},
+    {D_0601A940, D_0601A930, D_0601A920, 0x00000B1C, 0xE0000108, 0x00000000},
+    {D_0601A910, D_0601A900, D_0601A8F0, 0x00000B08, 0xE0000104, 0x03000000},
+    {D_0601A8E0, D_0601A8D0, D_0601A8C0, 0x00000B1A, 0xE000010B, 0x00000000},
+    {D_0601A8B4, D_0601A8A8, D_0601A89C, 0xFF000000, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601A890, D_0601A884, D_0601A878, 0x00000B06, 0xE000012B, 0x00000000},
+    {D_0601A868, D_0601A858, D_0601A848, 0x00000B2F, 0xFF000000, 0x00000000},
+    {D_0601A83C, D_0601A830, D_0601A824, 0x00000B08, 0xFF000000, 0x00000000},
+    {D_0601A818, D_0601A80C, D_0601A800, 0x00000B1A, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601A7F4, D_0601A7E8, D_0601A7DC, 0x00000B24, 0xFF000000, 0x00000000},
+    {D_0601A7CC, D_0601A7BC, D_0601A7AC, 0xFF000000, 0xFF000000, 0x00000000},
+    {D_0601A7A0, D_0601A794, D_0601A788, 0x00000B2C, 0xFF000000, 0x00000000},
+    {D_0601A77C, D_0601A770, D_0601A764, 0x00000B0E, 0xFF000000, 0x00000000},
+    {D_0601A758, D_0601A74C, D_0601A740, 0x00000B1F, 0xFF000000, 0x00000000},
+    {D_0601A734, D_0601A728, D_0601A71C, 0x00000B22, 0xFF000000, 0x00000000},
+    {D_0601A710, D_0601A704, D_0601A6F8, 0x00000B27, 0xFF000000, 0x00000000},
+    {D_0601A6EC, D_0601A6E0, D_0601A6D4, 0x00000B25, 0xFF000000, 0x00000000},
+    {D_0601A6C8, D_0601A6BC, D_0601A6B0, 0x00000B29, 0xFF000000, 0x00000000},
+    {D_0601A6A0, D_0601A690, D_0601A680, 0x00000B30, 0xE0000119, 0x3F000000},
+    {D_0601A670, D_0601A660, D_0601A650, 0x00000B07, 0xE0000117, 0x00000000},
+    {D_0601A640, D_0601A630, D_0601A620, 0x00000B0B, 0xE0000117, 0x00000000},
+    {D_0601A610, D_0601A600, D_0601A5F0, 0x00000B0D, 0xE0000110, 0x00000000},
+    {D_0601A5E0, D_0601A5D0, D_0601A5C0, 0x00000B15, 0xE0000112, 0x00000000},
+    {D_0601A5B0, D_0601A5A0, D_0601A590, 0x00000B19, 0xE0000117, 0x00000000},
+    {D_0601A580, D_0601A570, D_0601A560, 0x00000B13, 0xE0000103, 0x00000000},
+    {D_0601A550, D_0601A540, D_0601A530, 0x00000B0F, 0xE0000110, 0x00000000},
+    {D_0601A520, D_0601A510, D_0601A500, 0x00000B05, 0xE0000117, 0x00000000},
+    {D_0601A4F0, D_0601A4E0, D_0601A4D0, 0x00000B2D, 0xE000010E, 0x00000000},
+    {D_0601A4C0, D_0601A4B0, D_0601A4A0, 0x00000B11, 0xE0000110, 0x00000000},
+    {D_0601A490, D_0601A480, D_0601A470, 0x00000B17, 0xE0000111, 0x00000000},
+    {D_0601A460, D_0601A450, D_0601A440, 0x00000B1D, 0xE0000108, 0x00000000},
+    {D_0601A430, D_0601A420, D_0601A410, 0x00000B09, 0xE0000117, 0x00000000},
+    {D_0601A400, D_0601A3F0, D_0601A3E0, 0x00000B1B, 0xE0000117, 0x00000000},
+    {D_0601A3D4, D_0601A3C8, D_0601A3BC, 0xFF000000, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601A3AC, D_0601A39C, D_0601A38C, 0x00000B1B, 0xFF000000, 0x00000000},
+    {D_0601A37C, D_0601A36C, D_0601A35C, 0x00000B26, 0xFF000000, 0x00000000},
+    {D_0601A34C, D_0601A33C, D_0601A32C, 0x00000B2A, 0xFF000000, 0x00000000},
+    {D_0601A31C, D_0601A30C, D_0601A4D0, 0x00000B2E, 0xFF000000, 0x00000000},
+    {D_0601A2FC, D_0601A2EC, D_0601A2DC, 0x00000B1F, 0xFF000000, 0x00000000},
+    {D_0601A2CC, D_0601A2BC, D_0601A2AC, 0x00000B20, 0xFF000000, 0x00000000},
+    {D_0601A29C, D_0601A28C, D_0601A27C, 0x00000B21, 0xFF000000, 0x00000000},
+    {D_0601A26C, D_0601A25C, D_0601A24C, 0x00000B23, 0xFF000000, 0x00000000},
+    {D_0601A23C, D_0601A22C, D_0601A21C, 0x00000B1E, 0xFF000000, 0x00000000},
+    {D_0601A20C, D_0601A1FC, D_0601A1EC, 0x00000B28, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601A1E0, D_0601A1D4, D_0601A1C8, 0x00000B31, 0xFF000000, 0x00000000},
+    {D_0601A1B8, D_0601A1A8, D_0601A9E0, 0x00000B03, 0xE000010A, 0x00000000},
+    {D_0601A198, D_0601A188, D_0601A178, 0x00000B35, 0xE0000125, 0x00000000},
+    {D_0601A168, D_0601A158, D_0601A148, 0x00000B36, 0xE0000126, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601AA00, D_0601A9F0, D_0601A9E0, 0x00000B06, 0xFF000000, 0x00000000},
+    {D_0601A138, D_0601A128, D_0601A118, 0x00000B34, 0xE0000129, 0x00000000},
+    {D_0601A108, D_0601A0F8, D_0601A0E8, 0x00000B37, 0xE000012C, 0x00000000},
+};
+
+const char* g_StageAlternateMapNames[18] = {
+    D_0601AA10, D_0601AB60, D_0601A920, D_0601AB70, D_0601A980, D_0601AB90,
+    D_0601AB80, D_0601AAA0, D_0601ABA0, D_0601A530, D_0601ABB0, D_0601A440,
+    D_0601ABC0, D_0601A4A0, D_0601ABE0, D_0601ABD0, D_0601A5C0, D_0601ABF0,
+};
