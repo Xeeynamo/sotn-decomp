7b9d338094406bf670dd39313cfb815d69d2b087
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-22 08:02:15 +1000

    Add support to extract player sprites (#3399)
    Importing TOP assets require a change to the assets tool to handle the
    special player sprite banks which are in a different format.
    
    The TOP spritebanks `- [0x34, sprite_banks, sprite_banks]` has its
    second bank as the Richter sprite definition in the different format,
    making the tool barf when trying to import all the other sprite banks.
    Presumably this is done to support Richter in the boss fight / cutscene
    inside TOP.
    
    <img width="371" height="161" alt="image"
    src="https://github.com/user-attachments/assets/b3756566-47f2-4341-835c-7685fc85a4a6"
    />
    
    These had previously been imported manually into .c files in RIC/MARIA,
    but I have updated assets config to auto import them instead to further
    utilise the same logic and prove it's matching.
    
    Most likely this same logic will be required in CEN/RCEN assets on PSP
    side at least.

--- src/ric/pl_main.c
+++ src/ric/pl_main.c
@@ -134,13 +134,13 @@ void RicInit(s16 initParam) {
     }
     spriteptr = g_api.o.spriteBanks;
     spriteptr += 0x10;
-    *spriteptr = (SpriteParts*)D_801530AC;
+    *spriteptr = (SpriteParts*)spritebank0;
     spriteptr++;
-    *spriteptr = (SpriteParts*)D_80153AA0;
+    *spriteptr = (SpriteParts*)spritebank1;
     spriteptr++;
-    *spriteptr = (SpriteParts*)D_80153D24;
+    *spriteptr = (SpriteParts*)spritebank2;
     spriteptr++;
-    *spriteptr = (SpriteParts*)D_801541A8;
+    *spriteptr = (SpriteParts*)spritebank3;
     for (e = &g_Entities[E_AFTERIMAGE_1], i = 0; i < 3; i++, e++) {
         DestroyEntity(e);
         e->animSet = ANIMSET_OVL(0x10);
