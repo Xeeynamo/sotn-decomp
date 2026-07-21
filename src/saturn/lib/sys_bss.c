c6b426ba072d20529de18883a8de9ac686beecde
sozud <122322823+sozud@users.noreply.github.com>
2026-07-21 11:49:41 +0700

    sat: zero bss

---
+++ src/saturn/lib/sys_bss.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "sattypes.h"
+
+// probably all bss but easier to do data right now
+s32 SYS_state_060485C0 = 0;
+s32 SYS_state_060485C4 = 0;
+u8 SYS_pad_060485C8[0x18] = {0};
+u8 SYS_buf_060485E0[0x220] = {0};
+u8 SYS_buf_06048800[0x2460] = {0};
+u8 SYS_buf_0604AC60[0x980] = {0};
+u8 SYS_buf_0604B5E0[0x3000] = {0};
+u8 SYS_buf_0604E5E0[0x2000] = {0};
