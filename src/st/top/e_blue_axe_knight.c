4c75f364d98ac5c546ee8dd602ef08becac66d3f
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-07-28 19:25:36 +1000

    Import most `top` PSX assets

--- src/st/top/e_blue_axe_knight.c
+++ src/st/top/e_blue_axe_knight.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "top.h"
 
-#define SPRITE_BANK sprites_3
+#define SPRITE_BANK sprites_top_5
 #include "../e_blue_axe_knight.h"
---
+++ src/st/top/palette_def.c
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "top.h"
+
+u16 D_us_80180094[] = {
+#include "gen/D_us_80180094.h"
+};
+u16 D_us_801800B4[] = {
+#include "gen/D_us_801800B4.h"
+};
+u16 D_us_801800D4[] = {
+#include "gen/D_us_801800D4.h"
+};
+u16 D_us_801800F4[] = {
+#include "gen/D_us_801800F4.h"
+};
+u16 D_us_80180114[] = {
+#include "gen/D_us_80180114.h"
+};
+u16 D_us_80180134[] = {
+#include "gen/D_us_80180134.h"
+};
+u16 D_us_80180154[] = {
+#include "gen/D_us_80180154.h"
+};
+u16 D_us_80180174[] = {
+#include "gen/D_us_80180174.h"
+};
+u16 D_us_80180194[] = {
+#include "gen/D_us_80180194.h"
+};
+u16 D_us_801801B4[] = {
+#include "gen/D_us_801801B4.h"
+};
+u16 D_us_801801D4[] = {
+#include "gen/D_us_801801D4.h"
+};
+u16 D_us_801801F4[] = {
+#include "gen/D_us_801801F4.h"
+};
+u16 D_us_80180214[] = {
+#include "gen/D_us_80180214.h"
+};
+u16 D_us_80180234[] = {
+#include "gen/D_us_80180234.h"
+};
+u16 D_us_80180254[] = {
+#include "gen/D_us_80180254.h"
+};
+u16 D_us_80180274[] = {
+#include "gen/D_us_80180274.h"
+};
+u16 D_us_80180294[] = {
+#include "gen/D_us_80180294.h"
+};
+u16 D_us_801802B4[] = {
+#include "gen/D_us_801802B4.h"
+};
+u16 D_us_801802D4[] = {
+#include "gen/D_us_801802D4.h"
+};
+u16 D_us_801802F4[] = {
+#include "gen/D_us_801802F4.h"
+};
+u16 D_us_80180314[] = {
+#include "gen/D_us_80180314.h"
+};
+u16 D_us_80180334[] = {
+#include "gen/D_us_80180334.h"
+};
+
+#include "gen/palette_def.h"
