fe4a4bb77f820cb2512a5d69d248ef0afcea7224
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-08-01 16:17:09 +0100

    weapon: import all w_008 data

--- src/weapon/w_008.c
+++ src/weapon/w_008.c
@@ -3,18 +3,80 @@
 // Leather shield, Unknown#203
 #define SHIELDSPELL_S32
 #include "weapon_private.h"
-extern u16* g_WeaponCluts[];
-extern s32 g_HandId;
-#include "shared.h"
 #include "gen/w_008_1.h"
 #include "gen/w_008_2.h"
 #define g_Animset w_008_1
 #define g_Animset2 w_008_2
 #include "sfx.h"
 
-extern s8 D_3C000_8017A9F8;
-extern AnimationFrame* D_3C000_8017AA40;
-extern s32 D_3C000_8017AA78[];
+static u16 pal_0[] = {
+    0x0000, 0x8421, 0x8444, 0x8467, 0x84AB, 0x88ED, 0x8D30, 0x9573,
+    0x9994, 0x9DB6, 0xA1F7, 0xA619, 0xE2F4, 0xAE7C, 0xCA52, 0xB6DF,
+    0x0000, 0xD71D, 0xAD6F, 0x9D6D, 0x9972, 0xAA98, 0x9063, 0x9CC6,
+    0xA508, 0xB16B, 0xB9CE, 0xC210, 0xCE73, 0xD6B5, 0xE318, 0xEF7B};
+static AnimationFrame anim_00[] = {POSE(2, 0x01, 1), POSE_LOOP(0)};
+static AnimationFrame anim_01[] = {POSE(2, 0x02, 2), POSE_LOOP(0)};
+static AnimationFrame anim_09[] = {
+    POSE(2, 0x03, 3), POSE(2, 0x04, 3), POSE(2, 0x05, 3), POSE(2, 0x06, 3),
+    POSE(2, 0x07, 3), POSE(2, 0x08, 3), POSE(2, 0x09, 4), POSE(2, 0x0A, 1),
+    POSE(2, 0x0B, 1), POSE(2, 0x0C, 1), POSE_LOOP(0)};
+static AnimationFrame anim_08[] = {
+    POSE(3, 0x0D, 1),  POSE(3, 0x0E, 7),  POSE(3, 0x0F, 8),  POSE(3, 0x10, 9),
+    POSE(3, 0x11, 10), POSE(3, 0x12, 11), POSE(3, 0x13, 12), POSE(3, 0x14, 13),
+    POSE(3, 0x15, 14), POSE(3, 0x16, 13), POSE(3, 0x17, 12), POSE(3, 0x18, 11),
+    POSE(3, 0x19, 10), POSE(3, 0x1A, 9),  POSE(3, 0x1B, 8),  POSE(3, 0x1C, 7),
+    POSE_LOOP(0)};
+static AnimationFrame anim_07[] = {
+    POSE(1, 0x24, 1), POSE(1, 0x25, 1), POSE(1, 0x26, 1), POSE(1, 0x27, 1),
+    POSE(1, 0x28, 1), POSE(1, 0x29, 1), POSE(1, 0x2A, 1), POSE(1, 0x2B, 1),
+    POSE(1, 0x2C, 1), POSE(1, 0x2D, 1), POSE(1, 0x2E, 1), POSE(1, 0x2F, 1),
+    POSE(1, 0x30, 1), POSE_LOOP(0)};
+static AnimationFrame anim_06[] = {
+    POSE(1, 0x2A, 1), POSE(1, 0x2B, 1), POSE(1, 0x2C, 1), POSE(1, 0x2D, 1),
+    POSE(1, 0x2E, 1), POSE(1, 0x2F, 1), POSE(1, 0x30, 1), POSE_LOOP(0)};
+static AnimationFrame anim_05[] = {
+    POSE(2, 0x21, 1), POSE(2, 0x22, 1), POSE(2, 0x23, 1), POSE(2, 0x24, 1),
+    POSE(2, 0x25, 1), POSE(2, 0x26, 1), POSE(2, 0x27, 1), POSE(2, 0x28, 1),
+    POSE(2, 0x29, 1), POSE(2, 0x2A, 1), POSE(2, 0x2B, 1), POSE(2, 0x2C, 1),
+    POSE(2, 0x2D, 1), POSE(2, 0x2E, 1), POSE(2, 0x2F, 1), POSE(2, 0x30, 1),
+    POSE_LOOP(0)};
+static AnimationFrame anim_10[] = {
+    POSE(2, 0x31, 1), POSE(2, 0x32, 1), POSE(2, 0x33, 1), POSE_END};
+static AnimationFrame anim_11[] = {
+    POSE(2, 0x34, 2), POSE(2, 0x35, 2), POSE(2, 0x36, 2), POSE_END};
+static AnimationFrame anim_12[] = {
+    POSE(2, 0x33, 1), POSE(2, 0x32, 1), POSE(2, 0x31, 1), POSE_END};
+static AnimationFrame anim_13[] = {
+    POSE(2, 0x36, 2), POSE(2, 0x35, 2), POSE(2, 0x34, 2), POSE_END};
+static AnimationFrame anim_04[] = {
+    POSE(4, 0x1D, 1), POSE(4, 0x1E, 1), POSE_LOOP(0)};
+static AnimationFrame anim_02[] = {
+    POSE(2, 0x1F, 6), POSE(2, 0x20, 6), POSE_LOOP(0)};
+static AnimationFrame anim_03[] = {
+    POSE(3, 0x37, 17), POSE(3, 0x38, 17), POSE(3, 0x38, 17), POSE(3, 0x38, 16),
+    POSE(3, 0x38, 15), POSE(3, 0x39, 1),  POSE(3, 0x3A, 1),  POSE(3, 0x3B, 1),
+    POSE(3, 0x3C, 1),  POSE(3, 0x3D, 1),  POSE(3, 0x01, 1),  POSE(3, 0x01, 1),
+    POSE(3, 0x01, 1),  POSE(3, 0x01, 1),  POSE(3, 0x01, 1),  POSE_LOOP(0)};
+
+static s8 hitboxes[][4] = {
+    {0, 0, 0, 0},    {14, -7, 4, 12},  {14, 13, 4, 12}, {-11, -7, 4, 12},
+    {1, -5, 4, 12},  {-32, -31, 0, 0}, {22, -6, 4, 12}, {15, -6, 4, 12},
+    {16, -5, 4, 12}, {17, -4, 4, 12},  {18, -3, 4, 12}, {19, -2, 4, 12},
+    {20, -1, 4, 12}, {21, 0, 4, 12},   {22, 1, 4, 12},  {12, -5, 4, 12},
+    {10, -3, 4, 12}, {8, -1, 4, 12}};
+
+static AnimationFrame* anims[] = {
+    anim_00, anim_01, anim_02, anim_03, anim_04, anim_05, anim_06,
+    anim_07, anim_08, anim_09, anim_10, anim_11, anim_12, anim_13};
+
+static s32 shieldAngles[] = {
+    0x0000, 0x0200, 0x0400, 0x0600, 0x0800, 0x0A00, 0x0C00, 0x0E00,
+    0x1000, 0x1200, 0x1400, 0x1600, 0x1800, 0x1A00, 0x1C00, 0x1E00};
+
+static u16* g_WeaponCluts[] = {pal_0, pal_0};
+static s32 g_HandId = HAND_ID;
+
+#include "shared.h"
 
 static void EntityWeaponAttack(Entity* self) {
     s32 anim1;
@@ -143,7 +205,7 @@ static void EntityWeaponAttack(Entity* self) {
         break;
     }
     if (self->step != 4) {
-        g_api.PlayAnimation(&D_3C000_8017A9F8, &D_3C000_8017AA40);
+        g_api.PlayAnimation(&hitboxes[0][0], anims);
     }
     self->drawFlags = PLAYER.drawFlags;
     self->scaleY = PLAYER.scaleY;
@@ -390,31 +452,31 @@ static s32 EntityWeaponShieldSpell(Entity* self) {
     bottom++;
     prim = prim->next;
     for (i = 0; i < 9; i++) {
-        angle = D_3C000_8017AA78[i];
+        angle = shieldAngles[i];
         baseCoord = (rsin(angle) >> 5) * self->ext.shield.unk94 / 256;
         prim->y0 = top + baseCoord;
         prim->y2 = (bottom + baseCoord) - 1;
-        angle = D_3C000_8017AA78[(i + 1) % 9];
+        angle = shieldAngles[(i + 1) % 9];
         baseCoord = (rsin(angle) >> 5) * self->ext.shield.unk94 / 256;
         prim->y1 = top + baseCoord;
         prim->y3 = (bottom + baseCoord) - 1;
         prim->x0 = prim->x2 = back + (((front - back) * i) / 9);
         prim->x1 = prim->x3 = back + (((front - back) * (i + 1)) / 9);
-        angle = D_3C000_8017AA78[(i + 6) % 9];
+        angle = shieldAngles[(i + 6) % 9];
         baseCoord = (rsin(angle) >> 5) * self->ext.shield.unk92 / 256;
         prim->x0 += baseCoord;
         prim->x1 += baseCoord;
-        angle = D_3C000_8017AA78[(i + 7) % 9];
+        angle = shieldAngles[(i + 7) % 9];
         baseCoord = (rsin(angle) >> 5) * self->ext.shield.unk92 / 256;
         prim->x2 += baseCoord;
         prim->x3 += baseCoord;
-        angle = D_3C000_8017AA78[i];
+        angle = shieldAngles[i];
         prim->r0 = prim->g0 = prim->b0 = prim->r2 = prim->g2 = prim->b2 =
             ((rsin(angle) + 0x1000) >> 6) * self->ext.shield.unk90 / 256;
-        angle = D_3C000_8017AA78[(i + 1) % 9];
+        angle = shieldAngles[(i + 1) % 9];
         prim->r1 = prim->g1 = prim->b1 = prim->r3 = prim->g3 = prim->b3 =
             ((rsin(angle) + 0x1000) >> 6) * self->ext.shield.unk90 / 256;
-        D_3C000_8017AA78[i] += self->ext.shield.unk8A;
+        shieldAngles[i] += self->ext.shield.unk8A;
         prim = prim->next;
     }
 }
