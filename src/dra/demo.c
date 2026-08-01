Xeeynamo/sotn-decomp

commit 6a358e9dd4172d6d06673b8974ae582830504abc

pc: add demo replay (#3433)
A simple PR that wired the `--replay <file_name>` logic into the
`BIN/DEMOKEY.BIN` file used by the Librarian Tactics menu.

Doing `./build/pc/sotn --demo 0` will play the Prologue stage with
Richter, while `--demo 9` will play the Dream sequence against the
Succubus boss. As usual, `--replay-fast` and `--replay-and-exit` works
here too, useful to eventually run these on CI.

src/dra/demo.c
@@ -5,7 +5,7 @@
 static u8* g_DemoPtr;
 static s32 g_DemoKeyIdx;
 
-static u8 D_800A243C[] = {
+static u8 demo_stages[] = {
     STAGE_ST0,       STAGE_BO0,      STAGE_BO4,      STAGE_BO1,  STAGE_BO2,
     STAGE_BO3,       STAGE_NZ0_DEMO, STAGE_BO5,      STAGE_RBO1, STAGE_DRE,
     STAGE_NZ1_DEMO,  STAGE_RBO0,     STAGE_RBO2,     STAGE_BO7,  STAGE_BO6,
@@ -33,7 +33,7 @@ void DemoGameInit(s32 arg0) {
         }
     } else {
         g_DemoKeyIdx = D_80097C98 & STAGE_INVERTEDCASTLE_MASK;
-        g_StageId = D_800A243C[g_DemoKeyIdx];
+        g_StageId = demo_stages[g_DemoKeyIdx];
     }
 
     InitStatsAndGear(0);
@@ -217,6 +217,13 @@ void DemoOpenFile(s32 arg0) {
         g_LoadOvlIdx = g_DemoKeyIdx;
         return;
     }
+
+#ifdef VERSION_PC
+    memset(DEMO_KEY_PTR, 0, DEMO_MAX_LEN);
+    FileReadToBuf("disks/us/BIN/DEMOKEY.BIN", DEMO_KEY_PTR,
+                  g_DemoKeyIdx * DEMO_MAX_LEN, DEMO_MAX_LEN);
+    return;
+#endif
     if (arg0 == 0) {
         __builtin_memcpy(fileName, "sim:c:\\bin\\demo_key.bin",
                          sizeof("sim:c:\\bin\\demo_key.bin"));
@@ -288,9 +295,6 @@ void DemoUpdate(void) {
     u8 frameCount;
     s32 demoOffset;
 
-#ifdef VERSION_PC
-    g_DemoPtr = DEMO_KEY_PTR;
-#endif
     btnLo = g_DemoPtr[0];
     btnHi = g_DemoPtr[1];
     frameCount = g_DemoPtr[2];

src/dra_psp/demo.c
@@ -6,7 +6,7 @@
 static u8* g_DemoPtr = DEMO_KEY_PTR;
 s32 g_DemoKeyIdx;
 
-static u8 D_800A243C[] = {
+static u8 demo_stages[] = {
     STAGE_ST0,       STAGE_BO0,      STAGE_BO4,      STAGE_BO1,  STAGE_BO2,
     STAGE_BO3,       STAGE_NZ0_DEMO, STAGE_BO5,      STAGE_RBO1, STAGE_DRE,
     STAGE_NZ1_DEMO,  STAGE_RBO0,     STAGE_RBO2,     STAGE_BO7,  STAGE_BO6,
@@ -30,11 +30,11 @@ void DemoGameInit(s32 arg0) {
         } else if (g_StageId == STAGE_RNZ1) {
             g_StageId = STAGE_RNZ1_DEMO;
         } else {
-            g_StageId = D_800A243C[g_DemoKeyIdx];
+            g_StageId = demo_stages[g_DemoKeyIdx];
         }
     } else {
         g_DemoKeyIdx = D_80097C98 & STAGE_INVERTEDCASTLE_MASK;
-        g_StageId = D_800A243C[g_DemoKeyIdx];
+        g_StageId = demo_stages[g_DemoKeyIdx];
     }
 
     InitStatsAndGear(0);

include/game.h
@@ -288,25 +288,10 @@ typedef enum {
 #define WEAPON_0_END (WEAPON_1_START - 1)
 #define WEAPON_1_START 0xF0
 
-#if !defined(VERSION_PC) && (defined(VERSION_US) || defined(VERSION_HD))
-#define DRA_PRG_PTR 0x800A0000
-#define SAVE_DATA_PTR 0x801EA000
-#define RIC_PRG_PTR 0x8013C000
-#define SPRITESHEET_PTR 0x8013C020
-#define FAMILIAR_PTR 0x80170000
-#define WEAPON0_PTR 0x8017A000
-#define WEAPON1_PTR 0x8017D000
-#define STAGE_PRG_PTR 0x80180000
-#define CASTLE_MAP_PTR 0x801E0000
+#define DEMO_KEY_LEN 3
+#define DEMO_MAX_LEN 0x2000
 
-#ifndef DEMO_KEY_PTR
-#define DEMO_KEY_PTR 0x801E8000
-#endif
-
-#define SIM_CHR0 0x80280000
-#define SIM_CHR1 0x80284000
-#define SIM_PTR 0x80280000
-#else
+#if defined(VERSION_PSP) || defined(VERSION_PC)
 #define DRA_PRG_PTR 0x800A0000
 #define SAVE_DATA_PTR D_psp_08D97C40
 #define RIC_PRG_PTR &g_PlOvl
@@ -316,11 +301,7 @@ typedef enum {
 #define WEAPON1_PTR &D_8017D000
 #define STAGE_PRG_PTR D_psp_08D2DC40
 #define CASTLE_MAP_PTR g_BmpCastleMap
-
-#ifndef DEMO_KEY_PTR
-#define DEMO_KEY_PTR D_psp_08D95C40
-#endif
-
+#define DEMO_KEY_PTR g_DemoRecordingBuffer
 #define SIM_CHR0 D_psp_08C6BC40
 #define SIM_CHR1 D_psp_08C6FC40
 #define SIM_PTR D_psp_08C6BC40
@@ -329,9 +310,23 @@ extern GAME_IMPORT u8 g_BmpCastleMap[0x20000];
 extern GAME_IMPORT u8 D_psp_08C6BC40[];
 extern GAME_IMPORT u8 D_psp_08C6FC40[];
 extern GAME_IMPORT u8 D_psp_08D2DC40[];
-extern GAME_IMPORT u8 D_psp_08D95C40[];
+extern GAME_IMPORT u8 g_DemoRecordingBuffer[DEMO_MAX_LEN];
 extern GAME_IMPORT u8 D_psp_08D97C40[];
 
+#else
+#define DRA_PRG_PTR 0x800A0000
+#define SAVE_DATA_PTR 0x801EA000
+#define RIC_PRG_PTR 0x8013C000
+#define SPRITESHEET_PTR 0x8013C020
+#define FAMILIAR_PTR 0x80170000
+#define WEAPON0_PTR 0x8017A000
+#define WEAPON1_PTR 0x8017D000
+#define STAGE_PRG_PTR 0x80180000
+#define CASTLE_MAP_PTR 0x801E0000
+#define DEMO_KEY_PTR 0x801E8000
+#define SIM_CHR0 0x80280000
+#define SIM_CHR1 0x80284000
+#define SIM_PTR 0x80280000
 #endif
 
 // used with various equipment, enemy resistances, etc
@@ -513,9 +508,6 @@ typedef enum {
 #define CROSS 0xE9
 #define SQUARE 0xEA
 #define TRIANGLE 0xEB
-
-#define DEMO_KEY_LEN 3
-#define DEMO_MAX_LEN 0x2000
 
 #define FONT_W 8        // small font size used for dialogues and menu
 #define FONT_H 8        // small font size used for dialogues and menu

src/pc/main.c
@@ -58,14 +58,16 @@ static void printHelp(void) {
     printf("  --disk <path>      file name of the second track\n");
     printf("  --stage <stage>    stage name or ID (e.g., nz0)\n");
     printf("  --player <name>    player name or ID (e.g. ric)\n");
+    printf("  --demo <number>    play a specific Tactics demo\n");
     printf("  --scale <number>   game internal resolution integer scale "
            "(default 1)\n");
     printf("  --test <mode>      run automated tests\n");
     printf("         sndlib      test sound library\n");
     printf("  --record <path>    record controller input to a file\n");
     printf("  --replay <path>    replay controller input from a file\n");
-    printf("  --replay-and-exit  quit automatically once the replay ends\n");
-    printf("  --replay-fast      disable frame limit during replay\n");
+    printf("  --replay-and-exit  quit automatically once the replay or demo "
+           "ends\n");
+    printf("  --replay-fast      disable frame limit during replay or demo\n");
     printf("  --help             show this help message\n");
 }
 static void printAllowedParams(const char* allowedValues[], int n) {
@@ -86,6 +88,7 @@ static bool parseArgs(
     outParams->testMode = NO_TEST;
     outParams->stage = -1;
     outParams->player = -1;
+    outParams->demo = -1;
     outParams->scale = 1;
     outParams->recordPath = NULL;
     outParams->replayPath = NULL;
@@ -111,6 +114,12 @@ static bool parseArgs(
             if (outParams->player < 0) {
                 printf("player '%s' is invalid or not recognized\n", argv[i]);
                 printAllowedParams(allowed_players, LEN(allowed_players));
+                return false;
+            }
+        } else if (strcmp(argv[i], "--demo") == 0 && i + 1 < argc) {
+            outParams->demo = parseIntParam(argv[++i]);
+            if (outParams->demo < 0) {
+                printf("invalid demo index %s\n", argv[i]);
                 return false;
             }
         } else if (strcmp(argv[i], "--scale") == 0 && i + 1 < argc) {

src/pc/pc.h
@@ -34,6 +34,7 @@ struct InitGameParams {
     enum TestMode testMode;
     int stage;
     int player;
+    int demo;
     int scale;
     const char* recordPath;
     const char* replayPath;

src/pc/replay.c
@@ -83,6 +83,7 @@ static FILE* file = NULL;
 static bool didDrift = false;
 static bool exitAfterReplay = false;
 static bool fastReplay = false;
+static bool watchingDemo = false;
 
 static unsigned frameCount = 0;
 static long framCountFileOffset = 0;
@@ -420,15 +421,34 @@ static void ReplayFrameReal(void) {
     replayFrameCursor++;
 }
 
-static void ReplayFrame(void) {
-    if (replayFrameCursor < replayFrameCount) {
-        ReplayFrameReal();
-    } else if (exitAfterReplay) {
+static void ReplayEnded(void) {
+    if (exitAfterReplay) {
         g_IsQuitRequested = true;
     } else if (fastReplay) {
         fastReplay = false;
         Psyz_VideoSetVsyncMode(PSYZ_VSYNC_AUTO);
     }
+}
+
+static void ReplayFrame(void) {
+    if (replayFrameCursor < replayFrameCount) {
+        ReplayFrameReal();
+    } else {
+        ReplayEnded();
+    }
+}
+
+static void DemoFrame(void) {
+    static bool demoStarted = false;
+
+    if (g_DemoMode == Demo_PlaybackInit || g_DemoMode == Demo_Playback) {
+        demoStarted = true;
+        return;
+    }
+    if (!demoStarted) {
+        return;
+    }
+    ReplayEnded();
 }
 
 static void StartReplay(const struct InitGameParams* params) {
@@ -534,6 +554,13 @@ static void StartRecording(const struct InitGameParams* params) {
 void Replay_Init(const struct InitGameParams* params) {
     if (params->replayPath) {
         StartReplay(params);
+    } else if (params->demo >= 0) {
+        watchingDemo = true;
+        exitAfterReplay = params->exitAfterReplay;
+        fastReplay = params->replayBoundlessFramerate;
+        if (fastReplay) {
+            Psyz_VideoSetVsyncMode(PSYZ_VSYNC_LIMITLESS);
+        }
     } else {
         // always record, useful for catching bugs
         StartRecording(params);
@@ -562,6 +589,10 @@ void Replay_Reset(void) {
         replayFrameCursor = 0;
         exitAfterReplay = false;
         fastReplay = false;
+    } else if (watchingDemo) {
+        watchingDemo = false;
+        exitAfterReplay = false;
+        fastReplay = false;
     }
     mode = REPLAY_MODE_NONE;
 }
@@ -571,6 +602,8 @@ void Replay_OnFrame(void) {
         RecordFrame();
     } else if (mode == REPLAY_MODE_REPLAY) {
         ReplayFrame();
+    } else if (watchingDemo) {
+        DemoFrame();
     }
 }
 

src/pc/sim_pc.c
@@ -500,7 +500,13 @@ s32 LoadFileSim(s32 fileId, SimFileType type) {
             if (g_GameParams.player >= 0) {
                 g_PlayableCharacter = g_GameParams.player;
             }
-            if (g_GameParams.stage >= 0) {
+            if (g_GameParams.demo >= 0) {
+                D_80097C98 = 0x80000000 |
+                             (g_GameParams.demo & STAGE_INVERTEDCASTLE_MASK);
+                DemoInit(2);
+                SetGameState(Game_NowLoading);
+                g_GameStep = 1;
+            } else if (g_GameParams.stage >= 0) {
                 g_StageId = g_GameParams.stage;
                 SetGameState(Game_NowLoading);
                 g_GameStep = 1;

src/pc/sotn.c
@@ -48,7 +48,7 @@ extern u_long* D_800A3BB8[];
 
 FactoryBlueprint g_FactoryBlueprints[0xC0] = {0};
 u8 g_BmpCastleMap[0x20000];
-u8 D_psp_08D95C40[0x2000];
+u8 g_DemoRecordingBuffer[DEMO_MAX_LEN];
 
 extern u16 g_PalEquipIcon[320 * 16];
 

commit 3abde04a0effd2e8657b21e22f5ab929db75678a

Fix new crash on PC on non-demo runs (#3462)
PC build was broken since #3433 . This fixes is by initializing the same
way it's done on PSP

src/dra/demo.c
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "dra.h"
 
-// BSS
+#ifdef VERSION_PC
+// mimic PSP behavior
+static u8* g_DemoPtr = DEMO_KEY_PTR;
+#else
 static u8* g_DemoPtr;
+#endif
 static s32 g_DemoKeyIdx;
 
 static u8 demo_stages[] = {