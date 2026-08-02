c4c8ad5ebe9bd4753b637aed0826953759b7f626
Joey Murphy <tjmurphy@protonmail.com>
2026-08-02 08:14:25 -0400

    Next func matches

--- src/st/rno0_psp/unk_25FF0.c
+++ src/st/rno0_psp/unk_25FF0.c
@@ -23,7 +23,37 @@ static s16 func_us_801C1F98(Primitive* prim, s16 dx) {
     return dx;
 }
 
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_25FF0", func_us_801C2044);
+static s16 func_us_801C2044(Primitive* prim, s16 dy) {
+    prim->drawMode = DRAW_UNK02;
+    prim->u0 = prim->u2 = 0x50;
+    prim->u1 = prim->u3 = 0x60;
+    prim->x0 = prim->x2 = g_CurrentEntity->posX.i.hi - 8;
+    prim->x1 = prim->x3 = g_CurrentEntity->posX.i.hi + 8;
+    prim->v2 = prim->v3 = 0x26;
+    prim->y2 = prim->y3 = dy;
+    dy += 0x20;
+
+    if (dy >= 0xC0) {
+        dy = dy - 0xC0;
+        prim->v0 = prim->v1 = dy + 6;
+        prim->y0 = prim->y1 = 0xC0;
+
+        prim = prim->next;
+        prim->drawMode = DRAW_UNK02;
+        prim->v0 = prim->v1 = 0x50;
+        prim->v2 = prim->v3 = 0x60;
+        prim->y0 = prim->y1 = 0xBC;
+        prim->y2 = prim->y3 = 0xCC;
+        prim->u0 = prim->u2 = 0x22 - dy;
+        prim->u1 = prim->u3 = 0x22;
+        prim->x0 = prim->x2 = g_CurrentEntity->posX.i.hi;
+        prim->x1 = prim->x3 = g_CurrentEntity->posX.i.hi - dy;
+    } else {
+        prim->v0 = prim->v1 = 6;
+        prim->y0 = prim->y1 = dy;
+    }
+    return dy;
+}
 
 INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_25FF0", func_us_801C2184_from_no0);
 
