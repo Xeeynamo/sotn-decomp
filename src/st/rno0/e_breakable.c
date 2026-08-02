d3eff04c544672a1b05ff8c993232463cc860454
bismurphy <tjmurphy@protonmail.com>
2026-08-02 03:22:52 -0400

    RNO0 e_breakable (#3468)
    Very similar to the previous PR, this one involves more ifdef in the
    function.
    
    Instead of using hardcoded stages, we instead use ifdef based on what
    features to enable in the function, and then the stages enable those
    features when #include'ing the function. Much more semantic option.
    
    The function is getting a bit ugly, but for now seems worthwhile to keep
    in a .h file like this since the differences are still tolerable.

--- src/st/rno0/e_breakable.c
+++ src/st/rno0/e_breakable.c
@@ -1,4 +1,70 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno0.h"
+extern EInit g_EInitBreakable;
 
-INCLUDE_ASM("st/rno0/nonmatchings/e_breakable", EntityBreakable);
+static u8 anim_1[] = {4, 1, 4, 2, 0, 0};
+static u8 anim_2[] = {4, 7, 4, 8, 4, 9, 0, 0};
+static u8 anim_3[] = {4, 29, 4, 30, 4, 31, 0, 0};
+static u8 anim_4[] = {4, 1, 4, 2, 0, 0};
+static u8 anim_5[] = {4, 1, 4, 2, 0, 0};
+static u8 anim_6[] = {4, 1, 4, 2, 0, 0};
+static u8 anim_7[] = {4, 1, 4, 2, 0, 0};
+static u8 anim_8[] = {4, 1, 4, 2, 0, 0};
+static u8 anim_9[] = {4, 1, 4, 2, 0, 0};
+static u8 anim_10[] = {4, 12, 4, 13, 4, 14, 4, 15, 4, 16, 0, 0};
+
+static u8* g_eBreakableAnimations[9] = {
+    anim_1, anim_2, anim_3, anim_4, anim_5, anim_6, anim_7, anim_8, anim_10};
+static u8 g_eBreakableHitboxes[9] = {8, 8, 8, 8, 8, 8, 8, 8, 8};
+static u8 g_eBreakableExplosionTypes[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+static u16 g_eBreakableanimSets[9] = {
+    ANIMSET_DRA(3), ANIMSET_OVL(2), ANIMSET_OVL(2),
+    ANIMSET_DRA(3), ANIMSET_DRA(3), ANIMSET_DRA(3),
+    ANIMSET_DRA(3), ANIMSET_DRA(3), ANIMSET_OVL(2),
+};
+static u8 blend_modes[9] = {
+    BLEND_TRANSP | BLEND_QUARTER, BLEND_TRANSP | BLEND_ADD,
+    BLEND_TRANSP | BLEND_ADD,     BLEND_TRANSP | BLEND_ADD,
+    BLEND_TRANSP | BLEND_ADD,     BLEND_TRANSP | BLEND_ADD,
+    BLEND_TRANSP | BLEND_ADD,     BLEND_TRANSP | BLEND_ADD,
+    BLEND_TRANSP | BLEND_ADD,
+};
+
+// Stripped on PSP
+static u8 unused[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+void EntityBreakable(Entity* entity) {
+    u16 breakableType = entity->params >> 12;
+    if (entity->step) {
+        AnimateEntity(g_eBreakableAnimations[breakableType], entity);
+        if (entity->hitParams) { // If the candle is destroyed
+            Entity* entityDropItem;
+            if (!breakableType) {
+                entity->drawFlags = ENTITY_DEFAULT;
+                entity->rotate = ROT(0);
+            }
+
+            breakableType == 1 ? g_api.PlaySfx(SFX_GLASS_BREAK_C)
+                               : g_api.PlaySfx(SFX_CANDLE_HIT_WHOOSH_A);
+
+            entityDropItem = AllocEntity(&g_Entities[224], &g_Entities[256]);
+            if (entityDropItem != NULL) {
+                CreateEntityFromCurrentEntity(E_EXPLOSION, entityDropItem);
+                entityDropItem->params =
+                    g_eBreakableExplosionTypes[breakableType];
+            }
+            ReplaceBreakableWithItemDrop(entity);
+        }
+    } else {
+        InitializeEntity(g_EInitBreakable);
+        entity->zPriority = g_unkGraphicsStruct.g_zEntityCenter - 20;
+        entity->blendMode = blend_modes[breakableType];
+        entity->hitboxHeight = g_eBreakableHitboxes[breakableType];
+        entity->animSet = g_eBreakableanimSets[breakableType];
+        if (!breakableType) {
+            entity->drawFlags = ENTITY_ROTATE;
+            entity->rotate = ROT(180);
+        }
+    }
+}
