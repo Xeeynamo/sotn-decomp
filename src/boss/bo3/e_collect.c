52eaf5f3e03c4370d466d400e799748f1d98d245
sozud <122322823+sozud@users.noreply.github.com>
2026-08-04 05:38:08 +0000

    dedupe bo3 overlay sources

--- src/boss/bo3/e_collect.c
+++ src/boss/bo3/e_collect.c
@@ -1,56 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "bo3.h"
 
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", PrizeDropFall);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", PrizeDropFall2);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", CollectHeart);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", CollectGold);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", CollectSubweapon);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", CollectHeartVessel);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", CollectLifeVessel);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", CollectDummy);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB3C);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB44);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB4C);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB54);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB5C);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB64);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB6C);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB74);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB7C);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CB84);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", EntityPrizeDrop);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", EntityExplosion);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", BlinkItem);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", EntityEquipItemDrop);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", BlitChar);
-
-INCLUDE_RODATA("boss/bo3/nonmatchings/e_collect", D_us_8019CBC4);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", EntityRelicOrb);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", EntityHeartDrop);
-
-INCLUDE_ASM("boss/bo3/nonmatchings/e_collect", EntityMessageBox);
+#include "../../st/pfn_entity_update.h"
+#define GOLD_COLLECT_TEXT                                                      \
+    _S("$1"), _S("$25"), _S("$50"), _S("$100"), _S("$250"), _S("$400"),        \
+        _S("$700"), _S("$1000"), _S("$2000"), _S("$5000"),
+#define HEART_DROP_CASTLE_FLAG 0x40
+#include "../../st/e_collect.h"
