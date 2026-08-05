110c9b22d94f4fe5342dd0c34d339d4a1c002137
Joey Murphy <tjmurphy@protonmail.com>
2026-08-05 01:32:14 -0400

    palettes good

---
+++ src/st/rno0/stage_data.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+u16 D_80190D20[] = {
+#include "gen/D_80190D20.h"
+};
+u16 D_80190FE0[] = {
+#include "gen/D_80190FE0.h"
+};
+u16 D_80191160[] = {
+#include "gen/D_80191160.h"
+};
+u16 D_80191280[] = {
+#include "gen/D_80191280.h"
+};
+u16 D_80191300[] = {
+#include "gen/D_80191300.h"
+};
+u16 D_80191320[] = {
+#include "gen/D_80191320.h"
+};
+u16 D_80191360[] = {
+#include "gen/D_80191360.h"
+};
+u16 D_80191400[] = {
+#include "gen/D_80191400.h"
+};
+u16 D_80191520[] = {
+#include "gen/D_80191520.h"
+};
+u16 D_80191660[] = {
+#include "gen/D_80191660.h"
+};
+u16 D_801917C0[] = {
+#include "gen/D_801917C0.h"
+};
+u16 D_80191800[] = {
+#include "gen/D_80191800.h"
+};
+u16 D_80191840[] = {
+#include "gen/D_80191840.h"
+};
