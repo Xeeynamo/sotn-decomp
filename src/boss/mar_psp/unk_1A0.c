42ecfad000eed2a234687b2c9fdabaff2cb33ecb
sozud <122322823+sozud@users.noreply.github.com>
2026-08-05 19:47:37 +0700

    fixes

--- src/boss/mar_psp/unk_1A0.c
+++ src/boss/mar_psp/unk_1A0.c
@@ -1,33 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../mar/mar.h"
 
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", func_pspeu_09237820);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", func_pspeu_092379B8);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", FindFirstEntityToTheRight);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", FindFirstEntityToTheLeft);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", CreateEntitiesToTheRight);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", CreateEntitiesToTheLeft);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", FindFirstEntityAbove);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", FindFirstEntityBelow);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", CreateEntitiesAbove);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", CreateEntitiesBelow);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", InitRoomEntities);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", UpdateRoomPosition);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", CreateEntityFromCurrentEntity);
-
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", CreateEntityFromEntity);
+u8 D_pspeu_09255020[8];
 
 INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/unk_1A0", func_pspeu_092386A8);
 
