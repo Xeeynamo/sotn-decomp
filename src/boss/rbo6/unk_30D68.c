423e023b9788d5db8c1dc67ca65d4104a46e762d
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-06 18:50:17 -0300

    Code-share RBO6 prim_helpers

--- src/boss/rbo6/unk_30D68.c
+++ src/boss/rbo6/unk_30D68.c
@@ -3,8 +3,6 @@
 
 INCLUDE_ASM("boss/rbo6/nonmatchings/unk_30D68", EntityBackgroundVortex);
 
-INCLUDE_RODATA("boss/rbo6/nonmatchings/unk_30D68", D_us_8019D0AC);
-
 INCLUDE_ASM("boss/rbo6/nonmatchings/unk_30D68", func_us_801B1738);
 
 INCLUDE_ASM("boss/rbo6/nonmatchings/unk_30D68", func_us_801B2864);
