899fbe6a78f856ba82da38d005a6d55b7adaf393
Joey Murphy <tjmurphy@protonmail.com>
2026-08-08 08:25:37 -0400

    split psp file

---
+++ src/st/rno0_psp/e_nova_skeleton.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/e_nova_skeleton", TryShoot);
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/e_nova_skeleton", DrawLaserRing);
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/e_nova_skeleton", EntityNovaSkeleton);
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/e_nova_skeleton", EntityBladeSoldierDeathParts);
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/e_nova_skeleton", EntityNovaLaser);
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/e_nova_skeleton", EntityNovaLaserPulse);
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/e_nova_skeleton", EntityStoneSkull);
--- src/st/rno0_psp/unk_18870.c
+++ src/st/rno0_psp/unk_18870.c
@@ -6,17 +6,3 @@ INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", EntityBackgroundPilla
 INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", func_us_801CC8F8_from_no0);
 
 INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", func_us_801CC9B4_from_no0);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", TryShoot);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", DrawLaserRing);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", EntityNovaSkeleton);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", EntityBladeSoldierDeathParts);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", EntityNovaLaser);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", EntityNovaLaserPulse);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_18870", EntityStoneSkull);
