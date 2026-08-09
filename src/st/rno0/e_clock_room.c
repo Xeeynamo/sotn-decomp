d175d9db78568670335b15c32aa533d476c0a203
Joey Murphy <tjmurphy@protonmail.com>
2026-08-09 08:43:37 -0400

    first func ported intact

--- src/st/rno0/e_clock_room.c
+++ src/st/rno0/e_clock_room.c
@@ -1,7 +1,33 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno0.h"
 
-INCLUDE_ASM("st/rno0/nonmatchings/e_clock_room", func_us_801CCAAC_from_no0);
+void func_us_801CCAAC(Entity* self) {
+    Entity* tempEntity;
+    s16 angle;
+
+    if ((self->ext.clockRoom.unk88 & 0x1F) == 0) {
+        g_api.PlaySfxVolPan(SFX_STONE_MOVE_A, 0x40, 0);
+    }
+    self->ext.clockRoom.unk88++;
+
+    // Minute hand
+    tempEntity = self + 5;
+    angle =
+        tempEntity->ext.clockRoom.unk80 +
+        (LOW(tempEntity->ext.clockRoom.bellTimer) * self->ext.clockRoom.unk88) /
+            512;
+    angle %= (60 * 60);
+    tempEntity->ext.clockRoom.hand = angle;
+
+    // Hour hand
+    tempEntity++;
+    angle =
+        tempEntity->ext.clockRoom.unk80 -
+        (LOW(tempEntity->ext.clockRoom.bellTimer) * self->ext.clockRoom.unk88) /
+            512;
+    angle %= (60 * 60);
+    tempEntity->ext.clockRoom.hand = angle;
+}
 
 INCLUDE_ASM("st/rno0/nonmatchings/e_clock_room", UpdateBirdcages);
 
--- src/st/rno0_psp/e_clock_room.c
+++ src/st/rno0_psp/e_clock_room.c
@@ -1,7 +1,33 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rno0/rno0.h"
 
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_1028", func_us_801CCAAC);
+void func_us_801CCAAC(Entity* self) {
+    Entity* tempEntity;
+    s16 angle;
+
+    if ((self->ext.clockRoom.unk88 & 0x1F) == 0) {
+        g_api.PlaySfxVolPan(SFX_STONE_MOVE_A, 0x40, 0);
+    }
+    self->ext.clockRoom.unk88++;
+
+    // Minute hand
+    tempEntity = self + 5;
+    angle =
+        tempEntity->ext.clockRoom.unk80 +
+        (LOW(tempEntity->ext.clockRoom.bellTimer) * self->ext.clockRoom.unk88) /
+            512;
+    angle %= (60 * 60);
+    tempEntity->ext.clockRoom.hand = angle;
+
+    // Hour hand
+    tempEntity++;
+    angle =
+        tempEntity->ext.clockRoom.unk80 -
+        (LOW(tempEntity->ext.clockRoom.bellTimer) * self->ext.clockRoom.unk88) /
+            512;
+    angle %= (60 * 60);
+    tempEntity->ext.clockRoom.hand = angle;
+}
 
 INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_1028", UpdateBirdcages);
 
