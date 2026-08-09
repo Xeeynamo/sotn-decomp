bc8530e5bbcc52134f9d54cd086c3e29001113ff
Joey Murphy <tjmurphy@protonmail.com>
2026-08-09 08:41:07 -0400

    unsplit psp file

--- src/st/rno0_psp/e_clock_room.c
+++ src/st/rno0_psp/e_clock_room.c
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rno0/rno0.h"
 
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_1028", func_us_801CCAAC);
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_1028", UpdateBirdcages);
+
+INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_1028", UpdateClockHands);
+
 void UpdateBirdcages(Entity* self, u32 timerMinutes);
 
 // Two statues. One responds to the stopwatch subweapon.
