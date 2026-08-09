df399ec7108be40966c57997a7fe08588280e0ad
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-09 05:43:48 -0300

    rno1: wire psp e_paranthropus whole TU (5 fns) via shared header extraction from are

---
+++ src/st/rno1/e_paranthropus.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "rno1.h"
+
+#ifdef VERSION_PSP
+// psp symbols keep the D_us_* name used by sibling asm objects
+#define g_EInitParanthropusThrownBone D_us_801806E8
+#endif
+
+#include "../e_paranthropus.h"
