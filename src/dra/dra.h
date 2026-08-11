494b4676b1b2eb2b5a22cc18afbb2356ec0b9b5f
gamezter <charronalexandre@hotmail.com>
2026-08-10 20:42:07 -0400

    Decompile a few `sat/game` functions (#3528)

--- src/dra/dra.h
+++ src/dra/dra.h
@@ -766,7 +766,6 @@ void DemoGameInit(s32 arg0);
 void FreePrimitives(s32 index);
 void DemoOpenFile(s32);
 void DemoInit(s32 arg0);
-s32 func_800F087C(u32, u32);
 s32 SetNextRoomToLoad(u32 chunkX, u32 chunkY);
 s32 func_800F0CD8(s32 arg0);
 s32 func_800F16D0(void);
