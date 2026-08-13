b0f0e6b8e8b309325ad1f834b560293ed96dc207
Joey Murphy <tjmurphy@protonmail.com>
2026-08-13 19:36:47 -0400

    Split the skeletons

---
+++ src/st/rno0/e_jack_o_bones.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "rno0.h"
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_jack_o_bones", TryThrow);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_jack_o_bones", EntityJackOBones);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_jack_o_bones", EntityJackOBonesDeathParts);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_jack_o_bones", EntityJackOBonesJack);
---
+++ src/st/rno0/e_nova_skeleton.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_nova_skeleton", TryShoot);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_nova_skeleton", DrawLaserRing);
+
+INCLUDE_RODATA("st/rno0/nonmatchings/e_nova_skeleton", D_us_801B5D8C);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_nova_skeleton", EntityNovaSkeleton);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_nova_skeleton", EntityBladeSoldierDeathParts);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_nova_skeleton", EntityNovaLaser);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_nova_skeleton", EntityNovaLaserPulse);
