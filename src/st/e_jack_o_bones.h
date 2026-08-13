1d66431ea82ce09ae1f5f5cba5029b3c7013268e
Joey Murphy <tjmurphy@protonmail.com>
2026-08-13 19:49:43 -0400

    New animation thing

--- src/st/e_jack_o_bones.h
+++ src/st/e_jack_o_bones.h
@@ -2,12 +2,13 @@ extern EInit g_EInitJackOBones2;
 extern EInit g_EInitJackOBones3;
 extern EInit g_EInitJackOBones;
 
-static u8 anim_walk_fwd[] = {6, 1, 4, 2, 4, 3, 6, 4, 5, 5, 5, 6, 0};
-static u8 anim_walk_back[] = {6, 1, 5, 6, 5, 5, 6, 4, 4, 3, 4, 2, 0};
-static u8 anim_throw[] = {5, 1,  5,  2,  5, 7, 6, 8, 5, 9, 5,   10,
-                          5, 11, 31, 12, 4, 4, 4, 5, 4, 6, 255, 0};
-static u8 anim_jump_windup[] = {1, 1, 4, 13, 4, 14, 1, 1, 255, 0};
-static u8 anim_jump_landing[] = {1, 1, 4, 13, 6, 14, 4, 13, 1, 1, 255, 0};
+static AnimateEntityFrame anim_walk_fwd[] = {{6, 1}, {4, 2}, {4, 3}, {6, 4}, {5, 5}, {5, 6}, POSE_LOOP(0)};
+static AnimateEntityFrame anim_walk_back[] = {{6, 1}, {5, 6}, {5, 5}, {6, 4}, {4, 3}, {4, 2}, POSE_LOOP(0)};
+static AnimateEntityFrame anim_throw[] = {{5, 1},  {5,  2},  {5, 7}, {6, 8}, {5, 9}, {5,   10},
+                          {5, 11}, {31, 12}, {4, 4}, {4, 5}, {4, 6}, POSE_END};
+static AnimateEntityFrame anim_jump_windup[] = {{1, 1}, {4, 13}, {4, 14}, {1, 1}, POSE_END};
+static AnimateEntityFrame anim_jump_landing[] = {{1, 1}, {4, 13}, {6, 14}, {4, 13}, {1, 1}, POSE_END};
+
 static u16 death_parts_rotspeeds[] = {
     0x100, 0x80, 0x48, 0x20, 0x40, 0x10, 0x18, 0};
 static u8 death_parts_lifetimes[] = {48, 32, 20, 12, 24, 16, 20, 0};
