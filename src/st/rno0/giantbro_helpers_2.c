13583c02a047dd890bb9958f538afc999da14a5c
Joey Murphy <tjmurphy@protonmail.com>
2026-08-14 06:33:37 -0400

    Helpers 2 in this overlay

--- src/st/rno0/giantbro_helpers_2.c
+++ src/st/rno0/giantbro_helpers_2.c
@@ -1,15 +1,3 @@
-#include "common.h"
+#include "rno0.h"
 
-INCLUDE_ASM("st/rno0/nonmatchings/giantbro_helpers_2", func_801CE04C);
-
-INCLUDE_ASM("st/rno0/nonmatchings/giantbro_helpers_2", func_801CE120);
-
-INCLUDE_ASM("st/rno0/nonmatchings/giantbro_helpers_2", func_801CE1E8);
-
-INCLUDE_ASM("st/rno0/nonmatchings/giantbro_helpers_2", func_801CE228);
-
-INCLUDE_ASM("st/rno0/nonmatchings/giantbro_helpers_2", polarPlacePartsList);
-
-INCLUDE_ASM("st/rno0/nonmatchings/giantbro_helpers_2", func_801CE2CC);
-
-INCLUDE_ASM("st/rno0/nonmatchings/giantbro_helpers_2", func_801CE3FC);
+#include "../giantbro_helpers_2.h"
