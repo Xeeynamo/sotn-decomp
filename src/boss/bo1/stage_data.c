77e0b968c13d1a31c1429d7bbefee8f1c80b4846
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-08-14 15:32:41 +0100

    bo1: assets

---
+++ src/boss/bo1/stage_data.c
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+u8 gfx_1D84[] = {
+#include "gen/gfx_1D84.h"
+};
+u8 gfx_2F38[] = {
+#include "gen/gfx_2F38.h"
+};
+u8 gfx_3BA4[] = {
+#include "gen/gfx_3BA4.h"
+};
+u8 gfx_4858[] = {
+#include "gen/gfx_4858.h"
+};
+u8 gfx_5844[] = {
+#include "gen/gfx_5844.h"
+};
+u8 gfx_6934[] = {
+#include "gen/gfx_6934.h"
+};
+u8 gfx_7704[] = {
+#include "gen/gfx_7704.h"
+};
+u8 gfx_872C[] = {
+#include "gen/gfx_872C.h"
+};
+u8 gfx_966C[] = {
+#include "gen/gfx_966C.h"
+};
+u8 gfx_A73C[] = {
+#include "gen/gfx_A73C.h"
+};
+u8 gfx_B4E8[] = {
+#include "gen/gfx_B4E8.h"
+};
+u8 gfx_C214[] = {
+#include "gen/gfx_C214.h"
+};
+u8 gfx_D264[] = {
+#include "gen/gfx_D264.h"
+};
+u8 gfx_E170[] = {
+#include "gen/gfx_E170.h"
+};
+u8 gfx_F658[] = {
+#include "gen/gfx_F658.h"
+};
+u8 gfx_108E0[] = {
+#include "gen/gfx_108E0.h"
+};
+u8 gfx_11A08[] = {
+#include "gen/gfx_11A08.h"
+};
+u8 gfx_12984[] = {
+#include "gen/gfx_12984.h"
+};
+u8 gfx_13330[] = {
+#include "gen/gfx_13330.h"
+};
+u8 gfx_14228[] = {
+#include "gen/gfx_14228.h"
+};
+u16 pal_14918[] = {
+#include "gen/pal_14918.h"
+};
+u16 pal_149D8[] = {
+#include "gen/pal_149D8.h"
+};
+
+// The tilemaps and tile definitions are only emitted as data on PSP/PC; on the
+// PSX they live in the raw data region at 0x14BD8.
+#if defined(VERSION_PSP) || defined(VERSION_PC)
+#include "gen/bo1_tilemap_0.h"
+#include "gen/bo1_tilemap_1.h"
+#include "gen/bo1_tiledef_0.h"
+#endif
---
+++ src/boss/bo1/stage_palettes.c
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <stage.h>
+
+// The palettes the palette_def copies into the CLUT. Each one spans up to the
+// next, so a single entry may hold more than one 16 color palette.
+u16 pal_8C[] = {
+#include "gen/pal_8C.h"
+};
+u16 pal_AC[] = {
+#include "gen/pal_AC.h"
+};
+u16 pal_CC[] = {
+#include "gen/pal_CC.h"
+};
+u16 pal_12C[] = {
+#include "gen/pal_12C.h"
+};
+u16 pal_18C[] = {
+#include "gen/pal_18C.h"
+};
+u16 pal_1EC[] = {
+#include "gen/pal_1EC.h"
+};
+u16 pal_24C[] = {
+#include "gen/pal_24C.h"
+};
+u16 pal_26C[] = {
+#include "gen/pal_26C.h"
+};
+u16 pal_2CC[] = {
+#include "gen/pal_2CC.h"
+};
+u16 pal_2EC[] = {
+#include "gen/pal_2EC.h"
+};
+u16 pal_30C[] = {
+#include "gen/pal_30C.h"
+};
+u16 pal_32C[] = {
+#include "gen/pal_32C.h"
+};
+u16 pal_34C[] = {
+#include "gen/pal_34C.h"
+};
+u16 pal_38C[] = {
+#include "gen/pal_38C.h"
+};
+u16 pal_3CC[] = {
+#include "gen/pal_3CC.h"
+};
+u16 pal_3EC[] = {
+#include "gen/pal_3EC.h"
+};
+u16 pal_40C[] = {
+#include "gen/pal_40C.h"
+};
+u16 pal_4CC[] = {
+#include "gen/pal_4CC.h"
+};
