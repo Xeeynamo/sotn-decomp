5494f4094b069c7b35b1e3d57f53852ca3352e6f
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-08-14 16:35:06 +0100

    pc: hook bo1

---
+++ src/pc/bosses/boss_bo1.c
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include <game.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../stages/overlay.h"
+#include "../boss/bo1/bo1.h"
+
+extern AbbreviatedOverlay g_BossOverlay;
+extern PfnEntityUpdate EntityUpdates[];
+extern LayoutEntity* entityLayoutHorizontal[];
+extern LayoutEntity* entityLayoutVertical[];
+extern GAME_IMPORT PfnEntityUpdate* PfnEntityUpdates;
+extern GAME_IMPORT LayoutEntity** g_pStObjLayoutHorizontal;
+extern GAME_IMPORT LayoutEntity** g_pStObjLayoutVertical;
+
+// stubs for WIP entities
+void func_us_801A2F2C(Entity* self) { DestroyEntity(self); }
+void func_us_801A38EC(Entity* self) { DestroyEntity(self); }
+
+OVL_API void InitStage(Overlay* o) {
+    memcpy(o, &g_BossOverlay, sizeof(AbbreviatedOverlay));
+    PfnEntityUpdates = EntityUpdates;
+    g_pStObjLayoutHorizontal = entityLayoutHorizontal;
+    g_pStObjLayoutVertical = entityLayoutVertical;
+}
