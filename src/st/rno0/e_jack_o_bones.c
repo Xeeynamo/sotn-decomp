8bbe3950744117ed1ab847aae46c953820caef7f
bismurphy <tjmurphy@protonmail.com>
2026-08-14 01:22:55 -0400

    RNO0 Jack O Bones and Nova Skeleton (#3560)
    No differences from the existing versions, but had to move these to .h
    files, and updated the animation arrays while I was at it. Otherwise
    very straightforward.

--- src/st/rno0/e_jack_o_bones.c
+++ src/st/rno0/e_jack_o_bones.c
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "rno3.h"
+#include "rno0.h"
 
 #include "../e_jack_o_bones.h"
--- src/st/rno0/e_nova_skeleton.c
+++ src/st/rno0/e_nova_skeleton.c
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-#include "rno3.h"
+#include "rno0.h"
 
 #include "../e_nova_skeleton.h"
