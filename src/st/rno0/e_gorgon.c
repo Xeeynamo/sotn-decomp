cdd36456eebccfd2a5dd056bf6d4b749cc370454
Joey Murphy <tjmurphy@protonmail.com>
2026-08-15 07:36:11 -0400

    US Unsplit Gorgon

--- src/st/rno0/e_gorgon.c
+++ src/st/rno0/e_gorgon.c
@@ -18,3 +18,31 @@ INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", StepTowards);
 INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801CF64C);
 
 INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801CF7D0);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801CF968);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801CFB20);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801CFC98);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801CFD70);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801CFE6C);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", EntityGorgon);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801D068C);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801D0CFC);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801D136C);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801D15C0);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801D1BF0);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801D2038);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801D21C8);
+
+INCLUDE_ASM("st/rno0/nonmatchings/e_gorgon", func_us_801D2264);
