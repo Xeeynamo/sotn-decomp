5e82400153cf9975caf0fbcde018086a4a2b954c
Joey Murphy <tjmurphy@protonmail.com>
2026-08-15 07:44:03 -0400

    another

--- src/st/rno0_psp/unk_62A8.c
+++ src/st/rno0_psp/unk_62A8.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rno0/rno0.h"
 
-// Very similar to a giantbro helper function
+// These first two are copied from the Plate Lord?
 static void func_pspeu_0923D928(Point32* src, s32 speed, s16 angle, Point32* dst) {
     if (g_CurrentEntity->facingLeft) {
         angle = -angle;
@@ -12,7 +12,28 @@ static void func_pspeu_0923D928(Point32* src, s32 speed, s16 angle, Point32* dst
     dst->y += speed * rcos(angle) * 16;
 }
 
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_62A8", func_us_801D2424);
+static void func_us_801D2424(Pos* arg0, s16 arg1, Point16* arg2, Pos* arg3, s16 arg4,
+                             Point16* arg5, Primitive* prim) {
+    prim->x0 = prim->x1 = arg0->x.i.hi;
+    prim->y0 = prim->y1 = arg0->y.i.hi;
+    prim->x2 = prim->x3 = arg3->x.i.hi;
+    prim->y2 = prim->y3 = arg3->y.i.hi;
+    if (g_CurrentEntity->facingLeft) {
+        prim->x0 += FLT_TO_I(arg2->x * rcos(arg1));
+        prim->x1 -= FLT_TO_I(arg2->y * rcos(arg1));
+        prim->x2 += FLT_TO_I(arg5->x * rcos(arg4));
+        prim->x3 -= FLT_TO_I(arg5->y * rcos(arg4));
+    } else {
+        prim->x0 -= FLT_TO_I(arg2->x * rcos(arg1));
+        prim->x1 += FLT_TO_I(arg2->y * rcos(arg1));
+        prim->x2 -= FLT_TO_I(arg5->x * rcos(arg4));
+        prim->x3 += FLT_TO_I(arg5->y * rcos(arg4));
+    }
+    prim->y0 -= FLT_TO_I(arg2->x * rsin(arg1));
+    prim->y1 += FLT_TO_I(arg2->y * rsin(arg1));
+    prim->y2 -= FLT_TO_I(arg5->x * rsin(arg4));
+    prim->y3 += FLT_TO_I(arg5->y * rsin(arg4));
+}
 
 INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_62A8", func_pspeu_0923DD78);
 
