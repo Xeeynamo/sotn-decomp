4ab2128520f2d8a5472ba5467def70838a1e910c
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2026-08-17 15:53:18 +1000

    Dedupe some PSP `rtop` (#3584)
    Bit more to go but starting to require file splits to get the ordering,
    this seems a reasonable point to PR and I will do more in another later
    on

--- src/st/rtop_psp/e_lock_camera.c
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rtop/rtop.h"
-
-INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/e_lock_camera", PlayerIsWithinHitbox);
-
-INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/e_lock_camera", EntityLockCamera);
--- src/st/rtop_psp/unk_11508.c
+++ src/st/rtop_psp/unk_11508.c
@@ -4,15 +4,3 @@
 INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/unk_11508", EntityTriangleElevator);
 
 INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/unk_11508", func_us_801A1940);
-
-INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/unk_11508", EntityStairSwitch);
-
-INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/unk_11508", func_us_801A0BF4);
-
-INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/unk_11508", EntitySecretStairs);
-
-INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/unk_11508", EntityLionLamp);
-
-INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/unk_11508", func_us_801A21F8);
-
-INCLUDE_ASM("st/rtop_psp/nonmatchings/rtop_psp/unk_11508", EntityBackgroundBlock);
