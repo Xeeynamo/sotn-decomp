b82a45a88febeb3466931a77a1ea3073a0bbee07
Joey Murphy <tjmurphy@protonmail.com>
2026-08-17 17:41:46 -0400

    sealed door

--- src/st/rno0/unk_5289C.c
+++ src/st/rno0/unk_5289C.c
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno0.h"
 
-INCLUDE_ASM("st/rno0/nonmatchings/unk_5289C", func_us_801D289C);
-
-INCLUDE_ASM("st/rno0/nonmatchings/unk_5289C", EntitySealedDoor);
+#define SEALED_DOOR_PALETTE 0x259
+#include "../e_sealed_door.h"
