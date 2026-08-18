06417fe720e1268d0faa8694ec782a3f60992b6f
gamezter <gamezter93@gmail.com>
2026-08-18 09:45:20 -0400

    revert

--- src/st/mad/collision.c
+++ src/st/mad/collision.c
@@ -102,7 +102,7 @@ void HitDetection(void) {
     u16 miscVar1;
     u16 hitboxCheck2;
     s32 hitboxWidth;
-    s32 hitboxHeight;
+    s32 yCoord1;
     u32 hitboxCheck1;
     EnemyDef* enemyDef;
     u16 i;
