1fab0c8f235905aaff6be75ae2c49b7b2687dd16
Christian Paez <45642493+christianpaez@users.noreply.github.com>
2026-08-19 17:18:29 -0500

    decomp RicStepFall func (#3589)

--- src/boss/bo6/richter.c
+++ src/boss/bo6/richter.c
@@ -1095,7 +1095,22 @@ INCLUDE_ASM("boss/bo6/nonmatchings/richter", RicStepRun);
 
 INCLUDE_ASM("boss/bo6/nonmatchings/richter", RicStepJump);
 
-INCLUDE_ASM("boss/bo6/nonmatchings/richter", RicStepFall);
+void RicStepFall(void) {
+    if (RicCheckInput(
+            CHECK_GROUND | CHECK_FACING | CHECK_ATTACK | CHECK_GRAVITY_FALL)) {
+        return;
+    }
+    DecelerateX(FIX(1. / 16));
+    if (RIC.step_s == 0) {
+        if (g_Ric.timers[PL_T_5] && g_Ric.padTapped & PAD_CROSS) {
+            func_us_801B9E70();
+            return;
+        }
+        if (RicCheckFacing()) {
+            RicSetSpeedX(FIX(0.75));
+        }
+    }
+}
 
 INCLUDE_ASM("boss/bo6/nonmatchings/richter", RicStepCrouch);
 
