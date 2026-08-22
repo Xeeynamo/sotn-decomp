5395faa34393d4cab925221edbfe13fcf397a9eb
Joey Murphy <tjmurphy@protonmail.com>
2026-08-22 07:25:38 -0400

    psp split 1

---
+++ src/st/rnz1_psp/unk_1C058.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "../rnz1/rnz1.h"
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_1C058", EntityBreakable);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_1C058", EntityBreakableDebris);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_1C058", EntityWaterForeground);
