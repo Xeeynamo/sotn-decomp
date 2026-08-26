d4d39d26f7d10ca38d347662d19d8ad0d1bcf330
Joey Murphy <tjmurphy@protonmail.com>
2026-08-26 08:14:55 -0400

    Split EntityGearSidewaysSmall

--- src/st/rnz1/unk_27E60.c
+++ src/st/rnz1/unk_27E60.c
@@ -1,17 +1,3 @@
 #include "common.h"
 
 INCLUDE_ASM("st/rnz1/nonmatchings/unk_27E60", EntityGearSidewaysSmall);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_27E60", EntityWallGear);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_27E60", EntitySecretAreaDoor);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_27E60", EntityBreakableWall);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_27E60", EntityBreakableWallPartial);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_27E60", EntitySecretWallDebris);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_27E60", func_us_801A8F7C);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_27E60", EntityWaterForeground);
