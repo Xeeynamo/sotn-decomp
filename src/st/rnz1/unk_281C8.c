59d6968aa41f9090234bcd1967efb5a749b822ea
Joey Murphy <tjmurphy@protonmail.com>
2026-08-26 08:19:58 -0400

    Split e_gear_puzzle

--- src/st/rnz1/unk_281C8.c
+++ src/st/rnz1/unk_281C8.c
@@ -3,13 +3,3 @@
 INCLUDE_ASM("st/rnz1/nonmatchings/unk_281C8", EntityWallGear);
 
 INCLUDE_ASM("st/rnz1/nonmatchings/unk_281C8", EntitySecretAreaDoor);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_281C8", EntityBreakableWall);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_281C8", EntityBreakableWallPartial);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_281C8", EntitySecretWallDebris);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_281C8", func_us_801A8F7C);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_281C8", EntityWaterForeground);
---
+++ src/st/rnz1/unk_28668.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM("st/rnz1/nonmatchings/unk_28668", EntityBreakableWall);
+
+INCLUDE_ASM("st/rnz1/nonmatchings/unk_28668", EntityBreakableWallPartial);
+
+INCLUDE_ASM("st/rnz1/nonmatchings/unk_28668", EntitySecretWallDebris);
+
+INCLUDE_ASM("st/rnz1/nonmatchings/unk_28668", func_us_801A8F7C);
+
+INCLUDE_ASM("st/rnz1/nonmatchings/unk_28668", EntityWaterForeground);
