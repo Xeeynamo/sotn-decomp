6582514e519f53788aab78920bbc2cd2db2e0658
Joey Murphy <tjmurphy@protonmail.com>
2026-08-26 07:55:32 -0400

    good bg gears

--- src/st/rnz1/unk_276A8.c
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "rnz1.h"
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_276A8", EntityBackgroundGears);
--- src/st/rnz1_psp/unk_16E70.c
+++
@@ -1,3 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_16E70", EntityBackgroundGears);
