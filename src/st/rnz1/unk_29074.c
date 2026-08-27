2650b87504cfd7f73e55097bd931e710752759d6
Joey Murphy <tjmurphy@protonmail.com>
2026-08-26 22:00:07 -0400

    good foreground water us

--- src/st/rnz1/unk_29074.c
+++ src/st/rnz1/unk_29074.c
@@ -1,3 +1,49 @@
-#include "common.h"
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "rnz1.h"
 
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_29074", EntityWaterForeground);
+extern EInit g_EInitInteractable;
+
+static s16 D_us_80181094[][4] = {
+    {0x0A0, 0x020, 0x50, 0x20}, {0x010, 0x020, 0x80, 0x20},
+    {0x000, 0x030, 0x50, 0x20}, {0x0A0, 0x0B0, 0x50, 0x20},
+    {0x010, 0x020, 0x30, 0x20}, {0x050, 0x020, 0x60, 0x20},
+    {0x0C0, 0x020, 0x30, 0x20},
+};
+
+void EntityWaterForeground(Entity* self) {
+    Primitive* prim;
+    s32 primIndex;
+    s16* ptr;
+    s32 x, y;
+    s32 params;
+
+    switch (self->step) {
+    case 0:
+        InitializeEntity(g_EInitInteractable);
+        primIndex = g_api.AllocPrimitives(PRIM_TILE, 1);
+        if (primIndex == -1) {
+            DestroyEntity(self);
+            return;
+        }
+        self->flags |= FLAG_HAS_PRIMS;
+        self->primIndex = primIndex;
+        prim = &g_PrimBuf[primIndex];
+        self->ext.prim = prim;
+        prim->r0 = 0x48;
+        prim->g0 = 0x40;
+        prim->b0 = 0xC0;
+        params = self->params;
+        ptr = (s16*)D_us_80181094[params];
+        x = ptr[0] - g_Tilemap.scrollX.i.hi;
+        y = ptr[1] - g_Tilemap.scrollY.i.hi;
+        prim->x0 = x;
+        prim->y0 = y;
+        prim->u0 = ptr[2];
+        prim->v0 = ptr[3];
+        prim->priority = 0x9A;
+        prim->drawMode = DRAW_TPAGE | DRAW_UNK02 | DRAW_TRANSP;
+        break;
+    case 1:
+        break;
+    }
+}
