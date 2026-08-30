2b239d157005f320ff1e6f701277640e0adda0f8
sozud <122322823+sozud@users.noreply.github.com>
2026-08-30 23:38:38 +0700

    pc: build reverse castle stage overlays

--- src/pc/stages/stage_rno0.c
+++ src/pc/stages/stage_rno0.c
@@ -7,29 +7,29 @@
 extern RoomHeader rooms[];
 extern RoomDef rooms_layers[];
 extern GfxBank* gfxBanks[];
+extern LayoutEntity* entityLayoutHorizontal[];
+extern LayoutEntity* entityLayoutVertical[];
 extern GAME_IMPORT PfnEntityUpdate* PfnEntityUpdates;
 extern GAME_IMPORT LayoutEntity** g_pStObjLayoutHorizontal;
 extern GAME_IMPORT LayoutEntity** g_pStObjLayoutVertical;
 
-EInit g_EInitCommon;
+EInit g_EInitCommon = {ANIMSET_DRA(0), 0, 0, 0, 3};
 EInit g_EInitDamageNum;
+EInit g_EInitParticle;
+EInit g_EInitUnkId13;
+AnimateEntityFrame g_bigRedFireballAnim[1];
 static SpriteParts* s_SpriteBanks[] = {NULL};
 static u_long s_EmptyClut[] = {(u_long)-1};
 static u_long* s_Cluts[] = {s_EmptyClut};
 PfnEntityUpdate EntityUpdates[NUM_ENTITIES];
-static LayoutEntity s_EmptyLayout[] = {
-    {0xFFFE, 0xFFFE, E_NONE, 0, 0},
-    {0xFFFF, 0xFFFF, E_NONE, 0, 0},
-};
-LayoutEntity* entityLayoutHorizontal[64];
-LayoutEntity* entityLayoutVertical[64];
 
 static void UnsupportedEntity(Entity* entity) { DestroyEntity(entity); }
 static void Noop(void) {}
-static void InitRoomEntitiesDummy(s32 layoutId) { (void)layoutId; }
 void EntityExplosion(Entity* entity) { UnsupportedEntity(entity); }
 void EntityPrizeDrop(Entity* entity) { UnsupportedEntity(entity); }
 void EntityEquipItemDrop(Entity* entity) { UnsupportedEntity(entity); }
+void EntityRedDoor(Entity* entity);
+void EntityRoomForeground(Entity* entity);
 
 OVL_API void InitStage(Overlay* o) {
     s32 i;
@@ -38,21 +38,18 @@ OVL_API void InitStage(Overlay* o) {
     for (i = 0; i < LEN(EntityUpdates); i++) {
         EntityUpdates[i] = UnsupportedEntity;
     }
-    for (i = 0; i < LEN(entityLayoutHorizontal); i++) {
-        entityLayoutHorizontal[i] = s_EmptyLayout;
-        entityLayoutVertical[i] = s_EmptyLayout;
-    }
-
-    o->Update = Noop;
+    EntityUpdates[E_RED_DOOR - 1] = EntityRedDoor;
+    EntityUpdates[E_ROOM_FOREGROUND - 1] = EntityRoomForeground;
+    o->Update = Update;
     o->HitDetection = Noop;
-    o->UpdateRoomPosition = Noop;
-    o->InitRoomEntities = InitRoomEntitiesDummy;
+    o->UpdateRoomPosition = UpdateRoomPosition;
+    o->InitRoomEntities = InitRoomEntities;
+    o->UpdateStageEntities = UpdateStageEntities;
     o->rooms = rooms;
     o->spriteBanks = s_SpriteBanks;
     o->cluts = s_Cluts;
     o->tileLayers = rooms_layers;
     o->gfxBanks = gfxBanks;
-    o->UpdateStageEntities = Noop;
     PfnEntityUpdates = EntityUpdates;
     g_pStObjLayoutHorizontal = entityLayoutHorizontal;
     g_pStObjLayoutVertical = entityLayoutVertical;
