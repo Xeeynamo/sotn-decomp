e3a082e24ab3337a1949452bfe9cd8338d35faf6
Joey Murphy <tjmurphy@protonmail.com>
2026-08-30 09:00:49 -0400

    split for psp

--- src/st/rnz1_psp/unk_17168.c
+++ src/st/rnz1_psp/unk_17168.c
@@ -16,15 +16,3 @@ INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", func_pspeu_0924F610);
 INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", func_pspeu_0924F980);
 
 INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", func_us_801ABDE4);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", func_801CDC80);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", func_pspeu_092508B8);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", func_us_801A9DB8);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", EntityFrozenShadeCrystal);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", func_us_801AAF00);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_17168", func_us_801AB04C);
---
+++ src/st/rnz1_psp/unk_19150.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_19150", func_801CDC80);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_19150", func_pspeu_092508B8);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_19150", func_us_801A9DB8);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_19150", EntityFrozenShadeCrystal);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_19150", func_us_801AAF00);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/unk_19150", func_us_801AB04C);
