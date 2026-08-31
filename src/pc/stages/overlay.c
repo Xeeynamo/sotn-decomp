7870670fd737edb097da14051d10b9ee3740fc5a
sozud <122322823+sozud@users.noreply.github.com>
2026-08-31 13:47:28 +0700

    pc: support custom stage layer renderers

--- src/pc/stages/overlay.c
+++ src/pc/stages/overlay.c
@@ -135,6 +135,7 @@ bool LoadStageOverlay(const char* name, Overlay* o) {
     PfnInitStage entrypoint;
 
     if (CurrentStageOverlay) {
+        g_PcStageLayerRenderer = NULL;
         OvlClose(CurrentStageOverlay);
         CurrentStageOverlay = NULL;
     }
