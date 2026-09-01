7f9139ab5da9b3835929d9724a6b16e9c3f2edea
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-09-01 08:45:58 +0100

    saturn fix

--- src/saturn/sattypes.h
+++ src/saturn/sattypes.h
@@ -1385,6 +1385,7 @@ typedef enum {
     EFFECT_UNK_8000 = 1 << 15,
 
     // Aggregate helpers below:
+    EFFECT_FULL_SOLID = EFFECT_SOLID | EFFECT_SIDE,
     EFFECT_NOTHROUGH = EFFECT_SOLID | EFFECT_SINK,
     EFFECT_NOTHROUGH_PLUS = EFFECT_FULL_SOLID | EFFECT_SINK,
     // Should be renamed once we know what 8000 and 4000 are
