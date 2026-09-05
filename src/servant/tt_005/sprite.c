bd4e7429ff1f3e7f48db97d81a6c3b42fa5a6205
flafmg <flaafmg@gmail.com>
2026-09-05 01:17:13 -0300

    adds tt_005 and tt_006 overlays for psx

---
+++ src/servant/tt_005/sprite.c
@@ -0,0 +1,72 @@
+#include "common.h"
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", SetAnimationFrame);
+
+void unused_39C8(void) {
+}
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", ExecuteAbilityInitialize);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", CreateEventEntity_Local);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", SelectAnimationFrame);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", CheckForValidAbility);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", TT_005_ServantInit);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", TT_005_UpdateServantDefault);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantUseLifeApple);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantUseHammer);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantUseUncurse);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantUseAntivenom);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantUseElementalResist);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantUsePotion);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantAdditionalInit);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateSubEntityWings);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantSitOnShoulder);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantOfferHint);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateEntitySetRoomSpecialState);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateSubEntityUseLifeApple);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateServantSfxPassthrough);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", UpdateSubEntityUseItem);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", ServantUpdateAnim);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", DestroyEntity);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", AccumulateTowardZero);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", SearchForEntityInRange);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", CalculateAngleToEntity);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", StepAngleTowards);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", CalculateDistance);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", ProcessSfxState);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", ProcessEvent);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", CreateEventEntity);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", IsMovementAllowed);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", CheckAllEntitiesValid);
+
+INCLUDE_ASM("servant/tt_005/nonmatchings/sprite", ServantUnk0);
---
+++ src/servant/tt_006/nosedevil.c
@@ -0,0 +1,82 @@
+#include "common.h"
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", SetAnimationFrame);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", FindValidTarget);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", CheckEntityValid);
+
+void unused_2DBC(void) {
+}
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", ExecuteAbilityInitialize);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", DestroyEntityPassthrough);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_80172EF8);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_80173348);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_801737F0);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_80173D14);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", TT_006_ServantInit);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", TT_006_UpdateServantDefault);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", UpdateServantBasicAttack);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", UpdateServantSpecialAttack);
+
+void unused_5800(void) {
+}
+
+void unused_5808(void) {
+}
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", UpdateServantPressSwitch);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", UpdateSubentitySwitch);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", UpdateServantAdditionalInit);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", UpdateServantSfxPassthrough);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", UpdateEventAttack);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_801765A0);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_80176814);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_80176C1C);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_801771B0);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", func_us_80177690);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", ServantUpdateAnim);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", DestroyEntity);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", AccumulateTowardZero);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", SearchForEntityInRange);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", CalculateAngleToEntity);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", StepAngleTowards);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", CalculateDistance);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", ProcessSfxState);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", ProcessEvent);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", CreateEventEntity);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", IsMovementAllowed);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", CheckAllEntitiesValid);
+
+INCLUDE_ASM("servant/tt_006/nonmatchings/nosedevil", ServantUnk0);
