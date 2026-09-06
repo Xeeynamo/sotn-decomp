a72988da2175b59876d211cf6a8db857499ab255
Joey Murphy <tjmurphy@protonmail.com>
2026-09-06 11:15:11 -0400

    psp fix particles split

--- src/st/rnz1_psp/e_valhalla_knight.c
+++ src/st/rnz1_psp/e_valhalla_knight.c
@@ -6,8 +6,3 @@ INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", EntityValhall
 INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_us_801C8954);
 
 INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_us_801C8AAC);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_psp_0923AD68);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_psp_0923B2F0);
-
