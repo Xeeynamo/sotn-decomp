f14704b3fd509056ba687dd1d4aeeeb9fed5f0d4
Joey Murphy <tjmurphy@protonmail.com>
2026-09-06 11:16:59 -0400

    PSP valhalla knight

--- src/st/rnz1_psp/e_valhalla_knight.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "../rnz1/rnz1.h"
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", EntityValhallaKnight);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_us_801C8954);
-
-INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_us_801C8AAC);
