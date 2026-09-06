72f9a44af2938222ff0f2b3963e515dcf7a31977
Joey Murphy <tjmurphy@protonmail.com>
2026-09-06 11:13:42 -0400

    Unsplit psp

--- src/st/rnz1_psp/e_valhalla_knight.c
+++ src/st/rnz1_psp/e_valhalla_knight.c
@@ -2,3 +2,12 @@
 #include "../rnz1/rnz1.h"
 
 INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", EntityValhallaKnight);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_us_801C8954);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_us_801C8AAC);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_psp_0923AD68);
+
+INCLUDE_ASM("st/rnz1_psp/nonmatchings/rnz1_psp/e_valhalla_knight", func_psp_0923B2F0);
+
