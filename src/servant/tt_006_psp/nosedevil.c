ec84b9ef8a179864a64b0856075bfb8d0a30f7b1
flaffy <flaafmg@gmail.com>
2026-09-06 05:32:03 -0300

    adding tt_005 and tt_006 overlays (#3655)

---
+++ src/servant/tt_006_psp/nosedevil.c
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E7680);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E76F0);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E7B08);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E7C10);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E7C20);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E7D90);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E7DB8);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E8460);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E8B40);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E92F8);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092E9ED0);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EA1D8);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EAE98);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EB4C0);
+
+void func_pspeu_092EBA30(void) {}
+
+void func_pspeu_092EBA38(void) {}
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EBA40);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EBF78);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EC120);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092ECD18);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092ECD40);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092ECD88);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092ED0E0);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092ED718);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EDEA0);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EE650);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EE720);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EEAB0);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EEB48);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EEC28);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EECC8);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EEDD8);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EF098);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EF4C8);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EF5B8);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EF750);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_pspeu_092EF838);
+
+INCLUDE_ASM("servant/tt_006_psp/nonmatchings/tt_006_psp/nosedevil", func_092EF908);
