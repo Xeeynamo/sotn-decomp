95ac0251a01b00049bccebf0d35df1df1e3fb1da
Joey Murphy <tjmurphy@protonmail.com>
2026-09-06 22:23:12 -0400

    resplit US

--- src/st/rnz1/unk_3BE58.c
+++ src/st/rnz1/unk_3BE58.c
@@ -14,15 +14,3 @@ INCLUDE_ASM("st/rnz1/nonmatchings/unk_3BE58", func_us_801BCD80);
 INCLUDE_ASM("st/rnz1/nonmatchings/unk_3BE58", func_us_801BCE4C);
 
 INCLUDE_ASM("st/rnz1/nonmatchings/unk_3BE58", func_us_801BCFC8);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_3BE58", func_us_801BD0EC);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_3BE58", func_us_801BD184);
-
-// Grindy crushy platform things.
-// params 0 = one-wide, params 1 = three-wide
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_3BE58", func_us_801BD324);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_3BE58", func_us_801BD398);
-
-INCLUDE_ASM("st/rnz1/nonmatchings/unk_3BE58", func_us_801BDA24);
