d0acd1c8e4ca1e9dab9bac8092d486bc5fd7f1b2
Joey Murphy <tjmurphy@protonmail.com>
2026-09-08 23:36:43 -0400

    format that new data thing

--- src/st/rnz1/e_bomb_knight.c
+++ src/st/rnz1/e_bomb_knight.c
@@ -121,9 +121,8 @@ static deathParts deathPartsData[] = {
 static s8 fuseSparksXY[] = {
     0,   0,   19,  -41, 18,  -43, 19, -45, 15, -49, -79, -91,
     -13, -37, -17, -49, -20, -43, 17, -46, 20, -45, 16,  -50};
-static u8 fuseFrameMap[] = {
-    0, 1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 6, 7, 8, 9, 10, 10, 0, 0, 0, 0, 0, 0, 11, 0
-};
+static u8 fuseFrameMap[] = {0, 1, 2,  3,  4, 0, 0, 0, 0, 0, 0,  6, 7,
+                            8, 9, 10, 10, 0, 0, 0, 0, 0, 0, 11, 0};
 
 extern EInit g_EInitBombKnight;
 extern EInit g_EInitRockKnight;
