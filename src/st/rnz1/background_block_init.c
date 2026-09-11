8e219b729372c43599bb82318ecf5c43f281f2c6
Joey Murphy <tjmurphy@protonmail.com>
2026-09-11 07:40:55 -0400

    US background block init

---
+++ src/st/rnz1/background_block_init.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "rnz1.h"
+
+static u8 D_us_80180CF0[] = {32, 1, -1, 0};
+static u8 D_us_80180CF4[] = {32, 2, -1, 0};
+static u8 D_us_80180CF8[] = {32, 3, -1, 0};
+static u8 D_us_80180CFC[] = {32, 4, -1, 0};
+static u8 D_us_80180D00[] = {32, 5, -1, 0};
+static u8 D_us_80180D04[] = {32, 6, -1, 0};
+
+ObjInit2 BackgroundBlockInit[] = {
+    {ANIMSET_OVL(4), 0x9F, 0x54, 0, 0xD, 0x0000, BLEND_NO, 0x00000000, D_us_80180CF0},
+    {ANIMSET_OVL(4), 0x9F, 0x54, 0, 0xD, 0x0000, BLEND_NO, 0x00000000, D_us_80180CF4},
+    {ANIMSET_OVL(4), 0x9F, 0x54, 0, 0xD, 0x0000, BLEND_NO, 0x00000000, D_us_80180CF8},
+    {ANIMSET_OVL(4), 0x9F, 0x54, 0, 0xD, 0x0000, BLEND_NO, 0x00000000, D_us_80180CFC},
+    {ANIMSET_OVL(4), 0x9F, 0x54, 0, 0xD, 0x0000, BLEND_NO, 0x00000000, D_us_80180D00},
+    {ANIMSET_OVL(4), 0x9F, 0x54, 0, 0xD, 0x0000, BLEND_NO, 0x00000000, D_us_80180D04},
+};
