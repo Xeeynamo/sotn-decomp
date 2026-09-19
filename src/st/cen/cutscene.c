9ca7cd5d32b22ddafcd59fee3fa2931eb6260306
Jonathan Hohle <jonhohle@gmail.com>
2026-09-19 09:47:05 -0700

    Remove `sotn_str` changes

--- src/st/cen/cutscene.c
+++ src/st/cen/cutscene.c
@@ -38,10 +38,14 @@ static const char _pad[4] = "";
 #else
 // clang-format off
 static u16 actor_names[] = {
-    _WC('ア'), _WC('ル'), _WC('カ'), _WC('ー'), _WC('ド'),
-    _WC('マ'), _WC('リ'), _WC('ア'), _WC('・'), _WC('ラ'), _WC('ー'), _WC('ネ'), _WC('ッ'), _WC('ド'),
-    _WC('シ'), _WC('ャ'), _WC('フ'), _WC('ト'),
-    _WC('サ'), _WC('キ'), _WC('ュ'), _WC('バ'), _WC('ス'),
+    // アルカード
+    0x71, 0x99, 0x76, 0x70, 0x8084,
+    // マリア・ラーネッド
+    0x8F, 0x98, 0x71, 0x65, 0x97, 0x70, 0x88, 0x6F, 0x8084,
+    // シャフト
+    0x7C, 0x6C, 0x8C, 0x84,
+    // サキュバス
+    0x7B, 0x77, 0x6D, 0x808A, 0x7D,
 };
 // clang-format on
 static u8 actor_prims[] = {6, 10, 4, 6};
