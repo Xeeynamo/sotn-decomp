6fe12546126ea941d3585a9b236f6562d44bcb98
Joey Murphy <tjmurphy@protonmail.com>
2026-09-20 16:52:46 -0400

    share another file

--- src/st/rno1/unk_29A8C.c
+++ src/st/rno1/unk_29A8C.c
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno1.h"
 
+#ifdef VERSION_PSP
+extern s32 E_ID(UNK_2E);
+#endif
+
 extern EInit g_EInitInteractable;
 void func_us_801A9A8C(Entity* self) {
     Entity* child;
@@ -54,5 +58,3 @@ void func_us_80198A18_from_rbo4(Entity* self) {
         return;
     }
 }
-
-INCLUDE_ASM("st/rno1/nonmatchings/unk_29930", func_us_801A9BEC);
---
+++ src/st/rno1/unk_29BEC.c
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#include "rno1.h"
+
+INCLUDE_ASM("st/rno1/nonmatchings/unk_29930", func_us_801A9BEC);
