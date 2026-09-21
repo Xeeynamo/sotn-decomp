96052c5599e0872310605003d3b9fcc37a432347
Joey Murphy <tjmurphy@protonmail.com>
2026-09-20 21:19:12 -0400

    Decomp orphan debug funcs, unused

--- src/st/rno1/unk_267A8.c
+++ src/st/rno1/unk_267A8.c
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno1.h"
 
-INCLUDE_ASM("st/rno1/nonmatchings/unk_267A8", RNO1_DebugShowWaitInfo);
+// Likely copied out of DRA, unused in RNO1
+static void DebugShowWaitInfo(const char* msg) {
+    static s32 g_DebugWaitInfoTimer;
 
-INCLUDE_ASM("st/rno1/nonmatchings/unk_267A8", RNO1_DebugInputWait);
+    g_CurrentBuffer = g_CurrentBuffer->next;
+    FntPrint(msg);
+    if (g_DebugWaitInfoTimer++ & 4) {
+        FntPrint("\no\n");
+    }
+    DrawSync(0);
+    VSync(0);
+    PutDrawEnv(&g_CurrentBuffer->draw);
+    PutDispEnv(&g_CurrentBuffer->disp);
+    FntFlush(-1);
+}
+
+static void DebugInputWait(const char* msg) {
+    while (PadRead(0))
+        DebugShowWaitInfo(msg);
+    while (!PadRead(0))
+        DebugShowWaitInfo(msg);
+}
