ebda68c34dbf6e4f96bafb9dcc9eba77df123da8
PancakeFriday <roebrd@gmail.com>
2026-09-24 13:35:36 +0200

    Some rno4 functions (#3683)

--- include/entity.h
+++ include/entity.h
@@ -4415,6 +4415,19 @@ typedef struct {
     /* 0x8C */ s32 bloodyTimer;
 } ET_Crusher;
 
+typedef struct {
+    /* 0x7C */ s8 pad7C[8];
+    /* 0x84 */ s16 unk84; //
+    /* 0x86 */ s8 pad86[2];
+    /* 0x88 */ u8 unk88; //
+} ET_801D5BA4;
+
+typedef struct {
+    /* 0x7C */ Primitive* prim;
+    /* 0x80 */ s8 pad80[1];
+    /* 0x84 */ s32 unk84;
+} ET_801C5C78;
+
 typedef union { // offset=0x7C
     struct Primitive* prim;
     ET_Placeholder ILLEGAL;
@@ -4790,6 +4803,8 @@ typedef union { // offset=0x7C
     ET_DarkwingBatWings batwing;
     ET_BombKnight bombKnight;
     ET_Crusher crusher;
+    ET_801D5BA4 et_801D5BA4;
+    ET_801C5C78 et_801C5C78;
 } Ext;
 
 SYNC_FIELD(ET_Player, ET_Weapon, anim);
--- src/st/rno4_psp/unk_18CC0.c
+++ src/st/rno4_psp/unk_18CC0.c
@@ -5,4 +5,59 @@ INCLUDE_ASM("st/rno4_psp/nonmatchings/rno4_psp/unk_18CC0", func_us_801D511C);
 
 INCLUDE_ASM("st/rno4_psp/nonmatchings/rno4_psp/unk_18CC0", func_us_801D58FC);
 
-INCLUDE_ASM("st/rno4_psp/nonmatchings/rno4_psp/unk_18CC0", func_us_801D5BA4);
+// https://www.decomp.me/scratch/85Slq
+
+extern u16 D_us_80180B54;
+extern s16 D_us_801822DC;
+
+void func_us_801D5BA4(Entity* self) {
+    Entity* ent;
+
+    switch (self->step) {
+    case 0:
+        InitializeEntity(&D_us_80180B54);
+        self->drawFlags |= ENTITY_SCALEX | ENTITY_SCALEY | ENTITY_ROTATE;
+        self->drawFlags |= ENTITY_OPACITY;
+        self->blendMode = BLEND_SUB | BLEND_TRANSP;
+        self->opacity = 0x40;
+        self->scaleX = 0x10;
+        self->scaleY = 0x40;
+        ent = g_Entities;
+        self->zPriority = ent->zPriority + 1;
+        self->rotate = self->ext.et_801D5BA4.unk84;
+        self->ext.et_801D5BA4.unk88 = 0x30;
+        self->step_s = 0;
+        return;
+
+    case 1:
+        UnkCollisionFunc2(&D_us_801822DC);
+        self->velocityX =
+            rsin(self->ext.et_801D5BA4.unk84) * self->ext.et_801D5BA4.unk88;
+        self->velocityY =
+            -rcos(self->ext.et_801D5BA4.unk84) * self->ext.et_801D5BA4.unk88;
+        if (self->ext.et_801D5BA4.unk88 > 12) {
+            self->ext.et_801D5BA4.unk88 -= 2;
+        }
+        switch (self->step_s) {
+        case 0:
+            self->scaleX += 8;
+            self->scaleY += 0x10;
+            if (self->scaleX == 0x100) {
+                self->scaleY = 0x100;
+                self->ext.et_801D5BA4.unk88 = 4;
+                self->hitboxState = 0;
+                self->step_s += 1;
+                return;
+            }
+            break;
+
+        case 1:
+            self->scaleX += 8;
+            self->scaleY += 8;
+            self->opacity -= 2;
+            if (!self->opacity) {
+                DestroyEntity(self);
+            }
+        }
+    }
+}
